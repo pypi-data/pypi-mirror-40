483c0e778eb1271686f51c5a4374223674e3284e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package pymitv/1.4.1/pymitv-1.4.1-py3-none-any.whl

---
+++ packages/pymitv/pymitv-1.4.1-py3-none-any.whl/pymitv-1.4.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,39 @@
+pymitv
+======
+
+A Python 3 based control of the Mi Tv 3(s)
+
+.. figure:: https://9to5google.files.wordpress.com/2016/03/xiaomi-mi-tv-3s.jpg?quality=82&w=1600&h=1000
+   :alt: The Mi TV 3s 65-inch
+
+The Mi TV 3s 65-inch
+
+Introduction
+------------
+
+This package was developed to interface with the Xiaomi TV 3 series
+through their local HTTP API using Python. The package has both the
+ability to discover TVs and control them. The TV lineup in question is
+this `one.`_ It should be noted, that all the TV logic and hardware is
+in the soundbar. Thus, if you have a soundbar that looks identical to
+the one in the picture, you should be golden with this library.
+
+Supported models
+''''''''''''''''
+
+- Mi TV 3s (all sizes)
+
+Not sure if supported models
+''''''''''''''''''''''''''''
+
+- Mi TV 4A (all sizes)
+
+
+Documentation
+-------------
+You can find the documentation on Github here_.
+
+
+.. _one.: http://www.mi.com/en/mitv3s/65/
+.. _here: https://github.com/fattdev/pymitv
+
---
+++ packages/pymitv/pymitv-1.4.1-py3-none-any.whl/pymitv/tv.py
@@ -0,0 +1,107 @@
+"""Contains the class for interfacing with the TV."""
+from pymitv import Control
+from pymitv import Navigator
+
+
+class TV:
+    """A virtual representation of the TV that stores state, and takes controls."""
+    ip = None
+    state = True
+    source = None
+
+    def __init__(self, ip=None, source=None, initialized=True):
+        # Check if an IP address has been supplied to the constructor
+        if ip is None:
+            print('No TV supplied, hence it won\'t work')
+
+        # Check if TV has been initialized for pymitv control
+        if initialized is False:
+            print('If the TV hasn\'t been setup for full pymitv control, \
+using any of the polyfill controls, could produce weird results.')
+
+        # Make IP address global regardless of value
+        self.ip = ip
+
+        # Make active source global
+        self.source = source
+
+    def _send_keystroke(self, keystroke, wait=False):
+        # Check if an IP address has been supplied, if it hasn't return false.
+        if self.ip is None:
+            return False
+
+        # Make sure the TV is not already on, and then send keystroke
+        return Control().send_keystrokes(self.ip, keystroke, wait)
+
+        # Send True regardless of whether or not command was sent
+        #return True
+
+    @property
+    def is_on(self):
+        """Returns the assumed state of the TV."""
+        return self.state
+
+    def wake(self):
+        """Wakes up the TV from sleep."""
+        return self._send_keystroke(Control.wake)
+
+    def sleep(self):
+        """Puts the TV to sleep."""
+        return self._send_keystroke(Control.sleep)
+
+    def turn_off(self):
+        """Turns off the TV completely."""
+        return self._send_keystroke(Control.turn_off)
+
+    def enter(self):
+        """Presses enter to affirm."""
+        return self._send_keystroke(Control.enter)
+
+    def menu(self):
+        """Opens the menu."""
+        return self._send_keystroke(Control.menu)
+
+    def home(self):
+        """Goes home."""
+        return self._send_keystroke(Control.home)
+
+    def back(self):
+        """Goes back."""
+        return self._send_keystroke(Control.back)
+
+    def up(self):
+        """Presses up key."""
+        return self._send_keystroke(Control.up)
+
+    def down(self):
+        """Presses down key."""
+        return self._send_keystroke(Control.down)
+
+    def left(self):
+        """Presses left key."""
+        return self._send_keystroke(Control.left)
+
+    def right(self):
+        """Presses right key."""
+        return self._send_keystroke(Control.right)
+
+    def volume_up(self):
+        """Turns up the volume by one."""
+        return self._send_keystroke(Control.volume_up)
+
+    def volume_down(self):
+        """Turns down the volume by one."""
+        return self._send_keystroke(Control.volume_down)
+
+    def mute(self):
+        """Mutes the TV."""
+        return Control().mute(self.ip)
+
+    def set_source(self, source):
+        """Selects and saves source."""
+        route = Navigator(source=self.source).navigate_to_source(source)
+
+        # Save new source
+        self.source = source
+
+        return self._send_keystroke(route, wait=True)
