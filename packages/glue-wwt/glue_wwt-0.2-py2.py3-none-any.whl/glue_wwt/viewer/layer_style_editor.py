e67f5537c204a01d094fbf4fe8e966524bb44bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:32 +0000

    Add package glue-wwt/0.2/glue_wwt-0.2-py2.py3-none-any.whl

---
+++ packages/glue-wwt/glue_wwt-0.2-py2.py3-none-any.whl/glue_wwt/viewer/layer_style_editor.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+
+from qtpy import QtWidgets
+
+from glue.external.echo.qt import autoconnect_callbacks_to_qt
+from glue.utils.qt import load_ui
+
+
+class WWTLayerStyleEditor(QtWidgets.QWidget):
+
+    def __init__(self, layer, parent=None):
+
+        super(WWTLayerStyleEditor, self).__init__(parent=parent)
+
+        self.ui = load_ui('layer_style_editor.ui', self,
+                          directory=os.path.dirname(__file__))
+
+        connect_kwargs = {'alpha': dict(value_range=(0, 1))}
+
+        autoconnect_callbacks_to_qt(layer.state, self.ui, connect_kwargs)
+
+        self.ui.button_center.clicked.connect(layer.center)
---
+++ packages/glue-wwt/glue_wwt-0.2-py2.py3-none-any.whl/glue_wwt/viewer/options_widget.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+
+from qtpy import QtWidgets
+from glue.utils.qt import load_ui
+from glue.external.echo.qt import autoconnect_callbacks_to_qt
+
+__all__ = ['WWTOptionPanel']
+
+
+class WWTOptionPanel(QtWidgets.QWidget):
+
+    def __init__(self, viewer_state, session=None, parent=None):
+
+        super(WWTOptionPanel, self).__init__(parent=parent)
+
+        self._viewer_state = viewer_state
+
+        self.ui = load_ui('options_widget.ui', self,
+                          directory=os.path.dirname(__file__))
+
+        connect_kwargs = {'foreground_opacity': dict(value_range=(0, 1))}
+
+        autoconnect_callbacks_to_qt(self._viewer_state, self.ui, connect_kwargs)
