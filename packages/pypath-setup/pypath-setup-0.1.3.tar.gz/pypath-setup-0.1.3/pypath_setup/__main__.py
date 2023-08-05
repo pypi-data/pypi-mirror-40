e27db791c85859328af5a21fceaa9da3ccaff9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:29 +0000

    Add package pypath-setup/0.1.3/pypath-setup-0.1.3.tar.gz

---
+++ packages/pypath-setup/pypath-setup-0.1.3.tar.gz/pypath-setup-0.1.3/pypath_setup/__main__.py
@@ -0,0 +1,4 @@
+from pypath_setup import main
+
+if __name__ == '__main__':
+    main()
---
+++ packages/pypath-setup/pypath-setup-0.1.3.tar.gz/pypath-setup-0.1.3/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['pypath_setup']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['pypath-setup = pypath_setup:main']}
+
+setup_kwargs = {
+    'name': 'pypath-setup',
+    'version': '0.1.3',
+    'description': 'Start processes by setting PYTHONPATH to point to some virtualenv',
+    'long_description': None,
+    'author': 'Paul Maréchal',
+    'author_email': 'marechap.info@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
