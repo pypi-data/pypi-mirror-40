9cb1f4a1c6bfc7dcca058965d2b1ca1c748da350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:56 +0000

    Add package webargs/4.4.0/webargs-4.4.0-py2.py3-none-any.whl

---
+++ packages/webargs/webargs-4.4.0-py2.py3-none-any.whl/webargs/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from marshmallow.utils import missing
+
+# Make marshmallow's validation functions importable from webargs
+from marshmallow import validate
+
+from webargs.core import argmap2schema, dict2schema, WebargsError, ValidationError
+from webargs import fields
+
+__version__ = "4.4.0"
+__author__ = "Steven Loria"
+__license__ = "MIT"
+
+
+__all__ = (
+    "dict2schema",
+    "argmap2schema",
+    "WebargsError",
+    "ValidationError",
+    "fields",
+    "missing",
+    "validate",
+)
