dc5f5fae304fc62ce1e498c257b35d4b6137cab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package gameserver-response-parser/0.1/gameserver_response_parser-0.1.tar.gz

---
+++ packages/gameserver-response-parser/gameserver_response_parser-0.1.tar.gz/gameserver_response_parser-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="gameserver_response_parser",
+        version="0.1",
+        description="a library to parse the responses of gameservers",
+        url="https://github.com/991jo/gameserver_response_parser",
+        author="992jo",
+        license="WTF",
+        packages=["gameserver_response_parser"],
+        test_suite="gameserver_response_parser.test.srcds_new_format")
