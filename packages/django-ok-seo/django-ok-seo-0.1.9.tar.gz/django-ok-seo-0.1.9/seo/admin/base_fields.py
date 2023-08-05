7bb4d1ef9a659d022167a5445a491af3cffb7638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package django-ok-seo/0.1.9/django-ok-seo-0.1.9.tar.gz

---
+++ packages/django-ok-seo/django-ok-seo-0.1.9.tar.gz/django-ok-seo-0.1.9/seo/admin/base_fields.py
@@ -0,0 +1,19 @@
+__all__ = (
+    'base_seo_fields',
+)
+
+
+base_seo_fields = (
+    'object_type',
+    'twitter_type',
+    'index',
+    'follow',
+    'title',
+    'keywords',
+    'description',
+    'image', 
+    'alt',
+    'h1',
+    'top_text',
+    'bottom_text'
+)
