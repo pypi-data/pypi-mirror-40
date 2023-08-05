223d29afe7b76d2df9eae25b7deff831ec8a7192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package utmcon/0.30/utmcon-0.30.tar.gz

---
+++ packages/utmcon/utmcon-0.30.tar.gz/utmcon-0.30/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utmcon',
+    version='0.30',
+    description='Convert UTM to lat long.',
+    url='http://github.com/rbutus/utmcon',
+    author='Ryan Trigg',
+    author_email='rbutustree@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    )
+
