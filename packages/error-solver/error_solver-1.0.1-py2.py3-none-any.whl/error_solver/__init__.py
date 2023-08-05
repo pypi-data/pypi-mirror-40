b2f1445c86d6c483ecd6acd0be299b2b5ad02df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package error-solver/1.0.1/error_solver-1.0.1-py2.py3-none-any.whl

---
+++ packages/error-solver/error_solver-1.0.1-py2.py3-none-any.whl/error_solver/__init__.py
@@ -0,0 +1,20 @@
+"""
+===================================
+Error Solver (:mod:`.error_solver`)
+===================================
+
+The below classes are available for error propagation approximation.
+
+.. autosummary::
+    :toctree: generated/
+
+    ErrorSolver
+    ErrorSolverPy
+"""
+
+try:
+    from .error_solver import *
+except:
+    pass
+
+from .error_solver_py import *
