e9bc57b75e5487430162b51bfe47f59c555ce026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package NuO/0.1.1/NuO-0.1.1.tar.gz

---
+++ packages/NuO/NuO-0.1.1.tar.gz/NuO-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import codecs
+
+setup (
+
+    name = "NuO",
+    version = "0.1.1",
+    author = "Akshit Grover",
+    author_email = "akshit.grover2016@gmail.com",
+    description = "Static website templating engine, Generates HTML pages using .nuo template files and data in JSON files.",
+    packages = ["."],
+    long_description = open("README.rst").read(),
+    scripts = ["nuo"],
+    license = "LICENSE",
+    url = "https://github.com/akshitgrover/NuO",
+    keywords = "static html website parser nuo",
+    classifiers = [
+
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+    
+    ]
+
+)
