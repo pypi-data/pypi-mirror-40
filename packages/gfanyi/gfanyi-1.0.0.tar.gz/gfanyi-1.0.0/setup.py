b59e90f3a1f918a3209e1213a678bc9a28be29d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:41 +0000

    Add package gfanyi/1.0.0/gfanyi-1.0.0.tar.gz

---
+++ packages/gfanyi/gfanyi-1.0.0.tar.gz/gfanyi-1.0.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='gfanyi',
+    version='1.0.0',
+    packages=find_packages(),
+    requires=['requests','execjs','html'],
+    url='https://github.com/xujinjie151/gfanyi',
+    license='',
+    author='xujinjie151',
+    author_email='xujinjie151@gmail.com',
+    description='G翻译'
+)
+
+
+
