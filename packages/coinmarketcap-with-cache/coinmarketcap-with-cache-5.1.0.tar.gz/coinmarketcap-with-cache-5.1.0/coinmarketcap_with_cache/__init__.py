4b59c34c5961930449dc17918ffda0ccc1480dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:32 +0000

    Add package coinmarketcap-with-cache/5.1.0/coinmarketcap-with-cache-5.1.0.tar.gz

---
+++ packages/coinmarketcap-with-cache/coinmarketcap-with-cache-5.1.0.tar.gz/coinmarketcap-with-cache-5.1.0/coinmarketcap_with_cache/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__title__ = 'coinmarketcap-with-cache'
+__version__ = '5.1.0'
+
+# original author / url / license
+__author__ = 'Martin Simon <me@martinsimon.me>'
+__repo__ = 'https://github.com/barnumbirr/coinmarketcap'
+__license__ = 'Apache v2.0 License'
+
+from .core import Market
