e3b89589aa00fbe7a2b3d32231b49da89e6a9d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:00 +0000

    Add package facebookHandler/0.1/facebookHandler-0.1.tar.gz

---
+++ packages/facebookHandler/facebookHandler-0.1.tar.gz/facebookHandler-0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'facebookHandler',
+    packages = ['facebookHandler'],
+    version = '0.1',
+    license = 'MIT',
+    description = 'A python library made with selenium to control a facebook account \
+                without the use of the offical API',
+    author = 'Ahmed Tounsi',
+    author_email = 'ahmeddottounsi@gmail.com',
+    url = 'https://github.com/ahmed-tounsi',
+    download_url = 'https://github.com/ahmed-tounsi/facebookHandler/archive/0.1.tar.gz',
+    keywords = ['facebook', 'selenium', 'facebook-bot'],
+    install_requires =[
+        'selenium',
+    ],
+    classifiers =[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
