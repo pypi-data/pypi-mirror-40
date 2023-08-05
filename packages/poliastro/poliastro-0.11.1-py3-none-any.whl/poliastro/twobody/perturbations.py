83523cf74c6eaa20dd05d9ce0918d341454a3379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:21 +0000

    Add package poliastro/0.11.1/poliastro-0.11.1-py3-none-any.whl

---
+++ packages/poliastro/poliastro-0.11.1-py3-none-any.whl/poliastro/twobody/perturbations.py
@@ -0,0 +1,78 @@
+import numpy as np
+from poliastro.util import norm
+
+
+def J2_perturbation(t0, state, k, J2, R):
+    """Calculates J2_perturbation acceleration (km/s2)
+
+    .. versionadded:: 0.9.0
+
+    Parameters
+    ----------
+    t0 : float
+        Current time (s)
+    state : numpy.ndarray
+        Six component state vector [x, y, z, vx, vy, vz] (km, km/s).
+    k : float
+        gravitational constant, (km^3/s^2)
+    J2: float
+        obliteness factor
+    R: float
+        attractor radius
+
+    Notes
+    -----
+    The J2 accounts for the obliteness of the attractor. The formula is given in
+    Howard Curtis, (12.30)
+
+    """
+    r_vec = state[:3]
+    r = norm(r_vec)
+
+    factor = (3.0 / 2.0) * k * J2 * (R ** 2) / (r ** 5)
+
+    a_x = 5.0 * r_vec[2] ** 2 / r ** 2 - 1
+    a_y = 5.0 * r_vec[2] ** 2 / r ** 2 - 1
+    a_z = 5.0 * r_vec[2] ** 2 / r ** 2 - 3
+    return np.array([a_x, a_y, a_z]) * r_vec * factor
+
+
+def atmospheric_drag(t0, state, k, R, C_D, A, m, H0, rho0):
+    """Calculates atmospheric drag acceleration (km/s2)
+
+    .. versionadded:: 0.9.0
+
+    Parameters
+    ----------
+    t0 : float
+        Current time (s)
+    state : numpy.ndarray
+        Six component state vector [x, y, z, vx, vy, vz] (km, km/s).
+    k : float
+        gravitational constant, (km^3/s^2)
+    C_D: float
+        dimensionless drag coefficient ()
+    A: float
+        frontal area of the spacecraft (km^2)
+    m: float
+        mass of the spacecraft (kg)
+    H0 : float
+        atmospheric scale height, (km)
+    rho0: float
+        the exponent density pre-factor, (kg / m^3)
+
+    Notes
+    -----
+    This function provides the acceleration due to atmospheric drag. We follow
+    Howard Curtis, section 12.4
+    the atmospheric density model is rho(H) = rho0 x exp(-H / H0)
+
+    """
+    H = norm(state[:3])
+
+    v_vec = state[3:]
+    v = norm(v_vec)
+    B = C_D * A / m
+    rho = rho0 * np.exp(-(H - R) / H0)
+
+    return -(1.0 / 2.0) * rho * B * v * v_vec
