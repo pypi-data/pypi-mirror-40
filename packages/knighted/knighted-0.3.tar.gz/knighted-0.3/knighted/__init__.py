3c8d8f527e64fd6256935cee3c1117d448daf5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package knighted/0.3/knighted-0.3.tar.gz

---
+++ packages/knighted/knighted-0.3.tar.gz/knighted-0.3/knighted/__init__.py
@@ -0,0 +1,8 @@
+from .bases import Injector, annotate, attr, current_injector
+from ._version import get_versions
+
+
+__version__ = get_versions()["version"]
+del get_versions
+
+__all__ = ["__version__", "Injector", "annotate", "attr", "current_injector"]
