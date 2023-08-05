66b3baf003ad74fb69df8a8abdbf3710ff37a405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:52 +0000

    Add package mtg-proxyprint/1.0.2/mtg-proxyprint-1.0.2.tar.gz

---
+++ packages/mtg-proxyprint/mtg-proxyprint-1.0.2.tar.gz/mtg-proxyprint-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+setup(
+    name='mtg-proxyprint',
+    version='1.0.2',
+    packages=['proxyprint',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='Simple command line tool to create MTG deck proxies',
+    long_description=open('README.md').read(),
+    url='https://gitlab.com/snapcaster/proxyprinter',
+    author='Michael Sobczak',
+    author_email='michaelsobczak54@gmail.com',
+    entry_points={
+        'console_scripts': [
+            'proxyprint=proxyprint.print_proxy:main'
+        ]
+    },
+    install_requires=parse_requirements('requirements.txt')
+)
