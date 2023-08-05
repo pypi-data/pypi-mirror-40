1d0d643ab01d5badebffb56a18a52121a8fd95a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:17:59 +0000

    Add package github-release-notifier/0.3.0/github_release_notifier-0.3.0.tar.gz

---
+++ packages/github-release-notifier/github_release_notifier-0.3.0.tar.gz/github_release_notifier-0.3.0/github_release_notifier/cli.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import github_release_notifier
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--action",
+        '-a',
+        default='cron',
+        choices=['cron', 'subscribe', 'unsubscribe'],
+        help="Action to do (default: cron)"
+    )
+    parser.add_argument(
+        "--package",
+        '-p',
+        help="Github package name / url (required for subscribe/unsubscribe) - prints uuid on subscription"
+    )
+    parser.add_argument(
+        "--webhook",
+        '-w',
+        help="URL to your webhook (required for subscribe/unsubscribe)"
+    )
+    parser.add_argument(
+        "--uuid",
+        '-u',
+        help="UUID of your webhook (required for unsubscribe)"
+    )
+    args = parser.parse_args()
+    if args.action == 'cron':
+        print(github_release_notifier.notifier.run())
+    if args.action == 'subscribe':
+        print(github_release_notifier.webhook.subscribe(args.package, args.webhook))
+    if args.action == 'unsubscribe':
+        github_release_notifier.webhook.unsubscribe(args.uuid, args.package, args.webhook)
+
+
+if __name__ == "__main__":
+    main()
