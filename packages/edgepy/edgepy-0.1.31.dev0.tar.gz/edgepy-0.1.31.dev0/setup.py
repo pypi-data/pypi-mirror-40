5f130c81e1a607ce82ab787849902d34fc476880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:15 +0000

    Add package edgepy/0.1.31.dev0/edgepy-0.1.31.dev0.tar.gz

---
+++ packages/edgepy/edgepy-0.1.31.dev0.tar.gz/edgepy-0.1.31.dev0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+DATA = [
+    'docs/counts/*',
+    'docs/genelib/*',
+    'docs/info/*',
+    'docs/plots/*',
+    'docs/results/*',
+    'docs/rfiles/*',
+    'docs/trimmed_results/*',
+    'docs/workspace/*',
+]
+
+
+REQUIRES = [
+    'biopython>=1.71',
+    'numpy',
+    'pandas',
+    'bcbio-gff',
+]
+
+
+setup(
+    name='edgepy',
+    version='0.1.31dev0',
+    author='Alex Summers',
+    author_email='asummers.edgepy@gmail.com',
+    packages=find_packages(),
+    url='http://pypi.python.org/pypi/edgepy/',
+    license='LICENSE.txt',
+    long_description=open('README.txt').read(),
+    install_requires=REQUIRES,
+    include_package_data=True,
+    package_data={'edgepy': DATA},
+)
