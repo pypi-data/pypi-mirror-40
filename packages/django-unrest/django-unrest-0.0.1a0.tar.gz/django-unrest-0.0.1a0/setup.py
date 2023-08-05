03365bbd452067642e1cfcc44fe33f3765f7e40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:20 +0000

    Add package django-unrest/0.0.1a0/django-unrest-0.0.1a0.tar.gz

---
+++ packages/django-unrest/django-unrest-0.0.1a0.tar.gz/django-unrest-0.0.1a0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+
+setup(
+  name = 'django-unrest',
+  packages = ['unrest'],
+  version = '0.0.1a',
+  description = 'A collection of tools for django',
+  long_description="", #long_description,
+  long_description_content_type="text/markdown",
+  author = 'Chris Cauley',
+  author_email = 'chris@lablackey.com',
+  url = 'https://github.com/chriscauley/django-unrest',
+  keywords = ['utils','django'],
+  license = 'MIT',
+  include_package_data = True,
+)
