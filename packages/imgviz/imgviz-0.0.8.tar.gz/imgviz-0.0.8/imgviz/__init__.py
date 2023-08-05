7f70cc55b2dafbc11ce20071316d9687b4c9ee04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:29 +0000

    Add package imgviz/0.0.8/imgviz-0.0.8.tar.gz

---
+++ packages/imgviz/imgviz-0.0.8.tar.gz/imgviz-0.0.8/imgviz/__init__.py
@@ -0,0 +1,18 @@
+# flake8: noqa
+
+from . import data
+
+# -----------------------------------------------------------------------------
+
+from .centerize import centerize
+
+from .color import rgb2gray
+from .color import gray2rgb
+
+from .colorize import depth2rgb
+
+from .draw import rectangle
+
+from .resize import resize
+
+from .tile import tile
---
+++ packages/imgviz/imgviz-0.0.8.tar.gz/imgviz-0.0.8/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import distutils.spawn
+from setuptools import find_packages
+from setuptools import setup
+import shlex
+import subprocess
+import sys
+
+
+version = '0.0.8'
+
+
+if sys.argv[1] == 'release':
+    if not distutils.spawn.find_executable('twine'):
+        print(
+            'Please install twine:\n\n\tpip install twine\n',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    commands = [
+        'git tag v{:s}'.format(version),
+        'git push origin master --tag',
+        'python setup.py sdist',
+        'twine upload dist/imgviz-{:s}.tar.gz'.format(version),
+    ]
+    for cmd in commands:
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
+
+install_requires = []
+with open('requirements.txt') as f:
+    for req in f:
+        install_requires.append(req.strip())
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setup(
+    name='imgviz',
+    version=version,
+    packages=find_packages(),
+    install_requires=install_requires,
+    description='Image Visualization Tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_data={'imgviz': ['data/*']},
+    include_package_data=True,
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    url='http://github.com/wkentaro/imgviz',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
