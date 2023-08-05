2db7f74c4210767d36944e0c0fe81ce144c30445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:14 +0000

    Add package polyswarm-client/1.0/polyswarm_client-1.0-py3-none-any.whl

---
+++ packages/polyswarm-client/polyswarm_client-1.0-py3-none-any.whl/ambassador/eicar.py
@@ -0,0 +1,44 @@
+import base64
+import logging
+import random
+
+from polyswarmclient.abstractambassador import AbstractAmbassador
+
+logger = logging.getLogger(__name__)  # Initialize logger
+
+EICAR = base64.b64decode(
+    b'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo=')
+NOT_EICAR = 'this is not malicious'
+ARTIFACTS = [('eicar', EICAR), ('not_eicar', NOT_EICAR)]
+
+
+class Ambassador(AbstractAmbassador):
+    """Ambassador which submits the EICAR test file"""
+
+    async def next_bounty(self, chain):
+        """Submit either the EICAR test string or a benign sample
+
+        Args:
+            chain (str): Chain sample is being requested from
+        Returns:
+            (int, str, int): Tuple of amount, ipfs_uri, duration, None to terminate submission
+
+        Note:
+            | The meaning of the return types are as follows:
+            |   - **amount** (*int*): Amount to place this bounty for
+            |   - **ipfs_uri** (*str*): IPFS URI of the artifact to post
+            |   - **duration** (*int*): Duration of the bounty in blocks
+        """
+        amount = self.client.bounties.parameters[chain]['bounty_amount_minimum']
+        filename, content = random.choice(ARTIFACTS)
+        duration = 20
+
+        logger.info('Submitting %s', filename)
+        ipfs_uri = await self.client.post_artifacts([(filename, content)])
+        if not ipfs_uri:
+            logger.error('Could not submit artifact to IPFS')
+            self.client.exit_code = 1
+            self.client.stop()
+            return None
+
+        return amount, ipfs_uri, duration
