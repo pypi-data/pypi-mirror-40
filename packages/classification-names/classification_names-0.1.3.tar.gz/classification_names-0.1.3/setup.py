633790a4bcf7cc6aa63c00f058222f638f510dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:04 +0000

    Add package classification-names/0.1.3/classification_names-0.1.3.tar.gz

---
+++ packages/classification-names/classification_names-0.1.3.tar.gz/classification_names-0.1.3/setup.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: it-wqll
+# Mail: wangqiang6480@163.com
+# Created Time:  2018-4-16 19:17:34
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name="classification_names",  # 这里是pip项目发布的名称
+    version="0.1.3",  # 版本号，数值大的会优先被pip
+    keywords=("pip", "classification_names"),
+    description="人名,公司或机构名称分类",
+    long_description="",
+    license="MIT Licence",
+
+    url="https://gitlab.kingdomai.com/wangqiang/name_classification/wangqiang/classification_names",  # 项目相关文件地址，一般是github
+    author="it-wqll",
+    author_email="wangqiang6480@163.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["numpy==1.15.4", "scikit_learn==0.20.2", "atlas==0.27.0"]  # 这个项目需要的第三方库
+)
