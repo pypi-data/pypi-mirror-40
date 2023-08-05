88d6db44e7376fa9dc7113607eb05f50ccec5d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:17 +0000

    Add package pyextras/0.0.6/pyextras-0.0.6.tar.gz

---
+++ packages/pyextras/pyextras-0.0.6.tar.gz/pyextras-0.0.6/pyextras/cache/errors.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+# Basic Cache Errors
+class CacheError(Exception):
+    pass
+
+class CacheKeyError(CacheError):
+    pass
+
+class CacheDatetimeError(CacheError):
+    pass
+
+
+# DiskCache Errors
+class DiskCacheError(CacheError):
+    pass
+
+class DiskCacheNotAFileError(DiskCacheError):
+    pass
+
+class DiskCacheFileNotFoundError(DiskCacheError):
+    pass
+
+class DiskCacheDirectoryNotFoundError(DiskCacheError):
+    pass
+
+class DiskCacheLoadError(DiskCacheError):
+    pass
+
+class DiskCacheStoreError(DiskCacheError):
+    pass
+
+
+# EncryptedDiskCache Errors
+class EncryptedDiskCacheError(DiskCacheError):
+    pass
+
+
+class EncryptedDiskCacheDirectoryNotFoundError(DiskCacheDirectoryNotFoundError):
+    pass
+
+class EncryptedDiskCacheNotAFileError(DiskCacheNotAFileError):
+    pass
