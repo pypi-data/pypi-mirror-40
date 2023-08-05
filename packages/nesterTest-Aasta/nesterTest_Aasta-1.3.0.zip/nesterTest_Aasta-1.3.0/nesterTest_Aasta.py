b4f9fef9874a8415d990741482744e512937aab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:19 +0000

    Add package nesterTest-Aasta/1.3.0/nesterTest_Aasta-1.3.0.zip

---
+++ packages/nesterTest-Aasta/nesterTest_Aasta-1.3.0.zip/nesterTest_Aasta-1.3.0/nesterTest_Aasta.py
@@ -0,0 +1,11 @@
+"""
+python 練習
+"""
+def print_lol(thislist,level =0):
+	for each_item in thislist:
+		if isinstance(each_item,list):
+			print_lol(each_item,level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t",end='')
+			print(each_item)
