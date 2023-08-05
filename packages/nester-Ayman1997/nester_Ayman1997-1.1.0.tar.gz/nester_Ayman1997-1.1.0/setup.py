d6f0447694329ac82d36a238a8a267f9bca03a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:35 +0000

    Add package nester-Ayman1997/1.1.0/nester_Ayman1997-1.1.0.tar.gz

---
+++ packages/nester-Ayman1997/nester_Ayman1997-1.1.0.tar.gz/nester_Ayman1997-1.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(  name = "nester_Ayman1997",
+        version = "1.1.0",
+        py_modules = ["nester_Ayman1997"],
+        author  = "Ayman1997",
+        author_email = "aymanbassi@gmail.com",
+        url     = "https://pypi.org/manage/account/",
+    
+        description     = "A simple printer of nested lists",
+    
+
+        )
