21ba4b738c7e170e14c16ec92d75f4cd88beb22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:22 +0000

    Add package lcneo/0.0.0/lcneo-0.0.0.tar.gz

---
+++ packages/lcneo/lcneo-0.0.0.tar.gz/lcneo-0.0.0/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from distutils.core import setup
+
+setup(name='lcneo',
+      versin='1.0',
+      author='neo wong',
+      author_email='674292593@qq.com',
+      url='http://www.acecho.cn/lcneo',
+      license='MIT',
+      package=['lcneo', 'lcneo.utils'],
+)
+
