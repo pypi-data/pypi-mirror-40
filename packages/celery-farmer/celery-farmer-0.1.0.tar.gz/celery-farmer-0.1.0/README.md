763ccb1b20ec03b1f7eae49e029e4a92dd940ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:22 +0000

    Add package celery-farmer/0.1.0/celery-farmer-0.1.0.tar.gz

---
+++ packages/celery-farmer/celery-farmer-0.1.0.tar.gz/celery-farmer-0.1.0/README.md
@@ -0,0 +1,69 @@
+# Celery Farmer
+
+![Image of farmer](https://www.collinsdictionary.com/images/thumb/farmer_96736501_250.jpg)
+
+Farmer will monitor how the Celery cluster is behaving.
+
+## Usage
+
+### Requirements
+- Python, versions 3.6 and 3.7 are supported
+- Pip or Pipenv
+
+### Install
+Install package with pipenv:
+```
+pipenv install celery-farmer
+```
+
+Or pip:
+```
+pip install celery-farmer
+```
+
+### Run
+Run celery-farmer with shell command:
+```
+celery-farmer
+```
+
+### Configuration
+
+## Development
+
+### Requirements
+- Pipenv
+
+Clone repository and install development requirements with:
+```
+pipenv install --dev
+```
+
+### Running tests
+Run all tests, typecheck and linter with:
+```
+pipenv run all-tests
+```
+
+Run only typecheck with:
+```
+pipenv run typecheck
+```
+
+Run only tests with:
+```
+pipenv run test
+```
+Or invoke `pytest` directly:
+```
+pytest
+```
+
+Run only linter with:
+```
+pipenv run lint
+```
+Or invoke `flake` directly:
+```
+flake8
+```
---
+++ packages/celery-farmer/celery-farmer-0.1.0.tar.gz/celery-farmer-0.1.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import setuptools
+
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name='celery-farmer',
+    version='0.1.0',
+    author='Wolt Enterprises Oy',
+    author_email='support@wolt.com',
+    description='Farmer will monitor how the Celery cluster is behaving',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/woltapp/celery-farmer',
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src'},
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+    ],
+    install_requires=[
+        'statsd>=3.2.0,<4.0',
+        'redis>=2.10.0,<4.0',
+        'celery>=4.0,<5.0',
+        'click>=6.7,<8.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    entry_points=dict(
+        console_scripts='celery-farmer = celery_farmer.cli:cli'
+    ),
+)
