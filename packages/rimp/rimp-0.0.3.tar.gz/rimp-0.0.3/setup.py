a5010c5ffa423555ccf613a6543a69f389c2b124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:45 +0000

    Add package rimp/0.0.3/rimp-0.0.3.tar.gz

---
+++ packages/rimp/rimp-0.0.3.tar.gz/rimp-0.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+try:
+    with open('README.md') as f:
+        long_desc = f.read()
+except FileNotFoundError:
+    long_desc = "See pypi project for information"
+
+try:
+    with open('requirements.txt') as f:
+        requires = f.read().splitlines()
+except FileNotFoundError:
+    requires = ['requests', 'beautifulsoup4']
+
+setup(
+    name="rimp",
+    version="0.0.3",
+    url="https://github.com/Zwork101/rimp",
+    description="A hacky project that lets you import repls from https://repl.it into your project",
+    packages=['rimp'],
+    long_description=long_desc,
+    author="Nathan Zilora",
+    author_email="zwork101@gmail.com",
+    requires=requires,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: System :: Archiving :: Packaging'
+    ]
+)
