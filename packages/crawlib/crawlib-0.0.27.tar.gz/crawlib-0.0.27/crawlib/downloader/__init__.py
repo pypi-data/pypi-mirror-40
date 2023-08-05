2083c96f72d84e9cdad288ffe28e6fddede5c12c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:06 +0000

    Add package crawlib/0.0.27/crawlib-0.0.27.tar.gz

---
+++ packages/crawlib/crawlib-0.0.27.tar.gz/crawlib-0.0.27/crawlib/downloader/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .requests_downloader import RequestsDownloader
+from .selenium_downloader import (
+    ChromeDownloader,
+)
---
+++ packages/crawlib/crawlib-0.0.27.tar.gz/crawlib-0.0.27/crawlib/html_parser/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .parser import BaseHtmlParser
+from .decorator import soupify, access_binary, auto_decode_and_soupify
