e07a898480afd15f30be906ac1448c479df1c9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:22 +0000

    Add package filesystems/0.18.1/filesystems-0.18.1.tar.gz

---
+++ packages/filesystems/filesystems-0.18.1.tar.gz/filesystems-0.18.1/filesystems/_path.py
@@ -0,0 +1,102 @@
+import os
+
+from pyrsistent import pvector
+import attr
+
+from filesystems.exceptions import InvalidPath
+
+
+@attr.s(these={"segments": attr.ib()}, init=False, repr=False, hash=True)
+class Path(object):
+    def __init__(self, *segments):
+        self.segments = pvector(segments)
+
+    def __div__(self, other):
+        if not isinstance(other, str):  # FIXME: Unicode paths
+            return NotImplemented
+        return self.descendant(other)
+
+    def __repr__(self):
+        return "<Path {}>".format(self)
+
+    def __str__(self):
+        return os.sep + os.sep.join(self.segments)
+
+    @classmethod
+    def cwd(cls):
+        return cls.from_string(os.getcwd())
+
+    @classmethod
+    def root(cls):
+        return cls()
+
+    @classmethod
+    def from_string(cls, path):
+        """
+        Create a path out of an OS-specific string.
+        """
+
+        if not path:
+            raise InvalidPath(path)
+
+        drive, rest = os.path.splitdrive(path)
+        split = rest.split(os.sep)
+        if split[0]:
+            return RelativePath(*split)
+        return cls(*split[1:])
+
+    def basename(self):
+        return (self.segments or [""])[-1]
+
+    def dirname(self):
+        return str(self.parent())
+
+    def heritage(self):
+        """
+        The (top-down) direct ancestors of this path, including itself.
+        """
+
+        segments = pvector()
+        for segment in self.segments[:-1]:
+            segments = segments.append(segment)
+            yield self.__class__(*segments)
+        yield self
+
+    def descendant(self, *segments):
+        return self.__class__(*self.segments.extend(segments))
+
+    def parent(self):
+        return self.__class__(*self.segments[:-1])
+
+    def sibling(self, name):
+        if not self.segments:
+            raise ValueError("The root file path has no siblings.")
+        return self.parent() / name
+
+    def relative_to(self, path):
+        """
+        Resolve this path against another ``Path``.
+
+        A ``Path`` is always absolute, and therefore always resolves to itself.
+        """
+
+        return self
+
+
+@attr.s(these={"segments": attr.ib()}, init=False, repr=False, hash=True)
+class RelativePath(object):
+    def __init__(self, *segments):
+        self.segments = pvector(segments)
+
+    def __repr__(self):
+        return "<Path {}>".format(self)
+
+    def __str__(self):
+        return os.sep.join(self.segments)
+
+    def relative_to(self, path):
+        """
+        Resolve this path against another ``Path``.
+        """
+
+        return path.descendant(*self.segments)
