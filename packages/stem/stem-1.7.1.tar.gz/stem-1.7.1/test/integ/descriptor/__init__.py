55a59a70fd820b8a5d605a0f7cb0bc24076711f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:07 +0000

    Add package stem/1.7.1/stem-1.7.1.tar.gz

---
+++ packages/stem/stem-1.7.1.tar.gz/stem-1.7.1/test/integ/descriptor/__init__.py
@@ -0,0 +1,11 @@
+"""
+Integration tests for stem.descriptor.* contents.
+"""
+
+__all__ = [
+  'extrainfo_descriptor',
+  'microdescriptor',
+  'networkstatus',
+  'remote'
+  'server_descriptor',
+]
