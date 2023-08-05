6b3a8321a6cc54cb431470179310f51e3167cf00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:27 +0000

    Add package falcon-vue/0.1.4/falcon_vue-0.1.4-py3-none-any.whl

---
+++ packages/falcon-vue/falcon_vue-0.1.4-py3-none-any.whl/falcon_vue/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+'''
+Created on 2018/12/25
+@author: shimakaze-git
+'''
+from .router import register_vue_app
+
+__version__ = '0.1.4'
+__license__ = 'MIT'
+__author__ = 'shimakaze-git'
+__author_email__ = 'shimakaze.soft@gmail.com'
+__url__ = 'https://github.com/shimakaze-git/falcon-vue'
