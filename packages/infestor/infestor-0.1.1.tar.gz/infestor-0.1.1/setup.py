57d572f762a33915cf051fee26413e658d7a4579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:58 +0000

    Add package infestor/0.1.1/infestor-0.1.1.tar.gz

---
+++ packages/infestor/infestor-0.1.1.tar.gz/infestor-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='infestor',
+    version='0.1.1',
+    packages=find_packages(),
+    url='http://github.com/emre/transmitter',
+    license='MIT',
+    author='emre yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='CLI app to claim and create discounted accounts'
+                ' on the STEEM blockchain.',
+    entry_points={
+        'console_scripts': [
+            'infestor = infestor.main:main',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["lightsteem", "pymongo", "flask", "gunicorn", "steemconnect"],
+)
