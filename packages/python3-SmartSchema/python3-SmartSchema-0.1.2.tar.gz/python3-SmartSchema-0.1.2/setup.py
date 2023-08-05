3485cd148a18831b1fda6d24aca2976f79a05640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:07 +0000

    Add package python3-SmartSchema/0.1.2/python3-SmartSchema-0.1.2.tar.gz

---
+++ packages/python3-SmartSchema/python3-SmartSchema-0.1.2.tar.gz/python3-SmartSchema-0.1.2/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="python3-SmartSchema",
+    version="0.1.2",
+    description="Progressive update for schema",
+    author="Jaiswal-ruhil",
+    author_email="cschultz@example.com",
+    scripts=['SmartSchema.py'],
+    exclude=["*.tests", "*.tests.*", "tests.*", "tests", "test"],
+    packages=find_packages(),
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+    ]
+)
