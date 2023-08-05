ed8cc59be6e1127c97da4132032088b5cdc04639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:06 +0000

    Add package vulcan-api/0.1.2/vulcan-api-0.1.2.tar.gz

---
+++ packages/vulcan-api/vulcan-api-0.1.2.tar.gz/vulcan-api-0.1.2/vulcan/utils.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import uuid as _uuid
+import time
+import math
+from OpenSSL import crypto
+import json
+import base64
+
+class VulcanAPIException(Exception):
+    pass
+
+def now():
+    return math.floor(time.time())
+
+def uuid():
+    return str(_uuid.uuid4())
+
+def find(_list, key, value):
+    return next((i for i in _list if i[key] == value), None)
+
+def signature(cert, passphrase, data):
+    p12 = crypto.load_pkcs12(base64.b64decode(cert), passphrase.encode('utf-8'))
+    sign = crypto.sign(p12.get_privatekey(), json.dumps(data).encode('utf-8'), 'RSA-SHA1')
+    return base64.b64encode(sign).decode('utf-8')
