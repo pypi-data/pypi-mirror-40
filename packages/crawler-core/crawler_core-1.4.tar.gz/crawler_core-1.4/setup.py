4c2c3e7089ebd79f5cc60f41588512e9ca60098e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:36 +0000

    Add package crawler-core/1.4/crawler_core-1.4.tar.gz

---
+++ packages/crawler-core/crawler_core-1.4.tar.gz/crawler_core-1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='crawler_core',
+      version='1.4',
+      description='Core programs for crawling',
+      url='https://bitbucket.org/DraganMatesic/crawler_core',
+      author='Dragan Matesic',
+      author_email='dragan.matesic@gmail.com',
+      license='MIT',
+      packages=['core'],
+      zip_safe=False,
+      install_requires=['SQLAlchemy', 'pandas', 'requests', 'bs4', 'stem', 'pymssql', 'pyodbc', 'stem'],
+      )
+
