264549afa94f0993c8ddfd473caa22334b279eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:29 +0000

    Add package statepub/0.0.0.2/statepub-0.0.0.2.tar.gz

---
+++ packages/statepub/statepub-0.0.0.2.tar.gz/statepub-0.0.0.2/setup.py
@@ -0,0 +1,28 @@
+# the imports
+from setuptools import setup
+
+
+setup(
+    name='statepub',
+    version='0.0.0.2',
+    description='A program for publishing the current state of a computing machine to the MQTT network',
+    url='https://github.com/the16thpythonist/statepub',
+    author='Jonas Teufel',
+    author_email='jonseb1998@gmail.com',
+    license='MIT',
+    packages=[
+        'statepub'
+        '/scripts'
+    ],
+    install_requires=[
+        'psutil',
+        'paho-mqtt',
+        'click'
+    ],
+    entry_points={
+        'console_scripts':
+            ['publish-status=statepub.scripts.publishstatus:cli']
+    },
+    python_requires='~=3.5',
+    zip_safe=False
+)
