67e6b09f53741293e3b01527cdaa084933a422e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:26 +0000

    Add package drel/0.4.3/drel-0.4.3.tar.gz

---
+++ packages/drel/drel-0.4.3.tar.gz/drel-0.4.3/drel/core/schemas.py
@@ -0,0 +1,40 @@
+from typing import Dict
+
+from marshmallow import Schema, fields, post_dump
+
+from drel.core import config
+
+
+class RequestLogSchema(Schema):
+    class Meta:
+        fields = ("url", "data", "headers")
+
+
+class ResponseLogSchema(Schema):
+    class Meta:
+        fields = ("data", "status")
+
+
+class FullRequestLogSchema(Schema):
+    request = fields.Nested(RequestLogSchema)
+    response = fields.Nested(ResponseLogSchema)
+    timestamp = fields.DateTime()
+    user = config.USER_SERIALIZER
+
+    class Meta:
+        fields = ("type", "request", "response", "request_id", "timestamp", "app", "user")
+
+    @post_dump
+    def append_type(self, data: Dict) -> Dict:
+        type_ = data["type"]
+        if type_ == config.DEFAULT_LOG_TYPE:
+            return data
+
+        data[f"{type_}_request"] = data.pop("request")
+        data[f"{type_}_response"] = data.pop("response")
+
+        return data
+
+    @post_dump()
+    def drop_blank_fields(self, data: Dict) -> Dict:
+        return {field: value for field, value in data.items() if value}
