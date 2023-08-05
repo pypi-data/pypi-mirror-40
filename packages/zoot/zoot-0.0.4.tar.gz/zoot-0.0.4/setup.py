b447e7bde176806c9ff0fa63f54cf1a36e08489b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package zoot/0.0.4/zoot-0.0.4.tar.gz

---
+++ packages/zoot/zoot-0.0.4.tar.gz/zoot-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'zoot',
+  packages=['zoot', 'zoot.automation', 'zoot.automation.mobile', 'zoot.bdd', 'zoot.testing', 'zoot.utils'],
+  version = '0.0.4',
+  description = 'Web automation and REST API testing framework',
+  long_description_content_type="text/markdown",
+  author = 'Brent Ray',
+  author_email = 'brentlamarrayjr@gmail.com',
+  url = 'https://github.com/brentlamarrayjr/zoot-py',
+  keywords = ['testing', 'api', 'json', 'validation','automation','framework'],
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ],
+)
