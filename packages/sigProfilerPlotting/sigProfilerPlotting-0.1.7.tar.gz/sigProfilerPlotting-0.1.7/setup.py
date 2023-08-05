a3941f7393cfe4849bf35450771e7b2a96cec9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:15 +0000

    Add package sigProfilerPlotting/0.1.7/sigProfilerPlotting-0.1.7.tar.gz

---
+++ packages/sigProfilerPlotting/sigProfilerPlotting-0.1.7.tar.gz/sigProfilerPlotting-0.1.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+	with open('README.rst') as f:
+		return(f.read())
+
+
+setup(name='sigProfilerPlotting',
+		version='0.1.7',
+		description='SigProfiler plotting tool',
+		url='',
+		author='Erik Bergstrom',
+		author_email='ebergstr@eng.ucsd.edu',
+		license='UCSD',
+		packages=['sigProfilerPlotting'],
+		install_requires =[
+			"matplotlib"],
+		include_package_data=True,
+		zip_safe=False)
