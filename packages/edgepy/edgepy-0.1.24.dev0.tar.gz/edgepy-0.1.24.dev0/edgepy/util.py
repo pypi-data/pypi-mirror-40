58364438de10e577a59ddcff17143a16ea20cbce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:22 +0000

    Add package edgepy/0.1.24.dev0/edgepy-0.1.24.dev0.tar.gz

---
+++ packages/edgepy/edgepy-0.1.24.dev0.tar.gz/edgepy-0.1.24.dev0/edgepy/util.py
@@ -0,0 +1,44 @@
+def getdir():
+    import os
+    from edgepy import ref
+
+    sdir = ref.__file__
+    sdir = sdir.rstrip('.py').rstrip('/ref')
+
+    getdir.package = sdir
+    getdir.counts = sdir + '/docs/counts'
+    getdir.genelib = sdir + '/docs/genelib'
+    getdir.info = sdir + '/docs/info'
+    getdir.plots = sdir + '/docs/plots'
+    getdir.results = sdir + '/docs/results'
+    getdir.rfiles = sdir + '/docs/rfiles'
+    getdir.trimmed_results = sdir + '/docs/trimmed_results'
+    getdir.workspace = sdir + '/docs/workspace'
+
+    if os.path.exists(sdir + '/docs/counts/empty.txt'):
+        flist = ['counts', 'genelib', 'info', 'plots', 'results', 'trimmed_results', 'workspace']
+        for f in flist:
+            if os.path.exists(sdir + '/docs/%s/empty.txt' % f):
+                os.remove(sdir + '/docs/%s/empty.txt' % f)
+
+
+def runR(rname):
+    from edgepy import ref
+    import subprocess
+
+    sdir = ref.__file__
+    sdir = sdir.rstrip('.py').rstrip('/ref')
+    rfiles = sdir + '/docs/rfiles'
+    rfile = rfiles + '/%s.R' % rname
+
+    command = ['Rscript', rfile]
+    subprocess.call(command, universal_newlines=True)
+
+
+def namefromcsv(string):
+    l = string.split('/')
+    last = l.pop()
+    s = last.rstrip('.csv')
+    return s
+
+
