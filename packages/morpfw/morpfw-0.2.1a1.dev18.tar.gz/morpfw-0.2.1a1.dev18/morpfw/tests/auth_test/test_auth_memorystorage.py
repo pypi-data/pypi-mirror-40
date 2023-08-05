1d9ee1f7925c52ab04bb81bd8eea78984a8207ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:28 +0000

    Add package morpfw/0.2.1a1.dev18/morpfw-0.2.1a1.dev18.tar.gz

---
+++ packages/morpfw/morpfw-0.2.1a1.dev18.tar.gz/morpfw-0.2.1a1.dev18/morpfw/tests/auth_test/test_auth_memorystorage.py
@@ -0,0 +1,35 @@
+from .test_auth import _test_authentication, get_client
+from more.jwtauth import JWTIdentityPolicy
+from morpfw.app import BaseApp
+from morpfw.authn.app import App
+import os
+import yaml
+from morpfw.authn.policy.default import MemoryStorageAuthnPolicy
+from morpfw.authn.policy.default import MemoryStorageAuthApp as BaseAuthApp
+from morpfw.authz.default import DefaultAuthzPolicy
+
+
+class MemoryStorageAuthApp(BaseAuthApp, DefaultAuthzPolicy):
+    pass
+
+
+class MemoryStorageApp(BaseApp, DefaultAuthzPolicy):
+    pass
+
+
+class AuthnPolicy(MemoryStorageAuthnPolicy):
+    app_cls = MemoryStorageAuthApp
+
+
+@MemoryStorageApp.mount(app=AuthnPolicy.app_cls, path='/auth')
+def mount_app(app):
+    return AuthnPolicy.app_cls()
+
+
+def test_authentication_memorystorage():
+    with open(os.path.join(os.path.dirname(__file__),
+                           'settings-memorystorage.yml')) as f:
+        settings = yaml.load(f)
+
+    c = get_client(MemoryStorageApp, settings)
+    _test_authentication(c)
