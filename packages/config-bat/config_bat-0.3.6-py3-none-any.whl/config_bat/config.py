f7aa5ba7dcc1d157e12ae854051f6d93ea8c6f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:29 +0000

    Add package config-bat/0.3.6/config_bat-0.3.6-py3-none-any.whl

---
+++ packages/config-bat/config_bat-0.3.6-py3-none-any.whl/config_bat/config.py
@@ -0,0 +1,35 @@
+import os
+import json
+
+configPath = os.getcwd() + os.getenv("CONFIG_PATH", "/config/config.json")
+appStage = os.getenv("APP_STAGE", "development")
+
+
+class Config:
+    def __init__(self):
+        try:
+            with open(configPath, "r") as fileObj:
+                self.config = json.loads(fileObj.read())
+        except FileNotFoundError:
+            print("Warning: no config file found")
+            self.config = {}
+
+    def get(self, key, default=None):
+
+        try:
+            return self.getData(appStage, key, default)
+        except KeyError:
+            try:
+                return self.getData("common", key, default)
+            except KeyError:
+                return default
+
+    def getData(self, section, key, default):
+        data = self.config[section][key]
+        if isinstance(data,str):
+            if data[:1] == "$":
+                return os.getenv(data[1:], default)
+        return data
+
+
+config = Config()
