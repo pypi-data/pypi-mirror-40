4a22df5227db93a7bc31ac29781e64fc56c380b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:28 +0000

    Add package heroku.env/0.3.0/heroku.env-0.3.0-py2.py3-none-any.whl

---
+++ packages/heroku.env/heroku.env-0.3.0-py2.py3-none-any.whl/heroku_env/cli.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for heroku.env"""
+
+from __future__ import unicode_literals  # unicode support for py2
+
+import os
+
+import click
+
+from .heroku_env import upload_env
+from .param_types import APIKeyParamType
+
+
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.option(
+    '-a',
+    '--app',
+    required=True,
+    type=str,
+    help="The name of the heroku app."
+)
+@click.option(
+    '-e',
+    '--env-file',
+    type=click.Path(exists=True, file_okay=True, dir_okay=False,
+                    readable=True, resolve_path=True, allow_dash=False),
+    default=".env",
+    show_default=True,
+    help="Path to the .env file"
+)
+@click.option(
+    '-k',
+    '--api-key',
+    prompt="Please enter the Heroku API key to continue",
+    hide_input=True,
+    confirmation_prompt=True,
+    required=True,
+    type=APIKeyParamType(),
+    envvar="HEROKU_API_KEY",
+    help="Your Heroku API key"
+)
+@click.option(
+    '-t',
+    '--set-alt',
+    is_flag=True,
+    required=False,
+    help="Flag to enable reading of alternate values of env vars from"
+         " comments in the env file."
+         " Specify the alternate value to use with 'alt_value=VALUE'"
+         " in the line right before the actual env var you want to change."
+)
+def main(app, env_file, api_key, set_alt):
+    """
+    Simple CLI tool to upload environment variables to
+    Heroku from a .env file, through the Heroku CLI Toolbelt.
+
+    It is recommended for security purposes that you set API
+    key as an environment variable like this:
+
+    export HEROKU_API_KEY=a1b12c24-ab1d-123f-5678-1234b12a0a1b
+
+    Example usages:
+
+    heroku.env --app swimming-briskly-123 --env-file dot.env
+
+    heroku.env --app swimming-briskly-123 --env-file dot.env
+    --api-key a1b12c24-ab1d-123f-5678-1234b12a0a1b
+    """
+    # if not defined, then set it.
+    if not os.getenv('HEROKU_API_KEY'):
+        os.environ['HEROKU_API_KEY'] = api_key
+    try:
+        upload_env(app, env_file, set_alt)
+    except Exception as e:
+        raise click.ClickException(e)
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
---
+++ packages/heroku.env/heroku.env-0.3.0-py2.py3-none-any.whl/heroku_env/heroku_env.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+from __future__ import unicode_literals  # unicode support for py2
+
+import os
+
+import click
+
+
+def set_config_var(name, value, app_name):
+    command = 'heroku config:set "{}={}" --app {}'.format(
+        name, value, app_name
+    )
+    os.system(command)
+
+
+def upload_env(app_name, env_file, set_alt):
+    use_alt = False
+    alt_value = ''
+
+    with open(env_file) as e:
+
+        for line in e:
+            line = line.strip()
+
+            # check comments
+            if line.startswith("#"):
+                # enable --set-alt
+                if set_alt and "alt_value=" in line:
+                    alt_value = line.split("alt_value=", 1)[1]
+                    if alt_value:
+                        # use this value for the next env var
+                        use_alt = True
+                else:
+                    click.echo("Skipping comment...")
+                # any kind of comment warrants a skip
+                continue
+
+            # set env vars
+            if "=" in line:
+                kv_pair = line.split("=", 1)
+                if len(kv_pair) > 1:  # has to at least be of form: k=
+                    key = kv_pair[0]
+
+                    if use_alt:
+                        value = alt_value
+                        use_alt = False
+                    else:
+                        value = kv_pair[1]
+
+                    # an empty value is fine
+                    if key:
+                        set_config_var(key, value, app_name)
+            else:
+                click.echo("Skipping line : Not of the form key=value")
