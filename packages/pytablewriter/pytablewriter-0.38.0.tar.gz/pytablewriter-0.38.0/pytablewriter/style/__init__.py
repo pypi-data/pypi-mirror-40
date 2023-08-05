257361452a1394b088fdf16163f69eeb5b95e9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:45 +0000

    Add package pytablewriter/0.38.0/pytablewriter-0.38.0.tar.gz

---
+++ packages/pytablewriter/pytablewriter-0.38.0.tar.gz/pytablewriter-0.38.0/pytablewriter/style/__init__.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+from __future__ import absolute_import
+
+from ._font import FontSize, FontWeight
+from ._style import Align, Style, ThousandSeparator
+from ._styler import HtmlStyler, LatexStyler, NullStyler, TextStyler, MarkdownStyler
+from dataproperty import Format
