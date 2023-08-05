399f06d5660e02ba3100fb88fc4149f8a54312e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:02 +0000

    Add package BaiduFace/1.2/BaiduFace-1.2.tar.gz

---
+++ packages/BaiduFace/BaiduFace-1.2.tar.gz/BaiduFace-1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='BaiduFace',
+    version='1.2',
+    package_dir={'bdface': 'src'},
+    packages=['bdface'],
+    author='wcy',
+    author_email='1208640961@qq.com',
+    description='Call baidu face recognition recommendation system'
+)
