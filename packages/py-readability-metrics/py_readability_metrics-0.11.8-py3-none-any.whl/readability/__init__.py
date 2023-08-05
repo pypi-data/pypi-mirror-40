1526c9044af9e9649828c53ff53622f4d4b1ac5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:05 +0000

    Add package py-readability-metrics/0.11.8/py_readability_metrics-0.11.8-py3-none-any.whl

---
+++ packages/py-readability-metrics/py_readability_metrics-0.11.8-py3-none-any.whl/readability/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Calculate gunning fog readability score
+"""
+name = "py-readabilitiy-metrics"
+__title__ = name
+__version__ = '0.11.8'
+__author__ = 'Carmine DiMascio'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Carmine DiMascio'
+
+from .readability import Readability
