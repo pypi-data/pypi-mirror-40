c5d069f6734c1318599789d2832bdf59bd7e7427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:06 +0000

    Add package curieutil/0.0.3/curieutil-0.0.3.tar.gz

---
+++ packages/curieutil/curieutil-0.0.3.tar.gz/curieutil-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='curieutil',
+    version='0.0.3',
+    description='say hello',
+    py_modules=["curieutil"],
+    package_dir={'': 'src'},
+    url="https://github.com/geneontology/curie-util-py",
+    author="Laurent-Philippe Albou",
+    author_email="laurent.albou@lbl.gov",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ],
+
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
+
+
