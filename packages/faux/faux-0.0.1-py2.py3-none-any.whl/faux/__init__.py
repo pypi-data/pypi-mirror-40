c2b4a8f5f90053fa8249828d9fd4ebfc7c7c9c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:54 +0000

    Add package faux/0.0.1/faux-0.0.1-py2.py3-none-any.whl

---
+++ packages/faux/faux-0.0.1-py2.py3-none-any.whl/faux/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'bprinty'
+__email__ = 'bprinty@gmail.com'
+__version__ = '0.0.1'
+
+
+from .server import Server, request
+from .requests import get, post, put, delete, cache
