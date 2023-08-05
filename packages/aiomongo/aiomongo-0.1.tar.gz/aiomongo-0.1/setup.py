d7ca9be6f87a33f257e2f5eedb9037bb655f546a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:25 +0000

    Add package aiomongo/0.1/aiomongo-0.1.tar.gz

---
+++ packages/aiomongo/aiomongo-0.1.tar.gz/aiomongo-0.1/setup.py
@@ -0,0 +1,35 @@
+import sys
+from setuptools import setup
+
+if sys.version_info < (3, 5, 2):
+    raise RuntimeError('aiomongo requires Python 3.5.2+')
+
+
+install_requires = ['pymongo<3.5']
+tests_require = install_requires + ['pytest', 'pytest-asyncio'],
+
+setup(
+    name='aiomongo',
+    version='0.1',
+    description='Asynchronous Python driver for MongoDB <http://www.mongodb.org>',
+    author='Dmytro Domashevskyi',
+    author_email='domash@zeoalliance.com',
+    url='https://github.com/ZeoAlliance/aiomongo',
+    keywords=['mongo', 'mongodb', 'pymongo', 'aiomongo'],
+    packages=['aiomongo'],
+    install_requires=install_requires,
+    license='Apache License, Version 2.0',
+    include_package_data=True,
+    tests_require=tests_require,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Database']
+    )
