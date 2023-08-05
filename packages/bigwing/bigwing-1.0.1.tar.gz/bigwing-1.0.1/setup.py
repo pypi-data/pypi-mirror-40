1093ff56cb1feac2436d803f003bb31e824e9d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:28 +0000

    Add package bigwing/1.0.1/bigwing-1.0.1.tar.gz

---
+++ packages/bigwing/bigwing-1.0.1.tar.gz/bigwing-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+	'requests==2.21.0',
+	'jsonschema==2.6.0',
+	'pandas==0.23.4',
+	'ipython==7.2.0',
+	'ipython-genutils==0.2.0'
+]
+
+dependency_links = []
+setup_requires = []
+
+setup(
+	name				= 'bigwing',
+	version				= '1.0.1',
+	description			= 'bingwing project',
+	author 				= 'jihokwak',
+	author_email		= 'kakuteeko@naver.com',
+	packages         	= find_packages(exclude = ['docs']),
+	install_requires	= install_requires,
+	setup_requires		= setup_requires,
+	dependency_links	= dependency_links,
+	keywords			= ["geocoder", 'api processor'],
+	python_requires		= '>=3',
+	package_data     =  {
+		'api': ['api.py']
+	},
+	zip_safe			= False,
+    classifiers      	= [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]	
+)
