5249d0b172de2afaa574fee2716e630898cb1e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:01 +0000

    Add package appconfigpy/0.1.0/appconfigpy-0.1.0.tar.gz

---
+++ packages/appconfigpy/appconfigpy-0.1.0.tar.gz/appconfigpy-0.1.0/examples/common.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import appconfigpy
+
+
+app_config_manager = appconfigpy.ConfigManager(
+    config_name="example",
+    config_item_list=[
+        appconfigpy.ConfigItem(
+            name="token",
+            initial_value=None,
+            prompt_text="API Token",
+            default_display_style=appconfigpy.DefaultDisplayStyle.PART_VISIBLE,
+        ),
+        appconfigpy.ConfigItem(name="path", prompt_text="ABC Path", initial_value="."),
+        appconfigpy.ConfigItem(
+            name="number", prompt_text="XYZ Number", initial_value="", value_type=int
+        ),
+    ],
+)
