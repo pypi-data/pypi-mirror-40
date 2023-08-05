5962e01cc6b3f0e842c4e646dc3ee8609f6f6207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package aiolambda/0.2.14/aiolambda-0.2.14-py3-none-any.whl

---
+++ packages/aiolambda/aiolambda-0.2.14-py3-none-any.whl/aiolambda/security.py
@@ -0,0 +1,24 @@
+import aiohttp.web
+import six
+
+from connexion.decorators.security import validate_scope as connexion_validate_scope
+from connexion.exceptions import OAuthScopeProblem
+from werkzeug.exceptions import Unauthorized
+from jose import JWTError, jwt
+
+from aiolambda.config import JWT_PUBLIC_KEY, JWT_ALGORITHM
+
+
+def decode_token(token, required_scopes=None):
+    try:
+        info = jwt.decode(token, JWT_PUBLIC_KEY, algorithms=[JWT_ALGORITHM])
+    except JWTError as e:
+        six.raise_from(Unauthorized, e)
+    return info
+
+
+def validate_scope(required_scopes, token_scopes):
+    try:
+        return connexion_validate_scope(required_scopes, token_scopes)
+    except OAuthScopeProblem:
+        raise aiohttp.web.HTTPForbidden
