d00a2fcaacc4c43bf0ba7743af1b734cce5f924a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:51 +0000

    Add package baokaifang/0.0.1/baokaifang-0.0.1.tar.gz

---
+++ packages/baokaifang/baokaifang-0.0.1.tar.gz/baokaifang-0.0.1/bkf/bkf.py
@@ -0,0 +1,13 @@
+"""
+bkf's personal packages
+"""
+
+class Info(object):
+    def __init__(self):
+        self.birth_day = '1992/03/25'
+        self.name = 'baokaifang'
+        self.loction = 'shanghai'
+
+    def test(self):
+        # print('welcome!')
+        return 'hello pypi!'
---
+++ packages/baokaifang/baokaifang-0.0.1.tar.gz/baokaifang-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='baokaifang',
+    version='0.0.1',
+    author='bkf',
+    author_email='736781877@qq.com',
+    url='',
+    description=u'personal packages',
+    packages=['bkf'],
+    install_requires=[]
+)
