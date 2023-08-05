0a4b73963f92ce58adeb0d72a20a8c239b327a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:54 +0000

    Add package flask-session-plus/0.0.3/flask-session-plus-0.0.3.tar.gz

---
+++ packages/flask-session-plus/flask-session-plus-0.0.3.tar.gz/flask-session-plus-0.0.3/test/flask_app.py
@@ -0,0 +1,73 @@
+import os
+from flask import Flask, session
+from flask_session_plus import Session
+from flask_login import LoginManager, login_required
+from test.auth import login_user, logout_user
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(os.path.dirname(BASE_DIR), 'firebase.json')
+
+from test.models import User, db
+
+
+
+app = Flask(__name__)
+app.config['SESSION_CONFIG'] = [
+    {
+        'cookie_name': 'hola',
+        'session_type': 'secure_cookie',
+        'session_fields': 'auto'
+    },
+    {
+        'cookie_name': 'fire',
+        'session_type': 'firestore',
+        'session_fields': ['user_id', 'user_data', '_fresh', '_id'],
+        'client': db,
+        'collection': 'sessions',
+    }
+]
+app.config['SECRET_KEY'] = '_5#y7656ggu677hio'
+app.config['SESION_USER_FIELDS'] = ['name', 'email', 'timezone', 'language', 'active', 'clients', 'roles']
+
+
+mses = Session(app)
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.get_user_by_id(id)
+
+
+@app.route('/')
+def index():
+
+    session['perro'] = True
+
+    return 'hola'
+
+
+@app.route('/login')
+def login():
+    user = User.get_user_by_id('1kuU9610nMtUlqLqdjxR')
+
+    login_user(user)
+    return 'User logged in!'
+
+@app.route('/logout')
+def logout():
+    user = User.get_user_by_id('1kuU9610nMtUlqLqdjxR')
+
+    logout_user()
+
+    return 'User logged out!'
+
+
+@app.route('/protected')
+@login_required
+def protected():
+    return 'you have access!'
+
+
+if __name__ == '__main__':
+    app.run()
