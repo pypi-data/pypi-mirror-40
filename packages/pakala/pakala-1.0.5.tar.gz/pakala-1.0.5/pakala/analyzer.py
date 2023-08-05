b375668ed6595845735081cfadd23b3709197b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:10 +0000

    Add package pakala/1.0.5/pakala-1.0.5.tar.gz

---
+++ packages/pakala/pakala-1.0.5.tar.gz/pakala-1.0.5/pakala/analyzer.py
@@ -0,0 +1,177 @@
+import logging
+import numbers
+
+import claripy
+
+from pakala import utils
+
+from web3 import Web3
+
+
+logger = logging.getLogger(__name__)
+
+
+class BaseAnalyzer(object):
+    """Base class for an Analyzer.
+
+    Child classes need to define `caller` and `address`.
+    """
+
+    def __init__(self, max_wei_to_send, min_wei_to_receive, block="latest"):
+        self.web3 = Web3()
+        self.web3.eth.defaultBlock = block
+        self.max_wei_to_send = max_wei_to_send
+        self.min_wei_to_receive = min_wei_to_receive
+        self.storage_cache = {}
+
+    def _read_storage(self, state, key):
+        # TODO: We do an approximation here: if it cannot be computed or it can
+        # be multiple things, we assume the initial storage is 0...
+        # Instead we could use a cascade of claripy.If(key, value, claripy.If(...
+        # reflecting the actual storage (if there are not too many keys in storage.
+        logger.debug("Reading storage %r" % key)
+        try:
+            keys = state.solver.eval(key, 2)
+            if len(keys) > 1:
+                logger.info("Multiple values possible for key %r", key)
+                return utils.bvv(0)
+            assert len(keys) == 1
+            key = keys[0]
+            assert isinstance(key, numbers.Number)
+        except claripy.errors.UnsatError as e:
+            # Should not be too bad, because for the same key we will reuse the
+            # same cache.
+            logger.debug("Encountered an exception when resolving key %r: %r", key, e)
+            return utils.bvv(0)
+
+        if key in self.storage_cache:
+            value = self.storage_cache[key]
+        else:
+            hex_addr = self.web3.toChecksumAddress(
+                utils.number_to_address(utils.bvv_to_number(self.address))
+            )
+            value = self.web3.toInt(self.web3.eth.getStorageAt(hex_addr, key))
+            self.storage_cache[key] = value
+
+        return utils.bvv(value)
+
+    def check_state(self, state, path=None):
+        """Check a reachable state for bugs"""
+        logger.debug("Check state: %s", state)
+        logger.debug("Constraints: %s", state.solver.constraints)
+
+        read_constraints = []
+        extra_constraints = []  # From the environment (block number, whatever)
+
+        if path is None:
+            path = [state]
+            # Static read were we never wrote, but we know the key is not symbolic.
+            # So we go and fetch it.
+            for key, value in state.storage_read.items():
+                constraint = state.storage_read[key] == self._read_storage(state, key)
+                read_constraints.append(constraint)
+                logger.debug("Add constraint: %s", constraint)
+
+        for s in path:
+            extra_constraints += s.env.extra_constraints()
+            extra_constraints += [
+                s.env.caller == utils.DEFAULT_CALLER,
+                s.env.origin == utils.DEFAULT_CALLER,
+            ]
+
+        # Calls
+        total_sent = sum(s.env.value for s in path)
+        sent_constraints = [s.env.value < self.max_wei_to_send for s in path]
+        total_received_by_me = utils.bvv(0)
+        total_received_by_others = utils.bvv(0)
+
+        for call in state.calls:
+            value, to, gas = call[-3:]  # pylint: disable=unused-variable,invalid-name
+            if state.solver.satisfiable(
+                extra_constraints=[to[159:0] == self.caller[159:0]]
+            ):
+                state.solver.add(to[159:0] == self.caller[159:0])
+                total_received_by_me += value
+            else:
+                total_received_by_others += value
+
+        final_balance = (
+            path[0].env.balance
+            + total_sent
+            - total_received_by_me
+            - total_received_by_others
+        )
+
+        # Suicide
+        if state.selfdestruct_to is not None:
+            constraints = (
+                extra_constraints
+                + read_constraints
+                + [
+                    final_balance >= self.min_wei_to_receive,
+                    state.selfdestruct_to[159:0] == self.caller[159:0],
+                ]
+            )
+            logger.debug("Check for selfdestruct bug with constraints %s", constraints)
+            if state.solver.satisfiable(extra_constraints=constraints):
+                logger.info("Found selfdestruct bug.")
+                return True
+
+        if total_received_by_me is utils.bvv(0):
+            return False
+
+        logger.debug("Found calls back to caller: %s", total_received_by_me)
+
+        constraints = (
+            sent_constraints
+            + extra_constraints
+            + read_constraints
+            + [
+                final_balance >= 0,
+                total_received_by_me > total_sent,  # I get more than what I sent?
+                total_received_by_me > self.min_wei_to_receive,
+            ]
+        )
+
+        logger.debug("Extra constraints: %r", constraints)
+
+        if state.solver.satisfiable(extra_constraints=constraints):
+            logger.info("Found call bug.")
+            return True
+
+        return False
+
+
+class Analyzer(BaseAnalyzer):
+    """Simple Analyzer class, where caller and address are given explicitly."""
+
+    def __init__(self, address, caller, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.caller = caller
+        self.address = address
+
+
+class FakeStorage(dict):
+    """For testing, you can override the storage_cache with an instance of this
+    class. This allow you to simulate storage for a contract, and prevent the
+    code to try to fetch it from web3. Instead it will crash if it tries to
+    access something that you didn't specify."""
+
+    def __contains__(self, key):
+        if not super().__contains__(key):
+            raise KeyError(
+                "The analyzer is trying to access a FakeStorage"
+                " key that we didn't specify: '%s'." % key
+            )
+        return True
+
+
+class EmptyStorage(object):
+    """For testing, you can override the storage_cache with an instance of this
+    class, which will simulate a completely empty storage."""
+
+    def __contains__(self, key):
+        return True
+
+    def __getitem__(self, key):
+        return 0
---
+++ packages/pakala/pakala-1.0.5.tar.gz/pakala-1.0.5/pakala/test_recursive_analyzer.py
@@ -0,0 +1,259 @@
+import claripy
+import unittest
+import logging
+import random
+import itertools
+
+from pakala.recursive_analyzer import RecursiveAnalyzer
+from pakala.analyzer import FakeStorage
+from pakala.env import Env
+from pakala.state import State
+from pakala import utils
+from pakala.claripy_sha3 import Sha3
+
+from web3 import Web3
+
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("claripy").setLevel(logging.ERROR)
+
+
+class TestCheckStates(unittest.TestCase):
+    """The interesting tests.
+
+    Inventing various classic scenarios and making sure that we find the bug
+    if there is one. And that we don't if we are not supposed to find one.
+    """
+
+    def setUp(self):
+        self.env = Env(b"", caller=utils.DEFAULT_CALLER, address=utils.DEFAULT_ADDRESS)
+
+    def check_states(self, states, mock_storage=None):
+        self.analyzer = RecursiveAnalyzer(
+            max_wei_to_send=Web3.toWei(10, "ether"),
+            min_wei_to_receive=Web3.toWei(1, "milliether"),
+        )
+        self.analyzer.storage_cache = FakeStorage(mock_storage or {})
+        return self.analyzer.check_states(states, timeout=0, max_depth=4)
+
+    def get_call(self, value, to=None):
+        if to is None:
+            to = self.env.caller
+        return [
+            utils.bvv(0),
+            utils.bvv(0),
+            utils.bvv(0),
+            utils.bvv(0),
+            value,
+            to,
+            utils.bvv(0),
+        ]
+
+    def test_nothing(self):
+        self.assertFalse(self.check_states([]))
+
+    def test_simple(self):
+        state = State(self.env)
+        self.assertFalse(self.check_states([state]))
+
+    def test_selfdestruct_simple(self):
+        state = State(self.env)
+        state.selfdestruct_to = self.env.caller
+        self.assertTrue(self.check_states([state]))
+
+    def test_call_simple(self):
+        state = State(self.env)
+        state.calls.append(self.get_call(self.env.balance))
+        self.assertTrue(self.check_states([state]))
+
+    def test_write_and_selfdestruct(self):
+        state = State(self.env)
+
+        state_write = state.copy()
+        state_write.storage_written = {utils.bvv(0): self.env.calldata.read(4, 32)}
+
+        state_selfdestruct = state.copy()
+        state_selfdestruct.selfdestruct_to = self.env.calldata.read(4, 32)
+        storage_0 = claripy.BVS("storage[0]", 256)
+        state_selfdestruct.storage_read = {utils.bvv(0): storage_0}
+        state_selfdestruct.solver.add(storage_0 == 0xDEADBEEF0101)
+
+        storage = {0: 0xBAD1DEA}
+        self.assertTrue(
+            self.check_states([state_write, state_selfdestruct], mock_storage=storage)
+        )
+        self.assertFalse(self.check_states([state_selfdestruct], mock_storage=storage))
+        self.assertFalse(self.check_states([state_write]))
+
+    def test_sha3_key(self):
+        """Exercise solidity-like mappings, with the key being a sha3."""
+        state = State(self.env)
+
+        state_write = state.copy()
+        # Arbitrary write input[1], at SHA3(input[0])
+        state_write.storage_written = {
+            Sha3(self.env.calldata.read(4, 32)): self.env.calldata.read(36, 32)
+        }
+
+        # Needs that: storage[SHA3(input[0])] == 43, made possible by the previous call
+        state_selfdestruct = state.copy()
+        state_selfdestruct.selfdestruct_to = self.env.calldata.read(36, 32)
+        storage_input = claripy.BVS("storage[SHA3(input)]", 256)
+        state_selfdestruct.storage_read = {
+            Sha3(self.env.calldata.read(4, 32)): storage_input
+        }
+        state_selfdestruct.solver.add(storage_input == 0xDEADBEEF101010)
+
+        storage = {
+            55186156870478567193644641351382124067713781048612400765092754877653207859685: 0
+        }
+        self.assertTrue(
+            self.check_states([state_write, state_selfdestruct], mock_storage=storage)
+        )
+        self.assertFalse(self.check_states([state_selfdestruct], mock_storage=storage))
+        self.assertFalse(self.check_states([state_write]))
+
+    def test_sha3_value1(self):
+        """Exercise comparison of two SHA3 (as values)."""
+        state = State(self.env)
+
+        state_write = state.copy()
+        state_write.storage_written = {
+            utils.bvv(0): Sha3(self.env.calldata.read(4, 32))
+        }
+
+        state_selfdestruct = state.copy()
+        state_selfdestruct.selfdestruct_to = self.env.calldata.read(36, 32)
+        storage_input = claripy.BVS("storage[0]", 256)
+        state_selfdestruct.storage_read = {utils.bvv(0): storage_input}
+        state_selfdestruct.solver.add(
+            storage_input == Sha3(self.env.calldata.read(4, 32))
+        )
+
+        storage = {0: 0}
+        self.assertTrue(
+            self.check_states([state_write, state_selfdestruct], mock_storage=storage)
+        )
+        self.assertFalse(self.check_states([state_selfdestruct], mock_storage=storage))
+        self.assertFalse(self.check_states([state_write], mock_storage=storage))
+
+    def test_sha3_value2(self):
+        """Same as above, but we need to pass the computed SHA3."""
+        state = State(self.env)
+
+        state_write = state.copy()
+        state_write.storage_written = {
+            utils.bvv(0): Sha3(self.env.calldata.read(4, 32))
+        }
+
+        state_selfdestruct = state.copy()
+        state_selfdestruct.selfdestruct_to = self.env.calldata.read(36, 32)
+        storage_input = claripy.BVS("storage[0]", 256)
+        state_selfdestruct.storage_read = {utils.bvv(0): storage_input}
+        state_selfdestruct.solver.add(storage_input == self.env.calldata.read(4, 32))
+        state_selfdestruct.solver.add(storage_input != 0)
+
+        storage = {0: 0}
+        self.assertTrue(
+            self.check_states([state_write, state_selfdestruct], mock_storage=storage)
+        )
+        self.assertFalse(self.check_states([state_selfdestruct], mock_storage=storage))
+        self.assertFalse(self.check_states([state_write], mock_storage=storage))
+
+    def test_write_write_and_selfdestruct(self):
+        state = State(self.env)
+        # Anybody can set owner
+        state_write1 = state.copy()
+        state_write1.storage_written = {utils.bvv(0): self.env.calldata.read(4, 32)}
+
+        # Onlyowner: set a magic constant allowing the selfdestruct bug, at an
+        # user-controlled storage key.
+        state_write2 = state.copy()
+        read_0 = claripy.BVS("storage[0]", 256)
+        state_write2.storage_read = {utils.bvv(0): read_0}
+        state_write2.storage_written = {
+            self.env.calldata.read(36, 32): self.env.calldata.read(4, 32)
+        }
+        state_write2.solver.add(read_0 == self.env.caller)
+
+        # Suicide, when owner and magic constant set
+        state_selfdestruct = state.copy()
+        read_0 = claripy.BVS("storage[0]", 256)
+        read_40 = claripy.BVS("storage[4]", 256)
+        state_selfdestruct.storage_read = {utils.bvv(0): read_0, utils.bvv(40): read_40}
+        state_selfdestruct.solver.add(self.env.caller == read_0)
+        state_selfdestruct.solver.add(read_40 == 1337)
+        state_selfdestruct.selfdestruct_to = self.env.caller
+
+        states = [state_write1, state_write2, state_selfdestruct]
+        random.shuffle(states)
+
+        storage = {0: 123456789, 40: 387642}
+        for s in itertools.combinations(states, 2):
+            self.assertFalse(self.check_states(s, mock_storage=storage))
+        self.assertTrue(self.check_states(states, mock_storage=storage))
+
+    def test_send_after_write(self):
+        state = State(self.env)
+
+        # We send storage[0]
+        state_send = state.copy()
+        storage_0 = claripy.BVS("storage[0]", 256)
+        state_send.storage_read = {utils.bvv(0): storage_0}
+        state_send.calls.append(self.get_call(storage_0))
+
+        # storage[0] is 0.5 ETH
+        storage = {0: Web3.toWei(0.5, "ether")}
+        self.assertTrue(self.check_states([state_send], mock_storage=storage))
+
+        # storage[0] is 0 ETH
+        storage = {0: 0}
+        self.assertFalse(self.check_states([state_send], mock_storage=storage))
+
+        # storage[0] is still 0 ETH initially, but we have an arbitrary write now
+        state_write = state.copy()
+        state_write.storage_written = {utils.bvv(0): self.env.calldata.read(4, 32)}
+        state_write.solver.add(self.env.calldata.read(0, 4) == 0x1337)
+        state_write.solver.add(self.env.calldata.read(4, 32) < Web3.toWei(1, "ether"))
+
+        self.assertFalse(self.check_states([state_write], mock_storage=storage))
+        self.assertTrue(
+            self.check_states([state_send, state_write], mock_storage=storage)
+        )
+        self.assertTrue(
+            self.check_states([state_write, state_send], mock_storage=storage)
+        )
+
+        # ...arbitrary write of 1 wei only, which is too little
+        state_write_0 = state_write.copy()
+        state_write_0.solver.add(self.env.calldata.read(4, 32) == 1)
+        self.assertFalse(
+            self.check_states([state_write_0, state_send], mock_storage=storage)
+        )
+
+        # ...arbitrary write only if the block timestamp is <10, which is impossible.
+        state_write_ts = state_write.copy()
+        state_write_ts.solver.add(self.env.block_timestamp < 10)
+        self.assertFalse(
+            self.check_states([state_write_ts, state_send], mock_storage=storage)
+        )
+
+        self.assertFalse(
+            self.check_states(
+                [state_write_0, state_send, state_write_ts], mock_storage=storage
+            )
+        )
+
+        # now we put all these state_write* together, so there is a solution.
+        self.assertTrue(
+            self.check_states(
+                [state_write_0, state_send, state_write, state_write_ts],
+                mock_storage=storage,
+            )
+        )
+        self.assertTrue(
+            self.check_states(
+                [state_write_0, state_write, state_write_ts, state_send],
+                mock_storage=storage,
+            )
+        )
