dec24eb5ae42c0989ac39173cce64e8f3eda7524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:20 +0000

    Add package tensorspacejs/0.0.13/tensorspacejs-0.0.13-py3-none-any.whl

---
+++ packages/tensorspacejs/tensorspacejs-0.0.13-py3-none-any.whl/src/hello_world.py
@@ -0,0 +1,15 @@
+# import numpy as np
+
+def print_hello_world():
+    print("Hello World from converter")
+
+    # a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+    # print(a)
+
+
+def main():
+    print_hello_world()
+
+
+if __name__ == '__main__':
+    main()
