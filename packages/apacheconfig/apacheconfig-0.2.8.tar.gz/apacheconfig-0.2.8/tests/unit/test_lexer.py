7bd9509227a22d7b16f2c15ea1570d103f4740c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:25 +0000

    Add package apacheconfig/0.2.8/apacheconfig-0.2.8.tar.gz

---
+++ packages/apacheconfig/apacheconfig-0.2.8.tar.gz/apacheconfig-0.2.8/tests/unit/test_lexer.py
@@ -0,0 +1,138 @@
+#
+# This file is part of apacheconfig software.
+#
+# Copyright (c) 2018-2019, Ilya Etingof <etingof@gmail.com>
+# License: https://github.com/etingof/apacheconfig/LICENSE.rst
+#
+import sys
+
+from apacheconfig import *
+
+try:
+    import unittest2 as unittest
+
+except ImportError:
+    import unittest
+
+
+class LexerTestCase(unittest.TestCase):
+    def setUp(self):
+        ApacheConfigLexer = make_lexer()
+        self.lexer = ApacheConfigLexer()
+
+    def test_whitespace(self):
+        tokens = self.lexer.tokenize('   \t\t  \t \r  \n\n')
+        self.assertFalse(tokens)
+
+    def testOptionAndValueSet(self):
+        text = """\
+a b
+a = b
+a    b
+a= b
+a =b
+a   b
+a "b"
+a = "b"
+"""
+        tokens = self.lexer.tokenize(text)
+        self.assertEqual(tokens, [('a', 'b'), ('a', 'b'), ('a', 'b'),
+                                  ('a', 'b'), ('a', 'b'), ('a', 'b'),
+                                  ('a', 'b'), ('a', 'b')])
+
+    def testLiteralTags(self):
+        text = """\
+<a>
+</a>
+"""
+        tokens = self.lexer.tokenize(text)
+        self.assertEqual(tokens, ['a', 'a'])
+
+    def testExpressionTags(self):
+        text = """\
+    <if a == 1>
+    </if>
+    """
+        tokens = self.lexer.tokenize(text)
+        self.assertEqual(tokens, ['if a == 1', 'if'])
+
+    def testComments(self):
+        text = """\
+#
+# a
+#a
+# a b
+"""
+        tokens = self.lexer.tokenize(text)
+        self.assertEqual(tokens, ['', ' a', 'a', ' a b'])
+
+    def testBlockOptionsAndValues(self):
+        text = """\
+<a>
+a b
+a=b
+a =  b
+a = "b"
+a "b"
+</a>
+"""
+        tokens = self.lexer.tokenize(text)
+        self.assertEqual(tokens, ['a', ('a', 'b'), ('a', 'b'), ('a', 'b'), ('a', 'b'), ('a', 'b'), 'a'])
+
+    def testBlockComments(self):
+        text = """\
+<a>
+#
+# a
+# a b
+</a>
+"""
+        tokens = self.lexer.tokenize(text)
+        self.assertEqual(tokens, ['a', '', ' a', ' a b', 'a'])
+
+    def testBlockBlankLines(self):
+        text = """\
+<a>
+
+
+</a>
+"""
+        tokens = self.lexer.tokenize(text)
+        self.assertEqual(tokens, ['a', 'a'])
+
+    def testIncludesConfigGeneral(self):
+        text = """\
+<<include first.conf>>
+<a>
+<<Include second.conf>>
+</a>
+"""
+        tokens = self.lexer.tokenize(text)
+        self.assertEqual(tokens, ['first.conf', 'a', 'second.conf', 'a'])
+
+    def testIncludesApache(self):
+        text = """\
+include first.conf
+<a>
+Include second.conf
+</a>
+"""
+        tokens = self.lexer.tokenize(text)
+        self.assertEqual(tokens, ['first.conf', 'a', 'second.conf', 'a'])
+
+    def testConfiguration(self):
+        text = """\
+# h
+a = b
+<a>
+  a b
+</a>
+"""
+        tokens = self.lexer.tokenize(text)
+        self.assertEqual(tokens, [' h', ('a', 'b'), 'a', ('a', 'b'), 'a'])
+
+
+suite = unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ packages/apacheconfig/apacheconfig-0.2.8.tar.gz/apacheconfig-0.2.8/tests/unit/test_parser.py
@@ -0,0 +1,345 @@
+#
+# This file is part of apacheconfig software.
+#
+# Copyright (c) 2018-2019, Ilya Etingof <etingof@gmail.com>
+# License: https://github.com/etingof/apacheconfig/LICENSE.rst
+#
+import sys
+
+from apacheconfig import *
+
+try:
+    import unittest2 as unittest
+
+except ImportError:
+    import unittest
+
+
+class ParserTestCase(unittest.TestCase):
+
+    def testOptionAndValue(self):
+        ApacheConfigLexer = make_lexer()
+        ApacheConfigParser = make_parser()
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='statement')
+
+        ast = parser.parse('a b\n')
+        self.assertEqual(ast, ['statement', 'a', 'b'])
+
+        ast = parser.parse('a=b\n')
+        self.assertEqual(ast, ['statement', 'a', 'b'])
+
+        ast = parser.parse('a "b c"\n')
+        self.assertEqual(ast, ['statement', 'a', 'b c'])
+
+    def testHashComments(self):
+            text = """\
+#a
+# b
+c c# c
+c \# # c
+"""
+            ApacheConfigLexer = make_lexer()
+            ApacheConfigParser = make_parser()
+
+            parser = ApacheConfigParser(ApacheConfigLexer(), start='contents')
+
+            ast = parser.parse(text)
+            self.assertEqual(ast, ['contents',
+                                   ['comment', 'a'],
+                                   ['comment', ' b'],
+                                   ['statements', ['statement', 'c', 'c']],
+                                   ['comment', ' c'],
+                                   ['statements', ['statement', 'c', '# # c']]])
+
+    def testCStyleComments(self):
+        text = """\
+/*a*/
+/*
+# b
+*/
+    """
+        ApacheConfigLexer = make_lexer()
+        ApacheConfigParser = make_parser()
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='contents')
+
+        ast = parser.parse(text)
+        self.assertEqual(ast, ['contents', ['comment', 'a'], ['comment', '\n# b\n']])
+
+    def testCStyleCommentsDisabled(self):
+            text = """\
+/*a*/
+/*
+# b
+*/
+        """
+            options = {
+                'ccomments': False
+            }
+
+            ApacheConfigLexer = make_lexer(**options)
+            ApacheConfigParser = make_parser(**options)
+
+            parser = ApacheConfigParser(ApacheConfigLexer(), start='contents')
+
+            self.assertRaises(ApacheConfigError, parser.parse, text)
+
+    def testIncludes(self):
+        text = """\
+include first.conf
+<<include second.conf>>
+"""
+        ApacheConfigLexer = make_lexer()
+        ApacheConfigParser = make_parser()
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='contents')
+
+        ast = parser.parse(text)
+        self.assertEqual(ast, ['contents', ['include', 'first.conf'], ['include', 'second.conf']])
+
+    def testApacheIncludesDisabled(self):
+        text = """\
+include first.conf
+<<include second.conf>>
+"""
+        options = {
+            'useapacheincludes': False
+        }
+
+        ApacheConfigLexer = make_lexer(**options)
+        ApacheConfigParser = make_parser(**options)
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='contents')
+
+        ast = parser.parse(text)
+        self.assertEqual(ast, ['contents', ['include', 'first.conf'], ['include', 'second.conf']])
+
+    def testOptionAndValueSet(self):
+        text = """\
+a b
+a = b
+a    b
+a= b
+a =b
+a   b
+a "b"
+   a = "b"   
+   a =        'b'   
+"""
+        ApacheConfigLexer = make_lexer()
+        ApacheConfigParser = make_parser()
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='statements')
+
+        ast = parser.parse(text)
+        self.assertEqual(ast, ['statements',
+                               ['statement', 'a', 'b'],
+                               ['statement', 'a', 'b'],
+                               ['statement', 'a', 'b'],
+                               ['statement', 'a', 'b'],
+                               ['statement', 'a', 'b'],
+                               ['statement', 'a', 'b'],
+                               ['statement', 'a', 'b'],
+                               ['statement', 'a', 'b'],
+                               ['statement', 'a', 'b']])
+
+    def testBlockWithOptions(self):
+        text = """\
+<a>
+  #a
+  a = "b b"
+  # a b
+  a = "b b"
+</a>
+"""
+        ApacheConfigLexer = make_lexer()
+        ApacheConfigParser = make_parser()
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='block')
+
+        ast = parser.parse(text)
+        self.assertEqual(ast, ['block', 'a',
+                               ['contents',
+                                ['comment', 'a'],
+                                ['statements',
+                                 ['statement', 'a', 'b b']],
+                                ['comment', ' a b'],
+                                ['statements',
+                                 ['statement', 'a', 'b b']]], 'a'])
+
+    def testNestedBlock(self):
+            text = """\
+<a>
+  <b>
+     <c>
+     </c>
+  </b>
+</a>
+"""
+            ApacheConfigLexer = make_lexer()
+            ApacheConfigParser = make_parser()
+
+            parser = ApacheConfigParser(ApacheConfigLexer(), start='block')
+
+            ast = parser.parse(text)
+            self.assertEqual(ast, ['block', 'a',
+                                   ['contents',
+                                    ['block', 'b',
+                                     ['contents',
+                                      ['block', 'c', [], 'c']], 'b']], 'a'])
+
+    def testEmptyBlocks(self):
+        text = """\
+<a/>
+<b/>
+"""
+        ApacheConfigLexer = make_lexer()
+        ApacheConfigParser = make_parser()
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='contents')
+
+        ast = parser.parse(text)
+        self.assertEqual(ast, ['contents',
+                               ['block', 'a', [], 'a'],
+                               ['block', 'b', [], 'b']])
+
+    def testNamedEmptyBlocks(self):
+        text = """\
+<a A/>
+<b B />
+</b B />
+"""
+        ApacheConfigLexer = make_lexer()
+        ApacheConfigParser = make_parser()
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='contents')
+
+        ast = parser.parse(text)
+        self.assertEqual(ast, ['contents',
+                               ['block', 'a A', [], 'a A'],
+                               ['block', 'b B /', [], 'b B /']])
+
+    def testLowerCaseNames(self):
+        text = """\
+<A/>
+<aA>
+  Bb Cc
+</aA>
+"""
+        options = {
+            'lowercasenames': True
+        }
+
+        ApacheConfigLexer = make_lexer(**options)
+        ApacheConfigParser = make_parser(**options)
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='contents')
+
+        ast = parser.parse(text)
+        self.assertEqual(ast, ['contents',
+                               ['block', 'a', [], 'a'],
+                               ['block', 'aa',
+                                ['contents',
+                                 ['statements', ['statement', 'bb', 'Cc']]],
+                                'aa']])
+
+    def testNoStripValues(self):
+        text = """\
+<aA>
+  Bb Cc   \
+
+</aA>
+"""
+        options = {
+            'nostripvalues': True
+        }
+
+        ApacheConfigLexer = make_lexer(**options)
+        ApacheConfigParser = make_parser(**options)
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='contents')
+
+        ast = parser.parse(text)
+        self.assertEqual(ast, ['contents',
+                               ['block', 'aA',
+                                ['contents',
+                                 ['statements', ['statement', 'Bb', 'Cc   ']]],
+                                'aA']])
+
+    def testHereDoc(self):
+        text = """\
+<main>
+    PYTHON <<MYPYTHON
+        def a():
+            x = y
+            return
+    MYPYTHON
+</main>
+"""
+        ApacheConfigLexer = make_lexer()
+        ApacheConfigParser = make_parser()
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='contents')
+
+        ast = parser.parse(text)
+
+        self.assertEqual(ast, ['contents',
+                               ['block', 'main',
+                                ['contents',
+                                 ['statements',
+                                  ['statement',
+                                   'PYTHON', '        def a():\n            x = y\n            return'
+                                   ]
+                                  ]
+                                 ],
+                                'main']])
+
+    def testWholeConfig(self):
+        text = """\
+# a
+a = b
+
+<a>
+  a = b
+
+</a>
+a b
+ <a a>
+  a b \
+
+  c = d
+  # c
+ </a a>
+# a
+"""
+        ApacheConfigLexer = make_lexer()
+        ApacheConfigParser = make_parser()
+
+        parser = ApacheConfigParser(ApacheConfigLexer(), start='config')
+
+        ast = parser.parse(text)
+        self.assertEqual(ast, [
+            'config',
+            ['contents',
+                ['comment', ' a'],
+                ['statements', ['statement', 'a', 'b']],
+                ['block', 'a',
+                 ['contents', ['statements', ['statement', 'a', 'b']]],
+                 'a'],
+                ['statements', ['statement', 'a', 'b']],
+                ['block', 'a a',
+                 ['contents',
+                  ['statements',
+                   ['statement', 'a', 'b'],
+                   ['statement', 'c', 'd']],
+                  ['comment', ' c']],
+                 'a a'],
+                ['comment', ' a']]
+        ])
+
+
+suite = unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite)
