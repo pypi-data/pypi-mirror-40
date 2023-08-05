f98ab5ff777f772964b67f81cea57873a9544c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:37 +0000

    Add package ebusdpy/0.0.7/ebusdpy-0.0.7.tar.gz

---
+++ packages/ebusdpy/ebusdpy-0.0.7.tar.gz/ebusdpy-0.0.7/ebusdpy/ebusdpy.py
@@ -0,0 +1,58 @@
+
+import socket
+
+
+def init(address):
+    try:
+        """ Open the socket at the specified address, call the command sent and return data """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect(address)
+    except socket.timeout:
+        raise socket.timeout(socket.timeout)
+    except socket.error:
+        raise socket.error(socket.error)
+    finally:
+        sock.close()
+
+def read(address, circuit, name, ttl):
+    result = None
+    try:
+        """ Open the socket at the specified address, call the command sent and return data """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect(address)
+        """ Send the command """
+        READ_COMMAND = 'read -m {2} -c {0} {1}\n'
+        command = READ_COMMAND.format(circuit, name, ttl)
+        sock.sendall(command.encode())
+        """ Get the result decoded UTF-8 """
+        result = sock.recv(256).decode('utf-8').rstrip()
+    except socket.timeout:
+        raise socket.timeout(socket.timeout)
+    except socket.error:
+        raise socket.error(socket.error)
+    finally:
+        sock.close()
+    return result
+
+def write(address, circuit, name, value):
+    result = None
+    try:
+        """ Open the socket at the specified address, call the command sent and return data """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect(address)
+        """ Send the command """
+        WRITE_COMMAND = 'write -c {0} {1} {2}\n'
+        command = WRITE_COMMAND.format(circuit, name, value)
+        sock.sendall(command.encode())
+        """ Get the result decoded UTF-8 """
+        result = sock.recv(256).decode('utf-8').rstrip()
+    except socket.timeout:
+        raise socket.timeout(socket.timeout)
+    except socket.error:
+        raise socket.error(socket.error)
+    finally:
+        sock.close()
+    return result
