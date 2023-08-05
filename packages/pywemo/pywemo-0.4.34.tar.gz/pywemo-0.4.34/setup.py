201d6bc7244bf0c147a1d93aeb3165e3ddf6738c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:53 +0000

    Add package pywemo/0.4.34/pywemo-0.4.34.tar.gz

---
+++ packages/pywemo/pywemo-0.4.34.tar.gz/pywemo-0.4.34/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='pywemo',
+      version='0.4.34',
+      description='Access WeMo switches using their SOAP API',
+      url='http://github.com/pavoni/pywemo',
+      author='Greg Dowling',
+      author_email='mail@gregdowling.com',
+      license='MIT',
+      install_requires=['netifaces>=0.10.0', 'requests>=2.0', 'six>=1.10.0'],
+      packages=find_packages(),
+      zip_safe=True)
