e9f34ebd5db388f162278e8dd2962b8e0588e8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:04 +0000

    Add package vulcan-api/0.2.0/vulcan_api-0.2.0-py3-none-any.whl

---
+++ packages/vulcan-api/vulcan_api-0.2.0-py3-none-any.whl/vulcan/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from .vulcan import *
+
+__version__ = '0.2.0'
+__doc__ = 'Nieoficjalne API do dzienniczka elektronicznego UONET+'
+
+__all__ = [
+    'Vulcan',
+]
