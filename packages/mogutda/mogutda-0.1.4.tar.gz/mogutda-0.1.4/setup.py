9e8d101e4c322c28763db668ba4c88cc10075f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:42 +0000

    Add package mogutda/0.1.4/mogutda-0.1.4.tar.gz

---
+++ packages/mogutda/mogutda-0.1.4.tar.gz/mogutda-0.1.4/setup.py
@@ -0,0 +1,37 @@
+
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='mogutda',
+      version="0.1.4",
+      description="Topological Data Analysis in Python",
+      long_description="Topological Data Analysis in Python: Simplicial Complex",
+      classifiers=[
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "Topic :: Scientific/Engineering :: Physics",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "License :: OSI Approved :: MIT License",
+      ],
+      keywords="mogutda numerics topology data",
+      url="https://github.com/stephenhky/MoguTDA",
+      author="Kwan-Yuet Ho",
+      author_email="stephenhky@yahoo.com.hk",
+      license='MIT',
+      packages=['mogutda',],
+      install_requires=[
+          'numpy', 'scipy', 'networkx>=2.0',
+      ],
+      tests_require=[
+          'unittest2',
+      ],
+      include_package_data=True,
+      test_suite="test",
+      zip_safe=False)
+
