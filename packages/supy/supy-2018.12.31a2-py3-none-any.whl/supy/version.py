0aa8c559dd4a09cda473e08f45369644074adc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:37 +0000

    Add package supy/2018.12.31a2/supy-2018.12.31a2-py3-none-any.whl

---
+++ packages/supy/supy-2018.12.31a2-py3-none-any.whl/supy/version.py
@@ -0,0 +1,12 @@
+# version info for supy
+
+ver_milestone = 2018
+ver_major = 12
+ver_minor = 31
+ver_remark = 'a2'
+__version__ = '{ver_milestone}.{ver_major}.{ver_minor}{ver_remark}'.format(
+    ver_milestone=ver_milestone,
+    ver_major=ver_major,
+    ver_minor=ver_minor,
+    ver_remark=ver_remark,
+)
