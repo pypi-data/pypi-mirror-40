1b2f79af985dc997a59b5486dea5d166a1440258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:05 +0000

    Add package evalcache/1.6.0/evalcache-1.6.0-py3-none-any.whl

---
+++ packages/evalcache/evalcache-1.6.0-py3-none-any.whl/evalcache/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+from evalcache.dircache import DirCache
+from evalcache.lazy import Lazy, LazyObject, LazyHash, Memoize
+from evalcache.lazy import unlazy, encache, decache, print_tree
+from evalcache.lazyfile import LazyFile
+
+__version__ = "1.6.0"
