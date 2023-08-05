d422b7b25b2f064235a5a78ea83bda7697e5daec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:02 +0000

    Add package drel/0.4.0/drel-0.4.0-py3-none-any.whl

---
+++ packages/drel/drel-0.4.0-py3-none-any.whl/drel/django/api.py
@@ -0,0 +1,63 @@
+from typing import Dict, Callable
+
+from django.core.mail import mail_admins
+from django.http import HttpRequest, HttpResponse, RawPostDataException
+
+from drel.core import BaseFullRequestLogBuilder, log_to_es, ResponseLog, RequestLog, config
+from drel.utils import to_json
+
+
+class LoggingMiddleware:
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]) -> None:
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        response = self.get_response(request)
+
+        if config.IGNORE_LOGGING_HANDLER(request):
+            return response
+
+        log_entry = DjangoFullRequestLogBuilder(user=request.user)(request, response)
+        log_to_es(log_entry)
+
+        return response
+
+
+class DjangoFullRequestLogBuilder(BaseFullRequestLogBuilder):
+    def request_to_log(self, request: HttpRequest) -> RequestLog:
+        return RequestLog(
+            request.get_full_path(), get_request_data(request), get_request_headers(request)
+        )
+
+    def response_to_log(self, response: HttpResponse) -> ResponseLog:
+        return ResponseLog(get_response_data(response), response.status_code)
+
+
+def get_request_data(request: HttpRequest) -> Dict:
+    try:
+        return request.POST.dict() or to_json(request.body)
+    except RawPostDataException:
+        return {}
+
+
+def get_request_headers(request: HttpRequest) -> Dict:
+    return {key: value for key, value in request.META.items() if key.startswith("HTTP")}
+
+
+def get_response_data(response: HttpResponse) -> Dict:
+    return to_json(response.content)
+
+
+def mail_admins_on_es_exception(index: str, doc: Dict, exception: Exception) -> None:
+    message = _build_exception_message(index, doc, exception)
+    mail_admins("Logging to Elastic Search failed", message)
+
+
+def _build_exception_message(index: str, doc: Dict, exception: Exception) -> str:
+    return f"""Index: {index}
+
+Doc to insert: {doc}
+
+Exception class: {exception.__class__}
+
+Exception details: {exception}"""
---
+++ packages/drel/drel-0.4.0-py3-none-any.whl/drel/requests/api.py
@@ -0,0 +1,43 @@
+from typing import Any, Dict, Tuple, Optional
+
+import requests
+from requests import Response, Request
+
+from drel.core import BaseFullRequestLogBuilder, RequestLog, ResponseLog, log_to_es
+from drel.utils import to_json
+
+
+def log(request: Request, response: Response, **builder_kwargs: Any) -> Optional[str]:
+    log_entry = RequestsFullRequestLogBuilder(**builder_kwargs)(request, response)
+
+    return log_to_es(log_entry)
+
+
+class RequestsFullRequestLogBuilder(BaseFullRequestLogBuilder):
+    def request_to_log(self, request: Request) -> RequestLog:
+        assert request.url
+        return RequestLog(request.url, request.data or request.json, dict(request.headers))
+
+    def response_to_log(self, response: Response) -> ResponseLog:
+        data = self.__get_response_data(response)
+        return ResponseLog(data, response.status_code)
+
+    def __get_response_data(self, response: Response) -> Dict:
+        try:
+            return response.json()
+        except ValueError:
+            return to_json(response.content)
+
+
+def post(*args: Any, **kwargs: Any) -> Tuple[Request, Response]:
+    request = requests.Request("POST", *args, **kwargs)
+    session = requests.Session()
+    response = session.send(request.prepare())
+    return request, response
+
+
+def get(*args: Any, **kwargs: Any) -> Tuple[Request, Response]:
+    request = requests.Request("GET", *args, **kwargs)
+    session = requests.Session()
+    response = session.send(request.prepare())
+    return request, response
