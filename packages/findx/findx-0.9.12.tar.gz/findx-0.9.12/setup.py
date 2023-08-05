c6adf7fab0424ba1dcf4bf1f08ed38cb73f3c920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:20 +0000

    Add package findx/0.9.12/findx-0.9.12.tar.gz

---
+++ packages/findx/findx-0.9.12.tar.gz/findx-0.9.12/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import os
+
+import setuptools
+
+NAME = 'findx'
+
+
+def open_file(name):
+    return open(os.path.join(os.path.dirname(__file__), name))
+
+
+__version__ = None
+for line in open_file(NAME + '.py'):
+    if line.startswith('__version__'):
+        exec(line)
+        break
+
+setuptools.setup(
+    name=NAME,
+    version=__version__,
+    packages=setuptools.find_packages(exclude=['tests']),
+    py_modules=[NAME],
+    entry_points={
+        'console_scripts': [
+            'findx=findx:main',
+            'ffx=findx:ffx',
+            'ffg=findx:ffg',
+        ],
+    },
+    description='``findx``, an extended ``find`` command.',
+    long_description=open_file('README.rst').read(),
+    keywords='extended find file search',
+    url='https://github.com/drmikehenry/findx',
+    author='Michael Henry',
+    author_email='drmikehenry@drmikehenry.com',
+    license='MIT',
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
