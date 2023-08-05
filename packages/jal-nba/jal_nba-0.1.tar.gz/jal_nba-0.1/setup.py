87a64b69e4a827985648eb66a1247c22416afc83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:54 +0000

    Add package jal-nba/0.1/jal_nba-0.1.tar.gz

---
+++ packages/jal-nba/jal_nba-0.1.tar.gz/jal_nba-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jal_nba',
+      version='0.1',
+      description='Retrieve info from NBA API at stats.nba.com/stats/',
+      url='http://github.com/jalgraves/jal_nba',
+      author='Jonny Graves',
+      author_email='jal@jalgraves.com',
+      license='MIT',
+      packages=['jal_nba'],
+      zip_safe=False)
