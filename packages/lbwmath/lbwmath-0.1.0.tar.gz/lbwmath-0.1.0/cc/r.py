d06a535febe07ba5c87c07912aeaa2def55c5ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:28 +0000

    Add package lbwmath/0.1.0/lbwmath-0.1.0.tar.gz

---
+++ packages/lbwmath/lbwmath-0.1.0.tar.gz/lbwmath-0.1.0/cc/r.py
@@ -0,0 +1,2 @@
+def rr():
+    print("t")
---
+++ packages/lbwmath/lbwmath-0.1.0.tar.gz/lbwmath-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name = "lbwmath",
+    version = "0.1.0",
+    keywords = ("pip", "lbwmath","math", "magetool", "mage"),
+    description = "time and path tool",
+    long_description = "time and path tool",
+    license = "MIT Licence",
+
+    url = "https://github.com/fengmm521/pipProject",
+    author = "libowei",
+    author_email = "913911037@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
