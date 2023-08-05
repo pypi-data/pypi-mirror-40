29d28aa79eaa7654e9347014e6baa1ad6a7b8eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package pre-commit-hooks/2.1.0/pre_commit_hooks-2.1.0.tar.gz

---
+++ packages/pre-commit-hooks/pre_commit_hooks-2.1.0.tar.gz/pre_commit_hooks-2.1.0/pre_commit_hooks/debug_statement_hook.py
@@ -0,0 +1,73 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import ast
+import collections
+import traceback
+
+
+DEBUG_STATEMENTS = {'pdb', 'ipdb', 'pudb', 'q', 'rdb'}
+Debug = collections.namedtuple('Debug', ('line', 'col', 'name', 'reason'))
+
+
+class DebugStatementParser(ast.NodeVisitor):
+    def __init__(self):
+        self.breakpoints = []
+
+    def visit_Import(self, node):
+        for name in node.names:
+            if name.name in DEBUG_STATEMENTS:
+                st = Debug(node.lineno, node.col_offset, name.name, 'imported')
+                self.breakpoints.append(st)
+
+    def visit_ImportFrom(self, node):
+        if node.module in DEBUG_STATEMENTS:
+            st = Debug(node.lineno, node.col_offset, node.module, 'imported')
+            self.breakpoints.append(st)
+
+    def visit_Call(self, node):
+        """python3.7+ breakpoint()"""
+        if isinstance(node.func, ast.Name) and node.func.id == 'breakpoint':
+            st = Debug(node.lineno, node.col_offset, node.func.id, 'called')
+            self.breakpoints.append(st)
+        self.generic_visit(node)
+
+
+def check_file(filename):
+    try:
+        with open(filename, 'rb') as f:
+            ast_obj = ast.parse(f.read(), filename=filename)
+    except SyntaxError:
+        print('{} - Could not parse ast'.format(filename))
+        print()
+        print('\t' + traceback.format_exc().replace('\n', '\n\t'))
+        print()
+        return 1
+
+    visitor = DebugStatementParser()
+    visitor.visit(ast_obj)
+
+    for bp in visitor.breakpoints:
+        print(
+            '{}:{}:{} - {} {}'.format(
+                filename, bp.line, bp.col, bp.name, bp.reason,
+            ),
+        )
+
+    return int(bool(visitor.breakpoints))
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to run')
+    args = parser.parse_args(argv)
+
+    retv = 0
+    for filename in args.filenames:
+        retv |= check_file(filename)
+    return retv
+
+
+if __name__ == '__main__':
+    exit(main())
---
+++ packages/pre-commit-hooks/pre_commit_hooks-2.1.0.tar.gz/pre_commit_hooks-2.1.0/pre_commit_hooks/trailing_whitespace_fixer.py
@@ -0,0 +1,86 @@
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+
+
+def _fix_file(filename, is_markdown):
+    with open(filename, mode='rb') as file_processed:
+        lines = file_processed.readlines()
+    newlines = [_process_line(line, is_markdown) for line in lines]
+    if newlines != lines:
+        with open(filename, mode='wb') as file_processed:
+            for line in newlines:
+                file_processed.write(line)
+        return True
+    else:
+        return False
+
+
+def _process_line(line, is_markdown):
+    if line[-2:] == b'\r\n':
+        eol = b'\r\n'
+    elif line[-1:] == b'\n':
+        eol = b'\n'
+    else:
+        eol = b''
+    # preserve trailing two-space for non-blank lines in markdown files
+    if is_markdown and (not line.isspace()) and line.endswith(b'  ' + eol):
+        return line.rstrip() + b'  ' + eol
+    return line.rstrip() + eol
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--no-markdown-linebreak-ext',
+        action='store_true',
+        help=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        '--markdown-linebreak-ext',
+        action='append',
+        default=[],
+        metavar='*|EXT[,EXT,...]',
+        help=(
+            'Markdown extensions (or *) to not strip linebreak spaces.  '
+            'default: %(default)s'
+        ),
+    )
+    parser.add_argument('filenames', nargs='*', help='Filenames to fix')
+    args = parser.parse_args(argv)
+
+    if args.no_markdown_linebreak_ext:
+        print('--no-markdown-linebreak-ext now does nothing!')
+
+    md_args = args.markdown_linebreak_ext
+    if '' in md_args:
+        parser.error('--markdown-linebreak-ext requires a non-empty argument')
+    all_markdown = '*' in md_args
+    # normalize all extensions; split at ',', lowercase, and force 1 leading '.'
+    md_exts = [
+        '.' + x.lower().lstrip('.') for x in ','.join(md_args).split(',')
+    ]
+
+    # reject probable "eaten" filename as extension (skip leading '.' with [1:])
+    for ext in md_exts:
+        if any(c in ext[1:] for c in r'./\:'):
+            parser.error(
+                "bad --markdown-linebreak-ext extension {!r} (has . / \\ :)\n"
+                "  (probably filename; use '--markdown-linebreak-ext=EXT')"
+                .format(ext),
+            )
+
+    return_code = 0
+    for filename in args.filenames:
+        _, extension = os.path.splitext(filename.lower())
+        md = all_markdown or extension in md_exts
+        if _fix_file(filename, md):
+            print('Fixing {}'.format(filename))
+            return_code = 1
+    return return_code
+
+
+if __name__ == '__main__':
+    sys.exit(main())
