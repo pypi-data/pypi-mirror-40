40a9829f285245af1fb9ef2fbcba029e9bb240ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:35 +0000

    Add package sacred-tui/0.1.0/sacred_tui-0.1.0.tar.gz

---
+++ packages/sacred-tui/sacred_tui-0.1.0.tar.gz/sacred_tui-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='sacred_tui',
+    version='0.1.0',
+    description='Python library for ASCII art import and TUI development',
+    url='https://github.com/justaboredkid/sacred',
+    author='justaboredkid',
+    author_email='29010153+justaboredkid@users.noreply.github.com',
+    license='GNU GPLv3',
+    packages=['sacred'],
+    install_requires=[
+        'blessed',
+    ],
+    zip_safe=False)
