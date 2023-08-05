c422452239f15266e22f2d9d785b59dafc46dd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:00 +0000

    Add package fiscal-printer-adapter/0.0.1/fiscal_printer_adapter-0.0.1.tar.gz

---
+++ packages/fiscal-printer-adapter/fiscal_printer_adapter-0.0.1.tar.gz/fiscal_printer_adapter-0.0.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+from scripts import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fiscal_printer_adapter",
+    version=__version__,
+    author="jeo Software",
+    author_email="jorge.obiols@gmail.com",
+    description='A small tool to manage Dockerized Odoo',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jobiols/docker_odoo_env",
+    entry_points={
+        'console_scripts': ['oen=docker_odoo_env.command_line:main'],
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Testing :: Unit",
+        "Topic :: System :: Software Distribution",
+    ],
+      keywords="odoo docker environment",
+)
