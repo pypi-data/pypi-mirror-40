a058653a48d3d817b42eaa067635d7c33c09a823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:06 +0000

    Add package jaraco.net/6.0/jaraco.net-6.0-py2.py3-none-any.whl

---
+++ packages/jaraco.net/jaraco.net-6.0-py2.py3-none-any.whl/jaraco/net/site.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+import argparse
+
+from path import Path
+from jaraco.text import local_format as lf
+
+
+def make_index_cmd():
+	"""
+	Generate an index file for each directory that itself has an
+	index.htm file
+	"""
+	parser = argparse.ArgumentParser()
+	parser.add_argument('root', default=Path('.'), type=Path)
+	args = parser.parse_args()
+	print('<ul>')
+	for dir in sorted(args.root.dirs()):
+		if not (
+			(dir / 'index.htm').exists() or
+			(dir / 'index.html').exists()):
+			continue
+		dir = dir.basename()
+		print(lf('<li><a href="{dir}/index.htm">{dir}</a></li>'))
+	print('</ul>')
