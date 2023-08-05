fccf13c76d1302b9014b424850fd2c34c1400326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package credisuretl/0.1.27/credisuretl-0.1.27.tar.gz

---
+++ packages/credisuretl/credisuretl-0.1.27.tar.gz/credisuretl-0.1.27/credisur/extractors/customer_row_extractor.py
@@ -0,0 +1,10 @@
+def customer_row_extractor(results, unpacker):
+    result = dict()
+
+    name = unpacker.get_value_at(1)
+
+    result['address'] = unpacker.get_value_at(8)
+    result['city'] = unpacker.get_value_at(29)
+    result['phone'] = unpacker.get_value_at(12)
+
+    results[name] = result
