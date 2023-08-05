936011d68faa3abe83debb0c676be6841397b4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:52 +0000

    Add package ccpl/0.1.3/ccpl-0.1.3-py3-none-any.whl

---
+++ packages/ccpl/ccpl-0.1.3-py3-none-any.whl/ccpl/console.py
@@ -0,0 +1,8 @@
+def prints(text="Spam, eggs and bacon, good sir!"):
+    print(100*'\n',text)
+
+def help():
+    prints("Help shall be given thee, who seeked it.")
+
+if __name__ == '__main__':
+    prints("This is the ColorfulCore PythonLib module. It's meant for importing it, not running it.")
