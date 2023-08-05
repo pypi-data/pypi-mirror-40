734cb354312f58cd96e271b0fb34b34024c1eafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:36 +0000

    Add package rocketlog/0.1.3/rocketlog-0.1.3.tar.gz

---
+++ packages/rocketlog/rocketlog-0.1.3.tar.gz/rocketlog-0.1.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+ 
+setup(
+    name='rocketlog',
+    version="0.1.3",
+    author="grayzhang",
+    author_email="plyj0123@163.com",
+    description="update some bug",
+    license="MIT",
+    packages=find_packages(),
+    install_requires = [],
+    url="https://github.com/graylzhang/rocketlog"
+)
