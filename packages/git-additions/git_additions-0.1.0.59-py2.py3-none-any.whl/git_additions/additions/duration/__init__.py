4a177fd21a2374177b4378a5e075feefa2774114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:50 +0000

    Add package git-additions/0.1.0.59/git_additions-0.1.0.59-py2.py3-none-any.whl

---
+++ packages/git-additions/git_additions-0.1.0.59-py2.py3-none-any.whl/git_additions/additions/duration/__init__.py
@@ -0,0 +1,5 @@
+from git_additions.additions.duration.duration import Duration
+
+
+def runner():
+    Duration.report()
---
+++ packages/git-additions/git_additions-0.1.0.59-py2.py3-none-any.whl/git_additions/additions/users/__init__.py
@@ -0,0 +1,5 @@
+from git_additions.additions.users.users import Users
+
+
+def runner():
+    Users.report()
