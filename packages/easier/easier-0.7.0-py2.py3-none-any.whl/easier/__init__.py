2dd890277a2eedf107466d5a0c51c9a37b812299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:39 +0000

    Add package easier/0.7.0/easier-0.7.0-py2.py3-none-any.whl

---
+++ packages/easier/easier-0.7.0-py2.py3-none-any.whl/easier/__init__.py
@@ -0,0 +1,43 @@
+# flake8: noqa
+from .version import __version__
+
+from .timer import Timer
+from .clock import Clock, GlobalClock
+from .param_state import ParamState
+from .item import Item
+from .simple_cron import Cron
+from .memory import mem_show, mem_get
+import easier.hvtools as hv
+from .hvtools import cc, hist
+from .print_catcher import PrintCatcher
+from .plotting import figure
+from .fit import Fit
+from .crypt import Crypt
+from .ecdf import ecdf
+from .cached_loader import CachedLoader
+from .dataframe_tools import (
+    date_diff,
+    slugify,
+    iqr_outlier_killer,
+)
+from .utils import ChattyDict, mute_warnings, cached_property
+
+# alias for ease of remembering the name
+outlier_iqr_killer = iqr_outlier_killer
+warnings_mute = mute_warnings
+DictChatty = ChattyDict
+
+# loads a holoviews color cycler as cc defaulting to None if not available
+try:
+    def get_cc():
+        from string import ascii_lowercase
+        import holoviews as hv
+        cc = type('color', (), dict(zip(ascii_lowercase, hv.Cycle().default_cycles['default_colors'])))
+        return cc
+    cc = get_cc()
+except:  # noqa
+    cc = None
+
+
+
+
