1b2f0910e03dadfe8416bb3e9775e201eabbdc3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:37 +0000

    Add package wosPyFile/2.0.0/wosPyFile-2.0.0-py3-none-any.whl

---
+++ packages/wosPyFile/wosPyFile-2.0.0-py3-none-any.whl/wosPyFile/pyFile.py
@@ -0,0 +1,40 @@
+
+
+def read(fileName):
+    file = open(fileName)
+    lines = []
+    for line in file:
+        lines.append(line)
+    return lines
+
+def readWhen(file, data):
+    lines = []
+    for line in file:
+        for d in data:
+            if(line.startsWith(d)):
+                lines.append(line)
+    return lines
+
+def getLineData(line, array):
+    data = []
+    begin = 0
+    for i in array:
+        data.append(line[begin:i])
+        begin = i
+    return data  
+
+def generateMatrix(pos, lines, size = 1):
+    matriz = []
+    i = 0
+    x = 1
+    while(i < lines):
+        l = []
+        while(x <= pos):
+            l.append(x)
+            x = x + size
+        i = i + 1
+        matriz.append(l)
+    return matriz
+
+def lineString(line):
+    return ''.join(map(str, line))
