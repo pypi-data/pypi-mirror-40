57e6138681aed766cc5289eaf92c15e22d8493b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:16 +0000

    Add package pap-logger/0.0.3/pap_logger-0.0.3.tar.gz

---
+++ packages/pap-logger/pap_logger-0.0.3.tar.gz/pap_logger-0.0.3/setup.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pap_logger",
+    version="0.0.3",
+    author="KurisuD",
+    author_email="KurisuD@pypi.darnand.net",
+    description="A 'prêt-à-porter' logger",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KurisuD/pap_logger",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Logging"
+    ],
+)
