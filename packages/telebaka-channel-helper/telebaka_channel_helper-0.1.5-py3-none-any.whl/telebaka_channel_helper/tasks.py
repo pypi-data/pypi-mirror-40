ab6e5ff5df06f1b97dc60b93970a1e69396ba8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:47 +0000

    Add package telebaka-channel-helper/0.1.5/telebaka_channel_helper-0.1.5-py3-none-any.whl

---
+++ packages/telebaka-channel-helper/telebaka_channel_helper-0.1.5-py3-none-any.whl/telebaka_channel_helper/tasks.py
@@ -0,0 +1,18 @@
+import json
+from celery import shared_task
+from telegram import Update, Bot
+
+from bots.models import TelegramBot
+from telebaka_channel_helper.bot import post_update
+from telebaka_channel_helper.models import PlannedPost
+
+
+@shared_task
+def send_planned_posts():
+    bots = PlannedPost.objects.values('bot').distinct()
+    for bot in bots:
+        bot_instance = bot['bot']  # type: TelegramBot
+        bot = Bot(bot_instance.token)
+        post = PlannedPost.objects.filter(bot=bot_instance).first()
+        if post:
+            post_update(bot_instance, Update.de_json(json.loads(post.update), bot))
