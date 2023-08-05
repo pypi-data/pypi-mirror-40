5811e7999a23acfeeb1b4a8eff0e28dac2851179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:55 +0000

    Add package requests-ratelimit-adapter/0.1.2/requests_ratelimit_adapter-0.1.2-py2.py3-none-any.whl

---
+++ packages/requests-ratelimit-adapter/requests_ratelimit_adapter-0.1.2-py2.py3-none-any.whl/requests_ratelimit_adapter/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for requests-ratelimit-adapter."""
+
+__author__ = """Max Dymond"""
+__email__ = 'cmeister2@gmail.com'
+__version__ = '0.1.2'
+
+from .requests_ratelimit_adapter import RateLimitAdapter
+
+__all__ = ['RateLimitAdapter']
