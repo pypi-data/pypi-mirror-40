eb138175127490ef69fca221dacbd3d0a7160ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:49 +0000

    Add package spell-corrector-pt/0.0.2/spell_corrector_pt-0.0.2.tar.gz

---
+++ packages/spell-corrector-pt/spell_corrector_pt-0.0.2.tar.gz/spell_corrector_pt-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spell_corrector_pt",
+    version="0.0.2",
+    author="Samuel Heinrichs",
+    author_email="ti.samuelh@gmail.com",
+    description="This is a simple tool to correct portuguese misspells automatically.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samuelhei/spell-corrector-pt",
+    install_requires=[r.split('==')[0] for r in open("requirements.txt").read().split("\n")],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
