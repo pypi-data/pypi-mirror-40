296299c83c053d374b6daa2a30156fe8e33e8d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:07 +0000

    Add package stem-dry-run/1.7.1/stem-dry-run-1.7.1.tar.gz

---
+++ packages/stem-dry-run/stem-dry-run-1.7.1.tar.gz/stem-dry-run-1.7.1/cache_fallback_directories.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# Copyright 2016-2018, Damian Johnson and The Tor Project
+# See LICENSE for licensing information
+
+"""
+Caches tor's latest fallback directories.
+"""
+
+import re
+import sys
+
+import stem.descriptor.remote
+import stem.util.system
+
+try:
+  # account for urllib's change between python 2.x and 3.x
+  import urllib.request as urllib
+except ImportError:
+  import urllib2 as urllib
+
+GITWEB_MAN_LOG = 'https://gitweb.torproject.org/tor.git/log/src/or/fallback_dirs.inc'
+FALLBACK_DIR_LINK = "href='/tor.git/commit/src/or/fallback_dirs.inc\?id=([^']*)'"
+
+if __name__ == '__main__':
+  try:
+    fallback_dir_page = urllib.urlopen(GITWEB_MAN_LOG).read()
+    fallback_dir_commit = re.search(FALLBACK_DIR_LINK, fallback_dir_page).group(1)
+  except:
+    print("Unable to determine the latest commit to edit tor's fallback directories: %s" % sys.exc_info()[1])
+    sys.exit(1)
+
+  try:
+    stem_commit = stem.util.system.call('git rev-parse HEAD')[0]
+  except IOError as exc:
+    print("Unable to determine stem's current commit: %s" % exc)
+    sys.exit(1)
+
+  print('Latest tor commit editing fallback directories: %s' % fallback_dir_commit)
+  print('Current stem commit: %s' % stem_commit)
+  print('')
+
+  cached_fallback_directories = stem.descriptor.remote.FallbackDirectory.from_cache()
+  latest_fallback_directories = stem.descriptor.remote.FallbackDirectory.from_remote()
+
+  if cached_fallback_directories == latest_fallback_directories:
+    print('Fallback directories are already up to date, nothing to do.')
+    sys.exit(0)
+
+  # all fallbacks have the same header metadata, so just picking one
+
+  headers = latest_fallback_directories.values()[0].header if latest_fallback_directories else None
+
+  print('Differences detected...\n')
+  print(stem.descriptor.remote._fallback_directory_differences(cached_fallback_directories, latest_fallback_directories))
+  stem.descriptor.remote.FallbackDirectory._write(latest_fallback_directories, fallback_dir_commit, stem_commit, headers)
---
+++ packages/stem-dry-run/stem-dry-run-1.7.1.tar.gz/stem-dry-run-1.7.1/test/integ/util/connection.py
@@ -0,0 +1,83 @@
+"""
+Integration tests for stem.util.connection functions against the tor process
+that we're running.
+"""
+
+import unittest
+
+import stem.util.system
+import test.require
+import test.runner
+
+from stem.util.connection import RESOLVER_COMMAND, Resolver, get_connections, system_resolvers
+
+
+class TestConnection(unittest.TestCase):
+  @test.require.ptrace
+  def check_resolver(self, resolver):
+    runner = test.runner.get_runner()
+
+    if test.runner.Torrc.PORT not in runner.get_options():
+      self.skipTest('(no control port)')
+      return
+    elif resolver not in system_resolvers():
+      self.skipTest('(resolver unavailable on this platform)')
+      return
+
+    with runner.get_tor_socket():
+      connections = get_connections(resolver, process_pid = runner.get_pid())
+
+      for conn in connections:
+        if conn.local_address == '127.0.0.1' and conn.local_port == test.runner.CONTROL_PORT:
+          return
+
+      resolver_command = RESOLVER_COMMAND[resolver].format(pid = runner.get_pid())
+      resolver_output = stem.util.system.call(resolver_command)
+
+      self.fail('Unable to find our controller connection with %s (%s). Connections found were...\n\n%s\n\nCommand output was...\n\n%s' % (resolver, resolver_command, '\n'.join(map(str, connections)), resolver_output))
+
+  def test_connections_by_proc(self):
+    self.check_resolver(Resolver.PROC)
+
+  def test_connections_by_netstat(self):
+    self.check_resolver(Resolver.NETSTAT)
+
+  def test_connections_by_windows_netstat(self):
+    self.check_resolver(Resolver.NETSTAT_WINDOWS)
+
+  def test_connections_by_ss(self):
+    try:
+      self.check_resolver(Resolver.SS)
+    except OSError:
+      self.skipTest('(ticket 27479)')
+
+  def test_connections_by_lsof(self):
+    self.check_resolver(Resolver.LSOF)
+
+  def test_connections_by_bsd_sockstat(self):
+    self.check_resolver(Resolver.BSD_SOCKSTAT)
+
+  def test_connections_by_bsd_procstat(self):
+    self.check_resolver(Resolver.BSD_PROCSTAT)
+
+  def test_connections_by_bsd_fstat(self):
+    self.check_resolver(Resolver.BSD_FSTAT)
+
+  def test_that_we_are_checking_all_resolvers(self):
+    # Quick check to confirm that if we add a new Resolver, we include a test
+    # for it here.
+
+    recognized_resolvers = (
+      Resolver.PROC,
+      Resolver.NETSTAT,
+      Resolver.NETSTAT_WINDOWS,
+      Resolver.SS,
+      Resolver.LSOF,
+      Resolver.SOCKSTAT,
+      Resolver.BSD_SOCKSTAT,
+      Resolver.BSD_PROCSTAT,
+      Resolver.BSD_FSTAT,
+    )
+
+    for resolver in Resolver:
+      self.assertTrue(resolver in recognized_resolvers)
