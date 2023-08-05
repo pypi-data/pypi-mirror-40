d3cd57edf08447fa7297c378fc86a0083bcb83cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:27 +0000

    Add package format-pipfile/0.0.6/format_pipfile-0.0.6-py3-none-any.whl

---
+++ packages/format-pipfile/format_pipfile-0.0.6-py3-none-any.whl/format_pipfile/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Root module for the ``format_pipfile`` package."""
+from __future__ import absolute_import
+
+from future.standard_library import install_aliases
+
+install_aliases()
---
+++ packages/format-pipfile/format_pipfile-0.0.6-py3-none-any.whl/format_pipfile/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""The entry point to the ``format_pipfile`` package."""
+
+from __future__ import absolute_import
+
+if __name__ == "__main__":
+    from format_pipfile.cli import main
+
+    main()
