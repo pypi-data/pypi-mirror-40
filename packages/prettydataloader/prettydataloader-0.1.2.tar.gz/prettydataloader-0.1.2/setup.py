a6329366d1f72985dd37f4a436164a138a565195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:57 +0000

    Add package prettydataloader/0.1.2/prettydataloader-0.1.2.tar.gz

---
+++ packages/prettydataloader/prettydataloader-0.1.2.tar.gz/prettydataloader-0.1.2/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.txt') as f:
+    readme = f.read()
+
+setup(
+    name='prettydataloader',
+    version='0.1.2',
+    description='Pretty Data Loader',
+    long_description=readme,
+    author='Atsushi Nakajima',
+    author_email='summer4an@gmail.com',
+    url='https://github.com/summer4an',
+    license='MIT',
+    packages=find_packages(exclude=('temporal*', 'test*', 'tests', 'docs'))
+)
+
