0ad2319e1070db5894584cee456926b1190887de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:12 +0000

    Add package config-bat/0.1.0/config_bat-0.1.0.tar.gz

---
+++ packages/config-bat/config_bat-0.1.0.tar.gz/config_bat-0.1.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="config_bat",
+    version="0.1.0",
+    author="Ninh Chu",
+    author_email="chulucninh09@gmail.com",
+    description="A small example package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
