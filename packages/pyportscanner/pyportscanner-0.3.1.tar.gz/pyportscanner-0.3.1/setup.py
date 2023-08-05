3ef583d846bf762c93784bb7b0257d3da0a6181b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:46 +0000

    Add package pyportscanner/0.3.1/pyportscanner-0.3.1.tar.gz

---
+++ packages/pyportscanner/pyportscanner-0.3.1.tar.gz/pyportscanner-0.3.1/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGELOG.md')) as f:
+    CHANGES = f.read()
+
+dev_requires = [
+    'pytest',
+    'pytest-cov',
+]
+
+
+setup(
+    author='Yaokai Yang',
+    name='pyportscanner',
+    version='0.3.1',
+    description='Port Scanner for Python3+',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    classifiers=[
+      "Programming Language :: Python",
+    ],
+    url='https://github.com/YaokaiYang-assaultmaster/py3PortScanner',
+    packages=find_packages(),
+    package_data={'pyportscanner': ['etc/*.dat']},
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+      'dev': dev_requires,
+    },
+)
