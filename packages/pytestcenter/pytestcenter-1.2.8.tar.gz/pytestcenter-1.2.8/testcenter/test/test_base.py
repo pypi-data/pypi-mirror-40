e63e1756e6b2ec5d13ee6acf885f5a58c5972b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:59 +0000

    Add package pytestcenter/1.2.8/pytestcenter-1.2.8.tar.gz

---
+++ packages/pytestcenter/pytestcenter-1.2.8.tar.gz/pytestcenter-1.2.8/testcenter/test/test_base.py
@@ -0,0 +1,48 @@
+"""
+Base class for all STC package tests.
+
+@author yoram@ignissoft.com
+"""
+
+from os import path
+import logging
+import pytest
+
+from trafficgenerator.test.test_tgn import TestTgnBase
+
+from testcenter.stc_app import init_stc
+
+
+class TestStcBase(TestTgnBase):
+
+    stc = None
+
+    TestTgnBase.config_file = path.join(path.dirname(__file__), 'TestCenter.ini')
+
+    def setup(self):
+
+        super(TestStcBase, self).setup()
+        self._get_config()
+
+        logging.basicConfig(level=self.config.get('Logging', 'level'))
+        logging.getLogger().addHandler(logging.FileHandler(self.config.get('Logging', 'file_name')))
+        super(TestStcBase, self).setup()
+        self.stc = init_stc(self.api, self.logger, self.config.get('STC', 'install_dir'), self.server_ip,
+                            self.server_port)
+        log_level = self.config.get('STC', 'log_level')
+        self.stc.system.get_child('automationoptions').set_attributes(LogLevel=log_level)
+        self.stc.connect(self.ls)
+
+    def teardown(self):
+        super(TestStcBase, self).teardown()
+        self.stc.disconnect()
+
+    def test_hello_world(self, api):
+        pass
+
+    def _get_config(self):
+
+        server_ip = pytest.config.getoption('--server')  # @UndefinedVariable
+        self.server_ip = server_ip.split(':')[0]
+        self.server_port = server_ip.split(':')[1] if len(server_ip.split(':')) == 2 else 8888
+        self.ls = pytest.config.getoption('--ls')  # @UndefinedVariable
