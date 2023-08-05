d2627e729c0dd701bf580e9a7990a9a5cbddb799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:17 +0000

    Add package pdoc-prep/0.0.1/pdoc_prep-0.0.1.tar.gz

---
+++ packages/pdoc-prep/pdoc_prep-0.0.1.tar.gz/pdoc_prep-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as file:
+    long_description = file.read()
+    
+setup(
+    name = "pdoc_prep",
+    version = "0.0.1",
+    packages = find_packages(),
+
+    # Dependencies on other packages:
+    setup_requires   = [],
+    install_requires = ['pdoc>=0.3.2',
+                        ],
+
+    # Unit tests; they are initiated via 'python setup.py test'
+    test_suite       = 'nose.collector', 
+
+    # metadata for upload to PyPI
+    author="Andreas Paepcke",
+    author_email="paepcke@cs.stanford.edu",
+    description_content_type="text/markdown",
+    description="Add processing of sphinx-like docstring specs to pdoc via preprocessor.",
+    long_description=long_description,
+    license="BSD",
+    keywords="pdoc, python documentation",
+    url="https://github.com/paepcke/pdoc_prep"
+)
