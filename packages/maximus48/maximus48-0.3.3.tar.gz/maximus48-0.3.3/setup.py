20a39a57e79838d82f2820197dc4aed69a72aba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:03 +0000

    Add package maximus48/0.3.3/maximus48-0.3.3.tar.gz

---
+++ packages/maximus48/maximus48-0.3.3.tar.gz/maximus48-0.3.3/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+long_description = u"""
+This is the package made by Maxim Polikarpov (polikarpov.maxim@mail.ru),
+dedicated to flatifield correction, phase retrieval and X-ray tomography.
+"""
+
+
+setup(
+    name='maximus48',
+    version='0.3.3',
+    description='Basic package with my tools',
+    long_description=long_description,
+    author='Maxim Polikarpov',
+    author_email='polikarpov.maxim@mail.ru',
+    url='https://github.com/maximka48/maximus48',
+    platforms='OS Independent',
+    license='MIT License',
+    packages=['maximus48'])
