4c1ebd749e35407b588cdc74a2e939dcc66bc9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:31 +0000

    Add package joylee-jujube-pill/0.0.2/joylee_jujube_pill-0.0.2.tar.gz

---
+++ packages/joylee-jujube-pill/joylee_jujube_pill-0.0.2.tar.gz/joylee_jujube_pill-0.0.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+#  user: joylee
+#  pwd: 9YtZYWaiVR2fEP6
+setup(
+    name='joylee_jujube_pill',
+    version='0.0.2',
+    author='joylee',
+    author_email='1025012627@qq.com',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'吃枣药丸',
+    packages=find_packages(),
+    install_requires=[],
+)
