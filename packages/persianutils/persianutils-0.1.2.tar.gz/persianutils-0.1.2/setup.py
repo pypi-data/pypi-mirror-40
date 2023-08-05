941c0155aa5eab50298ed969c6658af71b7901ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:39 +0000

    Add package persianutils/0.1.2/persianutils-0.1.2.tar.gz

---
+++ packages/persianutils/persianutils-0.1.2.tar.gz/persianutils-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="persianutils",
+    version="0.1.2",
+    author="Iman Nazari",
+    author_email="imannazari@hotmail.com",
+    description="A [getting] wonderfull package to preprocess your Persian text for Search, Standardizing & NLP processes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ishto7/persianutils",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
