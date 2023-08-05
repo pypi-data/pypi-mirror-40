61a74c521bb65d74a7735c97349ce20d038aea9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:23 +0000

    Add package starid/1.3.8/starid-1.3.8.tar.gz

---
+++ packages/starid/starid-1.3.8.tar.gz/starid-1.3.8/starid/__main__.py
@@ -0,0 +1,15 @@
+
+def demo(conf, targetndx):
+    from starimage import Starimg
+    img = Starimg(conf, targetndx)
+    img.print_starlist()
+    img.plot_image()
+    from starlanguages import Sentence
+    sent = Sentence(img)
+    pass
+
+if __name__ == "__main__":
+    from config import Config
+    args = Config.read_args()
+    conf = Config(args)
+    if conf.args.demo: demo(conf, targetndx=3)
