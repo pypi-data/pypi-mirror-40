4cad5f52231c53bfb086c6e9b7887909f0eabd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:58 +0000

    Add package jet-django/0.4.4/jet-django-0.4.4.tar.gz

---
+++ packages/jet-django/jet-django-0.4.4.tar.gz/jet-django-0.4.4/jet_django/views/root.py
@@ -0,0 +1,13 @@
+from jet_django.deps.rest_framework import views
+from jet_django.deps.rest_framework.response import Response
+
+from jet_django import VERSION
+from jet_django.mixins.cors_api_view import CORSAPIViewMixin
+
+
+class RootView(CORSAPIViewMixin, views.APIView):
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'version': VERSION,
+            'type': 'jet_django'
+        })
