59bb8f605af9489342a14512722d50f2bb305721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:31 +0000

    Add package owatahatena/0.1.6/owatahatena-0.1.6.tar.gz

---
+++ packages/owatahatena/owatahatena-0.1.6.tar.gz/owatahatena-0.1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import PIL
+
+setup(
+    name='owatahatena',
+    version='0.1.6',
+    author='owatahatena',
+    author_email='owatahatena@gmail.com',
+    license='MIT',
+    packages=[
+        'owatahatena'
+    ],
+    install_requests=PIL,
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    include_package_data=True
+)
