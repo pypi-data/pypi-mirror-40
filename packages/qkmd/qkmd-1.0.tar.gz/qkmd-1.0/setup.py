9d1c682d3ae03ff9ed0473d3e8651baf7985e366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package qkmd/1.0/qkmd-1.0.tar.gz

---
+++ packages/qkmd/qkmd-1.0.tar.gz/qkmd-1.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='qkmd',
+    version='1.0',
+    author="alopex cheung",
+    author_email="alopex4@163.com",
+    description="Quick markdown what you need, just via a link.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Alopex4/qkmd",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Documentation",
+    ],
+    install_requires=[
+        'pyquery',
+        'pygments',
+        'requests',
+    ],
+    entry_points={'console_scripts': [
+        'qkmd = qkmd.qkmd:command_runner',
+    ]},
+)
