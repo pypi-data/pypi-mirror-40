a4dcf234946a895bedc9b827823e9b6a847bafa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:43 +0000

    Add package srb-lib-004/0.0.1/srb_lib_004-0.0.1-py3-none-any.whl

---
+++ packages/srb-lib-004/srb_lib_004-0.0.1-py3-none-any.whl/commonlib/util.py
@@ -0,0 +1,33 @@
+import sys
+
+from .colour import Colour
+from .debugger import debug
+
+
+_dependency_err_1 = """
+    You haven't installed the required dependencies.
+    Please install required dependencies using :"""
+_dependency_err_2 = """        python3 -m pip install -r requirements.txt
+"""
+def show_dependency_error_and_exit():
+    Colour.print(_dependency_err_1,Colour.RED)
+    Colour.print(_dependency_err_2,Colour.YELLOW)
+    if(debug):
+        import traceback
+        traceback.print_exc()
+    sys.exit(1)
+
+
+def line_adder(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        lines = [x.strip() for x in f.readlines()]
+        if(not line in lines):
+            f.seek(0, 0)
+            f.write(content + '\n' + line + '\n')
+
+def top(container):
+    for item in container:
+        return item
+    return None
---
+++ packages/srb-lib-004/srb_lib_004-0.0.1-py3-none-any.whl/srblib/debugger.py
@@ -0,0 +1,17 @@
+import os
+
+from .srb_json import SrbJson
+on_srbpc = False
+if(os.environ.get('USER') == 'srb'):
+    on_srbpc = True
+
+_debug_template = {'srblib':{'debug':False,'on_travis':False}}
+_debug_json = SrbJson('~/.config/srblib/debug.json',_debug_template)
+
+on_travis = False
+if(os.environ.get('USER') == 'travis'):
+    on_travis = True
+if(not on_travis):
+    on_travis = _debug_json['on_travis']
+
+debug = _debug_json['debug']
