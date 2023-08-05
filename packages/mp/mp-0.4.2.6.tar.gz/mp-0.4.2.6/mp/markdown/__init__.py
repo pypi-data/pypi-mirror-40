861dac349b7cd451ba0ada7f3b654efdd3197679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:37 +0000

    Add package mp/0.4.2.6/mp-0.4.2.6.tar.gz

---
+++ packages/mp/mp-0.4.2.6.tar.gz/mp-0.4.2.6/mp/markdown/__init__.py
@@ -0,0 +1,3 @@
+from mp.markdown.graph import draw_graph
+from mp.markdown.event import draw_event
+from mp.markdown.script import draw_script
---
+++ packages/mp/mp-0.4.2.6.tar.gz/mp-0.4.2.6/mp/version.py
@@ -0,0 +1,12 @@
+# Basic Description
+__name__ = 'mp'
+__version__ = '0.4.2.6'
+__doc__ = """Machine Pseudo-Code"""
+
+# Author Information
+__author__ = 'kerryeon'
+__email__ = 'besqer996@gnu.ac.kr'
+
+# Package Description
+__git__ = 'https://github.com/kerryeon/mp'
+__license__ = 'MIT'
