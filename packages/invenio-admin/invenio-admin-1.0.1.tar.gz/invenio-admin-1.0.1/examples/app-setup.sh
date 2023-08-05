3ce5b6e2b098175dfb15fbc47236430818226cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package invenio-admin/1.0.1/invenio-admin-1.0.1.tar.gz

---
+++ packages/invenio-admin/invenio-admin-1.0.1.tar.gz/invenio-admin-1.0.1/examples/app-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# quit on errors:
+set -o errexit
+
+# quit on unbound symbols:
+set -o nounset
+
+DIR=`dirname "$0"`
+
+cd $DIR
+export FLASK_APP=app.py
+
+# Install specific dependencies
+mkdir $DIR/instance
+
+npm install -g mkdirp
+npm install -g node-sass@3.8.0 clean-css-cli requirejs uglify-js
+
+# Install assets
+flask npm
+cd static
+npm install
+cd ..
+flask collect -v
+flask assets build
+
+# Create the database
+flask db init
+flask db create
