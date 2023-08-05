efab40458e06d061bca246d92a7b3d440bcc72f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:45 +0000

    Add package blacksheep/0.0.6/blacksheep-0.0.6.tar.gz

---
+++ packages/blacksheep/blacksheep-0.0.6.tar.gz/blacksheep-0.0.6/blacksheep/exceptions.pyx
@@ -0,0 +1,42 @@
+
+cdef class InvalidOperation(Exception):
+
+    def __init__(self, str message, object inner_exception=None):
+        super().__init__(message)
+        self.inner_exception = inner_exception
+
+
+cdef class BadRequestFormat(Exception):
+
+    def __init__(self, str message, object inner_exception=None):
+        super().__init__(message)
+        self.inner_exception = inner_exception
+
+
+cdef class HttpException(Exception):
+
+    def __init__(self, int status):
+        self.status = status
+
+
+cdef class BadRequest(HttpException):
+
+    def __init__(self):
+        super().__init__(400)
+
+
+cdef class HttpNotFound(HttpException):
+
+    def __init__(self):
+        super().__init__(404)
+
+
+cdef class InvalidArgument(Exception):
+
+    def __init__(self, str message):
+        super().__init__(message)
+
+
+cdef class MessageAborted(Exception):
+    def __init__(self):
+        super().__init__('The message was aborted before the client sent its whole content.')
