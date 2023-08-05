032346315c26d1c44512512af5abaf6a88eceabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:22 +0000

    Add package bv/0.1.3/bv-0.1.3.tar.gz

---
+++ packages/bv/bv-0.1.3.tar.gz/bv-0.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='bv',
+    version='0.1.3',
+    author='codechenx',
+    author_email='codechenx@gmail.com',
+    url='https://github.com/codechenx/bv',
+    description='Data Viewer in Terminal for Bioinformatician',
+    python_requires='>=3.5',
+    license="MIT License",
+    packages=['bv'],
+    install_requires=["chardet"],
+    package_data={'bv': ['config/*.cfg']},
+    entry_points={
+        'console_scripts': [
+            'bv=bv:bv'
+        ]
+    }
+)
