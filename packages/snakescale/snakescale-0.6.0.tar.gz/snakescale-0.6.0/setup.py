7ee4b25438a056cfb98d6cb31646bcc70a7ef032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:47 +0000

    Add package snakescale/0.6.0/snakescale-0.6.0.tar.gz

---
+++ packages/snakescale/snakescale-0.6.0.tar.gz/snakescale-0.6.0/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+import shutil
+from setuptools import find_packages
+
+from pathlib import Path
+
+PACKAGE = 'snakescale'
+VERSION = '0.6.0'
+
+for path in Path('snakescale/wrappers/').rglob('.snakemake'):
+    shutil.rmtree(path)
+
+setuptools.setup(
+    name=PACKAGE,
+    version=VERSION,
+    author='clintval',
+    author_email='valentine.clint@gmail.com',
+    description='Non-strict wrappers for the data pipelining language Snakemake.',
+    url=f'https://github.com/clintval/{PACKAGE}',
+    download_url=f'https://github.com/clintval/{PACKAGE}/archive/v{VERSION}.tar.gz',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    extras_requires={
+        'full': 'snakemake>=4.5.1'
+    },
+    keywords='snakemake pipeline wrapper',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    project_urls={
+        'Documentation': f'https://{PACKAGE}.readthedocs.io',
+        'Issue Tracker': f'https://github.com/clintval/{PACKAGE}/issues',
+    },
+)
