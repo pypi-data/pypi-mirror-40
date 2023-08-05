3419d9c6043a75633d678cb31fe27bea039aa5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:03 +0000

    Add package nbless/0.1.6/nbless-0.1.6-py3-none-any.whl

---
+++ packages/nbless/nbless-0.1.6-py3-none-any.whl/nbless/__init__.py
@@ -0,0 +1,6 @@
+from nbless.nbuild import nbuild
+from nbless.nbuild import nbuild
+from nbless.nbexec import nbexec
+from nbless.nbconv import nbconv
+
+__all__ = ['nbless', 'nbuild', 'nbexec', 'nbconv']
