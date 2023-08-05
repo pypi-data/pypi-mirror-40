8f599a9dd41c56f9255318b7189cc1b0dbf53912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:11 +0000

    Add package characterizethis/0.1.dev1/characterizethis-0.1.dev1.tar.gz

---
+++ packages/characterizethis/characterizethis-0.1.dev1.tar.gz/characterizethis-0.1.dev1/README.md
@@ -0,0 +1,23 @@
+# characerizeTHIS
+
+Tools to find out what we should characterize!
+
+### Assumptions
+
+* Assume all planets have atmospheres (this is likely true for most planets)
+* Assume no planets have clouds or hazes (this is unlikely)
+* Assume a circular orbit (this is largely true)
+* Assume HST WFC3 for followup instrument (this is reasonable)
+* Assume 1 second exposure time with a scan rate of 0.15s (this could be improved for each target)
+* Assume HST WFC3 has no saturation limit. (This could be acheived with scanning)
+* Assume one observation is taken (this is an underestimate)
+* Assume 50 pixels of the detector are illuminated for the scan (this is reasonable)
+* Assume 5 scale heights of atmsophere (this is an approximation)
+* Assume atmosphere is purely hyrdogen (this is likely to be wrong for most small planets)
+* Assume atmosphere is entirely opaque in the WFC3 bandpass (this is an overestimate)
+* Assume starspot free (this is unlikely)
+* Where masses are unknown, assume mass-radius relation from Weiss 2014 (this is an approximation)
+* Assume 2MASS photometry is correct (this is reasonable)
+* Assume planets have no reflection, all light us absorbed
+
+All told, this will provide an overestimate of the signal or the 'best case' scenario.
---
+++ packages/characterizethis/characterizethis-0.1.dev1.tar.gz/characterizethis-0.1.dev1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+import sys
+from setuptools import setup
+
+if "testpublish" in sys.argv[-1]:
+    os.system("python setup.py sdist upload -r pypitest")
+    sys.exit()
+elif "publish" in sys.argv[-1]:
+    os.system("python setup.py sdist upload -r pypi")
+    sys.exit()
+
+# Load the __version__ variable without importing the package
+exec(open('characterizethis/version.py').read())
+
+entry_points = {'console_scripts':
+                ['characterizethis = characterizethis.ui:characterizethis']}
+
+setup(name='characterizethis',
+      version=__version__,
+      description="Creates movies of Kepler/K2 asteroids based on target names. "
+                  ,
+      long_description=open('README.md').read(),
+      author='Christina Hedges',
+      author_email='christina.l.hedges@nasa.gov',
+      license='MIT',
+      packages=['characterizethis'],
+      package_data={'characterizethis': ['data/*.csv']},
+      install_requires=['astropy>=0.4',
+                        'numpy',
+                        'pandas'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'pytest-cov', 'pytest-remotedata'],
+      include_package_data=True,
+      entry_points=entry_points,
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Intended Audience :: Science/Research",
+          "Topic :: Scientific/Engineering :: Astronomy",
+          ],
+      )
