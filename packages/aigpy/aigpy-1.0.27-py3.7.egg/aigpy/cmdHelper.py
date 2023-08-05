53a86ee5a7560ae3c0d988ef68be2cb49f5a2f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:16 +0000

    Add package aigpy/1.0.27/aigpy-1.0.27-py3.7.egg

---
+++ packages/aigpy/aigpy-1.0.27-py3.7.egg/aigpy/cmdHelper.py
@@ -0,0 +1,13 @@
+import sys
+
+# Compatible with python 2.x version
+def myinput(desc):
+    v = sys.version_info
+    if v[0] > 2:
+        return input(desc)
+    else:
+        return raw_input(desc)
+
+
+
+
