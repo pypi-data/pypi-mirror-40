4c49bbfcb792abe5a675a57b2635b576ebe4e7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package lino-cosi/18.12.1/lino-cosi-18.12.1.tar.gz

---
+++ packages/lino-cosi/lino-cosi-18.12.1.tar.gz/lino-cosi-18.12.1/lino_cosi/lib/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2013-2017 Rumma & Ko Ltd
+# License: BSD (see file COPYING for details)
+
+"""
+
+Plugins
+=======
+
+.. autosummary::
+   :toctree:
+
+    cosi
+    contacts
+    b2c
+
+Not used
+========
+
+.. autosummary::
+   :toctree:
+
+    orders
+
+"""
+
