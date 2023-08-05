7fbe90225cde48ece37be8032ca2f08c99501b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:02 +0000

    Add package docker-odoo-env/0.0a10/docker_odoo_env-0.0a10-py2-none-any.whl

---
+++ packages/docker-odoo-env/docker_odoo_env-0.0a10-py2-none-any.whl/docker_odoo_env/__init__.py
@@ -0,0 +1,2 @@
+__name__ = "docker_odoo_env"
+__version__ = '0.0.a10'
---
+++ packages/docker-odoo-env/docker_odoo_env-0.0a10-py2-none-any.whl/docker_odoo_env/commands/update_all.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# For copyright and license notices, see __manifest__.py file in module root
+
+from docker_odoo_env.commands.command import Command
+
+
+class UpdateAll(Command):
+
+    def execute(self):
+        pass
