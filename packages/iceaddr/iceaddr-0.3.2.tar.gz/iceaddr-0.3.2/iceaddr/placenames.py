bfbfdf5c2033cb288896b1a01274b3a5c42f7cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:29 +0000

    Add package iceaddr/0.3.2/iceaddr-0.3.2.tar.gz

---
+++ packages/iceaddr/iceaddr-0.3.2.tar.gz/iceaddr-0.3.2/iceaddr/placenames.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+"""
+    iceaddr: Look up information about Icelandic street addresses and postcodes
+    Copyright (c) 2018 Sveinbjorn Thordarson
+"""
+
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import re
+from .db import shared_db
+
+def placename_lookup(placename, partial=False):
+    q = "SELECT * FROM ornefni WHERE nafn=?"
+    if partial:
+    	q = "SELECT * FROM ornefni WHERE nafn LIKE '?%'"
+
+    db_conn = shared_db.connection()
+    res = db_conn.cursor().execute(q, [placename])
+    return [dict(row) for row in res]
