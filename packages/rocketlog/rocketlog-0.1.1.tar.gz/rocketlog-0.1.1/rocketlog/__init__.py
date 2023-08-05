59d51c55901d7d7c350b385d70067a26b7cb649d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:16 +0000

    Add package rocketlog/0.1.1/rocketlog-0.1.1.tar.gz

---
+++ packages/rocketlog/rocketlog-0.1.1.tar.gz/rocketlog-0.1.1/rocketlog/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1.0'
+
+from .api.log_handler import LogHandler
+from .api.client import Client
+from .agent.agent import Agent
+
+# 向下兼容
+FlyLogHandler = LogHandler
---
+++ packages/rocketlog/rocketlog-0.1.1.tar.gz/rocketlog-0.1.1/rocketlog/constants.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+AGENT_LOGGER_NAME = 'rocketlog'
+AGENT_HOST = '127.0.0.1'
+AGENT_PORT = 12000
