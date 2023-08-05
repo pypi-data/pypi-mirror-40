9d0b724b6db7590b6256b67930d46f0103302e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:35 +0000

    Add package xintool/0.0.6/xintool-0.0.6.tar.gz

---
+++ packages/xintool/xintool-0.0.6.tar.gz/xintool-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from os import path
+from setuptools import setup
+
+current_path = path.abspath(path.dirname(__file__))
+
+with open(path.join(current_path, 'README.md'), 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='xintool',
+    version='0.0.6',
+    description='python opensource tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://blog.jiangyixin.top',
+    author='jiangyixin',
+    author_email='jyx15221613915@gmail.com',
+    keywords='tools library',
+    packages=['xintool'],
+    install_requires=[],
+    entry_points={}
+)
