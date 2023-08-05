7c1340eabc3aa71f62e170fffc3ff596ebcdb905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:05 +0000

    Add package sqlalchemy-vertica-python/0.4.3/sqlalchemy-vertica-python-0.4.3.tar.gz

---
+++ packages/sqlalchemy-vertica-python/sqlalchemy-vertica-python-0.4.3.tar.gz/sqlalchemy-vertica-python-0.4.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+version = '0.4.3'
+setup(
+    name='sqlalchemy-vertica-python',
+    version=version,
+    description='Vertica dialect for sqlalchemy using vertica_python',
+    long_description=open("README.rst").read(),
+    license="MIT",
+    url='https://github.com/LocusEnergy/sqlalchemy-vertica-python',
+    download_url = 'https://github.com/LocusEnergy/sqlalchemy-vertica-python/tarball/{}'.format(version),
+    author='Locus Energy',
+    author_email='dbio@locusenergy.com',
+    packages=[
+        'sqla_vertica_python',
+    ],
+    entry_points="""
+    [sqlalchemy.dialects]
+    vertica.vertica_python = sqla_vertica_python.vertica_python:VerticaDialect
+    """,
+    install_requires=[
+        'vertica_python[namedparams]'
+    ],
+)
+
