6f1fb736f55f0543c133e2c8be20c18dd7fee4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package bocadillo/0.8.1/bocadillo-0.8.1.tar.gz

---
+++ packages/bocadillo/bocadillo-0.8.1.tar.gz/bocadillo-0.8.1/bocadillo/__init__.py
@@ -0,0 +1,8 @@
+from .api import API
+from .staticfiles import static
+from .middleware import Middleware
+from .media import Media
+from .recipes import Recipe
+from .exceptions import HTTPError
+
+__version__ = "0.8.1"
