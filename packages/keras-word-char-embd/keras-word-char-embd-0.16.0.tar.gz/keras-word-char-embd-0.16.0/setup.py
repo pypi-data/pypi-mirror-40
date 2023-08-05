4969a7ee161a726c4405ea84c119caf7981162ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package keras-word-char-embd/0.16.0/keras-word-char-embd-0.16.0.tar.gz

---
+++ packages/keras-word-char-embd/keras-word-char-embd-0.16.0.tar.gz/keras-word-char-embd-0.16.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='keras-word-char-embd',
+    version='0.16.0',
+    packages=['keras_wc_embd'],
+    url='https://github.com/CyberZHG/keras-word-char-embd',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='Concatenate word and character embeddings in Keras',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'keras',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
