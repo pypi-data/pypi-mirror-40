6d2e71f7b474706adac22d0cb219a86b26ef5b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:32 +0000

    Add package kh-pypi-test/0.3/kh-pypi-test-0.3.tar.gz

---
+++ packages/kh-pypi-test/kh-pypi-test-0.3.tar.gz/kh-pypi-test-0.3/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='kh-pypi-test',
+    version='0.3',
+    description='A trivial test project to upload to PyPi',
+    author='Kyle Hornberg',
+    author_email='kyle.hornberg@gmail.com',
+    url='https://github.com/khornberg/pypi',
+    packages=['kh_pypi_test'],
+)
