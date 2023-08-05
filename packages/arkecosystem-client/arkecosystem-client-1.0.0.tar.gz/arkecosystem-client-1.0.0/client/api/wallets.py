7ca09cf89c12e2ed0e27ab0de4ab7fe5878f4267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:06 +0000

    Add package arkecosystem-client/1.0.0/arkecosystem-client-1.0.0.tar.gz

---
+++ packages/arkecosystem-client/arkecosystem-client-1.0.0.tar.gz/arkecosystem-client-1.0.0/client/api/wallets.py
@@ -0,0 +1,56 @@
+from client.resource import Resource
+
+
+class Wallets(Resource):
+
+    def all(self, page=None, limit=100):
+        params = {
+            'page': page,
+            'limit': limit,
+        }
+        return self.request_get('wallets', params)
+
+    def top(self, page=None, limit=100):
+        params = {
+            'page': page,
+            'limit': limit
+        }
+        return self.request_get('wallets/top', params)
+
+    def get(self, wallet_id):
+        return self.request_get('wallets/{}'.format(wallet_id))
+
+    def transactions(self, wallet_id, page=None, limit=100):
+        params = {
+            'page': page,
+            'limit': limit,
+        }
+        return self.request_get('wallets/{}/transactions'.format(wallet_id), params)
+
+    def transactions_sent(self, wallet_id, page=None, limit=100):
+        params = {
+            'page': page,
+            'limit': limit,
+        }
+        return self.request_get('wallets/{}/transactions/sent'.format(wallet_id), params)
+
+    def transactions_received(self, wallet_id, page=None, limit=100):
+        params = {
+            'page': page,
+            'limit': limit,
+        }
+        return self.request_get('wallets/{}/transactions/received'.format(wallet_id), params)
+
+    def votes(self, wallet_id, page=None, limit=100):
+        params = {
+            'page': page,
+            'limit': limit,
+        }
+        return self.request_get('wallets/{}/votes'.format(wallet_id), params)
+
+    def search(self, criteria, page=None, limit=100):
+        params = {
+            'page': page,
+            'limit': limit,
+        }
+        return self.request_post('wallets/search', data=criteria, params=params)
