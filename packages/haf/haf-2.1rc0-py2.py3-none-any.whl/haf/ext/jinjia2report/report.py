4631c2de30993a8ea8aedb2107d75c8bcb39ecb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:01 +0000

    Add package haf/2.1rc0/haf-2.1rc0-py2.py3-none-any.whl

---
+++ packages/haf/haf-2.1rc0-py2.py3-none-any.whl/haf/ext/jinjia2report/report.py
@@ -0,0 +1,57 @@
+# encoding='utf-8'
+import os
+
+from jinja2.environment import TemplateStream
+
+from haf.result import EndResult
+from jinja2 import Environment, FileSystemLoader
+from haf.common.log import Log
+
+logger = Log.getLogger(__name__)
+
+
+class Jinja2Report(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_template(key: str) -> None:
+        if key == "base":
+            template = "base.html"
+        elif key == "online":
+            template = "base_online.html"
+
+        local_dir = os.path.dirname(__file__)
+        j2r = FileSystemLoader(f"{local_dir}/../resource/report/")
+        j2r_e = Environment(loader=j2r, trim_blocks=True, lstrip_blocks=True, autoescape=True)
+        j2r_t = j2r_e.get_template(template)
+        return j2r_t
+
+    @staticmethod
+    def report(results: EndResult):
+        if results is None:
+            return {"status": "error", "msg": "results is None"}
+        template = Jinja2Report.get_template("base")
+        return template.generate(results=results)
+
+    @staticmethod
+    def write_report_to_file(info, report_path: str):
+        try:
+            logger.info(f"generate report to {report_path}")
+            stream = TemplateStream(info)
+            stream.dump(report_path)
+        except Exception as e:
+            logger.error(e)
+
+    @staticmethod
+    def report_online(results: EndResult):
+        if results is None:
+            return {"status": "error", "msg": "results is None"}
+        template = Jinja2Report.get_template("online")
+        return template.render(results=results)
+
+
+
+
+
+
---
+++ packages/haf/haf-2.1rc0-py2.py3-none-any.whl/haf/ext/resource/report/js/base.js
@@ -0,0 +1,13 @@
+
+function showElement(id){
+    console.log(id);
+    if(document.getElementById(id).style.display=="none"){
+        document.getElementById(id).style.display = "block";
+    } else {
+        document.getElementById(id).style.display = "none";
+    }
+}
+
+function showSuccess(ids){
+
+}
