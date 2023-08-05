cf83eae6a7cba7632648a9450b475d3042fc0649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:25 +0000

    Add package pytablewriter/0.37.0/pytablewriter-0.37.0.tar.gz

---
+++ packages/pytablewriter/pytablewriter-0.37.0.tar.gz/pytablewriter-0.37.0/pytablewriter/writer/binary/_interface.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+import abc
+
+import six
+
+from .._table_writer import AbstractTableWriter
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BinaryWriterInterface(object):
+    @abc.abstractmethod
+    def open(self, file_path):  # pragma: no cover
+        pass
+
+
+class AbstractBinaryTableWriter(AbstractTableWriter, BinaryWriterInterface):
+    def dumps(self):
+        raise NotImplementedError("binary format writers did not support dumps method")
