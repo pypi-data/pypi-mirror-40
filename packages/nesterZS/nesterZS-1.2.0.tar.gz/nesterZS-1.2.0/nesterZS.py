ef2dd459b8c39bbe311d85cd3622578cc27de253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:53 +0000

    Add package nesterZS/1.2.0/nesterZS-1.2.0.tar.gz

---
+++ packages/nesterZS/nesterZS-1.2.0.tar.gz/nesterZS-1.2.0/nesterZS.py
@@ -0,0 +1,16 @@
+"""This is the "nester-Zacsoft- module
+  a module that defines a function print_lol() that prints out a list
+  nested or not nested as a single list"""
+def print_lol(the_list, indentation=0):
+        """This function takes a positional argument called "the_list", that
+       which is a Python list (of possibly nested list) It checks if the list we passed
+           is a nested list or  not and each data item in the provided list is
+           recusrsively printed to the screen on its own line.
+           the argument "indentation" defines the number of tab stops used for indentation of lists"""
+        for each_item in the_list:
+            if isinstance(each_item, list):
+                print_lol(each_item, indentation+1)
+            else:
+                for tab_stop in range(indentation):
+                    print("\t", end='')
+                print(each_item)
