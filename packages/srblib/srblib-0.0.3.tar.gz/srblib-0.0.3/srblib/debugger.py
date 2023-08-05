fcf6d95b94b2db6c8995530909e87ac45a3b196c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:35 +0000

    Add package srblib/0.0.3/srblib-0.0.3.tar.gz

---
+++ packages/srblib/srblib-0.0.3.tar.gz/srblib-0.0.3/srblib/debugger.py
@@ -0,0 +1,14 @@
+import os
+
+from .srb_json import SrbJson
+on_srbpc = False
+if(os.environ['USER'] == 'srb'):
+    on_srbpc = True
+
+on_travis = False
+if(os.environ['USER'] == 'travis'):
+    on_travis = True
+
+_debug_template = {'srblib':{'debug':False}}
+_debug_json = SrbJson('~/.config/srblib/debug.json',_debug_template)
+debug = _debug_json['debug']
