968323afd8d7e1f2868b6e0bd6a40d18dbcacdf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:18 +0000

    Add package OMEMO/0.10.1/OMEMO-0.10.1.tar.gz

---
+++ packages/OMEMO/OMEMO-0.10.1.tar.gz/OMEMO-0.10.1/omemo/exceptions/untrustedexception.py
@@ -0,0 +1,28 @@
+class UntrustedException(Exception):
+    def __init__(self, bare_jid, device, ik):
+        self.__bare_jid = bare_jid
+        self.__device   = device
+        self.__ik       = ik
+
+    @property
+    def bare_jid(self):
+        return self.__bare_jid
+
+    @property
+    def device(self):
+        return self.__device
+
+    @property
+    def ik(self):
+        return self.__ik
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, UntrustedException) and
+            other.bare_jid == self.bare_jid and
+            other.device == self.device and
+            other.ik == self.ik
+        )
+
+    def __hash__(self):
+        return hash((self.bare_jid, self.device, self.ik))
