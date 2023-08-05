e01d089ceab6027f14c311b4738d0132d33f04b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:23 +0000

    Add package cognite-sdk/0.11.23/cognite-sdk-0.11.23.tar.gz

---
+++ packages/cognite-sdk/cognite-sdk-0.11.23.tar.gz/cognite-sdk-0.11.23/cognite/__init__.py
@@ -0,0 +1,21 @@
+"""Cognite API SDK
+
+This package has been created to ensure excellent excellent user experience for data scientists using the Cognite
+Data Platform (CDP).
+"""
+
+# RELEASE CONVENTIONS
+#
+# Format:
+# MAJOR.MINOR[.PATCH]
+#
+# Increment the:
+# * MAJOR version when you make changes which are NOT backwards-compatible
+# * MINOR version when you add functionality in a backwards-compatible manner
+# * PATCH version when you make backwards-compatible bug fixes.
+#
+# For more information on versioning see https://semver.org/
+#
+
+__all__ = ["v04", "v05", "v06", "preprocessing", "config", "data_transfer_service"]
+__version__ = "0.11.23"
