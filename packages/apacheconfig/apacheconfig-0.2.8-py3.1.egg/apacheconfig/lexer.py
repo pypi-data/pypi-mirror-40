7097d7c5616c64885239ce1318f5e1a101a0a784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:24 +0000

    Add package apacheconfig/0.2.8/apacheconfig-0.2.8-py3.1.egg

---
+++ packages/apacheconfig/apacheconfig-0.2.8-py3.1.egg/apacheconfig/lexer.py
@@ -0,0 +1,304 @@
+#
+# This file is part of apacheconfig software.
+#
+# Copyright (c) 2018-2019, Ilya Etingof <etingof@gmail.com>
+# License: https://github.com/etingof/apacheconfig/LICENSE.rst
+#
+import logging
+import re
+import ply.lex as lex
+
+from apacheconfig.error import ApacheConfigError
+
+log = logging.getLogger(__name__)
+
+
+class SingleQuotedString(str):
+    is_single_quoted = True
+
+
+class DoubleQuotedString(str):
+    is_double_quoted = True
+
+
+class HashCommentsLexer(object):
+    tokens = (
+        'HASHCOMMENT',
+    )
+
+    states = ()
+
+    def t_HASHCOMMENT(self, t):
+        r'(?<!\\)\#[^\n\r]*'
+        t.value = t.value[1:]
+        return t
+
+
+class CStyleCommentsLexer(object):
+    tokens = (
+        'CCOMMENT',
+    )
+
+    states = (
+        ('ccomment', 'exclusive'),
+    )
+
+    def t_CCOMMENT(self, t):
+        r'\/\*'
+        t.lexer.code_start = t.lexer.lexpos
+        t.lexer.ccomment_level = 1  # Initial comment level
+        t.lexer.begin('ccomment')
+
+    def t_ccomment_open(self, t):
+        r'\/\*'
+        t.lexer.ccomment_level += 1
+
+    def t_ccomment_close(self, t):
+        r'\*\/'
+        t.lexer.ccomment_level -= 1
+
+        if t.lexer.ccomment_level == 0:
+            t.value = t.lexer.lexdata[t.lexer.code_start:t.lexer.lexpos + 1 - 3]
+            t.type = "CCOMMENT"
+            t.lexer.lineno += t.value.count('\n')
+            t.lexer.begin('INITIAL')
+            return t
+
+    def t_ccomment_body(self, t):
+        r'.+?'
+
+    def t_ccomment_error(self, t):
+        raise ApacheConfigError("Illegal character '%s' in C-style comment" % t.value[0])
+
+
+class ApacheIncludesLexer(object):
+    tokens = (
+        'APACHEINCLUDE',
+        'APACHEINCLUDEOPTIONAL'
+    )
+
+    states = ()
+
+    def t_APACHEINCLUDE(self, t):
+        r'(?i)include[\t ]+[^\n\r]+'
+        t.value = t.value.split(None, 1)[1]
+        return t
+
+    def t_APACHEINCLUDEOPTIONAL(self, t):
+        r'(?i)includeoptional[\t ]+[^\n\r]+'
+        t.value = t.value.split(None, 1)[1]
+        return t
+
+
+class BaseApacheConfigLexer(object):
+
+    tokens = (
+        'INCLUDE',
+        'OPEN_TAG',
+        'CLOSE_TAG',
+        'OPEN_CLOSE_TAG',
+        'OPTION_AND_VALUE',
+    )
+
+    states = (
+        ('multiline', 'exclusive'),
+        ('heredoc', 'exclusive'),
+    )
+
+    def __init__(self, tempdir=None, debug=False):
+        self._tempdir = tempdir
+        self._debug = debug
+        self.engine = None
+        self.reset()
+
+    def reset(self):
+        self.engine = lex.lex(
+            module=self,
+            reflags=re.DOTALL,
+            outputdir=self._tempdir,
+            debuglog=log if self._debug else None,
+            errorlog=log if self._debug else None
+        )
+
+    def tokenize(self, text):
+        self.engine.input(text)
+
+        tokens = []
+
+        while True:
+            token = self.engine.token()
+            if not token:
+                break
+            tokens.append(token.value)
+
+        return tokens
+
+    # Tokenizer rules
+
+    def t_INCLUDE(self, t):
+        r'<<(?i)include[\t ]+[^\n\r\t]+>>'
+        t.value = t.value[2:-2].split(None, 1)[1]
+        return t
+
+    def t_CLOSE_TAG(self, t):
+        r'</[^\n\r]+>'
+        t.value = t.value[2:-1]
+        return t
+
+    def t_OPEN_CLOSE_TAG(self, t):
+        r'<[^\n\r/]*?[^\n\r/ ]/>'
+        t.value = t.value[1:-2]
+        return t
+
+    def t_OPEN_TAG(self, t):
+        r'<[^\n\r]+>'
+        t.value = t.value[1:-1]
+        return t
+
+    @staticmethod
+    def _parse_option_value(token):
+        if not re.match(r'.*?[ \n\r\t=]+', token):
+            raise ApacheConfigError('Syntax error in option-value pair %s' % token)
+        option, value = re.split(r'[ \n\r\t=]+', token, maxsplit=1)
+        if not option:
+            raise ApacheConfigError('Syntax error in option-value pair %s' % token)
+        if value:
+            stripped = value.strip()
+            if stripped[0] == '"' and stripped[-1] == '"':
+                value = DoubleQuotedString(stripped[1:-1])
+            if stripped[0] == "'" and stripped[-1] == "'":
+                value = SingleQuotedString(stripped[1:-1])
+        if '#' in value:
+            value = value.replace('\\#', '#')
+        return option, value
+
+    def _pre_parse_value(self, option, value):
+        try:
+            pre_parse_value = self.options['plug']['pre_parse_value']
+            return pre_parse_value(option, value)
+
+        except KeyError:
+            return True, option, value
+
+    def t_OPTION_AND_VALUE(self, t):
+        r'[^ \n\r\t=#]+[ \t=]+[^\r\n#]+'  # TODO(etingof) escape hash
+        if t.value.endswith('\\'):
+            t.lexer.multiline_newline_seen = False
+            t.lexer.code_start = t.lexer.lexpos - len(t.value)
+            t.lexer.begin('multiline')
+            return
+
+        lineno = len(re.findall(r'\r\n|\n|\r', t.value))
+
+        option, value = self._parse_option_value(t.value)
+
+        process, option, value = self._pre_parse_value(option, value)
+        if not process:
+            return
+
+        if value.startswith('<<'):
+            t.lexer.heredoc_anchor = value[2:].strip()
+            t.lexer.heredoc_option = option
+            t.lexer.code_start = t.lexer.lexpos
+            t.lexer.begin('heredoc')
+            return
+
+        t.value = option, value
+
+        t.lexer.lineno += lineno
+
+        return t
+
+    def t_multiline_OPTION_AND_VALUE(self, t):
+        r'[^\r\n]+'
+        t.lexer.multiline_newline_seen = False
+
+        if t.value.endswith('\\'):
+            return
+
+        t.type = "OPTION_AND_VALUE"
+        t.lexer.begin('INITIAL')
+
+        value = t.lexer.lexdata[t.lexer.code_start:t.lexer.lexpos + 1]
+        t.lexer.lineno += len(re.findall(r'\r\n|\n|\r', value))
+        value = value.replace('\\\n', '').replace('\r', '').replace('\n', '')
+
+        option, value = self._parse_option_value(value)
+
+        process, option, value = self._pre_parse_value(option, value)
+        if not process:
+            return
+
+        t.value = option, value
+
+        return t
+
+    def t_multiline_NEWLINE(self, t):
+        r'\r\n|\n|\r'
+        if t.lexer.multiline_newline_seen:
+            return self.t_multiline_OPTION_AND_VALUE(t)
+        t.lexer.multiline_newline_seen = True
+
+    def t_multiline_error(self, t):
+        raise ApacheConfigError("Illegal character '%s' in multiline text" % t.value[0])
+
+    def t_heredoc_OPTION_AND_VALUE(self, t):
+        r'[^\r\n]+'
+        if t.value.lstrip() != t.lexer.heredoc_anchor:
+            return
+
+        t.type = "OPTION_AND_VALUE"
+        t.lexer.begin('INITIAL')
+
+        value = t.lexer.lexdata[t.lexer.code_start + 1:t.lexer.lexpos - len(t.lexer.heredoc_anchor)]
+
+        t.lexer.lineno += len(re.findall(r'\r\n|\n|\r', t.value))
+
+        t.value = t.lexer.heredoc_option, value
+
+        return t
+
+    def t_heredoc_NEWLINE(self, t):
+        r'\r\n|\n|\r'
+        t.lexer.lineno += 1
+
+    def t_heredoc_error(self, t):
+        raise ApacheConfigError("Illegal character '%s' in here-document text" % t.value[0])
+
+    def t_WHITESPACE(self, t):
+        r'[ \t]+'
+
+    def t_NEWLINE(self, t):
+        r'\r\n|\n|\r|\\'
+        if t.value != '\\':
+            t.lexer.lineno += 1
+
+    def t_error(self, t):
+        raise ApacheConfigError("Illegal character '%s'" % t.value[0])
+
+
+def make_lexer(**options):
+
+    lexer_class = BaseApacheConfigLexer
+
+    lexer_class = type('ApacheConfigLexer',
+                       (lexer_class, HashCommentsLexer),
+                       {'tokens': lexer_class.tokens + HashCommentsLexer.tokens,
+                        'states': lexer_class.states + HashCommentsLexer.states,
+                        'options': options})
+
+    if options.get('ccomments', True):
+        lexer_class = type('ApacheConfigLexer',
+                           (lexer_class, CStyleCommentsLexer),
+                           {'tokens': lexer_class.tokens + CStyleCommentsLexer.tokens,
+                            'states': lexer_class.states + CStyleCommentsLexer.states,
+                            'options': options})
+
+    if options.get('useapacheinclude', True):
+        lexer_class = type('ApacheConfigLexer',
+                           (lexer_class, ApacheIncludesLexer),
+                           {'tokens': lexer_class.tokens + ApacheIncludesLexer.tokens,
+                            'states': lexer_class.states + ApacheIncludesLexer.states,
+                            'options': options})
+
+    return lexer_class
---
+++ packages/apacheconfig/apacheconfig-0.2.8-py3.1.egg/apacheconfig/parser.py
@@ -0,0 +1,174 @@
+#
+# This file is part of apacheconfig software.
+#
+# Copyright (c) 2018-2019, Ilya Etingof <etingof@gmail.com>
+# License: https://github.com/etingof/apacheconfig/LICENSE.rst
+#
+import logging
+import ply.yacc as yacc
+
+from apacheconfig.error import ApacheConfigError
+
+log = logging.getLogger(__name__)
+
+
+class HashCommentsParser(object):
+    def p_hashcomment(self, p):
+        """comment : HASHCOMMENT
+        """
+        p[0] = ['comment', p[1]]
+
+
+class CStyleCommentsParser(object):
+    def p_comment(self, p):
+        """comment : HASHCOMMENT
+                   | CCOMMENT
+        """
+        p[0] = ['comment', p[1]]
+
+
+class IncludesParser(object):
+    def p_include(self, p):
+        """include : INCLUDE
+           includeoptional : INCLUDE
+        """
+        p[0] = ['include', p[1]]
+
+
+class ApacheIncludesParser(object):
+    def p_apacheinclude(self, p):
+        """include : INCLUDE
+                   | APACHEINCLUDE
+        """
+        p[0] = ['include', p[1]]
+
+    def p_includeoptional(self, p):
+        """includeoptional : APACHEINCLUDEOPTIONAL
+        """
+        p[0] = ['includeoptional', p[1]]
+
+
+class BaseApacheConfigParser(object):
+
+    def __init__(self, lexer, start='config', tempdir=None, debug=False):
+        self._lexer = lexer
+        self.tokens = lexer.tokens  # parser needs this implicitly
+        self._tempdir = tempdir
+        self._debug = debug
+        self._start = start
+        self.engine = None
+        self.reset()
+
+    def reset(self):
+        self.engine = yacc.yacc(
+            module=self,
+            start=self._start,
+            outputdir=self._tempdir,
+            write_tables=bool(self._tempdir),
+            debug=False,
+            debuglog=log if self._debug else None,
+            errorlog=log if self._debug else None
+        )
+
+    def parse(self, text):
+        self.reset()
+        return self.engine.parse(text)
+
+    # Parsing rules
+
+    def p_statement(self, p):
+        """statement : OPTION_AND_VALUE
+        """
+        p[0] = ['statement', p[1][0], p[1][1]]
+
+        if self.options.get('lowercasenames'):
+            p[0][1] = p[0][1].lower()
+
+        if (not self.options.get('nostripvalues') and
+                not hasattr(p[0][2], 'is_single_quoted') and
+                not hasattr(p[0][2], 'is_double_quoted')):
+            p[0][2] = p[0][2].rstrip()
+
+    def p_statements(self, p):
+        """statements : statements statement
+                      | statement
+        """
+        n = len(p)
+        if n == 3:
+            p[0] = p[1] + [p[2]]
+        elif n == 2:
+            p[0] = ['statements', p[1]]
+
+    def p_contents(self, p):
+        """contents : contents statements
+                    | contents comment
+                    | contents include
+                    | contents includeoptional
+                    | contents block
+                    | statements
+                    | comment
+                    | include
+                    | includeoptional
+                    | block
+        """
+        n = len(p)
+        if n == 3:
+            p[0] = p[1] + [p[2]]
+        else:
+            p[0] = ['contents', p[1]]
+
+    def p_block(self, p):
+        """block : OPEN_TAG contents CLOSE_TAG
+                 | OPEN_TAG CLOSE_TAG
+                 | OPEN_CLOSE_TAG
+        """
+        n = len(p)
+        if n == 4:
+            p[0] = ['block', p[1], p[2], p[3]]
+        elif n == 3:
+            p[0] = ['block', p[1],  [], p[2]]
+        else:
+            p[0] = ['block', p[1], [], p[1]]
+
+        if self.options.get('lowercasenames'):
+            for tag in (1, 3):
+                p[0][tag] = p[0][tag].lower()
+
+    def p_config(self, p):
+        """config : config contents
+                  | contents
+        """
+        n = len(p)
+        if n == 3:
+            p[0] = p[1] + [p[2]]
+        elif n == 2:
+            p[0] = ['config', p[1]]
+
+    def p_error(self, p):
+        raise ApacheConfigError("Parser error at '%s'" % p.value if p else 'Unexpected EOF')
+
+
+def make_parser(**options):
+
+    parser_class = BaseApacheConfigParser
+
+    if options.get('ccomments', True):
+        parser_class = type('ApacheConfigParser',
+                            (parser_class, CStyleCommentsParser),
+                            {'options': options})
+    else:
+        parser_class = type('ApacheConfigParser',
+                            (parser_class, HashCommentsParser),
+                            {'options': options})
+
+    if options.get('useapacheinclude', True):
+        parser_class = type('ApacheConfigParser',
+                            (parser_class, ApacheIncludesParser),
+                            {'options': options})
+    else:
+        parser_class = type('ApacheConfigParser',
+                            (parser_class, IncludesParser),
+                            {'options': options})
+
+
+    return parser_class
