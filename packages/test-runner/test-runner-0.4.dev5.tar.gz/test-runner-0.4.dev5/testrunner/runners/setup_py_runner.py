0d80fa23cf58a173d1a74f569f3da84c421708e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:29 +0000

    Add package test-runner/0.4.dev5/test-runner-0.4.dev5.tar.gz

---
+++ packages/test-runner/test-runner-0.4.dev5.tar.gz/test-runner-0.4.dev5/testrunner/runners/setup_py_runner.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import os
+from typing import Union, Optional, Tuple
+
+from pytesting_utils import virtualenv
+
+from testrunner.runners.abstract_runner import AbstractRunner, RunResult
+
+
+class SetupPyRunner(AbstractRunner):
+    def __init__(
+        self, project_name: str, path: Union[bytes, str, os.PathLike]
+    ) -> None:
+        super().__init__(project_name, path)
+
+    def run(self) -> Optional[Tuple[str, str]]:
+        setup_py = os.path.join(self._path, "setup.py")
+        if not os.path.exists(setup_py) and not os.path.isfile(setup_py):
+            return None
+
+        with virtualenv(self._project_name) as env:
+            old_dir = os.getcwd()
+            os.chdir(self._path)
+            packages = self._extract_necessary_packages()
+            env.add_packages_for_installation(packages)
+            out, err = env.run_commands(["python setup.py test"])
+            os.chdir(old_dir)
+            return out, err
+
+    def get_run_result(self, log: str) -> RunResult:
+        raise NotImplementedError("Implement me!")
