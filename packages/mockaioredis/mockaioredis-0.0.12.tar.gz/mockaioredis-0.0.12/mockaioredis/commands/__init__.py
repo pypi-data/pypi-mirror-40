314695dc97335867d34516f04c52726d8f467240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:32 +0000

    Add package mockaioredis/0.0.12/mockaioredis-0.0.12.tar.gz

---
+++ packages/mockaioredis/mockaioredis-0.0.12.tar.gz/mockaioredis-0.0.12/mockaioredis/commands/__init__.py
@@ -0,0 +1,28 @@
+from mockredis import MockRedis as _MockRedis
+
+from .generic import GenericCommandsMixin
+from .hash import HashCommandsMixin
+from .list import ListCommandsMixin
+
+__all__ = ['MockRedis']
+
+
+class MockRedis(GenericCommandsMixin, HashCommandsMixin, ListCommandsMixin):
+    """Fake high-level aioredis.Redis interface"""
+
+    def __init__(self, connection=None, encoding=None):
+        # Just for API compatibility
+        self._conn = connection
+        self._redis = _MockRedis()
+
+        self._encoding = encoding
+
+
+async def create_redis(address, *, db=None, password=None, ssl=None,
+                       encoding=None, commands_factory=MockRedis,
+                       loop=None):
+    '''Create a fake high-level MockRedis interface
+
+    This function is a coroutine
+    '''
+    return commands_factory(None, encoding=encoding)
