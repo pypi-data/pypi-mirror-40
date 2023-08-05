9480c809019e69e604ebae1de099ae8bf44cc9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:17 +0000

    Add package blink-cameras/0.3.0/blink-cameras-0.3.0.tar.gz

---
+++ packages/blink-cameras/blink-cameras-0.3.0.tar.gz/blink-cameras-0.3.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+version = '0.3.0'
+setuptools.setup(
+    name='blink-cameras',
+    version=version,
+    author='Derek Anderson',
+    author_email='public@kered.org',
+    description='Python API for the Blink Home Security Camera System',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'python-dateutil',
+        'pyyaml',
+        'requests'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Operating System :: OS Independent'])
