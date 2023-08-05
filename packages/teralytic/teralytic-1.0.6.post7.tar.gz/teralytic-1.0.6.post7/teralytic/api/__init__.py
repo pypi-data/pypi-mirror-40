a312f82078bc3e42b884e9927935700ad38aa6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:38 +0000

    Add package teralytic/1.0.6.post7/teralytic-1.0.6.post7.tar.gz

---
+++ packages/teralytic/teralytic-1.0.6.post7.tar.gz/teralytic-1.0.6.post7/teralytic/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from teralytic.api.organization_api import OrganizationApi
+from teralytic.api.readings_api import ReadingsApi
