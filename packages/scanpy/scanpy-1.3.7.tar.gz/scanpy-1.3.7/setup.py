30932c0daf2217646a94352fc79a041245b63f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:20 +0000

    Add package scanpy/1.3.7/scanpy-1.3.7.tar.gz

---
+++ packages/scanpy/scanpy-1.3.7.tar.gz/scanpy-1.3.7/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+from io import open
+import versioneer
+
+with open('requirements.txt', encoding='utf-8') as requirements:
+    requires = [l.strip() for l in requirements]
+
+with open('README.rst', encoding='utf-8') as readme_f:
+    readme = readme_f.read()
+
+author = 'Alex Wolf, Philipp Angerer, Fidel Ramirez, Isaac Virshup, Sergei Rybakov, Davide Cittaro, Gokcen Eraslan, Tom White, Tobias Callies, Andrés R. Muñoz-Rojas.'
+
+setup(
+    name='scanpy',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Single-Cell Analysis in Python.',
+    long_description=readme,
+    url='http://github.com/theislab/scanpy',
+    author=author,
+    author_email='alex.wolf@helmholtz-muenchen.de',
+    license='BSD',
+    python_requires='>=3.5',
+    install_requires=requires,
+    extras_require=dict(
+        louvain=['python-igraph', 'louvain>=0.6'],
+        leiden=['python-igraph', 'leidenalg'],
+        bbknn=['bbknn'],
+        doc=['sphinx', 'sphinx_rtd_theme', 'sphinx_autodoc_typehints'],
+        test=['pytest'],
+    ),
+    packages=find_packages(),
+    # `package_data` does NOT work for source distributions!!!
+    # you also need MANIFTEST.in
+    # https://stackoverflow.com/questions/7522250/how-to-include-package-data-with-setuptools-distribute
+    package_data={'': '*.txt'},
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Framework :: Jupyter',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
