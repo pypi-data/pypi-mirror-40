c44f87fe7f701e8df78a93f4ebe3f00a74c7f864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:35 +0000

    Add package py-readability-metrics/0.21.1/py-readability-metrics-0.21.1.tar.gz

---
+++ packages/py-readability-metrics/py-readability-metrics-0.21.1.tar.gz/py-readability-metrics-0.21.1/readability/readability.py
@@ -0,0 +1,31 @@
+from .text import Analyzer
+from .scorers import ARI, ColemanLiau, DaleChall, Flesch, FleschKincaid, GunningFog
+
+
+class Readability:
+    def __init__(self, text):
+        self.analyzer = Analyzer(text)
+
+    def ari(self):
+        """Calculate Automated Readability Index (ARI)."""
+        return ARI(self.analyzer.statistics).score()
+
+    def coleman_liau(self):
+        """Calculate Coleman Liau Index."""
+        return ColemanLiau(self.analyzer.statistics).score()
+
+    def dale_chall(self):
+        """Calculate Dale Chall."""
+        return DaleChall(self.analyzer.statistics).score()
+
+    def flesch(self):
+        """Calculate Flesch Reading Ease score."""
+        return Flesch(self.analyzer.statistics).score()
+
+    def flesch_kincaid(self):
+        """Calculate Flesch Kincaid Grade Level."""
+        return FleschKincaid(self.analyzer.statistics).score()
+
+    def gunning_fog(self):  # , abbr=None, hyphen=None, vars={}):
+        """Calculate Gunning Fog score."""
+        return GunningFog(self.analyzer.statistics).score()
