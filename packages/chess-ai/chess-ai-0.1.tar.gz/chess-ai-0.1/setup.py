7c1a7d0447783ca9d03e64bfc9fd0bec7f0ee4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:45 +0000

    Add package chess-ai/0.1/chess-ai-0.1.tar.gz

---
+++ packages/chess-ai/chess-ai-0.1.tar.gz/chess-ai-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='chess-ai',
+        version='0.1',
+        description='Provides Chess API and AI',
+        url='http://github.com/thkim1011/chess-ai',
+        author='Tae Hyung Kim',
+        license='MIT')
