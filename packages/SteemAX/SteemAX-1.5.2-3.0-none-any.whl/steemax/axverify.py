7cb971cccdb9d319445f9ebd7952d73ffc716347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:26 +0000

    Add package SteemAX/1.5.2/SteemAX-1.5.2-3.0-none-any.whl

---
+++ packages/SteemAX/SteemAX-1.5.2-3.0-none-any.whl/steemax/axverify.py
@@ -0,0 +1,207 @@
+#!/usr/bin/python3
+
+from screenlogger.screenlogger import Msg
+from simplesteem.simplesteem import SimpleSteem
+from steemax import default
+from steemax import axdb
+
+
+class AXverify:
+    def __init__(self, screenmodeoverride="quiet"):
+        self.steem = SimpleSteem(client_id=default.client_id,
+                                 client_secret=default.client_secret,
+                                 callback_url=default.callback_url,
+                                 screenmode=screenmodeoverride)
+        self.msg = Msg(default.logfilename,
+                       default.logpath,
+                       screenmodeoverride)
+        self.response = None
+        self.post_one = ""
+        self.post_two = ""
+        self.vote_cut = 0
+        self.post_list = {}
+
+    def vote_on_it(self, voter, author, post, weight):
+        """ Use the tokens in the database to vote on
+        a post. If the vote fails, renews the token
+        and tries again.
+        """
+        db = axdb.AXdb(default.dbuser,
+                       default.dbpass,
+                       default.dbname)
+        if db.get_user_token(voter) is not False:
+            accesstoken = db.dbresults[0][1]
+            refreshtoken = db.dbresults[0][2]
+        else:
+            return False
+        # If the vote fails then we renew the token
+        if not self.sc_vote(voter, author, post, weight, accesstoken):
+            print("Renewing token for " + voter)
+            newtoken = self.renew_token(voter, refreshtoken)
+            if newtoken is not False:
+                return self.sc_vote(voter, author, post, weight, newtoken)
+            else:
+                self.msg.error_message("A NEW TOKEN COULD NOT BE CREATED")
+                return False
+
+    def sc_vote(self, voter, author, post, weight, token):
+        """ Takes the given token and initializes SteemConnect
+        to make a vote. Analyzes the result and prints the
+        outcome to screen as well as returns a boolean result.
+        """
+        self.steem.connect.sc = None
+        self.steem.connect.steemconnect(
+            token)
+        result = self.steem.connect.vote(
+            voter,
+            author,
+            post,
+            int(weight))
+        try:
+            result['error']
+        # We use a broad exception clause to "catch" everything
+        # that is not an error
+        except:
+            # The vote was successful
+            print(str(voter) + " has voted on "
+                  + str(post) + " "
+                  + str(weight) + "%")
+            return True
+        else:
+            self.msg.error_message(str(result))
+            return False
+
+    def renew_token(self, accountname, refreshtoken):
+        """ If the access token has expired
+        use the refresh token to get a new accss token.
+        """
+        if self.steem.verify_key(
+                acctname="", tokenkey=refreshtoken):
+            db = axdb.AXdb(default.dbuser,
+                           default.dbpass,
+                           default.dbname)
+            db.update_token(self.steem.username,
+                            self.steem.accesstoken,
+                            self.steem.refreshtoken)
+            return self.steem.accesstoken
+        else:
+            return False
+
+    def get_vote_value(self, acctname, voteweight=100,
+                       votepower=0):
+        """ Voteweight and votepower are entered 
+        as a percentage value (1 to 100)
+        If the default is used for votepower (0) 
+        it is set by the system at the time
+        the account last voted.
+        """
+        self.votevalue = self.steem.current_vote_value(
+            accountname=acctname,
+            voteweight=int(voteweight),
+            votepoweroverride=int(votepower))
+        self.voteweight = voteweight
+        self.votepower = votepower
+        return self.steem.rshares
+
+    def verify_post(self, account1, account2):
+        """ Gets only the most recent post, gets the 
+        timestamp and finds the age of the post in days.
+        Is the post too old? Did account2 
+        vote on the post already?
+        """
+        from_memory = False
+        if account1 in self.post_list:
+            identifier = self.post_list[account1]["id"]
+            from_memory = True
+        else:
+            identifier = self.steem.recent_post(account1, 0, 1)
+            self.post_list[account1] = {"id":identifier,"votes":None}
+        if identifier is False or identifier is None:
+            self.msg.message("No post for " + account1)
+            return False
+        else:
+            permlink = self.steem.util.permlink(identifier)
+            if from_memory:
+                votes = self.post_list[account1]["votes"]
+            else:
+                votes = self.steem.vote_history(permlink[1], account1)
+                self.post_list[account1]["votes"] = votes
+            if votes is not None and len(votes) > 0:
+                for v in votes:
+                    if v['voter'] == account2:
+                        self.msg.message(account2 +
+                                       " has already voted on " + permlink[1])
+                        return False
+        return permlink[1]
+
+    def eligible_posts(self, account1, account2):
+        """ Verify the posts of both accounts
+        """
+        post = self.verify_post(account1, account2)
+        if post is False or post is None:
+            self.msg.message(account1
+                             + " does not have an eligible post.")
+            return False
+        else:
+            self.post_one = post
+        post = self.verify_post(account2, account1)
+        if post is False or post is None:
+            self.msg.message(account2
+                             + " does not have an eligible post.")
+            return False
+        else:
+            self.post_two = post
+        return True
+
+    def eligible_votes(self, account1, account2,
+                       percentage, ratio, flag):
+        """ If the flag is raised use the full voting 
+        power to make the comparison rather
+        than the current voting power. 
+        If ratio was not acheived return false 
+        Otherwise display approximate 
+        upvote matches
+        """        
+        if flag == 1:
+            vpow = 100
+        else:
+            vpow = 0
+        v1 = self.get_vote_value(account1, percentage, vpow)
+        v2 = self.get_vote_value(account2, 100, vpow)
+        v3 = ((v1 / v2) * 100) / float(ratio)
+        v3a = round(v3, 2)
+        exceeds = False
+        if v3a < 1:
+            v3 = 1
+            exceeds = True
+        if v3a > 100:
+            v3 = 100
+            exceeds = True
+        self.msg.message(account2 + " needs to vote "
+                         + str(v3a) + "% in order to meet " + account1)
+        self.vote_cut = v3
+        v4 = self.get_vote_value(account2, v3, vpow)
+        v1s = self.steem.rshares_to_steem(v1)
+        v4s = self.steem.rshares_to_steem(v4)
+        if exceeds and flag != 2:
+            if v3 == 1:
+                v5 = v4 - v1
+                v5s = self.steem.rshares_to_steem(v5)
+                self.response = (account2 + "'s vote of " + str(v4s)
+                                 + " will be larger than " + account1
+                                 + "'s vote by: " + str(v5s))
+                self.msg.message(self.response)
+            if v3 == 100:
+                v5 = v1 - v4
+                v5s = self.steem.rshares_to_steem(v5)
+                self.response = (account1 + "'s vote of " + str(v1s)
+                                 + " will be larger than " + account2
+                                 + "'s vote by: " + str(v5s))
+                self.msg.message(self.response)
+            return False
+        else:
+            self.msg.message(account1 + " will upvote $" + str(v1s)
+                             + " and " + account2 + " will upvote $" + str(v4s))
+            return True
+
+# EOF
---
+++ packages/SteemAX/SteemAX-1.5.2-3.0-none-any.whl/steemax/web.py
@@ -0,0 +1,331 @@
+#!/usr/bin/python3
+
+import re
+import urllib3
+import json
+from urllib.parse import urlencode
+from screenlogger.screenlogger import Msg
+from simplesteem.simplesteem import SimpleSteem
+from steemax import axdb
+from steemax import axverify
+from steemax import default
+from steemax import sec
+
+
+class Web:
+    def __init__(self):
+        self.steem = SimpleSteem(
+            client_id=default.client_id,
+            client_secret=default.client_secret,
+            callback_url=default.callback_url,
+            screenmode=default.msgmode)
+        self.db = axdb.AXdb(
+            default.dbuser,
+            default.dbpass,
+            default.dbname)
+        self.msg = Msg(default.logfilename,
+                       default.logpath,
+                       default.msgmode)
+        self.json_resp = None
+
+    def load_template(self, templatefile=""):
+        """ opens a template file and loads it
+        into memory stored in a variable
+        """
+        templatepath = default.webpath + "/" + templatefile
+        with open(templatepath, 'r') as fh:
+            try:
+                template = fh.read()
+            except Exception as e:
+                self.msg.error_message(e)
+                return None
+            else:
+                return template
+
+    def make_page(self, template, **kwargs):
+        """ Fills in key / value pairs on a 
+        given template
+        """
+        regobj = re.compile(
+            r"^(.+)(?:\n|\r\n?)((?:(?:\n|\r\n?).+)+)",
+            re.MULTILINE)
+        newtemplate = regobj.sub('', template)
+        for key, value in kwargs.items():
+            newtemplate = re.sub(str(key),
+                                 str(value),
+                                 newtemplate)
+        return newtemplate
+
+    def index_page(self):
+        """ Creates the Home page for SteemAX and includes
+        the number of users and the number of exchanges.
+        """
+        return ("\r\n"
+                + self.make_page(self.load_template(
+                    "templates/index.html"),
+                    USERS=self.db.number_of_users(),
+                    EXCHANGES=self.db.number_of_exchanges()))
+
+    def login(self, token, dest="home"):
+        """ logs a user in using SteemConnect
+        adds the user to the database if it's
+        their first time.
+        """
+        if self.verify_token(token):
+            if self.db.get_user_token(self.steem.username):
+                self.db.update_token(self.steem.username,
+                                     self.steem.accesstoken,
+                                     self.steem.refreshtoken)
+            else:
+                self.db.add_user(self.steem.username,
+                                 self.steem.privatekey,
+                                 self.steem.refreshtoken,
+                                 self.steem.accesstoken)
+            if dest == "info":
+                return ("\r\n"
+                        + self.info_page(self.steem.username))
+            else:
+                autocheck = ""
+                if self.db.auto_accept(self.steem.username) is True:
+                    autocheck = "checked"
+                settings = (self.make_page(self.load_template(
+                            "templates/settings.html"),
+                            AUTOCHECK=autocheck))
+                return ("\r\n"
+                        + self.make_page(self.load_template(
+                            "templates/invite_form.html"),
+                            ACCOUNT1=self.steem.username,
+                            REFRESHTOKEN=self.steem.refreshtoken,
+                            INVITECOUNT=self.db.number_of_invites(self.steem.username),
+                            SETTINGS=settings))
+        else:
+            return self.auth_url()
+
+    def invite(self, token, account2, per,
+               ratio, dur, response, ip, ajax, autocheck):
+        """ Creates an invite. First we filter the users
+        data then we create the invite in the database.
+        If the request was sent via ajax then the returned
+        result is simply the number one to indicate success.
+        """
+        ajax = sec.filter_number(int(ajax))
+        response = sec.filter_token(response)
+        per = sec.filter_number(per)
+        ratio = sec.filter_number(ratio, 1000)
+        dur = sec.filter_number(dur, 365)
+        autocheck = sec.filter_number(int(autocheck))
+        if not self.verify_recaptcha(response, ip):
+            return self.error_page("Invalid captcha.", ajax)
+        if float(per) < 1 or float(per) > 100:
+           return self.error_page("The percentage can not be lower then "
+                                + "1 or greater than 100 and must be a whole number.", ajax)    
+        if float(dur) < 0 or float(dur) > 365:
+           return self.error_page("The duration must be between 7 and 365 days.", ajax)
+        if float(ratio) < 0.001 or float(ratio) > 1000:
+           return self.error_page("The ratio must be between 0.001 and 1000 to 1.", ajax)
+        if self.verify_token(sec.filter_token(token)):
+            account2 = sec.filter_account(account2)
+            if self.steem.account(account2) is False:
+                return self.error_page(account2 
+                    + " is an invalid account name.", ajax)
+            else:
+                verify = axverify.AXverify()
+                if verify.eligible_votes(self.steem.username, account2, per, ratio, 1) is False:
+                    return self.error_page(verify.response, ajax)
+                memoid = self.db.add_invite(self.steem.username, account2, per, ratio, dur)
+                self.db.set_settings(self.steem.username, autocheck)
+                if memoid is False:
+                    return self.error_page(self.db.errmsg, ajax)
+                elif int(memoid) > 0:
+                    if (int(ajax) == 1):
+                        return "\r\n1"
+                    else:
+                        return ("Location: https://steemax.info/@"
+                                + self.steem.username + "\r\n")
+        else:
+            return self.auth_url()
+
+    def archive_page(self, account=None):
+        """ Provides a list of all the exchanges that
+        have occured for a particular account. if
+        no account is provided then a list of all exchanges
+        for all accounts is returned.
+        """
+        infobox = ""
+        if account is not None:
+            account = sec.filter_account(account)
+            # If the token is invalid return user to the login page
+            if not self.db.get_user_token(account):
+                return self.auth_url()
+        axlist = self.db.get_exchange_archive(account)
+        boxtemplate = self.load_template("templates/archivebox.html")
+        for trade in axlist:
+            date = (str(trade[6].strftime("%B")) + " "
+                    + str(trade[6].day) + ", "
+                    + str(trade[6].year))
+            time = (str(trade[6].hour) + ":"
+                    + str(trade[6].minute))
+            box = self.make_page(boxtemplate,
+                                 DATE=date,
+                                 TIME=time,
+                                 ACCOUNT1=trade[0],
+                                 ACCOUNT2=trade[1],
+                                 IDENT1=trade[2],
+                                 IDENT2=trade[3],
+                                 VOTEVALUE1=trade[4],
+                                 VOTEVALUE2=trade[5])
+            infobox = infobox + box
+        pagetemplate = self.load_template("templates/archive.html")
+        # If the account was provide we display it with the @
+        # however if it's not we convert it to an empty string
+        # so that it is not displayed in HTML
+        if account is None:
+            account = ""
+        else:
+            account = "@" + account
+        return ("\r\n" + self.make_page(pagetemplate,
+                                        ACCOUNT=account,
+                                        INFOBOX=infobox))
+
+    def info_page(self, account):
+        """ Creates the page at steemax.info that displays
+        all the exchanges a user is involved in and which
+        provides the options to accept, barter or cancel a 
+        particular exchange.
+        """
+        account = sec.filter_account(account)
+        if not self.db.get_user_token(account):
+            return self.auth_url()
+        axlist = self.db.get_axlist(account)
+        boxtemplate = self.load_template("templates/infobox.html")
+        infobox = ""
+        invitee = 0
+        myaccount = ""
+        otheraccount = ""
+        buttoncode = ""
+        exp = ""
+        sendall = []
+        acceptall = []
+        cancelall = []
+        for value in axlist:
+            if account == value[2]:
+                invitee = 1
+                otheraccount = value[1]
+                myaccount = value[2]
+            else:
+                invitee = 0
+                otheraccount = value[2]
+                myaccount = value[1]
+            buttoncode = """
+                <div class="closemodal-info" onClick="command('{}', '{}', '{}')">&times;</div>
+                """.format(
+                value[0],
+                otheraccount,
+                "cancel")
+            cancelall.append(value[0])
+            if int(value[7]) == -1 and invitee == 0:
+                buttoncode += """
+                <div class="send-button" onClick="command('{}', '{}', '{}')">Send</div>
+                    """.format(
+                    value[0],
+                    otheraccount,
+                    "start")
+                sendall.append(value[0])
+            if ((int(value[7]) == 0 and invitee == 1)
+                    or (int(value[7]) == 2 and invitee == 1)
+                    or (int(value[7]) == 3 and invitee == 0)):
+                buttoncode += """
+                <div class="accept-button" onClick="command('{}', '{}', '{}')">Accept</div>
+                <div class="barter-button" onClick="barter_window('{}');">Barter</div>
+                    """.format(
+                    value[0],
+                    otheraccount,
+                    "accept",
+                    value[0])
+                acceptall.append(value[0])
+            elif ((int(value[7]) == 0 and invitee == 0)
+                    or (int(value[7]) == 3 and invitee == 1)
+                    or (int(value[7]) == 2 and invitee == 0)):
+                buttoncode += '<div id="pending">Pending</div>\n'
+            if int(value[7]) == 1:
+                buttoncode += '<div id="pending">Active</div>\n'
+            if int(value[5]) > 0:
+                if int(value[7]) == 1:
+                    exp = "Expires " + self.db.expiration_date(value[8], value[5])
+                else:
+                    exp = value[5] + " days"
+            else:
+                exp = "Until canceled"
+            if int(value[7]) != 4 and not (int(value[7]) == -1 and invitee == 1):
+                box = self.make_page(boxtemplate,
+                                    AXID=value[0],
+                                    ACCOUNT1=value[1],
+                                    ACCOUNT2=value[2],
+                                    PERCENTAGE=value[3],
+                                    DURATION=exp,
+                                    DARATIO=value[4],
+                                    MEMOID=value[6],
+                                    INVITEE=invitee,
+                                    OTHERACCOUNT=otheraccount,
+                                    MYACCOUNT=myaccount,
+                                    BTNCODE=buttoncode)
+                infobox = infobox + box
+        pagetemplate = self.load_template("templates/info.html")
+        return ("\r\n" + self.make_page(pagetemplate, 
+                                    ACCOUNT1=account,
+                                    SENDALL=','.join(map(str, sendall)),
+                                    CANCELALL=','.join(map(str, cancelall)),
+                                    ACCEPTALL=','.join(map(str, acceptall)),
+                                    INFOBOX=infobox))
+
+    def verify_token(self, token):
+        """ cleans and verifies a SteemConnect
+        refresh token
+        """
+        token = sec.filter_token(token)
+        if (token is not None
+            and self.steem.verify_key(
+                acctname="", tokenkey=token)):
+            return True
+        else:
+            return False
+
+    def auth_url(self):
+        """ Returns the SteemConnect authorization
+        URL for SteemAX
+        """
+        url = self.steem.connect.auth_url()
+        return "Location: " + url + "\r\n"
+
+    def error_page(self, msg, ajax):
+        """ Returns the HTML page with the
+        given error message
+        """
+        if (int(ajax) == 1):
+            return "\r\n"+msg
+        else:
+            msg += ("<br><br>You may be receiving this error in this format "
+                + "because you have disabled javascript. Please enable javascript "
+                + "for a better user experience.")
+            return ("\r\n"
+                    + self.make_page(
+                        self.load_template("templates/error.html"),
+                        ERRORMSG=msg))
+
+    def verify_recaptcha(self, response, remoteip):
+        """ Verifies a Google recaptcha v2 token
+        """
+        http = urllib3.PoolManager()
+        encoded_args = urlencode({'secret': default.recaptcha_secret,
+                                  'response': response,
+                                  'remoteip': remoteip})
+        url = default.recaptcha_url + "?" + encoded_args
+        req = http.request('POST', url)
+        if req.status == 200:
+            self.json_resp = json.loads(req.data.decode('utf-8'))
+        if self.json_resp['success']:
+            return True
+        else:
+            return False
+
+# EOF
