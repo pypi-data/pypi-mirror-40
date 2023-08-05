cd695d736d6df684d203e6e91e7c79b510037725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:02 +0000

    Add package jrpypytorch/0.1.0/jrpypytorch-0.1.0.tar.gz

---
+++ packages/jrpypytorch/jrpypytorch-0.1.0.tar.gz/jrpypytorch-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['jrpypytorch']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.0,<4.0',
+ 'numpy>=1.15,<2.0',
+ 'sklearn>=0.0.0,<0.0.1',
+ 'torch>=1.0,<2.0',
+ 'torchvision>=0.2.1,<0.3.0']
+
+setup_kwargs = {
+    'name': 'jrpypytorch',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Jamie',
+    'author_email': 'jamie@jumpingrivers.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
