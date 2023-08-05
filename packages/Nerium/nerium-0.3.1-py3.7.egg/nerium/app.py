48569979f3c6197a25288bf9e966d6023a8e9b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:01 +0000

    Add package Nerium/0.3.1/nerium-0.3.1-py3.7.egg

---
+++ packages/Nerium/nerium-0.3.1-py3.7.egg/nerium/app.py
@@ -0,0 +1,110 @@
+import json
+from pathlib import Path
+
+import aiohttp_cors
+from aiohttp import web
+from dotenv import load_dotenv
+from nerium import commit, query, __version__
+from nerium.utils import multi_to_dict, serial_date
+
+# Provision environment as needed
+# Load local .env first
+load_dotenv(Path.cwd() / '.env')
+# Load this one for use w/ Kubernetes secret mount
+load_dotenv('/dotenv/.env')
+
+
+async def base_route(request):
+    data = {"status": "ok",
+            "version": __version__,
+            "commit": commit}
+    return web.json_response(data)
+
+
+async def resultset(request):
+    """Calls nerium.query.get_result_set() to fetch results from ResultSet()
+    """
+    request['querystring'] = multi_to_dict(request.rel_url.query)
+    query_result = query.get_result_set(request.match_info['query_name'],
+                                    **request['querystring'])
+    # Using json.dumps instead of json_response for serialized datetimes
+    return web.Response(
+        text=json.dumps(query_result, default=serial_date),
+        content_type='application/json')
+
+
+# @web.middleware
+# @use_kwargs({'ne_format': fields.Str(missing='default')})
+# async def formatter(request, handler, ne_format):
+#     """Pass resultset through formatter
+#     """
+#     # TODO: Following functional refactor, we can probably go to the formatter
+#     # directly, and have it call the resultset
+#     resp = await handler(request)
+#     result = json.loads(resp.text)
+
+#     # Remaining query string params are for the database query
+#     # Take out 'ne_format' and pass the rest along to the formatter
+#     try:
+#         params = request['querystring']
+#     except KeyError:
+#         params = {}
+#     params.pop('ne_format', None)
+#     payload = query.format_results(result, ne_format, **params)
+#     if isinstance(payload, str):
+#         return web.Response(text=payload, content_type='text/csv')
+#     else:
+#         # we can use json_response here because dates are already serialized
+#         return web.json_response(payload)
+
+
+@web.middleware
+async def result_status(request, handler):
+    """Check if get_result_set returned an error
+    """
+    resp = await handler(request)
+    result = json.loads(resp.text)
+    try:
+        if 'error' in result[0].keys():
+            raise web.HTTPBadRequest(body=resp.text)
+        else:
+            return web.json_response(result)
+    # exception for health check OK method
+    except KeyError:
+        return web.json_response(result)
+    # empty result set
+    except IndexError:
+        # TODO: pass info once formatters better handle empty column sets
+        raise web.HTTPOk(text=resp.text)
+
+
+app = web.Application(middlewares=[result_status])
+app.router.add_get('/v1/{query_name}/', resultset)
+app.router.add_get('/v1/{query_name}', resultset)
+app.router.add_get('/', base_route)
+app.router.add_get('/v1/', base_route)
+
+# Configure default CORS settings.
+cors = aiohttp_cors.setup(
+    app,
+    defaults={
+        "*":
+        aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+            allow_methods="*",
+        )
+    })
+
+# Configure CORS on all routes.
+for route in list(app.router.routes()):
+    cors.add(route)
+
+
+def main():
+    web.run_app(app)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/Nerium/nerium-0.3.1-py3.7.egg/nerium/query.py
@@ -0,0 +1,59 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+import frontmatter
+from importlib import import_module
+from munch import munchify
+
+# Walking the query_files dir to get all the queries in a single list
+# NOTE: this means query names should be unique across subdirectories,
+#       *and* adding a new query file requires restart
+FLAT_QUERIES = list(Path(os.getenv('QUERY_PATH', 'query_files')).glob('**/*'))
+
+
+def get_query(query_name):
+    """Find file matching query_name, read and return query object
+    """
+    query_file_match = list(
+        filter(lambda i: query_name == i.stem, FLAT_QUERIES))
+    if not query_file_match:
+        return None
+    # TODO: Log warning if more than one match
+    query_file = query_file_match[0]
+    with open(query_file) as f:
+        metadata, query_body = frontmatter.parse(f.read())
+    result_mod = query_file.suffix.strip('.')
+    parsed_query = dict(
+        name=query_name,
+        metadata=metadata,
+        path=query_file,
+        result_mod=result_mod,
+        body=query_body,
+        error=False,
+        executed=datetime.now())
+    return munchify(parsed_query)
+
+
+def get_result_set(query_name, **kwargs):
+    """Call get_query, then submit query from file to resultset module
+    """
+    query = get_query(query_name)
+    if not query:
+        return [{'error': f"No query found matching '{query_name}'"}]
+    try:
+        result_mod = import_module(
+            f'nerium.contrib.resultset.{query.result_mod}')
+    except ModuleNotFoundError:
+        result_mod = import_module('nerium.resultset.sql')
+    query_result = result_mod.result(query, **kwargs)
+    return dict(metadata=query.metadata, data=query_result)
+
+
+def format_results(query_name, format_, **kwargs):
+    result = get_result_set(query_name, **kwargs)
+    try:
+        format_mod = import_module(f'nerium.formatter.{format_}')
+    except ModuleNotFoundError:
+        format_mod = import_module('nerium.formatter.default')
+    return format_mod.format_results(result, **kwargs)
