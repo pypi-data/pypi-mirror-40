3538c493e931f658031f1bdc524355cc6aab7921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:38 +0000

    Add package OctoBot-Launcher/1.0.0/OctoBot-Launcher-1.0.0.tar.gz

---
+++ packages/OctoBot-Launcher/OctoBot-Launcher-1.0.0.tar.gz/OctoBot-Launcher-1.0.0/README.md
@@ -0,0 +1,15 @@
+# OctoBot-Launcher [1.0.0](https://github.com/Drakkar-Software/OctoBot-Launcher/tree/master/CHANGELOG.md)
+
+A launcher for your [OctoBot](https://github.com/Drakkar-Software/OctoBot) ! 
+
+## Install
+With [Python 3.7](https://www.python.org/downloads/) : 
+``` {.sourceCode .bash}
+$ pip install octobot-launcher
+```
+
+## Usage
+Start OctoBot launcher with : 
+``` {.sourceCode .bash}
+$ octobot-launcher
+```
---
+++ packages/OctoBot-Launcher/OctoBot-Launcher-1.0.0.tar.gz/OctoBot-Launcher-1.0.0/entrypoint.py
@@ -0,0 +1,38 @@
+import argparse
+import importlib
+import logging
+import sys
+
+from launcher.launcher import update_launcher, start_launcher
+
+
+def main(args):
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser(description='OctoBot - Launcher')
+    parser.add_argument('-v', '--version', help='show OctoBot Launcher current version',
+                        action='store_true')
+    parser.add_argument('-u', '--update', help='update OctoBot with the latest version available',
+                        action='store_true')
+    parser.add_argument('-l', '--update_launcher', help='update OctoBot Launcher with the latest version available',
+                        action='store_true')
+    parser.add_argument('-e', '--export_logs', help="export Octobot's last logs",
+                        action='store_true')
+    parser.add_argument('-ng', '--no_gui', help="Without gui",
+                        action='store_true')
+
+    args = parser.parse_args(args)
+
+    update_launcher()
+
+    try:
+        import launcher.launcher_app
+    except ImportError:
+        importlib.import_module("launcher.launcher_app")
+
+    if not args.no_gui:
+        start_launcher(args)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
