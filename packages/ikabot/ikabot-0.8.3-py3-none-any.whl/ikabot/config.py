08d712dd571f0762bf7acf0d86ebaf1f107125a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:00 +0000

    Add package ikabot/0.8.3/ikabot-0.8.3-py3-none-any.whl

---
+++ packages/ikabot/ikabot-0.8.3-py3-none-any.whl/ikabot/config.py
@@ -0,0 +1,10 @@
+
+ids = None
+ciudades = None
+infoUser = ''
+cookieFile = '/tmp/.cookies.txt'
+telegramFile = '/tmp/.telegram.txt'
+urlCiudad = 'view=city&cityId='
+urlIsla = 'view=island&islandId='
+prompt = ' >>  '
+tipoDeBien = ['Madera', 'Vino', 'Marmol', 'Cristal', 'Azufre']
