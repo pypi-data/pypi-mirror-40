cc428bf461b81c8ecd0deaf2d424c8be3e34d279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:43 +0000

    Add package Neveregiveup/2.0.1/Neveregiveup-2.0.1.tar.gz

---
+++ packages/Neveregiveup/Neveregiveup-2.0.1.tar.gz/Neveregiveup-2.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+with open('README.rst') as file:
+    readme=file.read()
+setup(
+    name='Neveregiveup',
+    version='2.0.1',
+    packages=['wargame'],
+    url='http://localhost:8090/simple',
+    license='LICENSE.txt',
+    description='test pkg private',
+    long_description=readme,
+    author='huyahao',
+    author_email="1509232028@qq.com"
+)
