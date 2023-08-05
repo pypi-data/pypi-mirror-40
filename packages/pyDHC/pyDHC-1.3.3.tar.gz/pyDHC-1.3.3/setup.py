4c68af77b81adc6d08035a8f75e1b1dd8bad38ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:11 +0000

    Add package pyDHC/1.3.3/pyDHC-1.3.3.tar.gz

---
+++ packages/pyDHC/pyDHC-1.3.3.tar.gz/pyDHC-1.3.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyDHC",
+    version="1.3.3",
+    author="Sidney Kuyateh",
+    author_email="sidneyjohn23@icloud.com",
+    description="Communication to Devolo Home Control hub",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/autinerd/python-devoloDHC",
+    packages=setuptools.find_packages(),
+    install_requires=['certifi'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+    ],
+)
