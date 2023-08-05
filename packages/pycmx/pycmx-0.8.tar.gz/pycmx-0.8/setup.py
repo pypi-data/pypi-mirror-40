4f9d477965742bd5e2a25e3ecf623d74d36aa741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:22 +0000

    Add package pycmx/0.8/pycmx-0.8.tar.gz

---
+++ packages/pycmx/pycmx-0.8.tar.gz/pycmx-0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pycmx',
+      version='0.8',
+      author='Jamie Hardt',
+      author_email='jamiehardt@me.com',
+      description='CMX 3600 Edit Decision List Parser',
+      long_description_content_type="text/markdown",
+      long_description=long_description,
+      url='https://github.com/iluvcapra/pycmx',
+      classifiers=['Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Multimedia',
+          'Topic :: Multimedia :: Video',
+          'Topic :: Text Processing'],
+      packages=['pycmx'])
