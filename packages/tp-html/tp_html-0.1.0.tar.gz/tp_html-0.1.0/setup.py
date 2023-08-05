d95b1927e615fa709384ffec74a203f676b4452f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:21 +0000

    Add package tp-html/0.1.0/tp_html-0.1.0.tar.gz

---
+++ packages/tp-html/tp_html-0.1.0.tar.gz/tp_html-0.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tp_html",
+    version="0.1.0",
+    author="jhyao",
+    author_email="yaojinhonggg@gmail.com",
+    description="Get data from html",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jhyao/tp_html",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'beautifulsoup4'
+      ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
