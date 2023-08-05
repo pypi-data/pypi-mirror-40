61530b3f32e012134b61b2514bb1142b2507cb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:49 +0000

    Add package mxklabs/0.0.4/mxklabs-0.0.4.tar.gz

---
+++ packages/mxklabs/mxklabs-0.0.4.tar.gz/mxklabs-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'mxklabs',
+  packages = ['mxklabs'],
+  version = '0.0.4',
+  description = 'A selection python code written for mxklabs projects that may be useful elsewhere.',
+  author = 'Mark Kattenbelt',
+  author_email = 'mark.kattenbelt@gmail.com',
+  url = 'https://github.com/mxklabs/mxklabs-python',
+  download_url = 'https://github.com/mxklabs/mxklabs-python/tarball/0.0.4',
+  keywords = ['mxklabs', 'mxklabs.data', 'mxklabs.sat', 'DIMACS', 'SAT',
+              'satisfiability', 'CNF', 'circular', 'buffer', 'persistent'],
+  classifiers = [],
+)
