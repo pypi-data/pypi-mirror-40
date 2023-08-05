eb34a1f7839908b40244ffc2fe55516bef232e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:20 +0000

    Add package ftscursor/0.3.5/ftscursor-0.3.5.tar.gz

---
+++ packages/ftscursor/ftscursor-0.3.5.tar.gz/ftscursor-0.3.5/ftscursor/__init__.py
@@ -0,0 +1,14 @@
+"""The FTSCursor class and related functions
+
+Classes
+-------
+FTSCursor
+    A Cursor with additional methods to support FTS indexing & searching
+
+Functions
+---------
+latest_fts_version
+    Check the latest available FTS version, if any
+"""
+
+from ftscursor.ftscursor import latest_fts_version, FTSCursor
