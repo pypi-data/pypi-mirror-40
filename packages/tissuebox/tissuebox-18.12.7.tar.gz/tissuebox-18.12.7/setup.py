9304c55874242d0eac9dd709ad0bbab64f192797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:29 +0000

    Add package tissuebox/18.12.7/tissuebox-18.12.7.tar.gz

---
+++ packages/tissuebox/tissuebox-18.12.7.tar.gz/tissuebox-18.12.7/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='tissuebox',
+    version='18.12.7',
+    description='Tissuebox :: Pythonic payload validator',
+    author='nehem',
+    author_email='nehemiah.jacob@gmail.com',
+    url='https://github.com/nehemiahjacob/tissuebox.git',
+    packages=['tissuebox']
+)
