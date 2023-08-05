8986466a57c49f24c7f2fd0314fab30a5f40e12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:32 +0000

    Add package whisper/1.1.5/whisper-1.1.5.tar.gz

---
+++ packages/whisper/whisper-1.1.5.tar.gz/whisper-1.1.5/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from glob import glob
+from distutils.core import setup
+
+
+setup(
+  name='whisper',
+  version='1.1.5',
+  url='http://graphiteapp.org/',
+  author='Chris Davis',
+  author_email='chrismd@gmail.com',
+  license='Apache Software License 2.0',
+  description='Fixed size round-robin style database',
+  py_modules=['whisper'],
+  scripts=glob('bin/*') + glob('contrib/*'),
+  install_requires=['six'],
+  classifiers=[
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+  ],
+)
