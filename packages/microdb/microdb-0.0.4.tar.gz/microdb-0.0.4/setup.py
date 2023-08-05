acfeabce9d5b363d993acf5f4eda74493239237a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:56 +0000

    Add package microdb/0.0.4/microdb-0.0.4.tar.gz

---
+++ packages/microdb/microdb-0.0.4.tar.gz/microdb-0.0.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import requests
+
+USERNAME = "umihico"
+REPONAME = "microdb"
+
+
+github_api_url = f"https://api.github.com/repos/{USERNAME}/{REPONAME}"
+description = requests.get(github_api_url).json()['description']
+
+requirements = [
+]
+
+
+def _version_increment():
+    with open('version_raw.txt', 'r') as f:
+        version = int(float(f.read()))
+    version += 1
+    version = str(version)
+    with open('version_raw.txt', 'w') as f:
+        f.write(version)
+    version = '.'.join(str(version).zfill(3))
+    with open('version_digitgood.txt', 'w') as f:
+        f.write(version)
+    return version
+
+
+setup(
+    name=REPONAME,
+    version=_version_increment(),
+    description=description,
+    url=f'https://github.com/{USERNAME}/{REPONAME}',
+    author=USERNAME,
+    author_email=f'{USERNAME}@users.noreply.github.com',
+    license='MIT',
+    keywords='database',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
