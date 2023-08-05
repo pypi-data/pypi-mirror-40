32555134ebf82f9ee38135ef45c439ce598c29f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:09 +0000

    Add package pyscf/1.6/pyscf-1.6-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/pyscf/pyscf-1.6-cp27-cp27mu-manylinux1_x86_64.whl/pyscf/prop/ssc/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Copyright 2014-2018 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pyscf.prop.ssc import rhf
+from pyscf.prop.ssc import uhf
+from pyscf.prop.ssc import dhf
+from pyscf.prop.ssc import rks
+from pyscf.prop.ssc import uks
+
+RHF = rhf.SSC
+UHF = uhf.SSC
+DHF = dhf.SSC
+RKS = rks.SSC
+UKS = uks.SSC
