9acb625a6217cbdf6ff4d64d77ff25956e6532bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:27 +0000

    Add package gdown/3.6.2/gdown-3.6.2.tar.gz

---
+++ packages/gdown/gdown-3.6.2.tar.gz/gdown-3.6.2/gdown/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+import pkg_resources
+
+from .download import download
+
+
+__author__ = 'Kentaro Wada <www.kentaro.wada@gmail.com>'
+__version__ = pkg_resources.get_distribution('gdown').version
