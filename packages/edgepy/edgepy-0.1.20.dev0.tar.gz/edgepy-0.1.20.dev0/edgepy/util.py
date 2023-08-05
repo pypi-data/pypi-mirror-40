579af43fbb187c3fcd6659ecf1e8a796114aeac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:24 +0000

    Add package edgepy/0.1.20.dev0/edgepy-0.1.20.dev0.tar.gz

---
+++ packages/edgepy/edgepy-0.1.20.dev0.tar.gz/edgepy-0.1.20.dev0/edgepy/util.py
@@ -0,0 +1,29 @@
+def clear(sdir):
+    import os, glob
+    files = glob.glob(sdir + '/*')
+    for f in files:
+        os.remove(f)
+
+
+def cleanws():
+    from edgepy import Disp
+    import os
+    ogdir = os.getcwd()
+    mdir = Disp.__file__
+    os.chdir(mdir)
+    os.chdir('..')
+    os.chdir('..')
+    os.chdir(os.getcwd() + '/docs/workspace')
+    clear(os.getcwd())
+    os.chdir(ogdir)
+
+
+def test():
+    from edgepy import Disp
+    import os
+    mdir = Disp.__file__
+    os.chdir(mdir)
+    os.chdir('..')
+    os.chdir('..')
+    os.chdir(os.getcwd() + '/docs/workspace')
+    print(os.getcwd())
