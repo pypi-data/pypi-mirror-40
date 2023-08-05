43deadc568f30f1d98112b93631bab2d112c8c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package python-qt-live-coding/0.2.5/python-qt-live-coding-0.2.5.tar.gz

---
+++ packages/python-qt-live-coding/python-qt-live-coding-0.2.5.tar.gz/python-qt-live-coding-0.2.5/src/livecoding/projectbrowser.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import os
+
+from python_qt_binding.QtCore import QObject, Property, Signal, QUrl, Slot
+
+
+class ProjectBrowser(QObject):
+    projectPathChanged = Signal(QUrl)
+    qmlFilesChanged = Signal('QStringList')
+
+    def __init__(self, parent=None):
+        super(ProjectBrowser, self).__init__(parent)
+
+        path = os.path.dirname(os.path.abspath(__file__))
+        self._project_path = QUrl.fromLocalFile(
+            os.path.realpath(os.path.join(path, '..'))
+        )
+        self._qml_files = []
+
+        self.update()
+
+        self.projectPathChanged.connect(self._update_files)
+
+    @Property(QUrl, notify=projectPathChanged)
+    def projectPath(self):
+        return self._project_path
+
+    @projectPath.setter
+    def projectPath(self, value):
+        if self._project_path == value:
+            return
+        self._project_path = value
+        self.projectPathChanged.emit(value)
+
+    @Property('QStringList', notify=qmlFilesChanged)
+    def qmlFiles(self):
+        return self._qml_files
+
+    @Slot()
+    def update(self):
+        self._update_files()
+
+    def _update_files(self):
+        file_list = []
+        root = self._project_path.toLocalFile()
+        for subdir, dirs, files in os.walk(root):
+            for file in files:
+                path = os.path.join(root, subdir, file)
+                _, ext = os.path.splitext(path)
+                if ext == '.qml':
+                    file_list.append(path)
+        self._qml_files = file_list
+        self.qmlFilesChanged.emit(self._qml_files)
