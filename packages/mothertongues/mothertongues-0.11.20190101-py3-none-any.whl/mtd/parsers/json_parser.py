b63a43e2830da3c9704a23fa9c57bf7738b009fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:47 +0000

    Add package mothertongues/0.11.20190101/mothertongues-0.11.20190101-py3-none-any.whl

---
+++ packages/mothertongues/mothertongues-0.11.20190101-py3-none-any.whl/mtd/parsers/json_parser.py
@@ -0,0 +1,43 @@
+import json
+from jsonpointer import resolve_pointer, JsonPointerException
+import pandas as pd
+from mtd.exceptions import SchemaValidationError
+from mtd.parsers.utils import BaseParser
+from mtd.languages import MANIFEST_SCHEMA
+from jsonschema.exceptions import ValidationError
+from mtd.parsers.utils import ResourceManifest
+from typing import Dict, List, Union
+
+class Parser(BaseParser):
+    '''
+    Parse data for MTD. location (jsonpath) specifies location of data
+
+    :param ResourceManifest manifest: Manifest for parser
+    :param (str or json) resource_path: JSON or path to JSON
+    '''
+    def __init__(self, manifest: ResourceManifest, resource_path: Union[str, dict, list]):
+        self.manifest = manifest
+        try:
+            if isinstance(resource_path, str):
+                with open(resource_path, encoding='utf8') as f:
+                    self.resource = json.load(f)
+            elif isinstance(resource_path, (dict, list)):
+                self.resource = resource_path
+        except ValueError:
+            raise SchemaValidationError('json', resource_path)
+        if "location" in self.manifest:
+            self.resource = resolve_pointer(self.resource, self.manifest['location'])
+        self.entry_template = self.manifest['targets']
+
+    def resolve_targets(self) -> List[dict]:
+        word_list = []
+        for entry in self.resource:
+            word_list.append(self.fill_entry_template(self.entry_template, entry, resolve_pointer))
+        return word_list
+    
+    def parse(self) -> Dict[str, Union[dict, pd.DataFrame]]:
+        try:
+            data = self.resolve_targets()
+            return {"manifest": self.manifest, "data": pd.DataFrame(data)}
+        except JsonPointerException as e:
+            raise e
