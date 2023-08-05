23f879102597ad5dca44ec8eb92da0db3d4d98aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:34 +0000

    Add package blackhc.implicit-lambda/0.4.0/blackhc.implicit_lambda-0.4.0-py3-none-any.whl

---
+++ packages/blackhc.implicit-lambda/blackhc.implicit_lambda-0.4.0-py3-none-any.whl/blackhc/implicit_lambda/details/collect_args.py
@@ -0,0 +1,78 @@
+"""Collect all positional arguments and keyword arguments."""
+from dataclasses import dataclass
+import typing
+
+from blackhc.implicit_lambda.details import expression
+from blackhc.implicit_lambda.args_resolver import CollectedArgs, ResolvedArgs, strict_resolver
+
+
+def add_arg(context: CollectedArgs, accessor: expression.ArgsAccessor):
+    order = accessor.order
+    if order not in context.args:
+        s = context.args[order] = set()
+    else:
+        s = context.args[order]
+    s.add(accessor.name)
+
+
+def add_kwarg(context: CollectedArgs, accessor: expression.KwArgsAccessor):
+    if accessor.name not in context.kwargs:
+        context.kwargs.append(accessor.name)
+
+
+def collect_args_(expr, context: CollectedArgs):
+    if expr is None:
+        return
+
+    if isinstance(expr, expression.Expression):
+        if isinstance(expr, expression.AccessorExpression):
+            collect_args_(expr.target, context)
+            collect_args_(expr.key, context)
+        elif isinstance(expr, expression.OpExpression):
+            collect_args_(expr.arg0, context)
+            collect_args_(expr.arg1, context)
+            collect_args_(expr.arg2, context)
+        elif isinstance(expr, expression.CallExpression):
+            collect_args_(expr.target, context)
+            collect_args_(expr.args, context)
+            collect_args_(expr.kwargs, context)
+        elif isinstance(expr, expression.ArgsAccessor):
+            add_arg(context, expr)
+        elif isinstance(expr, expression.KwArgsAccessor):
+            add_kwarg(context, expr)
+        elif isinstance(expr, expression.LiteralExpression):
+            pass
+        else:
+            raise NotImplementedError(type(expr))
+    elif isinstance(expr, tuple):
+        for item in expr:
+            collect_args_(item, context)
+    elif isinstance(expr, list):
+        for item in expr:
+            collect_args_(item, context)
+    elif isinstance(expr, dict):
+        for key, value in expr.items():
+            collect_args_(key, context)
+            collect_args_(value, context)
+    elif isinstance(expr, set):
+        for item in expr:
+            collect_args_(item, context)
+    elif isinstance(expr, slice):
+        collect_args_(expr.start, context)
+        collect_args_(expr.stop, context)
+        collect_args_(expr.step, context)
+
+
+def collect_args(expr, context: CollectedArgs = None) -> CollectedArgs:
+    context = context or CollectedArgs(args={}, kwargs=[])
+    collect_args_(expr, context)
+    return context
+
+
+def compute_args(expr, *, args_resolver=None):
+    args_resolver = args_resolver or strict_resolver
+
+    context = collect_args(expr)
+    computed_args = args_resolver(context)
+
+    return computed_args
---
+++ packages/blackhc.implicit-lambda/blackhc.implicit_lambda-0.4.0-py3-none-any.whl/blackhc/implicit_lambda/tests/test_special_ops.py
@@ -0,0 +1,78 @@
+import math
+import pytest
+import hypothesis
+import hypothesis.strategies as strategies
+
+import blackhc.implicit_lambda as implicit_lambda
+from blackhc.implicit_lambda.details import lambda_dsl
+from blackhc.implicit_lambda.details import expression
+from blackhc.implicit_lambda.details import codegen
+from blackhc.implicit_lambda.details import interpret
+
+
+def test_repr():
+    assert repr(implicit_lambda._ + 5) == "<LambdaDSL: (lambda _: (_ + 5)) @ {}>"
+
+
+def test_str():
+    assert repr(implicit_lambda._ + 5) == "<LambdaDSL: (lambda _: (_ + 5)) @ {}>"
+
+
+def test_index():
+    with pytest.raises(NotImplementedError):
+        hex(implicit_lambda._ + 5)
+
+    with pytest.raises(NotImplementedError):
+        oct(implicit_lambda._ + 5)
+
+    with pytest.raises(NotImplementedError):
+        bin(implicit_lambda._ + 5)
+
+
+def test_int():
+    with pytest.raises(NotImplementedError):
+        int(implicit_lambda._)
+
+
+def test_float():
+    with pytest.raises(NotImplementedError):
+        float(implicit_lambda._)
+
+
+def test_complex():
+    with pytest.raises(NotImplementedError):
+        complex(implicit_lambda._)
+
+
+def test_len():
+    with pytest.raises(NotImplementedError):
+        len(implicit_lambda._)
+
+
+def test_bool():
+    with pytest.raises(NotImplementedError):
+        bool(implicit_lambda._)
+
+
+def test_contains():
+    with pytest.raises(NotImplementedError):
+        None in implicit_lambda._
+
+
+def test_not_contains():
+    with pytest.raises(NotImplementedError):
+        None not in implicit_lambda._
+
+
+def test_contains_wrappers():
+    assert (
+        repr(implicit_lambda.contains(implicit_lambda.a, implicit_lambda.b))
+        == "<LambdaDSL: (lambda a, b: (b) in (a)) @ {}>"
+    )
+
+
+def test_not_contains_wrappers():
+    assert (
+        repr(implicit_lambda.not_contains(implicit_lambda.a, implicit_lambda.b))
+        == "<LambdaDSL: (lambda a, b: (b) not in (a)) @ {}>"
+    )
