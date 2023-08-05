0dcc8b7c5cdcf0a7ad0ffac1688af464d027a583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:56 +0000

    Add package chrome-trex-gym/0.0.2/chrome_trex_gym-0.0.2.tar.gz

---
+++ packages/chrome-trex-gym/chrome_trex_gym-0.0.2.tar.gz/chrome_trex_gym-0.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="chrome_trex_gym",
+    version="0.0.2",
+    author="Badr Youbi Idrissi",
+    author_email="badryoubiidrissi@gmail.com",
+    description="A gym environement for the chrome trex game",
+    url="https://github.com/BadrYoubiIdrissi/chrome-trex-gym.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
