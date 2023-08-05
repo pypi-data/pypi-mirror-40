31d37d6aa877470ec4ab445318f281c76e09f561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:41 +0000

    Add package octokitpy-routes/0.0.1/octokitpy-routes-0.0.1.tar.gz

---
+++ packages/octokitpy-routes/octokitpy-routes-0.0.1.tar.gz/octokitpy-routes-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='octokitpy-routes',
+    version='0.0.1',
+    description='Routes for octokitpy',
+    long_description='Routes from the NPM published octokit routes',
+    author='Kyle Hornberg',
+    author_email='kyle.hornberg@gmail.com',
+    url='https://github.com/khornberg/routes',
+    packages=['routes'],
+)
