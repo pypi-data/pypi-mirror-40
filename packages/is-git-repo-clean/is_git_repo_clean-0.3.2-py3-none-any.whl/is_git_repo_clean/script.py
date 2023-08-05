f6380b8f7738a735a8156d9a5edbc8a82a68e8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:04 +0000

    Add package is-git-repo-clean/0.3.2/is_git_repo_clean-0.3.2-py3-none-any.whl

---
+++ packages/is-git-repo-clean/is_git_repo_clean-0.3.2-py3-none-any.whl/is_git_repo_clean/script.py
@@ -0,0 +1,17 @@
+from .cli import getIsGitRepoClean
+import sys
+
+
+def printErr(msg):
+    print(msg, file=sys.stderr)
+
+
+result = getIsGitRepoClean(sys.argv[1:])
+
+if result.stdout:
+    print(result.stdout)
+
+if result.stderr:
+    printErr(result.stderr)
+
+exit(result.code)
