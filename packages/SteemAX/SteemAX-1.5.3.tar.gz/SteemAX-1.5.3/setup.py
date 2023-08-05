097571f9b0c8f9257a666c7348a8183f305bc742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:38 +0000

    Add package SteemAX/1.5.3/SteemAX-1.5.3.tar.gz

---
+++ packages/SteemAX/SteemAX-1.5.3.tar.gz/SteemAX-1.5.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    python_requires='>=3.0',
+    name='SteemAX',
+    version='1.5.3',
+    packages=['steemax'],
+    license='MIT',
+    keywords='steemit steem upvote exchange',
+    url='http://github.com/artolabs/steemax',
+    author='ArtoLabs',
+    author_email='artopium@gmail.com',
+    install_requires=[
+        'python-dateutil',
+        'simplesteem',
+        'screenlogger',
+    ],
+    py_modules=['steemax'],
+    entry_points = {
+        'console_scripts': [
+            'steemax=steemax.steemax:run',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False
+)
