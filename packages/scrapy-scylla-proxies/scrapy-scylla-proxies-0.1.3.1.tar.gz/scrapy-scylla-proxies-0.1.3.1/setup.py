af5d5c54b1021eae64b98b1d9c19cab0438c37e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:57 +0000

    Add package scrapy-scylla-proxies/0.1.3.1/scrapy-scylla-proxies-0.1.3.1.tar.gz

---
+++ packages/scrapy-scylla-proxies/scrapy-scylla-proxies-0.1.3.1.tar.gz/scrapy-scylla-proxies-0.1.3.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+from distutils.core import setup
+
+setup(
+    name='scrapy-scylla-proxies',
+    version='0.1.3.1',
+    description='Scrapy-Scylla Proxies: random proxy middleware for Scrapy',
+    author='Kevin Glasson',
+    author_email='kevinglasson+scrapyscylla@gmail.com',
+    url='https://github.com/kevinglasson/scrapy-scylla-proxies.git',
+    packages=['scrapy_scylla_proxies'],
+    install_requires=['requests'],
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ]
+)
