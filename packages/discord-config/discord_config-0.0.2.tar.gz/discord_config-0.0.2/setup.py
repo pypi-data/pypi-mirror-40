9f2f1f3fd7bfaafefa30e5809db162b8ed68230d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:04 +0000

    Add package discord-config/0.0.2/discord_config-0.0.2.tar.gz

---
+++ packages/discord-config/discord_config-0.0.2.tar.gz/discord_config-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discord_config",
+    version="0.0.2",
+    author="Casimir Nowak",
+    author_email="nowakcasimir@outlook.com",
+    description="Easy config module for Discord bots",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/nowakcasimir/discord-config",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+       'appdirs'
+    ]
+)
