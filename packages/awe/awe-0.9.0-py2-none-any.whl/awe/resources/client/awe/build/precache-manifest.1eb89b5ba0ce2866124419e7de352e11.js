adfc0d66ef29e47c61a7f913fbde9fb78bc6be51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:00 +0000

    Add package awe/0.9.0/awe-0.9.0-py2-none-any.whl

---
+++ packages/awe/awe-0.9.0-py2-none-any.whl/awe/resources/client/awe/build/precache-manifest.1eb89b5ba0ce2866124419e7de352e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22d3d44b7c02275b5a5f",
+    "url": "/static/static/js/runtime~main.22d3d44b.js"
+  },
+  {
+    "revision": "deff51cd68ea867ab6ee",
+    "url": "/static/static/js/main.deff51cd.chunk.js"
+  },
+  {
+    "revision": "cbdaa6ffe3720f1f8afe",
+    "url": "/static/static/js/1.cbdaa6ff.chunk.js"
+  },
+  {
+    "revision": "deff51cd68ea867ab6ee",
+    "url": "/static/static/css/main.2faf5e0b.chunk.css"
+  },
+  {
+    "revision": "cbdaa6ffe3720f1f8afe",
+    "url": "/static/static/css/1.244a37d0.chunk.css"
+  },
+  {
+    "revision": "4a30b4821f65320942c950953cfb9375",
+    "url": "/static/index.html"
+  }
+];
---
+++ packages/awe/awe-0.9.0-py2-none-any.whl/awe/resources/client/awe/build/precache-manifest.6fe589c7e137bbf61c4229498c844c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22d3d44b7c02275b5a5f",
+    "url": "/static/static/js/runtime~main.22d3d44b.js"
+  },
+  {
+    "revision": "40716ed931d10688727b",
+    "url": "/static/static/js/main.40716ed9.chunk.js"
+  },
+  {
+    "revision": "7eb5196c821caf1b5b17",
+    "url": "/static/static/js/1.7eb5196c.chunk.js"
+  },
+  {
+    "revision": "40716ed931d10688727b",
+    "url": "/static/static/css/main.a35e5b61.chunk.css"
+  },
+  {
+    "revision": "7eb5196c821caf1b5b17",
+    "url": "/static/static/css/1.447672ec.chunk.css"
+  },
+  {
+    "revision": "d037f70867c307a7b96af24ec9051cee",
+    "url": "/static/index.html"
+  }
+];
