e1010ea928d9a05c87d89076d57dfc364d3066fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:12 +0000

    Add package SpliceV/0.1.1/SpliceV-0.1.1.tar.gz

---
+++ packages/SpliceV/SpliceV-0.1.1.tar.gz/SpliceV-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='SpliceV',
+    version='0.1.1',
+    author='Nathan Ungerleider',
+    author_email='nungerle@tulane.edu',
+    description='Visualize splice junctions, backsplice junctions (circleRNA) and coverage from RNA-Seq datasets',
+    url='https://github.com/flemingtonlab/SpliceV',
+    packages=setuptools.find_packages(),
+    long_description=open('README.md').read(),
+    classifiers=('Programming Language :: Python :: 3',
+                'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                'Operating System :: OS Independent',
+                ),
+    install_requires = ['matplotlib', 'numpy', 'pysam'],
+    python_requires='>=2.7,>=3',
+    scripts=['bin/fa.py','bin/SpliceV', 'bin/find_circ_convert', 'bin/star_sj_convert'],
+)
+
