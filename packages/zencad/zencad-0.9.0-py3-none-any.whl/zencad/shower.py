2c46433b2829d7bbcbff1fed8d134e29cc898dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:06 +0000

    Add package zencad/0.9.0/zencad-0.9.0-py3-none-any.whl

---
+++ packages/zencad/zencad-0.9.0-py3-none-any.whl/zencad/shower.py
@@ -0,0 +1,355 @@
+# coding: utf-8
+
+import zencad
+import zencad.lazy
+import pyservoce
+
+import sys
+import os
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+from PIL import Image
+import numpy as np
+
+import time
+import threading
+
+#from OpenGL.GL import *
+#from OpenGL.GLUT import *
+#from OpenGL.GLU import *
+
+import math
+
+class MainWidget(QMainWindow):
+	def __init__(self, dispw):
+		QMainWindow.__init__(self)
+		self.dispw = dispw
+
+		self.setWindowTitle("zenwidget");
+		#self.setWindowIcon(QIcon(":/industrial-robot.svg"));
+
+		self.createActions();
+		self.createMenus();
+		self.createToolbars();
+
+	def createActions(self):
+		self.mExitAction = QAction(self.tr("Exit"), self)
+		self.mExitAction.setShortcut(self.tr("Ctrl+Q"))
+		self.mExitAction.setStatusTip(self.tr("Exit the application"))
+		self.mExitAction.triggered.connect(self.close)
+	
+		self.mStlExport = QAction(self.tr("Export STL..."), self)
+		self.mStlExport.setStatusTip(self.tr("Export file with external STL-Mesh format"))
+		self.mStlExport.triggered.connect(self.exportStlAction);
+
+		self.mBrepExport = QAction(self.tr("Export BREP..."), self)
+		self.mBrepExport.setStatusTip(self.tr("Export file in BREP format"))
+		self.mBrepExport.triggered.connect(self.exportBrepAction);
+	
+		self.mScreen = QAction(self.tr("Screenshot..."), self)
+		self.mScreen.setStatusTip(self.tr("Do screen"))
+		self.mScreen.triggered.connect(self.screenshotAction)
+	
+		self.mAboutAction = QAction(self.tr("About"), self)
+		self.mAboutAction.setStatusTip(self.tr("About the application"))
+		self.mAboutAction.triggered.connect(self.aboutAction)
+	
+		self.mReset = QAction(self.tr("Reset"), self)
+		self.mReset.setStatusTip(self.tr("Reset"))
+		self.mReset.triggered.connect(self.resetAction)
+
+		self.mAutoscale = QAction(self.tr("Autoscale"), self)
+		self.mAutoscale.setStatusTip(self.tr("Autoscale"))
+		self.mAutoscale.triggered.connect(self.autoscaleAction)
+	
+		self.mOrient1 = QAction(self.tr("Orient1"), self)
+		self.mOrient1.setStatusTip(self.tr("Orient1"))
+		self.mOrient1.triggered.connect(self.orient1)
+	
+		self.mOrient2 = QAction(self.tr("Orient2"), self)
+		self.mOrient2.setStatusTip(self.tr("Orient2"))
+		self.mOrient2.triggered.connect(self.orient2)
+
+	def createMenus(self):
+		self.mFileMenu = self.menuBar().addMenu(self.tr("&File"))
+		self.mFileMenu.addAction(self.mStlExport)
+		self.mFileMenu.addAction(self.mBrepExport)
+		self.mFileMenu.addAction(self.mScreen)
+		self.mFileMenu.addSeparator()
+		self.mFileMenu.addAction(self.mExitAction)
+	
+		self.mNavigationMenu = self.menuBar().addMenu(self.tr("&Navigation"))
+		self.mNavigationMenu.addAction(self.mReset)
+		self.mNavigationMenu.addAction(self.mAutoscale)
+		self.mNavigationMenu.addAction(self.mOrient1)
+		self.mNavigationMenu.addAction(self.mOrient2)
+	
+		self.mHelpMenu = self.menuBar().addMenu(self.tr("&Help"))
+		self.mHelpMenu.addAction(self.mAboutAction)
+		
+	def createToolbars(self):
+		pass
+
+	def exportStlAction(self):
+		d, okPressed = QInputDialog.getDouble(self, "Get double","Value:", 0.01, 0, 10, 10)
+		if not okPressed:
+			return
+
+		filters = "*.stl;;*.*";
+		defaultFilter = "*.stl";
+
+		path = QFileDialog.getSaveFileName(self, "STL Export", 
+			QDir.currentPath(),
+			filters, defaultFilter);
+
+		path = path[0]
+
+		pyservoce.make_stl(self.dispw.scene[0].shape(), path, d)
+
+	def exportBrepAction(self):
+		filters = "*.brep;;*.*";
+		defaultFilter = "*.brep";
+
+		path = QFileDialog.getSaveFileName(self, "BREP Export", 
+			QDir.currentPath(),
+			filters, defaultFilter);
+
+		path = path[0]
+
+		pyservoce.brep_write(self.dispw.scene[0].shape(), path)
+
+	def autoscaleAction(self):
+		self.dispw.view.fit_all()
+
+	def orient1(self):
+		self.dispw.reset_orient1()
+
+	def orient2(self):
+		self.dispw.reset_orient2()
+
+	def resetAction(self):
+		self.dispw.view.reset_orientation()
+		self.dispw.view.autoscale()
+
+	def screenshotAction(self):
+		filters = "*.png;;*.bmp;;*.jpg;;*.*";
+		defaultFilter = "*.png";
+
+		path = QFileDialog.getSaveFileName(self, "Dump image", 
+			QDir.currentPath(),
+			filters, defaultFilter);
+
+		path = path[0]
+		
+		w = self.dispw.width()
+		h = self.dispw.height()
+
+		raw = self.dispw.view.rawarray()
+		print (raw)
+
+		return
+
+		npixels = np.reshape(np.asarray(raw), (w,h,3))
+		nnnpixels = np.flip(npixels, 0).reshape((w * h * 3))
+
+		rawiter = iter(nnnpixels)
+		pixels = list(zip(rawiter, rawiter, rawiter))
+		
+		image = Image.new("RGB", (w, h))
+		image.putdata(pixels)
+
+		image.save(path)
+
+	def aboutAction(self):
+		QMessageBox.about(self, self.tr("About ZenCad Shower"),
+			self.tr("<h2>Shower</h2>"
+			"<p>Author: mirmik(mirmikns@yandex.ru) 2018"
+			"<p>Widget for display zencad geometry."));
+
+	def keyPressEvent (self, event):
+		if event.key() == Qt.Key_PageDown:
+			self.dispw.pageDownKeyHandler()
+		elif event.key() == Qt.Key_PageUp:
+			self.dispw.pageUpKeyHandler()
+
+class DisplayWidget(QWidget):
+	def __init__(self, arg):
+		QWidget.__init__(self)
+
+		self.orient = 1
+
+		self.inited = False
+		self.painted = False
+
+		self.scene = arg
+		self.temporary1 = QPoint()	
+		self.psi =   math.cos(math.pi / 4)
+		self.phi = - math.cos(math.pi / 4)
+
+		self.setBackgroundRole( QPalette.NoRole )
+		self.setAttribute(Qt.WA_PaintOnScreen, True) 
+
+	def reset_orient1(self):		
+		self.orient = 1
+		self.psi =   math.cos(math.pi / 4)
+		self.phi = - math.cos(math.pi / 4)
+		self.view.reset_orientation()
+		self.view.autoscale()
+
+	def reset_orient2(self):		
+		self.orient = 2
+		#self.view.reset_orientation()
+		
+
+	def set_orient1(self):
+		self.view.set_projection(
+			math.cos(self.psi) * math.cos(self.phi), 
+			math.cos(self.psi) * math.sin(self.phi), 
+			math.sin(self.psi)
+		);
+
+	def paintEvent(self, ev):
+#		print("paintEvent")
+		if self.inited and not self.painted:
+			self.view.fit_all()
+			self.view.must_be_resized()
+			self.painted = True
+
+	def showEvent(self, ev):
+#		print("showEvent")
+		self.viewer = zencad.Viewer(self.scene)
+		self.view = self.viewer.create_view()
+		self.view.set_window(self.winId())
+		#self.view.set_gradient()
+		
+		self.set_orient1()
+		self.view.set_triedron()
+		self.viewer.set_triedron_axes()
+
+
+		self.inited = True
+
+	def resizeEvent(self, ev):
+		if self.inited:
+			self.view.must_be_resized()
+
+	def paintEngine(self):
+		return None
+
+	def onLButtonDown(self, theFlags, thePoint):
+		self.temporary1 = thePoint;
+		self.view.start_rotation(thePoint.x(), thePoint.y(), 1)
+
+	def onRButtonDown(self, theFlags, thePoint):
+		self.temporary1 = thePoint;
+
+	def onMButtonDown(self, theFlags, thePoint):
+		self.temporary1 = thePoint;
+
+	def onMouseWheel(self, theFlags, theDelta, thePoint):
+		aFactor = 16
+	
+		aX = thePoint.x()
+		aY = thePoint.y()
+	
+		if (theDelta.y() > 0):
+			aX += aFactor
+			aY += aFactor
+		else:
+			aX -= aFactor
+			aY -= aFactor
+		
+		self.view.zoom(thePoint.x(), thePoint.y(), aX, aY)
+
+	def onMouseMove(self, theFlags, thePoint):
+		mv = thePoint - self.temporary1
+		self.temporary1 = thePoint
+
+		if theFlags & Qt.LeftButton: 
+			if self.orient == 1:  
+				self.phi -= mv.x() * 0.01;
+				self.psi += mv.y() * 0.01;
+				if self.psi > math.pi*0.4999: self.psi = math.pi*0.4999
+				if self.psi < -math.pi*0.4999: self.psi = -math.pi*0.4999
+				self.set_orient1()
+		
+			if self.orient == 2:
+					self.view.rotation(thePoint.x(), thePoint.y());
+		
+		if theFlags & Qt.RightButton:
+			self.view.pan(mv.x(), -mv.y())
+	
+	def wheelEvent(self, e):
+		self.onMouseWheel(e.buttons(), e.angleDelta(), e.pos());
+
+	def mouseReleaseEvent(self, e):
+		pass
+
+	def mouseMoveEvent(self, e):
+		self.onMouseMove(e.buttons(), e.pos());
+
+	def mousePressEvent(self, e):
+		if e.button() == Qt.LeftButton:
+			self.onLButtonDown((e.buttons() | e.modifiers()), e.pos())
+		
+		elif e.button() == Qt.MidButton:
+			self.onMButtonDown((e.buttons() | e.modifiers()), e.pos())
+		
+		elif e.button() == Qt.RightButton:
+			self.onRButtonDown((e.buttons() | e.modifiers()), e.pos())
+
+	def pageDownKeyHandler(self):
+		x = self.width()/2
+		y = self.height()/2
+		factor = 16
+		self.view.zoom(x, y, x - factor, y - factor)
+
+	def pageUpKeyHandler(self):
+		x = self.width()/2
+		y = self.height()/2
+		factor = 16
+		self.view.zoom(x, y, x + factor, y + factor)
+
+def update_loop(updater_function, wdg, update_time):
+	while 1:
+		ensave = zencad.lazy.encache 
+		desave = zencad.lazy.decache
+		if wdg.inited:
+			zencad.lazy.encache = False
+			zencad.lazy.decache = False
+			updater_function(wdg.viewer)
+			zencad.lazy.encache = ensave
+			zencad.lazy.decache = desave
+			#time.sleep(50/1000)
+		
+
+def show(scene, updater_function = None, update_time = 50):
+	app = QApplication(sys.argv)
+	app.setWindowIcon(QIcon(os.path.dirname(__file__) + '/industrial-robot.svg'))
+
+	fmt = QSurfaceFormat()
+	fmt.setDepthBufferSize(24)
+	fmt.setStencilBufferSize(8)
+	fmt.setVersion(3, 2)
+	fmt.setProfile(QSurfaceFormat.CoreProfile)
+	QSurfaceFormat.setDefaultFormat(fmt)
+
+	disp = DisplayWidget(scene)
+	mw = MainWidget(disp);	
+
+	mw.setCentralWidget(disp)
+
+	mw.resize(800,600)
+
+	if updater_function != None:
+		thr = threading.Thread(target=update_loop, args=(updater_function, disp, update_time))
+		thr.start()
+
+
+#	print("show")
+	mw.show()
+
+	app.exec_()
---
+++ packages/zencad/zencad-0.9.0-py3-none-any.whl/zencad/transform.py
@@ -0,0 +1,89 @@
+import pyservoce
+from zencad.util import deg, point3
+from zencad.lazy import lazy
+from zencad.boolean import *
+
+import numpy as np
+
+@lazy
+def translate(*args, **kwargs): return pyservoce.translate(*args, **kwargs)
+
+@lazy
+def up(*args, **kwargs): return pyservoce.up(*args, **kwargs)
+
+@lazy
+def down(*args, **kwargs): return pyservoce.down(*args, **kwargs)
+
+@lazy
+def left(*args, **kwargs): return pyservoce.left(*args, **kwargs)
+
+@lazy
+def right(*args, **kwargs): return pyservoce.right(*args, **kwargs)
+
+@lazy
+def forw(*args, **kwargs): return pyservoce.forw(*args, **kwargs)
+
+@lazy
+def back(*args, **kwargs): return pyservoce.back(*args, **kwargs)
+
+@lazy
+def rotateX(*args, **kwargs): return pyservoce.rotateZ(*args, **kwargs)
+
+@lazy
+def rotateY(*args, **kwargs): return pyservoce.rotateZ(*args, **kwargs)
+
+@lazy
+def rotateZ(*args, **kwargs): return pyservoce.rotateZ(*args, **kwargs)
+
+@lazy
+def mirrorXZ(*args, **kwargs): return pyservoce.mirrorXZ(*args, **kwargs)
+
+@lazy
+def mirrorYZ(*args, **kwargs): return pyservoce.mirrorYZ(*args, **kwargs)
+
+@lazy
+def mirrorXY(*args, **kwargs): return pyservoce.mirrorXY(*args, **kwargs)
+
+@lazy
+def mirrorX(*args, **kwargs): return pyservoce.mirrorX(*args, **kwargs)
+
+@lazy
+def mirrorY(*args, **kwargs): return pyservoce.mirrorY(*args, **kwargs)
+
+@lazy
+def mirrorZ(*args, **kwargs): return pyservoce.mirrorZ(*args, **kwargs)
+
+@lazy
+def scale(factor, center): return pyservoce.scale(factor, point3(center).to_servoce())
+
+@lazy
+def scaleX(factor): return pyservoce.scaleX(factor)
+
+@lazy
+def scaleY(factor): return pyservoce.scaleY(factor)
+
+@lazy
+def scaleZ(factor): return pyservoce.scaleZ(factor)
+
+class multitransform:
+	def __init__(self, transes):
+		self.transes = transes
+
+	def __call__(self, shp):
+		return union([t(shp) for t in self.transes])
+
+def multitrans(transes):
+	return multitransform(transes)
+
+def nulltrans(): return translate(0,0,0) 
+
+def sqrtrans(): return multitransform([ 
+	nulltrans(), 
+	mirrorYZ(),
+	mirrorXZ(), 
+	mirrorZ() 
+])
+
+def rotate_array(n):
+	transes = [rotateZ(angle) for angle in np.linspace(0, deg(360), num=n, endpoint=False)]
+	return multitrans(transes) 
