d9599f3bf0be498c36e19a6f1eef757bf73a021d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package nnmnkwii/0.0.17/nnmnkwii-0.0.17.tar.gz

---
+++ packages/nnmnkwii/nnmnkwii-0.0.17.tar.gz/nnmnkwii-0.0.17/setup.py
@@ -0,0 +1,157 @@
+# coding: utf-8
+
+from __future__ import with_statement, print_function, absolute_import
+
+from setuptools import setup, find_packages, Extension
+import setuptools.command.develop
+import setuptools.command.build_py
+from distutils.version import LooseVersion
+from os.path import join, exists
+import subprocess
+import os
+import numpy as np
+
+version = '0.0.17'
+
+# Adapted from https://github.com/pytorch/pytorch
+cwd = os.path.dirname(os.path.abspath(__file__))
+if os.getenv('NNMNKWII_BUILD_VERSION'):
+    version = os.getenv('NNMNKWII_BUILD_VERSION')
+else:
+    try:
+        sha = subprocess.check_output(
+            ['git', 'rev-parse', 'HEAD'], cwd=cwd).decode('ascii').strip()
+        version += '+' + sha[:7]
+    except subprocess.CalledProcessError:
+        pass
+    except IOError:  # FileNotFoundError for python 3
+        pass
+
+
+class build_py(setuptools.command.build_py.build_py):
+
+    def run(self):
+        self.create_version_file()
+        setuptools.command.build_py.build_py.run(self)
+
+    @staticmethod
+    def create_version_file():
+        global version, cwd
+        print('-- Building version ' + version)
+        version_path = os.path.join(cwd, 'nnmnkwii', 'version.py')
+        with open(version_path, 'w') as f:
+            f.write("__version__ = '{}'\n".format(version))
+
+
+class develop(setuptools.command.develop.develop):
+
+    def run(self):
+        build_py.create_version_file()
+        setuptools.command.develop.develop.run(self)
+
+
+min_cython_ver = '0.21.0'
+try:
+    import Cython
+    ver = Cython.__version__
+    _CYTHON_INSTALLED = ver >= LooseVersion(min_cython_ver)
+except ImportError:
+    _CYTHON_INSTALLED = False
+
+try:
+    if not _CYTHON_INSTALLED:
+        raise ImportError('No supported version of Cython installed.')
+    from Cython.Distutils import build_ext
+    cython = True
+except ImportError:
+    cython = False
+
+if cython:
+    ext = '.pyx'
+    cmdclass = {'build_ext': build_ext}
+else:
+    ext = '.c'
+    cmdclass = {}
+    print("Building extentions from pre-generated C source")
+    if not os.path.exists(join("nnmnkwii", "paramgen", "mlpg_helper" + ext)):
+        raise RuntimeError("Cython is required to generate C code.")
+
+ext_modules = [
+    Extension(
+        name="nnmnkwii.util._linalg",
+        sources=[join("nnmnkwii", "util", "_linalg" + ext)],
+        include_dirs=[np.get_include()],
+        language="c",
+        extra_compile_args=["-std=c99"],
+    ),
+    Extension(
+        name="nnmnkwii.paramgen.mlpg_helper",
+        sources=[join("nnmnkwii", "paramgen", "mlpg_helper" + ext)],
+        include_dirs=[np.get_include()],
+        language="c",
+        extra_compile_args=["-std=c99"]
+    ),
+]
+
+cmdclass['build_py'] = build_py
+cmdclass['develop'] = develop
+
+
+def package_files(directory):
+    # https://stackoverflow.com/questions/27664504/
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+package_data = package_files("./nnmnkwii/util/_example_data")
+
+setup(
+    name='nnmnkwii',
+    version=version,
+    description='Library to build speech synthesis systems designed for easy and fast prototyping.',
+    long_description=open('README.md', 'rb').read().decode("utf-8"),
+    long_description_content_type='text/markdown',
+    author='Ryuichi Yamamoto',
+    author_email='zryuichi@gmail.com',
+    url='https://github.com/r9y9/nnmnkwii',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'': package_data},
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
+    install_requires=[
+        'numpy >= 1.11.0',
+        'scipy',
+        'cython >= ' + min_cython_ver,
+        'bandmat >= 0.7',
+        'fastdtw',
+        'sklearn',
+        'pysptk >= 0.1.7',
+        'tqdm',
+    ],
+    tests_require=['nose', 'coverage'],
+    extras_require={
+        'docs': ['numpydoc', 'sphinx_rtd_theme'],
+        'test': ['nose', 'pyworld', 'librosa'],
+    },
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+    ],
+    keywords=["Research"]
+)
