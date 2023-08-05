28066ad6bff3b8a4df72072db7165fb37e2abfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package eli-test-distrib/0.1/eli_test_distrib-0.1.tar.gz

---
+++ packages/eli-test-distrib/eli_test_distrib-0.1.tar.gz/eli_test_distrib-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='eli_test_distrib',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['eli_test_distrib'],
+      zip_safe=False)
