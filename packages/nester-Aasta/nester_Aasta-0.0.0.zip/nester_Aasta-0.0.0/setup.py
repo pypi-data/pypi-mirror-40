a0fdb8d4cfdc55a35603923b7b55fd5776f82bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:22 +0000

    Add package nester-Aasta/0.0.0/nester_Aasta-0.0.0.zip

---
+++ packages/nester-Aasta/nester_Aasta-0.0.0.zip/nester_Aasta-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester_Aasta',
+	varsion = '1.0.0',
+	py_modules = ['nester_Aasta'],
+	author = 'aasta',
+	author_email = 'aasta@wirklich.tw',
+	url = 'http://wirklich.tw',
+	description = 'python Practice'
+)
