953206803008b97287fb40436f81a1e49754361e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:38 +0000

    Add package persianutils/0.1.1/persianutils-0.1.1-py3-none-any.whl

---
+++ packages/persianutils/persianutils-0.1.1-py3-none-any.whl/persianutils/__init__.py
@@ -0,0 +1,8 @@
+"""
+   Persian Utilities
+   source : persianutils 'https://github.com/ishto7/persianutils'
+"""
+
+# __author__ = "Iman Nazari"
+
+from persianutils.processes import standardize, standardize4Word2vec
