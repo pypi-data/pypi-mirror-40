12a3a6b2120ddb233e5f6c67b1bbf251233bd4cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:29 +0000

    Add package threedvector/0.3.2/threedvector-0.3.2.tar.gz

---
+++ packages/threedvector/threedvector-0.3.2.tar.gz/threedvector-0.3.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="threedvector",
+    version="0.3.2",
+    author="Paul Nel",
+    author_email="gpaul.nel@gmail.com",
+    description="A package to work with 3D vectors in Spherical and Cartesian coordinates.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/gpaul.nel/threedvector",
+    packages=setuptools.find_packages(exclude=["UnitTests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
