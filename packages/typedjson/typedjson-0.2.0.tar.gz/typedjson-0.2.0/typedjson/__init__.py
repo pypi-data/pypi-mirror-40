5ad94d82fdb44135b7584cebb006c245566edfdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:41 +0000

    Add package typedjson/0.2.0/typedjson-0.2.0.tar.gz

---
+++ packages/typedjson/typedjson-0.2.0.tar.gz/typedjson-0.2.0/typedjson/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from typedjson.decoding import decode
+from typedjson.decoding import DecodingError
+
+from typedjson.loading import load
+from typedjson.loading import loads
