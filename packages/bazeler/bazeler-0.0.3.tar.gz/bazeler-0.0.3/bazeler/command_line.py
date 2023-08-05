5f2df43df3406270254dc2df88bd0432f1c41798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:53 +0000

    Add package bazeler/0.0.3/bazeler-0.0.3.tar.gz

---
+++ packages/bazeler/bazeler-0.0.3.tar.gz/bazeler-0.0.3/bazeler/command_line.py
@@ -0,0 +1,12 @@
+import argparse
+import sys
+import bazeler 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('root')
+    parser.add_argument('--offline', action='store_true')
+    parser.add_argument('--search_paths', default='')
+    parser.add_argument('--permissive', action='store_true')
+    parser.add_argument('--verbose', action='store_true')
+    sys.exit(bazeler.execute(**vars(parser.parse_args())))
