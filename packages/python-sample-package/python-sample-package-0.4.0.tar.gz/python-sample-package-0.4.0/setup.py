d3bed6d7896662088c2fec3863bb845841ae5665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package python-sample-package/0.4.0/python-sample-package-0.4.0.tar.gz

---
+++ packages/python-sample-package/python-sample-package-0.4.0.tar.gz/python-sample-package-0.4.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='python-sample-package',
+    version='0.4.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An example python package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['numpy'],
+    url='https://github.com/iiddoo/python-sample-package.git',
+    author='Ido Lev',
+    author_email='iiddoolleevv@gmail.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
