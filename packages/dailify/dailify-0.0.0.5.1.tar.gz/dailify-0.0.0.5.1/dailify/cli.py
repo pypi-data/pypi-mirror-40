af168e0d34f5bdb121246883d36adb9a912775a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:05 +0000

    Add package dailify/0.0.0.5.1/dailify-0.0.0.5.1.tar.gz

---
+++ packages/dailify/dailify-0.0.0.5.1.tar.gz/dailify-0.0.0.5.1/dailify/cli.py
@@ -0,0 +1,36 @@
+
+
+
+import argparse, random, os
+
+
+
+
+
+
+def main():
+  parser = argparse.ArgumentParser(description="Ghent Theft Module")
+  parser.add_argument("-d", "--daily", type=str, required=True,  dest="daily", help="String input for the daily name.")
+  parser.add_argument("-l", "--launch", action="store_true", required=False, help="Opens a new Blender file at the correct location.")
+  
+  identifier = "".join([str(random.randint(0, 9)) for x in range(6)])
+  uid = " ".join([str(random.randint(0, 9)) for x in range(6)])
+  
+  args = parser.parse_args()
+  name = args.daily
+  spaced = " ".join(name.upper())
+  temp = " ".join([uid, "-", spaced])
+  friendly = "_".join([str(identifier), name])
+  
+
+  os.makedirs(friendly)
+  folderpath = "/".join([os.getcwd(), friendly])
+  
+  if args.launch == True:
+    os.system('blender --python file_handler.py -- {}'.format(folderpath))
+    
+  #print(os.getcwd())
+
+
+if __name__ == "__main__":
+  main()
