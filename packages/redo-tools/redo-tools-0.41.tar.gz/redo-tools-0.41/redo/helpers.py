58b962bd82f36cb62bf0a9da09feaeb298ddfe07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:58 +0000

    Add package redo-tools/0.41/redo-tools-0.41.tar.gz

---
+++ packages/redo-tools/redo-tools-0.41.tar.gz/redo-tools-0.41/redo/helpers.py
@@ -0,0 +1,27 @@
+"""Some helper functions that don't fit anywhere else."""
+import os, errno, fcntl
+
+
+def join(between, l):
+    return between.join(l)
+
+
+def unlink(f):
+    """Delete a file at path 'f' if it currently exists.
+
+    Unlike os.unlink(), does not throw an exception if the file didn't already
+    exist.
+    """
+    try:
+        os.unlink(f)
+    except OSError, e:
+        if e.errno == errno.ENOENT:
+            pass  # it doesn't exist, that's what you asked for
+
+
+def close_on_exec(fd, yes):
+    fl = fcntl.fcntl(fd, fcntl.F_GETFD)
+    fl &= ~fcntl.FD_CLOEXEC
+    if yes:
+        fl |= fcntl.FD_CLOEXEC
+    fcntl.fcntl(fd, fcntl.F_SETFD, fl)
---
+++ packages/redo-tools/redo-tools-0.41.tar.gz/redo-tools-0.41/redo/version/_version.py
@@ -0,0 +1,5 @@
+"""Auto-generated file with git version information."""
+# pylint: disable=bad-whitespace
+COMMIT='61f3e4672e16e5742ea0ffde7d00c058e1d89e36'
+TAG='0.41'
+DATE='2019-01-02'
