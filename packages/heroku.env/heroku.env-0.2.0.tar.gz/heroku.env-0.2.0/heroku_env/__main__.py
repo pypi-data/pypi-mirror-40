2fe6775c15b82a8910272c97511757815acec9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:09 +0000

    Add package heroku.env/0.2.0/heroku.env-0.2.0.tar.gz

---
+++ packages/heroku.env/heroku.env-0.2.0.tar.gz/heroku.env-0.2.0/heroku_env/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import  # enable absolute imports of this module for Python2.x
+
+from .cli import main
+
+if __name__ == '__main__':
+    main()
