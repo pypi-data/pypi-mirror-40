8b546513a94d4ce80734c27a513ced39dc5a4a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:53 +0000

    Add package nesterZS/1.1.0/nesterZS-1.1.0.tar.gz

---
+++ packages/nesterZS/nesterZS-1.1.0.tar.gz/nesterZS-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'nesterZS',
+	version	= '1.1.0',
+	py_modules = ['nesterZS'],
+	author	= 'Zacsoft',
+	author_email = 'zanistefano@libero.it',
+	url	= 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested list',
+     )
