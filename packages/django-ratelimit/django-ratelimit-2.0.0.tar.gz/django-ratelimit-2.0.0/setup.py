cf1f4ad21aa3b8fcfb2079b57ee382b731de0564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:43 +0000

    Add package django-ratelimit/2.0.0/django-ratelimit-2.0.0.tar.gz

---
+++ packages/django-ratelimit/django-ratelimit-2.0.0.tar.gz/django-ratelimit-2.0.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+from ratelimit import __version__
+
+
+setup(
+    name='django-ratelimit',
+    version=__version__,
+    description='Cache-based rate-limiting for Django.',
+    long_description=open('README.rst').read(),
+    author='James Socol',
+    author_email='me@jamessocol.com',
+    url='https://github.com/jsocol/django-ratelimit',
+    license='Apache Software License',
+    packages=find_packages(exclude=['test_settings']),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
