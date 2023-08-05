d2679e9b4fa324e487880d2982bcc260c5db80cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:25 +0000

    Add package iduoliao-ml/1.0.76/iduoliao_ml-1.0.76.tar.gz

---
+++ packages/iduoliao-ml/iduoliao_ml-1.0.76.tar.gz/iduoliao_ml-1.0.76/iduoliao_ml/data/dataV2.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def updateHistoryData(whiteAids=[], blackVids=[]):
+	subjectDict, worksDict = createWorksDict(aids, blackVids)
+
+def createWorksDict(whiteAids, blackVids):
+	subjectDict = {}
+	worksDict = {}
+	
