d0de0d8c461952a49f007628e74b80c2f41b000f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:50 +0000

    Add package cn2an/0.1.1/cn2an-0.1.1.tar.gz

---
+++ packages/cn2an/cn2an-0.1.1.tar.gz/cn2an-0.1.1/cn2an/__init__.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+from __future__ import absolute_import
+
+import cn2an.version as version
+import cn2an.cn2an as cn2an
+import cn2an.an2cn as an2cn
+
+
+__version__ = version.VERSION
+cn2an = cn2an.Cn2An().cn2an
+an2cn = an2cn.An2Cn().an2cn
---
+++ packages/cn2an/cn2an-0.1.1.tar.gz/cn2an-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+from __future__ import absolute_import
+
+from setuptools import setup
+
+from cn2an import version
+
+setup(
+    name='cn2an',
+    version=version.VERSION,
+    author='HaveTwoBrush',
+    author_email='kinggreenhall@gmail.com',
+    url='https://www.v2ai.cn/python/2018/12/30/PY-5.html',
+    packages=['cn2an'],
+    install_requires=open("./requirements.txt", "r").read().splitlines(),
+    description='Convert Chinese numerals and Arabic numerals.',
+    long_description=open("./README.md", "r").read(),
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'cn2an=cn2an:cn2an',
+            'an2cn=cn2an:an2cn'
+        ]
+    },
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
