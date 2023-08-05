95b155b04da0ee104f5d3b37c30c920ef59dd361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package aiolambda/0.2.16/aiolambda-0.2.16-py3-none-any.whl

---
+++ packages/aiolambda/aiolambda-0.2.16-py3-none-any.whl/aiolambda/functools.py
@@ -0,0 +1,44 @@
+import functools
+
+from inspect import iscoroutinefunction
+from toolz import curry
+from typing import Any, Callable, TypeVar
+
+from aiolambda.typing import Maybe
+
+T = TypeVar('T')
+
+
+def _iscoroutinefunction_or_partial(object):
+    if isinstance(object, functools.partial) or isinstance(object, curry):
+        object = object.func
+    return iscoroutinefunction(object)
+
+
+def compose(*funcs: Callable) -> Callable:
+    first, second, *rest = funcs
+    if rest:
+        second = compose(second, *rest)
+    if _iscoroutinefunction_or_partial(first) and _iscoroutinefunction_or_partial(second):
+        async def _async(*args, **kwargs):
+            return await second(await first(*args, **kwargs))
+        return _async
+    if _iscoroutinefunction_or_partial(second):
+        async def _async(*args, **kwargs):
+            return await second(first(*args, **kwargs))
+        return _async
+    if _iscoroutinefunction_or_partial(first):
+        async def _async(*args, **kwargs):
+            return second(await first(*args, **kwargs))
+        return _async
+
+    def _func(*args, **kwargs):
+        return second(first(*args, **kwargs))
+    return _func
+
+
+@curry
+def bind(f: Callable, x: Maybe[T]) -> Any:
+    if isinstance(x, Exception):
+        return x
+    return f(x)
