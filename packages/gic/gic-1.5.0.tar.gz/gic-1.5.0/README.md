9ae1cd87e4b54e5f22bfb81246cfe27372b70fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package gic/1.5.0/gic-1.5.0.tar.gz

---
+++ packages/gic/gic-1.5.0.tar.gz/gic-1.5.0/README.md
@@ -0,0 +1,23 @@
+# GIC 
+
+## An image compressor that compresses all images in the working directory.  
+
+
+### To execute:  
+Run `gic` in a shell.  
+#### To use in your python code:  
+A code that the **user** sets the compression level.
+```python
+import gic.main
+#change the working directory to whatever you want
+gic.main.run()
+```
+#### To be quiet:  
+A code that **you** set the compression level.
+```python
+import gic.main
+#change the working directory to whatever you want
+#set gic.main.image_quality to 1|2|3|4
+gic.main.compress()
+```
+
---
+++ packages/gic/gic-1.5.0.tar.gz/gic-1.5.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md") as f:
+    long_desc = f.read()
+
+
+setuptools.setup(
+        name="gic",
+        version="1.5.0",
+        description="Group Image Compressor (GIC)",
+        long_description=long_desc,
+        long_description_content_type="text/markdown",
+        url="https://github.com/poyynt",
+        author="Parsa Torbati",
+        author_email="parsa@programmer.net",
+        packages=setuptools.find_packages(),
+        python_requires=">=3.2",
+        install_requires=[
+            "opencv-python",
+            ],
+        entry_points={
+            "console_scripts": [
+                "gic=gic.main:run",
+                ],
+            },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License (GPL)",
+            "Operating System :: OS Independent",
+            ],
+        )
