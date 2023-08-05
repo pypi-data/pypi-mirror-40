59d743eba3bfee0f4c9fce84f12e6b6a9ba0534e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package falcon-oas/0.1.1/falcon-oas-0.1.1.tar.gz

---
+++ packages/falcon-oas/falcon-oas-0.1.1.tar.gz/falcon-oas-0.1.1/falcon_oas/oas/spec.py
@@ -0,0 +1,126 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from jsonschema import RefResolver
+from six import iteritems
+
+try:
+    from functools import lru_cache
+except ImportError:  # pragma: no cover
+    from functools32 import lru_cache
+from six.moves.urllib_parse import urlparse
+
+from .exceptions import UndocumentedRequest, UndocumentedMediaType
+
+DEFAULT_SERVER = {'url': '/'}
+
+
+def create_spec_from_dict(spec_dict, base_path=None):
+    return Spec(spec_dict, base_path=base_path)
+
+
+class Spec(object):
+    def __init__(self, spec_dict, base_path=None):
+        self.spec_dict = spec_dict
+        self.base_path = (
+            base_path if base_path is not None else get_base_path(spec_dict)
+        )
+        self._base_security = get_security(spec_dict)
+
+    def deref(self, schema):
+        resolver = self._create_resolver()
+        while '$ref' in schema:
+            _, schema = resolver.resolve(schema['$ref'])
+        return schema.copy()
+
+    @lru_cache(maxsize=None)
+    def get_operation(self, uri_template, method, media_type):
+        if not uri_template.startswith(self.base_path):
+            raise UndocumentedRequest()
+
+        path = uri_template[len(self.base_path) :]
+        try:
+            path_item = self.deref(self.spec_dict['paths'][path])
+            operation = path_item[method]
+        except KeyError:
+            raise UndocumentedRequest()
+
+        result = operation.copy()
+        result['parameters'] = list(
+            self._iter_parameters(path_item.copy(), result)
+        )
+        try:
+            result['requestBody'] = self._deref_request_body(
+                result, media_type
+            )
+        except KeyError:
+            pass
+        result['security'] = get_security(
+            result, base_security=self._base_security
+        )
+        return result
+
+    def get_security_schemes(self):
+        try:
+            security_schemes = self.spec_dict['components']['securitySchemes']
+        except KeyError:
+            return None
+        else:
+            return {
+                key: self.deref(security_scheme)
+                for key, security_scheme in iteritems(security_schemes)
+            }
+
+    def _iter_parameters(self, path_item, operation):
+        seen = set()
+        for spec_dict in (operation, path_item):
+            if 'parameters' not in spec_dict:
+                continue
+            for parameter_spec_dict in spec_dict['parameters']:
+                parameter_spec_dict = self.deref(parameter_spec_dict)
+                key = (parameter_spec_dict['in'], parameter_spec_dict['name'])
+                if key in seen:
+                    continue
+                seen.add(key)
+                try:
+                    schema = parameter_spec_dict['schema']
+                except KeyError:
+                    pass
+                else:
+                    parameter_spec_dict['schema'] = self.deref(schema)
+                yield parameter_spec_dict
+
+    def _deref_request_body(self, operation, media_type):
+        result = self.deref(operation['requestBody'])
+        try:
+            # TODO: Support media type range
+            media_type_spec_dict = result['content'][media_type]
+        except KeyError:
+            raise UndocumentedMediaType()
+        try:
+            schema = media_type_spec_dict['schema']
+        except KeyError:
+            pass
+        else:
+            media_type_spec_dict['schema'] = self.deref(schema)
+        return result
+
+    def _create_resolver(self):
+        return RefResolver.from_schema(self.spec_dict)
+
+
+def get_base_path(spec_dict):
+    try:
+        server = spec_dict['servers'][0]
+    except (KeyError, IndexError):
+        server = DEFAULT_SERVER
+    return urlparse(server['url']).path.rstrip('/')
+
+
+def get_security(spec_dict, base_security=None):
+    try:
+        return spec_dict['security']
+    except KeyError:
+        return base_security
---
+++ packages/falcon-oas/falcon-oas-0.1.1.tar.gz/falcon-oas-0.1.1/tests/middlewares/test_operation.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import falcon
+from falcon import testing
+
+from falcon_oas.middlewares.operation import OperationMiddleware
+from falcon_oas.oas.spec import create_spec_from_dict
+from tests.helpers import yaml_load_dedent
+
+
+def create_app(spec_dict):
+    spec = create_spec_from_dict(spec_dict)
+    app = falcon.API(middleware=[OperationMiddleware(spec)])
+    return app
+
+
+def test_undocumented_request(resource):
+    app = create_app({'paths': {}})
+    app.add_route('/undocumented', resource)
+
+    client = testing.TestClient(app)
+    client.simulate_get(path='/undocumented')
+
+    req = resource.captured_req
+    assert req.context['oas._operation'] is None
+
+
+def test_undocumented_media_type(resource):
+    spec_dict = yaml_load_dedent(
+        """\
+        paths:
+          /path:
+            get:
+              requestBody:
+                content:
+                  application/json: {}
+        """
+    )
+    app = create_app(spec_dict)
+    app.add_route('/path', resource)
+
+    client = testing.TestClient(app)
+    client.simulate_get(
+        path='/path', headers={'Content-Type': str('text/plain')}
+    )
+
+    req = resource.captured_req
+    assert req.context['oas._operation'] is None
+
+
+def test_documented_request(resource):
+    app = create_app({'paths': {'/path/{id}': {'post': {}}}})
+    app.add_route('/path/{id}', resource)
+
+    client = testing.TestClient(app)
+    client.simulate_post(
+        path='/path/2',
+        query_string=str('q=3&r=5&r=7'),
+        headers={
+            'Content-Type': str('application/json; charset=UTF-8'),
+            'Cookie': str('x=5'),
+            'X-Key': str('key'),
+        },
+        body='"foo"',
+    )
+
+    req = resource.captured_req
+    assert req.context['oas._operation'] is not None
+    assert req.context['oas._request'].parameters['query'] == {
+        'q': '3',
+        'r': ['5', '7'],
+    }
+    assert req.context['oas._request'].parameters['header']['X-Key'] == 'key'
+    assert req.context['oas._request'].parameters['path'] == {'id': '2'}
+    assert req.context['oas._request'].parameters['cookie'] == {'x': '5'}
+    assert req.context['oas._request'].media_type == 'application/json'
+    assert req.context['oas._request'].get_media() == 'foo'
