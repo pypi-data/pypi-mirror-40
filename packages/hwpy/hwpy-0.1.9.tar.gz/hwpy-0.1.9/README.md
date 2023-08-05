bec61e3c618463b400f0fd41470f93ff057fed7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:33 +0000

    Add package hwpy/0.1.9/hwpy-0.1.9.tar.gz

---
+++ packages/hwpy/hwpy-0.1.9.tar.gz/hwpy-0.1.9/README.md
@@ -0,0 +1,23 @@
+## install(python2.7 and python3.x)
+
+```
+pip install hwpy
+```
+
+## usage
+```
+# python -m hwpy
+```
+```
+from hwpy import info
+info.main()
+info.hwlist
+```
+```
+info.cpu
+info.net    
+info.disk
+info.part   
+info.host
+info.mem
+```
---
+++ packages/hwpy/hwpy-0.1.9.tar.gz/hwpy-0.1.9/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: hwpy
+# Author: Stars Liao
+# Mail: starsliaop@163.com
+# Created Time:  2018-12-24 19:17:34
+#############################################
+
+from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "hwpy",
+    version = "0.1.9",
+    keywords = ("pip", "hwpy","Hardware"),
+    description = "Show Linux Hardware Info",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    license = "MIT Licence",
+
+    url = "https://github.com/starsliao/hwpy",
+    author = "Stars Liao",
+    author_email = "starsliao@163.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["psutil"],
+    classifiers=[
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        ],
+)
