844ad4b182b760c698b7990525b2e2b3df9555d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:25 +0000

    Add package pypco/0.0.2/pypco-0.0.2.tar.gz

---
+++ packages/pypco/pypco-0.0.2.tar.gz/pypco-0.0.2/setup.py
@@ -0,0 +1,40 @@
+
+from setuptools import (
+    setup,
+    find_packages
+)
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pypco',
+    version='0.0.2',
+    description='A Python wrapper for the Planning Center Online API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/billdeitrick/pypco',
+    author='Bill Deitrick',
+    author_email='hello@billdeitrick.com',
+    python_requires='>=3.6.0',
+    license='MIT',
+    packages=find_packages(
+        exclude=[
+            'tests.*',
+            'tests'
+        ]
+    ),
+    install_requires=[
+        'requests'
+    ],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
