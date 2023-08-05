957f816fa01efd84718220239fadc54b5cf9833e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:04 +0000

    Add package XCaliburMethodReader/0.1.3/XCaliburMethodReader-0.1.3.tar.gz

---
+++ packages/XCaliburMethodReader/XCaliburMethodReader-0.1.3.tar.gz/XCaliburMethodReader-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='XCaliburMethodReader',
+    py_modules=['XCaliburMethodReader'],
+    version='0.1.3',
+    url='https://github.com/nickdelgrosso/XCaliburMethodReader',
+    license='GNU',
+    author='Nicholas A. Del Grosso',
+    author_email='delgrosso.nick@gmail.com',
+    description='A command-line program for mass spectrometry researchers that extracts and converts data from Thermo XCalibur Method (.meth) files.',
+    entry_points="""
+        [console_scripts]
+        XCaliburMethodReader=XCaliburMethodReader:main
+    """,
+    install_requires=['olefile', 'xmltodict'],
+)
