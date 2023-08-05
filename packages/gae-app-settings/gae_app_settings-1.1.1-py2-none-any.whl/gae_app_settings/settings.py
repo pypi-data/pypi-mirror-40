7d58052e09ac2e885f1a7895809ebcdae097a9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package gae-app-settings/1.1.1/gae_app_settings-1.1.1-py2-none-any.whl

---
+++ packages/gae-app-settings/gae_app_settings-1.1.1-py2-none-any.whl/gae_app_settings/settings.py
@@ -0,0 +1,29 @@
+from google.appengine.ext import ndb
+
+
+UNSET = '<NOT SET>'
+UNSET_MSG_FMT = 'attempt to retrieve setting {0} failed'
+
+
+class AppSetting(ndb.Model):
+
+    key = ndb.StringProperty(required=True)
+    value = ndb.TextProperty(required=True)
+
+    @staticmethod
+    def get(key, default=None, raise_exception=True):
+        setting = AppSetting.query(AppSetting.key == key).get()
+        if not setting:
+            setting = AppSetting(key=key, value=UNSET)
+            setting.put()
+        if setting.value == UNSET:
+            if default is not None:
+                return str(default)
+            if raise_exception:
+                raise KeyError(UNSET_MSG_FMT.format(key))
+            return None
+        return setting.value
+
+    @classmethod
+    def _get_kind(cls):
+        return '! GAE-APP-SETTINGS !'
