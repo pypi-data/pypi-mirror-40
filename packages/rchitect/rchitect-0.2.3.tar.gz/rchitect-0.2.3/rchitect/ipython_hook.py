d4665167380684b1b7fbd8f633925ac6300d1400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:38 +0000

    Add package rchitect/0.2.3/rchitect-0.2.3.tar.gz

---
+++ packages/rchitect/rchitect-0.2.3.tar.gz/rchitect-0.2.3/rchitect/ipython_hook.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+import sys
+from .interface import process_events
+
+
+def register_hook():
+
+    shell = None
+    try:
+        if hasattr(sys, "ps1"):
+            import IPython
+            if IPython.__version__ >= "5":
+                shell = IPython.get_ipython()
+    except ImportError:
+        pass
+
+    if shell:
+        def inputhook(context):
+            while True:
+                if context.input_is_ready():
+                    break
+                process_events()
+
+        IPython.terminal.pt_inputhooks.register("r", inputhook)
+
+
+def enable_gui():
+    try:
+        if hasattr(sys, "ps1"):
+            import IPython
+            shell = IPython.get_ipython()
+            if shell:
+                shell.enable_gui('r')
+    except ImportError:
+        pass
