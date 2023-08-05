ae7f5af610a0b19974e05b116ceeedf6a8c0b736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package firestoreMQ/1.0/firestoreMQ-1.0.tar.gz

---
+++ packages/firestoreMQ/firestoreMQ-1.0.tar.gz/firestoreMQ-1.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='firestoreMQ',
+      version='1.0',
+      description='Firestore MQ python library',
+      author='Alex Robinson',
+      author_email='alex@arrx.uk',
+      packages=find_packages(),
+      long_descriptio=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
