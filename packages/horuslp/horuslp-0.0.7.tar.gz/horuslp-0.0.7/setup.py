49242ec1ae7bccb556ba28e00d1ec276cad12b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:50 +0000

    Add package horuslp/0.0.7/horuslp-0.0.7.tar.gz

---
+++ packages/horuslp/horuslp-0.0.7.tar.gz/horuslp-0.0.7/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='horuslp',
+    version='0.0.7',
+    packages=['horuslp', 'horuslp.core'],
+    url='https://github.com/shanglun/horuslp',
+    license='LGPL',
+    author='shanglunwang',
+    author_email='sean.shanglun.wang@gmail.com',
+    description='A library to boost productivty of optimization algorithm developers '
+)
