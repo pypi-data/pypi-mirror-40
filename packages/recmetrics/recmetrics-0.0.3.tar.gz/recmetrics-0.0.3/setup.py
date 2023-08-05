8aef846127880e20e479bb656085d33e61f321ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:36 +0000

    Add package recmetrics/0.0.3/recmetrics-0.0.3.tar.gz

---
+++ packages/recmetrics/recmetrics-0.0.3.tar.gz/recmetrics-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='recmetrics',
+    url='https://github.com/statisticianinstilettos/recommender_metrics',
+    author='Claire Longo',
+    author_email='longoclaire@gmail.com',
+    packages=['recmetrics'],
+    install_requires=['numpy',
+        'pandas',
+        'scikit-learn',
+        'seaborn'],
+    version='0.0.3',
+    description='Evaluation metrics for recommender systems',
+)
