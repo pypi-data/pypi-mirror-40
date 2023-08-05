3035eb9b76653062fcda8454b6292568051dbecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:07 +0000

    Add package firstProject/1.1.0/firstProject-1.1.0.tar.gz

---
+++ packages/firstProject/firstProject-1.1.0.tar.gz/firstProject-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+		name	=	'firstProject',
+		version	=	'1.1.0',
+		py_modules	=	['firstProject'],
+		author	=	'wallace.wang',
+		author_email	=	'wangwen5366@163.com',
+		url	=	'http://www.headfirstlabs.com',
+		description	=	'A simple printer of nested lists',
+	)
