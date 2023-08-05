ed723bacd7a296460dbb61fef8f2824b5263c5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:47 +0000

    Add package inquant/1.2.4/inquant-1.2.4.tar.gz

---
+++ packages/inquant/inquant-1.2.4.tar.gz/inquant-1.2.4/inquant/framework/IMarketApi.py
@@ -0,0 +1,36 @@
+﻿# -*- coding: utf-8 -*-
+class IMarketApi(object):
+    """行情接口"""
+
+    def __init__(self,tickCallback,barCallback):
+        """构造函数"""
+        pass
+
+    def Init(self,strategyID):
+        """初始化"""
+        pass
+
+    def GetStatus(self):
+        """状态 -1:未初始化 0:正常 1:已断开"""
+        pass
+
+    def Subscribe(self,subInfos, isClear, isPlayback):
+        """行情订阅"""
+        pass
+
+    def Unsubscribe(self,subInfos):
+        """取消订阅"""
+        pass
+
+    def GetLastTick(self,symbol, exchange, count):
+        """获取最近几笔Tick数据"""
+        pass
+
+    def GetLastBar(self,symbol, exchange, barType, count):
+        """获取最近几笔Bar数据"""
+        pass
+
+    def GetHisBar(self,symbol, exchange, barType, startTime, count):
+        """获取历史K线数据"""
+        pass
+
