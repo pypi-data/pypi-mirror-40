417c7bbb3ed282cb5ea08b84fe353109d62c9673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:17:21 +0000

    Add package vkconnections/0.0.8/vkconnections-0.0.8.tar.gz

---
+++ packages/vkconnections/vkconnections-0.0.8.tar.gz/vkconnections-0.0.8/vkconnections/SecondLevel.py
@@ -0,0 +1,63 @@
+from vkconnections import VkAPI as API
+import threading
+
+
+class SecondDivision:
+    def __init__(self, listOfVkScript, token):
+        self.listOfVkScript = listOfVkScript
+        self.token = token
+        self.data = []
+
+    def execute(self):
+        listOutput = []
+        for item in self.listOfVkScript:
+            temp = API.getExecute(item, self.token)
+            listOutput.append(temp)
+        self.data = listOutput
+
+    def getData(self):
+        return self.data
+
+
+# input: list of users' ids
+# output: list of lists of friends
+def getListOfFriends(allTheUsers, twoTokens, threadName):
+    firstList, secondList = getHalfOfVkScript(allTheUsers)
+    firstList, secondList = makeSecondDivision(firstList, secondList, twoTokens, threadName)
+    return getFullList(firstList, secondList)
+
+
+def getHalfOfVkScript(allTheUsers):
+    listOfVkScript = API.getVkScriptForExecute(allTheUsers)
+
+    middle = len(listOfVkScript) // 2
+    firstList = listOfVkScript[:middle]
+    secondList = listOfVkScript[middle:]
+    return firstList, secondList
+
+
+def makeSecondDivision(firstList, secondList, twoTokens, threadName):
+    firstHalfSecondDivision = SecondDivision(firstList, twoTokens[1])
+    secondHalfSecondDivision = SecondDivision(secondList, twoTokens[2])
+
+    firstList = threading.Thread(target=firstHalfSecondDivision.execute, name=threadName + 'Second Division - 1')
+    secondList = threading.Thread(target=secondHalfSecondDivision.execute, name=threadName + 'Second Division - 2')
+
+    firstList.start()
+    secondList.start()
+
+    firstList.join()
+    secondList.join()
+
+    firstList = firstHalfSecondDivision.getData()
+    secondList = secondHalfSecondDivision.getData()
+    return firstList, secondList
+
+
+def getFullList(firstList, secondList):
+    listOutput = firstList + secondList
+    returnList = []
+    for item in listOutput:
+        for iterator in item:
+            returnList.append(iterator)
+    return returnList
---
+++ packages/vkconnections/vkconnections-0.0.8.tar.gz/vkconnections-0.0.8/vkconnections/VkAPI.py
@@ -0,0 +1,89 @@
+import requests
+import math
+import time
+from json import loads
+from vkconnections import APIKeys as c
+
+keys = {}
+
+def setKeys(vkKeys):
+    global keys
+    keys = vkKeys
+
+
+def getUsers(usersUrl):
+    global keys
+    temp = None
+    count = 0
+    while temp is None:
+        response = requests.get(c.usersGet.format(str(usersUrl), keys[0])).text
+        response = loads(response)
+        temp = response.get('response')
+        if count > 1:
+            time.sleep(1)
+        count += 1
+        if count == 50:
+            return None
+    return response.get('response')
+
+
+def getFriends(id):
+    temp = None
+    count = 0
+    while temp is None:
+        response = requests.get(c.friendsGet.format(str(id), keys[0])).text
+        response = loads(response)
+        temp = response.get('response')
+        if count > 1:
+            time.sleep(1)
+        count += 1
+        if count == 50:
+            return None
+    return response.get('response').get('items')
+
+
+def getExecute(code, token):
+    temp = None
+    count = 0
+    while temp is None:
+        myString = c.executeGet.format(code, token)
+        execute = requests.get(myString).text
+        execute = loads(execute)
+        temp = execute.get('response')
+        if count > 1:
+            time.sleep(1)
+        count += 1
+        if count == 50:
+            return None
+    return execute.get('response')
+
+
+def getVkScriptForExecute(allTheFriends):
+    lenFriends = len(allTheFriends)
+    requestsCount = lenFriends // 25 + math.ceil((lenFriends - lenFriends // 25 * 25) / 25)
+    outputString = []
+    startIndex = 0
+    endIndex = 25
+    for number in range(0, requestsCount):
+        requestString = 'return [ '
+        for item in range(startIndex, endIndex):
+            if lenFriends > item:
+                requestString += 'API.friends.get({"user_id": ' + str(allTheFriends[item]) + ' }), '
+        requestString = requestString[0:-2]
+        requestString += ' ];'
+        outputString.append(requestString)
+        startIndex += 25
+        endIndex += 25
+    return outputString
+
+def getVkScriptForUsersExecute(allTheUsers):
+    requestString = 'return [ '
+    for index in range(len(allTheUsers)):
+        stringOfUsers = ""
+        for item in allTheUsers[index]:
+            stringOfUsers += str(item) + ','
+        stringOfUsers = stringOfUsers[0:-1]
+        requestString += 'API.users.get({"user_ids": "' + stringOfUsers + '", "fields": "photo_100" }), '
+    requestString = requestString[0:-1]
+    requestString += ' ];'
+    return requestString
