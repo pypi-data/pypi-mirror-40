fd81e76eb8f5dff4a9c168f744faadc4d149b3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:57 +0000

    Add package dailify/0.0.0.3/dailify-0.0.0.3-py3-none-any.whl

---
+++ packages/dailify/dailify-0.0.0.3-py3-none-any.whl/dailify/cli.py
@@ -0,0 +1,27 @@
+
+
+
+import argparse, random, os, pyperclip
+
+
+
+
+
+def main():
+  parser = argparse.ArgumentParser(description="Ghent Theft Module")
+  parser.add_argument("-d", "--daily", type=str, dest="daily")
+  
+  uid = " ".join([str(random.randint(0, 9)) for x in range(6)])
+  
+  
+  args = parser.parse_args()
+  name = args.daily.upper()
+  spaced = " ".join(name)
+  temp = " ".join([uid, "-", spaced])
+  pyperclip.copy(temp)
+  print(temp)
+  #print(os.getcwd())
+
+
+if __name__ == "__main__":
+  main()
