de97cd92cf6abb727a534f8ee8ff6809d3d92eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:17 +0000

    Add package mekpie/0.0.6/mekpie-0.0.6-py3-none-any.whl

---
+++ packages/mekpie/mekpie-0.0.6-py3-none-any.whl/mekpie/runner.py
@@ -0,0 +1,27 @@
+from sys        import stdout, stderr
+from subprocess import run, PIPE
+
+import mekpie.messages as messages
+
+from .util import panic, log
+
+def lrun(args, quiet=False, error=True):
+    log('Running...\n' + serialize_command(args))
+    try:
+        if quiet:
+            if run(args, stdout=PIPE, stderr=PIPE).returncode != 0:
+                raise OSError
+        else:
+            if run(args).returncode != 0:
+                raise OSError
+        return True
+    except KeyboardInterrupt:
+        exit(1)
+    except OSError:
+        if error:
+            panic(messages.failed_program_call.format(serialize_command(args)))
+        else:
+            False
+
+def serialize_command(args):
+    return '$ ' + ' '.join(args)
