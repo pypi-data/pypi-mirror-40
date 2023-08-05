51e3d9cdc0a1c074c753c925bfb25bc58c82215a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:07 +0000

    Add package blink-celery/0.1.0/blink-celery-0.1.0.tar.gz

---
+++ packages/blink-celery/blink-celery-0.1.0.tar.gz/blink-celery-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+version = '0.1.0'
+setuptools.setup(
+    name='blink-celery',
+    version=version,
+    author='Daniel Munoz',
+    author_email='dmunozg@gmail.com',
+    description='Library to create/execute celery tasks for blink cameras',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'blink-cameras',
+        'celery'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent'])
