661bce8ee24edffa7f573655f220e94e93eb14bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:24 +0000

    Add package hrpc/1.0.6/hrpc-1.0.6.tar.gz

---
+++ packages/hrpc/hrpc-1.0.6.tar.gz/hrpc-1.0.6/setup.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename='requirements.txt'):
+    """ load requirements from a pip requirements file. (replacing from pip.req import parse_requirements)"""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+setup(
+    name='hrpc',
+    version='1.0.6',
+    keywords="hrpc rpc",
+    description='A common interface based RPC framework',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=parse_requirements(),
+    license='Apache License 2.0',
+    url='https://github.com/AirtestProject/hrpc',
+)
