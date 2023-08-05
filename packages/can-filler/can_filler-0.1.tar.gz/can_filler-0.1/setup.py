36f6fb9040cec9641b63685ec1b8c05d703dee08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:05 +0000

    Add package can-filler/0.1/can_filler-0.1.tar.gz

---
+++ packages/can-filler/can_filler-0.1.tar.gz/can_filler-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='can_filler',
+      version='0.1',
+      description='Beer Can Filler Program',
+      url='https://github.com/senoird/can_filler',
+      author='Moore Brewing',
+      author_email='senoird@gmail.com',
+      license='MIT',
+      packages=['can_filler'],
+      zip_safe=False)
