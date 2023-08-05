e39a27f3b4fee1c73e8da371dd309b7bb2fb8929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package lucky-choice-jinli-v1/0.0.3/lucky_choice_jinli_v1-0.0.3-py3-none-any.whl

---
+++ packages/lucky-choice-jinli-v1/lucky_choice_jinli_v1-0.0.3-py3-none-any.whl/lucky_choice_jinli/__init__.py
@@ -0,0 +1,2 @@
+# encoding : utf-8
+name = 'lucky_choice_jinli'
---
+++ packages/lucky-choice-jinli-v1/lucky_choice_jinli_v1-0.0.3-py3-none-any.whl/lucky_choice_jinli/jinli.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import random
+from hashlib import md5
+
+__all__ = ['Jinli']
+
+
+class Jinli(object):
+    def __init__(self, Jinli_name, choiceList):
+        self.jinli_name = Jinli_name
+        self.jinli_choice = choiceList
+
+    @property
+    def choice(self):
+        md_value = md5()
+        md_value.update(self.jinli_name.encode("utf-8"))
+        random.seed(md_value.hexdigest())
+        return random.choice(self.jinli_choice)
+
+
+
+
+
+
+
+
