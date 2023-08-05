d8e1c2160cca54a6c53091e722206484f6987df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:59 +0000

    Add package flask-deploy/0.2.2/flask-deploy-0.2.2.tar.gz

---
+++ packages/flask-deploy/flask-deploy-0.2.2.tar.gz/flask-deploy-0.2.2/flaskdeploy/scripts/deploy.py
@@ -0,0 +1,69 @@
+from ..config import *
+from ..validation import *
+from ..utils import *
+
+import subprocess
+import getpass
+import os
+
+import click
+
+@click.command(context_settings=dict(
+    allow_extra_args=True
+))
+@click.option('--email', prompt='Your email', help='Email,Apply ssl certification,CloudFlare.',
+              callback=validate_email)
+@click.option('--key', prompt='Your secret key', help='Secret Key,Apply ssl certification,CloudFlare.')
+@click.option('--domain')
+@click.pass_context
+def miss_tmp(ctx,email, key,domain):
+    ssl_file_gen(DOMAIN, USR, CUR_LOC, email, key)
+    raise JumpOutFuckingClick
+
+
+@click.command()
+@click.option('--domain', prompt='Your domain', help='The domain to be configured.',
+              callback=validate_domain
+              )
+@click.option('--email', help='Email,Apply ssl certification,CloudFlare.',
+              callback=validate_email
+              )
+@click.option('--key', help='Key,Apply ssl certification,CloudFlare.')
+@click.option('--docker', help='Confirm having database or not.')
+@click.pass_context
+def cli(ctx, domain, email, key,docker):
+    """Deploy the flask app right now."""
+    global DOMAIN, USR, CUR_LOC
+    usr = getpass.getuser()
+    loc = os.path.join(os.getcwd(), domain)
+    DOMAIN, USR, CUR_LOC = domain, usr, loc
+
+    if not os.path.exists(CUR_LOC):
+        try:
+            os.makedirs(CUR_LOC)
+        except:
+            if click.confirm("You have no privilege of current location Would you like to own it?"):
+                subprocess.call(['sudo', 'chown', '-R', usr+":"+usr, './'])
+                os.makedirs(loc)
+            else:
+                click.echo("You have no previlege!!!")
+                return
+
+    uwsgi_file_gen(DOMAIN, USR, CUR_LOC)
+    nginx_file_gen(DOMAIN, USR, CUR_LOC)
+    service_file_gen(DOMAIN, USR, CUR_LOC)
+    
+    if not docker:
+        if not click.confirm('Do you have database already?'):
+            docker_file_gen(DOMAIN, USR, CUR_LOC)
+    if not email or not key:
+        if not click.confirm('Do you have SSL certification?'):
+            try:
+                miss_tmp()
+            except JumpOutFuckingClick:
+                pass
+    else:
+        ssl_file_gen(DOMAIN, USR, CUR_LOC, email, key)
+
+    script_files_gen(DOMAIN, USR, CUR_LOC)
+    script_files_run(DOMAIN, USR, CUR_LOC)
---
+++ packages/flask-deploy/flask-deploy-0.2.2.tar.gz/flask-deploy-0.2.2/setup.py
@@ -0,0 +1,44 @@
+
+import setuptools
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='flask-deploy',
+    version='0.2.2',      
+    description='A quick deploy script for productive flask app',
+    url='https://github.com/Mon-ius/flask-deploy',
+    author='Mon-ius',
+    author_email='m1105930979@gmail.com',
+
+    packages=setuptools.find_packages(),
+    package_data={
+        'flaskdeploy': ['data/*']
+    },
+    license='MIT',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    entry_points={
+    'console_scripts': [
+        'flask-deploy =flaskdeploy.scripts.core:cli',
+        'fd = flaskdeploy.scripts.core:cli',
+    ],
+    },
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    platforms='linux',
+)
