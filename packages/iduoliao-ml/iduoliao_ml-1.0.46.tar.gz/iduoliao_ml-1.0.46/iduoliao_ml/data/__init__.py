1814efbf8456307c1be4c2b9caba3efc74e52d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:14 +0000

    Add package iduoliao-ml/1.0.46/iduoliao_ml-1.0.46.tar.gz

---
+++ packages/iduoliao-ml/iduoliao_ml-1.0.46.tar.gz/iduoliao_ml-1.0.46/iduoliao_ml/data/__init__.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from worksPersonas import WorksPersonas, DAU
+from data import *
+
+class Data(object):
+
+	def __init__(self):
+		self.dau = DAU()
+		self.worksPersonas = WorksPersonas()
+
+	def update(self):
+		updateNewestActions(self.dau, self.worksPersonas)
+
+	def save(self):
+		self.dau.saveData()
+		self.worksPersonas.saveData()
+
+	#input 需要统计的主体列表,不传则统计全部
+	#return 主体列表、作品列表
+	def getSubWorksList(self, aids=[]):
+		return createWorksArgsList(self.worksPersonas, aids)
+
+	#return 当前的日活系数
+	def getDauFactor(self):
+		return self.dau.getDauFactor()
+
+	#return 当天的新作品曝光数上限
+	def getNewShowMax(self):
+		return self.dau.getNewShowMax()
+
+	#return 当前统计的最后时间
+	def getFinalTime(self):
+		return self.worksPersonas.lastPersonasTime
+
+
+
+
