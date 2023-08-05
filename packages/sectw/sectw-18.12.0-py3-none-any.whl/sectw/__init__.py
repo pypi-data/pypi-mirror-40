e897d856d648409c33a4182844aa0d57e3663310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:12 +0000

    Add package sectw/18.12.0/sectw-18.12.0-py3-none-any.whl

---
+++ packages/sectw/sectw-18.12.0-py3-none-any.whl/sectw/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from os.path import dirname, join
+from .database.config import setup_session
+from .util import Directory
+
+__version__ = '18.12.0'
+
+# paths
+_package_root = dirname(__file__)
+_csv_version = '2018-08-14'
+_csv_path = join(_package_root, '{}.csv'.format(_csv_version))
+
+# create a instance
+_dir = Directory(_csv_path)
+
+# expose function
+find = _dir.find
+find_complex = _dir.find_complex
+load_db = _dir.load_db
