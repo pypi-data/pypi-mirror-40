11edbfc9591501d10037c7c21816126fe7ed4439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:00 +0000

    Add package ipip-ipdb/1.2.1/ipip-ipdb-1.2.1.tar.gz

---
+++ packages/ipip-ipdb/ipip-ipdb-1.2.1.tar.gz/ipip-ipdb-1.2.1/ipdb/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: ©2018 by IPIP.net
+"""
+
+from .city import City
+from .district import District
+from .idc import IDC
+from .base_station import BaseStation
