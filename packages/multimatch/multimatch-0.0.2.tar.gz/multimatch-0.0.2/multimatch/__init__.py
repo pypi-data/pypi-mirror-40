ee5840fba62bdb681b1f6b3c6f51c34fe73edda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:41 +0000

    Add package multimatch/0.0.2/multimatch-0.0.2.tar.gz

---
+++ packages/multimatch/multimatch-0.0.2.tar.gz/multimatch-0.0.2/multimatch/__init__.py
@@ -0,0 +1,14 @@
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See LICENSE file distributed along with the multimatch package for the
+#   license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
+__version__ = '0.0.2'
+
+#from __future__ import absolute_import
+from .multimatch import docomparison
+from .multimatch_forrest import docomparison_forrest
