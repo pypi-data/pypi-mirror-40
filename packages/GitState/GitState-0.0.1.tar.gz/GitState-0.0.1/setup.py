1e599a9508bfe0a246298b536de59549befa48cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:36 +0000

    Add package GitState/0.0.1/GitState-0.0.1.tar.gz

---
+++ packages/GitState/GitState-0.0.1.tar.gz/GitState-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='GitState',
+    version='0.0.1',
+    packages=['gitstate',],
+    license='Apache',
+    long_description='Git state management'
+)
