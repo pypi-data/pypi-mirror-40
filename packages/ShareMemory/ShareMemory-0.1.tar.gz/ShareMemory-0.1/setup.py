bd76328ed6ab8cf8c89e5767aff6f2c0f8620b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package ShareMemory/0.1/ShareMemory-0.1.tar.gz

---
+++ packages/ShareMemory/ShareMemory-0.1.tar.gz/ShareMemory-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(name = 'ShareMemory',
+      version = '0.1' ,
+      descroption = 'The light weight share memory for inter process communication',
+      url = 'https://github.com/squintjet811/ShareMemory',
+      author = 'squintjet811',
+      author_email = 'squintjet811@gmail.com',
+      license = 'MIT',
+      packages = find_packages("src"),
+      install_requires = ['numpy == 1.15.4'],
+      package_data = {
+           'doc' :['*.txt'] ,
+           'memorymapfile' : ['*.txt'] ,
+      },
+      #include_package_data = True,
+      zip_safe = False)
+
+
