7cc9296b51e75fcb084a462a8695e102e541bf36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package confedit/0.1.1/confedit-0.1.1.tar.gz

---
+++ packages/confedit/confedit-0.1.1.tar.gz/confedit-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['confedit']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0', 'pick>=0.6.4,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['conf = confedit:conf']}
+
+setup_kwargs = {
+    'name': 'confedit',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Dominic Looser',
+    'author_email': 'dominic.looser@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
