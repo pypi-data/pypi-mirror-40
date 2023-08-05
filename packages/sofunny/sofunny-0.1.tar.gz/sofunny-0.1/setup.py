c61bf508e472016deb53ffcb22853bafa094d659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:11 +0000

    Add package sofunny/0.1/sofunny-0.1.tar.gz

---
+++ packages/sofunny/sofunny-0.1.tar.gz/sofunny-0.1/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="sofunny",
+    version="0.1",
+    description="The funniest joke in the world",
+    url="http://github.com/storborg/funniest",
+    author="Flying Circus",
+    author_email="flyingcircus@example.com",
+    license="MIT",
+    packages=["sofunny"],
+    zip_safe=False
+    )
+
+
+
+# crea el virtual env
+# crea el repo funniest_algo
