44c7eb72e97afc4ded3948c7ff69578d32b7c95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:21 +0000

    Add package dailify/0.0.0.5.17/dailify-0.0.0.5.17.tar.gz

---
+++ packages/dailify/dailify-0.0.0.5.17.tar.gz/dailify-0.0.0.5.17/dailify/file_handler.py
@@ -0,0 +1,15 @@
+import bpy, os, sys
+
+argv = sys.argv
+argv = argv[argv.index("--") + 1:] # get all args after "--"
+folder = argv[0]
+
+print(folder)
+
+stripped = folder.split('/')
+full = stripped[-1]
+split = full.split(' ')
+name = split[1]
+out = "{}/{}.blend".format(folder, name)
+
+bpy.ops.wm.save_mainfile(filepath=out)
