f729caef108793c9c34db723fb57554cc6542b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:13 +0000

    Add package planingfsi/0.1.1/planingfsi-0.1.1-py3-none-any.whl

---
+++ packages/planingfsi/planingfsi-0.1.1-py3-none-any.whl/planingfsi/__init__.py
@@ -0,0 +1,15 @@
+"""PlaningFSI is a Python package for solving 2d FSI problems.
+
+PlaningFSI solves the general FSI problem of 2-D planing surfaces on a free
+surface, where each surface can be rigid or flexible. The substructures are
+part of a rigid body, which can be free in sinkage and trim.
+
+The fluid solver is based on linearized potential-flow assumptions,
+and the structural solver considers a large-deformation simple beam element.
+
+"""
+import logging
+from .__version__ import __version__
+
+logger = logging.getLogger("planingfsi")
+logger.setLevel(logging.DEBUG)
