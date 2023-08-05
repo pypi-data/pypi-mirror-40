92f4b60f2bb3118b103dba21aedf15e060059f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:34 +0000

    Add package nacf/0.1.1/nacf-0.1.1-py3-none-any.whl

---
+++ packages/nacf/nacf-0.1.1-py3-none-any.whl/nacf/__init__.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+r"""
+Life's pathetic, have fun ("▔□▔)/hi~♡ Nasy.
+
+Excited without bugs::
+
+    |             *         *
+    |                  .                .
+    |           .
+    |     *                      ,
+    |                   .
+    |
+    |                               *
+    |          |\___/|
+    |          )    -(             .              ·
+    |         =\ -   /=
+    |           )===(       *
+    |          /   - \
+    |          |-    |
+    |         /   -   \     0.|.0
+    |  NASY___\__( (__/_____(\=/)__+1s____________
+    |  ______|____) )______|______|______|______|_
+    |  ___|______( (____|______|______|______|____
+    |  ______|____\_|______|______|______|______|_
+    |  ___|______|______|______|______|______|____
+    |  ______|______|______|______|______|______|_
+    |  ___|______|______|______|______|______|____
+
+author   : Nasy https://nasy.moe
+date     : Dec 23, 2018
+email    : Nasy <nasyxx+python@gmail.com>
+filename : __init__.py
+project  : nacf
+license  : GPL-3.0+
+
+There are more things in heaven and earth, Horatio, than are dreamt.
+ --  From "Hamlet"
+"""
+# Local Packages
+from .models import css, get, post, text, urls, xpath, parallel
+
+__version__ = "0.1.1"
+
+
+assert css
+assert get
+assert post
+assert text
+assert urls
+assert xpath
+assert parallel
