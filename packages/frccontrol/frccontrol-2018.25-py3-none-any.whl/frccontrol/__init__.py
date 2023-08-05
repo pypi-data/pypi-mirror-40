069e0eaf169c7835030882d17d117332ef5e1448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:38 +0000

    Add package frccontrol/2018.25/frccontrol-2018.25-py3-none-any.whl

---
+++ packages/frccontrol/frccontrol-2018.25-py3-none-any.whl/frccontrol/__init__.py
@@ -0,0 +1,9 @@
+"""The WPILib state-space control library."""
+
+from frccontrol import models
+from frccontrol.ctrlutil import *
+from frccontrol.dlqr import *
+from frccontrol.kalmd import *
+from frccontrol.profiles import *
+from frccontrol.system import *
+from frccontrol.system_writer import *
