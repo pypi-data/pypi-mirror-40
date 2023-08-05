5c0f79e4124ad9c58aaa7eaa75384406bdcfd181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:48 +0000

    Add package pymidi/0.4.0/pymidi-0.4.0.tar.gz

---
+++ packages/pymidi/pymidi-0.4.0.tar.gz/pymidi-0.4.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='pymidi',
+    version='0.4.0',
+    license='MIT',
+    url='https://github.com/mik3y/pymidi',
+    author='mike wakerly',
+    author_email='opensource@hoho.com',
+    description='Python RTP-MIDI / AppleMIDI driver',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'construct >= 2.9',
+        'future >= 0.17.0',
+        'six >= 1.10.0',
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'flake8',
+    ],
+)
