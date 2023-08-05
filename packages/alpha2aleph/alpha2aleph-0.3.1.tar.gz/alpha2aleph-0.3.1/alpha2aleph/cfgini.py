2a049d30a632be7ef5f55157e1a38114107fce77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:10 +0000

    Add package alpha2aleph/0.3.1/alpha2aleph-0.3.1.tar.gz

---
+++ packages/alpha2aleph/alpha2aleph-0.3.1.tar.gz/alpha2aleph-0.3.1/alpha2aleph/cfgini.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################################################################
+#    alpha2aleph Copyright (C) 2018 Suizokukan
+#    Contact: suizokukan _A.T._ orange dot fr
+#
+#    This file is part of alpha2aleph.
+#    alpha2aleph is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    alpha2aleph is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with alpha2aleph.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+"""
+        alpha2aleph by suizokukan (suizokukan AT orange DOT fr)
+
+        A (Python3/GPLv3/Linux/CLI) project, using no additional
+        modules than the ones installed with Python3.
+
+        alphabetic text → translitteration → Hebrew text
+
+        see README.md for more documentation.
+        ________________________________________________________________________
+
+        cfgini.py : read the configuration file.
+"""
+import os.path
+import configparser
+import alpha2aleph.glob
+from .regex import get_rtlreader_regex
+from .utils import normpath
+
+
+def read_cfg_file(filename):
+    """
+        read_cfg_file()
+
+        Read the config file <filename>.
+
+        PARAMETER : (str)filename
+
+        RETURNED VALUE : (bool_success, errors, configparser.ConfigParser object)
+    """
+    if not os.path.exists(filename):
+        return (False,
+                ["Where is config file '{0}', namely '{1}' ?".format(filename,
+                                                                     normpath(filename))],
+                None)
+
+    success = True
+    errors = []
+    cfgini = configparser.ConfigParser()
+
+    try:
+        cfgini.read(filename)
+    except configparser.DuplicateOptionError as err:
+        success = False
+        errors.append("Ill-formed config file '{0}' : duplicate key {1}".format(filename, err))
+
+    if success:
+        try:
+            # let's check the presence of some values :
+            _ = cfgini["output.console"]
+            _ = cfgini["output.console"]["invert_rtltext"]
+            _ = cfgini["output.console"]["rtl symbols"]
+            _ = cfgini["output.console"]["use fribidi"]
+
+            _ = cfgini["output.html"]
+            _ = cfgini["output.html"]["body"]
+            _ = cfgini["output.html"]["rtltext"]
+
+            _ = cfgini["pipeline.trace"]
+            _ = cfgini["pipeline.trace"]["yes"]
+            _ = cfgini["pipeline.trace"]["no"]
+
+            _ = cfgini["pipeline.use FB1D-FB4F chars"]
+
+            _ = cfgini["pipeline.improve rtlalphatext"]
+            _ = cfgini["pipeline.improve rtlalphatext"]["final kaf"]
+            _ = cfgini["pipeline.improve rtlalphatext"]["alef + holam > alef + point_on_right"]
+            _ = cfgini["pipeline.improve rtlalphatext"]["ḥe + holam + shin > ḥe + shin"]
+
+        except KeyError as err:
+            success = False
+            errors.append("Ill-formed config file '{0}' : missing key {1}".format(filename, err))
+
+    if success:
+        alpha2aleph.glob.RTL_SYMBOLS = (cfgini["inputdata.format"]["RTL_SYMBOL_START"],
+                                        cfgini["inputdata.format"]["RTL_SYMBOL_END"])
+        alpha2aleph.glob.RTLREADER_REGEX = get_rtlreader_regex()
+
+    return success, errors, cfgini
+
+
+# should be initialized by read_cfg_file()
+CFGINI = None
---
+++ packages/alpha2aleph/alpha2aleph-0.3.1.tar.gz/alpha2aleph-0.3.1/alpha2aleph/cmdline.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################################################################
+#    alpha2aleph Copyright (C) 2018 Suizokukan
+#    Contact: suizokukan _A.T._ orange dot fr
+#
+#    This file is part of alpha2aleph.
+#    alpha2aleph is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    alpha2aleph is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with alpha2aleph.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+"""
+        alpha2aleph by suizokukan (suizokukan AT orange DOT fr)
+
+        A (Python3/GPLv3/Linux/CLI) project, using no additional
+        modules than the ones installed with Python3.
+
+        alphabetic text → translitteration → Hebrew text
+
+        see README.md for more documentation.
+        ________________________________________________________________________
+
+        cmdline.py : read_command_line_arguments() function
+"""
+import argparse
+
+from .glob import __projectname__, __version__, \
+                  __license__, __author__, __email__
+
+
+def read_command_line_arguments():
+    """
+        read_command_line_arguments()
+        ________________________________________________________________________
+
+        Read the command line arguments.
+        ________________________________________________________________________
+
+        no PARAMETER
+
+        RETURNED VALUE
+                return the argparse object.
+    """
+    parser = argparse.ArgumentParser(description="{0} v. {1}".format(__projectname__, __version__),
+                                     epilog="{0} v. {1} ({2}), a project by {3} "
+                                            "({4})".format(__projectname__,
+                                                           __version__,
+                                                           __license__,
+                                                           __author__,
+                                                           __email__),
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--log',
+                        choices=['DEBUG', 'INFO', 'ERROR', 'CRITICAL'],
+                        default="INFO",
+                        help="set log level")
+
+    parser.add_argument('--version',
+                        action="store_true",
+                        help="show version number and exit")
+
+    parser.add_argument('--about',
+                        action="store_true",
+                        help="show informations about the version, license, author and exit")
+
+    parser.add_argument('--downloadbasics',
+                        action="store_true",
+                        help="download basic files like default symbols.txt "
+                             "and config.ini, and exit")
+
+    parser.add_argument('--checksymbols',
+                        action="store_true",
+                        help="check symbols coherency and exit")
+
+    parser.add_argument('--explicitinput',
+                        action="store_true",
+                        help="explicit input data")
+
+    parser.add_argument('--explicitsymbols',
+                        action="store_true",
+                        help="explicit symbols, displaying unicode analyse of each character")
+
+    parser.add_argument('--explicitoutput',
+                        action="store_true",
+                        help="explicit output string, displaying unicode analyse of each character")
+
+    parser.add_argument('--transform_alpha2alephrew',
+                        choices=['yes', 'no'],
+                        default="yes",
+                        help="if 'no', do not read the input file, do not transform it")
+
+    parser.add_argument('--inputfile',
+                        type=str,
+                        default="input.txt",
+                        help="name of the inputfile to be read and transformed")
+
+    parser.add_argument('--symbolsfile',
+                        type=str,
+                        default="symbols.txt",
+                        help='name of the symbols file to be read')
+
+    parser.add_argument('--outputformat',
+                        choices=['html', 'console'],
+                        default="console",
+                        help="output format")
+
+    parser.add_argument("--cfgfile",
+                        type=str,
+                        default="config.ini",
+                        help="name of the configuration file to be read")
+
+    parser.add_argument("--checkinputdata",
+                        choices=['yes', 'no'],
+                        default="yes",
+                        help="if 'yes', check the coherency of the input data")
+
+    parser.add_argument("--source",
+                        choices=['stdin', 'inputfile'],
+                        default="inputfile",
+                        help="choose 'stdin' to use the script through a pipe")
+
+    return parser.parse_args()
