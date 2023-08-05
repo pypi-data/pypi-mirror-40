3b9cb44df384990a4cb712b78ffb46b1822275d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:23 +0000

    Add package elasticsearch-loader/0.2.13/elasticsearch-loader-0.2.13.tar.gz

---
+++ packages/elasticsearch-loader/elasticsearch-loader-0.2.13.tar.gz/elasticsearch-loader-0.2.13/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import sys
+
+extras = []
+if sys.version_info.major == 2:
+    extras.append('unicodecsv')
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except Exception:
+    long_description = ''
+
+setup(
+    name='elasticsearch-loader',
+    author='Moshe Zada',
+    version='0.2.13',
+    packages=['elasticsearch_loader'],
+    keywords=['elastic', 'elasticsearch', 'csv', 'json', 'parquet', 'bulk', 'loader'],
+    url='https://github.com/Moshe/elasticsearch_loader',
+    license='',
+    long_description=long_description,
+    description='A pythonic tool for batch loading data files (json, parquet, csv, tsv) into ElasticSearch',
+    install_requires=['elasticsearch>=6', 'click==6.7', 'click-stream', 'click-conf'] + extras,
+    extras_require={
+        'parquet': ['parquet'],
+        'tests': ['pytest', 'mock'],
+    },
+    entry_points={
+        'console_scripts': [
+            'elasticsearch_loader = elasticsearch_loader:main',
+        ]
+    }
+)
