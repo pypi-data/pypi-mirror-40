57391d56c08d26248df2f99119fdb23b16e4225d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:34 +0000

    Add package xintool/0.0.2/xintool-0.0.2.tar.gz

---
+++ packages/xintool/xintool-0.0.2.tar.gz/xintool-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='xintool',
+    version='0.0.2',
+    author='jiangyixin',
+    author_email='jyx15221613915@gmail.com',
+    url='http://blog.jiangyixin.top',
+    description='python opensource tools',
+    packages=['xintool'],
+    install_requires=[],
+    entry_points={}
+)
