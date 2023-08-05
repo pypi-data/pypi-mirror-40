c2c03ce80bb9e0d92ba59bc552ab282ed3dbd50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:41 +0000

    Add package pyarchops/0.0.3/pyarchops-0.0.3.tar.gz

---
+++ packages/pyarchops/pyarchops-0.0.3.tar.gz/pyarchops-0.0.3/pyarchops/__init__.py
@@ -0,0 +1,8 @@
+""" __init__.py for pyarchops """
+
+__version__ = '0.0.3'
+
+
+from pyarchops_os_updates import os_updates  # noqa
+from pyarchops_helpers import helpers  # noqa
+from pyarchops_tinc import tinc  # noqa
