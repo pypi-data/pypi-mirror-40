f5f1d34d35cc8514bf06d6cf3c658fcba9029227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package NuO/0.1.4/NuO-0.1.4.tar.gz

---
+++ packages/NuO/NuO-0.1.4.tar.gz/NuO-0.1.4/nuoParser/handlers/utils.py
@@ -0,0 +1,122 @@
+import siteData as sd
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def top(self):
+        return self.stack[len(self.stack) - 1]
+
+    def isEmpty(self):
+        return True if (len(self.stack) == 0) else False
+
+#Function to access properties of object
+def chainedPropertyAccess(obj, arr = []):
+
+    #Check for unit case
+    if(len(arr) == 1):
+        try:
+            #Unit case, Return Value of chained properties of an object
+            return obj[arr[0]]
+        except KeyError:
+            #Return empty string when interrupt is a KeyError
+            return ""
+    else:
+        #Handle KeyError
+        try:
+            #Recursive to call access property of property of an object
+            return chainedPropertyAccess(obj[arr[0]], arr[1:])
+        except KeyError:
+            #Return empty string when interrupt is a KeyError
+            return ""
+
+def getPostfix(exp):
+
+    postfix = ""
+    operators = ["+", "-", "/", "*"]
+    pre = {"+": 1, "-": 1, "*": 2, "/": 2}
+    stack = Stack()
+
+    for char in exp:
+        if(char == '('):
+            stack.push(char)
+            continue
+
+        if(char not in operators and char is not "(" and char is not ")"):
+            postfix += char
+            continue
+
+        if(char == ")"):
+            while(stack.top() is not "("):
+                postfix += " " + stack.pop() + " "
+            stack.pop()
+            continue
+
+        if(stack.isEmpty() or stack.top() == "(" or pre[stack.top()] < pre[char]):
+            stack.push(char)
+            continue
+        else:
+            while(1):
+                if(stack.isEmpty() or stack.top() == "(" or pre[stack.top()] < pre[char]):
+                    break
+                postfix += " " + stack.pop() + " "
+            stack.push(char)
+
+    while(not stack.isEmpty()):
+        postfix += " " + stack.pop() + " "
+
+    return postfix
+
+def evalPostfix(expression):
+
+    tempExp = expression.split()
+    operators = ["+", "-", "/", "*"]
+    stack = Stack()
+
+    for op in tempExp:
+        if(op in operators):
+            x, y = stack.pop(), stack.pop()
+            if(type(x) is int or x.isdigit()):
+                stack.push(_eval(int(x), int(y), op))
+            else:
+                x = getValue(x)
+                stack.push(_eval(x, y, op))
+            continue
+        if(op is not " "):
+            stack.push(op)
+
+    return stack.top()
+
+def _eval(x, y, op):
+
+    if(op == "+"):
+        return y + x
+
+    if(op == "-"):
+        return y - x
+
+    if(op == "/"):
+        return int(y / x)
+
+    if(op == "*"):
+        return y * x
+
+def getValue(ch, rangeData = False, rangeDataObj = {}):
+
+    if(rangeData and ch[0] in rangeDataObj.keys()):
+        return chainedPropertyAccess(rangeDataObj, ch)
+
+    if(ch[0] in sd.GLOBALS.keys()):
+        return chainedPropertyAccess(sd.GLOBALS, ch)
+
+    if(ch[0] in sd.DATAOBJECT.keys()):
+        return chainedPropertyAccess(sd.DATAOBJECT, ch)
+
+    if(ch[0] in sd.DEFINEDOBJECTS.keys()):
+        return chainedPropertyAccess(sd.DEFINEDOBJECTS, ch)
---
+++ packages/NuO/NuO-0.1.4.tar.gz/NuO-0.1.4/nuoParser/parser.py
@@ -0,0 +1,79 @@
+import os
+import re
+
+from siteData import *
+from . import patterns as p
+from .action import setAction, takeAction, openFile, closeFile
+from .handlers import var, define, rangeHandler, arithmetic as a
+
+def start():
+    files = os.listdir(NUODIR)
+    parse(files = files)
+
+def parse(files = [], prefix = NUODIR):
+    pattern = re.compile("^.*\.nuo$")
+    for file in files:
+        file = os.path.join(prefix, file)
+        if(os.path.isdir(file)):
+            os.mkdir("build/" + file[len(NUODIR):])
+            parse(os.listdir(file), file)
+        if pattern.match(file) is not None:
+            parseFile(file)
+            pass
+        else:
+            pass
+
+def parseFile(file):
+    if os.path.exists(file):
+        baseName = file[len(NUODIR):]
+        outputFileName = "build/" + baseName.split(".")[0] + ".html"
+        openFile(outputFileName)
+
+        f = open(file, "r")
+        for line in f.readlines():
+            method = detectPattern(line)
+
+            if method == "var":
+                line = var.exp(line)
+                takeAction(line)
+
+            if method == "define":
+                define.exp(line)
+
+            if method == "range":
+                setAction("range")
+                rangeHandler.startRangeBlock(line)
+
+            if method == "rangeEnd":
+                setAction("rangeEnd")
+                takeAction(line)
+
+            if method == "arithmetic":
+                line = a.eval(line)
+                takeAction(line)
+
+            if method == "raw":
+                takeAction(line)
+
+        closeFile()
+
+    else:
+        raise Exception("File '{}' does not exist".format(file))
+
+def detectPattern(expression):
+    if p._var.search(expression) is not None:
+        return "var"
+
+    if p._range.search(expression) is not None:
+        return "range"
+
+    if p._define.search(expression) is not None:
+        return "define"
+
+    if p._rangeEnd.search(expression) is not None:
+        return "rangeEnd"
+
+    if p._arithmetic.search(expression) is not None:
+        return "arithmetic"
+
+    return "raw"
