74012a45360e0632889814862ba4484dd61df49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:46 +0000

    Add package pyfluentformio/0.1.1/pyfluentformio-0.1.1.tar.gz

---
+++ packages/pyfluentformio/pyfluentformio-0.1.1.tar.gz/pyfluentformio-0.1.1/README.md
@@ -0,0 +1,17 @@
+# pyfluentformio
+
+Python version of FluentJS. Allows users to perform GET and POST requests to Form.io resources and forms. It also allows the use of filters and method chaining.
+
+## Usage
+
+```python
+from pyfluent-formio import Fluent
+
+connector = Fluent(baseUrl = 'FORMIO BASE URL', resourcePath='FORMIO RESOURCE PATH', token='FORMIO API TOKEN')
+
+connector.get()
+```
+
+## License
+
+MIT
---
+++ packages/pyfluentformio/pyfluentformio-0.1.1.tar.gz/pyfluentformio-0.1.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='pyfluentformio',
+    url='https://github.com/Goat-Lab/pyfluentformio',
+    author='Tomas Castro',
+    author_email='tomas.castro@goatlab.io',
+    # Needed to actually package something
+    packages=['pyfluent'],
+    download_url='https://github.com/Goat-Lab/pyfluentformio/archive/v0.1.1.tar.gz',
+    keywords=['fluent', 'form.io', 'formio'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    # Needed for dependencies
+    install_requires=['requests', 'pandas'],
+    # *strongly* suggested for sharing
+    version='0.1.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Fluent wrapper for creating requests to Form.io',
+    # We will also need a readme eventually (there will be a warning)
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
