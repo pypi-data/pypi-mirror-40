d31031e4f1a71903b951df8ec341480c361d79e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:49 +0000

    Add package squabble/0.0.0/squabble-0.0.0.tar.gz

---
+++ packages/squabble/squabble-0.0.0.tar.gz/squabble-0.0.0/squabble/__main__.py
@@ -0,0 +1,134 @@
+'''
+Usage:
+  squabble [options] [FILES...]
+  squabble (-h | --help)
+
+Arguments:
+  FILES              One or more files to lint
+
+Options:
+  -c --config=PATH     Path to configuration file
+  -h --help            Show this screen
+  -p --preset=PRESET   Start with a base preset rule configuration
+  -P --list-presets    List the available preset configurations
+  -r --show-rule=RULE  Show detailed information about RULE
+  -R --list-rules      Print out information about all available rules
+  -V --verbose         Turn on debug level logging
+  -v --version         Show version information
+'''
+
+import json
+import os.path
+import sys
+from pkg_resources import get_distribution
+
+import colorama
+import docopt
+from colorama import Style
+
+import squabble
+from squabble import config, lint, logger, rules, reporter
+
+
+def main():
+    version = get_distribution('squabble').version
+    args = docopt.docopt(__doc__, version=version)
+
+    if args['--verbose']:
+        logger.setLevel('DEBUG')
+
+    migrations = args['FILES']
+    config_file = args['--config'] or config.discover_config_location()
+    preset = args['--preset']
+
+    if args['--list-presets']:
+        return list_presets()
+
+    if config_file and not os.path.exists(config_file):
+        print('Could not find a valid config file!')
+        sys.exit(1)
+
+    base_config = config.load_config(config_file, preset)
+
+    # Load all of the rule classes into memory
+    rules.load(plugin_paths=base_config.plugins)
+
+    if args['--list-rules']:
+        return list_rules()
+    elif args['--show-rule']:
+        return show_rule(name=args['--show-rule'])
+
+    issues = []
+    for file_name in migrations:
+        issues += lint_file(base_config, file_name)
+
+    reporter.report(base_config.reporter, issues)
+
+    # Make sure we have an error status if something went wrong.
+    if issues:
+        sys.exit(1)
+
+
+def lint_file(base_config, file_name):
+    file_config = config.apply_file_config(base_config, file_name)
+    return lint.check_file(file_config, file_name)
+
+
+def show_rule(name):
+    color = {
+        'bold': Style.BRIGHT,
+        'reset': Style.RESET_ALL,
+    }
+
+    try:
+        rule = rules.Registry.get_meta(name)
+    except squabble.UnknownRuleException:
+        print('{bold}Unknown rule:{reset} {name}'.format(**{
+            'name': name,
+            **color
+        }))
+        sys.exit(1)
+
+    meta = rule['meta']
+    print('{bold}{name}{reset} - {description}\n{help}'.format(**{
+        **meta,
+        **color
+    }))
+
+
+def list_rules():
+    color = {
+        'bold': Style.BRIGHT,
+        'reset': Style.RESET_ALL,
+    }
+
+    all_rules = sorted(rules.Registry.all(), key=lambda r: r['meta']['name'])
+
+    for rule in all_rules:
+        meta = rule['meta']
+
+        print('{bold}{name: <32}{reset} {description}'.format(**{
+            **color,
+            **meta
+        }))
+
+
+def list_presets():
+    for name, preset in config.PRESETS.items():
+        print('{bold}{name}{reset} - {description}'.format(
+            name=name,
+            description=preset.get('description', ''),
+            bold=Style.BRIGHT,
+            reset=Style.RESET_ALL
+        ))
+
+        # npm here i come
+        left_pad = '    '
+        cfg = json.dumps(preset['config'], indent=4)\
+                  .replace('\n', '\n' + left_pad)
+        print(left_pad + cfg)
+
+
+if __name__ == '__main__':
+    colorama.init()
+    main()
---
+++ packages/squabble/squabble-0.0.0.tar.gz/squabble-0.0.0/squabble/lint.py
@@ -0,0 +1,136 @@
+""" linting engine """
+
+import collections
+
+import pglast
+
+from squabble import RuleConfigurationException
+from squabble.rules import Registry
+
+
+LintIssue = collections.namedtuple('LintIssue', [
+    'message_id',
+    'message_text',
+    'message_params',
+    'node',
+    'file',
+    'rule',
+    'severity',
+    'location'
+])
+
+# Make all the fields nullable
+LintIssue.__new__.__defaults__ = (None,) * len(LintIssue._fields)
+
+
+def parse_file(file_name):
+    with open(file_name, 'r') as fp:
+        contents = fp.read()
+
+    return pglast.Node(pglast.parse_sql(contents))
+
+
+def configure_rules(rule_config):
+    rules = []
+
+    for name, options in rule_config.items():
+        meta = Registry.get_meta(name)
+        cls = meta['class']
+
+        rules.append(cls(options))
+
+    return rules
+
+
+def check_file(config, file_name):
+    try:
+        rules = configure_rules(config.rules)
+    except RuleConfigurationException as exc:
+        return [LintIssue(
+            message_text=exc.msg,
+            rule=exc.rule,
+            node=None,
+            file=file_name,
+            severity='ERROR',
+            location=None,
+        )]
+
+    s = Session(rules, file_name)
+    return s.lint()
+
+
+class Session:
+    def __init__(self, rules, file_name):
+        self._rules = rules
+        self._file = file_name
+        self._issues = []
+
+    def report_issue(self, issue):
+        i = issue._replace(file=self._file)
+        self._issues.append(i)
+
+    def lint(self):
+        root_ctx = LintContext(self)
+
+        for rule in self._rules:
+            rule.enable(root_ctx)
+
+        try:
+            ast = parse_file(self._file)
+            root_ctx.traverse(ast)
+
+        except pglast.parser.ParseError as exc:
+            root_ctx.report_issue(LintIssue(
+                severity='ERROR',
+                message_text=exc.args[0],
+                location=exc.location
+            ))
+
+        return self._issues
+
+
+class LintContext:
+    def __init__(self, session):
+        self._hooks = {}
+        self._exit_hooks = []
+        self._session = session
+
+    def traverse(self, parent_node):
+        for node in parent_node.traverse():
+            # Ignore scalar values
+            if not isinstance(node, pglast.node.Node):
+                continue
+
+            tag = node.node_tag
+            for hook in self._hooks.get(tag, []):
+                child_ctx = LintContext(self._session)
+                hook(child_ctx, node)
+
+                # children can set up their own hooks, so recurse
+                child_ctx.traverse(node)
+
+        for exit_fn in self._exit_hooks:
+            exit_fn(self)
+
+    def register_exit(self, fn):
+        self._exit_hooks.append(fn)
+
+    def register(self, node, fn):
+        if node not in self._hooks:
+            self._hooks[node] = []
+
+        self._hooks[node].append(fn)
+
+    def report_issue(self, issue):
+        self._session.report_issue(issue)
+
+    def report(self, rule, message_id, params=None, node=None, severity='ERROR'):
+        self._session.report_issue(LintIssue(
+            rule=rule,
+            message_id=message_id,
+            message_params=params,
+            node=node,
+            severity=severity,
+            # This is filled in later
+            file=None,
+        ))
