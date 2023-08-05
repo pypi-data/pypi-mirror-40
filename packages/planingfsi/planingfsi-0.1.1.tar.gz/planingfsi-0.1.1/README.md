b886e5890ec62e8e5a2ce7851c60c75ddca490a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:13 +0000

    Add package planingfsi/0.1.1/planingfsi-0.1.1.tar.gz

---
+++ packages/planingfsi/planingfsi-0.1.1.tar.gz/planingfsi-0.1.1/README.md
@@ -0,0 +1,31 @@
+# PlaningFSI
+
+![Bitbucket Pipelines](https://img.shields.io/bitbucket/pipelines/mattkram/planingfsi/master.svg)
+[![Documentation Status](https://readthedocs.org/projects/planingfsi/badge/?version=latest)](https://planingfsi.readthedocs.io/en/latest/?badge=latest)
+[![image](https://img.shields.io/pypi/v/planingfsi.svg)](https://pypi.org/project/planingfsi/)
+[![image](https://img.shields.io/pypi/l/planingfsi.svg)](https://pypi.org/project/planingfsi/)
+
+## Cautionary Note
+
+I am currently working on releasing this package as open source.
+Since this is my first open-source release, the next few releases on PyPi should not be used for production.
+I will release version 1.0.0 once I feel that I have sufficiently cleaned up and documented the code. 
+
+## Summary
+
+PlaningFSI is a scientific Python program use to calculate the steady-state response of 2-d marine structures subject to planing flow with consideration for Fluid-Structure Interaction (FSI) as well as rigid body motion.
+It was originally written in 2012-2013 to support my Ph.D. research and has recently (2018) been updated and released as open source.
+
+## Required Python version
+
+The code is written in Python and was originally written in Python 2.6.5.
+it has since been updated to require Python 3.6+.
+Although future versions of Python **should** support, the code, I can make no guarantees.
+
+## Installation
+
+PlaningFSI can be installed with pip:
+
+```
+pip install planingfsi
+```
---
+++ packages/planingfsi/planingfsi-0.1.1.tar.gz/planingfsi-0.1.1/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['krampy',
+ 'krampy.iotools',
+ 'planingfsi',
+ 'planingfsi.fe',
+ 'planingfsi.fsi',
+ 'planingfsi.potentialflow']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0',
+ 'matplotlib>=3.0,<4.0',
+ 'numpy>=1.15,<2.0',
+ 'scipy>=1.2,<2.0',
+ 'six>=1.12,<2.0']
+
+entry_points = \
+{'console_scripts': ['generateMesh = planingfsi:cli.mesh.main',
+                     'planingFSI = planingfsi:cli.planingfsi.main']}
+
+setup_kwargs = {
+    'name': 'planingfsi',
+    'version': '0.1.1',
+    'description': 'Fluid-Structure Interaction for large deformation planing surfaces',
+    'long_description': '# PlaningFSI\n\n![Bitbucket Pipelines](https://img.shields.io/bitbucket/pipelines/mattkram/planingfsi/master.svg)\n[![Documentation Status](https://readthedocs.org/projects/planingfsi/badge/?version=latest)](https://planingfsi.readthedocs.io/en/latest/?badge=latest)\n[![image](https://img.shields.io/pypi/v/planingfsi.svg)](https://pypi.org/project/planingfsi/)\n[![image](https://img.shields.io/pypi/l/planingfsi.svg)](https://pypi.org/project/planingfsi/)\n\n## Cautionary Note\n\nI am currently working on releasing this package as open source.\nSince this is my first open-source release, the next few releases on PyPi should not be used for production.\nI will release version 1.0.0 once I feel that I have sufficiently cleaned up and documented the code. \n\n## Summary\n\nPlaningFSI is a scientific Python program use to calculate the steady-state response of 2-d marine structures subject to planing flow with consideration for Fluid-Structure Interaction (FSI) as well as rigid body motion.\nIt was originally written in 2012-2013 to support my Ph.D. research and has recently (2018) been updated and released as open source.\n\n## Required Python version\n\nThe code is written in Python and was originally written in Python 2.6.5.\nit has since been updated to require Python 3.6+.\nAlthough future versions of Python **should** support, the code, I can make no guarantees.\n\n## Installation\n\nPlaningFSI can be installed with pip:\n\n```\npip install planingfsi\n```\n',
+    'author': 'Matt Kramer',
+    'author_email': 'matthew.robert.kramer@gmail.com',
+    'url': 'https://bitbucket.org/mattkram/planingfsi',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
