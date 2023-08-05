1d5c7c69f0ad944e3bfd11508606b61d9e0e3ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:01 +0000

    Add package py2jn/0.0.1b2/py2jn-0.0.1b2.tar.gz

---
+++ packages/py2jn/py2jn-0.0.1b2.tar.gz/py2jn-0.0.1b2/tests/test_py2nb.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for py2jn module"""
+
+import os
+from tempfile import gettempdir
+import pytest
+import py2jn
+from py2jn.__main__ import python_to_notebook
+
+
+@pytest.fixture
+def samplepy(fname='example.py'):
+    return os.path.join('tests', fname)
+
+
+@pytest.fixture
+def sampleipynb(fname='example.ipynb'):
+    return os.path.join('tests', fname)
+
+
+def test_py2jn(samplepy, sampleipynb):
+    tmpdir = gettempdir()
+    outfile = os.path.join(tmpdir, 'py2jn_example.ipynb')
+    python_to_notebook(input_filename=samplepy, output_filename=outfile)
+    with open(os.path.join(tmpdir, 'py2jn_example.ipynb'), 'r') as outfileobj:
+        outfile = outfileobj.read()
+    with open(sampleipynb, 'r') as reffileobj:
+        refile = reffileobj.read()
+    assert outfile == refile
+    os.remove(os.path.join(tmpdir, 'py2jn_example.ipynb'))
+
+
+def test_str_to_str(samplepy, sampleipynb):
+    with open(samplepy, 'r') as fin:
+        pystr = fin.read()
+    nbstr = py2jn.py_string_to_nb_string(pystr)
+    nb = py2jn.nb_string_to_notebook(nbstr)
+    outstr = py2jn.write_notebook_to_string(nb)
+    with open(sampleipynb, 'r') as fin:
+        refstr = fin.read()
+    assert outstr == refstr
