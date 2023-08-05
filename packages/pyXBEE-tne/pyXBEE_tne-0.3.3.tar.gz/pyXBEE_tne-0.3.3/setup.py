5747b4db8c54be9abd8c1e08c6c09a1b1e35a882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:40 +0000

    Add package pyXBEE-tne/0.3.3/pyXBEE_tne-0.3.3.tar.gz

---
+++ packages/pyXBEE-tne/pyXBEE_tne-0.3.3.tar.gz/pyXBEE_tne-0.3.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name = 'pyXBEE_tne',
+    packages = ['xbee_tne'],
+    install_requires=['pyserial>=2.7'],
+    version = '0.3.3',
+    description = 'a library to communicate with the XBEE',
+    author='Ruud van der Horst',
+    author_email='ik@ruudvdhorst.nl',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ' +
+            'GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
