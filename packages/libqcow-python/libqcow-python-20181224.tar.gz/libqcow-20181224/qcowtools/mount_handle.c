f3612787d3ebe43cb0939ee8b4a4404752f20d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:59 +0000

    Add package libqcow-python/20181224/libqcow-python-20181224.tar.gz

---
+++ packages/libqcow-python/libqcow-python-20181224.tar.gz/libqcow-20181224/qcowtools/mount_handle.c
@@ -0,0 +1,884 @@
+/*
+ * Mount handle
+ *
+ * Copyright (C) 2010-2018, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+#include <memory.h>
+#include <narrow_string.h>
+#include <system_string.h>
+#include <types.h>
+#include <wide_string.h>
+
+#include "mount_file_entry.h"
+#include "mount_file_system.h"
+#include "mount_handle.h"
+#include "qcowtools_libcerror.h"
+#include "qcowtools_libcpath.h"
+#include "qcowtools_libqcow.h"
+#include "qcowtools_libuna.h"
+
+/* Creates a mount handle
+ * Make sure the value mount_handle is referencing, is set to NULL
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_initialize(
+     mount_handle_t **mount_handle,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_initialize";
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( *mount_handle != NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_VALUE_ALREADY_SET,
+		 "%s: invalid mount handle value already set.",
+		 function );
+
+		return( -1 );
+	}
+	*mount_handle = memory_allocate_structure(
+	                 mount_handle_t );
+
+	if( *mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_MEMORY,
+		 LIBCERROR_MEMORY_ERROR_INSUFFICIENT,
+		 "%s: unable to create mount handle.",
+		 function );
+
+		goto on_error;
+	}
+	if( memory_set(
+	     *mount_handle,
+	     0,
+	     sizeof( mount_handle_t ) ) == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_MEMORY,
+		 LIBCERROR_MEMORY_ERROR_SET_FAILED,
+		 "%s: unable to clear mount handle.",
+		 function );
+
+		goto on_error;
+	}
+	if( mount_file_system_initialize(
+	     &( ( *mount_handle )->file_system ),
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_INITIALIZE_FAILED,
+		 "%s: unable to initialize file system.",
+		 function );
+
+		goto on_error;
+	}
+	return( 1 );
+
+on_error:
+	if( *mount_handle != NULL )
+	{
+		memory_free(
+		 *mount_handle );
+
+		*mount_handle = NULL;
+	}
+	return( -1 );
+}
+
+/* Frees a mount handle
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_free(
+     mount_handle_t **mount_handle,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_free";
+	int result            = 1;
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( *mount_handle != NULL )
+	{
+		if( ( *mount_handle )->basename != NULL )
+		{
+			memory_free(
+			 ( *mount_handle )->basename );
+		}
+		if( mount_file_system_free(
+		     &( ( *mount_handle )->file_system ),
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_FINALIZE_FAILED,
+			 "%s: unable to free file system.",
+			 function );
+
+			result = -1;
+		}
+		if( memory_set(
+		     ( *mount_handle )->key_data,
+		     0,
+		     16 ) == NULL )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_MEMORY,
+			 LIBCERROR_MEMORY_ERROR_SET_FAILED,
+			 "%s: unable to clear key data.",
+			 function );
+
+			result = -1;
+		}
+		memory_free(
+		 *mount_handle );
+
+		*mount_handle = NULL;
+	}
+	return( result );
+}
+
+/* Signals the mount handle to abort
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_signal_abort(
+     mount_handle_t *mount_handle,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_signal_abort";
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( mount_file_system_signal_abort(
+	     mount_handle->file_system,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_SET_FAILED,
+		 "%s: unable to signal file system abort.",
+		 function );
+
+		return( -1 );
+	}
+	return( 1 );
+}
+
+/* Sets the keys
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_set_keys(
+     mount_handle_t *mount_handle,
+     const system_character_t *string,
+     libcerror_error_t **error )
+{
+	static char *function   = "mount_handle_set_keys";
+	size_t string_length    = 0;
+	uint32_t base16_variant = 0;
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	string_length = system_string_length(
+	                 string );
+
+	if( memory_set(
+	     mount_handle->key_data,
+	     0,
+	     16 ) == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_MEMORY,
+		 LIBCERROR_MEMORY_ERROR_SET_FAILED,
+		 "%s: unable to clear key data.",
+		 function );
+
+		goto on_error;
+	}
+	base16_variant = LIBUNA_BASE16_VARIANT_RFC4648;
+
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+	if( _BYTE_STREAM_HOST_IS_ENDIAN_BIG )
+	{
+		base16_variant |= LIBUNA_BASE16_VARIANT_ENCODING_UTF16_BIG_ENDIAN;
+	}
+	else
+	{
+		base16_variant |= LIBUNA_BASE16_VARIANT_ENCODING_UTF16_LITTLE_ENDIAN;
+	}
+#endif
+	if( string_length != 32 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_UNSUPPORTED_VALUE,
+		 "%s: unsupported string length.",
+		 function );
+
+		goto on_error;
+	}
+	if( libuna_base16_stream_copy_to_byte_stream(
+	     (uint8_t *) string,
+	     string_length,
+	     mount_handle->key_data,
+	     16,
+	     base16_variant,
+	     0,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_COPY_FAILED,
+		 "%s: unable to copy key data.",
+		 function );
+
+		goto on_error;
+	}
+	mount_handle->key_data_is_set = 1;
+
+	return( 1 );
+
+on_error:
+	memory_set(
+	 mount_handle->key_data,
+	 0,
+	 16 );
+
+	mount_handle->key_data_is_set = 0;
+
+	return( -1 );
+}
+
+/* Sets the password
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_set_password(
+     mount_handle_t *mount_handle,
+     const system_character_t *string,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_set_password";
+	size_t string_length  = 0;
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( string == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid string.",
+		 function );
+
+		return( -1 );
+	}
+	string_length = system_string_length(
+	                 string );
+
+	mount_handle->password        = string;
+	mount_handle->password_length = string_length;
+
+	return( 1 );
+}
+
+/* Opens the mount handle
+ * Returns 1 if successful, 0 if the keys could not be read or -1 on error
+ */
+int mount_handle_open(
+     mount_handle_t *mount_handle,
+     const system_character_t *filename,
+     libcerror_error_t **error )
+{
+	libqcow_file_t *image            = NULL;
+	system_character_t *basename_end = NULL;
+	static char *function            = "mount_handle_open";
+	size_t basename_length           = 0;
+	size_t filename_length           = 0;
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( filename == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid filename.",
+		 function );
+
+		return( -1 );
+	}
+	filename_length = system_string_length(
+	                   filename );
+
+	basename_end = system_string_search_character_reverse(
+	                filename,
+	                (system_character_t) LIBCPATH_SEPARATOR,
+	                filename_length + 1 );
+
+	if( basename_end != NULL )
+	{
+		basename_length = (size_t) ( basename_end - filename ) + 1;
+	}
+	if( basename_length > 0 )
+	{
+		if( mount_handle_set_basename(
+		     mount_handle,
+		     filename,
+		     basename_length,
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_SET_FAILED,
+			 "%s: unable to set basename.",
+			 function );
+
+			goto on_error;
+		}
+	}
+	if( libqcow_file_initialize(
+	     &image,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_INITIALIZE_FAILED,
+		 "%s: unable to initialize image.",
+		 function );
+
+		goto on_error;
+	}
+	if( mount_handle->key_data_is_set != 0 )
+	{
+		if( libqcow_file_set_keys(
+		     image,
+		     mount_handle->key_data,
+		     16,
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_SET_FAILED,
+			 "%s: unable to set keys.",
+			 function );
+
+			goto on_error;
+		}
+	}
+	else if( mount_handle->password != NULL )
+	{
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+		if( libqcow_file_set_utf16_password(
+		     image,
+		     (uint16_t *) mount_handle->password,
+		     mount_handle->password_length,
+		     error ) != 1 )
+#else
+		if( libqcow_file_set_utf8_password(
+		     image,
+		     (uint8_t *) mount_handle->password,
+		     mount_handle->password_length,
+		     error ) != 1 )
+#endif
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_SET_FAILED,
+			 "%s: unable to set password.",
+			 function );
+
+			goto on_error;
+		}
+	}
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+	if( libqcow_file_open_wide(
+	     image,
+	     filename,
+	     LIBQCOW_OPEN_READ,
+	     error ) != 1 )
+#else
+	if( libqcow_file_open(
+	     image,
+	     filename,
+	     LIBQCOW_OPEN_READ,
+	     error ) != 1 )
+#endif
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_IO,
+		 LIBCERROR_IO_ERROR_OPEN_FAILED,
+		 "%s: unable to open image.",
+		 function );
+
+		goto on_error;
+	}
+	if( mount_file_system_append_image(
+	     mount_handle->file_system,
+	     image,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_APPEND_FAILED,
+		 "%s: unable to append image to file system.",
+		 function );
+
+		goto on_error;
+	}
+	return( 1 );
+
+on_error:
+	if( image != NULL )
+	{
+		libqcow_file_free(
+		 &image,
+		 NULL );
+	}
+	return( -1 );
+}
+
+/* Closes the mount handle
+ * Returns the 0 if succesful or -1 on error
+ */
+int mount_handle_close(
+     mount_handle_t *mount_handle,
+     libcerror_error_t **error )
+{
+	libqcow_file_t *image = NULL;
+	static char *function = "mount_handle_close";
+	int image_index       = 0;
+	int number_of_images  = 0;
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( mount_file_system_get_number_of_images(
+	     mount_handle->file_system,
+	     &number_of_images,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_GET_FAILED,
+		 "%s: unable to retrieve number of images.",
+		 function );
+
+		return( -1 );
+	}
+	for( image_index = number_of_images - 1;
+	     image_index > 0;
+	     image_index-- )
+	{
+		if( mount_file_system_get_image_by_index(
+		     mount_handle->file_system,
+		     image_index,
+		     &image,
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_GET_FAILED,
+			 "%s: unable to retrieve image: %d.",
+			 function,
+			 image_index );
+
+			return( -1 );
+		}
+		if( libqcow_file_close(
+		     image,
+		     error ) != 0 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_IO,
+			 LIBCERROR_IO_ERROR_CLOSE_FAILED,
+			 "%s: unable to close image: %d.",
+			 function,
+			 image_index );
+
+			return( -1 );
+		}
+	}
+	return( 0 );
+}
+
+/* Sets the basename
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_set_basename(
+     mount_handle_t *mount_handle,
+     const system_character_t *basename,
+     size_t basename_size,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_set_basename";
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( mount_handle->basename != NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_VALUE_ALREADY_SET,
+		 "%s: invalid mount handle - basename value already set.",
+		 function );
+
+		return( -1 );
+	}
+	if( basename == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid basename.",
+		 function );
+
+		return( -1 );
+	}
+	if( basename_size == 0 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_VALUE_MISSING,
+		 "%s: missing basename.",
+		 function );
+
+		goto on_error;
+	}
+	if( basename_size > (size_t) ( SSIZE_MAX / sizeof( system_character_t ) ) )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_VALUE_EXCEEDS_MAXIMUM,
+		 "%s: invalid basename size value exceeds maximum.",
+		 function );
+
+		goto on_error;
+	}
+	mount_handle->basename = system_string_allocate(
+	                          basename_size );
+
+	if( mount_handle->basename == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_MEMORY,
+		 LIBCERROR_MEMORY_ERROR_INSUFFICIENT,
+		 "%s: unable to create basename string.",
+		 function );
+
+		goto on_error;
+	}
+	if( system_string_copy(
+	     mount_handle->basename,
+	     basename,
+	     basename_size ) == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_COPY_FAILED,
+		 "%s: unable to copy basename.",
+		 function );
+
+		goto on_error;
+	}
+	mount_handle->basename[ basename_size - 1 ] = 0;
+
+	mount_handle->basename_size = basename_size;
+
+	return( 1 );
+
+on_error:
+	if( mount_handle->basename != NULL )
+	{
+		memory_free(
+		 mount_handle->basename );
+
+		mount_handle->basename = NULL;
+	}
+	mount_handle->basename_size = 0;
+
+	return( -1 );
+}
+
+/* Sets the path prefix
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_set_path_prefix(
+     mount_handle_t *mount_handle,
+     const system_character_t *path_prefix,
+     size_t path_prefix_size,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_set_path_prefix";
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( mount_file_system_set_path_prefix(
+	     mount_handle->file_system,
+	     path_prefix,
+	     path_prefix_size,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_SET_FAILED,
+		 "%s: unable to set path prefix.",
+		 function );
+
+		return( -1 );
+	}
+	return( 1 );
+}
+
+/* Retrieves a file entry for a specific path
+ * Returns 1 if successful, 0 if no such file entry or -1 on error
+ */
+int mount_handle_get_file_entry_by_path(
+     mount_handle_t *mount_handle,
+     const system_character_t *path,
+     mount_file_entry_t **file_entry,
+     libcerror_error_t **error )
+{
+	libqcow_file_t *image              = NULL;
+	const system_character_t *filename = NULL;
+	static char *function              = "mount_handle_get_file_entry_by_path";
+	size_t path_length                 = 0;
+	int image_index                    = 0;
+	int result                         = 0;
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( path == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid path.",
+		 function );
+
+		return( -1 );
+	}
+	path_length = system_string_length(
+	               path );
+
+	if( path_length == 0 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_VALUE_OUT_OF_BOUNDS,
+		 "%s: invalid path length value out of bounds.",
+		 function );
+
+		return( -1 );
+	}
+	result = mount_file_system_get_image_index_from_path(
+	          mount_handle->file_system,
+	          path,
+	          path_length,
+	          &image_index,
+	          error );
+
+	if( result == -1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_GET_FAILED,
+		 "%s: unable to retrieve image index.",
+		 function );
+
+		return( -1 );
+	}
+	else if( result == 0 )
+	{
+		return( 0 );
+	}
+	if( image_index != -1 )
+	{
+		if( mount_file_system_get_image_by_index(
+		     mount_handle->file_system,
+		     image_index,
+		     &image,
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_GET_FAILED,
+			 "%s: unable to retrieve image: %d.",
+			 function,
+			 image_index );
+
+			return( -1 );
+		}
+		if( image == NULL )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_VALUE_MISSING,
+			 "%s: missing image: %d.",
+			 function,
+			 image_index );
+
+			return( -1 );
+		}
+		filename = &( path[ 0 ] );
+	}
+	if( mount_file_entry_initialize(
+	     file_entry,
+	     mount_handle->file_system,
+	     image_index,
+	     filename,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_INITIALIZE_FAILED,
+		 "%s: unable to initialize file entry for image: %d.",
+		 function,
+		 image_index );
+
+		return( -1 );
+	}
+	return( 1 );
+}
