4d29a02c9979ebccd9b63f47067b4b53d03ba9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:45 +0000

    Add package equadratures/7.6/equadratures-7.6.tar.gz

---
+++ packages/equadratures/equadratures-7.6.tar.gz/equadratures-7.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='equadratures',
+      version='7.6',
+      description='Machine learning with polynomials',
+      long_description=readme(),
+      classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics'
+      ],
+      keywords='polynomial chaos effective quadratures polynomial approximations gradients',
+      url='https://github.com/Effective-Quadratures/Effective-Quadratures',
+      author='Pranay Seshadri',
+      author_email='ps583@cam.ac.uk',
+      license='LPGL-2.1',
+      packages=['equadratures', 'equadratures.distributions'],
+      install_requires=[
+          'numpy',
+          'scipy >= 0.15.0',
+          'matplotlib'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False)
