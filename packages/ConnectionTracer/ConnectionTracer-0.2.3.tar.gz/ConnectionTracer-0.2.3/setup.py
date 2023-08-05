d3a6394f77534a5da9b2a0be277d790d0791377e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:48 +0000

    Add package ConnectionTracer/0.2.3/ConnectionTracer-0.2.3.tar.gz

---
+++ packages/ConnectionTracer/ConnectionTracer-0.2.3.tar.gz/ConnectionTracer-0.2.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ConnectionTracer',
+    version='0.2.3',
+    description='when connected devices changed, do sth :)',
+    author='williamfzc',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/williamfzc/ConnectionTracer',
+    packages=find_packages()
+)
