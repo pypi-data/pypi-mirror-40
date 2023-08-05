cacc5f4c391fdfe0bf854aac6e444ed580282f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:58 +0000

    Add package strava-cli/0.4.4/strava-cli-0.4.4.tar.gz

---
+++ packages/strava-cli/strava-cli-0.4.4.tar.gz/strava-cli-0.4.4/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+VERSION = '0.4.4'
+DESCRIPTION = 'Strava Command-Line Tools'
+
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    python_requires='>=3.0',
+    name='strava-cli',
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=VERSION,
+    author='Bartlomiej Wilczynski',
+    author_email='me@bwilczynski.com',
+    url='https://github.com/bwilczynski/strava-cli',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'click',
+        'requests',
+        'requests_oauthlib',
+        'tabulate',
+        'dateparser'
+    ],
+    entry_points='''
+        [console_scripts]
+        strava=strava.cli:cli
+    ''',
+)
