6fe35edd8afcecdcc79d3bf6b69077e186ff3760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:41 +0000

    Add package pytm/0.4/pytm-0.4.tar.gz

---
+++ packages/pytm/pytm-0.4.tar.gz/pytm-0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+packages=setuptools.find_packages()
+
+setuptools.setup(
+    name='pytm',
+    version='0.4',
+    packages=['pytm'],
+    summary='A Python-based framework for threat modeling.',
+    long_description=open('README.md').read(),
+    license='MIT License',
+    url="https://github.com/izar/pytm",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+	"Environment :: Console",
+	"Intended Audience :: Developers",
+	"Topic :: Security",
+    ],
+    python_requires='>=3',
+    include_package_data=True
+)
