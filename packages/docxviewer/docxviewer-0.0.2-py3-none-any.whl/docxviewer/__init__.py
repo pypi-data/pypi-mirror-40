1c1d1119ee8a7c0c0d7f3cb62e3af1ddb9d86ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:31 +0000

    Add package docxviewer/0.0.2/docxviewer-0.0.2-py3-none-any.whl

---
+++ packages/docxviewer/docxviewer-0.0.2-py3-none-any.whl/docxviewer/__init__.py
@@ -0,0 +1,14 @@
+# docviewer
+# Copyright 2018-2019 Eunhou Esther Song
+# See LICENSE for details.
+"""
+docxviewer
+
+"""
+
+__version__ = '0.0.0'
+__author__ = 'Eunhou Esther Song'
+__license__ = 'MIT'
+
+
+from docxviewer.doc_viewer import directory_listing, display_directory_listing, doc_displayer
