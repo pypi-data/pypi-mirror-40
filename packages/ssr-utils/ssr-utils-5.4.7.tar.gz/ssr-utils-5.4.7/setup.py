6d63d9989e042cefba3b7c988d63b80b69fe3480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:51 +0000

    Add package ssr-utils/5.4.7/ssr-utils-5.4.7.tar.gz

---
+++ packages/ssr-utils/ssr-utils-5.4.7.tar.gz/ssr-utils-5.4.7/setup.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ssr-utils",
+    version="5.4.7",
+    author="@neoctobers",
+    author_email="neoctobers@gmail.com",
+    description="Shadowsocks(R) utils.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neoctobers/py_ssr_utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'profig',
+        'xbase64',
+        'requests[socks]',
+        'xprint',
+        'list_ext',
+        'common-patterns',
+        'proxychains-conf-generator',
+    ],
+)
