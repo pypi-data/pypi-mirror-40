05fb8bc7c07e6687ad3ad21df72a13ce3d72b308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:25 +0000

    Add package commit-helper/3.4.9/commit-helper-3.4.9.tar.gz

---
+++ packages/commit-helper/commit-helper-3.4.9.tar.gz/commit-helper-3.4.9/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", 'r') as fi:  # pragma: no cover
+    long_desc = fi.read()
+
+with open("VERSION.txt", 'r') as ver:   # pragma: no cover
+    version = ver.read()
+
+setup(  # pragma: no cover
+    name='commit-helper',
+    description="A python program that helps you write commits following commit conventions",  # nopep8
+    url='https://github.com/andre-filho/commit-helper',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    author='Andre de Sousa Costa Filho',
+    author_email='andre.filho001@outlook.com',
+    version=str(version).replace('\n', ''),
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'commit = commit_helper.__main__:main',
+            'commit-helper = commit_helper.__main__:main',
+        ]
+    },
+    install_requires=[
+        'pathlib',
+        'pyyaml',
+        'argparse',
+        'colored',
+    ],
+)
