5fe457559f80b1fee28c291cba8ccc202d1e104d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:46 +0000

    Add package Rsql/0.2/Rsql-0.2.tar.gz

---
+++ packages/Rsql/Rsql-0.2.tar.gz/Rsql-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='Rsql',
+      version='0.2',
+      description='Rsql',
+      long_description='sql',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='sql',
+      author='Raf',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'PyMySQL==0.9.3',
+      ],
+      include_package_data=True,
+      zip_safe=False)
