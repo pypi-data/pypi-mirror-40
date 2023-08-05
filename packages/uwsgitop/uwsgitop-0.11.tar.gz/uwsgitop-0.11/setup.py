f5bb68f57131c822a63a0c04f0a6855b346bdb8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:28 +0000

    Add package uwsgitop/0.11/uwsgitop-0.11.tar.gz

---
+++ packages/uwsgitop/uwsgitop-0.11.tar.gz/uwsgitop-0.11/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+
+VERSION = '0.11'
+
+setup(
+    maintainer='Riccardo Magliocchetti',
+    maintainer_email='riccardo.magliocchetti@gmail.com',
+    name='uwsgitop',
+    version=VERSION,
+    description='uWSGI top-like interface',
+    license='MIT',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    scripts=['uwsgitop'],
+    install_requires=[
+        'argparse;python_version<"3"',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
