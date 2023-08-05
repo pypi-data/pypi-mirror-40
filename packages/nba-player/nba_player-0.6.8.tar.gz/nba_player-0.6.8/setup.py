802c9817bbbb7b8d0a5dd9c893f6e4bac71033d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:44 +0000

    Add package nba-player/0.6.8/nba_player-0.6.8.tar.gz

---
+++ packages/nba-player/nba_player-0.6.8.tar.gz/nba_player-0.6.8/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nba_player",
+    version="0.6.8",
+    author="Avyay Varadarajan",
+    author_email="avyayv@gmail.com",
+    description="Get all NBA player data into JSON format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avyayv/python_nba_players",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
