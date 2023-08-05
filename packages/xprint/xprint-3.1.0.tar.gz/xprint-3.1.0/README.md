532d926dd1f2eaef1bcc6b954ee74395c136aab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:32 +0000

    Add package xprint/3.1.0/xprint-3.1.0.tar.gz

---
+++ packages/xprint/xprint-3.1.0.tar.gz/xprint-3.1.0/README.md
@@ -0,0 +1,39 @@
+# xprint
+
+## Development Environment
+
+- Windows 10
+- Python 3.7.0
+
+## Installation
+
+```
+$ pip3 install xprint
+```
+
+## Usage
+
+Import:
+
+```
+import xprint as xp
+
+# xp.init()
+```
+
+Base:
+
+- xp.wr()
+- xp.fi()
+
+Usage:
+
+- xp.success()
+- xp.error()
+- xp.value()
+- xp.step()
+- xp.job()
+- xp.about_t()
+- xp.about_to()
+- xp.getting()
+- ...
---
+++ packages/xprint/xprint-3.1.0.tar.gz/xprint-3.1.0/setup.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xprint",
+    version="3.1.0",
+    author="@neoctobers",
+    author_email="neoctobers@gmail.com",
+    description="xprint",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neoctobers/py_xprint",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'colorama',
+    ],
+)
