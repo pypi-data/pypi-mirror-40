95c6f19fd8f8de72ef76f0b75ab76d4e635a63ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:16 +0000

    Add package iterm-file-handler/0.0.1/iterm_file_handler-0.0.1-py3.7.egg

---
+++ packages/iterm-file-handler/iterm_file_handler-0.0.1-py3.7.egg/iterm_file_handler/iterm_file_handler.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+__version__ = "0.0.1"
+
+import sys, os, json
+
+cli_map = {}
+
+
+def main():
+    _file, _line = _sanitize_params()
+    _run([_get_cli(_file), _file, _line])
+
+
+def _sanitize_params():
+    cli_map = {
+        'java': 'idea',
+        'rb': 'mine',
+        'py': 'charm',
+        'kt': 'studio',
+        'txt': os.getenv('EDITOR', ''),
+    }
+
+    config_file = os.path.expanduser('~/.iterm_file_handler.json')
+
+    if 'override' in sys.argv:
+        with open(config_file, "w+") as jf:
+            json.dump(cli_map, jf)
+        print("Config file created at '{}'".format(config_file))
+        sys.exit()
+
+    if os.path.exists(config_file):
+        with open(config_file, 'r') as jf:
+            cli_map = json.load(jf)
+            print(cli_map)
+
+    if ':' in sys.argv[1]:
+        file, line = sys.argv[1].split(':')
+    else:
+        file, line = sys.argv[1:3]
+
+    if not file.startswith('/'):
+        """ Appending PWD if file path is not absolute"""
+        file = sys.argv[3] + '/' + file
+
+    if len(line) < 1:
+        line = '1'
+    return file, line
+
+
+def _get_extention(file):
+    if '.' in file:
+        return file.split('.')[-1]
+    raise RuntimeError("File extenstion not found in '{}'".format(file))
+
+
+def _get_cli(for_file):
+    try:
+        from shutil import which
+        name = cli_map.get(_get_extention(for_file), '')
+        cli = (which(name, path='/usr/local/bin') or '/usr/bin/open')
+        return cli
+    except Exception as e:
+        _log(e)
+        _log('cli app is ---> ' + name)
+        _log('cli path ---> ' + cli)
+
+
+def _run(cmd):
+    import subprocess
+    args = sys.argv[1:]
+    if '/usr/bin/open' in cmd:
+        del cmd[-1]
+    else:
+        cmd[-2:] = [':'.join(cmd[-2:])]
+
+    out = subprocess.call(cmd)
+    if (out is not 0) or ('test' in args):
+        _log('cmd --> ' + ' '.join(cmd))
+        _log(''.join(args))
+
+
+def _log(msg):
+    import datetime
+    time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    with open("~/iterm_file_handler.log", 'a') as f:
+        f.write("[{}] {} \n".format(time_stamp, msg))
