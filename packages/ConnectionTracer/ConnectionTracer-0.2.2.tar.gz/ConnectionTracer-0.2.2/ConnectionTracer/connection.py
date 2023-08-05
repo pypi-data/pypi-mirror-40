3ac7e75ed950cfb49900615753f732d1f76b60ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:07 +0000

    Add package ConnectionTracer/0.2.2/ConnectionTracer-0.2.2.tar.gz

---
+++ packages/ConnectionTracer/ConnectionTracer-0.2.2.tar.gz/ConnectionTracer-0.2.2/ConnectionTracer/connection.py
@@ -0,0 +1,62 @@
+import socket
+import warnings
+import subprocess
+
+from ConnectionTracer import config
+
+
+# GLOBAL CONNECTION
+adb_socket = None
+
+
+def connect():
+    """ create socket and connect to adb server """
+    global adb_socket
+    if adb_socket is not None:
+        raise RuntimeError('connection already existed')
+
+    host, port = config.HOST, config.PORT
+
+    connection = socket.socket()
+    try:
+        connection.connect((host, port))
+    except ConnectionError as _:
+        warn_msg = 'failed when connecting to adb server: {}:{}, retrying ...'.format(host, port)
+        warnings.warn(warn_msg)
+        reboot_adb_server()
+        connect()
+        return
+
+    adb_socket = connection
+
+
+def disconnect():
+    """ close socket """
+    global adb_socket
+    if adb_socket is not None:
+        adb_socket.close()
+        adb_socket = None
+        return
+    warnings.warn('connection already closed')
+
+
+def reboot_adb_server():
+    """ execute 'adb devices' to start adb server """
+    _reboot_count = 0
+    _max_retry = 1
+
+    def _reboot():
+        nonlocal _reboot_count
+        if _reboot_count >= _max_retry:
+            raise RuntimeError('fail after retry {} times'.format(_max_retry))
+        _reboot_count += 1
+
+        return_code = subprocess.call(['adb', 'devices'], stdout=subprocess.DEVNULL)
+        if bool(return_code):
+            warnings.warn('return not zero, execute "adb version" failed')
+            raise EnvironmentError('adb did not work :(')
+
+    return _reboot
+
+
+reboot_adb_server = reboot_adb_server()
---
+++ packages/ConnectionTracer/ConnectionTracer-0.2.2.tar.gz/ConnectionTracer-0.2.2/README.md
@@ -0,0 +1,50 @@
+# ConnectionTracer
+
+[![PyPI version](https://badge.fury.io/py/ConnectionTracer.svg)](https://badge.fury.io/py/ConnectionTracer)
+
+> when connected devices changed, do sth :)
+
+## How it works?
+
+Use a socket to connect adb server. Nothing about `subprocess` or `os.system`.
+
+View [socket2adb](https://github.com/williamfzc/socket2adb) for detail.
+
+## Installation
+
+```
+pip install ConnectionTracer 
+```
+
+## How to use?
+
+```python
+import ConnectionTracer
+import time
+
+
+# also, you can custom port and host
+ConnectionTracer.config.PORT = 5037
+
+
+# bind hook function
+def hook_function(devices: set):
+    print(devices)
+
+
+ConnectionTracer.start(hook_function)
+print('tracer already started :)')
+
+# do something else you want
+time.sleep(30)
+
+# stop it
+ConnectionTracer.stop()
+print('tracer stopped')
+```
+
+When connected devices changed, `hook_function` would be called.
+
+## More?
+
+Code is quite simple, you can view [100-lines-ver](https://github.com/williamfzc/ConnectionTracer/blob/99aaea27e7014ded49bae02bfee3ce9f8bd2e14c/ConnectionTracer.py) for detail.
