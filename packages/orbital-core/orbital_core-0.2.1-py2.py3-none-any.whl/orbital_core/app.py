bc4bba83476db7a2e12a8f636a5a5f058fdbfa5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:03 +0000

    Add package orbital-core/0.2.1/orbital_core-0.2.1-py2.py3-none-any.whl

---
+++ packages/orbital-core/orbital_core-0.2.1-py2.py3-none-any.whl/orbital_core/app.py
@@ -0,0 +1,14 @@
+from .routes import add_routes
+
+def bootstrap_app(app, root_dir=None,
+                  service_name="my service",
+                  service_description="this is a service.",
+                  min_cpu_percent_for_capture=None):
+    """
+    :param min_cpu_percent_for_capture: setting this to a floating point value
+        configures the percentage that is required to capture that stack
+        for flamegraph analysis.
+    """
+    app["service_name"] = service_name
+    app["service_description"] = service_description
+    add_routes(app, root_dir)
