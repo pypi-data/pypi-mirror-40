9cf56d50106ff85adb32e8e32bffd4a5a640e2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:16 +0000

    Add package onegov.org/0.46.4/onegov.org-0.46.4.tar.gz

---
+++ packages/onegov.org/onegov.org-0.46.4.tar.gz/onegov.org-0.46.4/onegov/org/forms/__init__.py
@@ -0,0 +1,61 @@
+from onegov.org.forms.allocation import DaypassAllocationEditForm
+from onegov.org.forms.allocation import DaypassAllocationForm
+from onegov.org.forms.allocation import RoomAllocationEditForm
+from onegov.org.forms.allocation import RoomAllocationForm
+from onegov.org.forms.directory import DirectoryForm
+from onegov.org.forms.directory import DirectoryImportForm
+from onegov.org.forms.event import EventForm
+from onegov.org.forms.form_definition import FormDefinitionForm
+from onegov.org.forms.form_export import FormSubmissionsExport
+from onegov.org.forms.form_registration import FormRegistrationWindowForm
+from onegov.org.forms.generic import ExportForm, DateRangeForm
+from onegov.org.forms.imageset import ImageSetForm
+from onegov.org.forms.newsletter import NewsletterForm
+from onegov.org.forms.newsletter import NewsletterSendForm
+from onegov.org.forms.newsletter import NewsletterTestForm
+from onegov.org.forms.page import LinkForm, PageForm
+from onegov.org.forms.person import PersonForm
+from onegov.org.forms.reservation import ReservationForm
+from onegov.org.forms.resource import ResourceCleanupForm
+from onegov.org.forms.resource import ResourceExportForm
+from onegov.org.forms.resource import ResourceForm
+from onegov.org.forms.resource_recipient import ResourceRecipientForm
+from onegov.org.forms.settings import SettingsForm
+from onegov.org.forms.signup import SignupForm
+from onegov.org.forms.ticket_note import TicketNoteForm
+from onegov.org.forms.user import ManageUserForm, NewUserForm
+from onegov.org.forms.userprofile import UserProfileForm
+
+
+__all__ = [
+    'DateRangeForm',
+    'DaypassAllocationEditForm',
+    'DaypassAllocationForm',
+    'DirectoryForm',
+    'DirectoryImportForm',
+    'EventForm',
+    'ExportForm',
+    'FormDefinitionForm',
+    'FormRegistrationWindowForm',
+    'FormSubmissionsExport',
+    'ImageSetForm',
+    'LinkForm',
+    'ManageUserForm',
+    'NewsletterForm',
+    'NewsletterSendForm',
+    'NewsletterTestForm',
+    'NewUserForm',
+    'PageForm',
+    'PersonForm',
+    'ReservationForm',
+    'ResourceCleanupForm',
+    'ResourceExportForm',
+    'ResourceForm',
+    'ResourceRecipientForm',
+    'RoomAllocationEditForm',
+    'RoomAllocationForm',
+    'SettingsForm',
+    'SignupForm',
+    'TicketNoteForm',
+    'UserProfileForm',
+]
