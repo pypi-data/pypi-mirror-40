e61b3ed3bb71de451196a284b05b01c4bb2362ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package laomianyang915002/1.0.1/laomianyang915002-1.0.1.tar.gz

---
+++ packages/laomianyang915002/laomianyang915002-1.0.1.tar.gz/laomianyang915002-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 		= 'laomianyang915002',
+	version		= '1.0.1',
+	py_modules	 = ['laomianyang915002'],
+	author 		= 'laomianyang',
+	author_email	 = '289477290@qq.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested list',
+)
