522b76b62b568ac49bb6c441683a8d8779539428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:35 +0000

    Add package proxy-ext/2.0.3/proxy-ext-2.0.3.tar.gz

---
+++ packages/proxy-ext/proxy-ext-2.0.3.tar.gz/proxy-ext-2.0.3/setup.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="proxy-ext",
+    version="2.0.3",
+    author="@neoctobers",
+    author_email="neoctobers@gmail.com",
+    description="A Python proxy extension.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neoctobers/py_proxy_ext",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'list-ext',
+        'file-ext',
+        'ip-query',
+        'xprint',
+    ],
+)
