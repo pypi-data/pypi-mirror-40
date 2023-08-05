96832406179f07399db6cddc67aa36ae5acdeed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:55 +0000

    Add package eventcore-kafka/0.3.3/eventcore-kafka-0.3.3.tar.gz

---
+++ packages/eventcore-kafka/eventcore-kafka-0.3.3.tar.gz/eventcore-kafka-0.3.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name='eventcore-kafka',
+    version='0.3.3',
+    description='Produce and consume events with Kafka.',
+    author='Maikel van den Boogerd',
+    author_email='maikelboogerd@gmail.com',
+    url='https://github.com/maikelboogerd/eventcore-kafka',
+    keywords=['event', 'kafka', 'producer', 'consumer'],
+    packages=['eventcore_kafka'],
+    install_requires=['eventcore', 'confluent-kafka'],
+    license='MIT',
+    zip_safe=False
+)
