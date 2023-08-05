43e30e16c9e532e8aaa263beb879ce286bea3d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package alioth/1.0.5/alioth-1.0.5.tar.gz

---
+++ packages/alioth/alioth-1.0.5.tar.gz/alioth-1.0.5/alioth/lib/core/settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : jeffzhang
+# @Time    : 2018/12/26
+# @File    : settings.py
+# @Desc    : ""
+
+DEFAULT_SCAN_THREATS = 5
+DEFAULT_SCAN_THREAT_MAX = 30
+RE_POC_CHECK = "class\s+(.*?)\(BasePoC\)"
+
