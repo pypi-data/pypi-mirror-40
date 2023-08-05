d10edd7cadf9bf59bbeccf073ea2adaf019ccf57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:50 +0000

    Add package statepub/0.0.0.9/statepub-0.0.0.9.tar.gz

---
+++ packages/statepub/statepub-0.0.0.9.tar.gz/statepub-0.0.0.9/statepub/scripts/publishstatus.py
@@ -0,0 +1,42 @@
+import time
+
+import click
+
+from statepub.state import ComputingState
+from statepub.publish import StatePublisher
+
+
+@click.command(name='publish-status', short_help='Publish the status of this machine to the MQTT network')
+@click.option('--interval', default=2, help='Amount of seconds between two publishes')
+@click.option('--broker_port', default=1883, help='The port on which the MQTT broker operates')
+@click.argument('broker_ip', metavar='<broker ip>')
+@click.argument('topic', metavar='<topic>')
+def cli(interval, broker_port, broker_ip, topic):
+    """
+    After invoking this command, it will repeatedly after set intervals sent the status of this machine to the MQTT
+    network
+    """
+    click.echo('Starting to publish status to broker at "{}::{}"'.format(broker_ip, broker_port))
+
+    publisher = StatePublisher(topic, broker_ip, broker_port=broker_port)
+
+    try:
+        while True:
+            time_seconds = round(time.time())
+            if time_seconds % interval == 0:
+
+                # Actually execute the relevant code for the publishing here
+                computing_state = ComputingState()
+                publisher.publish(computing_state)
+
+            time.sleep(0.05)
+
+    except OSError:
+        click.echo('Couldnt connect to broker')
+
+    except KeyboardInterrupt:
+        publisher.close()
+
+
+if __name__ == '__main__':
+    cli()
