5045c54cb35bd80870c9f9faefea2dd96a3b4358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:39 +0000

    Add package glica/0.4.4rc2/glica-0.4.4rc2.tar.gz

---
+++ packages/glica/glica-0.4.4rc2.tar.gz/glica-0.4.4rc2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+# Note that the version is populated via a CI/CD script and is invalid until populated.
+
+setuptools.setup(
+    name="glica",
+    version="0.4.4-rc2",
+    entry_points={
+        'console_scripts': [
+            'glica = glica.__main__:main',
+        ],
+    },
+    author="Tian Hao Wang",
+    author_email="dev@paced.me",
+    description="GitLab Instant Changelog Assurance (GLICA) for ensuring up-to-date CHANGELOG.md files!",
+    long_description="Please refer to the repository's README.md file for the longer description.",
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/halfbakedstudio/tools/glica",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Version Control :: Git",
+        "Operating System :: OS Independent",
+    ],
+)
