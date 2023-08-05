3d49de590d2c1bfbbc068262314a209cc841e9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:18 +0000

    Add package gpio4/0.0.5/gpio4-0.0.5.tar.gz

---
+++ packages/gpio4/gpio4-0.0.5.tar.gz/gpio4-0.0.5/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="gpio4",
+    version="0.0.5",
+    author="hankso",
+    author_email="hankso1106@gmail.com",
+    description=("Improved gpio module based on Sysfs, "
+                 "features same as RPi.GPIO and gpio3"),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/hankso/gpio4",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: System :: Hardware :: Hardware Drivers"
+    ]
+)
