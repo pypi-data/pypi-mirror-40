2d1ed750622ff13adad318cc88d433e39ead21e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:30 +0000

    Add package searchguard/0.8.1/searchguard-0.8.1.tar.gz

---
+++ packages/searchguard/searchguard-0.8.1.tar.gz/searchguard-0.8.1/searchguard/exceptions.py
@@ -0,0 +1,82 @@
+class SearchGuardException(Exception):
+    pass
+
+
+class CreateUserException(SearchGuardException):
+    pass
+
+
+class UserAlreadyExistsException(CreateUserException):
+    pass
+
+
+class DeleteUserException(SearchGuardException):
+    pass
+
+
+class CreateRoleException(SearchGuardException):
+    pass
+
+
+class RoleAlreadyExistsException(CreateRoleException):
+    pass
+
+
+class DeleteRoleException(SearchGuardException):
+    pass
+
+
+class ViewRoleException(SearchGuardException):
+    pass
+
+
+class ViewUserException(SearchGuardException):
+    pass
+
+
+class ModifyUserException(SearchGuardException):
+    pass
+
+
+class ModifyRoleException(SearchGuardException):
+    pass
+
+
+class ListUsersException(SearchGuardException):
+    pass
+
+
+class CheckUserExistsException(SearchGuardException):
+    pass
+
+
+class CheckRoleExistsException(SearchGuardException):
+    pass
+
+
+class RoleMappingException(SearchGuardException):
+    pass
+
+
+class CreateRoleMappingException(SearchGuardException):
+    pass
+
+
+class CheckRoleMappingExistsException(SearchGuardException):
+    pass
+
+
+class ModifyRoleMappingException(SearchGuardException):
+    pass
+
+
+class ViewAllRoleMappingException(SearchGuardException):
+    pass
+
+
+class ViewRoleMappingException(SearchGuardException):
+    pass
+
+
+class DeleteRoleMappingException(SearchGuardException):
+    pass
---
+++ packages/searchguard/searchguard-0.8.1.tar.gz/searchguard-0.8.1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup, find_packages
+
+project_root = os.path.dirname(__file__)
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+long_description = ''
+
+try:
+    from contextlib import suppress
+    with suppress(OSError):
+        with open(os.path.join(project_root, 'README.md'), 'rt') as readme_fh:
+            long_description = readme_fh.read()
+except ImportError:
+    with open(os.path.join(project_root, 'README.md'), 'rt') as readme_fh:
+        long_description = readme_fh.read()
+
+setup(
+    name='searchguard',
+    version='0.8.1',
+    packages=find_packages(exclude=['tests']),
+    license='MIT',
+    url='https://github.com/ByteInternet/searchguard-python',
+    author='Jeroen van Heugten',
+    author_email='jeroen@byte.nl',
+    classifiers=classifiers,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'requests==2.20'
+    ],
+)
+
