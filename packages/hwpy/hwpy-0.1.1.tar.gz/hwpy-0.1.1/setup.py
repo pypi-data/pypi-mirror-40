52d3371af5e51cefba726a16ea4a0543d30c4f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:23 +0000

    Add package hwpy/0.1.1/hwpy-0.1.1.tar.gz

---
+++ packages/hwpy/hwpy-0.1.1.tar.gz/hwpy-0.1.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: hwinfo
+# Author: Stars Liao
+# Mail: starsliaop@163.com
+# Created Time:  2018-12-24 19:17:34
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "hwpy",
+    version = "0.1.1",
+    keywords = ("pip", "hwinfo","Hardware"),
+    description = "hwpy",
+    long_description = "Show Linux Hardware Info",
+    license = "MIT Licence",
+
+    url = "https://github.com/starsliao/hwpy",
+    author = "Stars Liao",
+    author_email = "starsliao@163.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["psutil"],
+    classifiers=[
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        ],
+)
