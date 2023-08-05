9b5848e3a2fba039ce9b725cd05030918b899724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:55 +0000

    Add package metrovlc/0.5.0/metrovlc-0.5.0.tar.gz

---
+++ packages/metrovlc/metrovlc-0.5.0.tar.gz/metrovlc-0.5.0/metrovlc/__init__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+__author__ = "Pedro Nicolás"
+__copyright__ = "Copyright 2018"
+__license__ = "MIT"
+__version__ = "0.5.0"
+__email__ = "png1981@gmail.com"
+
+from .metrovlc import metrosaldo
+from .metrovlc import metrohorarios
+
+__all__ = ['metrosaldo', 'metrohorarios']
