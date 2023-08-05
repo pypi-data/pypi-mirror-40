7831e6b298e6e4f5b28ebb5b539c9b501b6ac181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:30 +0000

    Add package odoo11-addon-mail-digest/11.0.1.1.0.99.dev3/odoo11_addon_mail_digest-11.0.1.1.0.99.dev3-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-mail-digest/odoo11_addon_mail_digest-11.0.1.1.0.99.dev3-py2.py3-none-any.whl/odoo/addons/mail_digest/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2017-2018 Camptocamp - Simone Orsi
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Mail digest',
+    'summary': """Basic digest mail handling.""",
+    'version': '11.0.1.1.0',
+    'license': 'AGPL-3',
+    'author': 'Camptocamp, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/social',
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        'data/ir_cron.xml',
+        'data/config_param.xml',
+        'security/ir.model.access.csv',
+        'security/record_rules.xml',
+        'views/mail_digest_views.xml',
+        'views/user_notification_views.xml',
+        'views/user_views.xml',
+        'templates/digest_default.xml',
+        'templates/digest_layout_preview.xml',
+    ],
+    'images': [
+        'static/description/preview.png',
+    ]
+}
