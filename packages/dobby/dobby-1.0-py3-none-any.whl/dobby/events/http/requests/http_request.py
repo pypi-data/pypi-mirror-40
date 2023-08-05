92a2a627d175517c70228c951bafae57f2f68635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:25 +0000

    Add package dobby/1.0/dobby-1.0-py3-none-any.whl

---
+++ packages/dobby/dobby-1.0-py3-none-any.whl/dobby/events/http/requests/http_request.py
@@ -0,0 +1,25 @@
+from dobby.events.base.requests.base_request import BaseRequest
+import json
+
+
+class HTTPRequest(BaseRequest):
+    """Http request class"""
+
+    def __init__(self, event, context):
+        print(event)
+        body = json.loads(event['body']) or {}
+        BaseRequest.__init__(self, body)
+
+        self._path_parameters = event['pathParameters']
+        self._queryStringParameters = event['queryStringParameters']
+        self._requestContext = context
+
+    def getPathParameters(self):
+        return self._path_parameters
+
+    def getQueryStringParameters(self):
+        return self._queryStringParameters
+
+    def getRequestContext(self):
+        return self._requestContext
+
