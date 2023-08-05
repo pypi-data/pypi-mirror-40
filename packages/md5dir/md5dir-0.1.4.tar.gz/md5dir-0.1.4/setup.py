c3a32ba58d0c0281be2c0228cab8fba2384805f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:32 +0000

    Add package md5dir/0.1.4/md5dir-0.1.4.tar.gz

---
+++ packages/md5dir/md5dir-0.1.4.tar.gz/md5dir-0.1.4/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import md5dir
+
+setup(
+    name="md5dir",
+    version=md5dir.__version__,
+    packages=find_packages(),
+    author="MachinBrol",
+    author_email="zazor@riseup.net",
+    description="Compute the md5 sum of a directory",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    licence="WTFPL",
+    include_package_data=True,
+    url='http://github.com/machinbrol/md5dir',
+    install_requires=["click"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Natural Language :: French",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'md5dir = md5dir.md5dir:cli',
+        ],
+    },
+)
