311b1f3d01ba3e41a966d1043f3e252036097bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:47 +0000

    Add package go-ml-core/0.1.0.dev9/go-ml-core-0.1.0.dev9.tar.gz

---
+++ packages/go-ml-core/go-ml-core-0.1.0.dev9.tar.gz/go-ml-core-0.1.0.dev9/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+setup(
+        name='go-ml-core',
+        version='0.1.0.dev9',
+	  	description='machine learning core library',
+	  	packages=find_packages(exclude=['tests']),
+		url='http://10.0.1.31/ML/ml-storage/ml-etl/',
+		author='henrychi',
+		author_email='shyuusaku@gmail.com',
+		license='Gogoro Inc.',
+		keywords='machine learning',
+        classifiers = [
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+        ],
+		install_requires=[
+            'beautifulsoup4>=4.6.3',
+			'boto3>=1.9.57',
+			'configparser>=3.5.0',
+			'ordereddict>=1.1',
+			'pymongo>=3.7.2',
+            'requests>=2.20.1'
+		]
+)
