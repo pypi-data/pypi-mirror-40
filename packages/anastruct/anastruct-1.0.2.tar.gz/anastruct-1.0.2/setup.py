55b5d99040d4a77aab63d1d48cfc0a79fc8048a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:54 +0000

    Add package anastruct/1.0.2/anastruct-1.0.2.tar.gz

---
+++ packages/anastruct/anastruct-1.0.2.tar.gz/anastruct-1.0.2/setup.py
@@ -0,0 +1,33 @@
+import sys
+from setuptools import setup
+
+try:
+    from Cython.Build import cythonize
+    em = cythonize(['anastruct/cython/cbasic.pyx', 'anastruct/fem/cython/celements.pyx'])
+except Exception:
+    em = []
+
+if sys.version_info[0] == 3 and sys.version_info[1] < 5:
+    sys.exit('Sorry, Python < 3.5 is not supported')
+
+setup(
+    name='anastruct',
+    version='1.0.2',
+    description='analyse 2D structures.',
+    author='Ritchie Vink',
+    author_email='ritchie46@gmail.com',
+    url='https://ritchievink.com',
+    download_url='https://github.com/ritchie46/anaStruct',
+    license='GPL-3.0',
+    packages=['anastruct', 'anastruct.fem', 'anastruct.fem.system_components', 'anastruct.fem.examples',
+              'anastruct.material', 'anastruct.cython', 'anastruct.fem.cython', 'anastruct.fem.plotter',
+              'anastruct.fem.util'],
+    package_dir='',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scipy'
+    ],
+    ext_modules=em
+
+)
