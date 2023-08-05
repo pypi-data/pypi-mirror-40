9931f3d68c12424e68eb9570603cef43d02cf872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:41 +0000

    Add package cn2num/0.1.0/cn2num-0.1.0.tar.gz

---
+++ packages/cn2num/cn2num-0.1.0.tar.gz/cn2num-0.1.0/README.md
@@ -0,0 +1,20 @@
+## cn2num
+
+这是一个中文数字(含货币)到阿拉伯数字的python小模块。不依赖于其他的python模块。
+
+> 原始版本来自于: [wswuai/cn2num.py](https://github.com/wswuai/cn2num.py)
+
+---
+
+### Installation
+
+```shell
+pip install cn2num --user
+```
+
+### Usage:
+
+```python
+import cn2num
+cn2num.transform("两百三十万")
+```
---
+++ packages/cn2num/cn2num-0.1.0.tar.gz/cn2num-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import codecs
+import os
+import re
+from setuptools import setup
+
+
+with open('README.md') as f:
+	long_description = f.read()
+
+
+setup(name='cn2num',
+		description='A Simple package transform Chinese numerals into Arabic numerals',
+		long_description=long_description,
+		long_description_content_type='text/markdown',
+		version='0.1.0',
+		author='anhenghuang',
+		author_email='contact@huanganheng.com',
+		url='http://github.com/anhenghuang/cn2num',
+		license='MIT',
+		packages=['cn2num'],
+		zip_safe=False,
+		classifiers=(
+			"Programming Language :: Python :: 3",
+			"License :: OSI Approved :: MIT License",
+			"Operating System :: OS Independent",
+		),
+)
