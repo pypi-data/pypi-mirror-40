5a490e18e4987b297a91c9634ceb388f275944e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:27 +0000

    Add package crossplane/0.4.14/crossplane-0.4.14-py2.py3-none-any.whl

---
+++ packages/crossplane/crossplane-0.4.14-py2.py3-none-any.whl/crossplane/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from .parser import parse
+from .lexer import lex
+from .builder import build
+from .ext.lua import LuaBlockPlugin
+
+__all__ = ['parse', 'lex', 'build']
+
+__title__ = 'crossplane'
+__summary__ = 'Reliable and fast NGINX configuration file parser.'
+__url__ = 'https://github.com/nginxinc/crossplane'
+
+__version__ = '0.4.14'
+
+__author__ = 'Arie van Luttikhuizen'
+__email__ = 'aluttik@gmail.com'
+
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018 NGINX, Inc.'
+
+default_enabled_extensions = [LuaBlockPlugin()]
+for extension in default_enabled_extensions:
+    extension.register_extension()
