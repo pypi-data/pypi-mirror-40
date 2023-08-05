06db95c98619f9b92a73170d35c7911f2bf941c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:21 +0000

    Add package BitcoinExchangeFH/2019.1.dev0/BitcoinExchangeFH-2019.1.dev0-py2.py3-none-any.whl

---
+++ packages/BitcoinExchangeFH/BitcoinExchangeFH-2019.1.dev0-py2.py3-none-any.whl/befh/exchange/exchange.py
@@ -0,0 +1,236 @@
+import logging
+from datetime import datetime
+from time import sleep
+
+import ccxt
+from ccxt.base.errors import RequestTimeout, NetworkError
+
+from befh.table.order_book_table import OrderBook
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Exchange:
+    """Exchange.
+    """
+
+    DEFAULT_ORDER_BOOK_CLASS = OrderBook
+    TIMEOUT_TOLERANCE = 5
+    DEFAULT_DEPTH = 5
+
+    def __init__(self, name, config, is_debug, is_cold):
+        """Constructor.
+        """
+        self._name = name
+        self._config = config
+        self._is_debug = is_debug
+        self._is_cold = is_cold
+        self._instruments = {}
+        self._depth = Exchange.DEFAULT_DEPTH
+        self._last_request_time = datetime(1990, 1, 1)
+        self._exchange_interface = None
+        self._handlers = {}
+
+    @classmethod
+    def get_order_book_class(cls):
+        """Get order book class.
+        """
+        return OrderBook
+
+    @property
+    def name(self):
+        """Name.
+        """
+        return self._name
+
+    @property
+    def instruments(self):
+        """Instruments.
+        """
+        return self._instruments
+
+    @property
+    def handlers(self):
+        """Handlers.
+        """
+        return self._handlers
+
+    def load(self, handlers, **kwargs):
+        """Load.
+        """
+        LOGGER.info('Loading exchange %s', self._name)
+        self._load_handlers(handlers=handlers)
+        self._load_instruments()
+        self._load_depth()
+
+    def _load_handlers(self, handlers):
+        """Load handlers.
+        """
+        self._handlers = handlers
+
+    def _load_instruments(self):
+        """Load instruments.
+        """
+        instruments = self._config['instruments']
+        for symbol in instruments:
+            instmt_info = self.DEFAULT_ORDER_BOOK_CLASS(
+                exchange=self._name,
+                symbol=symbol)
+            self._instruments[symbol] = instmt_info
+
+            for handler in self._handlers.values():
+                handler.prepare_create_table(
+                    table_name=instmt_info.table_name,
+                    fields=instmt_info.fields)
+
+    def _load_depth(self):
+        """Load depth.
+        """
+        if 'depth' in self._config:
+            self._depth = self._config['depth']
+            assert isinstance(self._depth, int), (
+                "Depth ({}) must be an integer".format(
+                    self._depth))
+
+
+class RestApiExchange(Exchange):
+    """Rest API exchange.
+    """
+
+    def load(self, **kwargs):
+        """Load.
+        """
+        super().load(**kwargs)
+        self._exchange_interface = getattr(ccxt, self._name.lower())()
+        self._exchange_interface.load_markets()
+        self._check_valid_instrument()
+        self._initialize_instmt_info()
+
+    def run(self):
+        """Run.
+        """
+        while True:
+            for symbol, instmt_info in self._instruments.items():
+                self._update_order_book(
+                    symbol=symbol,
+                    instmt_info=instmt_info)
+
+                if instmt_info.is_possible_trade():
+                    self._update_trades(
+                        symbol=symbol,
+                        instmt_info=instmt_info)
+
+    def _check_valid_instrument(self):
+        """Check valid instrument.
+        """
+        for instrument_code in self._config['instruments']:
+            if instrument_code not in self._exchange_interface.markets:
+                raise RuntimeError(
+                    'Instrument %s is not found in exchange %s',
+                    instrument_code, self._name)
+
+    def _initialize_instmt_info(self):
+        """Initialize instrument info.
+        """
+        for symbol, instmt_info in self._instruments.items():
+            self._update_order_book(
+                symbol=symbol,
+                instmt_info=instmt_info,
+                is_update_handler=False)
+
+            self._update_trades(
+                symbol=symbol,
+                instmt_info=instmt_info,
+                is_update_handler=False)
+
+    def _update_order_book(self, symbol, instmt_info, is_update_handler=True):
+        """Callback order book.
+        """
+        self._load_balance()
+        tolerence_count = 0
+
+        while tolerence_count < self.TIMEOUT_TOLERANCE:
+            try:
+                order_book = self._exchange_interface.fetch_order_book(
+                    symbol=symbol)
+                break
+            except (RequestTimeout, NetworkError) as e:
+                tolerence_count += 1
+                LOGGER.warning('Request timeout %s', e)
+
+        bids = order_book['bids']
+        asks = order_book['asks']
+
+        is_updated = instmt_info.update_bids_asks(
+            bids=bids,
+            asks=asks)
+
+        if not is_updated:
+            return
+
+        if is_update_handler:
+            for handler in self._handlers.values():
+                self._rotate_order_table(handler=handler,
+                                         instmt_info=instmt_info)
+                instmt_info.update_table(handler=handler)
+
+    def _update_trades(self, symbol, instmt_info, is_update_handler=True):
+        """Update trades.
+        """
+        self._load_balance()
+        tolerence_count = 0
+
+        while tolerence_count < self.TIMEOUT_TOLERANCE:
+            try:
+                trades = self._exchange_interface.fetch_trades(symbol=symbol)
+                break
+            except (RequestTimeout, NetworkError) as e:
+                tolerence_count += 1
+                LOGGER.warning('Request timeout %s', e)
+
+        current_timestamp = datetime.utcnow()
+
+        for trade in trades:
+            if not instmt_info.update_trade(trade, current_timestamp):
+                continue
+
+            if is_update_handler:
+                for handler in self._handlers.values():
+                    self._rotate_order_table(handler=handler,
+                                             instmt_info=instmt_info)
+                    instmt_info.update_table(handler=handler)
+
+    @staticmethod
+    def _rotate_order_table(handler, instmt_info):
+        """Rotate order table.
+        """
+        if handler.is_rotate:
+            prev_update_time = (
+                instmt_info._prev_update_time.value.strftime(
+                    handler.rotate_frequency))
+            update_time = (
+                instmt_info._update_time.value.strftime(
+                    handler.rotate_frequency))
+
+            if (prev_update_time != update_time and
+                    instmt_info._prev_update_time.value.year > 2000):
+                # Rotate the table
+                handler.rotate_table(
+                    table=instmt_info,
+                    last_datetime=instmt_info._prev_update_time.value)
+
+    def _load_balance(self):
+        """Load balance.
+        """
+        current_time = datetime.now()
+        time_diff = (current_time -
+                     self._last_request_time).microseconds / 1000.0
+
+        # Rate limit is represented as microseconds,
+        # number of requests per seconds = 1000 / rateLimit
+        if time_diff < self._exchange_interface.rateLimit:
+            wait_second = (
+                self._exchange_interface.rateLimit - time_diff) / 1000.0
+            sleep(wait_second)
+
+        self._last_request_time = datetime.now()
---
+++ packages/BitcoinExchangeFH/BitcoinExchangeFH-2019.1.dev0-py2.py3-none-any.whl/befh/handler/handler.py
@@ -0,0 +1,157 @@
+import logging
+from time import sleep
+
+from .handler_operator import (
+    HandlerOperator,
+    HandlerCreateTableOperator,
+    HandlerInsertOperator,
+    HandlerRenameTableOperator,
+    HandlerCloseOperator,
+)
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Handler:
+    """Handler.
+    """
+
+    def __init__(self, is_debug, is_cold,
+                 is_rotate=False,
+                 rotate_frequency='%Y%m%d',
+                 batch_frequency=1):
+        """Constructor.
+        """
+        self._is_debug = is_debug
+        self._is_cold = is_cold
+        self._is_rotate = is_rotate
+        self._rotate_frequency = rotate_frequency
+        self._batch_frequency = batch_frequency
+        self._is_running = False
+        self._queue = None
+
+    @property
+    def queue(self):
+        """Queue.
+        """
+        return self._queue
+
+    @property
+    def is_rotate(self):
+        """Is rotate.
+        """
+        return self._is_rotate
+
+    @property
+    def rotate_frequency(self):
+        """Rotate frequency.
+        """
+        return self._rotate_frequency
+
+    def load(self):
+        """Load.
+        """
+        LOGGER.info('Loading handler %s', self.__class__.__name__)
+
+    def prepare_create_table(self, table_name, fields, **kwargs):
+        """Prepare create table.
+        """
+        self._queue.put(HandlerCreateTableOperator(
+            table_name=table_name,
+            fields=fields,
+            **kwargs))
+
+    def create_table(self, **kwargs):
+        """Create table.
+        """
+        raise NotImplementedError(
+            'Not implemented on handler %s' %
+            self.__class__.__name__)
+
+    def prepare_insert(self, table_name, fields, **kwargs):
+        """Prepare insert.
+        """
+        self._queue.put(HandlerInsertOperator(
+            table_name=table_name,
+            fields=fields,
+            **kwargs))
+
+    def insert(self, **kwargs):
+        """Insert.
+        """
+        raise NotImplementedError(
+            'Not implemented on handler %s' %
+            self.__class__.__name__)
+
+    def prepare_rename_table(
+            self, from_name, to_name, fields=None,
+            keep_table=True, **kwargs):
+        """Prepare rename table.
+        """
+        self._queue.put(HandlerRenameTableOperator(
+            from_name=from_name,
+            to_name=to_name,
+            fields=fields,
+            keep_table=keep_table,
+            **kwargs))
+
+    def rename_table(self, from_name, to_name, fields=None, keep_table=True):
+        """Rename table.
+        """
+        raise NotImplementedError(
+            'Not implemented on handler %s' %
+            self.__class__.__name__)
+
+    def update_order_book(self, exchange, symbol, bids, asks):
+        """Update order book.
+        """
+        raise NotImplementedError(
+            'Not implemented on exchange %s' %
+            self.__class__.__name__)
+
+    def update_trade(self, exchange, symbol, bids, asks):
+        """Update trades.
+        """
+        raise NotImplementedError(
+            'Not implemented on exchange %s' %
+            self.__class__.__name__)
+
+    def run(self):
+        """Run.
+        """
+        LOGGER.info('Running %s', self.__class__.__name__)
+
+        self._is_running = True
+
+        while self._is_running:
+            while not self._queue.empty():
+                element = self._queue.get()
+                assert isinstance(element, HandlerOperator), (
+                    "Element type is not handler operator (%s)" % (
+                        element.__class__.__name__))
+                try:
+                    element.execute(handler=self)
+                except Exception as e:
+                    if element.allow_fail:
+                        LOGGER.warn(
+                            'Execution failed on element %s (%s)',
+                            element,
+                            str(e))
+                    else:
+                        raise
+
+            sleep(self._batch_frequency)
+
+        LOGGER.info('Completed running  %s', self.__class__.__name__)
+
+    def prepare_close(self):
+        """Close.
+        """
+        LOGGER.debug('Publishing close operator')
+        self._queue.put(HandlerCloseOperator())
+
+    def close(self):
+        """Close.
+        """
+        LOGGER.debug('Publishing close operator')
+        self._is_running = False
