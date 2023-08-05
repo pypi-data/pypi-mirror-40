c2b3ae42c536842b2ac014cdb579fcf45f75daf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:41 +0000

    Add package dsnd-distributions-QianLiu-nd/0.1/dsnd_distributions_QianLiu_nd-0.1.tar.gz

---
+++ packages/dsnd-distributions-QianLiu-nd/dsnd_distributions_QianLiu_nd-0.1.tar.gz/dsnd_distributions_QianLiu_nd-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_QianLiu_nd',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['dsnd_distributions_QianLiu_nd'],
+      author = 'Qian Liu',
+      zip_safe=False)
