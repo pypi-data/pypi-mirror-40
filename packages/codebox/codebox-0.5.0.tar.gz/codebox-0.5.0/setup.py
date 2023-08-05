9b47c96c128200f86851c097760b377d4b4c84c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:47 +0000

    Add package codebox/0.5.0/codebox-0.5.0.tar.gz

---
+++ packages/codebox/codebox-0.5.0.tar.gz/codebox-0.5.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+this_folder = path.abspath(path.dirname(__file__))
+
+# Get package version from .version file
+with open(path.join(this_folder, '.version')) as f:
+    version = f.read()
+
+# Get long description from README.rst file
+with open(path.join(this_folder, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='codebox',
+    version=version,
+    author='Wilson Santos',
+    author_email='wilson@codeminus.org',
+    url='https://gitlab.com/cathaldallan/codebox',
+    description='Python utility code collection',
+    long_description=long_description,
+    license='MIT',
+    keywords='dict, yaml, dir',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[
+        'PyYAML',
+        'jinja2'
+    ],
+)
