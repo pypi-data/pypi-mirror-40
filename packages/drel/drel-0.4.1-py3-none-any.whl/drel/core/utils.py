29d77435f40d1f87adcc15edbe804ff7a89cec7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:28 +0000

    Add package drel/0.4.1/drel-0.4.1-py3-none-any.whl

---
+++ packages/drel/drel-0.4.1-py3-none-any.whl/drel/core/utils.py
@@ -0,0 +1,23 @@
+from typing import Dict
+
+from django.http import HttpRequest
+from marshmallow import Schema, fields
+
+from drel.utils import datetime_to_week_range
+
+
+def handle_es_exception(index: str, doc: Dict, exception: Exception) -> None:
+    raise exception
+
+
+class EmailUserSchema(Schema):
+    email = fields.Email()
+
+
+def get_index_name() -> str:
+    week_start, week_end = datetime_to_week_range()
+    return f"logs-{week_start.date()}-{week_end.date()}"
+
+
+def log_only_post(request: HttpRequest) -> bool:
+    return request.method != "POST"
