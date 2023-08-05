45fc04cf0c9c18dd18da45d24bc673c9a7ff35b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:35 +0000

    Add package PyFunceble-dev/0.139.0/PyFunceble-dev-0.139.0.tar.gz

---
+++ packages/PyFunceble-dev/PyFunceble-dev-0.139.0.tar.gz/PyFunceble-dev-0.139.0/PyFunceble/syntax.py
@@ -0,0 +1,98 @@
+# pylint:disable=line-too-long
+"""
+The tool to check the availability or syntax of domains, IPv4 or URL.
+
+::
+
+
+    ██████╗ ██╗   ██╗███████╗██╗   ██╗███╗   ██╗ ██████╗███████╗██████╗ ██╗     ███████╗
+    ██╔══██╗╚██╗ ██╔╝██╔════╝██║   ██║████╗  ██║██╔════╝██╔════╝██╔══██╗██║     ██╔════╝
+    ██████╔╝ ╚████╔╝ █████╗  ██║   ██║██╔██╗ ██║██║     █████╗  ██████╔╝██║     █████╗
+    ██╔═══╝   ╚██╔╝  ██╔══╝  ██║   ██║██║╚██╗██║██║     ██╔══╝  ██╔══██╗██║     ██╔══╝
+    ██║        ██║   ██║     ╚██████╔╝██║ ╚████║╚██████╗███████╗██████╔╝███████╗███████╗
+    ╚═╝        ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝╚══════╝
+
+This submodule will provide the interface for Syntax testing.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks:
+    https://pyfunceble.readthedocs.io/en/dev/special-thanks.html
+
+Contributors:
+    http://pyfunceble.readthedocs.io/en/dev/special-thanks.html
+
+Project link:
+    https://github.com/funilrys/PyFunceble
+
+Project documentation:
+    https://pyfunceble.readthedocs.io/en/dev/
+
+Project homepage:
+    https://funilrys.github.io/PyFunceble/
+
+License:
+::
+
+
+    MIT License
+
+    Copyright (c) 2017-2018 Nissar Chababy
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+import PyFunceble
+from PyFunceble.check import Check
+from PyFunceble.status import SyntaxStatus
+
+
+class Syntax:  # pragma: no cover pylint: disable=too-few-public-methods
+    """
+    Manage everything around the Syntax testing.
+    """
+
+    @classmethod
+    def get(cls):
+        """
+        Execute the logic behind the Syntax handling.
+
+        :return: The syntax status.
+        :rtype: str
+        """
+
+        if PyFunceble.CONFIGURATION["to_test_type"] == "domain":
+            # We are testing for domain or ip.
+
+            if Check().is_domain_valid() or Check().is_ip_valid():
+                # * The domain is valid.
+                # or
+                # * The IP is valid.
+                return SyntaxStatus(PyFunceble.STATUS["official"]["valid"]).handle()
+        elif PyFunceble.CONFIGURATION["to_test_type"] == "domain":
+            # We are testing for URL.
+
+            if Check().is_url_valid():
+                # * The url is valid.
+
+                return SyntaxStatus(PyFunceble.STATUS["official"]["valid"]).handle()
+        else:
+            raise Exception("Unknow test type.")
+
+        return SyntaxStatus(PyFunceble.STATUS["official"]["invalid"]).handle()
