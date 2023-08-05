1d14f92bfc26496ec7825b9a8986956679e0c753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:34 +0000

    Add package fastapi/0.1.14/fastapi-0.1.14.tar.gz

---
+++ packages/fastapi/fastapi-0.1.14.tar.gz/fastapi-0.1.14/docs/tutorial/bigger-applications.md
@@ -0,0 +1,13 @@
+Coming soon...
+
+```Python
+{!./src/bigger_applications/app/routers/tutorial001.py!}
+```
+
+```Python
+{!./src/bigger_applications/app/routers/tutorial002.py!}
+```
+
+```Python
+{!./src/bigger_applications/app/tutorial003.py!}
+```
---
+++ packages/fastapi/fastapi-0.1.14.tar.gz/fastapi-0.1.14/fastapi/security/http.py
@@ -0,0 +1,42 @@
+from fastapi.openapi.models import (
+    HTTPBase as HTTPBaseModel,
+    HTTPBearer as HTTPBearerModel,
+)
+from fastapi.security.base import SecurityBase
+from starlette.requests import Request
+
+
+class HTTPBase(SecurityBase):
+    def __init__(self, *, scheme: str, scheme_name: str = None):
+        self.model = HTTPBaseModel(scheme=scheme)
+        self.scheme_name = scheme_name or self.__class__.__name__
+
+    async def __call__(self, request: Request) -> str:
+        return request.headers.get("Authorization")
+
+
+class HTTPBasic(HTTPBase):
+    def __init__(self, *, scheme_name: str = None):
+        self.model = HTTPBaseModel(scheme="basic")
+        self.scheme_name = scheme_name or self.__class__.__name__
+
+    async def __call__(self, request: Request) -> str:
+        return request.headers.get("Authorization")
+
+
+class HTTPBearer(HTTPBase):
+    def __init__(self, *, bearerFormat: str = None, scheme_name: str = None):
+        self.model = HTTPBearerModel(bearerFormat=bearerFormat)
+        self.scheme_name = scheme_name or self.__class__.__name__
+
+    async def __call__(self, request: Request) -> str:
+        return request.headers.get("Authorization")
+
+
+class HTTPDigest(HTTPBase):
+    def __init__(self, *, scheme_name: str = None):
+        self.model = HTTPBaseModel(scheme="digest")
+        self.scheme_name = scheme_name or self.__class__.__name__
+
+    async def __call__(self, request: Request) -> str:
+        return request.headers.get("Authorization")
