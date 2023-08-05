6fa6f23aab13ace50d8e952dcad45ab5557eed86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package Telecontrol/1.1/Telecontrol-1.1.tar.gz

---
+++ packages/Telecontrol/Telecontrol-1.1.tar.gz/Telecontrol-1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup #如果没有需要先安装
+setup(name='Telecontrol',  #打包后的包文件名 
+      version='1.1',  #版本
+      description='a Telecontrol',   
+      author=' Mr.yan',   
+      license="Apache-2.0",
+      author_email=' mryan050415@gmail.com',   
+      url=' https://github.com/SuperSystemStudio/TelecontrolCodeRepairwoman',   
+      py_modules=['TelecontrolCodeRepairwoman'],  #与前面的新建文件名一致 
+)
