1aa682d3eb76e8c3ee5278ea2c7e3e206ea6f0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:27 +0000

    Add package pybitmap/0.0.2/pybitmap-0.0.2.tar.gz

---
+++ packages/pybitmap/pybitmap-0.0.2.tar.gz/pybitmap-0.0.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#coding:utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pybitmap",
+    packages=find_packages(),
+    version='0.0.2',
+    description="使用bitmap存储海量整型数据",
+    author="ghostcat_kun",
+    author_email='ghostcat_kun@163.com',
+    url="https://github.com/Alucardmini/basic_algrithms/blob/master/Bit_maps.py",
+    download_url='https://github.com/Alucardmini/basic_algrithms/blob/master/Bit_maps.py',
+    keywords=['bitmap', 'python', 'tool'],
+    classifiers=[],
+    entry_points={
+        'console_scripts': [
+        # 'command3 = adviserserver.run_algorithm:run'
+    ]
+    },
+    install_requires=[
+
+    ]
+)
