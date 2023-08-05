1bfb7cf251663f9a8ef9f6a911c315080fa012e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:07 +0000

    Add package edgepy/0.1.28.dev0/edgepy-0.1.28.dev0-py3-none-any.whl

---
+++ packages/edgepy/edgepy-0.1.28.dev0-py3-none-any.whl/edgepy/Disp.py
@@ -0,0 +1,31 @@
+
+def calcdisp(location, xid, groups, results_name=''):
+    import csv, os, re
+    import pandas as pd
+    from edgepy import util
+    util.getdir()
+
+    if results_name == '':
+        results_name = util.namefromcsv(location)
+
+    if not (os.path.exists(util.getdir.genelib + '/%s.csv' % xid)):
+        print('Need genelib for the organism.')
+    else:
+
+        f = open(util.getdir.workspace + '/runcsv.txt', 'w')
+        f.write(location)
+        f.close()
+
+        with open(util.getdir.workspace + '/groups.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(groups)
+
+        loc = util.getdir.docs
+        util.runR('calcdisp', loc)
+
+        df = pd.read_csv(util.getdir.workspace + '/results.csv')
+        df2 = pd.read_csv(util.getdir.genelib + '/%s.csv' % xid)
+        df.rename(columns = {'Unnamed: 0':'geneNum'}, inplace = True)
+        results = pd.merge(df, df2, how='left', left_on='geneNum', right_on='geneNum')
+        results.to_csv(util.getdir.results + '/%s.csv' % results_name)
+
