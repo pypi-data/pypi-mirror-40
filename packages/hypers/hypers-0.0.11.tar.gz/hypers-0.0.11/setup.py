91005c915535f027779c10a01e4e8a0742207a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:01 +0000

    Add package hypers/0.0.11/hypers-0.0.11.tar.gz

---
+++ packages/hypers/hypers-0.0.11.tar.gz/hypers-0.0.11/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='hypers',
+
+    version='0.0.11',
+
+    packages=['hypers',
+              'hypers._view',
+              'hypers._view._form',
+              'hypers._tools',
+              'hypers._learning'],
+
+    python_requires='>=3.5.0',
+
+    url='https://github.com/priyankshah7/hypers',
+
+    download_url='https://github.com/priyankshah7/hypers/archive/v0.0.11.tar.gz',
+
+    license='BSD 3-Clause',
+
+    author='Priyank Shah',
+
+    author_email='priyank.shah@kcl.ac.uk',
+
+    description='Hyperspectral data analysis and machine learning',
+
+    long_description=LONG_DESCRIPTION,
+
+    long_description_content_type='text/markdown',
+
+    keywords=['hyperspectral',
+              'data-analysis',
+              'clustering',
+              'matrix-decompositions',
+              'hyperspectral-analysis',
+              'machine learning'],
+
+    install_requires=['numpy>=1.14.2',
+                      'matplotlib',
+                      'scipy>=1.1.0',
+                      'pyqt5>=5.10.1',
+                      'pyqtgraph>=0.10.0',
+                      'scikit-learn>=0.20.1']
+)
