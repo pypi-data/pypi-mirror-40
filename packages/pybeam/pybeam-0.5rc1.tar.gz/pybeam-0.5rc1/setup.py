e76d2a45b045935ac3ffad807fb68673098864c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:35 +0000

    Add package pybeam/0.5rc1/pybeam-0.5rc1.tar.gz

---
+++ packages/pybeam/pybeam-0.5rc1.tar.gz/pybeam-0.5rc1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from sphinx.setup_command import BuildDoc
+cmdclass = {'build_sphinx': BuildDoc}
+
+name="pybeam"
+version="0.5-rc1"
+
+setup(name=name,
+	version=version,
+	description='Python module to parse Erlang BEAM files',
+	url='http://github.com/matwey/pybeam',
+	author='Matwey V. Kornilov',
+	author_email='matwey.kornilov@gmail.com',
+	license='MIT',
+	packages=['pybeam'],
+	test_suite='test',
+	install_requires=['construct>=2.9,<2.10', 'six', 'sphinx'],
+	command_options={
+		'build_sphinx': {
+			'project': ('setup.py', name),
+			'version': ('setup.py', version),
+			'release': ('setup.py', version),
+			'source_dir': ('setup.py', 'doc')
+		}
+	},
+	zip_safe=False)
