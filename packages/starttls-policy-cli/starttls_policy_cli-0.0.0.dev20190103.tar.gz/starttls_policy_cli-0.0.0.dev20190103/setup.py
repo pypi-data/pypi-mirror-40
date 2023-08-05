a24e02206c29c5083f135dc8e161cd922c03c230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:03 +0000

    Add package starttls-policy-cli/0.0.0.dev20190103/starttls_policy_cli-0.0.0.dev20190103.tar.gz

---
+++ packages/starttls-policy-cli/starttls_policy_cli-0.0.0.dev20190103.tar.gz/starttls_policy_cli-0.0.0.dev20190103/setup.py
@@ -0,0 +1,71 @@
+import sys
+
+from setuptools import setup
+from setuptools import find_packages
+from os import path
+
+version = '0.0.0.dev20190103'
+
+install_requires = [
+    'datetime',
+    'python-dateutil',
+    'setuptools',
+    'six',
+]
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), 'rb') as f:
+    long_description = f.read().decode('utf-8-sig')
+
+setup(
+    name='starttls_policy_cli',
+    version=version,
+    description="Policy API for STARTTLS Preload",
+    url='https://github.com/EFForg/starttls-everywhere',
+    author="Sydney Li",
+    author_email='sydney@eff.org',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Communications :: Email :: Mail Transport Agents',
+        'Topic :: Security',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        'dev': [
+            'coverage',
+            'mock',
+            'pylint',
+            'pytest',
+            'pytest-cov',
+            # Requirements for proper packing of markdown description:
+            'setuptools>=38.6.0',
+            'wheel>=0.31.0',
+            'twine>=1.11.0',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'starttls-policy-cli = starttls_policy_cli.main:main',
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
+
---
+++ packages/starttls-policy-cli/starttls_policy_cli-0.0.0.dev20190103.tar.gz/starttls_policy_cli-0.0.0.dev20190103/starttls_policy_cli/configure.py
@@ -0,0 +1,131 @@
+"""
+Config generator
+"""
+import sys
+import abc
+import os
+import six
+
+from starttls_policy_cli import constants
+from starttls_policy_cli import policy
+from starttls_policy_cli import util
+
+class ConfigGenerator(object):
+    # pylint: disable=useless-object-inheritance
+    """
+    Generic configuration generator.
+    The two primary public functions:
+        generate()
+        print_instruct()
+    """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, policy_dir):
+        self._policy_dir = policy_dir
+        self._policy_filename = os.path.join(self._policy_dir, constants.POLICY_FILENAME)
+        self._config_filename = os.path.join(self._policy_dir, self.default_filename)
+        self._policy_config = None
+
+    def _load_config(self):
+        if self._policy_config is None:
+            self._policy_config = policy.Config(filename=self._policy_filename)
+            self._policy_config.load()
+        return self._policy_config
+
+    def _write_config(self, result, output):
+        six.print_(result, file=output)
+
+    def _expired_warning(self):
+        """Warns user about policy list expiration.
+        """
+        six.print_("\nACTION REQUIRED: your policy list at {config_location} has expired! "
+                   "Generating empty config.\n"
+                   "Check to see whether your update mechanism "
+                   "(cronjob, systemd timer) is working.\n"
+                       .format(config_location=self._policy_filename),
+                   file=sys.stderr)
+
+    def generate(self):
+        """Generates and dumps MTA configuration file to `policy_dir`.
+        """
+        policy_list = self._load_config()
+        if util.is_expired(policy_list.expires):
+            self._expired_warning()
+            result = self._generate_expired_fallback(policy_list)
+        else:
+            result = self._generate(policy_list)
+        with open(self._config_filename, "w") as config_file:
+            self._write_config(result, config_file)
+
+    def manual_instructions(self):
+        """Prints manual installation instructions to stdout.
+        """
+        six.print_("{line}Manual installation instructions for {mta_name}{line}{instructions}"
+            .format(line="\n" + ("-" * 50) + "\n",
+                    mta_name=self.mta_name,
+                    instructions=self._instruct_string()))
+
+    @abc.abstractmethod
+    def _generate(self, policy_list):
+        """Creates configuration file. Returns a unicode string (text to write to file)."""
+
+    @abc.abstractmethod
+    def _generate_expired_fallback(self, policy_list):
+        """Creates configuration file for expired policy list.
+        Returns a unicode string (text to write to file)."""
+
+    @abc.abstractmethod
+    def _instruct_string(self):
+        """Explain how to install the configuration file that was generated."""
+
+    @abc.abstractproperty
+    def mta_name(self):
+        """The name of the MTA this generator is for."""
+
+    @abc.abstractproperty
+    def default_filename(self):
+        """The expected default filename of the generated configuration file."""
+
+def _policy_for_domain(domain, tls_policy, max_domain_len):
+    line = ("{0:%d} " % max_domain_len).format(domain)
+    if tls_policy.mode == "enforce":
+        line += " secure match="
+        line += ":".join(tls_policy.mxs)
+    elif tls_policy.mode == "testing":
+        line = "# " + line + "undefined due to testing policy"
+    return line
+
+class PostfixGenerator(ConfigGenerator):
+    """Configuration generator for postfix.
+    """
+
+    def _generate(self, policy_list):
+        policies = []
+        max_domain_len = len(max(policy_list, key=len))
+        for domain, tls_policy in sorted(six.iteritems(policy_list)):
+            policies.append(_policy_for_domain(domain, tls_policy, max_domain_len))
+        return "\n".join(policies)
+
+    def _generate_expired_fallback(self, policy_list):
+        return "# Policy list is outdated. Falling back to opportunistic encryption."
+
+    def _instruct_string(self):
+        filename = self._config_filename
+        abs_path = os.path.abspath(filename)
+        return ("\nFirst, run:\n\n"
+            "postmap {abs_path}\n\n"
+            "Then, you'll need to point your Postfix configuration to {filename}.\n"
+            "Check if `postconf smtp_tls_policy_maps` includes this file.\n"
+            "If not, run:\n\n"
+            "postconf -e \"smtp_tls_policy_maps=$(postconf -h smtp_tls_policy_maps)"
+            " hash:{abs_path}\"\n\n"
+            "And finally:\n\n"
+            "postfix reload\n").format(abs_path=abs_path, filename=filename)
+
+    @property
+    def mta_name(self):
+        return "Postfix"
+
+    @property
+    def default_filename(self):
+        return "postfix_tls_policy"
