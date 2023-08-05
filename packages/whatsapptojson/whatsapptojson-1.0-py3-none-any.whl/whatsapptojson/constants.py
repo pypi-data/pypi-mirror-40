fb1516e1178ef006f180783578c49a4965d3699f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:44 +0000

    Add package whatsapptojson/1.0/whatsapptojson-1.0-py3-none-any.whl

---
+++ packages/whatsapptojson/whatsapptojson-1.0-py3-none-any.whl/whatsapptojson/constants.py
@@ -0,0 +1,14 @@
+devices = {
+    'iphone': {
+        'date_format': '%d/%m/%y %I:%M:%S %p',
+        'delimeter_format': ']|: ',
+        'attachment_tag': '<\u200eattached>',
+        'attachment_delimeters': ' • ‎| <\u200eattached>'
+    },
+    'android': {
+        'date_format': '%m/%d/%y %I:%M %p',
+        'delimeter_format': ' - |: ',
+        'attachment_tag': '(file attached)',
+        'attachment_delimeters': '\(file attached\)'
+    }
+}
