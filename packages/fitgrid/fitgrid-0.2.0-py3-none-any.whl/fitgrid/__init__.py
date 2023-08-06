528e24e055fc34543c3dc16acfe5ea4162bb31c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:04 +0000

    Add package fitgrid/0.2.0/fitgrid-0.2.0-py3-none-any.whl

---
+++ packages/fitgrid/fitgrid-0.2.0-py3-none-any.whl/fitgrid/__init__.py
@@ -0,0 +1,46 @@
+from .fake_data import generate
+from .io import epochs_from_hdf, epochs_from_dataframe, load_grid
+from .models import run_model, lm, lmer
+from . import utils
+
+#: default epoch identifier
+EPOCH_ID = 'Epoch_idx'
+
+#: default time identifier
+TIME = 'Time'
+
+#: default channel list
+CHANNELS = [
+    'lle',
+    'lhz',
+    'MiPf',
+    'LLPf',
+    'RLPf',
+    'LMPf',
+    'RMPf',
+    'LDFr',
+    'RDFr',
+    'LLFr',
+    'RLFr',
+    'LMFr',
+    'RMFr',
+    'LMCe',
+    'RMCe',
+    'MiCe',
+    'MiPa',
+    'LDCe',
+    'RDCe',
+    'LDPa',
+    'RDPa',
+    'LMOc',
+    'RMOc',
+    'LLTe',
+    'RLTe',
+    'LLOc',
+    'RLOc',
+    'MiOc',
+    'A2',
+    'HEOG',
+    'rle',
+    'rhz',
+]
