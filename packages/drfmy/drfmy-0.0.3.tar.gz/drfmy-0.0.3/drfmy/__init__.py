196cae99509507a81f3844f0aa44ed3f43d30c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:12 +0000

    Add package drfmy/0.0.3/drfmy-0.0.3.tar.gz

---
+++ packages/drfmy/drfmy-0.0.3.tar.gz/drfmy-0.0.3/drfmy/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+from .DimReduction import MVR
+all = ['MVR']
---
+++ packages/drfmy/drfmy-0.0.3.tar.gz/drfmy-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'drfmy',
+    version = '0.0.3',
+    description='dimensionality reduction',
+    packages = ['drfmy'],
+    author = 'franckmy',
+    author_email='jsh127@163.com',
+    license='',
+)
