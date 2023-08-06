beb4be546418c262acd0af4ec7c04b0735cd0e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:19 +0000

    Add package inspire-classifier/0.1.12/inspire-classifier-0.1.12.tar.gz

---
+++ packages/inspire-classifier/inspire-classifier-0.1.12.tar.gz/inspire-classifier-0.1.12/inspire_classifier/utils.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2014-2018 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+#
+# Modified from the fastai library (https://github.com/fastai/fastai).
+
+from fastai.text import (
+    num_cpus,
+    Tokenizer
+)
+from flask import current_app
+from pathlib import Path
+from concurrent.futures import ProcessPoolExecutor
+import re
+from spacy.lang.en import English
+from spacy.symbols import ORTH
+
+
+def path_for(name):
+    base_path = Path(current_app.config.get('CLASSIFIER_BASE_PATH') or current_app.instance_path)
+    config_key = f'CLASSIFIER_{name}_PATH'.upper()
+
+    return base_path / current_app.config[config_key]
+
+
+class FastLoadTokenizer(Tokenizer):
+    """
+    Tokenizer which avoids redundant loading of spacy language model
+
+    The FastAI Tokenizer class loads all the pipeline components of the spacy model which significantly increases
+    loading time, especially when doing inference on CPU. This class inherits from the FastAI Tokenizer and is
+    refactored to avoid redundant loading of the classifier.
+    """
+    def __init__(self):
+        self.re_br = re.compile(r'<\s*br\s*/?>', re.IGNORECASE)
+        self.tok = English()
+        for w in ('<eos>', '<bos>', '<unk>'):
+            self.tok.tokenizer.add_special_case(w, [{ORTH: w}])
+
+    def proc_all(self, ss):
+        return [self.proc_text(s) for s in ss]
+
+    def proc_all_mp(self, ss, ncpus=None):
+        ncpus = ncpus or num_cpus() // 2
+        with ProcessPoolExecutor(ncpus) as executor:
+            return sum(executor.map(self.proc_all, ss), [])
