15580791604c6debcc12aee11f4585633d574899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:01 +0000

    Add package pylibgen/2.0.1/pylibgen-2.0.1.tar.gz

---
+++ packages/pylibgen/pylibgen-2.0.1.tar.gz/pylibgen-2.0.1/setup.py
@@ -0,0 +1,52 @@
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+version = "2.0.1"
+url = "https://github.com/JoshuaRLi/pylibgen"
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+with open(os.path.join(here, "requirements.txt")) as f:
+    install_requires = f.read()
+
+setup(
+    name="pylibgen",
+    version=version,
+    description="Python interface to Library Genesis.",
+    long_description=long_description,
+    license="MIT",
+    url=url,
+    download_url="{}/archive/v{}.tar.gz".format(url, version),
+    author="Joshua Li",
+    author_email="josh@jrl.ninja",
+    maintainer="Joshua Li",
+    maintainer_email="josh@jrl.ninja",
+    keywords=[
+        "libgen",
+        "library",
+        "genesis",
+        "search",
+        "download",
+        "books",
+        "ebooks",
+        "textbooks",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
