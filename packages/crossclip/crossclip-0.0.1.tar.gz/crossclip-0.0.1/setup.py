779baaff278a3e5479266ab8d1ee353cd08f4ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:57 +0000

    Add package crossclip/0.0.1/crossclip-0.0.1.tar.gz

---
+++ packages/crossclip/crossclip-0.0.1.tar.gz/crossclip-0.0.1/setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="crossclip",
+    version="0.0.1",
+    author="Charlie Sale",
+    author_email="softwaresale01@gmail.com",
+    description="A Cross Platform clipboard manager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/softwaresale/crossclip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
