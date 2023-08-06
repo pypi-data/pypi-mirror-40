a81f444c051c0f8341375bc2d3050eb826639909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:53 +0000

    Add package magestore-aim/1.2.1/magestore_aim-1.2.1.tar.gz

---
+++ packages/magestore-aim/magestore_aim-1.2.1.tar.gz/magestore_aim-1.2.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+package_name = 'magestore_aim'
+packages = setuptools.find_packages(include=[package_name, "{}.*".format(package_name)])
+
+setuptools.setup(
+    name=package_name,
+    version="1.2.1",
+    author="Mars",
+    author_email="mars@trueplus.vn",
+    description="New connection type to HOST machine, using private key.\n"
+                "Get magento source from github repo.",
+    long_description=long_description,
+    long_description_content_type="",
+    url="https://github.com/Magestore/go-environment",
+    packages=packages,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3',
+    install_requires=[
+       'fabric'
+    ]
+)
