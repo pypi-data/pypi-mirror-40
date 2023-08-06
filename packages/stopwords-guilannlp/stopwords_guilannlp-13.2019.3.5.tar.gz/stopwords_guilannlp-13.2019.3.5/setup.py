730290aa9b11afb9da5a151510bab72c1ce39829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:23 +0000

    Add package stopwords-guilannlp/13.2019.3.5/stopwords_guilannlp-13.2019.3.5.tar.gz

---
+++ packages/stopwords-guilannlp/stopwords_guilannlp-13.2019.3.5.tar.gz/stopwords_guilannlp-13.2019.3.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stopwords_guilannlp",
+    version=__import__("stopwords_guilannlp").stopword_version() + "." + "3.5",
+    author="Javad PourMostafa",
+    author_email="javad.pourmostafa@gmail.com",
+    description="A comprehensive package for stopwords in NLP and text mining",
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    url="https://github.com/joyebright/stopwords_guilannlp",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    package_data={
+        'stopwords_guilannlp': [
+            'Languages/*.csv'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing",
+        "Topic :: Utilities"
+    ],
+)
