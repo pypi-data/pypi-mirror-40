8b958d1af0eb4e7e30c63add3cef51b6dd679858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:55 +0000

    Add package sappgen/1.0.8/sappgen-1.0.8.tar.gz

---
+++ packages/sappgen/sappgen-1.0.8.tar.gz/sappgen-1.0.8/sappgen/__init__.py
@@ -0,0 +1,11 @@
+"""sappgen - Simple App Generator Python
+
+    >>> sappgen [options] <project_name> <app_name>
+
+See https://github.com/aayushuppal/sappgen for more information
+"""
+
+# Version of sappgen package
+__version__ = "1.0.8"
+
+from .sappgen import process, main
