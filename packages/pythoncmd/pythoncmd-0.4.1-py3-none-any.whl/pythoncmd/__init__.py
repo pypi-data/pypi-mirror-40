8197b9dcc2bfbb8390808de635e444057da70e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:27 +0000

    Add package pythoncmd/0.4.1/pythoncmd-0.4.1-py3-none-any.whl

---
+++ packages/pythoncmd/pythoncmd-0.4.1-py3-none-any.whl/pythoncmd/__init__.py
@@ -0,0 +1,14 @@
+import os,sys
+class __main__:
+    def __getattribute__(self,attr):
+        class _cmd:
+            def __init__(self,command):
+                self.cmd=command
+            def __call__(self,*args):
+                if self.cmd=='cd':
+                    return os.chdir(*args[0])
+                return os.system(self.cmd+' '+(' '.join(args)))
+            def __repr__(self):
+                return '<cmd object command='+self.cmd+'>'
+        return _cmd(attr)
+sys.modules[__name__]=__main__()
