6aa5b6e9338f16f3169f5a69552f377d2a9f6060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:29 +0000

    Add package tcplite/0.0.3/tcplite-0.0.3.tar.gz

---
+++ packages/tcplite/tcplite-0.0.3.tar.gz/tcplite-0.0.3/setup.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+PACKAGE_NAME = 'tcplite'
+
+EXTRAS = {}
+REQUIRES = []
+with open('requirements.txt') as f:
+    for line in f:
+        line, _, _ = line.partition('#')
+        line = line.strip()
+        if ';' in line:
+            requirement, _, specifier = line.partition(';')
+            for_specifier = EXTRAS.setdefault(':{}'.format(specifier), [])
+            for_specifier.append(requirement)
+        else:
+            REQUIRES.append(line)
+
+with open('test-requirements.txt') as f:
+    TESTS_REQUIRES = f.readlines()
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=(
+        'a lite, flexible and extendable tcp framework'
+    ),
+    long_description=open('README.rst').read(),
+    author='Vincent',
+    author_email='wang.yuanqiu007@gmail.com',
+    maintainer='Vincent',
+    maintainer_email='wang.yuanqiu007@gmail.com',
+    license='MIT License',
+    packages=['tcplite'],
+    platforms=['all'],
+    url='https://github.com/Vincent0700/tcplite.git',
+    install_requires=REQUIRES,
+    tests_require=TESTS_REQUIRES,
+    extras_require=EXTRAS,
+)
