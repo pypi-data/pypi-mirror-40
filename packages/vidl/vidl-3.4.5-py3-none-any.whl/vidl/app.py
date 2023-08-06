7c6f01edf677cfa89f45eaf2ef5cb9f8a4164592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:26 +0000

    Add package vidl/3.4.5/vidl-3.4.5-py3-none-any.whl

---
+++ packages/vidl/vidl-3.4.5-py3-none-any.whl/vidl/app.py
@@ -0,0 +1,52 @@
+from colorboy import green, cyan, red
+from pathlib import Path
+
+package_name = 'vidl' # used for getting config location and package version
+package_author = 'Kasper Henningsen' # used for getting config location
+
+def log(*args, error=False, quiet=False, callback=None, quit=True, **named_args):
+    vidl_text = cyan('[vidl]')
+    if error:
+        print(vidl_text, red('Error:'), *args, **named_args)
+    elif quiet == False:
+        print(vidl_text, *args, **named_args)
+
+script_filename = "vidl"
+# used to be sys.argv[0], but Poetry changes sys.argv[0] to the full path
+def vidl_help():
+    print(          '')
+    print(green(    'Download Usage:'))
+    print(          '    '+cyan(script_filename)+' [format] [options] <URL>')
+    print(          '')
+    print(green(    'Download Options:'))
+    print(cyan(     '    format             ')+'mp3, mp4, wav or m4a. Default mp3.')
+    print(cyan(     '    --no-md            ')+'Don\'t add metadata to downloaded files.')
+    print(cyan(     '    --no-smart-md      ')+'Don\'t extract artist and song name from title.')
+    print(cyan(     '    --no-dl            ')+'Don\'t download anything. Usually used with -v')
+    print(cyan(     '    -v, --verbose      ')+'Display all logs.')
+    print(          '')
+    print(green(    'Global Options:'))
+    print(cyan(     '    --version          ')+'Display vidl version. "vidl -v" also works.')
+    print(cyan(     '    -h, --help         ')+'Display this help message.')
+    print(cyan(     '    --config-path      ')+'Display the location of vidl\'s configuration file.')
+    print(cyan(     '')
+    print(cyan(     'Update vidl:')
+    print(cyan(     '    pip install vidl --upgrade --upgrade-strategy eager')
+    print(          '')
+    quit()
+
+def main():
+    import sys
+    if len(sys.argv) <= 1 or '--help' in sys.argv or '-h' in sys.argv:
+        vidl_help()
+    elif '--version' in sys.argv or sys.argv[1:] == ['-v']:
+        import vidl.version
+        log("Version", vidl.version.get_package_version())
+    elif '--config-path' in sys.argv:
+        import vidl.config
+        log("Config path:", vidl.config.config_path)
+    else:
+        from vidl import dl
+        dl.main()
+if __name__ == '__main__':
+    main()
---
+++ packages/vidl/vidl-3.4.5-py3-none-any.whl/vidl/dl.py
@@ -0,0 +1,202 @@
+import sys, os, copy
+import pprint; pprint = pprint.PrettyPrinter(indent=4).pprint
+import youtube_dl
+from colorboy import cyan, green
+from deep_filter import deep_filter
+
+from vidl import app, config
+from vidl.app import log
+
+def main():
+
+    options = {
+        'url': '',
+        'file_format': 'mp3',
+        'audio_only': True,
+        'no_md': False,
+        'no_smart_md': False,
+        'no_dl': False,
+        'verbose': False,
+        'download_folder': config.get_config('download_folder'),
+        'output_template': config.get_config('output_template'),
+    }
+    if options['download_folder'] == None:
+        log('download_folder config has not been set. Add a download folder to the vidl config file.', error=True)
+        log("Config path:", config.config_path)
+        quit()
+
+    video_formats = ['mp4']
+    audio_formats = ['mp3', 'wav', 'm4a']
+    id3_metadata_formats = ['mp3']
+    ytdl_output_template = os.path.join(options['download_folder'], options['output_template'])
+
+    # parse arguments
+    for arg in sys.argv[1:]:
+        if arg in audio_formats:
+            options['audio_only'] = True
+            options['file_format'] = arg
+        elif arg in video_formats:
+            options['audio_only'] = False
+            options['file_format'] = arg
+        elif arg in ['--no-md']:
+            options['no_md'] = True
+        elif arg in ['--no-smart-md']:
+            options['no_smart_md'] = True
+        elif arg in ['--no-dl']:
+            options['no_dl'] = True
+        elif arg in ['-v', '--verbose']:
+            options['verbose'] = True
+        elif '.' in arg:
+            options['url'] = arg
+        else:
+            log('Unknown argument:', arg, error=True)
+            quit()
+    if options['url'] == '':
+        log('No URL provided', error=True)
+        quit()
+
+    # get info
+    log('Fetching URL info')
+    ytdl_get_info_options = {
+        'outtmpl': ytdl_output_template,
+        'quiet': False if options['verbose'] else True,
+    }
+    with youtube_dl.YoutubeDL(ytdl_get_info_options) as ytdl:
+        try:
+            info_result = ytdl.extract_info(options['url'], download=False)
+        except:
+            quit()
+        if options['verbose']:
+            pprint(info_result)
+
+    # delete None properties/indexes
+    def callback(value):
+        return value != None
+    cleaned_info_result = deep_filter(copy.deepcopy(info_result), callback)
+
+    # restructure
+    url_info = copy.deepcopy(cleaned_info_result)
+    if 'entries' in cleaned_info_result:
+        videos = cleaned_info_result['entries']
+        playlist_info = copy.deepcopy(cleaned_info_result)
+        del playlist_info['entries']
+    else:
+        videos = [cleaned_info_result]
+        playlist_info = {}
+
+    # generate ytdl arguments
+    ytdl_args = []
+    if options['audio_only']:
+        ytdl_args += ['-x']
+        ytdl_args += ['-f', 'best']
+        ytdl_args += ['--audio-format', options['file_format']]
+    else:
+        ytdl_args += ['-f', 'bestvideo+bestaudio']
+        ytdl_args += ['--recode-video', options['file_format']]
+    ytdl_args += ['--audio-quality', '0']
+    ytdl_args += ['-o', ytdl_output_template]
+    if options['file_format'] in ['mp3', 'm4a', 'mp4']:
+        ytdl_args += ['--embed-thumbnail']
+    if not options['verbose']:
+        ytdl_args += ['--quiet']
+    # ytdl_args += [options['url']]
+
+    video_index = -1
+    for video in videos:
+        video_index += 0
+        try:
+            filename = ytdl.prepare_filename(video)
+        except:
+            quit()
+        filename_split = filename.split('.')
+        filename_split[len(filename_split)-1] = options['file_format']
+        filename = '.'.join(filename_split)
+        if options['verbose']:
+            log(green('youtube-dl command:'), 'youtube-dl', ' '.join(ytdl_args+[video['webpage_url']]))
+        if options['no_dl']:
+            continue
+        log('Downloading')
+        try:
+            youtube_dl.main(ytdl_args+[video['webpage_url']])
+        except:
+            pass
+        log('Saved as', filename)
+        if options['file_format'] in id3_metadata_formats and not options['no_md']:
+            log('Adding metadata to file')
+
+            # get artist/title from title
+            parsed_title = {}
+            if not options['no_smart_md']:
+                if 'title' in video and video['title'].count(' - ') == 1:
+                    split_title = video['title'].split(' - ')
+                    parsed_title['artist'] = split_title[0]
+                    parsed_title['title'] = split_title[1]
+
+            md = {}
+            playlist = True if len(videos) > 1 else False
+
+            # title
+            if 'title' in parsed_title:
+                smart_title = True
+            else:
+                smart_title = False
+            if 'title' in video:
+                md['title'] = video['title']
+            elif 'track' in video:
+                md['title'] = video['track']
+            # artist
+            if 'artist' in parsed_title:
+                smart_artist = True
+            else:
+                smart_artist = False
+            if 'uploader' in video:
+                md['artist'] = video['uploader']
+            elif 'artist' in video:
+                md['artist'] = video['artist']
+
+            if playlist:
+                #album
+                if 'title' in playlist_info:
+                    md['album'] = playlist_info['title']
+                elif 'playlist_title' in video:
+                    md['album'] = video['playlist_title']
+                elif 'playlist' in video and type(video['playlist']) == str:
+                    md['album'] = video['playlist']
+                #album_artist
+                if 'uploader' in playlist_info:
+                    md['album_artist'] = playlist_info['uploader']
+                elif 'playlist_uploader' in video:
+                    md['album_artist'] = video['playlist_uploader']
+                # track_number
+                if 'playlist_index' in video:
+                    md['track_number'] = video['playlist_index']
+                else:
+                    md['track_number'] = video_index+1
+                # track_count
+                if 'n_entries' in video:
+                    md['track_count'] = video['n_entries']
+                else:
+                    md['track_count'] = len(videos)
+            # year
+            def is_int(number):
+                try:
+                    int(number)
+                    return True
+                except ValueError:
+                    return False
+            if 'release_date' in video and is_int(video['release_date'][:4]):
+                md['year'] = video['release_date'][:4]
+            elif 'publish_date' in video and is_int(video['publish_date'][:4]):
+                md['year'] = video['publish_date'][:4]
+            elif 'upload_date' in video and is_int(video['upload_date'][:4]):
+                md['year'] = video['upload_date'][:4]
+            
+            dumb_md = copy.deepcopy(md)
+            if smart_title: md['title'] = parsed_title['title']
+            if smart_artist: md['artist'] = parsed_title['artist']
+
+            md = config.user_md_parser(md, dumb_md, video, url_info)
+            
+            from vidl import md as md_module
+            md_module.add_metadata(filename, md)
+    log('Done')
