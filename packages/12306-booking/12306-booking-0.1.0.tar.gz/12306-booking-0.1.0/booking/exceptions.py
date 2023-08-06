2cc25145cc730fb5bd88d02f5190aee433d0d94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:46 +0000

    Add package 12306-booking/0.1.0/12306-booking-0.1.0.tar.gz

---
+++ packages/12306-booking/12306-booking-0.1.0.tar.gz/12306-booking-0.1.0/booking/exceptions.py
@@ -0,0 +1,38 @@
+# encoding: utf8
+"""
+exceptions.py
+@author Meng.yangyang
+@description 异常
+@created Mon Jan 07 2019 17:48:48 GMT+0800 (CST)
+@last-modified Wed Jan 09 2019 22:37:07 GMT+0800 (CST)
+"""
+
+
+class BookingBaseException(Exception):
+    """
+    订票异常
+    """
+
+
+class BookingOrderNoExists(BookingBaseException):
+    """
+    订单不存在
+    """
+
+
+class BookingTrainNoLeftTicket(BookingBaseException):
+    """
+    无票
+    """
+
+
+class BookingOrderQueryTimeOut(BookingBaseException):
+    """
+    订单查询超时
+    """
+
+
+class BookingOrderCancelExceedLimit(BookingBaseException):
+    """
+    订单取消次数超限
+    """
