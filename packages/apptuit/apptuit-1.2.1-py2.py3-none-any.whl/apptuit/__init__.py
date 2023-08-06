8a3bcff844d07f979b04e29cb6b4c3e4a5e10875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:27 +0000

    Add package apptuit/1.2.1/apptuit-1.2.1-py2.py3-none-any.whl

---
+++ packages/apptuit/apptuit-1.2.1-py2.py3-none-any.whl/apptuit/__init__.py
@@ -0,0 +1,11 @@
+APPTUIT_PY_TOKEN = "APPTUIT_PY_TOKEN"
+APPTUIT_PY_TAGS = "APPTUIT_PY_TAGS"
+
+from .apptuit_client import Apptuit, DataPoint, ApptuitException, ApptuitSendException, \
+    TimeSeriesName, TimeSeries
+from apptuit import pyformance, timeseries
+
+__version__ = '1.2.1'
+
+__all__ = ['Apptuit', 'DataPoint', 'ApptuitException', 'TimeSeriesName', 'TimeSeries',
+           'pyformance', 'timeseries', 'ApptuitSendException']
