2cec37cb2f06c7da4804e82e29d5261b46e9d34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:11 +0000

    Add package robotframework-salabsutils/0.0.4/robotframework-salabsutils-0.0.4.tar.gz

---
+++ packages/robotframework-salabsutils/robotframework-salabsutils-0.0.4.tar.gz/robotframework-salabsutils-0.0.4/src/salabsutils/utils.py
@@ -0,0 +1,34 @@
+try:
+    from .base_class import DynamicRobotApiClass
+except:
+    from base_class import DynamicRobotApiClass
+
+from robot.api.deco import keyword
+from furl import furl
+from http.cookies import SimpleCookie
+
+
+class SalabsUtils(DynamicRobotApiClass):
+    def __init__(self):
+        pass
+
+    @keyword("Add Basic Authentication To Url")
+    def add_authentication(self, url, l, p):
+        data = furl(url)
+        data.username = l
+        data.password = p
+        return data.tostr()
+
+    @keyword
+    def split_url_to_host_and_path(self, url):
+        data = furl(url)
+        return {'base': str(data.copy().remove(path=True)), 'path': str(data.path)}
+
+    @keyword
+    def cookies_to_dict(self, cookies):
+        ret = {}
+        cookie = SimpleCookie()
+        cookie.load(cookies)
+        for key, morsel in cookie.keys():
+            ret[key] = morsel.value
+        return ret
