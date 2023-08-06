2866ad2ddeac670d0198532a98b0d35901d60a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:52 +0000

    Add package sparkflow/0.6.0/sparkflow-0.6.0.tar.gz

---
+++ packages/sparkflow/sparkflow-0.6.0.tar.gz/sparkflow-0.6.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='sparkflow',
+      version='0.6.0',
+      description='Deep learning on Spark with Tensorflow',
+      keywords = ['tensorflow', 'spark', 'sparkflow', 'machine learning', 'lifeomic', 'deep learning'],
+      url='https://github.com/lifeomic/sparkflow',
+      download_url='https://github.com/lifeomic/sparkflow/archive/0.6.0.tar.gz',
+      author='Derek Miller',
+      author_email='dmmiller612@gmail.com',
+      install_requires=['tensorflow', 'flask', 'protobuf', 'requests'],
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
