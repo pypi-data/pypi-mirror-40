f8e6ab1049e6f4bcf59795abb5a679feb2edee46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:33 +0000

    Add package zeam.form.base/1.3.2/zeam.form.base-1.3.2.tar.gz

---
+++ packages/zeam.form.base/zeam.form.base-1.3.2.tar.gz/zeam.form.base-1.3.2/src/zeam/form/base/sorters.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import operator
+
+def sort_components(components, order, reverse=False):
+    if not isinstance(order, list) and not isinstance(order, tuple):
+        raise ValueError(
+            "Please provide a valid list or tuple of component identifiers.")
+    if len(order) < 2:
+        raise ValueError(
+            "Please provide a list of, at least, two component identifiers.")
+
+    def compare(c1, c2, order=order):
+
+        try:
+            o1 = order.index(c1)
+        except ValueError:
+            return 1
+
+        try:
+            o2 = order.index(c2)
+        except ValueError:
+            return -1
+
+        if o1 - o2 > 1:
+            return 1
+        if o1 - o2 < 1:
+            return -1
+
+        return 0
+
+    return components.sort(
+        cmp=compare,
+        key=operator.attrgetter('identifier'),
+        reverse=reverse)
