640ce4049b410e086411e9227614fb56ec754810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:08 +0000

    Add package add-parent-path/0.1.1/add_parent_path-0.1.1-py3-none-any.whl

---
+++ packages/add-parent-path/add_parent_path-0.1.1-py3-none-any.whl/add_parent_path/__init__.py
@@ -0,0 +1,19 @@
+"""add_parent_path - """
+
+__version__ = '0.1.0'
+__author__ = 'fx-kirin <fx.kirin@gmail.com>'
+__all__ = []
+
+import os
+import sys
+import inspect
+
+
+def add_parent_path(count=1):
+    caller_file_path = os.path.abspath(inspect.stack()[1].filename)
+    import_file_path = os.path.dirname(caller_file_path)
+    for _ in range(count):
+        import_file_path = os.path.dirname(import_file_path)
+
+    if import_file_path not in sys.path:
+        sys.path.append(import_file_path)
