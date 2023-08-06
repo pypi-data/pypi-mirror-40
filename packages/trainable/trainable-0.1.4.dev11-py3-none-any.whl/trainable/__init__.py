56c65c9d4f1c91853b7cff30cbc2a3123c3e0576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:04 +0000

    Add package trainable/0.1.4.dev11/trainable-0.1.4.dev11-py3-none-any.whl

---
+++ packages/trainable/trainable-0.1.4.dev11-py3-none-any.whl/trainable/__init__.py
@@ -0,0 +1,7 @@
+from .algorithm import Algorithm
+from .epoch import Epoch, DefaultEpoch
+from .progress import ProgressManager
+from .session import Session, AutoSession
+from .trainer import Trainer
+from .validate import ValidationManager
+from .visualize import Visualizer, Plotter, Saver, PlotterSaver
