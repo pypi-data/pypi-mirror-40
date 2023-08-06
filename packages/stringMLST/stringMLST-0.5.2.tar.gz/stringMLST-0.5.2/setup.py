2e192e08533c6fb7df0031c90549a1f2eed4669f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:00 +0000

    Add package stringMLST/0.5.2/stringMLST-0.5.2.tar.gz

---
+++ packages/stringMLST/stringMLST-0.5.2.tar.gz/stringMLST-0.5.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+try:
+  import os
+  from setuptools import setup, find_packages
+except ImportError:
+  from distutils.core import setup
+from os import path
+here = path.abspath(path.dirname(__file__))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'stringMLST',
+  scripts = ['stringMLST.py'], 
+  version = '0.5.2',
+  description = 'Fast k-mer based tool for alignment and assembly-free multi locus sequence typing (MLST) directly from genome sequencing reads.',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Jordan Lab',
+  author_email = 'pypi@atc.io',
+  url = 'https://github.com/jordanlab/stringMLST',
+  keywords = ['MLST', 'kmer', "NGS", "stringMSLT"], 
+  classifiers = [
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3.5',
+  ],
+  install_requires=['lxml','pyfaidx'],
+)
