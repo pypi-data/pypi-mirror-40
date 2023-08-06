ad0ce90fc9b51d1616fc599ce57bae2930dba564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:05 +0000

    Add package oci-cli/2.4.41/oci_cli-2.4.41-py2.py3-none-any.whl

---
+++ packages/oci-cli/oci_cli-2.4.41-py2.py3-none-any.whl/oci_cli/__init__.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+# Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
+
+import os
+from . import fips
+
+# This needs to be done prior to other imports otherwise things like hashlib
+# may not get properly bound to the desired libcrypto.
+fips_libcrypto_file = os.getenv("OCI_CLI_FIPS_LIBCRYPTO_FILE")
+if fips_libcrypto_file:
+    fips.override_libcrypto(fips_libcrypto_file)
+    fips.patch_hashlib_md5()
+
+
+# These imports are used by tests. The primary entry point for the CLI is cli.py.
+from .cli_root import cli  # noqa: F401,E402
+from .custom_types import cli_datetime  # noqa: F401,E402
+from .custom_types import cli_from_json  # noqa: F401,E402
+
+from .generated import *   # noqa: F401,F403,E402
+from .extended import *  # noqa: F401,F403,E402
+
+from . import aliasing  # noqa: F401,E402
+from . import file_filters  # noqa: F401,E402
+from . import final_command_processor  # noqa: F401,E402
+from . import cli_setup  # noqa: F401,E402
+from . import cli_util  # noqa: F401,E402
+from . import cli_exceptions  # noqa: F401,E402
+from . import object_storage_transfer_manager  # noqa: F401,E402
+from . import json_skeleton_utils  # noqa: F401,E402
+from . import string_utils  # noqa: F401,E402
+from . import help_text_producer  # noqa: F401,E402
+from oci import config  # noqa: F401,E402
+from .version import __version__  # noqa: F401,E402
---
+++ packages/oci-cli/oci_cli-2.4.41-py2.py3-none-any.whl/oci_cli/cli_clients.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
+
+CLIENT_MAP = {}
+MODULE_TO_TYPE_MAPPINGS = {}
+
+# This import populates CLIENT_MAP and MODULE_TO_TYPE_MAPPINGS
+import oci_cli.generated_client_mappings  # noqa: F401,E402
