51d0ddf481f7d84cceae3666021061221a53a47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:18 +0000

    Add package bootstrap-env/1.0.1/bootstrap_env-1.0.1-py3.6.egg

---
+++ packages/bootstrap-env/bootstrap_env-1.0.1-py3.6.egg/bootstrap_env/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+"""
+    bootstrap-env
+    ~~~~~~~~~~~~~
+
+    :copyleft: 2015-2018 by bootstrap-env team, see AUTHORS for more details.
+    :created: 2015 by JensDiemer.de
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+from .version import __version__
