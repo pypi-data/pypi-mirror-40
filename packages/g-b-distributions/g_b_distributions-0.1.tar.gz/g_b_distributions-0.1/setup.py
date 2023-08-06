4c3178b960ff173930ae71ea6c798f6d6c22c3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:13 +0000

    Add package g-b-distributions/0.1/g_b_distributions-0.1.tar.gz

---
+++ packages/g-b-distributions/g_b_distributions-0.1.tar.gz/g_b_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='g_b_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['g_b_distributions'],
+      zip_safe=False)
