b1d33d4b5a63d30ac65a653338759a99e2f77ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:59 +0000

    Add package bidtoolsdk/0.0.7/bidtoolsdk-0.0.7-py3-none-any.whl

---
+++ packages/bidtoolsdk/bidtoolsdk-0.0.7-py3-none-any.whl/google/keyword_performance.py
@@ -0,0 +1,45 @@
+import requests
+import pandas as pd
+from io import StringIO
+from enum import Enum
+from bidtoolsdk.settings import os
+
+
+class Columns(Enum):
+    Date = 'Date'
+    CampaignId = 'CampaignId'
+    CampaignName = 'CampaignName'
+    AdGroupId = 'AdGroupId',
+    AdGroupName = 'AdGroupName'
+    CpcBid = 'CpcBid'
+    Device = 'Device'
+    AverageCost = 'AverageCost'
+    SearchExactMatchImpressionShare = 'SearchExactMatchImpressionShare'
+    SearchImpressionShare = 'SearchImpressionShare'
+    Conversions = 'Conversions'
+    Cost = 'Cost'
+    Impressions = 'Impressions'
+    Clicks = 'Clicks'
+    Criteria = 'Criteria'
+    Id = 'Id'
+    CostPerConversion = 'CostPerConversion'
+    SystemServingStatus = 'SystemServingStatus'
+    KeywordMatchType = 'KeywordMatchType'
+    QualityScore = 'QualityScore'
+    AveragePosition = 'AveragePosition'
+
+
+def get_keyword_performance_df(platform_id, days_from_today=14, columns=None):
+
+    bidtoolapibaseurl = os.environ.get("bidtoolapibaseurl")
+    response = requests.post(
+        url=f'{bidtoolapibaseurl}/keyword-performance-report',
+        auth=(os.environ.get('bidtoolapiuser'), os.environ.get('bidtoolapipass')),
+        json={
+            "platform_id": platform_id,
+            "days_from_today": days_from_today,
+            "columns": [col.value for col in columns]
+        }
+    )
+
+    return pd.read_csv(StringIO(response.content))
