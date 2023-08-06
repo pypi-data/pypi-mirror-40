85e787b505ffa5f071345c31769a2682cbdbc3ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:54 +0000

    Add package cat-fact/0.1.2/cat_fact-0.1.2.tar.gz

---
+++ packages/cat-fact/cat_fact-0.1.2.tar.gz/cat_fact-0.1.2/setup.py
@@ -0,0 +1,58 @@
+"""
+cat_fact
+--------
+
+The client interface for cat fact
+
+Install
+~~~~~~~
+
+Install the stable version from PyPi
+```bash
+$ pip install cat-fact
+```
+"""
+
+import sys
+import re
+import os.path
+
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """Returns the package version taken from version.py
+    """
+    root = os.path.dirname(__file__)
+    version_path = os.path.join(root, "cat_fact/__init__.py")
+    text = open(version_path).read()
+    rx = re.compile("^__version__ = '(.*)'", re.M)
+    m = rx.search(text)
+    version = m.group(1)
+    return version
+
+
+install_requires = [
+    "click>=6.7",
+    "requests>=2.21"
+]
+
+
+setup(
+    name="cat_fact",
+    version=get_version(),
+    author="Shaik Sohail Yunus",
+    author_email="sohail.yunusha1@gmail.com",
+    maintainer="Shaik Sohail Yunus",
+    maintainer_email="sohail.yunusha1@gmail.com",
+    description="Get random cat facts fromt the internet " + u"\U0001F638",
+    long_description=__doc__,
+    install_requires=install_requires,
+    packages=find_packages(),
+    url="https://github.com/sohail535/cat_fact",
+    entry_points="""
+        [console_scripts]
+        catFact = cat_fact.cli:cli
+    """
+)
+
