760b718c1e5f5dd7555b8338f4690bdcd9115b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:55 +0000

    Add package CGATReport/0.8.2/CGATReport-0.8.2.tar.gz

---
+++ packages/CGATReport/CGATReport-0.8.2.tar.gz/CGATReport-0.8.2/CGATReport/Plugins/GGPlotter.py
@@ -0,0 +1,96 @@
+'''Plotting using the ggplot module.'''
+import sys
+
+from CGATReport.Plugins.Renderer import Renderer
+from CGATReport.Plugins.Plotter import Plotter
+from docutils.parsers.rst import directives
+
+import matplotlib.pyplot as plt
+import pandas
+import copy
+import warnings
+
+
+SHAPES = [
+    'o',  # circle
+    '^',  # triangle up
+    '<',
+    '>',
+    'D',  # diamond
+    'v',  # triangle down
+    's',  # square
+    '*',  # star
+    'p',  # pentagon
+    '*',  # octagon
+    'h',
+    'H',
+    'd',
+]
+
+try:
+    from plotnine import *
+except ImportError:
+    pass
+
+
+class GGPlot(Renderer, Plotter):
+
+    """Use the python ggplot libary for plotting.
+    """
+    options = (
+        ('aes',  directives.unchanged),
+        ('geom', directives.unchanged),
+    ) + Renderer.options + Plotter.options
+
+    group_level = 0
+
+    def __init__(self, *args, **kwargs):
+        Renderer.__init__(self, *args, **kwargs)
+        Plotter.__init__(self, *args, **kwargs)
+
+        if "aes" not in kwargs:
+            raise ValueError("ggplot renderer requires an aesthetics option")
+
+        if "geom" not in kwargs:
+            raise ValueError("ggplot renderer requires a geom option")
+
+        self.aes = kwargs.get('aes')
+        self.geom = kwargs.get('geom')
+
+    def render(self, dataframe, path):
+
+        # the index in the dataframe is reset in order
+        # to add the index as a column.
+
+        # Currently, ggplot is not using hierarchical indices, but
+        # see this thread: https://github.com/yhat/ggplot/issues/285
+        dataframe.reset_index(inplace=True)
+
+        if len(dataframe.dropna()) == 0:
+            warnings.warn("dataframe is empty after dropping all NA columns")
+            return None
+
+        s = "plot = ggplot(aes(%s), data=dataframe) + %s" % (self.aes, self.geom)
+        ll = copy.copy(locals())
+        gl = copy.copy(globals())
+        try:
+            exec(s, gl, ll)
+        except Exception as msg:
+            raise Exception(
+                "ggplot raised error for statement '%s': msg=%s" %
+                (s, msg))
+        plot = ll["plot"]
+
+        if self.title:
+            plt.title(self.title)
+
+        # plot.make() calls plt.close() command, so create a dummy plot that will be
+        # closed instead
+        plt.figure()
+        try:
+            plts = [plot.draw()]
+        except Exception as msg:
+            raise Exception(
+                "ggplot raised error on rendering for statement '%s': msg=%s" %
+                (s, msg))
+        return self.endPlot(plts, None, path)
