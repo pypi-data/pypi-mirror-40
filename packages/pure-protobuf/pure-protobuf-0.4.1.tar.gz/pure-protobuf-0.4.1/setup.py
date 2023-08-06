10d1394fa73df3406ddb7c93ad4ee529a6d1cb17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:01 +0000

    Add package pure-protobuf/0.4.1/pure-protobuf-0.4.1.tar.gz

---
+++ packages/pure-protobuf/pure-protobuf-0.4.1.tar.gz/pure-protobuf-0.4.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pure-protobuf',
+    version='0.4.1',
+    description=(
+        'Python implementation of Protocol Buffer (protobuf) data types'
+    ),
+    long_description=open('README.markdown').read(),
+    long_description_content_type='text/markdown',
+    author='Pavel Perestoronin',
+    author_email='eigenein@gmail.com',
+    url='https://github.com/eigenein/protobuf',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
