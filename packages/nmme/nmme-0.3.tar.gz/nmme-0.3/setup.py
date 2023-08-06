e29eb32637bff6a8ad4e9ae31b11905d5e607291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:22 +0000

    Add package nmme/0.3/nmme-0.3.tar.gz

---
+++ packages/nmme/nmme-0.3.tar.gz/nmme-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='nmme',
+      version='0.3',
+      description='NMME pip install script',
+      url='https://nemoest.com',
+      author='Nemo Est',
+      author_email='nemo_est@outlook.com',
+      license='MIT',
+      packages=['nmme'],
+      install_requires=[
+      'requests',
+      ],
+      zip_safe=False)
