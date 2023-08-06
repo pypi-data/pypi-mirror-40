3e3fc8dd4628a87bd3b6884d2593ce9565a9129b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:44 +0000

    Add package sql-connectors/1.0.1/sql_connectors-1.0.1.tar.gz

---
+++ packages/sql-connectors/sql_connectors-1.0.1.tar.gz/sql_connectors-1.0.1/tests/test_sql_connectors.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `sql_connectors` package."""
+
+import unittest
+
+class TestSql_connectors(unittest.TestCase):
+    """Tests for `sql_connectors` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
