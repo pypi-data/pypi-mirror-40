8dfe67f720d6c2f94a851459e03ef3c4ced1a940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:24 +0000

    Add package launchkey/3.3.0/launchkey-3.3.0.tar.gz

---
+++ packages/launchkey/launchkey-3.3.0.tar.gz/launchkey-3.3.0/CHANGES.rst
@@ -0,0 +1,81 @@
+CHANGELOG for LaunchKey Python SDK
+==================================
+
+3.3.0
+-----
+
+* Added tooling around code quality and ensured that CI build would fail without meeting expectations.
+* Added dynamic auth TTL and title functionality
+* Added dynamic auth push message body and title functionality
+* Added auth busy signal error handling
+* Added new auth response format
+* Added auth denial context functionality
+
+3.2.0
+-----
+
+* Remove PyCrypto and replace with pycryptodomex that is already required by PyJWKEST
+* Fixed geofence missing name bug
+* Added more expected error conditions to pydocs
+* Removed version lock for pytz requirement
+* Added 3rd party push enhancements
+* Added tox config for local testing in multiple versions of Python
+* Added missing response validation
+* Added full webhook validation
+* Cleaned up error handling and raising for webhooks
+* Switch RequestsTransport to use session in order to provide connection sharing between requests
+* Added dynamic auth TTL and title functionality
+
+3.1.1
+-----
+
+* Added patch method for transports
+* Support for many new endpoints added involving Organization, Directory, and Service management
+* Moved all entity objects into their own submodule
+* Added UUID validation for factory entity IDs
+
+3.0.2
+-----
+
+* Improved 401 error handling
+* Bug fix for SessionEndRequest object
+* Service PINs bug fix
+
+3.0.1
+-----
+
+* Typo and manifest fixes
+* Added Unauthorized status code error handler
+* Nose version lock for test requirements
+
+3.0.0
+-----
+
+* Complete revamp for new V3 LaunchKey API
+
+2.0.1
+-----
+
+* Make tests run under Python 3.5+
+* Make PEP-8 compliant (deprecated some non-PEP-8 compliant functions)
+
+1.3.0
+-----
+
+* Python 3 compatibility.
+* Ability to send policies in auth request.
+
+1.2.7
+-----
+
+* Update manifest for new CHANGES file
+
+1.2.6
+-----
+
+* Fix for bad build regarding CHANGES.md
+
+1.2.5
+-----
+
+* Remove references to LK Identifier as the API no longer returns it.
---
+++ packages/launchkey/launchkey-3.3.0.tar.gz/launchkey-3.3.0/launchkey/entities/directory.py
@@ -0,0 +1,83 @@
+"""Directory level entities"""
+
+# pylint: disable=invalid-name,too-few-public-methods,
+# pylint: disable=too-many-instance-attributes
+
+
+from launchkey.exceptions import InvalidDeviceStatus
+
+
+class Directory(object):
+    """Directory entity"""
+    def __init__(self, data):
+        self.service_ids = data['service_ids']
+        self.sdk_keys = data['sdk_keys']
+        self.premium = data['premium']
+        self.name = data['name']
+        self.android_key = data['android_key']
+        self.ios_certificate_fingerprint = data['ios_certificate_fingerprint']
+        self.active = data['active']
+        self.id = data['id']
+        self.denial_context_inquiry_enabled = \
+            data['denial_context_inquiry_enabled']
+
+
+class Session(object):
+    """User Service Session"""
+
+    def __init__(self, session):
+        self.auth_request = session['auth_request']
+        self.created = session['date_created']
+        self.service_icon = session['service_icon']
+        self.service_id = session['service_id']
+        self.service_name = session['service_name']
+
+
+class DirectoryUserDeviceLinkData(object):
+    """Directory user device data used to finish the linking process"""
+
+    def __init__(self, data):
+        self.qrcode = data['qrcode']
+        self.code = data['code']
+
+
+class DeviceStatus(object):
+    """Activation status of a directory user's device"""
+
+    _status_map = {
+        0: ("LINK_PENDING", False),
+        1: ("LINKED", True),
+        2: ("UNLINK_PENDING", True)
+    }
+
+    def __init__(self, status_code):
+        if status_code not in self._status_map:
+            raise InvalidDeviceStatus(
+                "Status code %s was not recognized" % status_code)
+        self._status_code = status_code
+
+    @property
+    def is_active(self):
+        """
+        Is the device active?
+        :return: active status
+        """
+        return self._status_map[self._status_code][1]
+
+    @property
+    def status_code(self):
+        """
+        What's the status iof the device
+        :return: Status code
+        """
+        return self._status_map[self._status_code][0]
+
+
+class Device(object):
+    """Device object belonging to a directory user"""
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.status = DeviceStatus(data['status'])
+        self.type = data['type']
