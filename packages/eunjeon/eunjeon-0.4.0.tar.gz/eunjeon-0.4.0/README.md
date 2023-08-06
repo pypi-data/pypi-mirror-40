5611c74f73bb5da72328a9d6c8e5e3b0fbfcd176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:54 +0000

    Add package eunjeon/0.4.0/eunjeon-0.4.0.tar.gz

---
+++ packages/eunjeon/eunjeon-0.4.0.tar.gz/eunjeon-0.4.0/README.md
@@ -0,0 +1,61 @@
+# pyeunjeon (python + eunjeon)
+
+[![Build Status](https://travis-ci.org/koshort/pyeunjeon.svg?branch=master)](https://travis-ci.org/koshort/pyeunjeon)  
+
+pyeunjeon은 [은전한닢 프로젝트](http://eunjeon.blogspot.kr/)와 mecab 기반의 한국어 형태소 분석기의 독립형 python 인터페이스입니다.  
+pyeunjeon is a stand-alone python interface for morphological analyzer mecab and [project eunjeon](http://eunjeon.blogspot.kr/).
+
+## Koshort :purple_heart: KoNLPy
+[KoNLPy와 Koshort의 통합이 진행되고 있습니다!](https://github.com/konlpy/konlpy/issues/202)
+
+# Installation 설치
+
+## Linux or Mac
+
+```bash
+# Install mecab first / mecab을 먼저 설치해주세요.
+bash <(curl -s https://raw.githubusercontent.com/koshort/peunjeon/master/scripts/mecab.sh)
+
+# Install eunjeon / eunjeon을 설치합니다.
+pip install eunjeon
+```
+
+## Windows x64
+
+```bash
+pip install eunjeon
+```
+
+# Usage 사용법
+```python
+>>> from eunjeon import Mecab  # KoNLPy style mecab wrapper
+>>> tagger = Mecab() 
+>>> tagger.nouns("고양이가 냐 하고 울면 나는 녜 하고 울어야지")
+['고양이', '나', '녜']
+>>> # 빛 아래 유령
+>>> poem = """
+... 흘러내린 머리카락이 흐린 호박빛 아래 빛난다.
+... 유영하며.
+... 저건가보다.
+... 세월의 힘을 이겨낸 마지막 하나 남은 가로등.
+... 미래의 색, 역겨운 청록색으로 창백하게 바뀔 마지막 가로등
+... 난 유영한다. 차분하게 과거에 살면서 현재의 공기를 마신다.
+... 가로등이 깜빡인다.
+...
+... 나도 깜빡여준다.
+... """
+>>> tagger.morphs(poem)  # 형태소 단위로 나누기
+['흘러내린', '머리카락', '이', '흐린', '호박', '빛', '아래', '빛난다', '.', '유영', '하', '며', '.', '저건가', '보', '다', '.', '세월', '의', '힘', '을', '이겨', '낸', '마지막', '하나', '남', '은', '가로등', '.', '미래', '의', '색', ',', ' 역겨운', '청록색', '으로', '창백', '하', '게', '바뀔', '마지막', '가로등', '난', '유영', '한다', '.', '차분', '하', '게', '과거', '에', '살', '면서', '현재', '의', '공기', '를', '마신다', '.', '가로등', '이', '깜빡인다', '.', '나', '도', ' 깜빡', '여', '준다', '.']
+>>> tagger.pos("다람쥐 헌 쳇바퀴에 타고 파")
+[('다람쥐', 'NNG'), ('헌', 'XSV+ETM'), ('쳇바퀴', 'NNG'), ('에', 'JKB'), ('타', 'VV'), ('고', 'EC'), ('파', 'VX+EC')]
+```
+
+# Open-source
+
+pyeunjeon에서는 다음과 같은 오픈소스를 이용하였습니다.  
+pyeunjeon used following open source projects.
+
+* [은전한닢 프로젝트](http://eunjeon.blogspot.kr/)  
+* [KoNLPy](http://konlpy.org) by Lucy Park  
+* [mecab](https://github.com/taku910/mecab) by taku910  
+* [mecab-python-windows](https://github.com/ikegami-yukino/mecab/releases) by ikegami-yukino  
---
+++ packages/eunjeon/eunjeon-0.4.0.tar.gz/eunjeon-0.4.0/setup.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import string
+import platform
+import distutils.sysconfig as config
+from setuptools import find_packages, setup, Extension
+
+
+__version__ = '0.4.0'
+
+SYSTEM = platform.system()
+DATA_FILES = None
+EXTENSION = []
+PACKAGE_DATA = {
+    'eunjeon': [
+        'data/*.cxx',
+        'data/*.bin',
+        'data/*.def',
+        'data/*.dic',
+        'data/dicrc',
+        'data/mecabrc',
+        ]
+}
+
+
+def requirements():
+    """get requirements
+
+    Returns:
+        list: Requirements
+    """
+
+    def _openreq(reqfile):
+        with open(os.path.join(os.path.dirname(__file__), reqfile)) as req:
+            return req.read().splitlines()
+
+    return _openreq('requirements.txt')
+
+
+if SYSTEM == "Windows":
+    # Experimental stand-alone extension and data_files
+    EXTENSION = [
+        Extension(
+            "_MeCab",
+            ["eunjeon/MeCab_wrap.cxx",],
+            include_dirs=["eunjeon/data/sdk"],
+            library_dirs=["eunjeon/data/sdk"],
+            libraries=["libmecab"]
+        )
+    ]
+    PACKAGE_DATA['eunjeon'].append('data/*')
+    PACKAGE_DATA['eunjeon'].append('data/sdk/*')
+
+else:
+    def cmd1(strings):
+        return os.popen(strings).readlines()[0][:-1]
+
+    if sys.version > '3':
+        def cmd2(strings):
+            return cmd1(strings).split()
+    else:
+        def cmd2(strings):
+            return string.split(cmd1(strings))
+
+    EXTENSION = [Extension(
+        "_MeCab",
+        ["eunjeon/MeCab_wrap.cxx"],
+        include_dirs=cmd2("mecab-config --inc-dir"),
+        library_dirs=cmd2("mecab-config --libs-only-L"),
+        libraries=cmd2("mecab-config --libs-only-l")
+    )]
+
+
+setup(
+    name='eunjeon',
+    version=__version__,
+    description='Python interface for eunjeon project & mecab based morphological analyzer.',
+    url='https://github.com/koshort/pyeunjeon',
+    author='nyanye',
+    author_email='iam@nyanye.com',
+    keywords=['Korean', 'CJK',
+              'NLP', 'natural language processing',
+              'CL', 'computational linguistics',
+              'tagging', 'tokenizing', 'linguistics', 'text analytics'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: General',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Text Processing :: Linguistic',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        ],
+    entry_points={
+        'console_scripts': [],
+    },
+    package_data=PACKAGE_DATA,
+    data_files=DATA_FILES,
+    license='GPL v3+',
+    ext_modules=EXTENSION,
+    platforms=["Windows", "Linux", "Mac"],
+    packages=find_packages(),
+    install_requires=requirements()
+)
