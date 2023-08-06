4734fc88e454e41200cc89234a7257e1cfd1b2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:42 +0000

    Add package sceptre/2.0.2/sceptre-2.0.2.tar.gz

---
+++ packages/sceptre/sceptre-2.0.2.tar.gz/sceptre-2.0.2/sceptre/cli/template.py
@@ -0,0 +1,105 @@
+import click
+import webbrowser
+
+from sceptre.context import SceptreContext
+from sceptre.cli.helpers import (
+    catch_exceptions,
+    write
+)
+from sceptre.plan.plan import SceptrePlan
+
+
+@click.command(name="validate")
+@click.argument("path")
+@click.pass_context
+@catch_exceptions
+def validate_command(ctx, path):
+    """
+    Validates the template.
+
+    Validates the template used for stack in PATH.
+
+    :param path: Path to execute the command on.
+    :type path: str
+    """
+    context = SceptreContext(
+        command_path=path,
+        project_path=ctx.obj.get("project_path"),
+        user_variables=ctx.obj.get("user_variables"),
+        options=ctx.obj.get("options"),
+        output_format=ctx.obj.get("output_format"),
+        ignore_dependencies=ctx.obj.get("ignore_dependencies")
+    )
+
+    plan = SceptrePlan(context)
+    responses = plan.validate()
+
+    for stack, response in responses.items():
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            del response['ResponseMetadata']
+            click.echo("Template {} is valid. Template details:\n".format(stack.name))
+        write(response, context.output_format)
+
+
+@click.command(name="generate")
+@click.argument("path")
+@click.pass_context
+@catch_exceptions
+def generate_command(ctx, path):
+    """
+    Prints the template.
+
+    Prints the template used for stack in PATH.
+
+    :param path: Path to execute the command on.
+    :type path: str
+    """
+    context = SceptreContext(
+        command_path=path,
+        project_path=ctx.obj.get("project_path"),
+        user_variables=ctx.obj.get("user_variables"),
+        options=ctx.obj.get("options"),
+        output_format=ctx.obj.get("output_format"),
+        ignore_dependencies=ctx.obj.get("ignore_dependencies")
+    )
+
+    plan = SceptrePlan(context)
+    responses = plan.generate()
+    output = [template for template in responses.values()]
+    write(output, context.output_format)
+
+
+@click.command(name="estimate-cost")
+@click.argument("path")
+@click.pass_context
+@catch_exceptions
+def estimate_cost_command(ctx, path):
+    """
+    Estimates the cost of the template.
+
+    Prints a URI to STOUT that provides an estimated cost based on the
+    resources in the stack. This command will also attempt to open a web
+    browser with the returned URI.
+
+    :param path: Path to execute the command on.
+    :type path: str
+    """
+    context = SceptreContext(
+        command_path=path,
+        project_path=ctx.obj.get("project_path"),
+        user_variables=ctx.obj.get("user_variables"),
+        options=ctx.obj.get("options"),
+        output_format=ctx.obj.get("output_format"),
+        ignore_dependencies=ctx.obj.get("ignore_dependencies")
+    )
+
+    plan = SceptrePlan(context)
+    responses = plan.estimate_cost()
+
+    for stack, response in responses.items():
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            del response['ResponseMetadata']
+            click.echo("View the estimated cost for {} at:".format(stack.name))
+            response = response["Url"]
+            webbrowser.open(response, new=2)
+        write(response + "\n", 'str')
