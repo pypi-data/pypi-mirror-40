35d2d4ff0b9876a1f38abec80ca6c96717cb4ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:37 +0000

    Add package django-neurobank/0.8.0/django-neurobank-0.8.0.tar.gz

---
+++ packages/django-neurobank/django-neurobank-0.8.0.tar.gz/django-neurobank-0.8.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+import os
+import sys
+if sys.hexversion < 0x02060000:
+    raise RuntimeError("Python 2.6 or higher required")
+from setuptools import setup
+
+VERSION = '0.8.0'
+cls_txt = """
+Development Status :: 3 - Alpha
+Framework :: Django
+Intended Audience :: Science/Research
+License :: OSI Approved :: GNU General Public License (GPL)
+Programming Language :: Python
+Topic :: Scientific/Engineering
+Topic :: Internet :: WWW/HTTP
+Topic :: Internet :: WWW/HTTP :: Dynamic Content
+"""
+
+setup(
+    name="django-neurobank",
+    version=VERSION,
+    description="A Django-based registry for the neurobank system",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    classifiers=[x for x in cls_txt.split("\n") if x],
+    author='C Daniel Meliza',
+    maintainer='C Daniel Meliza',
+    url="https://github.com/melizalab/django-neurobank",
+    packages=['neurobank'],
+    include_package_data=True,
+)
