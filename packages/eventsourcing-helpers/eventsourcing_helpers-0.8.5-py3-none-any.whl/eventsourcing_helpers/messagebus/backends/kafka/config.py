a09aea590b29e0f31e59ad05ce03a2cb143e2bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:17 +0000

    Add package eventsourcing-helpers/0.8.5/eventsourcing_helpers-0.8.5-py3-none-any.whl

---
+++ packages/eventsourcing-helpers/eventsourcing_helpers-0.8.5-py3-none-any.whl/eventsourcing_helpers/messagebus/backends/kafka/config.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+from copy import deepcopy
+
+
+def get_producer_config(config):
+    config = deepcopy(config)
+    producer_config = config.get('producer', None)
+    if not producer_config or not producer_config.get('enabled', True):
+        return None
+
+    producer_config.update({
+        'bootstrap.servers': config['bootstrap.servers'],
+        'schema.registry.url': config['schema.registry.url']
+    })
+    return producer_config
+
+
+def get_consumer_config(config):
+    config = deepcopy(config)
+    consumer_config = config.get('consumer')
+    if not consumer_config or not consumer_config.get('enabled', True):
+        return None
+
+    consumer_config.pop('offset_watchdog', None)
+    consumer_config.update({
+        'bootstrap.servers': config['bootstrap.servers'],
+        'schema.registry.url': config['schema.registry.url']
+    })
+    return consumer_config
+
+
+def get_offset_watchdog_config(config):
+    config = deepcopy(config)
+    consumer_config = config.get('consumer', {})
+    if not consumer_config:
+        return None
+
+    offset_wd_config = defaultdict(
+        dict, consumer_config.get('offset_watchdog', {})
+    )
+    offset_wd_config['backend_config'].update(
+        {'group.id': consumer_config['group.id']}
+    )
+    return offset_wd_config
