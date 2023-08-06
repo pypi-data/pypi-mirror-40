b834015d97e2cd2419c2e3eda5949dd47d9f6814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:13 +0000

    Add package pytablewriter/0.39.0/pytablewriter-0.39.0-py2.py3-none-any.whl

---
+++ packages/pytablewriter/pytablewriter-0.39.0-py2.py3-none-any.whl/pytablewriter/style/_font.py
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+from __future__ import absolute_import
+
+import enum
+
+
+@enum.unique
+class FontSize(enum.Enum):
+    NONE = 0
+    TINY = 1
+    SMALL = 2
+    MEDIUM = 3
+    LARGE = 4
+
+
+@enum.unique
+class FontStyle(enum.Enum):
+    NORMAL = 0
+    ITALIC = 1
+
+
+@enum.unique
+class FontWeight(enum.Enum):
+    NORMAL = 0
+    BOLD = 1
