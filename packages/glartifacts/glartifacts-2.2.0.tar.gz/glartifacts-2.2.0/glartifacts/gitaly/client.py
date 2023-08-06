f9b1301833925c15773ca3a14031c141073185ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:24 +0000

    Add package glartifacts/2.2.0/glartifacts-2.2.0.tar.gz

---
+++ packages/glartifacts/glartifacts-2.2.0.tar.gz/glartifacts-2.2.0/glartifacts/gitaly/client.py
@@ -0,0 +1,112 @@
+import grpc
+
+from .proto import (
+    ref_pb2, ref_pb2_grpc,
+    commit_pb2, commit_pb2_grpc,
+    blob_pb2_grpc,
+    )
+from .proto import shared_pb2
+from ..errors import GitlabArtifactsError
+
+GITALY_ADDR = 'unix:/var/opt/gitlab/gitaly/gitaly.socket'
+
+def _gitaly_repo(project):
+    return shared_pb2.Repository(
+        storage_name=project.storage,
+        relative_path=project.disk_path,
+        gl_repository=project.gl_repository,
+        )
+
+
+class GitalyClient():
+    def __init__(self, addr=GITALY_ADDR):
+        self.addr = addr
+        self._channel = None
+        self._refsvc = None
+        self._commitsvc = None
+        self._blobsvc = None
+
+    def __enter__(self):
+        self._channel = grpc.insecure_channel(self.addr)
+        self._refsvc = ref_pb2_grpc.RefServiceStub(self._channel)
+        self._commitsvc = commit_pb2_grpc.CommitServiceStub(self._channel)
+        self._blobsvc = blob_pb2_grpc.BlobServiceStub(self._channel)
+
+        return self
+
+    def __exit__(self, *args):
+        self._channel.close()
+
+    def get_branches(self, project):
+        repository = _gitaly_repo(project)
+        request = ref_pb2.FindAllBranchesRequest(
+            repository=repository
+            )
+
+        # Gitaly "chunks?" responses at 20 items
+        # https://tinyurl.com/ycuazk7w
+        try:
+            for page in self._refsvc.FindAllBranches(request):
+                branches = []
+                for branch in page.branches:
+                    ref = (
+                        branch.name.decode('utf-8').split('/')[-1],
+                        branch.target.id,
+                        )
+                    branches.append(ref)
+
+                # Safety check for failed requests
+                if not branches:
+                    raise GitlabArtifactsError(
+                        'Gitaly returned no branches for {}'.format(
+                            project.full_path
+                            )
+                        )
+
+        except grpc.RpcError as e:
+            raise GitlabArtifactsError(
+                'RefSvc.FindAllBranches for {} failed with error {}:{}'.format(
+                    project.full_path,
+                    e.code(),
+                    e.details()
+                    )
+                )
+
+        return branches
+
+    def get_tree_entry(self, ref, path):
+        repository = _gitaly_repo(ref.project)
+
+        request = commit_pb2.TreeEntryRequest(
+            repository=repository,
+            revision=ref.commit.encode('utf-8'),
+            path=path.encode('utf-8'),
+            limit=0,
+            )
+
+        try:
+            # This should raise RpcError - on notfound, but it doesn't?
+            response = list(self._commitsvc.TreeEntry(request))
+
+            # Note: I don't know how this response gets chunked
+            assert len(response) == 1 # Shouldn't this be Unary?
+        except grpc.RpcError as e:
+            raise GitlabArtifactsError(
+                'CommitSvc.TreeEntry failed with error {}:{}'.format(
+                    e.code(),
+                    e.details()
+                    )
+                )
+
+        entry = response[0]
+
+        # Ensure we receive type=BLOB, failed requests return type=COMMIT
+        blob = None
+        if entry.type == 1:
+            blob = (
+                entry.oid,
+                entry.size,
+                entry.data,
+                )
+
+        return blob if blob else (None,)*3
