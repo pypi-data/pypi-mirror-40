4e0f58f1e82af9f84df07d6d6c7fe36e9fad7ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:26 +0000

    Add package reversefold.util/1.19/reversefold.util-1.19.tar.gz

---
+++ packages/reversefold.util/reversefold.util-1.19.tar.gz/reversefold.util-1.19/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='reversefold.util',
+    version='1.19',
+    description='SSH, Proc, Multiproc, tail.py, log.py, stream.py, daemonize.py, etc.',
+    author='Justin Patrin',
+    author_email='papercrane@reversefold.com',
+    url='https://github.com/reversefold/util',
+    packages=find_packages(),
+    scripts=[
+        'scripts/tail.py',
+        'scripts/log.py',
+        'scripts/stream.py',
+        'scripts/sort_json.py',
+    ],
+    entry_points={
+        'console_scripts': [
+            'daemonize.py = reversefold.util.daemonize:main'
+        ],
+    },
+    license='MIT',
+    install_requires=[
+        'colorama',
+        'docopt',
+        'lockfile',
+        'python-daemon==2.1.1',
+        'psutil',
+        'watchdog',
+    ],
+)
