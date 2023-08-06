0f2397997cc745ca507aa2f9de7bc68f4adc7133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:01 +0000

    Add package lightnovel-crawler/2.6.6/lightnovel_crawler-2.6.6-py3-none-any.whl

---
+++ packages/lightnovel-crawler/lightnovel_crawler-2.6.6-py3-none-any.whl/lightnovel_crawler/core/novel_info.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+To get the novel info
+"""
+import re
+import os
+import json
+
+from .prompts import get_output_path
+
+
+def format_volume_list(crawler):
+    for vol in crawler.volumes:
+        vol['chapter_count'] = 0
+        title = 'Volume %d' % vol['id']
+        vol['title'] = vol['title'] or title
+        if not re.search(r'vol(ume)? .?\d+', vol['title'], re.IGNORECASE):
+            vol['title'] = title + ' - ' + vol['title'].title()
+        # end if
+    # end for
+# end def
+
+
+def format_chapter_list(crawler):
+    for item in crawler.chapters:
+        title = 'Chapter #%d' % item['id']
+        item['title'] = item['title'] or title
+        if not re.search(r'ch(apter)? .?\d+', item['title'], re.IGNORECASE):
+            item['title'] = title + ' - ' + item['title'].title()
+        # end if
+
+        item['volume'] = item['volume'] or (1 + (item['id'] - 1) // 100)
+        item['volume_title'] = 'Volume %d' % item['volume']
+        for vol in crawler.volumes:
+            if vol['id'] == item['volume']:
+                item['volume_title'] = vol['title']
+                vol['chapter_count'] += 1
+                break
+            # end if
+        # end for
+    # end for
+# end def
+
+
+def save_metadata(crawler, output_path):
+    file_name = os.path.join(output_path, 'meta.json')
+    data = {
+        'title': crawler.novel_title,
+        'author': crawler.novel_author,
+        'cover': crawler.novel_cover,
+        'volumes': crawler.volumes,
+        'chapters': crawler.chapters,
+    }
+    with open(file_name, 'w') as file:
+        json.dump(data, file, indent=2)
+    # end with
+# end def
+
+
+def novel_info(app):
+    app.logger.warn('Retrieving novel info...')
+    app.crawler.read_novel_info()
+    app.logger.warn('NOVEL: %s', app.crawler.novel_title)
+
+    app.logger.info('Checking output path...')
+    good_name = re.sub(r'[\\/*?:"<>|\']', '', app.crawler.novel_title)
+    app.output_path = get_output_path(os.path.join('Lightnovels', good_name))
+
+    app.logger.info('Getting chapters...')
+    app.crawler.download_chapter_list()
+
+    format_volume_list(app.crawler)
+    format_chapter_list(app.crawler)
+
+    save_metadata(app.crawler, app.output_path)
+# end def
