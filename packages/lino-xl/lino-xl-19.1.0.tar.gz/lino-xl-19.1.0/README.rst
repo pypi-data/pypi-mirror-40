6b163bb558abfefd0ad31f2c8d66ed6d7cb6a583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:49 +0000

    Add package lino-xl/19.1.0/lino-xl-19.1.0.tar.gz

---
+++ packages/lino-xl/lino-xl-19.1.0.tar.gz/lino-xl-19.1.0/README.rst
@@ -0,0 +1,44 @@
+=======================
+The ``lino-xl`` package
+=======================
+
+
+
+
+
+.. raw:: html
+
+    <a href="https://travis-ci.org/lino-framework/xl" target="_blank"><img
+    src="https://travis-ci.org/lino-framework/xl.svg?branch=master"/></a>
+
+    <a class="reference external"
+    href="http://lino.readthedocs.io/en/latest/?badge=latest"><img
+    alt="Documentation Status"
+    src="https://readthedocs.org/projects/lino/badge/?version=latest"
+    /></a>
+
+    <a class="reference external"
+    href="https://coveralls.io/github/lino-framework/book?branch=master"><img
+    alt="coverage"
+    src="https://coveralls.io/repos/github/lino-framework/book/badge.svg?branch=master"
+    /></a>
+
+    <a class="reference external" href="https://travis-ci.org/lino-framework/book?branch=stable"><img alt="build" src="https://travis-ci.org/lino-framework/book.svg?branch=stable" /></a>
+
+    <a class="reference external" href="https://pypi.python.org/pypi/lino/"><img alt="pypi_v" src="https://img.shields.io/pypi/v/lino.svg" /></a>
+
+    <a class="reference external"
+    href="https://pypi.python.org/pypi/lino/"><img alt="PyPI"
+    src="https://img.shields.io/pypi/l/lino.svg" /></a>
+
+
+The **Lino Extensions Library** is a collection of plugins used by
+many Lino projects.
+
+This package is in a separate repository because it adds a given set
+of solutions for "Enterprise" style applications.  It is considered an
+integral part of the Lino framework, it is maintained, documented and
+tested along with the core at `www.lino-framework.org
+<http://www.lino-framework.org>`__.
+
+
---
+++ packages/lino-xl/lino-xl-19.1.0.tar.gz/lino-xl-19.1.0/lino_xl/lib/products/models.py
@@ -0,0 +1,106 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2008-2018 Rumma 6 Ko Ltd
+# License: BSD (see file COPYING for details)
+
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from lino.api import dd
+from lino import mixins
+
+from .choicelists import DeliveryUnits, ProductTypes
+from .roles import ProductsUser, ProductsStaff
+
+vat = dd.resolve_app('vat')
+
+
+class ProductCat(mixins.BabelNamed):
+
+    class Meta:
+        app_label = 'products'
+        verbose_name = _("Product Category")
+        verbose_name_plural = _("Product Categories")
+        abstract = dd.is_abstract_model(__name__, 'ProductCat')
+
+    description = models.TextField(blank=True)
+
+
+class ProductCats(dd.Table):
+    model = 'products.ProductCat'
+    required_roles = dd.login_required(ProductsStaff)
+    order_by = ["id"]
+    detail_layout = """
+    id name
+    description
+    ProductsByCategory
+    """
+
+
+class Product(mixins.BabelNamed):
+
+    class Meta:
+        app_label = 'products'
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+        abstract = dd.is_abstract_model(__name__, 'Product')
+
+    description = dd.BabelTextField(
+        verbose_name=_("Long description"),
+        blank=True, null=True)
+    cat = dd.ForeignKey(
+        ProductCat, verbose_name=_("Category"),
+        blank=True, null=True)
+
+    delivery_unit = DeliveryUnits.field(default='piece')
+    product_type = ProductTypes.field(default='default')
+
+    if vat:
+        vat_class = vat.VatClasses.field(blank=True)
+    else:
+        vat_class = dd.DummyField()
+
+
+class ProductDetail(dd.DetailLayout):
+
+    main = """
+    id cat #sales_price vat_class delivery_unit
+    name
+    description
+    """
+    
+
+class Products(dd.Table):
+    _product_type = None
+    required_roles = dd.login_required(ProductsUser)
+    model = 'products.Product'
+    order_by = ["name"]
+    column_names = "id name cat vat_class *"
+
+    insert_layout = """
+    cat
+    name
+    """
+    detail_layout = "products.ProductDetail"
+
+    @classmethod
+    def get_actor_label(cls):
+        pt = cls._product_type or ProductTypes.default
+        return pt.text
+
+    @classmethod
+    def create_instance(cls, ar, **kwargs):
+        kwargs.update(product_type=cls._product_type or ProductTypes.default)
+        return super(Products, cls).create_instance(ar, **kwargs)
+
+    @classmethod
+    def get_queryset(cls, ar, **filter):
+        filter.update(product_type=cls._product_type or ProductTypes.default)
+        return super(Products, cls).get_queryset(ar, **filter)
+
+
+class ProductsByCategory(Products):
+    master_key = 'cat'
+
+
+
