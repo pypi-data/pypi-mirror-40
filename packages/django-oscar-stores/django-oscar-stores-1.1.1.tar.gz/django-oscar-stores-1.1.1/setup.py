015ca2f12f12757e2e2e868cc6b30fe7fa0c2ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:35 +0000

    Add package django-oscar-stores/1.1.1/django-oscar-stores-1.1.1.tar.gz

---
+++ packages/django-oscar-stores/django-oscar-stores-1.1.1.tar.gz/django-oscar-stores-1.1.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import find_packages, setup
+
+setup(
+    name='django-oscar-stores',
+    version="1.1.1",
+    url='https://github.com/django-oscar/django-oscar-stores',
+    author="David Winterbottom",
+    author_email="david.winterbottom@gmail.com",
+    description="An extension for Oscar to include stores",
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    keywords="django, oscar, e-commerce",
+    license='BSD',
+    platforms=['linux'],
+    packages=find_packages(exclude=["sandbox*", "tests*"]),
+    include_package_data=True,
+    install_requires=[
+        'django-oscar>=1.6,<1.7',
+        'requests>=1.1',
+    ],
+
+    # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ])
