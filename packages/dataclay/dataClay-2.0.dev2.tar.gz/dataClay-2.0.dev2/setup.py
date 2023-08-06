2e9ae4807e2022284cfad928a3010f74552a759b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:02 +0000

    Add package dataclay/2.0.dev2/dataClay-2.0.dev2.tar.gz

---
+++ packages/dataclay/dataClay-2.0.dev2.tar.gz/dataClay-2.0.dev2/setup.py
@@ -0,0 +1,54 @@
+#-------------------------------------------------------------------------------
+# Copyright (C) 2018, Barcelona Supercomputing Center (www.bsc.es)
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#-------------------------------------------------------------------------------
+from setuptools import setup, find_packages
+
+setup(name='dataClay',
+      version='2.0.dev2',
+      install_requires=["enum34;python_version<\"3.4\"",
+                        "pyyaml",
+                        "lru-dict",
+                        "Jinja2",
+                        "PyYAML",
+                        "decorator",
+                        "grpcio==1.17.1",
+                        "grpcio-tools==1.17.1",
+                        "protobuf",
+                        "psutil",
+                        "six"
+                        ],
+      description='Python library for dataClay',
+      packages=find_packages("src"),
+      package_dir={'':'src'},
+      package_data={
+        # All .properties files are valuable "package data"
+        '': ['*.properties'],
+      },
+      )
