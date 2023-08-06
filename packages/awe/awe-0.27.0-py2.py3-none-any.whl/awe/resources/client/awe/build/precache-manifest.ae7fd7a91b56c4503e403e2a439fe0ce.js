b90e22f4a94100d2aab5de017b8f229021ffa3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:23 +0000

    Add package awe/0.27.0/awe-0.27.0-py2.py3-none-any.whl

---
+++ packages/awe/awe-0.27.0-py2.py3-none-any.whl/awe/resources/client/awe/build/precache-manifest.ae7fd7a91b56c4503e403e2a439fe0ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22d3d44b7c02275b5a5f",
+    "url": "/static/static/js/runtime~main.22d3d44b.js"
+  },
+  {
+    "revision": "7810c5446b486c41b8c3",
+    "url": "/static/static/js/main.7810c544.chunk.js"
+  },
+  {
+    "revision": "4d6b2623a71cf2ac456b",
+    "url": "/static/static/js/1.4d6b2623.chunk.js"
+  },
+  {
+    "revision": "7810c5446b486c41b8c3",
+    "url": "/static/static/css/main.87d51191.chunk.css"
+  },
+  {
+    "revision": "4d6b2623a71cf2ac456b",
+    "url": "/static/static/css/1.0ed9f2c4.chunk.css"
+  },
+  {
+    "revision": "7729e901e256ec3a58b6477f73e0a1d2",
+    "url": "/static/index.html"
+  }
+];
