26d725266a4d00b09edab31c96e67fd3ec30b04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:09 +0000

    Add package fx-kirin/0.1/fx-kirin-0.1.tar.gz

---
+++ packages/fx-kirin/fx-kirin-0.1.tar.gz/fx-kirin-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="fx-kirin",
+    author="fx-kirin",
+    author_email="ono.kirin@gmail.com",
+    url="https://github.com/fx-kirin/kanipath",
+    version="0.1",
+    packages=["kanipath"],
+    license="MIT license",
+    long_description=open("README.txt").read())
+    
