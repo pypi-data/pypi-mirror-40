a3c7509afc8897bb315c67fa129c3c39c2eddcf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:59 +0000

    Add package xslha/1.0.2/xslha-1.0.2.tar.gz

---
+++ packages/xslha/xslha-1.0.2.tar.gz/xslha-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xslha",
+    version="1.0.2",
+    author="Florian Staub",
+    author_email="florian.staub@gmail.com",
+    description="A python package to read (big/many) SLHA files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fstaub/xSLHA",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS"
+    ],
+)
