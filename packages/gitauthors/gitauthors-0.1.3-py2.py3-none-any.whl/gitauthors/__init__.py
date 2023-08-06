5ffd055c549a7272ba615d4707b9238c298a0b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:27 +0000

    Add package gitauthors/0.1.3/gitauthors-0.1.3-py2.py3-none-any.whl

---
+++ packages/gitauthors/gitauthors-0.1.3-py2.py3-none-any.whl/gitauthors/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+#
+# GitAuthors - A simple tool that prints a quick summary of a repo's authors.
+#
+# Ansgar Grunseid
+# grunseid.com
+# grunseid@gmail.com
+#
+# License: MIT
+#
+
+
+from gitauthors import collateGitAuthors, formatGitAuthors
