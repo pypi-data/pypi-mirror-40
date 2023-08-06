ae3525c22862e6f6e0a44d4fc4ff108cdc61fc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:26 +0000

    Add package 3890612457908641/1.0.0/3890612457908641-1.0.0.tar.gz

---
+++ packages/3890612457908641/3890612457908641-1.0.0.tar.gz/3890612457908641-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup  (
+             name              = '3890612457908641',
+             version            = '1.0.0',
+             py_modules    = ['3890612457908641'],
+             author             = 'Monicabear',
+             author_email  = '118010331@link.cuhk.edu.cn',
+             url                   = 'https://i.cuhk.edu.cn',
+             description      = 'A simple function that show elements in a list that may or may not contain a nested list.',
+            )
