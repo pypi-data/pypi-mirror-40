abffd936a34b62961643f7343cbdc60fef826a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:10 +0000

    Add package fithic/2.0.6/fithic-2.0.6.tar.gz

---
+++ packages/fithic/fithic-2.0.6.tar.gz/fithic-2.0.6/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+version_py = "fithic/_version.py"
+exec(open(version_py).read())
+
+setup(
+    name = "fithic",
+    version =__version__,
+    description = 'Hi-C Analysis software created and maintained by the Ay Lab',
+    url = 'http://github.com/ay-lab/fithic/',
+    entry_points = {
+        "console_scripts": ['fithic = fithic.fithic:main']
+        },
+    python_requires = '~=3.6',
+    author = 'Ferhat Ay',
+    author_email = 'ferhatay@lji.org',
+    license = 'MIT',
+    packages = ['fithic'],
+    install_requires = [
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'scikit-learn',
+        'sortedcontainers',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    zip_safe = False,
+  )
