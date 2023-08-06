aa5b7419270d2217989774c4d534f437253777e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:18 +0000

    Add package cellunet/0.1.8/cellunet-0.1.8.tar.gz

---
+++ packages/cellunet/cellunet-0.1.8.tar.gz/cellunet-0.1.8/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['cellunet', 'cellunet.utils']
+
+package_data = \
+{'': ['*'], 'cellunet': ['data/*', 'etc/*']}
+
+install_requires = \
+['keras==2.0.0',
+ 'numpy==1.14.6',
+ 'pandas==0.23.0',
+ 'scikit-image==0.14.1',
+ 'scipy==1.1.0',
+ 'tensorflow==1.8.0',
+ 'tifffile>=2019.1,<2020.0']
+
+setup_kwargs = {
+    'name': 'cellunet',
+    'version': '0.1.8',
+    'description': '',
+    'long_description': None,
+    'author': 'Takamasa Kudo',
+    'author_email': 'kudo@stanford.edu',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
