1ae4edc71b652c05f4719d3e8cf1965e242f8384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:58 +0000

    Add package getup/0.0.10/getup-0.0.10.tar.gz

---
+++ packages/getup/getup-0.0.10.tar.gz/getup-0.0.10/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['getup']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['black', 'django-environ>=0.4.5,<0.5.0', 'django>=2.1,<3.0']
+
+entry_points = \
+{'console_scripts': ['getup = getup.getup:main']}
+
+setup_kwargs = {
+    'name': 'getup',
+    'version': '0.0.10',
+    'description': 'Projectless Django setup tool',
+    'long_description': "\n# Getup\n\nProjectless Django setup tool\n\nSee Gitlab repository for small helper apps.\n\n## Getup config tool\n\nIncluded getup.py can function as projectless Django starter. Use provided sample conf file with it or substitute own with `GETUP_CONF_PATH` env variable.\n\nRun `python getup.py`, `./getup.py` or add the file into global $PATH to have it available everywhere there's Django app.\n\nFollowing settings can be set by env vars:\n\n  - GETUP_READ_ENV (set this to get others read)\n  - DATABASE_URL\n  - SECRET_KEY\n  - ALLOWED_HOSTS\n  - DATABASE_URL\n  - SENTRY_DSN\n",
+    'author': 'Jussi Arpalahti',
+    'author_email': 'jussi.arpalahti@gmail.com',
+    'url': 'https://gitlab.com/jussiarpalahti/getup',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
