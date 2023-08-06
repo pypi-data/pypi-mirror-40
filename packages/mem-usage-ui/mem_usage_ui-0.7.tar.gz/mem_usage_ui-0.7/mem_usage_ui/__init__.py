e10b8f3acb02a3f6027f0b9e4fe3a5e493d2cf54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:38 +0000

    Add package mem-usage-ui/0.7/mem_usage_ui-0.7.tar.gz

---
+++ packages/mem-usage-ui/mem_usage_ui-0.7.tar.gz/mem_usage_ui-0.7/mem_usage_ui/__init__.py
@@ -0,0 +1,12 @@
+import asyncio
+import sys
+
+import uvloop
+uvloop.install()
+
+
+__version__ = '0.7'
+
+if sys.version_info.minor < 7:
+    # alias for previous python versions
+    asyncio.create_task = asyncio.ensure_future
