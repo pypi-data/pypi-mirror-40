175ddf448115a0ead4bbb15344b707a97826d703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:01 +0000

    Add package sim-sm/0.8/sim-sm-0.8.tar.gz

---
+++ packages/sim-sm/sim-sm-0.8.tar.gz/sim-sm-0.8/setup.py
@@ -0,0 +1,28 @@
+#coding=utf-8
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='sim-sm',
+    description="Linhas de ônibus do SIM-SM no seu terminal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='0.8',
+    url='https://github.com/jviriato/sim-sm/',
+    author='José Victor Viriato',
+    license='MIT',
+    entry_points={
+               'console_scripts': ['simsm=simsm:main'],
+    },
+    author_email='josevviriato@gmail.com',
+    install_requires=['terminaltables', 'colorama', 'requests'],
+    packages=setuptools.find_packages(),   
+    keywords=['CLI', 'bus', 'python'],
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+)
