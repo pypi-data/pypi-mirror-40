8d3bced890c57d7ebf599b89cc2025e248c6b5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:56 +0000

    Add package clippings/0.4.0/clippings-0.4.0.tar.gz

---
+++ packages/clippings/clippings-0.4.0.tar.gz/clippings-0.4.0/setup.py
@@ -0,0 +1,54 @@
+import sys
+
+from setuptools import setup
+
+from clippings import __version__
+
+
+requirements = [
+    'python-dateutil==2.7.5',
+]
+
+if sys.version_info[0] == 2:
+    requirements.append('mock==2.0.0')
+
+
+setup(
+    name='clippings',
+    version=__version__,
+    description='Amazon Kindle clippings parser',
+    long_description=(open('README.rst').read()),
+    url='http://github.com/samueldg/clippings/',
+    download_url = 'https://github.com/samueldg/clippings/tarball/' + __version__,
+    install_requires=requirements,
+    license='MIT',
+    author='Samuel Dion-Girardeau',
+    author_email='samuel.diongirardeau@gmail.com',
+    packages=[
+        'clippings'
+    ],
+    entry_points={
+        'console_scripts': [
+            'clippings = clippings.parser:main',
+        ],
+    },
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    keywords=['amazon', 'kindle', 'clipping', 'e-book'],
+    zip_safe=False,
+)
