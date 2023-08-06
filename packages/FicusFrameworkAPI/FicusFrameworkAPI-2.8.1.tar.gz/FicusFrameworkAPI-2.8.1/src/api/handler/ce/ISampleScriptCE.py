4972beb96c79900fa801c4fbcd2d78f6e9df0f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:13 +0000

    Add package FicusFrameworkAPI/2.8.1/FicusFrameworkAPI-2.8.1.tar.gz

---
+++ packages/FicusFrameworkAPI/FicusFrameworkAPI-2.8.1.tar.gz/FicusFrameworkAPI-2.8.1/src/api/handler/ce/ISampleScriptCE.py
@@ -0,0 +1,18 @@
+from abc import abstractmethod
+
+from api.model.FdInputPipe import FdInputPipe
+
+class ISampleScriptCE:
+    """
+    Python脚本式的CE的基类
+    """
+
+    @abstractmethod
+    def do_compute(self,source_fds:FdInputPipe, params:dict):
+        """
+        计算器的业务逻辑
+        :param source_fds: 输入源FD
+        :param params: 计算器上下文参数
+        :return: 返回一个 OutputWrapper的 数组
+        """
+        pass
