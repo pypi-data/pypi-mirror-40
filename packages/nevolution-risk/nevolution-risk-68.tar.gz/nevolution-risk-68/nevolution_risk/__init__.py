9897e3c95cb1d67b189e3c25a00db10c36c6ffcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:06 +0000

    Add package nevolution-risk/68/nevolution-risk-68.tar.gz

---
+++ packages/nevolution-risk/nevolution-risk-68.tar.gz/nevolution-risk-68/nevolution_risk/__init__.py
@@ -0,0 +1,11 @@
+from gym.envs.registration import register
+
+register(
+    id='nevolution-risk-v1',
+    entry_point='nevolution_risk.v1:RiskEnv',
+)
+
+# register(
+#     id='nevolution-risk-v2',
+#     entry_point='nevolution_risk.v2.env:RiskEnv',
+# )
