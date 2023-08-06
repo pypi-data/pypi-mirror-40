4eb375bd7591ecf5fe4c49407773509fc2d4c16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:30 +0000

    Add package raiseorlaunch/2.2.1/raiseorlaunch-2.2.1-py3-none-any.whl

---
+++ packages/raiseorlaunch/raiseorlaunch-2.2.1-py3-none-any.whl/raiseorlaunch/__init__.py
@@ -0,0 +1,8 @@
+from raiseorlaunch.raiseorlaunch import (Raiseorlaunch,  # noqa
+                                         RaiseorlaunchError,  # noqa
+                                         check_positive,  # noqa
+                                         __title__,  # noqa
+                                         __description__,  # noqa
+                                         __version__,  # noqa
+                                         __license__,  # noqa
+                                         __author__)  # noqa
