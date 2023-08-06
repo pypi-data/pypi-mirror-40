54350b69936f543142b61ec21dd48ed95e0d469d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:49 +0000

    Add package sappgen/1.0.6/sappgen-1.0.6.tar.gz

---
+++ packages/sappgen/sappgen-1.0.6.tar.gz/sappgen-1.0.6/setup.py
@@ -0,0 +1,31 @@
+"""Setup script for sappgen"""
+
+import os.path
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+    name="sappgen",
+    version="1.0.6",
+    description="Simple App Generator - Python",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aayushuppal/sappgen",
+    author="Aayush Uppal",
+    author_email="aayuppal@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+    ],
+    packages=find_packages(exclude=("tests")),
+    include_package_data=True,
+    install_requires=[
+    ],
+    entry_points={"console_scripts": ["sappgen=sappgen.sappgen:main"]},
+)
