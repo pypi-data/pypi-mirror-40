4697d9c4124c6f224840fa4148e3f42424c952bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:45 +0000

    Add package cellxgene/0.5.0/cellxgene-0.5.0.tar.gz

---
+++ packages/cellxgene/cellxgene-0.5.0.tar.gz/cellxgene-0.5.0/server/app/web/webapp.py
@@ -0,0 +1,24 @@
+import os
+from flask import Blueprint, render_template, send_from_directory, current_app, request
+
+
+bp = Blueprint("webapp", __name__, template_folder="templates")
+
+
+@bp.route("/")
+def index():
+    url_base = request.url_root + "api/"
+    dataset_title = current_app.config["DATASET_TITLE"]
+    return render_template("index.html", prefix=url_base, datasetTitle=dataset_title)
+
+
+# renders swagger documentation
+@bp.route("/swagger")
+def swag():
+    return render_template("swagger.html")
+
+
+# renders swagger documentation
+@bp.route("/favicon.png")
+def favicon():
+    return send_from_directory(os.path.join(bp.root_path, "static/img/"), "favicon.png")
