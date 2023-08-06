9e93544e3d6869558fff0c2cf3128ed5d2b8ff94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:54 +0000

    Add package iduoliao-ml/1.0.101/iduoliao_ml-1.0.101.tar.gz

---
+++ packages/iduoliao-ml/iduoliao_ml-1.0.101.tar.gz/iduoliao_ml-1.0.101/iduoliao_ml/__init__.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import hot, es, dataDispose, itemCF, test
+
+def startUpdateHot(isTest):
+	es.setEnv(isTest)
+	hot.startUpdate(isTest, 'config.ini')
+
+def disposeData(isTest):
+	es.setEnv(isTest)
+	dataDispose.cycleDisposePersonas()
+
+def cfItem(isTest):
+	es.setEnv(isTest)
+	itemCF.cf()
+
+def updateHistoryActions(isTest):
+	es.setEnv(isTest)
+	dataDispose.updateHistoryActions()
+
+def test():
+	test.test()
