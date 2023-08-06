f1ee411e27b3aad253d89e4c5b10e000d8b61a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:38 +0000

    Add package videosplit/0.3/videosplit-0.3.tar.gz

---
+++ packages/videosplit/videosplit-0.3.tar.gz/videosplit-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='videosplit',
+    description="Utility to extract frames out of a video",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    version='0.3',
+    author="Johan Cervantes"
+)
