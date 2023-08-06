82ce550326b88996966eec74e71117d2ee64c218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:15 +0000

    Add package PySiTK/0.2.4/PySiTK-0.2.4.tar.gz

---
+++ packages/PySiTK/PySiTK-0.2.4.tar.gz/PySiTK-0.2.4/setup.py
@@ -0,0 +1,43 @@
+###
+# \file setup.py
+#
+# Install with symlink: 'pip install -e .'
+# Changes to the source file will be immediately available to other users
+# of the package
+#
+# \author     Michael Ebner (michael.ebner.14@ucl.ac.uk)
+# \date       July 2017
+#
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='PySiTK',
+      version='0.2.4',
+      description='Python SimpleITK/WrapITK helper scripts',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/gift-surg/PySiTK',
+      author='Michael Ebner',
+      author_email='michael.ebner.14@ucl.ac.uk',
+      license='BSD-3-Clause',
+      packages=['pysitk'],
+      zip_safe=False,
+      keywords='development ITK SimpleITK',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: Healthcare Industry',
+          'Intended Audience :: Science/Research',
+
+          'License :: OSI Approved :: BSD License',
+
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Scientific/Engineering :: Medical Science Apps.',
+
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+      )
