c1aaf6a2c999101df7b04e687805487d6e444f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:44 +0000

    Add package qibullet/0.0.3/qibullet-0.0.3.tar.gz

---
+++ packages/qibullet/qibullet-0.0.3.tar.gz/qibullet-0.0.3/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qibullet",
+    version="0.0.3",
+    author="Maxime Busy, Maxime Caniot",
+    author_email="",
+    description="Bullet simulation for SBR robots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ProtolabSBRE/qibullet",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'pybullet'],
+    package_data={"qibullet": [
+        "robot_data/pepper_1.7/*.urdf",
+        "robot_data/pepper_1.7/meshes/*.obj",
+        "robot_data/pepper_1.7/meshes/*.mtl",
+        "robot_data/pepper_1.7/meshes/*.stl",
+        "robot_data/pepper_1.7/meshes/*.png",
+        "robot_data/nao_V40/*.urdf",
+        "robot_data/nao_V40/meshes/*.obj",
+        "robot_data/nao_V40/meshes/*.mtl",
+        "robot_data/nao_V40/meshes/*.stl",
+        "robot_data/nao_V40/meshes/*.png"]},
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft"
+    ]
+)
