e1db9123203542430d1d1fbf7b738223cab6c9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:09 +0000

    Add package randtext/0.1.2/randtext-0.1.2.tar.gz

---
+++ packages/randtext/randtext-0.1.2.tar.gz/randtext-0.1.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="randtext",
+    version="0.1.2",
+    author="Mantaseus",
+    description = 'Generate random text with a considerable amount of control ',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/Mantaseus/randtext',
+    license = 'MIT',
+
+    packages=setuptools.find_packages(),
+    package_dir = {'randtext': 'randtext'},
+    package_data = {'randtext': ['dictionary/*']},
+    include_package_data = True,
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'pathlib'
+    ]
+)
