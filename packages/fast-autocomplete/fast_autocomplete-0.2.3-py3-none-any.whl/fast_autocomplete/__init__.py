6e840c88bf54b6157f837fb0502cc648d5bd1723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:56 +0000

    Add package fast-autocomplete/0.2.3/fast_autocomplete-0.2.3-py3-none-any.whl

---
+++ packages/fast-autocomplete/fast_autocomplete-0.2.3-py3-none-any.whl/fast_autocomplete/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+__version__ = '0.2.3'
+import sys
+pyversion = float(sys.version[:3])
+if pyversion < 3.6:
+    sys.exit('fast-autocomplete requires Python 3.6 or later.')
+
+from fast_autocomplete.dwg import AutoComplete
+from fast_autocomplete.draw import DrawGraphMixin
+from fast_autocomplete.demo import demo
