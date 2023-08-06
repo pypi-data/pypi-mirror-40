dcf55f189ea05a99ccbd3106c618372ec35ec4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:19 +0000

    Add package astroML/0.4rc1/astroML-0.4rc1.tar.gz

---
+++ packages/astroML/astroML-0.4rc1.tar.gz/astroML-0.4rc1/astroML/plotting/settings.py
@@ -0,0 +1,23 @@
+def setup_text_plots(fontsize=8, usetex=True):
+    """
+    This function adjusts matplotlib settings so that all figures in the
+    textbook have a uniform format and look.
+    """
+    import matplotlib
+    from distutils.version import LooseVersion
+    matplotlib.rc('legend', fontsize=fontsize, handlelength=3)
+    matplotlib.rc('axes', titlesize=fontsize)
+    matplotlib.rc('axes', labelsize=fontsize)
+    matplotlib.rc('xtick', labelsize=fontsize)
+    matplotlib.rc('ytick', labelsize=fontsize)
+    matplotlib.rc('text', usetex=usetex)
+    matplotlib.rc('font', size=fontsize, family='serif',
+                  style='normal', variant='normal',
+                  stretch='normal', weight='normal')
+    matplotlib.rc('patch', force_edgecolor=True)
+    if LooseVersion(matplotlib.__version__) < LooseVersion("3.1"):
+        matplotlib.rc('_internal', classic_mode=True)
+    else:
+        # New in mpl 3.1
+        matplotlib.rc('scatter.edgecolors', 'b')
+    matplotlib.rc('grid', linestyle=':')
