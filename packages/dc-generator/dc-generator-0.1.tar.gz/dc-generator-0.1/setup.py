cff27e755e7457359cc768cde7bdee11ed188034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:22 +0000

    Add package dc-generator/0.1/dc-generator-0.1.tar.gz

---
+++ packages/dc-generator/dc-generator-0.1.tar.gz/dc-generator-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(name='dc-generator',
+        version='0.1',
+        description='efficiency formula of DC Generator  ',
+        url='https://github.com/zizonk/nbvcx',
+        author='M afif Nur Azizi',
+        author_email='afifnurazizi@gmail.com',
+        license='MIT',
+        packages=['dc-generator'],
+        
+        dependency_links=[
+                'https://github.com/zizonk/nbvcx'
+        ],
+        
+    long_description_content_type="text/markdown",
+        zip_safe=False)
