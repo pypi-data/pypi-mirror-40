e71f0d457c93c04ed3cdb7c1126198540e675bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:58 +0000

    Add package rpctool/0.1.0/rpctool-0.1.0.tar.gz

---
+++ packages/rpctool/rpctool-0.1.0.tar.gz/rpctool-0.1.0/rpctool/format_markdown.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: DIYer22@github
+@mail: ylxx@live.com
+Created on Wed Jan  9 17:42:16 2019
+"""
+from boxx import Markdown
+import pandas as pd
+import numpy as np
+
+
+
+
+if __name__ == "__main__":
+    Markdown.test()
+    df = pd.DataFrame([{"a":1, "b":1,}, {"a":1, "bbbbbb":1,}, {"a":1, "bbbbbb":1/2, "c":1/3}])
+    pass
+    
+    
+    
