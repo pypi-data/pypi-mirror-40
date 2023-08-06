8ae2fa609fb97c48eb1618d9fcae88e53875c972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:50 +0000

    Add package kgekit/0.10.0/kgekit-0.10.0.tar.gz

---
+++ packages/kgekit/kgekit-0.10.0.tar.gz/kgekit-0.10.0/kgedata/triple_expander.cpp
@@ -0,0 +1,88 @@
+#include "triple_expander.h"
+
+namespace kgedata {
+
+pair<py::array_t<int64_t, py::array::c_style>, py::list> expand_triple_batch(
+    py::array_t<bool, py::array::c_style | py::array::forcecast>& batch,
+    int64_t num_entity,
+    int64_t num_relation,
+    bool expand_entity,
+    bool expand_relation)
+{
+    int64_t num_expands = 0;
+    if (expand_entity) {
+        num_expands += num_entity * 2;
+    } else {
+        num_entity = 0;
+    }
+    if (expand_relation) {
+        num_expands += num_relation;
+    } else {
+        num_relation = 0;
+    }
+
+    ssize_t batch_size = batch.size() * num_expands;
+    const size_t size = batch_size * detail::kNumTripleElements;
+    int64_t* data = new int64_t[size];
+    auto p = static_cast<int64_t*>(batch.request().ptr);
+    py::list split_points;
+
+    auto base_offset = 0;
+    for (auto i = 0; i < batch.size(); ++i) {
+        py::tuple splits(4);
+        auto adr = p + i*detail::kNumTripleElements;
+        auto head = *adr;
+        auto relation = *(adr + detail::kTripleRelationOffestInABatch);
+        auto tail = *(adr + detail::kTripleTailOffestInABatch);
+
+        /* link prediction on head */
+        splits[0] = base_offset;
+        auto base_adr = data + i*base_offset*detail::kNumTripleElements;
+        for (auto i = 0; i < num_entity; ++i) {
+            auto data_adr = base_adr + i*detail::kNumTripleElements;
+            *data_adr = i;
+            *(data_adr + detail::kTripleRelationOffestInABatch) = relation;
+            *(data_adr + detail::kTripleTailOffestInABatch) = tail;
+        }
+
+        base_offset += num_entity;
+        base_adr = data + i*base_offset*detail::kNumTripleElements;
+        /* link prediction on tail */
+        splits[1] = base_offset;
+        for (auto i = 0; i < num_entity; ++i) {
+            auto data_adr = base_adr + i*detail::kNumTripleElements;
+            *data_adr = head;
+            *(data_adr + detail::kTripleRelationOffestInABatch) = relation;
+            *(data_adr + detail::kTripleTailOffestInABatch) = i;
+        }
+
+        base_offset += num_entity;
+        base_adr = data + i*base_offset*detail::kNumTripleElements;
+        /* link prediction on relation */
+        splits[2] = base_offset;
+        for (auto i = 0; i < num_relation; ++i) {
+            auto data_adr = base_adr + i*detail::kNumTripleElements;
+            *data_adr = head;
+            *(data_adr + detail::kTripleRelationOffestInABatch) = i;
+            *(data_adr + detail::kTripleTailOffestInABatch) = tail;
+        }
+        base_offset += num_relation;
+        splits[3] = base_offset;
+        split_points.append(splits);
+    }
+
+    // Create a Python object that will free the allocated
+    // memory when destroyed:
+    py::capsule free_when_done(data, [](void* f) {
+        int64_t* data = reinterpret_cast<int64_t*>(f);
+        delete[] data;
+    });
+
+    auto arr = py::array_t<int64_t, py::array::c_style>(
+        {batch_size, static_cast<ssize_t>(detail::kNumTripleElements)}, // shape
+        data, // the data pointer
+        free_when_done); // numpy array references this parent
+    return make_pair(arr, split_points);
+}
+
+}; // namespace kgedata
