1362ce11792a62f3f052ad29d95b1248e5d5cb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:22 +0000

    Add package omfvtk/0.0.4/omfvtk-0.0.4.tar.gz

---
+++ packages/omfvtk/omfvtk-0.0.4.tar.gz/omfvtk-0.0.4/omfvtk/surface.py
@@ -0,0 +1,93 @@
+"""Methods to convert surface objects to VTK data objects"""
+
+
+__all__ = [
+    'surface_geom_to_vtk',
+    'surface_grid_geom_to_vtk',
+    'surface_to_vtk',
+]
+
+__displayname__ = 'Surface'
+
+import vtk
+from vtk.util import numpy_support as nps
+import vtki
+
+import numpy as np
+
+def surface_geom_to_vtk(surfgeom):
+    """Convert the triangulated surface to a :class:`vtki.UnstructuredGrid`
+    object
+
+    Args:
+        surfgeom (:class:`omf.surface.SurfaceGeometry`): the surface geomotry to
+            convert
+    """
+
+    output = vtk.vtkUnstructuredGrid()
+    pts = vtk.vtkPoints()
+    cells = vtk.vtkCellArray()
+
+    # Generate the points
+    pts.SetNumberOfPoints(surfgeom.num_nodes)
+    pts.SetData(nps.numpy_to_vtk(surfgeom.vertices))
+
+    # Generate the triangle cells
+    cellConn = surfgeom.triangles.array
+    cellsMat = np.concatenate(
+        (np.ones((cellConn.shape[0], 1), dtype=np.int64)*cellConn.shape[1], cellConn),
+        axis=1).ravel()
+    cells = vtk.vtkCellArray()
+    cells.SetNumberOfCells(cellConn.shape[0])
+    cells.SetCells(cellConn.shape[0],
+            nps.numpy_to_vtk(cellsMat, deep=True, array_type=vtk.VTK_ID_TYPE))
+
+    # Add to output
+    output.SetPoints(pts)
+    output.SetCells(vtk.VTK_TRIANGLE, cells)
+    return vtki.wrap(output)
+
+
+def surface_grid_geom_to_vtk(surfgridgeom):
+    """Convert the 2D grid to a :class:`vtki.StructuredGrid` object.
+
+    Args:
+        surfgridgeom (:class:`omf.surface.SurfaceGridGeometry`): the surface
+            grid geometry to convert
+
+    """
+
+    output = vtk.vtkStructuredGrid()
+
+    # TODO: build!
+    # Build out all nodes in the mesh
+
+    # Add to output
+
+    return vtki.wrap(output)
+
+def surface_to_vtk(surfel):
+    """Convert the surface to a its appropriate VTK data object type.
+
+    Args:
+        surfel (:class:`omf.surface.SurfaceElement`): the surface element to
+            convert
+    """
+
+    output = surface_geom_to_vtk(surfel.geometry)
+
+    # TODO: handle textures
+
+    # Now add point data:
+    for data in surfel.data:
+        arr = data.array.array
+        c = nps.numpy_to_vtk(num_array=arr)
+        c.SetName(data.name)
+        output.GetPointData().AddArray(c)
+
+    return vtki.wrap(output)
+
+
+surface_to_vtk.__displayname__ = 'Surface to VTK'
+surface_grid_geom_to_vtk.__displayname__ = 'Surface Grid Geometry to VTK'
+surface_geom_to_vtk.__displayname__ = 'Surface Geometry to VTK'
