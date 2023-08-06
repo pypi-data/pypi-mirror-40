f10aae4e06f37ca80473e04847f83376cebad0fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:11 +0000

    Add package pyepayco/1.0.0/pyepayco-1.0.0.tar.gz

---
+++ packages/pyepayco/pyepayco-1.0.0.tar.gz/pyepayco-1.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="pyepayco",
+    version="1.0.0",
+    author="ePayco Development Team",
+    author_email="desarrollo@epayco.co",
+    packages=['pyepayco'],
+    url="https://github.com/epayco/epayco-python/tarball/0.1",
+    license="MIT",
+    description="Python library for ePayco Payment API",
+    long_description="Basic python library to interact with ePayco Payment API",
+    keywords = ['epayco', 'python', 'payco'],
+)
