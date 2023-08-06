46942a90e4bca24cf839d662cc451d78f584ddbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:19 +0000

    Add package contact-map/0.3.3/contact_map-0.3.3-py2.py3-none-any.whl

---
+++ packages/contact-map/contact_map-0.3.3-py2.py3-none-any.whl/contact_map/__init__.py
@@ -0,0 +1,20 @@
+try:
+    from . import version
+except ImportError:  # pragma: no cover
+    from . import _version as version
+
+__version__ = version.version
+
+from .contact_map import (
+    ContactMap, ContactFrequency, ContactDifference
+)
+
+from .contact_count import ContactCount
+
+from .min_dist import NearestAtoms, MinimumDistanceCounter
+
+from .dask_runner import DaskContactFrequency
+
+from . import plot_utils
+
+# import concurrence
