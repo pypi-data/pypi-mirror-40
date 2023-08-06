98c38d801115644a5b09c801ee7c0f774110e09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:28 +0000

    Add package dvtDecimal/1.3.0/dvtDecimal-1.3.0.tar.gz

---
+++ packages/dvtDecimal/dvtDecimal-1.3.0.tar.gz/dvtDecimal-1.3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='dvtDecimal',
+    version='1.3.0',
+    description='Repeating digits of rational numbers',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url='https://twitter.com/davidequantique',
+    author='David COBAC',
+    author_email='david.cobac@gmail.com',
+    license='CC-BY-NC-SA',
+    packages=find_packages()
+)
