b11dcfa6a5661ca8e59ac1ff4c0620ac431c11de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:13 +0000

    Add package polka/2.1.0/polka-2.1.0.tar.gz

---
+++ packages/polka/polka-2.1.0.tar.gz/polka-2.1.0/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['polka']
+setup_kwargs = {
+    'name': 'polka',
+    'version': '2.1.0',
+    'description': 'A module provides access to the public Polka API. (https://polka.academy/).',
+    'long_description': '',
+    'author': 'Dima Koskin',
+    'author_email': 'dmksknn@gmail.com',
+    'url': 'https://github.com/dmkskn/polka/',
+    'py_modules': modules,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
