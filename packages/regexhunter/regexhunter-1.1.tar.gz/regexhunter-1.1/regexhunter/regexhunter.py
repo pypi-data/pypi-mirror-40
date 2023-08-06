2f2309dc55be2036b95c27207138c90bad4fe863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:38 +0000

    Add package regexhunter/1.1/regexhunter-1.1.tar.gz

---
+++ packages/regexhunter/regexhunter-1.1.tar.gz/regexhunter-1.1/regexhunter/regexhunter.py
@@ -0,0 +1,28 @@
+import re
+
+class RegexHunter: 
+    
+    # Sniff any email adress:
+    def sniff_emails(self, data):        
+        return re.findall(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", data)
+    
+    # Sniff Phones
+    def sniff_phones(self, data, phone_format="all"):        
+        phone_regex = {
+            'fr': r"(?:\+33|06|07)(?:\s|\.|-)\d{1,2}(?:(?:\s|.|-)\d{2}){3,4}",
+            'us': r"(?:\(\d{3}\)|\d{3}-)(?:\s|\.|-|\d)\d{2,3}(?:\s|\.|-)\d{4}",
+            'int': r"\+\d{1,3}(?:(?:-|\.|\s)\d{3,4}){3,4}"
+        }
+        
+        phone_regex['all'] = re.compile("(%s|%s|%s)" % (phone_regex['fr'], phone_regex['us'], phone_regex['int']))
+        
+        if phone_format not in phone_regex.keys():
+            print('Wrong format, check out available ones:\n{}'.format(list(phone_regex.keys())))
+        else:        
+            return re.findall(phone_regex[phone_format], data)
+    
+    # Sniff postal code
+    def sniff_cp_zip(self, data):
+        return re.findall(r"\D(\d{5})\D", data)
+
+    name="lol"
---
+++ packages/regexhunter/regexhunter-1.1.tar.gz/regexhunter-1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="regexhunter",
+    version="1.1",
+    author="Tom Moritz",
+    author_email="tom.moritz@outlook.fr",
+    description="A small package allowing you to search for regex pre-formatted for you.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SevenOfHurt/RegexHunter/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
