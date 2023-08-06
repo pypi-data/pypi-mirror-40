be53dce20e2e048a9fbe0136d04790ff4286e468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:20 +0000

    Add package serve-swagger/1.2/serve-swagger-1.2.tar.gz

---
+++ packages/serve-swagger/serve-swagger-1.2.tar.gz/serve-swagger-1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from os import path
+from sys import exit
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='serve-swagger',
+    version='1.2',
+    url='https://github.com/joranbeasley/FlaskSwaggerAPIServer',
+    license='GPL',
+    author='Joran Beasley',
+    author_email='joranbeasley@gmail.com',
+    description='serve swagger api docs locally fast',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['serve_swagger'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask'],
+    entry_points={
+        'console_scripts': ['serve-swagger=serve_swagger.app:serve']
+    }
+)
