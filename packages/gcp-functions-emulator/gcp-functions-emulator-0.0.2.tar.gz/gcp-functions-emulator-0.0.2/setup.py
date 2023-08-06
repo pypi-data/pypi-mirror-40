4da6857ad36deadaf025347ce428ea94a8e24362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:27 +0000

    Add package gcp-functions-emulator/0.0.2/gcp-functions-emulator-0.0.2.tar.gz

---
+++ packages/gcp-functions-emulator/gcp-functions-emulator-0.0.2.tar.gz/gcp-functions-emulator-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    install_requires=['Flask'],
+    packages=find_packages(),
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
