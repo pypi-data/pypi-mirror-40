fa171bbddb30fde46d1173f66f80b0bde85be132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:48 +0000

    Add package easy-multip/0.1/easy_multip-0.1.tar.gz

---
+++ packages/easy-multip/easy_multip-0.1.tar.gz/easy_multip-0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='easy_multip',
+      version='0.1',
+      packages=['easy_multip'],
+      license='MIT',
+      author='Zach Bateman',
+      url='https://github.com/zachbateman/easy_multip.git',
+      download_url='https://github.com/zachbateman/easy_multip/archive/v_0.1.tar.gz',
+      keywords=['MULTIPROCESSING', 'SIMPLE', 'EASY', 'PARALLEL'],
+      install_requires=['tqdm'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   ]
+)
