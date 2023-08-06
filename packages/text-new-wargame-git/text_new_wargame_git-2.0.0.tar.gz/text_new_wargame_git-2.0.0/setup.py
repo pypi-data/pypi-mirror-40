4866ef852aa2c6b15a7faaa0131dad1b5f479854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:18 +0000

    Add package text-new-wargame-git/2.0.0/text_new_wargame_git-2.0.0.tar.gz

---
+++ packages/text-new-wargame-git/text_new_wargame_git-2.0.0.tar.gz/text_new_wargame_git-2.0.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import setuptools
+with open('README') as file:
+    readme = file.read()
+
+# NOTE: change the 'name' field below with some unique package name.
+# then update the url field accordingly.
+
+setup(
+    name='text_new_wargame_git',
+    version='2.0.0',
+    packages=['wargame'],
+    url='https://github.com/imshadab/wargame',
+    license='LICENSE.txt',
+    description='It is a test pkg ignore. I just want to test whether I am able to prepare it',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Mohd Shadab Khan',
+    author_email='khanshadab96@yahoo.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
