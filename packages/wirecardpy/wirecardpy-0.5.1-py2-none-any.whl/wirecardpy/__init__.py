7dd34c63cc3b0c5decd1f6ca53f1c2e095a36c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:57 +0000

    Add package wirecardpy/0.5.1/wirecardpy-0.5.1-py2-none-any.whl

---
+++ packages/wirecardpy/wirecardpy-0.5.1-py2-none-any.whl/wirecardpy/__init__.py
@@ -0,0 +1,8 @@
+from wirecardpy.utils.util import set_api_authorization, RequestException
+from wirecardpy.models.account import Account
+from wirecardpy.models.order import Order
+from wirecardpy.models.payment import Payment
+from wirecardpy.models.bank_account import BankAccount
+from wirecardpy.models.balance import Balance
+from wirecardpy.models.transfer import Transfer
+from wirecardpy.models.anticipation import Anticipation
