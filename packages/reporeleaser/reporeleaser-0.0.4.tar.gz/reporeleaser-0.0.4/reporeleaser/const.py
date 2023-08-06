89552cdb0213194cb7039b48a020043af2b33b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:32 +0000

    Add package reporeleaser/0.0.4/reporeleaser-0.0.4.tar.gz

---
+++ packages/reporeleaser/reporeleaser-0.0.4.tar.gz/reporeleaser-0.0.4/reporeleaser/const.py
@@ -0,0 +1,14 @@
+"""Default values."""
+BODY = "## Changes\n\n"
+CHANGELOG = "[changelog]: https://github.com/{}/compare/{}...{}"
+FOOTER = """
+***
+
+This release was created with [reporeleaser][reporeleaser] :tada:
+
+[reporeleaser]: https://pypi.org/project/reporeleaser/
+"""
+SEPERATOR = "----------------------------------------------------"
+RELEASETYPES = ['major', 'minor', 'patch']
+RELEASEURL = "https://github.com/{}/releases/tag/{}"
+VERSION = "{}.{}.{}"
