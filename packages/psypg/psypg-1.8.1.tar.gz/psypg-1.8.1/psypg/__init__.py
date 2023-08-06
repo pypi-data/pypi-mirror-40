fdb8df6d6f0d451f12044d453399c8f6138e0c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:53 +0000

    Add package psypg/1.8.1/psypg-1.8.1.tar.gz

---
+++ packages/psypg/psypg-1.8.1.tar.gz/psypg-1.8.1/psypg/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Simple Psycopg2 Wrapper
+
+:copyright: (c) 2015-2018 Gary Chambers
+:license: MIT
+'''
+
+from psycopg2 import IntegrityError
+from psycopg2 import InterfaceError
+from psycopg2 import DatabaseError
+from psycopg2 import OperationalError
+from psycopg2 import ProgrammingError
+from psycopg2 import InternalError
+from .psypg_wrap import PgConfig
+from .psypg_wrap import pg_query
+from .psypg_wrap import pg_commit
+from .psypg_wrap import pg_rollback
+from .psypg_wrap import pg_notify
+
+__title__ = 'psypg'
+__version__ = '1.8.1'
+__all__ = [
+    'IntegrityError', 'InterfaceError', 'DatabaseError',
+    'OperationalError', 'ProgrammingError', 'InternalError',
+    'PgConfig', 'pg_query', 'pg_notify', 'pg_commit', 'pg_rollback'
+]
