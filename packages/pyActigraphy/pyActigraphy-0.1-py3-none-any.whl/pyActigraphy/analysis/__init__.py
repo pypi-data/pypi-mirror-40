cc1389b9710ca10172f8a9dbcb24f412eb88b7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:51 +0000

    Add package pyActigraphy/0.1/pyActigraphy-0.1-py3-none-any.whl

---
+++ packages/pyActigraphy/pyActigraphy-0.1-py3-none-any.whl/pyActigraphy/analysis/__init__.py
@@ -0,0 +1,10 @@
+"""Functions to analyse actigraphy data."""
+
+# Authors: Grégory Hammad <gregory.hammad@uliege.be>
+#
+# License: BSD (3-clause)
+
+from .flm import FLM
+from .ssa import SSA
+
+__all__ = ["FLM", "SSA"]
