50db6abe462fcec0143ff002ef0386f6a25e2028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:01 +0000

    Add package intelex/0.0.24/intelex-0.0.24.tar.gz

---
+++ packages/intelex/intelex-0.0.24.tar.gz/intelex-0.0.24/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_desc = fh.read()
+
+setup(
+    name='intelex',
+    version='0.0.24',
+    description='Intelex SDK',
+    url='https://github.com/thomassampson/intelex_sdk',
+    author='Thomas Sampson',
+    author_email='sampsont91@gmail.com',
+    py_modules=["intelex"],
+    package_dir={'':'src'},
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    install_requires = [
+        'requests',
+    ],
+)
