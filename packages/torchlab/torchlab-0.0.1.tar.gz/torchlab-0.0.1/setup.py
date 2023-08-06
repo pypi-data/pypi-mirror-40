d2c245d98d5b6c1ba0d09e74ad8e9f35f1cd76bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:53 +0000

    Add package torchlab/0.0.1/torchlab-0.0.1.tar.gz

---
+++ packages/torchlab/torchlab-0.0.1.tar.gz/torchlab-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='torchlab',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.torchlab.ai',
+    author='TorchLab',
+    author_email='torchlab.ai@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
