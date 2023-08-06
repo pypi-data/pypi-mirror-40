46f99df48abb99b04e0f7ee6c3632f2ab8aac396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:38 +0000

    Add package satrapy/0.0dev/satrapy-0.0dev.tar.gz

---
+++ packages/satrapy/satrapy-0.0dev.tar.gz/satrapy-0.0dev/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='satrapy',
+      version='0.0dev',
+      description='autocorrect for the win',
+      author='Brigitta Sipocz',
+      )
