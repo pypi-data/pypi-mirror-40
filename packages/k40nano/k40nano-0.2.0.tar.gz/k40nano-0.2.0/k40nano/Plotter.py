48f3dece6c9fa263bfffd6d04ce4b35b20975130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:34 +0000

    Add package k40nano/0.2.0/k40nano-0.2.0.tar.gz

---
+++ packages/k40nano/k40nano-0.2.0.tar.gz/k40nano-0.2.0/k40nano/Plotter.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+
+class Plotter:
+    def __init__(self, current_x=0, current_y=0):
+        self.current_x = current_x
+        self.current_y = current_y
+        self.max_x = current_x
+        self.max_y = current_y
+        self.min_x = current_x
+        self.min_y = current_y
+        self.start_x = current_x
+        self.start_y = current_y
+        self.pen_down = False
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def open(self):
+        pass
+
+    def close(self):
+        pass
+
+    def move_abs(self, x, y):
+        self.move(x - self.current_x, y - self.current_y)
+
+    def move(self, dx, dy):
+        self.current_x += dx
+        self.current_y += dy
+        self.check_bounds()
+
+    def down(self):
+        self.pen_down = True
+
+    def up(self):
+        self.pen_down = False
+
+    def v_switch(self):
+        return False
+
+    def h_switch(self):
+        return False
+
+    def enter_concat_mode(self):
+        pass
+
+    def enter_compact_mode(self, speed=None, raster_step=None):
+        pass
+
+    def exit_compact_mode_finish(self):
+        pass
+
+    def exit_compact_mode_reset(self):
+        pass
+
+    def exit_compact_mode_break(self):
+        pass
+
+    def check_bounds(self):
+        self.min_x = min(self.min_x, self.current_x)
+        self.min_y = min(self.min_y, self.current_y)
+        self.max_x = max(self.max_x, self.current_x)
+        self.max_y = max(self.max_y, self.current_y)
---
+++ packages/k40nano/k40nano-0.2.0.tar.gz/k40nano-0.2.0/test/test_nanoplotter.py
@@ -0,0 +1,180 @@
+import os
+import unittest
+
+from k40nano import *
+
+
+class TestNanoController(unittest.TestCase):
+
+    def test_nano_controller_home(self):
+        filename = "test.egv"
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.home()
+        match = "IPP\n"
+        lines = []
+        with open(filename, "r+") as f:
+            lines.append(f.readline())
+        self.assertEqual(match, lines[0])
+        self.addCleanup(os.remove, filename)
+
+    def test_nano_controller_move(self):
+        filename = "test.egv"
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.move(1, 1)
+        match = "IBaRaS1P\n"
+        lines = []
+        with open(filename, "r+") as f:
+            lines.append(f.readline())
+        self.assertEqual(match, lines[0])
+        self.addCleanup(os.remove, filename)
+
+    def test_nano_controller_string_move(self):
+        filename = "test.egv"
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.enter_concat_mode()
+            plotter.move(1, 0)
+            plotter.move(0, 1)
+        match = "IBaNRaNS1P\n"
+        lines = []
+        with open(filename, "r+") as f:
+            lines.append(f.readline())
+        self.assertEqual(match, lines[0])
+        self.addCleanup(os.remove, filename)
+
+    def test_nano_controller_compact(self):
+        filename = "test.egv"
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.enter_compact_mode(50)
+            plotter.move(1, 0)
+            plotter.move(0, 1)
+            speed_code = LaserSpeed.get_code_from_speed(50.0, 0, plotter.board)
+        match = "I" + speed_code + "NRBS1EBaRaFNSE\n"
+        lines = []
+        with open(filename, "r+") as f:
+            lines.append(f.readline())
+        self.assertEqual(match, lines[0])
+        self.addCleanup(os.remove, filename)
+
+    def test_nano_controller_diagonal(self):
+        filename = "test.egv"
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.enter_compact_mode(50)
+            plotter.move(1, 1)
+            speed_code = LaserSpeed.get_code_from_speed(50.0, 0, plotter.board)
+        match = "I" + speed_code + "NRBS1EMaFNSE\n"
+        lines = []
+        with open(filename, "r+") as f:
+            lines.append(f.readline())
+        self.assertEqual(match, lines[0])
+        self.addCleanup(os.remove, filename)
+
+    def test_nano_controller_reset(self):
+        filename = "test.egv"
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.enter_compact_mode(50)
+            plotter.move(1, 0)
+            plotter.move(0, 1)
+            plotter.exit_compact_mode_reset()
+            plotter.enter_compact_mode()
+            plotter.move(1, 1)
+            speed_code = LaserSpeed.get_code_from_speed(50.0, 0, plotter.board)
+        match = "I" + speed_code + "NRBS1EBaRa@NSE" + speed_code + "NRBS1EMaFNSE\n"
+        lines = []
+        with open(filename, "r+") as f:
+            lines.append(f.readline())
+        self.assertEqual(match, lines[0])
+        self.addCleanup(os.remove, filename)
+
+    def test_nano_controller_speedchange(self):
+        filename = "test.egv"
+
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.enter_compact_mode(50)
+            plotter.move(1, 0)
+            plotter.move(0, 1)
+            plotter.exit_compact_mode_reset()
+            plotter.enter_compact_mode(75)
+            plotter.move(1, 1)
+            speed_code0 = LaserSpeed.get_code_from_speed(50.0, 0, plotter.board)
+            speed_code1 = LaserSpeed.get_code_from_speed(75.0, 0, plotter.board)
+        match = "I" + speed_code0 + "NRBS1EBaRa@NSE" + speed_code1 + "NRBS1EMaFNSE\n"
+        lines = []
+        with open(filename, "r+") as f:
+            lines.append(f.readline())
+        self.assertEqual(match, lines[0])
+        self.addCleanup(os.remove, filename)
+
+    def test_nano_controller_sc_rapidmove(self):
+        filename = "test.egv"
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.enter_compact_mode(50)
+            plotter.move(1, 0)
+            plotter.move(0, 1)
+            plotter.exit_compact_mode_reset()
+            plotter.move(2, 2)
+            plotter.enter_compact_mode(75)
+            plotter.move(1, 1)
+            speed_code0 = LaserSpeed.get_code_from_speed(50.0, 0, plotter.board)
+            speed_code1 = LaserSpeed.get_code_from_speed(75.0, 0, plotter.board)
+        match = "I" + speed_code0 + "NRBS1EBaRa@NSEBbRbN" + speed_code1 + "NRBS1EMaFNSE\n"
+        lines = []
+        with open(filename, "r+") as f:
+            lines.append(f.readline())
+        self.assertEqual(match, lines[0])
+        self.addCleanup(os.remove, filename)
+
+    def test_nano_controller_rapidmove(self):
+        filename = "test.egv"
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.enter_compact_mode(50)
+            plotter.move(1, 0)
+            plotter.move(0, 1)
+            plotter.exit_compact_mode_reset()
+            plotter.move(2, 2)
+            plotter.enter_compact_mode()
+            plotter.move(1, 1)
+            speed_code0 = LaserSpeed.get_code_from_speed(50.0, 0, plotter.board)
+        match = "I" + speed_code0 + "NRBS1EBaRa@NSEBbRbN" + speed_code0 + "NRBS1EMaFNSE\n"
+        lines = []
+        with open(filename, "r+") as f:
+            lines.append(f.readline())
+        self.assertEqual(match, lines[0])
+        self.addCleanup(os.remove, filename)
+
+    def test_nano_controller_default(self):
+        filename = "test.egv"
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.move(1, 0)
+            plotter.move(0, 1)
+            plotter.move(2, 2)
+            plotter.move(-1, -1)
+        lines = []
+        with open(filename, "r+") as f:
+            while True:
+                line = f.readline()
+                if line is None or len(line) == 0:
+                    break
+                lines.append(line)
+        self.assertEqual("IBaS1P\n", lines[0])
+        self.assertEqual("IRaS1P\n", lines[1])
+        self.assertEqual("IBbRbS1P\n", lines[2])
+        self.assertEqual("ITaLaS1P\n", lines[3])
+        self.addCleanup(os.remove, filename)
+
+    def test_nano_controller_concat(self):
+        filename = "test.egv"
+        with NanoPlotter(connection=FileWriteConnection(filename)) as plotter:
+            plotter.enter_concat_mode()
+            plotter.move(1, 0)
+            plotter.move(0, 1)
+            plotter.move(2, 2)
+            plotter.move(-1, -1)
+        lines = []
+        with open(filename, "r+") as f:
+            while True:
+                line = f.readline()
+                if line is None or len(line) == 0:
+                    break
+                lines.append(line)
+        self.assertEqual("IBaNRaNBbRbNTaLaNS1P\n", lines[0])
+        self.addCleanup(os.remove, filename)
