cecc69fab724f288818b06b441026c6a7d6953f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package kerwin/0.2/kerwin-0.2.tar.gz

---
+++ packages/kerwin/kerwin-0.2.tar.gz/kerwin-0.2/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup, find_packages
+
+except ImportError:
+    from distutils.core import setup
+
+
+
+setup(
+    name = 'kerwin',
+    version = '0.2',
+    keywords = ['kerwin', 'fun', 'try'],
+    description = 'just for fun',
+    license = 'MIT License',
+
+    url = 'https://github.com/kerwin6182828/kerwin_code_files',
+    author = 'kerwin',
+    author_email = 'kerwin19950830@gmail.com',
+
+    packages = find_packages(where=".", exclude=(), include=("*",)),
+    install_requires = ["numpy"],
+    include_package_data = True,
+    platforms = 'any',
+    long_description = 'just learn it, nothing special function...   ',
+)
+
