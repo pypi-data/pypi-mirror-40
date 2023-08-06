e019dab7b9f025d35a96c75af9e5657c8078a880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:28 +0000

    Add package genedom/0.1.2/genedom-0.1.2.tar.gz

---
+++ packages/genedom/genedom-0.1.2.tar.gz/genedom-0.1.2/setup.py
@@ -0,0 +1,19 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+exec(open('genedom/version.py').read()) # loads __version__
+
+setup(name='genedom',
+      version=__version__,
+      author='Zulko',
+    description='Genetic parts standardization',
+    long_description=open('pypi-readme.rst').read(),
+    license='see LICENSE.txt',
+    keywords="genetic DNA part standardization synthetic biology",
+    packages= find_packages(exclude='docs'),
+    include_package_data=True,
+    install_requires=("dnachisel", "snapgene_reader", "pdf_reports",
+                      "pandas", "dna_features_viewer", "python-box",
+                      "openpyxl", "sequenticon"))
