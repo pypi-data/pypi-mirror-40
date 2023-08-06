b90d1b3f6c4d93b1e848b701d276f8b32017334d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:15 +0000

    Add package elink-sdk/1.0.1/elink-sdk-1.0.1.tar.gz

---
+++ packages/elink-sdk/elink-sdk-1.0.1.tar.gz/elink-sdk-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='elink-sdk',
+    version='1.0.1',
+    author="ELinkGate",
+    author_email="kevin.truong@elinkgate.com",
+    description="ELink SDK for python",
+    long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="http://www.elinkgate.com",
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
