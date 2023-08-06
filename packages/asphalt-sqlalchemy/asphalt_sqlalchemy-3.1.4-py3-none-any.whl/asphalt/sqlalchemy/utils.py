739d87745a0d52f32286e6a157d2bbfe1401e5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:10 +0000

    Add package asphalt-sqlalchemy/3.1.4/asphalt_sqlalchemy-3.1.4-py3-none-any.whl

---
+++ packages/asphalt-sqlalchemy/asphalt_sqlalchemy-3.1.4-py3-none-any.whl/asphalt/sqlalchemy/utils.py
@@ -0,0 +1,27 @@
+from typing import Iterable, Tuple, Optional  # noqa: F401
+
+from sqlalchemy.engine import Connectable
+from sqlalchemy.sql.schema import MetaData
+from typeguard import check_argument_types
+
+
+def clear_database(engine: Connectable, schemas: Iterable[str] = ()) -> None:
+    """
+    Clear any tables from an existing database.
+
+    :param engine: the engine or connection to use
+    :param schemas: full list of schema names to expect (ignored for SQLite)
+
+    """
+    assert check_argument_types()
+    metadatas = []
+    all_schemas = (None,)  # type: Tuple[Optional[str], ...]
+    all_schemas += tuple(schemas)
+    for schema in all_schemas:
+        # Reflect the schema to get the list of the tables, views and constraints
+        metadata = MetaData()
+        metadata.reflect(engine, schema=schema, views=True)
+        metadatas.append(metadata)
+
+    for metadata in metadatas:
+        metadata.drop_all(engine, checkfirst=False)
