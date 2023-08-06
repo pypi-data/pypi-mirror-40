91cb21db9c0499cb4f2eced2f2c88f44c5aff9a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:20 +0000

    Add package nft-sdk/2.1.0.2/nft-sdk-2.1.0.2.tar.gz

---
+++ packages/nft-sdk/nft-sdk-2.1.0.2.tar.gz/nft-sdk-2.1.0.2/nf/__version__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from __future__ import unicode_literals, print_function, absolute_import
+
+__title__ = 'nft-sdk'
+__description__ = 'NextFintech sdk'
+__url__ = 'https://nextFintech.io/'
+# __version__ = '0.3.1.1'
+__version__ = '2.1.0.2'
+__author__ = 'NextFintech'
+__author_email__ = 'support@NextFintech.com'
+__license__ = 'Apache 2.0'
+
