4cb4f1573f6ab3d7160e925814d091c38c9db352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:23 +0000

    Add package irrpro2/0.1/irrpro2-0.1.tar.gz

---
+++ packages/irrpro2/irrpro2-0.1.tar.gz/irrpro2-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='irrpro2',
+      version='0.1',
+      author='Dmitriy Proshkin',
+      author_email='dmitriyproshkin@gmail.com',
+      license='MIT',
+      packages=['irrpro2'],
+      install_requires=[
+      		'numpy',
+      		'lxml',
+      		'requests',
+      		'math',
+      		'time',
+      ],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      zip_safe=False)
