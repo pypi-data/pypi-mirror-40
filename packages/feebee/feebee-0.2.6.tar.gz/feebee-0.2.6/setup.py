e28a5a07e5571aad389983d2734cb3041da90152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:34 +0000

    Add package feebee/0.2.6/feebee-0.2.6.tar.gz

---
+++ packages/feebee/feebee-0.2.6.tar.gz/feebee-0.2.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='feebee',
+      version='0.2.6',
+      description='datawork tools',
+      url='https://github.com/nalssee/feebee.git',
+      author='nalssee',
+      author_email='kenjin@sdf.org',
+      license='MIT',
+      packages=['feebee'],
+      # Install statsmodels manually using conda install
+      # TODO: Not easy to install numpy and stuff without conda
+      install_requires=[
+          'sas7bdat>=2.0.7',
+          'psutil>=5.4.3',
+          'graphviz>=0.8.2',
+          'pathos>=0.2.2.1',
+          'xlrd>=0.9.0',
+          'pandas',
+      ],
+      zip_safe=False)
