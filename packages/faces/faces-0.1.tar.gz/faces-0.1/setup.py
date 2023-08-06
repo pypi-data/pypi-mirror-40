85e94425ecf253d7088c9a83929b0260c26d873e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:28 +0000

    Add package faces/0.1/faces-0.1.tar.gz

---
+++ packages/faces/faces-0.1.tar.gz/faces-0.1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+  name = 'faces',
+  packages = ['faces'],
+  version = '0.1',
+  license='MIT',
+  description = 'A Python library for developing desktop apps in a reactive way',
+  author = 'Ricardo Gomes',
+  author_email = 'desk467@gmail.com',
+  url = 'https://github.com/teamfaces/faces',
+  download_url = 'https://github.com/teamfaces/faces/releases/download/v0.1/faces-0.1.tar.gz',
+  keywords = ['gui', 'desktop-apps', 'mobile'],
+  install_requires=[
+          'tinycss',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: User Interfaces',
+
+    'Environment :: Win32 (MS Windows)',
+    'Environment :: X11 Applications',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
