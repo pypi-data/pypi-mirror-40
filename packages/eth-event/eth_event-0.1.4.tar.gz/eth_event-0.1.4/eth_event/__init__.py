03a20661495b217e9f32ed6edd8f1f188e791f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:02 +0000

    Add package eth-event/0.1.4/eth_event-0.1.4.tar.gz

---
+++ packages/eth-event/eth_event-0.1.4.tar.gz/eth_event-0.1.4/eth_event/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from .main import (
+    get_topics,
+    get_event_abi,
+    decode_event,
+    decode_logs,
+    decode_trace,
+    ABIError,
+    EventError,
+    StructLogError
+)
---
+++ packages/eth-event/eth_event-0.1.4.tar.gz/eth_event-0.1.4/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'eth_event',
+  packages = find_packages(),
+  version = '0.1.4',
+  license = 'MIT',
+  description = 'Ethereum event decoder and topic generator',
+  long_description =  long_description,
+  long_description_content_type="text/markdown",
+  author = 'Benjamin Hauser',
+  author_email = 'ben.hauser@hyperlink.capital',
+  url = 'https://github.com/iamdefinitelyahuman/eth-event',
+  download_url = 'https://github.com/iamdefinitelyahuman/eth-event/archive/0.1.4.tar.gz',
+  keywords = ['ethereum'],
+  install_requires=[
+          'eth-abi',
+          'eth-hash',
+          'hexbytes'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+) 
