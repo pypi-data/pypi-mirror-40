81c070572ea8773110906bd890da266c98c4ad30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:14 +0000

    Add package Monicabear1128/1.0.0/Monicabear1128-1.0.0.tar.gz

---
+++ packages/Monicabear1128/Monicabear1128-1.0.0.tar.gz/Monicabear1128-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+            name            = 'Monicabear1128',
+            version         = '1.0.0',
+            py_modules  = ['Monicabear1128'],
+            author           = 'Monicabear1128',
+            author_email = '118010331@link.cuhk.edu.cn',
+            description    = 'A simple printer of nested lists',
+          )
