093364a9f69690fe20efba634edcd0e40217e0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:41 +0000

    Add package wangjia20190108-nester/1.0.0/wangjia20190108_nester-%201.0.0.tar.gz

---
+++ packages/wangjia20190108-nester/wangjia20190108_nester-%201.0.0.tar.gz/wangjia20190108_nester- 1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'wangjia20190108_nester',
+        version     = ' 1.0.0',
+        py_modules  = ['nester'],
+        author      = 'wangjia911',
+        author_email= 'arsenal19871007@126.com',
+        url         = 'http://www.wangjia911.com',
+        description = 'A simple printer of nested lists',
+    )
