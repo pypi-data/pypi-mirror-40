7e38ed56da5f2aeaa1e879d30fd3b784bf6cb266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:51 +0000

    Add package addrext/0.0.6/addrext-0.0.6.tar.gz

---
+++ packages/addrext/addrext-0.0.6.tar.gz/addrext-0.0.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='addrext',
+    version='0.0.6',
+    description='Address Extractor',
+    url='https://github.com/hydraseq/addrext',
+    author='Efrain Olivares',
+    author_email='efrain.olivares@gmail.com',
+    license='MIT',
+    packages=['addrext'],
+    package_dir={'addrext': 'addrext'},
+    package_data={'addrext': ['data/*.csv', 'data/address/*.csv']},
+    include_package_data=True,
+    install_requires=['hydraseq'],
+    zip_safe=False
+    )
