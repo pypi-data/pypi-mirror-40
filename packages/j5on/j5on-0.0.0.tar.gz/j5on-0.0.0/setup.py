a370cb7aadd8c2c11b1e5d3aca7745b8f74b5b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:54 +0000

    Add package j5on/0.0.0/j5on-0.0.0.tar.gz

---
+++ packages/j5on/j5on-0.0.0.tar.gz/j5on-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='j5on',
+    version='0.0.0',
+    author='Anthony Sottile',
+    author_email='asottile@umich.edu',
+    url='https://github.com/asottile/j5on',
+)
