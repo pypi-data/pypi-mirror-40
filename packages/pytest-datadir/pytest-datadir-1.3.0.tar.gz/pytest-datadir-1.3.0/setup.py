f2bb9008fe51cfb38cf0aef3ca61d7b219d7956b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:32 +0000

    Add package pytest-datadir/1.3.0/pytest-datadir-1.3.0.tar.gz

---
+++ packages/pytest-datadir/pytest-datadir-1.3.0.tar.gz/pytest-datadir-1.3.0/setup.py
@@ -0,0 +1,43 @@
+import io
+import re
+
+from setuptools import setup
+
+
+with io.open('README.md', encoding='UTF-8') as fp:
+    long_description = fp.read()
+
+
+setup(
+    name="pytest-datadir",
+    use_scm_version={"write_to": "pytest_datadir/_version.py"},
+    setup_requires=['setuptools_scm'],
+    packages=['pytest_datadir'],
+    entry_points={
+        'pytest11': ['pytest-datadir = pytest_datadir.plugin'],
+    },
+    install_requires=['pytest>=2.7.0'],
+    data_files = [("", ["LICENSE"])],
+    author='Gabriel Reis',
+    author_email='gabrielcnr@gmail.com',
+    description='pytest plugin for test data directories and files',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    license='MIT',
+    keywords='pytest test unittest directory file',
+    extras_require={':python_version<"3.4"': ['pathlib2']},
+    url='http://github.com/gabrielcnr/pytest-datadir',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+    ],
+)
