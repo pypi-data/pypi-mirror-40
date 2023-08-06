02cc193e59c4fd750e3fc107b61f3fbcefc818a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:44 +0000

    Add package fitsio/0.9.12/fitsio-0.9.12.tar.gz

---
+++ packages/fitsio/fitsio-0.9.12.tar.gz/fitsio-0.9.12/fitsio/__init__.py
@@ -0,0 +1,30 @@
+"""
+A python library to read and write data to FITS files using cfitsio.
+See the docs at https://github.com/esheldon/fitsio for example
+usage.
+"""
+
+__version__='0.9.12'
+
+from . import fitslib
+from . import util
+
+from .fitslib import FITS
+from .fitslib import FITSHDR
+from .fitslib import FITSRecord
+from .fitslib import FITSCard
+
+from .fitslib import read
+from .fitslib import read_header
+from .fitslib import read_scamp_head
+from .fitslib import write
+from .fitslib import READONLY
+from .fitslib import READWRITE
+
+from .fitslib import BINARY_TBL, ASCII_TBL, IMAGE_HDU
+
+from .fitslib import FITSRuntimeWarning
+
+from .util import cfitsio_version
+
+from . import test
