54b0418e2faf5c7f695bb7d80d964668862bce22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:31 +0000

    Add package seveno-pyutil/0.5.1/seveno_pyutil-0.5.1.tar.gz

---
+++ packages/seveno-pyutil/seveno_pyutil-0.5.1.tar.gz/seveno_pyutil-0.5.1/src/seveno_pyutil/string_utilities.py
@@ -0,0 +1,42 @@
+from datetime import date, datetime
+
+try:
+    import simplejson as json
+except Exception:
+    import json
+
+
+def is_blank(obj):
+    """
+    True if line is empty string, None, string that contains only spaces and
+    space like characters, or line is iterable that contains only these kinds
+    of strings/objects
+    """
+    if not obj:
+        return True
+
+    if isinstance(obj, str):
+        return not obj.strip()
+
+    retv = False
+    try:
+        retv = all(is_blank(x) for x in obj)
+    except TypeError:
+        # ... not iterable
+        retv = False
+
+    return retv
+
+
+class JSONEncoderWithDateTime(json.JSONEncoder):
+    """
+    JSON encoder that supports date and datetime objects.
+
+    Example:
+
+        json.dumps(obj, cls=JSONEncoderWithDateTime)
+    """
+    def default(self, o):
+        if isinstance(o, (date, datetime)):
+            return o.isoformat()
+        return super().default(o)
