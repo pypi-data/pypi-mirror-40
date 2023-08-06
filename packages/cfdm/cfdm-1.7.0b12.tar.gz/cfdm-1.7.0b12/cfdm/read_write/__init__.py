76550ab004d924550388cd6ef56f9300d3f6e0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:29 +0000

    Add package cfdm/1.7.0b12/cfdm-1.7.0b12.tar.gz

---
+++ packages/cfdm/cfdm-1.7.0b12.tar.gz/cfdm-1.7.0b12/cfdm/read_write/__init__.py
@@ -0,0 +1,4 @@
+from .abstract           import IO, IORead, IOWrite, Implementation
+from .cfdmimplementation import CFDMImplementation
+from .read               import read
+from .write              import write
