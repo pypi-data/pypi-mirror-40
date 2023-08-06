30517f99c5bc4482758e2f2e70593806acc57812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:19 +0000

    Add package spvcm/0.2.1/spvcm-0.2.1.tar.gz

---
+++ packages/spvcm/spvcm-0.2.1.tar.gz/spvcm-0.2.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+import os
+
+fh = os.path.join(os.path.dirname(__file__), 'README.rst')
+with open(fh, 'r') as f:
+    long_description = f.read()
+
+setup(name='spvcm',
+      version='0.2.1',
+      long_description = long_description,
+      description='Fit spatial multilevel models and diagnose convergence',
+      url='https://github.com/ljwolf/spvcm',
+      author='Levi John Wolf',
+      author_email='levi.john.wolf@gmail.com',
+      license='3-Clause BSD',
+      packages= find_packages(),
+      install_requires=['numpy','scipy','libpysal', 'spreg', 'pandas','seaborn'],
+      include_package_data=True,
+      zip_safe=False)
