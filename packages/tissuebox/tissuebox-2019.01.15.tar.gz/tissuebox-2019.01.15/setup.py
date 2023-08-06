4826871fb385532a6e5721d0e896c73596a41e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:27 +0000

    Add package tissuebox/2019.01.15/tissuebox-2019.01.15.tar.gz

---
+++ packages/tissuebox/tissuebox-2019.01.15.tar.gz/tissuebox-2019.01.15/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+with open("tissuebox/pypi.rst", "r") as fh:
+    long_description = fh.read()
+
+import pdb;
+pdb.set_trace()
+
+setup(
+    name='tissuebox',
+    version='2019.01.15',
+    description='Tissuebox :: Pythonic payload validator',
+    author='nehem',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author_email='nehemiah.jacob@gmail.com',
+    url='https://github.com/nehemiahjacob/tissuebox.git',
+    packages=['tissuebox']
+)
