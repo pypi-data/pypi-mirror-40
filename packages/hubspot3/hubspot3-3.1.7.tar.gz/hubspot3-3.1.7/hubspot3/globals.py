efe8fbfb52aac0b339a373e30ddae88c6c545ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:29 +0000

    Add package hubspot3/3.1.7/hubspot3-3.1.7.tar.gz

---
+++ packages/hubspot3/hubspot3-3.1.7.tar.gz/hubspot3-3.1.7/hubspot3/globals.py
@@ -0,0 +1,9 @@
+"""
+globals file for hubspot3
+"""
+
+
+__version__ = "3.1.7"
+
+
+BASE_URL = "https://api.hubapi.com"
---
+++ packages/hubspot3/hubspot3-3.1.7.tar.gz/hubspot3-3.1.7/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+pip setup file
+"""
+from hubspot3.globals import __version__
+from setuptools import setup
+
+
+setup(
+    name="hubspot3",
+    version=__version__,
+    description=(
+        "A python wrapper around HubSpot's APIs, for python 3."
+        " Built initially around hapipy, but heavily modified."
+    ),
+    author="HubSpot Dev Team, Jacobi Petrucciani",
+    author_email="jacobi@mimirhq.com",
+    url="https://github.com/jpetrucciani/hubspot3.git",
+    download_url="https://github.com/jpetrucciani/hubspot3.git",
+    license="LICENSE",
+    packages=["hubspot3", "hubspot3.mixins"],
+    install_requires=["nose"],
+    zip_safe=False,
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
