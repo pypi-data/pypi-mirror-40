f15685df2bb680edb90c6c1d62ca48a40224f47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:26 +0000

    Add package pyscses/0.9.2/pyscses-0.9.2.tar.gz

---
+++ packages/pyscses/pyscses-0.9.2.tar.gz/pyscses-0.9.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from pyscses import __version__ as VERSION
+
+readme = 'README.md'
+long_description = open( readme ).read()
+
+config = {
+    'description': 'PYthon Space-Charge Site Explicit Solver',
+    'long_description': long_description,
+    'long_description_content_type': 'text/markdown',
+    'name': 'pyscses',
+    'author': 'Georgina L. Wellock',
+    'author_email': 'g.l.wellock@bath.ac.uk',
+    'packages': ['pyscses'],
+    'url': 'https://github.com/bjmorgan/pyscses',
+    'download_url': 'https://github.com/bjmorgan/pyscses/archive/%s.tar.gz' % (VERSION),
+    'version': VERSION,
+    'install_requires': open( 'requirements.txt' ).read(),
+    'license': 'MIT'
+}
+
+setup(**config)
