47e42e8fd733b088761de2a1c795bd9d3d7d76dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:32 +0000

    Add package pip-plant/0.2/pip-plant-0.2.tar.gz

---
+++ packages/pip-plant/pip-plant-0.2.tar.gz/pip-plant-0.2/setup.py
@@ -0,0 +1,37 @@
+"""
+pip-plant installs your dependencies.
+"""
+
+from setuptools import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+  name = 'pip-plant',
+  packages = ['pip-plant'],
+  version = '0.2',
+  license='MIT',
+  description = 'Plant simplifies Python package management for your projects.',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Ideas Labs',
+  author_email = 'saurabh.chaturvedi63@gmail.com',
+  url = 'https://github.com/Ideas-Labs/plant',
+  scripts=['plant/pip-plant'],
+  install_requires=[
+    'envoy',
+    ],
+  # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['pip', 'packaging', 'python'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
