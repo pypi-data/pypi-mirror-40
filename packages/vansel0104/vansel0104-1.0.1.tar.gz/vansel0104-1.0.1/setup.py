a4a06daa0f054b62699f59fa763f10fcf0a6f0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:21 +0000

    Add package vansel0104/1.0.1/vansel0104-1.0.1.tar.gz

---
+++ packages/vansel0104/vansel0104-1.0.1.tar.gz/vansel0104-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'vansel0104',
+        version = '1.0.1',
+        py_modules = ['vansel0104'],
+        author = 'van',
+        author_email = 'van@sina.com',
+        url = 'http://www.baidu.com',
+        description = 'add parameters',
+    )
