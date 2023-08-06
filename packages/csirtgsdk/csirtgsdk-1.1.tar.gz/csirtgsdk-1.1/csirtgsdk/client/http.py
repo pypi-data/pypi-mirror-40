50944917e2327eeb731e13a0f18e9360a667dcd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:31 +0000

    Add package csirtgsdk/1.1/csirtgsdk-1.1.tar.gz

---
+++ packages/csirtgsdk/csirtgsdk-1.1.tar.gz/csirtgsdk-1.1/csirtgsdk/client/http.py
@@ -0,0 +1,142 @@
+import json
+import requests
+import logging
+from csirtgsdk.exceptions import AuthError, TimeoutError, NotFound, SubmissionFailed, RateLimitExceeded, SystemBusy
+import os
+import random
+from time import sleep
+from csirtgsdk.constants import API_VERSION, TIMEOUT, REMOTE, LIMIT, TOKEN, \
+    VERSION
+import gzip
+
+RETRIES = os.getenv('CSIRTGSDK_CLIENT_HTTP_RETRIES', 5)
+RETRIES_DELAY = os.getenv('CSIRTGSDK_CLIENT_HTTP_RETRIES_DELAY', '30,60')
+
+s, e = RETRIES_DELAY.split(',')
+RETRIES_DELAY = random.uniform(int(s), int(e))
+
+logger = logging.getLogger(__name__)
+
+if os.getenv('CSIRTGSDK_HTTP_TRACE'):
+    logger.setLevel(logging.DEBUG)
+
+
+class HTTP(object):
+    def __init__(self, remote=REMOTE, token=TOKEN, proxy=None, timeout=TIMEOUT, verify_ssl=True):
+
+        self.logger = logger
+        self.remote = remote
+        self.token = str(token)
+        self.proxy = proxy
+        self.timeout = timeout
+        self.verify_ssl = verify_ssl
+
+        if not self.verify_ssl:
+            self.logger.debug('TLS Verification is OFF')
+
+        self.session = requests.session()
+        self.session.headers["Accept"] = 'application/vnd.csirtg.v{0}'.format(str(API_VERSION))
+        self.session.headers['User-Agent'] = 'csirtgsdk-python/{0}'.format(VERSION)
+        self.session.headers['Authorization'] = 'Token token=' + self.token
+        self.session.headers['Content-Type'] = 'application/json'
+        self.session.headers['Accept-Encoding'] = 'gzip'
+
+        #self.logger.debug(self.session.headers)
+
+    def _check_return(self, resp, expects=[200, 201]):
+        if isinstance(expects, int):
+            expects = [expects]
+
+        if resp.status_code in expects:
+            return True
+
+        if resp.status_code == 401:
+            raise AuthError()
+
+        if resp.status_code == 404:
+            raise NotFound()
+
+        if resp.status_code == 408:
+            raise TimeoutError()
+
+        if resp.status_code == 422:
+            raise SubmissionFailed()
+
+        if resp.status_code == 429:
+            raise RateLimitExceeded()
+
+        if resp.status_code in [500, 501, 502, 503, 504]:
+            raise SystemBusy()
+
+        raise RuntimeError(resp.text)
+
+    def _get(self, uri, params={}):
+        """
+        HTTP GET function
+
+        :param uri: REST endpoint
+        :param params: optional HTTP params to pass to the endpoint
+        :return: list of results (usually a list of dicts)
+
+        Example:
+            ret = cli.get('/search', params={ 'q': 'example.org' })
+        """
+
+        if not uri.startswith(self.remote):
+            uri = '{}{}'.format(self.remote, uri)
+
+        self.logger.debug(uri)
+        self.logger.debug(params)
+
+        resp = self.session.get(uri, params=params, verify=self.verify_ssl)
+
+        self._check_return(resp)
+        self.logger.debug(resp.headers)
+        return json.loads(resp.text)
+
+    get = _get
+
+    def _post(self, uri, data):
+        """
+        HTTP POST function
+
+        :param uri: REST endpoint to POST to
+        :param data: list of dicts to be passed to the endpoint
+        :return: list of dicts, usually will be a list of objects or id's
+
+        Example:
+            ret = cli.post('/indicators', { 'indicator': 'example.com' })
+        """
+
+        if not uri.startswith(self.remote):  # append self.remote if the uri doesn't include it
+            uri = '{}/{}'.format(self.remote, uri)
+            self.logger.debug(uri)
+
+        data = json.dumps(data)
+
+        resp = self.session.post(uri, data=data, verify=self.verify_ssl)
+
+        n = RETRIES
+        try:
+            self._check_return(resp)
+            n = 0
+        except Exception as e:
+            logger.error(e)
+            if resp.status_code not in [500, 501, 502, 503, 504]:
+                raise e
+
+        while n != 0:
+            logger.info('setting random retry interval to spread out the load')
+            logger.info('retrying in %.00fs' % RETRIES_DELAY)
+            sleep(RETRIES_DELAY)
+
+            resp = self.session.post(uri, data=data, verify=self.verify_ssl)
+            if self._check_return(resp):
+                break
+
+            if n == 0:
+                raise SystemBusy('system seems busy.. try again later')
+
+        return json.loads(resp.text)
+
+    post = _post
---
+++ packages/csirtgsdk/csirtgsdk-1.1.tar.gz/csirtgsdk-1.1/csirtgsdk/indicator.py
@@ -0,0 +1,227 @@
+import csirtgsdk.utils as utils
+import arrow
+import logging
+import base64
+import binascii
+from pprint import pprint
+import hashlib
+from six import string_types
+import os.path
+
+from csirtgsdk.client.http import HTTP as Client
+
+
+class Indicator(object):
+    """
+    Represents an Indicator object
+    """
+    def __init__(self, client, args):
+        """
+        :param client: csirtgsdk.client.Client object
+        :param args: dict https://github.com/csirtgadgets/csirtgsdk/wiki/API#indicators
+        :return: Indicator object
+
+        Example:
+            Indicator(cli, {
+                'indicator': 'example.org',
+                'tags': 'botnet',
+                'lasttime': '2015-01-01T00:00:59Z',
+                'comment': 'example comment',
+                'attachment': '/tmp/malware.zip'
+            }).create()
+        """
+
+        if not client:
+            client = Client()
+
+        self.logger = logging.getLogger(__name__)
+        self.client = client
+
+        required = set(['user', 'feed'])
+
+        if args is None or len(required - set(args.keys())) > 0:
+            raise Exception("invalid arguments. missing: {}"
+                            .format(required-set(args.keys())))
+
+        self.args = utils.Map(args)
+
+        if self.args.tags and isinstance(self.args.tags, string_types):
+            self.args.tags = self.args.tags.split(',')
+        
+        if self.args.firsttime:
+            self.args.firsttime = arrow.get(self.args.firsttime).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+        if self.args.lasttime:
+            self.args.lasttime = arrow.get(self.args.lasttime).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+    def show(self, user, feed, id):
+        """
+        Show a specific indicator by id
+
+        :param user: feed username
+        :param feed: feed name
+        :param id: indicator endpoint id [INT]
+        :return: dict
+
+        Example:
+            ret = Indicator.show('csirtgadgets','port-scanners', '1234')
+        """
+        uri = '/users/{}/feeds/{}/indicators/{}'.format(user, feed, id)
+        return self.client.get(uri)
+
+    def _file_to_attachment(self, blob, filename=None):
+        """
+
+        :param blob: a local file path or base64 encoded blob
+        :param filename: file path
+        :return: dict of base64 encoded filestring, with orig filename
+        """
+
+        if os.path.isfile(blob):
+            filename = blob
+            with open(blob, encoding="ISO-8859-1") as f:
+                data = f.read()
+        else:
+            if filename is None:
+                raise RuntimeError('missing filename')
+            try:
+                data = base64.b64decode(blob)
+            except TypeError:
+                raise RuntimeError('attachment must be base64 encoded')
+
+        md5 = hashlib.md5(data.encode('utf-8')).hexdigest()
+        sha1 = hashlib.sha1(data.encode('utf-8')).hexdigest()
+        sha256 = hashlib.sha256(data.encode('utf-8')).hexdigest()
+        sha512 = hashlib.sha512(data.encode('utf-8')).hexdigest()
+        data = base64.b64encode(data.encode('utf-8'))
+
+        return {
+            'data': data.decode("utf-8"),
+            'filename': filename,
+            'sha1': sha1,
+            'md5': md5,
+            'sha256': sha256,
+            'sha512': sha512
+        }
+
+    def comments(self, user, feed, id):
+        """
+        Return comments for a specific indicator id
+
+        :param user: feed username
+        :param feed: feed name
+        :param id: indicator id [INT]
+        :return: list
+
+        Example:
+            ret = Indicator.comments('csirtgadgets','port-scanners', '1234')
+        """
+        uri = '/users/{}/feeds/{}/indicators/{}/comments'.format(user, feed, id)
+        return self.client.get(uri)
+
+    def create(self):
+        """
+        Submit action on the Indicator object
+
+        :return: Indicator Object
+        """
+        uri = '/users/{0}/feeds/{1}/indicators'.format(self.args.user, self.args.feed)
+
+        data = {
+            "indicator": {
+                "indicator": self.args.indicator,
+                "tags": self.args.tags,
+                "description": self.args.description,
+                "portlist": self.args.portlist,
+                "protocol": self.args.protocol,
+                'firsttime': self.args.firsttime,
+                'lasttime': self.args.lasttime,
+                'portlist_src': self.args.portlist_src,
+                'content': self.args.content,
+                'provider': self.args.provider,
+            },
+            "comment": self.args.comment
+        }
+
+        if self.args.attachment:
+            attachment = self._file_to_attachment(self.args.attachment, filename=self.args.attachment_name)
+            data['attachment'] = {
+                'data': attachment['data'],
+                'filename': attachment['filename']
+            }
+
+        if not data['indicator'].get('indicator'):
+            data['indicator']['indicator'] = attachment['sha1']
+
+        if not data['indicator'].get('indicator'):
+            raise Exception('Missing indicator')
+
+        return self.client.post(uri, data)
+
+    submit = create
+
+    def create_bulk(self, indicators, user, feed):
+        from .constants import API_VERSION
+        if API_VERSION == '1':
+            print("create_bulk currently un-avail with APIv1")
+            raise SystemExit
+
+        """
+        Submit action against the IndicatorBulk endpoint
+
+        :param indicators: list of Indicator Objects
+        :param user: feed username
+        :param feed: feed name
+        :return: list of Indicator Objects submitted
+
+        from csirtgsdk.client import Client
+        from csirtgsdk.indicator import Indicator
+
+        remote = 'https://csirtg.io/api'
+        token = ''
+        verify_ssl = True
+
+        i = {
+            'indicator': 'example.com',
+            'feed': 'test',
+            'user': 'admin',
+            'comment': 'this is a test',
+        }
+
+        data = []
+
+        cli = Client(remote=remote, token=token, verify_ssl=verify_ssl)
+
+        for x in range(0, 5):
+            data.append(
+                Indicator(cli, i)
+            )
+
+        ret = cli.submit_bulk(data, 'csirtgadgets', 'test-feed')
+        """
+
+        uri = '/users/{0}/feeds/{1}/indicators_bulk'.format(user, feed)
+
+        data = {
+            'indicators': [
+                {
+                    'indicator': i.args.indicator,
+                    'feed_id': i.args.feed,
+                    'tag_list': i.args.tags,
+                    "description": i.args.description,
+                    "portlist": i.args.portlist,
+                    "protocol": i.args.protocol,
+                    'firsttime': i.args.firsttime,
+                    'lasttime': i.args.lasttime,
+                    'portlist_src': i.args.portlist_src,
+                    'comment': {
+                        'content': i.args.comment
+                    },
+                    'content': i.args.content,
+                    'provider': i.args.provider,
+                } for i in indicators
+                ]
+        }
+        return self.client._post(uri, data)
+
+    submit_bulk = create_bulk
