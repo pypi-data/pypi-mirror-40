d4ba0550f999ce571ad2d98c4b324ed4c1fe002c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:08 +0000

    Add package sunpy-sphinx-theme/1.1.7/sunpy-sphinx-theme-1.1.7.tar.gz

---
+++ packages/sunpy-sphinx-theme/sunpy-sphinx-theme-1.1.7.tar.gz/sunpy-sphinx-theme-1.1.7/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="sunpy-sphinx-theme",
+    version="1.1.7",
+    use_2to3=False,
+    description="The sphinx theme for the SunPy website and documentation.",
+    long_description="The sphinx theme for the SunPy website and documentation.",
+    author="The SunPy Developers",
+    install_requires=[
+        "setuptools",
+        "sphinx",
+        "sphinx-bootstrap-theme"
+    ],
+    packages=['sunpy_sphinx_theme'],
+    include_package_data=True,
+    license='2-clause BSD',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
