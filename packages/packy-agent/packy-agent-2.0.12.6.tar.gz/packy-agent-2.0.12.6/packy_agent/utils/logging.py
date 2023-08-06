86f8b129f72650d5b3c45dac88161e963e6d9e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:28 +0000

    Add package packy-agent/2.0.12.6/packy-agent-2.0.12.6.tar.gz

---
+++ packages/packy-agent/packy-agent-2.0.12.6.tar.gz/packy-agent-2.0.12.6/packy_agent/utils/logging.py
@@ -0,0 +1,21 @@
+import logging
+import logging.config
+from packy_agent.configuration.settings import settings
+
+DEFAULT_LOGGING_FORMAT = '%(asctime)s %(levelname)s %(name)s %(message)s'
+
+
+logger = logging.getLogger(__name__)
+
+
+def configure_logging_basic(level=logging.DEBUG):
+    logging.basicConfig(format=DEFAULT_LOGGING_FORMAT, level=level)
+
+
+def configure_logging(basic_level=logging.DEBUG):
+    configure_logging_basic(level=basic_level)
+    logger.info('Applied basic logging configuration first to debug logging configuration '
+                'issues themselves')
+    logging.config.dictConfig(settings.get_logging())
+    logging.getLogger().setLevel(settings.get_log_level())
+    logger.info('FINISHED applying logging configuration')
