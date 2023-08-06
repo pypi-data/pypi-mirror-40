d6a16d918f63d56b55ba4d790919c94788111098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:29 +0000

    Add package django-graphql-playground/1.0/django-graphql-playground-1.0.tar.gz

---
+++ packages/django-graphql-playground/django-graphql-playground-1.0.tar.gz/django-graphql-playground-1.0/README.rst
@@ -0,0 +1,3 @@
+# django-graphql-playground
+
+[Apollo GraphQL Playground](https://github.com/prisma/graphql-playground) as a Django view
---
+++ packages/django-graphql-playground/django-graphql-playground-1.0.tar.gz/django-graphql-playground-1.0/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-graphql-playground',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='Apollo GraphQL Playground as a Django view',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://jaydenwindle.com/',
+    author='Jayden Windle',
+    author_email='jaydenwindle@gmail.com',
+)
