458697c87c4e499c0f6481be7474fe686a1f1e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:57 +0000

    Add package iceaddr/0.3.3/iceaddr-0.3.3-py3-none-any.whl

---
+++ packages/iceaddr/iceaddr-0.3.3-py3-none-any.whl/iceaddr/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+"""
+    iceaddr: Look up information about Icelandic street addresses and postcodes
+    Copyright (c) 2018 Sveinbjorn Thordarson
+"""
+
+__author__ = "Sveinbjorn Thordarson"
+
+from .addresses import *
+from .postcodes import *
+from .placenames import *
