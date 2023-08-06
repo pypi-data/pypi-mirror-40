5bf79d906bb6d78f4e849f8576e08e214e5ad79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:43 +0000

    Add package py-buildsystem/0.3.2/py_buildsystem-0.3.2.tar.gz

---
+++ packages/py-buildsystem/py_buildsystem-0.3.2.tar.gz/py_buildsystem-0.3.2/py_buildsystem/__main__.py
@@ -0,0 +1,23 @@
+import argparse
+
+from .Project.Project import Project
+from .Toolchain.Toolchain import Toolchain
+
+sys.tracebacklimit = 0
+
+
+parser = argparse.ArgumentParser(description='Python based build system.')
+
+parser.add_argument('compiler_config', metavar='CC', type=str, nargs=1,
+                    help='Compiler configuration file')
+
+parser.add_argument('project_config', metavar='PC', type=str, nargs=1,
+                    help='Project configuration file')
+
+parser.add_argument('compiler_path', metavar='path', type=str, nargs='?', default='',
+                    help='Path to compiler')
+
+args = parser.parse_args()
+
+toolchain = Toolchain(args.compiler_config[0], args.compiler_path)
+project = Project(args.project_config[0], toolchain)
