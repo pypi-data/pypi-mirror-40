a3b15133001d2b7336f1fc99b451d6dfcee879c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:57 +0000

    Add package pytorch-stateful-lstm/1.1.0/pytorch_stateful_lstm-1.1.0.tar.gz

---
+++ packages/pytorch-stateful-lstm/pytorch_stateful_lstm-1.1.0.tar.gz/pytorch_stateful_lstm-1.1.0/pytorch_stateful_lstm/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for pytorch-stateful-lstm."""
+
+__author__ = """Hunt Zhan"""
+__email__ = 'huntzhan.dev@gmail.com'
+__version__ = '1.1.0'
+
+# pylint: disable=no-name-in-module
+from _pytorch_stateful_lstm import (
+        UnidirectionalSingleLayerLstm,
+        UnidirectionalLstm,
+        StatefulUnidirectionalLstm,
+)
