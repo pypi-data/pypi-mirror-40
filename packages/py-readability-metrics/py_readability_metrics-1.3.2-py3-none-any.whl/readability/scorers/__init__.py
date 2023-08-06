7bf07a08a53ac1612157074028a42552d6dad596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:06 +0000

    Add package py-readability-metrics/1.3.2/py_readability_metrics-1.3.2-py3-none-any.whl

---
+++ packages/py-readability-metrics/py_readability_metrics-1.3.2-py3-none-any.whl/readability/scorers/__init__.py
@@ -0,0 +1,9 @@
+
+from .flesch import Flesch
+from .flesch_kincaid import FleschKincaid
+from .gunning_fog import GunningFog
+from .coleman_liau import ColemanLiau
+from .dale_chall import DaleChall
+from .ari import ARI
+from .linsear_write import LinsearWrite
+from .smog import Smog
