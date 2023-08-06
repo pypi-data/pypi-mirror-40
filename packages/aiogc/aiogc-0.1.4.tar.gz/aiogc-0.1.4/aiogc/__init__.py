fd5b7deb09e711ee9778f1fe041812458a363850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:01 +0000

    Add package aiogc/0.1.4/aiogc-0.1.4.tar.gz

---
+++ packages/aiogc/aiogc-0.1.4.tar.gz/aiogc-0.1.4/aiogc/__init__.py
@@ -0,0 +1,7 @@
+GOOGLEAPIS_BASE_URL = 'https://www.googleapis.com'
+GOOGLE_TOKEN_URI = 'https://oauth2.googleapis.com/token'
+
+__name__ = "aiogc"
+
+from . import events, free_busy
+from .models import Credentials, Event, Time, Person
