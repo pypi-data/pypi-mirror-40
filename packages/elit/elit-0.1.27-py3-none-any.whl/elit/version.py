3f6f6ae115763e8bf5409955eacc52c1ad240684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:31 +0000

    Add package elit/0.1.27/elit-0.1.27-py3-none-any.whl

---
+++ packages/elit/elit-0.1.27-py3-none-any.whl/elit/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.1.27'
+version = '0.1.27'
+full_version = '0.1.27'
+dev_version = '1547828247'
+release = True
+if not release:
+    version = full_version
