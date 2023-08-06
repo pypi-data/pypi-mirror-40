a9e1c4916fd772bfbab6fb4b603f6f0511056bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:12 +0000

    Add package simpletime/0.0.3/simpletime-0.0.3.tar.gz

---
+++ packages/simpletime/simpletime-0.0.3.tar.gz/simpletime-0.0.3/README.md
@@ -0,0 +1,11 @@
+simpletime
+simple to date and time conver
+
+ver 0.0.1
+test to build module and install test
+
+ver 0.0.2
+change toStamp() return variable to int
+
+ver 0.0.3
+modify every function default argument to a empty list
---
+++ packages/simpletime/simpletime-0.0.3.tar.gz/simpletime-0.0.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+# from distutils.core import setup
+
+"""
+发布simpletime模块
+"""
+
+#__version__ = '0.0.1'
+#__author__  = 'Area0 Li <area0cn@gmail.com>'
+
+setup(
+    name          = 'simpletime',
+    version       = '0.0.3',
+    author        = 'Area0 Li <area0cn@gmail.com>',
+    author_email  = 'area0cn@gmail.com',
+    license       = 'Mozilla Public License 2.0',
+    description   = ('simple to date and time conver'),
+    #platforms     = '',
+    url           = 'https://github.com/area0li/simpletime',
+    #packages      =['simpletime'],
+    py_modules    = ['simpletime'],
+)
