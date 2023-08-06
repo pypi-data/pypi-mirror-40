d5f6277ae76c971d6fdd3b95f4d2147bc91119ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:59 +0000

    Add package rockset/0.5.10b20190114/rockset-0.5.10b20190114.tar.gz

---
+++ packages/rockset/rockset-0.5.10b20190114.tar.gz/rockset-0.5.10b20190114/rockset/integration/__init__.py
@@ -0,0 +1,9 @@
+from .integration import Integration
+from .aws import AWSIntegration
+from .aws import AWSExternalIdIntegration
+
+__all__ = [
+    'AWSExternalIdIntegration',
+    'AWSIntegration',
+    'Integration',
+]
