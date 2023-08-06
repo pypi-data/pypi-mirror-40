1a741925ebcf37b39e6f4287be3bc177de17337f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:06 +0000

    Add package ldapdomaindump/0.9.1/ldapdomaindump-0.9.1.tar.gz

---
+++ packages/ldapdomaindump/ldapdomaindump-0.9.1.tar.gz/ldapdomaindump-0.9.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(name='ldapdomaindump',
+      version='0.9.1',
+      description='Active Directory information dumper via LDAP',
+      author='Dirk-jan Mollema',
+      author_email='dirkjan@sanoweb.nl',
+      url='https://github.com/dirkjanm/ldapdomaindump/',
+      packages=['ldapdomaindump'],
+      install_requires=['dnspython', 'ldap3==2.5.1', 'future'],
+      package_data={'ldapdomaindump': ['style.css']},
+      include_package_data=True,
+      scripts=['bin/ldapdomaindump', 'bin/ldd2bloodhound']
+      )
