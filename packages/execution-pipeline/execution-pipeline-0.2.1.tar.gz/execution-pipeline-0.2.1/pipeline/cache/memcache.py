4b3ad5af1bf5a5bdd1ab9ef3cc19a6a21f28774e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:57 +0000

    Add package execution-pipeline/0.2.1/execution-pipeline-0.2.1.tar.gz

---
+++ packages/execution-pipeline/execution-pipeline-0.2.1.tar.gz/execution-pipeline-0.2.1/pipeline/cache/memcache.py
@@ -0,0 +1,40 @@
+import pickle
+import logging
+
+logger = logging.getLogger(__name__)
+
+try:
+    from memcache import Client
+except ImportError as e:
+    logger.warning('Could not import from memcache, using MockCache object instead.')
+    from . mock import MockCache as Client
+
+
+class MemCache(Client):
+
+    def __init__(self, host='localhost', port=11211, **kwargs):
+        """
+
+        :param args: (host, port)
+        """
+        args = (host, int(port))
+        super().__init__([
+            args
+        ], **kwargs)
+
+    def get(self, key=None, default=None):
+        cached_pickle = super().get(key)
+        cached_obj = pickle.loads(cached_pickle) if cached_pickle else default
+        return cached_obj
+
+    def set(self, key=None, obj=None, expiration_seconds=3600):
+        if key is None or obj is None:
+            logging.warning('object was not cached.')
+            return False
+        else:
+            pickled_obj = pickle.dumps(obj)
+            super().set(key, pickled_obj, expiration_seconds)
+        return True
+
+    def clear(self):
+        self.flushdb()
