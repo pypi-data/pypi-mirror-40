28ecf820b0bd4a45a67a04eb09edaf0ea5fd7d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:41 +0000

    Add package pychecktype/1.4.1/pychecktype-1.4.1.tar.gz

---
+++ packages/pychecktype/pychecktype-1.4.1.tar.gz/pychecktype-1.4.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+version = "1.4.1"
+
+long_description = open('README.rst', 'r').read()
+
+setup(
+    name="pychecktype",
+    version=version,
+    author="Hu Bo",
+    author_email="hubo1016@126.com",
+    long_description=long_description,
+    description="asyncio wrapper for grpc.io",
+    license="Apache",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    url="https://github.com/hubo1016/pychecktype",
+    platforms=['any'],
+    packages=[
+        'pychecktype',
+    ]
+)
