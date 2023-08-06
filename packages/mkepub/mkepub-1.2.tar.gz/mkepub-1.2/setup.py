e8164a22a993fcfb48db00252bcfdab20583ee19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:06 +0000

    Add package mkepub/1.2/mkepub-1.2.tar.gz

---
+++ packages/mkepub/mkepub-1.2.tar.gz/mkepub-1.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='mkepub',
+    version='1.2',
+    description='Simple minimalistic library for creating EPUB3 files',
+    long_description=readme,
+    url='https://github.com/anqxyr/mkepub/',
+    author='anqxyr',
+    author_email='anqxyr@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'],
+    packages=['mkepub'],
+    package_data={'mkepub': ['templates/*']},
+    tests_require=['epubcheck', 'pytest', 'pytest-cov', 'python-coveralls'],
+    install_requires=['jinja2'],
+)
