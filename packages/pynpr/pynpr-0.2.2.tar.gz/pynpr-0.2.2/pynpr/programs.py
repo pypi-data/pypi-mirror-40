a11b79248e262923b74ba818d421d37c45f14cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:16 +0000

    Add package pynpr/0.2.2/pynpr-0.2.2.tar.gz

---
+++ packages/pynpr/pynpr-0.2.2.tar.gz/pynpr-0.2.2/pynpr/programs.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class NPRProgram(object):
+    base_url = "https://www.npr.org"
+
+    def __init__(self, url, name=""):
+        self._name = name
+        if not url.startswith("http"):
+            url = self.base_url + url
+        self.url = url
+
+    @property
+    def name(self):
+        if self._name:
+            return self._name
+        rss_id, name = self.url.replace("https://www.npr.org/programs/", "") \
+            .split("/")
+        return name.replace("-", " ").strip()
+
+    @property
+    def show_type(self):
+        if "/podcasts/" in self.url:
+            return "podcast"
+        elif "/programs/" in self.url:
+            return "program"
+        elif "/series/" in self.url:
+            return "series"
+        elif "/sections/" in self.url:
+            return "section"
+        else:
+            return "external"
+
+    def as_json(self):
+        data = self.parse_page()
+        data["url"] = self.url
+        data["streams"] = [s.as_json() for s in self.streams]
+        data["name"] = self.name
+        data["type"] = self.show_type
+        return data
+
+    def parse_page(self):
+        html = requests.get(self.url).text
+        soup = BeautifulSoup(html, "html.parser")
+        data = {}
+        # TODO
+        return data
+
+
+class ExternalNPRProgram(NPRProgram):
+    def __init__(self, url, name=""):
+        NPRProgram.__init__(self, url, name)
+        self.base_url = url
+
+
---
+++ packages/pynpr/pynpr-0.2.2.tar.gz/pynpr-0.2.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pynpr',
+    version='0.2.2',
+    packages=['pynpr'],
+    url='https://github.com/JarbasAl/pynpr',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='unofficial NPR api',
+    install_requires=["requests", "bs4", "feedparser", "algoliasearch"],
+    keywords='public, radio, stream, metadata, api, service, npr'
+)
