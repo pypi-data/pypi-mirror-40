84dca08af474726c09218fd859f8ed43401f3bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:18 +0000

    Add package AutomatedDrinkDispensingSystem/0.1a1/AutomatedDrinkDispensingSystem-0.1a1.tar.gz

---
+++ packages/AutomatedDrinkDispensingSystem/AutomatedDrinkDispensingSystem-0.1a1.tar.gz/AutomatedDrinkDispensingSystem-0.1a1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+
+setuptools.setup(
+    name="AutomatedDrinkDispensingSystem",
+    version="0.1a1",
+    author="Chris Blanks",
+    author_email="christopherablanks@gmail.com",
+    url="https://github.com/ChrisBlanks/AutomatedDrinkDispensingSystem",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    license="LICENSE.txt",
+    long_description=open("README.txt").read(),
+    install_requires=[
+        "setuptools >= 18.5", "cryptography >= 1.7.1",
+
+    ],
+)
