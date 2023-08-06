29fcea08eaf390f20ca69ea9ec381c245c0b325b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:16 +0000

    Add package cleanlab/0.0.7/cleanlab-0.0.7-py2.py3-none-any.whl

---
+++ packages/cleanlab/cleanlab-0.0.7-py2.py3-none-any.whl/cleanlab/version.py
@@ -0,0 +1,9 @@
+__version__ = '0.0.7'
+
+# 0.0.7 - Massive speed increases across the board. Estimating confident joint now nearly instant. NO major API changes.
+# 0.0.6 - NO API changes. README updates. Examples added. Tutorials added.
+# 0.0.5 - Numerous small bug fixes, but not major API changes. 100% testing code coverage.
+# 0.0.4 - FIRST CROSS-PLATFORM WORKING VERSION OF CLEANLAB. Adding test support.
+# 0.0.3 - Adding working logo to README, pypi working
+# 0.0.2 - Added logo to README, but link does not load on pypi
+# 0.0.1 - initial commit
