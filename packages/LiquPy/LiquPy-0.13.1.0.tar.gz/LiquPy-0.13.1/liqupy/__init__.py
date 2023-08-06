ef2733a64c188658c1db7ff10fa44c5b18869aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:27 +0000

    Add package LiquPy/0.13.1/LiquPy-0.13.1.0.tar.gz

---
+++ packages/LiquPy/LiquPy-0.13.1.0.tar.gz/LiquPy-0.13.1/liqupy/__init__.py
@@ -0,0 +1,14 @@
+# **************************************************************************************
+# LiquPy: Open-source Python Library for Soil Liquefaction and Lateral Spread Analysis
+# https://github.com/LiquPy/LiquPy
+# **************************************************************************************
+from . import points 
+from . import boreholes
+
+__all__ = ['points',
+           'boreholes']
+
+__version__ = '0.13.1'
+
+name = 'LiquPy'
+
---
+++ packages/LiquPy/LiquPy-0.13.1.0.tar.gz/LiquPy-0.13.1/liqupy/default_datasets/__init__.py
@@ -0,0 +1,10 @@
+# **************************************************************************************
+# LiquPy: Open-source Python Library for Soil Liquefaction and Lateral Spread Analysis
+# https://github.com/LiquPy/LiquPy
+# **************************************************************************************
+
+from .base import load_youd_bartlett_demo
+from .base import load_spt_idriss_boulanger
+
+__all__ = ['load_youd_bartlett_demo',
+           'load_spt_idriss_boulanger']
