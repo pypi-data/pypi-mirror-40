39772a8f56810bc4098c0eb779336b6ce5f9e652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:56 +0000

    Add package divvy/0.0.1/divvy-0.0.1.tar.gz

---
+++ packages/divvy/divvy-0.0.1.tar.gz/divvy-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='divvy',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/databio/divvy',
+    author='Nathan Sheffield',
+    author_email='nathan@code.databio.org',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
