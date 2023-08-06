e7932ea68eb783649ddef6a9fb213672e2d482a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:10 +0000

    Add package redditmonitor/0.1/redditmonitor-0.1-py3-none-any.whl

---
+++ packages/redditmonitor/redditmonitor-0.1-py3-none-any.whl/redditmonitor/config.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+client_id = os.environ["CLIENT_ID"]
+client_secret = os.environ["CLIENT_SECRET"]
+reddit_username = os.environ["REDDIT_USERNAME"]
+reddit_password = os.environ["REDDIT_PASSWORD"]
+discored_webhook = os.environ["DISCORD_WEBHOOK"]
+subreddit = os.environ["SUBREDDIT"]
