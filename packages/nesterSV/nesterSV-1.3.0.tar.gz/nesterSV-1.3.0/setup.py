9826849d846c1165359dbd6708017e0e6920faa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:24 +0000

    Add package nesterSV/1.3.0/nesterSV-1.3.0.tar.gz

---
+++ packages/nesterSV/nesterSV-1.3.0.tar.gz/nesterSV-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nesterSV',
+    version = '1.3.0',
+    py_modules = ['nesterSV'],
+    author = 'SanderVos2401',
+    author_email = 'sandervos2401@hotmail.nl',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
