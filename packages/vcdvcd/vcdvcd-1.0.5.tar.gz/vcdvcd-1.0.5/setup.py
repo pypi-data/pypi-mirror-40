1a686021325d15d651fca61a393a8ad6344a42d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:02 +0000

    Add package vcdvcd/1.0.5/vcdvcd-1.0.5.tar.gz

---
+++ packages/vcdvcd/vcdvcd-1.0.5.tar.gz/vcdvcd-1.0.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.adoc') as f:
+        return f.read()
+
+setup(
+    name='vcdvcd',
+    version='1.0.5',
+    description='Python Verilog value change dump (VCD) parser library + the nifty vcdcat VCD command line viewer',
+    long_description=readme(),
+    url='https://github.com/cirosantilli/vcdvcd',
+    author='Ciro Santilli',
+    author_email='ciro.santilli.contact@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=['vcdcat'],
+)
