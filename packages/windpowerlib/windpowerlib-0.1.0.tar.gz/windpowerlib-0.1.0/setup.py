70708f4b553e42746d7bce0b7dd6e68c0bb1d3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:54 +0000

    Add package windpowerlib/0.1.0/windpowerlib-0.1.0.tar.gz

---
+++ packages/windpowerlib/windpowerlib-0.1.0.tar.gz/windpowerlib-0.1.0/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='windpowerlib',
+      version='0.1.0',
+      description='Creating time series from wind power plants.',
+      url='http://github.com/wind-python/windpowerlib',
+      author='oemof developing group',
+      author_email='windpowerlib@rl-institut.de',
+      license=None,
+      packages=['windpowerlib'],
+      package_data={
+          'windpowerlib': [os.path.join('data', '*.csv')]},
+      long_description=read('README.rst'),
+      zip_safe=False,
+      install_requires=['pandas >= 0.19.1',
+                        'requests'])
