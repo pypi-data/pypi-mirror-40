b9730ad1fd46390248e4906d57c3e2f78faaa98d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:58 +0000

    Add package rabbitmq-asynqp/0.1.1/rabbitmq_asynqp-0.1.1-py3-none-any.whl

---
+++ packages/rabbitmq-asynqp/rabbitmq_asynqp-0.1.1-py3-none-any.whl/rabbitmq_asynqp-0.1.1.data/scripts/example.py
@@ -0,0 +1,24 @@
+#!python
+
+from rabbitmq_asynqp.messaging import Messaging
+
+if __name__ == "__main__":
+    rabbitmq_config = dict(host="localhost", port=5672, username="guest", password="guest")
+
+    queue_config = dict(
+        exchange="rabbit_sample_exchange",
+        queues=["sample_queue"],
+        routing_key="sample.routing_key",
+        exchange_type="direct"
+    )
+
+    queue_settings = {'reconnect_backoff_secs': 2, 'connection_check_polling_secs': 10}
+
+    def print_callback(msg):
+        print(msg)
+
+    # Use this as a global object
+    messaging = Messaging(rabbitmq_config=rabbitmq_config, queue_config=queue_config, consumer_func=print_callback,
+                          queue_settings=queue_settings)
+
+    messaging.send_message({"msg_i": 1})
