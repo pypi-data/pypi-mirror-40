2afec22bd158ef743bc8df229f28a8eee19039c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:10 +0000

    Add package nista_learn/0.0.4/nista_learn-0.0.4.tar.gz

---
+++ packages/nista_learn/nista_learn-0.0.4.tar.gz/nista_learn-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setuptools.setup(
+    name="nista_learn",
+    version="0.0.4",
+    author="Cheikh Tidjane Konteye",
+    author_email="cheikh@cheikhkonteye.com",
+    description="A small machine learning package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wariored/nista_learn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
