67c118832d64bffbeeedbd3d265f7047778a7cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:44 +0000

    Add package controlM-rest/0.1.3/controlM-rest-0.1.3.tar.gz

---
+++ packages/controlM-rest/controlM-rest-0.1.3.tar.gz/controlM-rest-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='controlM-rest',
+    version='0.1.3',
+    packages=find_packages(),
+    long_description=open('README.txt').read(),
+    keywords=['ControlM', 'REST-api'],
+    python_requires='>=2.7',
+    install_requires=['requests >= 2.8, < 3.0.0a0'],
+)
