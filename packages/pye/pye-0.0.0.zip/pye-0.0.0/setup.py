7793a4da9461a96aff85c2c5cdbb3c1b50a9e1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:26 +0000

    Add package pye/0.0.0/pye-0.0.0.zip

---
+++ packages/pye/pye-0.0.0.zip/pye-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='pye',
+    version='0.0.0',
+    author='Roger D. Serwy',
+    author_email='roger.serwy@gmail.com',
+    packages=['pye'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+    ],
+)
