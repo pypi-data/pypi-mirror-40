8c6b71a8461f1dfb279ac950f5c327878fe9d1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:03 +0000

    Add package BotpySE/0.7.2/BotpySE-0.7.2.tar.gz

---
+++ packages/BotpySE/BotpySE-0.7.2.tar.gz/BotpySE-0.7.2/BotpySE/AllCommands.py
@@ -0,0 +1,17 @@
+#
+# AllCommands.py
+# Botpy
+#
+# Created by Ashish Ahuja on 13th January 2019.
+#
+
+from Commands.CommandAlive import CommandAlive
+from Commands.CommandListRunningCommands import CommandListRunningCommands
+from Commands.CommandPrivilegeUser import CommandPrivilegeUser
+from Commands.CommandStop import CommandStop
+from Commands.CommandUnprivilegeUser import CommandUnprivilegeUser
+from Commands.CommandAmiprivileged import CommandAmiprivileged
+from Commands.CommandListPrivilegedUsers import CommandListPrivilegedUsers
+from Commands.CommandReboot import CommandReboot
+from Commands.Command import Command
+
