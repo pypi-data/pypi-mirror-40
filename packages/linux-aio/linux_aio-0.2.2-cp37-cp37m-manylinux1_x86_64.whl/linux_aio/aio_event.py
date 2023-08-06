101ca46fe8d98e4053950aedc1ba521d484e4828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:34 +0000

    Add package linux-aio/0.2.2/linux_aio-0.2.2-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/linux-aio/linux_aio-0.2.2-cp37-cp37m-manylinux1_x86_64.whl/linux_aio/aio_event.py
@@ -0,0 +1,36 @@
+# coding: UTF-8
+
+from ctypes import py_object
+from typing import Optional
+
+from .aio_block import AIOBlock, BUF_TYPE
+from .raw import IOEvent
+
+
+class AIOEvent:
+    __slots__ = ('_event',)
+
+    _event: IOEvent
+
+    def __init__(self, event: IOEvent) -> None:
+        self._event = event
+
+    @property
+    def aio_block(self) -> AIOBlock:
+        return py_object.from_address(self._event.data).value
+
+    @property
+    def buffer(self) -> Optional[BUF_TYPE]:
+        return self.aio_block.buffer
+
+    def stripped_buffer(self) -> Optional[BUF_TYPE]:
+        """`\0` pads removed buffer"""
+        return self.buffer.rstrip(b'\0')
+
+    @property
+    def response(self) -> int:
+        return self._event.res
+
+    @property
+    def response2(self) -> int:
+        return self._event.res2
