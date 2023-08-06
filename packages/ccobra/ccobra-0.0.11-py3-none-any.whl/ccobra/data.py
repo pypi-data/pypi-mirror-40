6cffda0268db8ad98170ffa877eda9c33cae43d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:13 +0000

    Add package ccobra/0.0.11/ccobra-0.0.11-py3-none-any.whl

---
+++ packages/ccobra/ccobra-0.0.11-py3-none-any.whl/ccobra/data.py
@@ -0,0 +1,116 @@
+""" CCOBRA data containers.
+
+"""
+
+
+class CCobraData():
+    """ CCobra experimental data container.
+
+    """
+
+    def __init__(self, data, required_fields=None):
+        """ Initializes the CCOBRA data container by passing a data frame
+        and validating its contents.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            DataFrame to store in the CCOBRA data container.
+
+        required_fields : list(str), optional
+            List of required columns in the data. Defaults to ['id', 'sequence',
+            'task', 'choices', 'response', 'response_type', 'domain']
+
+        """
+
+        self.required_fields = [
+            'id', 'sequence', 'task', 'choices', 'response',
+            'response_type', 'domain'
+        ]
+        if required_fields:
+            self.required_fields = required_fields
+
+        self.verify_data(data)
+        self._data = data
+
+    def verify_data(self, data):
+        """ Verifies if all required fields are in the data.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            DataFrame to verify.
+
+        """
+
+        missing = set(self.required_fields) - set(data.columns)
+        if missing:
+            raise ValueError(
+                "Data does not contain columns: {}".format(missing))
+
+    def get(self):
+        """ Returns the contained data.
+
+        Returns
+        -------
+        pd.DataFrame
+            Dataframe containing the data.
+
+        """
+
+        return self._data
+
+class Item():
+    """ Container class for representing task items.
+
+    """
+
+    def __init__(self, identifier, domain, task, resp_type, choices):
+        """ Constructs the task item container with information about the
+        domain, task premises, response type, and response choices.
+
+        Parameters
+        ----------
+        identifier : object
+            Unique identifier for the participant.
+
+        domain : str
+            Task domain (e.g., 'syllogistic').
+
+        task : str
+            Task text in tuple string encoding (e.g.,
+            'All;pilots;gardeners/Some;gardeners;cooks').
+
+        """
+
+        #: Unique identifier of the participant
+        self.identifier = identifier
+
+        #: Response type of the task
+        self.response_type = resp_type
+
+        #: Task string representation
+        self.task_str = task
+
+        #: Task in list representation
+        self.task = [x.split(";") for x in task.split("/") if x]
+
+        #: Choices string representation
+        self.choices_str = choices
+
+        #: Choices in list representation
+        self.choices = [x.split('/') for x in choices.split('|')]
+        for idx in range(len(self.choices)):
+            self.choices[idx] = [x.split(';') for x in self.choices[idx]]
+
+        #: Domain of the task
+        self.domain = domain
+
+    def __str__(self):
+        rep = 'CCOBRA Item:\n'
+        rep += '\tIdentifier: {}\n'.format(self.identifier)
+        rep += '\tTask: {}\n'.format(self.task)
+        rep += '\tDomain: {}\n'.format(self.domain)
+        rep += '\tResponse Type: {}\n'.format(self.response_type)
+        rep += '\tChoices: {}'.format(self.choices)
+        return rep
---
+++ packages/ccobra/ccobra-0.0.11-py3-none-any.whl/ccobra/syllogistic/parsing.py
@@ -0,0 +1,120 @@
+""" Collection of syllogistic helper functions.
+
+"""
+
+#: List of syllogistic task identifiers.
+SYLLOGISMS = []
+for prem1 in ['A', 'I', 'E', 'O']:
+    for prem2 in ['A', 'I', 'E', 'O']:
+        for fig in ['1', '2', '3', '4']:
+            SYLLOGISMS.append(prem1 + prem2 + fig)
+
+#: List of syllogistic responses.
+RESPONSES = []
+for quant in ['A', 'I', 'E', 'O']:
+    for direction in ['ac', 'ca']:
+        RESPONSES.append(quant + direction)
+RESPONSES.append('NVC')
+
+def encode_task(task_tuple):
+    """ Encodes a syllogistic task by transforming a task tuple string into
+    its corresponding identifier.
+
+    Parameters
+    ----------
+    task_tuple : list(list(str))
+        Task tuple in list representation (e.g.,
+        [['Some', 'models', 'managers'], ['All', 'models', 'clerks']])
+
+    Returns
+    -------
+    str
+        Syllogistic task identifier (e.g., 'AI1'). Figures are defined in
+        accordance to Khemlani et al. (2012).
+
+    """
+
+    p1, p2 = task_tuple
+
+    quant1 = p1[0].replace('All', 'A').replace(
+        'Some not', 'O').replace('Some', 'I').replace('No', 'E')
+    quant2 = p2[0].replace('All', 'A').replace(
+        'Some not', 'O').replace('Some', 'I').replace('No', 'E')
+    figure = 1
+
+    if p1[1] == p2[1]:
+        figure = 4
+    elif p1[2] == p2[1]:
+        figure = 1
+    elif p1[2] == p2[2]:
+        figure = 3
+    elif p1[1] == p2[2]:
+        figure = 2
+    else:
+        raise ValueError('Could not determine figure of:', task_tuple)
+
+    return quant1 + quant2 + str(figure)
+
+def encode_response(response, task):
+    """ Encodes a syllogistic response by transforming the tuple containing the
+    response string and the task tuple into its string representation.
+
+    Parameters
+    ----------
+    response : list(list(str))
+        Response encodings.
+
+    task : list(list(str))
+        Task tuple representation.
+
+    Returns
+    -------
+    str
+        Encoded response (e.g., 'Iac').
+
+    """
+
+    if not isinstance(response[0], list):
+        response = [response]
+
+    if response[0][0] == 'NVC':
+        return 'NVC'
+
+    object_sets = [set(x[1:]) for x in task]
+    midterm = object_sets[0].intersection(object_sets[1])
+    obj_a = object_sets[0] - midterm
+
+    quant = response[0][0].replace('All', 'A').replace(
+        'Some not', 'O').replace('Some', 'I').replace('No', 'E')
+
+    return quant + ('ac' if response[0][1] == list(obj_a)[0] else 'ca')
+
+def decode_response(enc_response, task):
+    """ Decodes an encoded syllogistic response by transforming it to the
+    corresponding tuple representation and inserting the appropriate terms.
+
+    Parameters
+    ----------
+    enc_response : str
+        Encoded syllogistic response (e.g., 'Aac').
+
+    task : list(str)
+        Syllogistic task in the tuple list representation (e.g.,
+        [['Some', 'models', 'managers'], ['All', 'models', 'clerks']]).
+
+    """
+
+    if enc_response == 'NVC':
+        return ['NVC']
+    if enc_response == ['NVC']:
+        return enc_response
+
+    obj_a = set(task[0][1:]) - set(task[1][1:])
+    obj_c = set(task[1][1:]) - set(task[0][1:])
+
+    quant = enc_response[0].replace('A', 'All').replace(
+        'I', 'Some').replace('O', 'Some not').replace('E', 'No')
+    if enc_response[1:] == 'ac':
+        return [quant, list(obj_a)[0], list(obj_c)[0]]
+
+    return [quant, list(obj_c)[0], list(obj_a)[0]]
