1023973d243c582bdfe7e83739db3f9fe70fea99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:40 +0000

    Add package changeHttp/1.0.0/changeHttp-1.0.0.tar.gz

---
+++ packages/changeHttp/changeHttp-1.0.0.tar.gz/changeHttp-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'changeHttp',
+	version = '1.0.0',
+	description = 'Convert a http package into a dictionary, you can change the value by changing the function inside the changeHttp class.',
+	author = 'windy_2',
+	py_modules = ["changeHttp"],
+	keywords = 'http cookie',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+		],
+	url = 'https://github.com/windy-purple/changeHttp',
+)
