3e52f36af7ff7e2542572032871a22cb6298fb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:32 +0000

    Add package udata-piwik/1.3.2/udata_piwik-1.3.2-py2.py3-none-any.whl

---
+++ packages/udata-piwik/udata_piwik-1.3.2-py2.py3-none-any.whl/udata_piwik/client.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+import requests
+
+from datetime import date
+
+from flask import current_app
+
+log = logging.getLogger(__name__)
+
+DEFAULT_TRACK_TIMEOUT = 60  # in seconds
+DEFAULT_ANALYZE_TIMEOUT = 60 * 5  # in seconds
+
+
+def analyze(method, **kwargs):
+    """Retrieve JSON stats from Piwik for a given `method` and parameters."""
+    base_url = '{0}://{1}/index.php'.format(
+        current_app.config.get('PIWIK_SCHEME', 'http'),
+        current_app.config['PIWIK_URL'],
+    )
+    data = {
+        'module': 'API',
+        'idSite': current_app.config['PIWIK_ID'],
+        'method': method,
+        'format': kwargs.pop('format', 'json'),
+    }
+    if current_app.config['PIWIK_AUTH']:
+        data['token_auth'] = current_app.config['PIWIK_AUTH']
+    if 'date' in kwargs:
+        dt = kwargs.pop('date')
+        if isinstance(dt, date):
+            dt = dt.isoformat()
+        kwargs['date'] = dt
+    data.update(kwargs)
+    timeout = current_app.config.get('PIWIK_ANALYZE_TIMEOUT',
+                                     DEFAULT_ANALYZE_TIMEOUT)
+    r = requests.get(base_url, params=data, timeout=timeout)
+    return r.json()
+
+
+def track(url, **kwargs):
+    """Track a request to a given `url` by issuing a POST against Piwik."""
+    base_url = '{0}://{1}/piwik.php'.format(
+        current_app.config.get('PIWIK_SCHEME', 'http'),
+        current_app.config['PIWIK_URL'],
+    )
+    data = {
+        'rec': 1,
+        'idsite': current_app.config['PIWIK_ID'],
+        'url': url,
+        'token_auth': current_app.config['PIWIK_AUTH']
+    }
+    data.update(kwargs)
+    timeout = current_app.config.get('PIWIK_TRACK_TIMEOUT',
+                                     DEFAULT_TRACK_TIMEOUT)
+    requests.post(base_url, data=data, timeout=timeout)
---
+++ packages/udata-piwik/udata_piwik-1.3.2-py2.py3-none-any.whl/udata_piwik/tasks.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+from datetime import date, timedelta
+
+from flask import current_app
+
+from udata.tasks import connect, job
+from udata.api.signals import on_api_call
+from udata.core.dataset.signals import on_dataset_published
+from udata.core.reuse.signals import on_reuse_published
+from udata.core.followers.signals import on_new_follow
+
+from .client import track
+from .counter import counter
+
+
+log = logging.getLogger(__name__)
+
+
+@job('piwik-current-metrics', route='low.piwik')
+def piwik_current_metrics(self):
+    '''Fetch piwik metrics for the current day'''
+    day = date.today()
+
+    self.log.info('Loading Piwik metrics for today ({day})'.format(day=day))
+
+    counter.count_for(day)
+
+
+@job('piwik-yesterday-metrics', route='low.piwik')
+def piwik_yesterday_metrics(self):
+    '''Bump piwik daily metrics for yesterday'''
+    day = date.today() - timedelta(days=1)
+
+    self.log.info('Bump Piwik metrics for {day}'.format(day=day))
+
+    counter.count_for(day)
+
+
+@connect(on_api_call, route='low.piwik')
+def piwik_track_api(url, **params):
+    '''Track an API request into Piwik.'''
+    log.debug('Sending to piwik: {url}'.format(url=url))
+    track(url, **params)
+
+
+@connect(on_dataset_published, route='low.piwik')
+def piwik_track_dataset_published(url, **params):
+    '''Track a dataset publication into Piwik.'''
+    log.debug('Sending to piwik: {url}'.format(url=url))
+    goals = current_app.config.get('PIWIK_GOALS')
+    if goals:
+        params.update({
+            'idgoal': goals['NEW_DATASET'],
+        })
+    track(url, **params)
+
+
+@connect(on_reuse_published, route='low.piwik')
+def piwik_track_reuse_published(url, **params):
+    '''Track a reuse publication into Piwik.'''
+    log.debug('Sending to piwik: {url}'.format(url=url))
+    goals = current_app.config.get('PIWIK_GOALS')
+    if goals:
+        params.update({
+            'idgoal': goals['NEW_REUSE'],
+        })
+    track(url, **params)
+
+
+@connect(on_new_follow, route='low.piwik')
+def piwik_track_new_follow(url, **params):
+    '''Track a new follow into Piwik.'''
+    log.debug('Sending to piwik: {url}'.format(url=url))
+    goals = current_app.config.get('PIWIK_GOALS')
+    if goals:
+        params.update({
+            'idgoal': goals['NEW_FOLLOW'],
+        })
+    track(url, **params)
