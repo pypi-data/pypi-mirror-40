2b441e36b60c6c6400049cac52f4f02f6ce52ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:39 +0000

    Add package keras-transformer/0.14.0/keras-transformer-0.14.0.tar.gz

---
+++ packages/keras-transformer/keras-transformer-0.14.0.tar.gz/keras-transformer-0.14.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='keras-transformer',
+    version='0.14.0',
+    packages=find_packages(),
+    url='https://github.com/CyberZHG/keras-transformer',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='Transformer implemented in Keras',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'numpy',
+        'Keras',
+        'keras-multi-head==0.14.0',
+        'keras-layer-normalization>=0.2.0',
+        'keras-position-wise-feed-forward==0.1.0',
+        'keras-pos-embd==0.6.0',
+        'keras-embed-sim==0.1.0',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
