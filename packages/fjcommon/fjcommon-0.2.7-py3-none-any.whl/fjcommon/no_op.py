ad33d32c4b008f2069abbab740d1198534da9ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:13 +0000

    Add package fjcommon/0.2.7/fjcommon-0.2.7-py3-none-any.whl

---
+++ packages/fjcommon/fjcommon-0.2.7-py3-none-any.whl/fjcommon/no_op.py
@@ -0,0 +1,35 @@
+
+class _NoOp(object):
+    """
+    Class that silently ignores calls to any function. Use the singleton instance defined below, i.e.,
+        from fjcommon.no_op import NoOp
+    (See README.md)
+    """
+    def __getattr__(self, attr):
+        return _no_op
+
+    def __call__(self, *args, **kwargs):
+        """ Means NoOp() works, but also NoOp()()()... """
+        return NoOp
+
+    def __enter__(self):
+        return NoOp
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return NoOp
+
+
+# Singleton instance that should be used
+NoOp = _NoOp()
+
+
+def _no_op(*args, **kwargs):
+    return NoOp  # return NoOp s.t. NoOp.foo().bar().baz() works.
+
+
+def test_no_op():
+    n = NoOp()
+    n.foo().bar().baz()
+
+    with n.some_ctx_mgr() as t:
+        print(t.foo())
