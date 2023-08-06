96c58250b73393cec638ef360d9cd77fe4b6b59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:51 +0000

    Add package dshelper/0.1.0/dshelper-0.1.0-py3-none-any.whl

---
+++ packages/dshelper/dshelper-0.1.0-py3-none-any.whl/dshelper/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+dshelper is a GUI for visualization of pandas dataframes. 
+In addition, it provides some functionalities in helping with some exploratory analysis and examination of raw data
+
+Copyright (c) 2018 - 2019, Minchang (Carson) Zhang.
+License: MIT (see LICENSE for details)
+"""
+from __future__ import absolute_import
+
+from .dshelper import dshelp
+from . import data, plot
+
+
+__version__ = "0.1.0"
+
+__all__ = ["dshelp"]
