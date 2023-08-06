fa2fdba5ccfcf45fe6600d407da0d349d376ddaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package ocrd/0.15.2/ocrd-0.15.2-py2-none-any.whl

---
+++ packages/ocrd/ocrd-0.15.2-py2-none-any.whl/test/base.py
@@ -0,0 +1,9 @@
+# pylint: disable=unused-import
+
+import os
+import sys
+from unittest import TestCase, skip, main
+
+from test.assets import assets
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../ocrd')
