8aed58a573247c6db8831975469aa8b36d8d28d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:57 +0000

    Add package heudiconv/0.5.3/heudiconv-0.5.3.tar.gz

---
+++ packages/heudiconv/heudiconv-0.5.3.tar.gz/heudiconv-0.5.3/heudiconv/tests/test_regression.py
@@ -0,0 +1,45 @@
+"""Testing conversion with conversion saved on datalad"""
+import json
+from glob import glob
+
+import pytest
+
+have_datalad = True
+try:
+    from datalad import api # to pull and grab data
+    from datalad.support.exceptions import IncompleteResultsError
+except ImportError:
+    have_datalad = False
+
+import heudiconv
+from heudiconv.cli.run import main as runner
+# testing utilities
+from .utils import fetch_data, gen_heudiconv_args
+
+
+@pytest.mark.parametrize('subject', ['sub-sid000143'])
+@pytest.mark.parametrize('heuristic', ['reproin.py'])
+@pytest.mark.parametrize('anon_cmd', [None, 'anonymize_script.py'])
+@pytest.mark.skipif(not have_datalad, reason="no datalad")
+def test_conversion(tmpdir, subject, heuristic, anon_cmd):
+    tmpdir.chdir()
+    try:
+        datadir = fetch_data(tmpdir.strpath, subject)
+    except IncompleteResultsError as exc:
+        pytest.skip("Failed to fetch test data: %s" % str(exc))
+    outdir = tmpdir.mkdir('out').strpath
+
+    args = gen_heudiconv_args(datadir, outdir, subject, heuristic, anon_cmd)
+    runner(args) # run conversion
+
+    # verify functionals were converted
+    assert glob('{}/{}/func/*'.format(outdir, subject)) == \
+           glob('{}/{}/func/*'.format(datadir, subject))
+
+    # compare some json metadata
+    json_ = '{}/task-rest_acq-24mm64sl1000tr32te600dyn_bold.json'.format
+    orig, conv = (json.load(open(json_(datadir))),
+                  json.load(open(json_(outdir))))
+    keys = ['EchoTime', 'MagneticFieldStrength', 'Manufacturer', 'SliceTiming']
+    for key in keys:
+        assert orig[key] == conv[key]
---
+++ packages/heudiconv/heudiconv-0.5.3.tar.gz/heudiconv-0.5.3/heudiconv/tests/utils.py
@@ -0,0 +1,30 @@
+import os.path as op
+import heudiconv.heuristics
+
+HEURISTICS_PATH = op.join(heudiconv.heuristics.__path__[0])
+TESTS_DATA_PATH = op.join(op.dirname(__file__), 'data')
+
+
+def gen_heudiconv_args(datadir, outdir, subject, heuristic_file, anon_cmd=None, xargs=None):
+    heuristic = op.realpath(op.join(HEURISTICS_PATH, heuristic_file))
+    args = ["-d", op.join(datadir, 'sourcedata/{subject}/*/*/*.tgz'),
+            "-c", "dcm2niix",
+            "-o", outdir,
+            "-s", subject,
+            "-f", heuristic,
+            "--bids",]
+    if anon_cmd:
+        args += ["--anon-cmd", op.join(op.dirname(__file__), anon_cmd), "-a", outdir]
+    if xargs:
+        args += xargs
+
+    return args
+
+
+def fetch_data(tmpdir, subject):
+    """Fetches some test dicoms using datalad"""
+    from datalad import api
+    targetdir = op.join(tmpdir, 'QA')
+    api.install(path=targetdir, source='http://datasets-tests.datalad.org/dbic/QA')
+    api.get('{}/sourcedata/{}'.format(targetdir, subject))
+    return targetdir
