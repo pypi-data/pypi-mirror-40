882f4f0040246e3bc5c7a1819d3a345a48e43c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:20 +0000

    Add package morpfw/0.2.1a1.dev32/morpfw-0.2.1a1.dev32.tar.gz

---
+++ packages/morpfw/morpfw-0.2.1a1.dev32.tar.gz/morpfw-0.2.1a1.dev32/morpfw/authn/pas/apikey/schema.py
@@ -0,0 +1,13 @@
+import jsonobject
+from morpfw.crud import Schema
+from dataclasses import dataclass, field
+import typing
+
+
+@dataclass
+class APIKeySchema(Schema):
+
+    userid: typing.Optional[str] = None
+    label: typing.Optional[str] = None
+    api_identity: typing.Optional[str] = None
+    api_secret: typing.Optional[str] = None
