0de8ff461ae13c8dcef01fe57ecc803014384944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:18 +0000

    Add package woodland/0.0.1/woodland-0.0.1.tar.gz

---
+++ packages/woodland/woodland-0.0.1.tar.gz/woodland-0.0.1/README.md
@@ -0,0 +1,8 @@
+# Forests
+### Helping you see the forest from the trees.
+
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)
+[![Build Status](https://travis-ci.org/JHowell45/Forests.svg?branch=master)](https://travis-ci.org/JHowell45/Forests)
+[![codecov](https://codecov.io/gh/JHowell45/Forests/branch/master/graph/badge.svg)](https://codecov.io/gh/JHowell45/Forests)
+
+Python data types for tree data structures.
---
+++ packages/woodland/woodland-0.0.1.tar.gz/woodland-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="woodland",
+    version="0.0.1",
+    url="https://github.com/JHowell45/Forests",
+    author="Jacob Howell",
+    author_email="email@gmail.com",
+    description="Pre-made Tree data structure.",
+    long_description=open("README.md").read(),
+    packages=["woodland"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
