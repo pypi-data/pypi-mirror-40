d80f6451c4045da35d170163c6a42b544dae55a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:49 +0000

    Add package signalfx-tracing/0.0.3/signalfx-tracing-0.0.3.tar.gz

---
+++ packages/signalfx-tracing/signalfx-tracing-0.0.3.tar.gz/signalfx-tracing-0.0.3/signalfx_tracing/constants.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2018-2019 SignalFx, Inc. All rights reserved.
+
+instrumented_attr = '__sfx_instrumented'
+traceable_libraries = ('django', 'flask', 'psycopg2', 'pymongo', 'pymysql', 'redis', 'requests', 'tornado')
+auto_instrumentable_libraries = ('flask', 'psycopg2', 'pymongo', 'pymysql', 'redis', 'requests', 'tornado')
