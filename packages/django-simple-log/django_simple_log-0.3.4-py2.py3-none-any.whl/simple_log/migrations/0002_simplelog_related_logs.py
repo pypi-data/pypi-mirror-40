d6e857d2873dae55c378630b715ccc06b386eaeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:12 +0000

    Add package django-simple-log/0.3.4/django_simple_log-0.3.4-py2.py3-none-any.whl

---
+++ packages/django-simple-log/django_simple_log-0.3.4-py2.py3-none-any.whl/simple_log/migrations/0002_simplelog_related_logs.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.conf import settings
+
+from simple_log.fields import SimpleManyToManyField
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.SIMPLE_LOG_MODEL),
+        ('simple_log', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='simplelog',
+            name='related_logs',
+            field=SimpleManyToManyField(to='self', verbose_name='related log', blank=True),
+        ),
+    ]
