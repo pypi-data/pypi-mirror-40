40379b57df6c02114254785c5ac9940dfb41b6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:01 +0000

    Add package djcli/1.1.0/djcli-1.1.0.tar.gz

---
+++ packages/djcli/djcli-1.1.0.tar.gz/djcli-1.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='djcli',
+    version='1.1.0',
+    setup_requires='setupmeta',
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/djcli',
+    include_package_data=True,
+    license='MIT',
+    keywords='django cli',
+    python_requires='>=3',
+)
