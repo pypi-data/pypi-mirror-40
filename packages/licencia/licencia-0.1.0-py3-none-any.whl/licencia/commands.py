47eb6f9212ca47f80aa2acf6edb595715b36a9ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:47 +0000

    Add package licencia/0.1.0/licencia-0.1.0-py3-none-any.whl

---
+++ packages/licencia/licencia-0.1.0-py3-none-any.whl/licencia/commands.py
@@ -0,0 +1,41 @@
+import sys
+
+from cleo import Command
+from pip._internal.commands.show import search_packages_info
+
+from .core import create_table
+from .core import get_package_names
+from .core import load_toml
+
+
+class ListCommand(Command):
+    """
+    List the licenses for external packages used in your repository.
+
+    list
+        {--o|order=name : Control display order by "name" or "license".}
+        {--f|format=compact : Select the output format among: "compact", "markdown", or "rst"}
+
+    """
+
+    def handle(self):
+        try:
+            pyproject = load_toml("poetry.lock")
+        except FileNotFoundError:
+            self.line_error("ERROR: The lock file not found.", style="error")
+            sys.exit(1)
+
+        package_names = get_package_names(pyproject)
+        if not package_names:
+            self.line("No data")
+            sys.exit(1)
+
+        results = search_packages_info(package_names)
+
+        rows = [(p["name"], p["license"]) for p in results]
+
+        order = self.option("order")
+        style = self.option("format")
+        table = create_table(rows, order=order, style=style)
+
+        print(table)
