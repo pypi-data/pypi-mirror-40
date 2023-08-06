16b0ee8d59beb9a6ebfc677befcc15ac62faff78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:25 +0000

    Add package django-prepared-queries/0.3.0/django_prepared_queries-0.3.0.tar.gz

---
+++ packages/django-prepared-queries/django_prepared_queries-0.3.0.tar.gz/django_prepared_queries-0.3.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+try:
+    # noinspection PyPackageRequirements
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+
+setup(
+    name='django_prepared_queries',
+    long_description=read_md('README.md'),
+    version='0.3.0',
+    packages=['django_pq'],
+    url='https://github.com/rutube/django_prepared_queries',
+    license='Beer license',
+    author='Tumbler',
+    author_email='zimbler@bk.ru',
+    description='Caches SQL queries built with Django ORM',
+    install_requires=['typing', 'Django>=1.8,<2.2', 'six']
+)
