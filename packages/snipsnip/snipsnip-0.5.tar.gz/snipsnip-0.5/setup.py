0a9faedd5eccac33a26382a40a82edbc22384fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:18 +0000

    Add package snipsnip/0.5/snipsnip-0.5.tar.gz

---
+++ packages/snipsnip/snipsnip-0.5.tar.gz/snipsnip-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='snipsnip',
+    version='0.5',
+    description='Remote mac clipboard',
+    url='http://github.com/AGhost-7/snipsnip',
+    author='Jonathan Boudreau',
+    author_email='jonathan.boudreau.92@gmail.com',
+    license='MIT',
+    scripts=['snipsnip'],
+    zip_safe=False,
+    install_requires=[])
