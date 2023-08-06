195427e593859346810e1defb5d93e0175eb1d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:09 +0000

    Add package tnestlist/1.2.0/tnestlist-1.2.0.tar.gz

---
+++ packages/tnestlist/tnestlist-1.2.0.tar.gz/tnestlist-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'tnestlist',
+    version = '1.2.0',
+    py_modules = ['tnestlist'],
+    author = 'mengtaoc',
+    author_email = 'chentao623tmc@126.com',
+    url = 'https://pypi.org/project/tnestlist',
+    description = 'A simple printer of nested lists',
+)
