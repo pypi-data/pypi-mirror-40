26930cb46457572912ca54d1875fe217331b8a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:56 +0000

    Add package iduoliao-ml/1.0.119/iduoliao_ml-1.0.119-py2-none-any.whl

---
+++ packages/iduoliao-ml/iduoliao_ml-1.0.119-py2-none-any.whl/iduoliao_ml/hot/__init__.py
@@ -0,0 +1,140 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json, time, urllib2
+from .. import ilog, es, dataDispose
+from hot import *
+
+def startUpdate(isTest, configFileName):
+	cycleUpdateHotList(configFileName, isTest)
+
+def cycleUpdateHotList(configFileName, isTest):
+	subjectHistory = dataDispose.getSubjectHistory()
+	worksHistory = dataDispose.getWorksHistory()
+	endTime = worksHistory.values()[0]['time']
+	while True:
+		try:
+			start = time.time()
+			ilog.info('start:' + str(start) + '(' + time.strftime(es.esTimeFormatNoMs, time.localtime(start)) + ')')
+			
+			endTime = onceUpdateHotList(subjectHistory, worksHistory, dataDispose.getWorksList(), endTime, configFileName, isTest)
+
+			cost = time.time() - start
+			ilog.info('-------------------------cost: ' + str(cost) + '---------------------------------')
+
+			maxInterval = 60 if isTest else 600
+			if cost < maxInterval:
+				time.sleep(maxInterval - cost)
+		except Exception as error:
+			ilog.info('Error: ' + str(error))
+			time.sleep(60)
+
+def onceUpdateHotList(subjectHistory, worksHistory, worksList, startTime, configFileName, isTest):
+	endTime = updateHotList(subjectHistory, worksHistory, worksList, startTime, readConfig(configFileName))
+	#10秒后通知后台更新
+	time.sleep(10)
+	noticeServer(isTest)
+	saveHotTopList()
+	return endTime
+
+def updateHotList(subjectHistory, worksHistory, worksList, startTime, config):
+	start = time.time()
+
+	
+	dbConfig = es.searchDbConfig()
+	ilog.info('dbConfig:' + str(dbConfig))
+	hotVids = dbConfig['hotVids'] + getOldHotVids(config)
+	#print(hotVids)
+	worksDict = {}
+	for works in worksList:
+		if works.vid in hotVids:
+			worksDict[works.vid] = works
+
+
+	endTime = int(time.time())
+	newHits = es.searchStatisticsDataByBody("works_actions", {"query": {"bool": {"must": [{"range": {"createTime": {"gte": startTime, "lt": endTime}}}, {"terms": {"action": [1, 2, 12, 13, 14, 15, 16, 17]}}]}}, "size": 10000})
+	for hit in newHits:
+		action = hit['_source']
+		vid = action['vid']
+		if worksDict.has_key(vid):
+			worksDict[vid].updateAction(action)
+
+	ilog.info('add new actions finished:' + str(time.time() - start))
+
+	updateWeight(config['weight'])
+	
+	dauFactor = dataDispose.getDauFactor()
+	ilog.info('dauFactor:' + str(dauFactor))
+	worksHots = calHotV2(subjectHistory, worksHistory, worksDict.values(), dauFactor, dbConfig, config['aid']['half'])
+
+	ilog.info('got hot list:' + str(time.time() - start))
+
+	ilog.info('update works hot: ' + ('success ' if es.coverStatisticsData('works_hot', worksHots) else 'fail ') + str(time.time() - start))
+	#ilog.info('update redis works hot: ' + ('success ' if es.updateRedisWorksHots(getRedisWorksHots(worksHots)) else 'fail ') + str(time.time() - start))
+
+	return endTime
+
+def getOldHotVids(config):
+	oldHotVids = []
+	for hit in es.scrollSearch('works_video_read', 'video', {"query": {"bool": {"must": [{"terms": {"aid": config['aid']['white']}}], "must_not": [{"terms": {"vid": config['vid']['black']}}]}}, "size":1000}):
+		oldHotVids.append(hit['_id'])
+	return oldHotVids
+
+def getRedisWorksHots(worksHots):
+	redisWorksHots = {}
+	aids = []
+	for vid, works in worksHots.items():
+		aid = works['aid']
+		if aid not in aids:
+			aids.append(aid)
+			redisWorksHots[vid] = works['hot']
+	return redisWorksHots
+
+def readConfig(configFileName):
+	with open(configFileName, 'r') as f:
+		config = json.loads(f.read())
+	ilog.info('config: ' + str(config))
+	return config
+
+def noticeServer(isTest):
+	try:
+		noticeUpdate(1, isTest)
+		noticeUpdate(2, isTest)
+	except urllib2.HTTPError as error:
+		ilog.info('urllib2.HTTPError: ' + str(error))
+
+def noticeUpdate(listType, isTest):
+	url = 'http://nttest.iduoliao.cn:8080' if isTest else 'https://micro.mitao.iduoliao.cn'
+	data = {
+		"head":{
+     		"@type":"type.googleapis.com/ja.common.proto.AutReqHead",
+  			"ver": 1,
+  			"platform": 999,
+  			"seqid": long(time.time() * 1000)
+    	},
+    	"type": listType
+	}
+	headers = {'Content-Type': 'application/json'}
+	request = urllib2.Request(url=url+'/recommend/IRecommend/UpdateDataNotice', headers=headers, data=json.dumps(data))
+	infoInfo = urllib2.urlopen(request).read()
+	ilog.info('notice ' + ('hot' if listType == 1 else 'new') + ' list update resp: ' + str(infoInfo))
+	return json.loads(infoInfo)['head']['status'] == 1
+
+def saveHotTopList():
+	body = {"sort": [{"ranking": {"order": "asc"}}],"size": 50}
+	now = time.strftime('%Y-%m-%dT%H:%M:%S+0800', time.localtime(time.time()))
+	print(now)
+	topList = {}
+	for index, hit in enumerate(es.searchStatisticsDataByBody('works_hot', body, False)):
+		info = hit['_source']
+		info['time'] = now
+		topList[hit['_id'] + '$' + str(now)] = info
+	es.updateStatisticsData('works_hot_tops', topList)
+
+
+
+
+
+
+
+
