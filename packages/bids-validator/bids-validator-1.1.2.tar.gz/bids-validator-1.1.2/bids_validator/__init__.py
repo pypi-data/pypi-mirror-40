30569aacdadcf65880b29c5887d00466349fdec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:20 +0000

    Add package bids-validator/1.1.2/bids-validator-1.1.2.tar.gz

---
+++ packages/bids-validator/bids-validator-1.1.2.tar.gz/bids-validator-1.1.2/bids_validator/__init__.py
@@ -0,0 +1,4 @@
+"""BIDS validator common Python package."""
+from .bids_validator import BIDSValidator
+__version__ = '1.1.2'
+__all__ = ['BIDSValidator']
---
+++ packages/bids-validator/bids-validator-1.1.2.tar.gz/bids-validator-1.1.2/setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from setuptools import setup
+import bids_validator
+
+setup(version=bids_validator.__version__)
