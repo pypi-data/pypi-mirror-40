b03fb8cb350b725ca4754a8f992b89cdba243cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:59 +0000

    Add package smsframework-vianett/0.0.2.post0/smsframework-vianett-0.0.2.post0.tar.gz

---
+++ packages/smsframework-vianett/smsframework-vianett-0.0.2.post0.tar.gz/smsframework-vianett-0.0.2.post0/setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+""" SMS framework: Vianett provider """
+
+from setuptools import setup, find_packages
+
+setup(
+    # http://pythonhosted.org/setuptools/setuptools.html
+    name='smsframework-vianett',
+    version='0.0.2-0',
+    author='Mark Vartanyan',
+    author_email='kolypto@gmail.com',
+
+    url='https://github.com/kolypto/py-smsframework-vianett',
+    license='BSD',
+    description=__doc__,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['sms', 'message', 'notification', 'receive', 'send', 'vianett'],
+
+    packages=find_packages(),
+    scripts=[],
+    entry_points={},
+
+    install_requires=[
+        'smsframework >= 0.0.9',
+    ],
+    extras_require={
+        'receiver': ['flask >= 0.10'],  # sms receiving
+    },
+    test_suite='nose.collector',
+    include_package_data=True,
+
+    platforms='any',
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+)
