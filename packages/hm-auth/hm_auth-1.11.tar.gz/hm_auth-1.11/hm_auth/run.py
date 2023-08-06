93628c4e72888454c8945b67a8c6ed7b68b14b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:45 +0000

    Add package hm-auth/1.11/hm_auth-1.11.tar.gz

---
+++ packages/hm-auth/hm_auth-1.11.tar.gz/hm_auth-1.11/hm_auth/run.py
@@ -0,0 +1,47 @@
+from urllib.parse import urlparse
+import webbrowser
+
+from hm_auth import do_auth
+from ytiny.store_userinfo import *
+from hm_auth.config.url import *
+
+
+def do_ping(ip):
+    os.system('start ping '+ip)
+
+
+def open_on_browser(url):
+    webbrowser.open(url)
+
+
+if __name__ == '__main__':
+    print('(*｀∀´*)ノ亻!, 欢迎使用auth~ ')
+    print('输入r:\t\t重新设定账户信息')
+    print('其他输入:\t使用上次账户信息')
+    first_input = input()
+
+    if not user_info_exists() or first_input == 'r':
+        dump_user()
+
+    print('正在发送认证请求...\n')
+    try:
+        if do_auth.do_auth(*load_user()):
+            print("Portal认证成功")
+        else:
+            print('Portal认证失败，请检查账户信息')
+    except TimeoutError or ConnectionError as e:
+        print(e)
+        y_or_n = input('\n是否使用? [ping %s] (y/n)' % urlparse(auth_get_url).netloc)
+        if y_or_n in ('y', 'Y'):
+            do_ping(urlparse(auth_get_url).netloc)
+
+        y_or_n = input('\n是否打开浏览器访问认证页面? [%s] (y/n)' % auth_page_get_url)
+        if y_or_n in ('y', 'Y'):
+            open_on_browser(auth_page_get_url)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        input('\nenter any key...')
+
