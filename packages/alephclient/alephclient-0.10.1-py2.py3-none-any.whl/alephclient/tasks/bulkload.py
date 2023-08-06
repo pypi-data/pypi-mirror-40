3137fa62b90f29c7c4f98bd79531d235a21bbfc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:06 +0000

    Add package alephclient/0.10.1/alephclient-0.10.1-py2.py3-none-any.whl

---
+++ packages/alephclient/alephclient-0.10.1-py2.py3-none-any.whl/alephclient/tasks/bulkload.py
@@ -0,0 +1,13 @@
+import logging
+
+from alephclient.util import load_config_file
+
+log = logging.getLogger(__name__)
+
+
+def bulk_load(api, mapping_file):
+    data = load_config_file(mapping_file)
+    for foreign_id, config in data.items():
+        collection_id = api.load_collection_by_foreign_id(foreign_id, config)
+        log.info("Bulk mapping collection ID: %s", collection_id)
+        api.map_collection(collection_id, data)
