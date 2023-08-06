4dd7ffdeeac81279e7139dc3b614bfbfee5b65ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:50 +0000

    Add package fix-utcnow/0.5/fix_utcnow-0.5.tar.gz

---
+++ packages/fix-utcnow/fix_utcnow-0.5.tar.gz/fix_utcnow-0.5/setup.py
@@ -0,0 +1,31 @@
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+config = {
+    'description': 'fix-utcnow',
+    'author': 'Maksim Afanasevsky',
+    'url': 'https://github.com/maxtwen/fix-utcnow',
+    'download_url': 'https://github.com/maxtwen/fix-utcnow',
+    'author_email': 'maxtwen1@gmail.com',
+    'version': '0.5',
+    'install_requires': [''],
+    'packages': ['fix_utcnow'],
+    'scripts': [],
+    'name': 'fix_utcnow',
+    'data_files': [('fix_utcnow/c', ['fix_utcnow/c/libfixutcnow.so'])],
+    'include_package_data': True
+    # 'long_description': read('README.md'),
+    # 'long_description_content_type': 'text/markdown'
+}
+
+setup(**config)
