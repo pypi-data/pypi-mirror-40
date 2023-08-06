5d6aeca6f6fee4bad65b621c605137531867dcc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:23 +0000

    Add package financeager/0.16/financeager-0.16-py2.py3-none-any.whl

---
+++ packages/financeager/financeager-0.16-py2.py3-none-any.whl/financeager/fflask.py
@@ -0,0 +1,32 @@
+"""Utilities to create and launch flask webservice."""
+
+from flask import Flask
+from flask_restful import Api
+
+from . import PERIODS_TAIL, COPY_TAIL
+from .resources import (PeriodsResource, PeriodResource, EntryResource,
+                        CopyResource)
+
+
+def create_app(config=None):
+    """Create web app with RESTful API built from resources."""
+    app = Flask(__name__)
+    app.config.update(config or {})
+    api = Api(app)
+
+    api.add_resource(PeriodsResource, PERIODS_TAIL)
+    api.add_resource(CopyResource, COPY_TAIL)
+    api.add_resource(PeriodResource,
+            "{}/<period_name>".format(PERIODS_TAIL))
+    api.add_resource(EntryResource,
+        "{}/<period_name>/<table_name>/<eid>".format(PERIODS_TAIL))
+
+    return app
+
+
+def launch_server(debug=False, host=None):
+    """Create and launch flask webservice application. Debug mode and host name
+    are optional.
+    """
+    app = create_app(config={"DEBUG": debug, "SERVER_NAME": host})
+    app.run()
