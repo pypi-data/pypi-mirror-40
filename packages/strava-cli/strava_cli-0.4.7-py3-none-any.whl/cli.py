6a58a3c0df68f0f7a6dc23cbd14531b2585d53cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:59 +0000

    Add package strava-cli/0.4.7/strava_cli-0.4.7-py3-none-any.whl

---
+++ packages/strava-cli/strava_cli-0.4.7-py3-none-any.whl/cli.py
@@ -0,0 +1,20 @@
+import click
+
+from strava.commands import login, logout, get_activities, get_activity, get_profile, get_stats, set_config
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(login)
+cli.add_command(logout)
+cli.add_command(get_activities)
+cli.add_command(get_activity)
+cli.add_command(get_profile)
+cli.add_command(get_stats)
+cli.add_command(set_config)
+
+if __name__ == '__main__':
+    cli()
