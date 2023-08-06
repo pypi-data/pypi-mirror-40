39c0af1b2ccaad946befc92f08df071133e54d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:02 +0000

    Add package edumated/0.0.1/edumated-0.0.1-py3-none-any.whl

---
+++ packages/edumated/edumated-0.0.1-py3-none-any.whl/edumated/edumate.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from tqdm import tqdm
+
+from .fetch import Fetcher
+from .googcal import CalendarTool
+
+
+def main():
+    with open("pass") as conf:
+        username = conf.readline().strip()
+        password = conf.readline().strip()
+        calendar_id = conf.readline().strip()
+
+    cal = CalendarTool(calendar_id)
+    fetcher = Fetcher(username, password)
+
+    print("Fetching Events")
+    dates = pd.date_range(pd.datetime(2019, 1, 31), periods=200)
+    week_data = fetcher.get_simple_dates(dates)
+
+    print("Insert to calendar")
+    [cal.make_events(day) for day in tqdm(week_data)]
