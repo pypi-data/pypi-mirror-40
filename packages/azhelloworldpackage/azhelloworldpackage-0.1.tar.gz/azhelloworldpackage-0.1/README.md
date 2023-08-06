fa6aac78f2dc5230950f6cb691e1ffe3f3313f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:16 +0000

    Add package azhelloworldpackage/0.1/azhelloworldpackage-0.1.tar.gz

---
+++ packages/azhelloworldpackage/azhelloworldpackage-0.1.tar.gz/azhelloworldpackage-0.1/README.md
@@ -0,0 +1,12 @@
+# helloworld
+It's a simple python package that prints hello world!
+
+to test locally
+`pip install .`
+`python test.py`
+
+steps to register your package:
+1- get user account https://test.pypi.org
+2- get user account https://test.pypi.org
+3- `python setup.py sdist bdist_wheel`
+
---
+++ packages/azhelloworldpackage/azhelloworldpackage-0.1.tar.gz/azhelloworldpackage-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name='azhelloworldpackage',  
+    version='0.1',
+    description="hello world!",
+    url="https://github.com/Azadehkhojandi/helloworld",
+    author="Azadeh Khojandi",
+    author_email="azadehkhojandi@gmail.com",
+    license='MIT',
+    packages=['azhelloworldpackage'],
+    zip_safe=False
+ )
