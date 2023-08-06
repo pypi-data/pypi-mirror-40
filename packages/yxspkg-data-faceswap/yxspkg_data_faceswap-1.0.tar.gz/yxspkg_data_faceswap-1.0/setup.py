867f1863e2b8d8f5399cddb247a61f853d9de0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:51 +0000

    Add package yxspkg-data-faceswap/1.0/yxspkg_data_faceswap-1.0.tar.gz

---
+++ packages/yxspkg-data-faceswap/yxspkg_data_faceswap-1.0.tar.gz/yxspkg_data_faceswap-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup 
+import sys
+setup(name='yxspkg_data_faceswap',   
+      version='1.0',    
+      description='mydata',    
+      author='blacksong',    
+      install_requires=[],
+      py_modules=['yxspkg_data_faceswap'],
+      platforms='any',
+      author_email='blacksong@yeah.net',       
+      url='https://github.com/blacksong',
+      classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)   
