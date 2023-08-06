834ecc0a252e592e3b85517d4df7a19ed6947cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:59 +0000

    Add package confluent-kafka-helpers/0.7.3/confluent-kafka-helpers-0.7.3.tar.gz

---
+++ packages/confluent-kafka-helpers/confluent-kafka-helpers-0.7.3.tar.gz/confluent-kafka-helpers-0.7.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(name="confluent-kafka-helpers",
+      version="0.7.3",
+      description="Helpers for Confluent's Kafka Python client",
+      url="https://github.com/fyndiq/confluent_kafka_helpers",
+      author="Fyndiq AB",
+      author_email="support@fyndiq.com",
+      license="MIT",
+      packages=find_packages(),
+      install_requires=[
+          'structlog>=17.2.0',
+          'confluent-kafka>=0.11.5',
+          'fastavro>=0.18.0',
+          'avro-python3>=1.8.2'
+      ],
+      zip_safe=False)
