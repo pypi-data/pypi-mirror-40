21a719a677cdd1e7d8f170aa124225eba52378f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:07 +0000

    Add package adafruit-circuitpython-esp-atcontrol/0.2.1/adafruit-circuitpython-esp-atcontrol-0.2.1.tar.gz

---
+++ packages/adafruit-circuitpython-esp-atcontrol/adafruit-circuitpython-esp-atcontrol-0.2.1.tar.gz/adafruit-circuitpython-esp-atcontrol-0.2.1/examples/espatcontrol_settings.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+settings = {
+    'ssid' : 'my access point',
+    'password' : 'my password',
+    'timezone' : -5, # this is offset from UTC
+    'github_token' : 'abcdefghij0123456789',
+    'aio_feed_webhook' : 'abcdefghij0123456789',
+}
