174574eca17cd7eeb684687177024d9b89a87ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:44 +0000

    Add package chert/19.0.0/chert-19.0.0.tar.gz

---
+++ packages/chert/chert-19.0.0.tar.gz/chert-19.0.0/setup.py
@@ -0,0 +1,42 @@
+"""Sharp and sparky static-site generator for periodic writers.
+"""
+
+from setuptools import setup
+from chert import __version__
+
+__author__ = 'Mahmoud Hashemi'
+__contact__ = 'mahmoud@hatnote.com'
+__url__ = 'https://github.com/mahmoud/chert'
+__license__ = 'BSD'
+
+
+setup(name='chert',
+      version=__version__,
+      description="Sharp and sparky static-site generation.",
+      long_description=__doc__,
+      author=__author__,
+      author_email=__contact__,
+      url=__url__,
+      packages=['chert'],
+      include_package_data=True,
+      zip_safe=False,
+      license=__license__,
+      platforms='any',
+      entry_points={'console_scripts': ['chert = chert.core:main']},
+      install_requires=['ashes>=17.0.0',
+                        'boltons>=17.1.0',
+                        'Markdown==2.6.2',
+                        'Pygments==1.6',
+                        'python-dateutil>=2.2',
+                        'PyYAML==3.13',
+                        'lithoxyl>=0.4.3',
+                        'html5lib>=1.0.1',
+                        'hyperlink>=17.3.1'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Topic :: Internet :: WWW/HTTP',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7']
+      )
