477512f07a8769b97c94283b35dfa7fb84ed97ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:15 +0000

    Add package zencad/0.12.0/zencad-0.12.0.tar.gz

---
+++ packages/zencad/zencad-0.12.0.tar.gz/zencad-0.12.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from wheel.bdist_wheel import bdist_wheel as bdist_wheel_
+from setuptools import setup, Extension, Command
+from distutils.util import get_platform
+
+import zencad
+
+import sys
+
+#try:
+#	import PyQt5
+#except Exception(e):
+#	print(e)
+#	print("PyQt5 needed.")
+#	print("try:")
+#	print("apt-get install python3-pyqt5")
+#	sys.exit()
+
+setup(
+	name = 'zencad',
+	packages = ['zencad'],
+	version = zencad.__version__,
+	license='MIT',
+	description = 'CAD system for righteous zen programmers ',
+	author = 'Sorokin Nikolay',
+	author_email = 'mirmikns@yandex.ru',
+	url = 'https://mirmik.github.io/zencad/',
+	long_description=open("README.md", "r").read(),
+	long_description_content_type='text/markdown',
+	keywords = ['testing', 'cad'],
+	classifiers = [],
+	scripts = ["routine/zencad"],
+
+    include_package_data=True,
+	install_requires=[
+        'evalcache==1.7.0',
+        'pyservoce==1.7.0',
+        'numpy',
+        'pillow',
+        'inotify',
+        'PyQt5',
+    ],
+)
