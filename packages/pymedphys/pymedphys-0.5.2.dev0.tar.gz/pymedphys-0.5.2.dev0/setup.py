22d30c78f98b87cceffd5e2cabfbb7d09196c3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:25 +0000

    Add package pymedphys/0.5.2.dev0/pymedphys-0.5.2.dev0.tar.gz

---
+++ packages/pymedphys/pymedphys-0.5.2.dev0.tar.gz/pymedphys-0.5.2.dev0/setup.py
@@ -0,0 +1,93 @@
+import io
+from glob import glob
+
+import os
+from os.path import basename
+from os.path import dirname
+from os.path import splitext
+
+from setuptools import setup, find_packages
+
+basename = os.path.basename
+dirname = os.path.dirname
+splitext = os.path.splitext
+
+isfile = os.path.isfile
+pjoin = os.path.join
+repo_root = os.path.dirname(os.path.abspath(__file__))
+
+
+def execfile(fname, globs, locs=None):
+    locs = locs or globs
+    exec(compile(open(fname).read(), fname, "exec"), globs, locs)
+
+
+version_ns = {}
+execfile(pjoin(repo_root, 'src', 'pymedphys', '_version.py'), version_ns)
+
+version = version_ns['__version__']
+
+
+def read(*names, **kwargs):
+    return io.open(
+        pjoin(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+def get_data_files():
+    """Get the data files for the package.
+    """
+    return [
+        ('etc/jupyter/jupyter_notebook_config.d', [
+            os.path.relpath(
+                pjoin(repo_root, 'src', 'pymedphys', 'pymedphys.json'), '.')
+        ])
+    ]
+
+
+setup(
+    name="pymedphys",
+    version=version,
+    author="Simon Biggs",
+    author_email="me@simonbiggs.net",
+    description='Medical Physics python modules',
+    long_description=read('README.rst'),
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry'
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    package_data={'pymedphys': []},
+    entry_points={
+        'console_scripts': [
+            'trf2csv=pymedphys.entry_points.trf2csv:trf2csv_cli',
+            'pymedphys=pymedphys.entry_points.gui:gui'
+        ],
+    },
+    license='AGPLv3+',
+    data_files=get_data_files(),
+    install_requires=[
+        'numpy>=1.12',
+        'scipy',
+        'pandas',
+        'matplotlib',
+        'attrs',
+        'psutil',
+        'pymssql',
+        'keyring',
+        'shapely',
+        'pydicom>=1.0',
+        'python-dateutil'
+    ]
+)
---
+++ packages/pymedphys/pymedphys-0.5.2.dev0.tar.gz/pymedphys-0.5.2.dev0/src/pymedphys/gui/build/precache-manifest.eaa0348613ef4553e3a722ceee30851e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a673b48bbcbbe10cc898",
+    "url": "./static/js/runtime~main.a673b48b.js"
+  },
+  {
+    "revision": "25dfe1dcf2171dcb1578",
+    "url": "./static/js/main.25dfe1dc.chunk.js"
+  },
+  {
+    "revision": "2ca52a40fa713c3ed2a4",
+    "url": "./static/js/3.2ca52a40.chunk.js"
+  },
+  {
+    "revision": "da1b2806ae5fae805a30",
+    "url": "./static/js/2.da1b2806.chunk.js"
+  },
+  {
+    "revision": "13053c52e4db504f22e5",
+    "url": "./static/js/1.13053c52.chunk.js"
+  },
+  {
+    "revision": "25dfe1dcf2171dcb1578",
+    "url": "./static/css/main.8a62600a.chunk.css"
+  },
+  {
+    "revision": "da1b2806ae5fae805a30",
+    "url": "./static/css/2.a50748a8.chunk.css"
+  },
+  {
+    "revision": "3232c40b2548fde9b1a81e52697fa21a",
+    "url": "./index.html"
+  }
+];
