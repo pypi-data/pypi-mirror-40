16cb3f1cc646534a46e2c3f5dfe1ae61fd7e4a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:05 +0000

    Add package sam-algorithm/0.3.0/sam-algorithm-0.3.0.tar.gz

---
+++ packages/sam-algorithm/sam-algorithm-0.3.0.tar.gz/sam-algorithm-0.3.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='sam-algorithm',
+    version='0.3.0',
+    description='The Self-Assembling-Manifold algorithm',
+    long_description="The Self-Assembling-Manifold algorithm for analyzing single-cell RNA sequencing data.",
+    long_description_content_type='text/markdown',
+    url='https://github.com/atarashansky/self-assembling-manifold',
+    author='Alexander J. Tarashansky',
+    author_email='tarashan@stanford.edu',
+    keywords='scrnaseq analysis manifold reconstruction',
+    py_modules=["SAM", "utilities"],
+    install_requires=[
+        'numpy>=1.14,<=1.15.2',
+        'scipy',
+        'pandas',
+        'scikit-learn==0.20.0',
+	'numba>=0.37,<0.40',
+	'umap-learn<=0.3.6'],
+    extras_require={
+        'louvain': [
+            'louvain',
+            'scanpy'],
+        'plot': [
+            'matplotlib'],
+        },
+)
