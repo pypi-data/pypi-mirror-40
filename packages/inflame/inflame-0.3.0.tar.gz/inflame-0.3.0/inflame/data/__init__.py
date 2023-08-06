ebce0cbf93a036d4213366763a103b8ca8c06a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:28 +0000

    Add package inflame/0.3.0/inflame-0.3.0.tar.gz

---
+++ packages/inflame/inflame-0.3.0.tar.gz/inflame-0.3.0/inflame/data/__init__.py
@@ -0,0 +1,36 @@
+"""
+The data_pipeline function strings together bits and pieces of text processing
+functionality that are defined in the util folder. It basically takes
+data and target, which are two lists of texts, embeds them in GloVe,
+and finally converts them to pytorch DataLoader objects.   
+"""
+from .util import *
+from sklearn.model_selection import train_test_split
+
+def data_pipeline(data, target: 'lists of texts'):
+   (data_trn, data_vld,
+   target_trn, target_vld) = train_test_split(data, 
+                                              target,
+                                              train_size = .75,
+                                              test_size = .25,
+                                              stratify = target,
+                                              random_state = 999
+                                             )
+   textdata_trn = TextData(data_trn, target_trn)
+   textdata_vld = TextData(data_vld, target_vld)
+
+   textdata_trn.embed()
+   textdata_vld.embed()
+
+   _, target_trn, embedding_trn = textdata_trn.getter()   
+   _, target_vld, embedding_vld = textdata_vld.getter()   
+
+   training_batches = mk_dataloader(standardize_dataset(embedding_trn, 
+                                                        target_trn
+                                                       )
+                                   )
+   validati_batches = mk_dataloader(standardize_dataset(embedding_vld, 
+                                                        target_vld
+                                                       )
+                                   )
+   return training_batches, validati_batches
---
+++ packages/inflame/inflame-0.3.0.tar.gz/inflame-0.3.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+   long_description = f.read()
+
+setup(
+   name="inflame",
+   version="0.3.0",
+   license='MIT',
+   description="Deep learning applied to text classification, in PyTorch",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author="John Jung",
+   author_email="tojohnjung@gmail.com",
+   url="https://github.com/zjohn77/inflame",
+   packages=find_packages(),
+   classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: POSIX :: Linux",
+   ],
+   keywords='deep learning',
+   install_requires=[
+      'scipy>=1.1',
+      'numpy>=1.15',
+      'torch>=0.4',
+      'spacy>=2.0',
+      'scikit_learn>=0.20',
+      'PyYAML>=3',
+   ],
+   dependency_links=['https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.0.0/en_core_web_sm-2.0.0.tar.gz']
+)
