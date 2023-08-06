a803da5a572bf408db8e5bafeb264d5d40a549ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:32 +0000

    Add package lluser/1.32/lluser-1.32.tar.gz

---
+++ packages/lluser/lluser-1.32.tar.gz/lluser-1.32/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+        name='lluser',
+        version='1.32',
+        author='oyy', 
+        author_email='oyy284688@gmail.com', 
+        url='http://dadadakeai.com/lluser/index.html',
+        license='GUN',
+        summary='记录登录',
+        description='上次登录的用户，last login user',
+        packages=['lluser'],
+        install_requires=['requests>=2'],
+        )
