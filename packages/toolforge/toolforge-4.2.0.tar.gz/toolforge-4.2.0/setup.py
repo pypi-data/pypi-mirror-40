1686892bbce246053a20e620081b9b1dc67d9072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:40 +0000

    Add package toolforge/4.2.0/toolforge-4.2.0.tar.gz

---
+++ packages/toolforge/toolforge-4.2.0.tar.gz/toolforge-4.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='toolforge',
+    version='4.2.0',
+    packages=['toolforge'],
+    url='https://wikitech.wikimedia.org/wiki/User:Legoktm/toolforge_library',
+    license='GPL-3.0+',
+    author='Kunal Mehta',
+    author_email='legoktm@member.fsf.org',
+    description='Small library for common tasks on Wikimedia Toolforge',
+    install_requires=['requests', 'pymysql'],
+    test_suite="tests",
+)
