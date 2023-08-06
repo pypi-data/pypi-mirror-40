3678717162a526a41cb1b300237d6f5c4804b24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:35 +0000

    Add package conceptscript/0.0.2.dev0/conceptscript-0.0.2.dev0.tar.gz

---
+++ packages/conceptscript/conceptscript-0.0.2.dev0.tar.gz/conceptscript-0.0.2.dev0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='conceptscript',
+      version='0.0.2dev',
+      description='ConceptScript',
+      author='Ole Meyer',
+      author_email='mail@olemeyer.com',
+      packages=[],#find_packages(exclude=['tests','test']),
+      #install_requires=['numpy','keras-rl']
+    )
