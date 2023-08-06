939b8fc931b5cad525354e0469ce586d4ccae293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:35 +0000

    Add package chatora.mmid/0.1/chatora.mmid-0.1.tar.gz

---
+++ packages/chatora.mmid/chatora.mmid-0.1.tar.gz/chatora.mmid-0.1/chatora/mmid/constant.py
@@ -0,0 +1,53 @@
+__all__ = (
+    'EPOCH_TSNS100_DIFF_BETWEEN_UUID1_AND_UNIX',
+    'EPOCH_TSNS_DIFF_BETWEEN_UUID1_AND_UNIX',
+    'MAX_CLOCK_SEQ',
+    'NODE_MASK',
+    'UTC',
+    'UUID_TS_DECIMAL_CTX',
+    'TSNS_DECIMAL_EXP',
+    'TSNS100_DECIMAL_EXP',
+    'TSUS_DECIMAL_EXP',
+    'TSMS_DECIMAL_EXP',
+)
+
+import datetime
+import decimal
+
+
+UTC = datetime.timezone.utc
+MAX_CLOCK_SEQ: int = (1 << 14) - 1
+NODE_MASK: int = 0xffffffffffff
+
+
+# The number of 100-ns intervals between the
+# UUID epoch 1582-10-15 00:00:00 and the Unix epoch 1970-01-01 00:00:00.
+EPOCH_TSNS100_DIFF_BETWEEN_UUID1_AND_UNIX: int = 0x01b21dd213814000
+EPOCH_TSNS_DIFF_BETWEEN_UUID1_AND_UNIX: int = EPOCH_TSNS100_DIFF_BETWEEN_UUID1_AND_UNIX * 100
+
+# 4865183701896396700
+#MMID1V0_MAX_TSNS = 0x7f_ffff_ffff_ffff * 100 + MMID1V0_EPOCH_TSNS
+#MMID1V0_MAX_DT = datetime.datetime.fromtimestamp(
+#    # 4865183701.896396
+#    float((decimal.Decimal(MMID1V0_MAX_TSNS) / 1000_000_000).quantize(
+#        exp=decimal.Decimal('0.000000'),
+#        rounding=decimal.ROUND_DOWN,
+#    )),
+#    tz=datetime.timezone.utc,
+#)
+
+
+UUID_TS_DECIMAL_CTX = decimal.Context(
+    prec=decimal.DefaultContext.prec,
+    rounding=decimal.ROUND_DOWN,
+    traps=[
+        decimal.InvalidOperation,
+        decimal.DivisionByZero,
+        decimal.Overflow,
+    ],
+)
+
+TSNS_DECIMAL_EXP = decimal.Decimal('0.000_000_001')
+TSNS100_DECIMAL_EXP = decimal.Decimal('0.000_000_1')
+TSUS_DECIMAL_EXP = decimal.Decimal('0.000_001')
+TSMS_DECIMAL_EXP = decimal.Decimal('0.001')
