09c6b7650bbb5509a4c7c3447088a0adb2a2f202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:21 +0000

    Add package COMPAS/0.4.0/COMPAS-0.4.0.tar.gz

---
+++ packages/COMPAS/COMPAS-0.4.0.tar.gz/COMPAS-0.4.0/src/compas/interop/__init__.py
@@ -0,0 +1,51 @@
+"""
+********************************************************************************
+compas.interop
+********************************************************************************
+
+.. currentmodule:: compas.interop
+
+.. warning::
+
+    The functionality of this package is experimental and subject to frequent change.
+    For now, don't use it for anything important :)
+
+
+C++
+===
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    cpp.xdarray.Array1D
+    cpp.xdarray.Array2D
+    cpp.xdarray.Array3D
+    cpp.xdarray.shape
+    cpp.xdarray.zeros
+    cpp.xdarray.ones
+    cpp.xdarray.eye
+
+
+CSharp
+======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+
+Fortran
+=======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__all__ = []
