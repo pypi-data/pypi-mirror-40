3bd0fdc9cb142e9a801725c95fd6a375b8982e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:40 +0000

    Add package python-logstash-g/0.6.0/python-logstash-g-0.6.0.tar.gz

---
+++ packages/python-logstash-g/python-logstash-g-0.6.0.tar.gz/python-logstash-g-0.6.0/logstash/handler_file.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from logging.handlers import RotatingFileHandler
+from logstash.mixins import ConfigFormatterClassMixin
+
+
+class FileLogstashHandler(ConfigFormatterClassMixin, RotatingFileHandler, object):
+    """
+    Python logging handler for Logstash.
+    Write log into disk files.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(FileLogstashHandler, self).__init__(*args, **kwargs)
+        message_type = kwargs.get('message_type', 'logstash')
+        tags = kwargs.get('tags', None)
+        fqdn = kwargs.get('fqdn', False)
+        version = kwargs.get('version', 0)
+        formatter_path = kwargs.get('formatter_path', None)
+        self.config_formatter(
+            message_type=message_type,
+            tags=tags,
+            fqdn=fqdn,
+            version=version,
+            class_path=formatter_path,
+        )
