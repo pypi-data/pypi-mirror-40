11f711efa58f7f96bc5a59af1170cf89f72c1f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:50 +0000

    Add package lapdog/0.9.9/lapdog-0.9.9.tar.gz

---
+++ packages/lapdog/lapdog-0.9.9.tar.gz/lapdog-0.9.9/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+from lapdog import __version__
+setup(
+    name = 'lapdog',
+    version = __version__,
+    packages = [
+        'lapdog',
+        'lapdog.api'
+    ],
+    package_data={
+        '':[
+            'cromwell/wdl_pipeline.yaml',
+            'cromwell/LICENSE',
+            'cromwell/README.md',
+            'vue/.babelrc',
+            'vue/index.html',
+            'vue/package.json',
+            'vue/webpack.config.js',
+            'vue/src/main.js',
+            'vue/src/App.vue',
+            'vue/src/Pages/*.vue',
+            'api/swagger/lapdog.yaml',
+            'wdl_pipeline.yaml'
+        ],
+    },
+    description = 'A relaxed wrapper for FISS and dalmatian',
+    author = 'Aaron Graubert - Broad Institute - Cancer Genome Computational Analysis',
+    author_email = 'aarong@broadinstitute.org',
+    long_description = 'A wrapper for FISS and dalmatian',
+    entry_points = {
+        'console_scripts': [
+            'lapdog = lapdog.__main__:main'
+        ]
+    },
+    install_requires = [
+        'firecloud-dalmatian',
+        'google-cloud-storage>=1.9.0',
+        'pyyaml',
+        'agutil>=4.0.0',
+        'flask_cors',
+        'crayons',
+        'connexion',
+        'oauth2client',
+        'requests'
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+    ],
+)
