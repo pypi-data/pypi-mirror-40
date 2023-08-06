6162b4ec6176e89f22e10da4b37bfdb105fe01be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:49 +0000

    Add package rallf/0.2.3/rallf-0.2.3-py3-none-any.whl

---
+++ packages/rallf/rallf-0.2.3-py3-none-any.whl/rallf/sdk/listener.py
@@ -0,0 +1,29 @@
+from json.decoder import JSONDecodeError
+
+from rallf.sdk.rallf_error import RallfError
+from rallf.tools.communicator import Communicator
+
+
+class Listener(Communicator):
+    def __init__(self, input, output):
+        super().__init__(input, output)
+
+    def listen(self, task):
+        try:
+            req = self.wait()
+            if 'method' in req:
+                try:
+                    if req['method'] != "delegate":
+                        raise RallfError(-32601, "Invalid method '%s'" % req['method'])
+
+                    resp = getattr(task, req['params']['routine'])(req['params']['args'])
+                    task.network.rpcresponse(id=req['id'], result=resp, error=False)
+                except RallfError as e:
+                    task.network.rpcresponse(id=req['id'], error=e.dict(), result=False)
+            else:
+                self.mq.append(req)
+
+        except JSONDecodeError:
+            task.network.rpcresponse(id=None, error={"code": -32700, "message": "Parse error"})
+        except (EOFError, InterruptedError):
+            task.finish()
