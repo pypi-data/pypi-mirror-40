67416ff65f8ccb40b21e26b84dedf33dbe9e355b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:36 +0000

    Add package stochastic-arrow/0.0.11/stochastic-arrow-0.0.11.tar.gz

---
+++ packages/stochastic-arrow/stochastic-arrow-0.0.11.tar.gz/stochastic-arrow-0.0.11/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from distutils.core import setup
+
+with open("README.md", 'r') as readme:
+	long_description = readme.read()
+
+setup(
+	name='stochastic-arrow',
+	version='0.0.11',
+	packages=['arrow'],
+	author='Ryan Spangler',
+	author_email='spanglry@stanford.edu',
+	url='https://github.com/CovertLab/arrow',
+	license='MIT',
+	long_description=long_description,
+	long_description_content_type='text/markdown')
