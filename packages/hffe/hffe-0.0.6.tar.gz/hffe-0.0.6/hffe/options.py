30f39c0630d8ec95640ca9f1f0034652ee908ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:15 +0000

    Add package hffe/0.0.6/hffe-0.0.6.tar.gz

---
+++ packages/hffe/hffe-0.0.6.tar.gz/hffe-0.0.6/hffe/options.py
@@ -0,0 +1,41 @@
+from pandas import read_csv
+
+
+class optionsFromCSV:
+    def __init__(self, filepath, N, usecols=None, dtype=None):
+        """Constructor: creates an iterable over options data stored
+                        in a CSV file.
+
+        Args:
+            filepath (str): String to csv file containing options data.
+            N (int): Number of obversvations per option. For example: N=405
+                     means there are 405 observations per option in the file,
+                     which amounts to 1-minute observations.
+            usecols (tuple of int): A tuple of integers denoting which
+                     columns to import from the file.
+                     For example: usecols=(0, 2) will import the first and
+                     third columns of the file. The default is None and
+                     imports all columns.
+            dtype (str or dict): If a string is passed, then all values
+                     are imported using the same data type. If a dict is
+                     passed, then the keys should denote the column numbers
+                     and the values should be strings with the desired types.
+                     For example: {0: 'float_', 1: 'int_', 13: 'str_'}.
+        """
+        self.filepath = filepath
+        self.N = N              # number of observations per option per day
+        config = {'sep': ',', 'header': 0, 'usecols': usecols, 'dtype': dtype}
+        self.data = read_csv(filepath, **config).values
+        if len(self.data) % N != 0:
+            raise ValueError("Total observations not divisible by N.")
+
+    def optionsIterator(self):
+        """Iterates over the options data, yielding data for each of
+        the options.
+        """
+        total_obs = len(self.data)//self.N
+        for i in range(total_obs):
+            yield self.data[i*self.N:(i+1)*self.N, :]
+
+    def __iter__(self):
+        return self.optionsIterator()
