f464ad7ac6f1143b4facec583c717f54b7d3f605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:18 +0000

    Add package polt/0.9.0/polt-0.9.0.tar.gz

---
+++ packages/polt/polt-0.9.0.tar.gz/polt-0.9.0/polt/version.py
@@ -0,0 +1,11 @@
+__version__ = "0.9.0"
+
+POLT_PARSER_ENTRY_POINT = "polt.parser"
+"""
+The entry point name for :class:`Parser` extensions
+"""
+
+POLT_ANIMATOR_ENTRY_POINT = "polt.animator"
+"""
+The entry point name for :class:`Animator` extensions
+"""
