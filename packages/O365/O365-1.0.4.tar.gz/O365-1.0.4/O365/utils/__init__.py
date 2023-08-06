610c0bcc0b86349a3c2a52850f4feef416e2c38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:39 +0000

    Add package O365/1.0.4/O365-1.0.4.tar.gz

---
+++ packages/O365/O365-1.0.4.tar.gz/O365-1.0.4/O365/utils/__init__.py
@@ -0,0 +1,6 @@
+from .attachment import BaseAttachments, BaseAttachment, AttachableMixin
+from .utils import ApiComponent, OutlookWellKnowFolderNames
+from .utils import ImportanceLevel, TrackerSet
+from .utils import NEXT_LINK_KEYWORD, ME_RESOURCE, USERS_RESOURCE
+from .utils import OneDriveWellKnowFolderNames, Pagination, Query
+from .windows_tz import IANA_TO_WIN, WIN_TO_IANA
