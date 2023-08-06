bfe9139f48e0cd08b2a3b65bed8d35a50d6970f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:22 +0000

    Add package pyjones/0.6.0/pyjones-0.6.0-py3-none-any.whl

---
+++ packages/pyjones/pyjones-0.6.0-py3-none-any.whl/pyjones/opticalelements.py
@@ -0,0 +1,123 @@
+"""This module provides optical elements which influence the polarization of light. The basic class is
+JonesMatrix from which all predefined optical elements inherit from. Predefined optical elements are:
+
+* PolarizerHorizontal
+* PolarizerVertical
+* Polarizer(angle)
+* QuarterWavePlate(angle)
+* HalfWavePlate(angle)
+
+"""
+
+from __future__ import print_function
+from pyjones.polarizations import *
+
+
+class JonesMatrix(object):
+    def __init__(self, matrix):
+        """This is the baseclass which describes a polarization influencing optical element.
+
+        :param matrix: A 2x2 matrix either of type np.matrix or a list of two two-element lists describing
+                       the effect of that optical element to the polarization state of passing light.
+        """
+        if hasattr(matrix, '__iter__'):
+            np_not_right_shape = True
+            if isinstance(matrix, np.matrix):
+                np_not_right_shape = True if matrix.shape != (2, 2) else False
+            if (len(matrix) != 2 or len(matrix[0]) != 2 or len(matrix[1]) != 2) and np_not_right_shape:
+                raise ValueError('Shape of array/matrix must be 2x2')
+            else:
+                self.matrix = np.matrix(matrix).astype(complex)
+        else:
+            raise ValueError('Parameter must be either 2x2 array or numpy.matrix')
+
+    def __repr__(self):
+        return 'JonesMatrix([[%s, %s], [%s, %s]])' % (self.matrix[0, 0], self.matrix[0, 1], self.matrix[1, 0],
+                                                      self.matrix[1, 1])
+
+    def __mul__(self, other):
+        """The multiplication operator is overloaded to allow for multiplication of two Jones matrices as well as
+        multiplication with a Jones Vector
+
+        :param other: JonesMatrix or JonesVector with which the current instance is multiplied
+        :return: JonesMatrix or JonesVector
+        """
+        if isinstance(other, JonesVector):
+            resulting_polarisation = self.matrix * other.polarization_vector.T
+            return JonesVector([resulting_polarisation[0, 0], resulting_polarisation[1, 0]], normalize=False)
+        elif isinstance(other, JonesMatrix):
+            return JonesMatrix(self.matrix * other.matrix)
+        else:
+            raise TypeError('Multiplication does only work for JonesMatrix or JonesVector')
+
+
+class PolarizerHorizontal(JonesMatrix):
+    def __init__(self):
+        """This is a subclass of JonesMatrix corresponding to a horizontal polarizer"""
+
+        matrix = [[1.0, 0.0], [0.0, 0.0]]
+        super(PolarizerHorizontal, self).__init__(matrix)
+
+
+class PolarizerVertical(JonesMatrix):
+    def __init__(self):
+        """This is a subclass of JonesMatrix corresponding to a vertical polarizer"""
+
+        matrix = [[0.0, 0.0], [0.0, 1.0]]
+        super(PolarizerVertical, self).__init__(matrix)
+
+
+class Polarizer(JonesMatrix):
+    def __init__(self, angle):
+        """This is a subclass of JonesMatrix corresponding to a polarizer with angle
+
+        :param angle: Angle of the polarizer with respect to horizontal plane
+        """
+        angle = np.radians(angle)
+        matrix = [[np.cos(angle) ** 2, np.sin(angle) * np.cos(angle)],
+                  [np.sin(angle) * np.cos(angle), np.sin(angle) ** 2]]
+        super(Polarizer, self).__init__(matrix)
+
+
+class QuarterWavePlate(JonesMatrix):
+    def __init__(self, angle):
+        """This is a subclass of JonesMatrix corresponding to a quarter wave plate with angle
+
+        :param angle: Angle of the fast axis of the quarter wave plate with respect to horizontal plane
+        """
+
+        angle = np.radians(angle)
+        matrix = np.exp(1.0j * np.pi / 4.0) * np.matrix([[np.cos(angle) ** 2 + 1.0j * np.sin(angle) ** 2,
+                                                          (1.0 - 1.0j) * np.sin(angle) * np.cos(angle)],
+                                                         [(1.0 - 1.0j) * np.sin(angle) * np.cos(angle),
+                                                          np.sin(angle) ** 2 + 1.0j * np.cos(angle) ** 2]])
+        super(QuarterWavePlate, self).__init__(matrix)
+
+
+class HalfWavePlate(JonesMatrix):
+    def __init__(self, angle):
+        """This is a subclass of JonesMatrix corresponding to a half wave plate with angle
+
+        :param angle: Angle of the fast axis of the half wave plate with respect to horizontal plane
+        """
+
+        angle = 2 * np.radians(angle)
+        matrix = [[np.cos(angle), np.sin(angle)], [np.sin(angle), -np.cos(angle)]]
+        super(HalfWavePlate, self).__init__(matrix)
+
+
+class PhaseRetarder(JonesMatrix):
+    def __init__(self, angle, eta):
+        """This is a subclass of JonesMatrix corresponding to an arbitrary phase retarder with angle
+
+        :param angle: Angle of the fast axis of the phase retarder with respect to horizontal plane
+        :param eta: Phase retardance in degree
+        """
+
+        angle = np.radians(angle)
+        eta = np.radians(eta)
+        matrix = np.exp(- 1.0j * eta / 2.0) * np.matrix([[np.cos(angle) ** 2 + np.exp(1j * eta) * np.sin(angle) ** 2,
+                                                          (1.0 - np.exp(1j * eta)) * np.sin(angle) * np.cos(angle)],
+                                                         [(1.0 - np.exp(1j * eta)) * np.sin(angle) * np.cos(angle),
+                                                          np.sin(angle) ** 2 + np.exp(1j * eta) * np.cos(angle) ** 2]])
+        super(PhaseRetarder, self).__init__(matrix)
---
+++ packages/pyjones/pyjones-0.6.0-py3-none-any.whl/pyjones/polarizations.py
@@ -0,0 +1,224 @@
+"""This module provides Jones vectors as a two dimensional representation of the polarization of light.
+The basic class is JonesVector from which all predefined polarizations inherit from. Predefined polarizations are:
+
+* LinearHorizontal
+* LinearVertical
+* LinearDiagonal
+* LinearAntiDiagonal
+* Linear(angle)
+* CircularRight
+* CircularLeft
+
+"""
+
+from __future__ import print_function
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_Poincare_sphere():
+    """Sets up a figure and a matplotlib axes instance with a Poincare sphere on which a polarization can be plotted
+
+    :return: matplotlib.ax
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    u = np.linspace(-np.pi, np.pi, 50)
+    v = np.linspace(0, np.pi, 50)
+    x = np.outer(np.cos(u), np.sin(v))
+    y = np.outer(np.sin(u), np.sin(v))
+    z = np.outer(np.ones(np.size(u)), np.cos(v))
+    ax.plot_surface(x, y, z, rstride=5, cstride=5,
+                    color='#c0c0c0', alpha=0.1)
+    ax.plot_wireframe(x, y, z, rstride=5, cstride=5,
+                      color='grey',
+                      alpha=0.2)
+    ax.plot(np.cos(u), np.sin(u), zdir='z', lw=1, color='grey')
+    ax.plot(np.cos(u), np.sin(u), zdir='x', lw=1, color='grey')
+    ax.plot(np.cos(u), np.sin(u), zdir='y', lw=1, color='grey')
+    style = {'fontsize': 16,
+             'horizontalalignment': 'center',
+             'verticalalignment': 'center'}
+    ax.text(0, 1.1, 0, 'D', **style)
+    ax.text(0, -1.1, 0, 'A', **style)
+    ax.text(1.1, 0, 0, 'H', **style)
+    ax.text(-1.1, 0, 0, 'V', **style)
+    ax.text(0, 0, 1.1, 'L', **style)
+    ax.text(0, 0, -1.1, 'R', **style)
+    ax.set_axis_off()
+    ax.set_aspect(0.95)
+    return fig, ax
+
+
+def show_plots():
+    """Invokes the interactive plotting window
+
+    """
+    plt.show()
+
+
+class JonesVector(object):
+    eps = 1e-15
+
+    def __init__(self, polarization, normalize=True, normal_form=True):
+        """This represents a Jones vector and is one representation of the polarisation of light.
+
+        :param polarization: An two element iterable with complex numbers representing the value and phase
+                             of the Ex and Ey light component
+        """
+        if hasattr(polarization, '__iter__'):
+            if len(polarization) != 2:
+                raise ValueError('Length of vector/list must be excactly 2')
+            else:
+                self.polarization_vector = np.matrix(np.array(polarization).astype(complex))
+        else:
+            raise ValueError('Parameter must be either a list or a numpy.array')
+        if normalize:
+            self._normalize()
+        if normal_form:
+            self._make_normal_form()
+        #  truncation for small values
+        for idx, num in enumerate([self.Ex, self.Ey]):
+            new_real = num.real
+            new_imag = num.imag
+            if abs(new_real) < JonesVector.eps:
+                new_real = 0.0
+            if abs(new_imag) < JonesVector.eps:
+                new_imag = 0.0
+            self[idx] = new_real+1j*new_imag
+
+    def __repr__(self):
+        return 'JonesVector([%s, %s])' % (self.polarization_vector[0, 0], self.polarization_vector[0, 1])
+
+    def __getitem__(self, item):
+        if not isinstance(item, int):
+            raise TypeError('Needs to be integer')
+        elif item not in [0, 1]:
+            raise IndexError('Index can be either 0 or 1')
+        else:
+            return self.polarization_vector[0, item]
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, int):
+            raise TypeError('Needs to be integer')
+        elif key not in [0, 1]:
+            raise IndexError('Index can be either 0 or 1')
+        elif not isinstance(value, (int, float, complex)):
+            raise ValueError('Value needs to be numeric')
+        else:
+            self.polarization_vector[0, key] = value
+
+    def _normalize(self):
+        self.polarization_vector /= np.sqrt(self.intensity)
+
+    def _make_normal_form(self):
+        E_x_abs = np.abs(self.Ex)
+        E_y_abs = np.abs(self.Ey)
+        phi_x = np.angle(self.Ex)
+        phi_y = np.angle(self.Ey)
+        phi_y_rotated = phi_y - phi_x
+        E_x_new = E_x_abs * np.exp(1j*0)
+        E_y_new = E_y_abs * np.exp(1j*phi_y_rotated)
+        self[0] = E_x_new
+        self[1] = E_y_new
+
+    def plot(self, poincare_sphere_axis, color='r', size=10):
+        """Visualizes the polarization on the Poincare sphere
+
+        """
+        poincare_sphere_axis.scatter(*self.Stokes[1:], c=color, s=size)
+
+    @property
+    def intensity(self):
+        """Property which returns the intensity of the Jones vector
+
+        :return: The intensity of the Jones vector
+        :rtype: float
+        """
+        return np.sum(np.square(np.abs(self.polarization_vector)))
+
+    @property
+    def Ex(self):
+        """Property which returns the x component of the electric field
+
+        :return: The x component of the electric field
+        :rtype: complex
+        """
+        return self[0]
+
+    @property
+    def Ey(self):
+        """Property which returns the y component of the electric field
+
+        :return: The y component of the electric field
+        :rtype: complex
+        """
+        return self[1]
+
+    @property
+    def Stokes(self):
+        """Property which returns the Stokes parameter representation of the Polarization
+
+        :return: Stokes parameters
+        :rtype: tuple
+        """
+        S0 = self.intensity
+        S1 = np.abs(self.Ex) ** 2 - np.abs(self.Ey) ** 2
+        S2 = 2 * np.real(self.Ex * np.conjugate(self.Ey))
+        S3 = -2 * np.imag(self.Ex * np.conjugate(self.Ey))
+        return S0, S1, S2, S3
+
+
+class LinearHorizontal(JonesVector):
+    def __init__(self):
+        """This is a subclass of JonesVector corresponding to linear horizontal polarisation"""
+
+        super(LinearHorizontal, self).__init__([1.0, 0.0])
+
+
+class LinearVertical(JonesVector):
+    def __init__(self):
+        """This is a subclass of JonesVector corresponding to linear vertical polarisation"""
+
+        super(LinearVertical, self).__init__([0.0, 1.0])
+
+
+class Linear(JonesVector):
+    def __init__(self, angle):
+        """This is a subclass of JonesVector corresponding to a linear polarization with angle
+
+        :param angle: Angle of the linear polarization with respect to horizontal plane
+        """
+
+        angle = np.radians(angle)
+        super(Linear, self).__init__([np.cos(angle), np.sin(angle)])
+
+
+class LinearDiagonal(JonesVector):
+    def __init__(self):
+        """This is a subclass of JonesVector corresponding to linear diagonal polarisation"""
+
+        super(LinearDiagonal, self).__init__([1.0, 1.0])
+
+
+class LinearAntidiagonal(JonesVector):
+    def __init__(self):
+        """This is a subclass of JonesVector corresponding to linear antidiagonal polarisation"""
+
+        super(LinearAntidiagonal, self).__init__([1.0, -1.0])
+
+
+class CircularRight(JonesVector):
+    def __init__(self):
+        """This is a subclass of JonesVector corresponding to right circular polarisation"""
+
+        super(CircularRight, self).__init__([1.0, -1.0j])
+
+
+class CircularLeft(JonesVector):
+    def __init__(self):
+        """This is a subclass of JonesVector corresponding to left circular polarisation"""
+
+        super(CircularLeft, self).__init__([1.0, 1.0j])
