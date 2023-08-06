25364ae56fbefc73698eed3d390da289c3326dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:45 +0000

    Add package attotimebuilder/0.0.3/attotimebuilder-0.0.3.tar.gz

---
+++ packages/attotimebuilder/attotimebuilder-0.0.3.tar.gz/attotimebuilder-0.0.3/setup.py
@@ -0,0 +1,42 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils import setup
+
+readme = open('README.rst', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+setup(
+    name='attotimebuilder',
+    version='0.0.3',
+    description='A library for using the attotime datetime API with aniso8601',
+    long_description=README_TEXT,
+    author='Brandon Nielsen',
+    author_email='nielsenb@jetfuse.net',
+    url='https://bitbucket.org/nielsenb/attotimebuilder',
+    install_requires=[
+        'aniso8601>=4.0.0, <5.0.0',
+        'attotime>=0.1.3, <0.2.0'
+    ],
+    packages=['attotimebuilder'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='iso8601 attotime aniso8601 datetime',
+    project_urls={
+        'Source': 'https://bitbucket.org/nielsenb/attotimebuilder',
+        'Tracker': 'https://bitbucket.org/nielsenb/attotimebuilder/issues'
+    }
+)
