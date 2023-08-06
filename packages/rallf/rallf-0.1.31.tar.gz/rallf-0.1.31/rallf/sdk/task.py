4d6a1e481073a7c75174a80a16dd7329cc499ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:23 +0000

    Add package rallf/0.1.31/rallf-0.1.31.tar.gz

---
+++ packages/rallf/rallf-0.1.31.tar.gz/rallf-0.1.31/rallf/sdk/task.py
@@ -0,0 +1,33 @@
+from rallf.sdk.network import Network
+from rallf.sdk.listener import Listener
+from rallf.sdk.logger import Logger
+
+
+class Task:
+
+    def __init__(self, manifest, robot, input, output):
+        self.manifest = manifest
+        self.robot = robot
+        self.finished = False
+        self.status = "stopped"
+        self.logger = Logger(input, output)
+        self.network = Network(input, output)
+        self.listener = Listener(input, output)
+        self.home = "%s/data/%s" % (robot.home, manifest['fqtn'])
+
+    def warmup(self):
+        self.status = "ready"
+
+    def waitloop(self):
+        while not self.finished:
+            self.listener.listen(self)
+
+    def main(self, input):
+        pass
+
+    def cooldown(self):
+        self.status = "finished"
+
+    def finish(self):
+        self.status = "terminating"
+        self.finished = True
