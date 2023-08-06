19054e806e2b2b10a1b34b195bee049b4d10f1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:48 +0000

    Add package magnum/7.1.0/magnum-7.1.0.tar.gz

---
+++ packages/magnum/magnum-7.1.0.tar.gz/magnum-7.1.0/magnum/drivers/common/templates/kubernetes/fragments/enable-ingress-controller.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+step="enable-ingress-controller"
+printf "Starting to run ${step}\n"
+
+# Enables the specified ingress controller.
+#
+# Currently there is only support for traefik.
+. /etc/sysconfig/heat-params
+
+function writeFile {
+    # $1 is filename
+    # $2 is file content
+
+    [ -f ${1} ] || {
+        echo "Writing File: $1"
+        mkdir -p $(dirname ${1})
+        cat << EOF > ${1}
+$2
+EOF
+    }
+}
+
+if [ "$(echo $INGRESS_CONTROLLER | tr '[:upper:]' '[:lower:]')" = "traefik" ]; then
+    $enable-ingress-traefik
+fi
+
+printf "Finished running ${step}\n"
