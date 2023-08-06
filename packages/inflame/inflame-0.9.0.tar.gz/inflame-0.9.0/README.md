3e37e98511ad6a597b26c942cd5b95078f03d0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:04 +0000

    Add package inflame/0.9.0/inflame-0.9.0.tar.gz

---
+++ packages/inflame/inflame-0.9.0.tar.gz/inflame-0.9.0/README.md
@@ -0,0 +1,13 @@
+## 1. The Goal
+PyTorch is among the best deep learning frameworks out there. It's relatively easy to use and the coding style is readable and pythonic. Nevertheless, there's still a fair amount of boilerplate code and hard-coded numbers in a typical pytorch project. The goal of this project (a.k.a. inflame) is to abstract away the pieces that remain the same from model to model, so that the same deep learning template, or archetype, can be quickly reapplied to new problems. 
+
+## 2. Install
+```sh
+pip install inflame
+```
+
+## 3. Quick Start
+```sh
+inflame-example news
+inflame-example newsgrp
+```
---
+++ packages/inflame/inflame-0.9.0.tar.gz/inflame-0.9.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+   long_description = f.read()
+
+setup(
+   name="inflame",
+   version="0.9.0",
+   license='MIT',
+   description="Deep learning applied to text classification, in PyTorch",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author="John Jung",
+   author_email="tojohnjung@gmail.com",
+   url="https://github.com/zjohn77/inflame",
+   classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: POSIX :: Linux",
+   ],
+   keywords='deep-learning',
+   install_requires=[
+      'scipy>=1.1',
+      'numpy>=1.15',
+      'torch>=0.4',
+      'scikit_learn>=0.20',
+      'PyYAML>=3',
+      'gensim>=3'
+   ],
+   packages=find_packages(),
+   scripts=['usage/inflame-example']
+)
