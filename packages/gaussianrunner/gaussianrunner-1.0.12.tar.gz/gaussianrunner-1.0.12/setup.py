bae313f22f2ec51859d97c06c338a48c0a97ce2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:17 +0000

    Add package gaussianrunner/1.0.12/gaussianrunner-1.0.12.tar.gz

---
+++ packages/gaussianrunner/gaussianrunner-1.0.12.tar.gz/gaussianrunner-1.0.12/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='gaussianrunner',
+      version='1.0.12',
+      description='A script to run Gaussian automatically.',
+      keywords="Gaussian",
+      url='https://github.com/njzjz/gaussianrunner',
+      author='Jinzhe Zeng',
+      author_email='jzzeng@stu.ecnu.edu.cn',
+      packages=['gaussianrunner'],
+      install_requires=['mpi4py'])
