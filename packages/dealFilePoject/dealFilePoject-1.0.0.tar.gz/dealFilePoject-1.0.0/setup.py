2d676646a4e7e6c882f4a48ab0e4db1f6be7d4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:19 +0000

    Add package dealFilePoject/1.0.0/dealFilePoject-1.0.0.tar.gz

---
+++ packages/dealFilePoject/dealFilePoject-1.0.0.tar.gz/dealFilePoject-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+		name			=	'dealFilePoject',
+		version			=	'1.0.0',
+		py_modules		=	['dealFilePoject'],
+		author			=	'wallace.wang',
+		author_email	=	'wangwen5366@163.com',
+		url				=	'http://www.headfirstlabs.com',
+		description		=	'Deal data of file V1.0.0',
+	)
