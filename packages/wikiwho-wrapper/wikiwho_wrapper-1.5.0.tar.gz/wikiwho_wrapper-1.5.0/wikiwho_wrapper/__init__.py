d5458a0d1ba8fc491e95ef52319d2d316b30bc15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:08 +0000

    Add package wikiwho-wrapper/1.5.0/wikiwho_wrapper-1.5.0.tar.gz

---
+++ packages/wikiwho-wrapper/wikiwho_wrapper-1.5.0.tar.gz/wikiwho_wrapper-1.5.0/wikiwho_wrapper/__init__.py
@@ -0,0 +1,11 @@
+import pkg_resources
+name = "wikiwho_wrapper"
+try:
+	__version__ = pkg_resources.require(name)[0].version
+except:
+	__version__ = None
+
+
+from .api import WikiWhoAPI
+from .views import DataView
+from .wikiwho import WikiWho
