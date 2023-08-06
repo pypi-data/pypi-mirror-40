f9ae37716276cb90f87812158de4eb8925572bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:12 +0000

    Add package pywmi/0.2.21/pywmi-0.2.21.tar.gz

---
+++ packages/pywmi/pywmi-0.2.21.tar.gz/pywmi-0.2.21/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Distribute: python setup.py sdist upload
+
+setup(
+    name='pywmi',
+    version='0.2.21',
+    description='Essential tools and interfaces for WMI',
+    url='http://github.com/samuelkolb/pywmi',
+    author='Samuel Kolb',
+    author_email='samuel.kolb@me.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['pysmt', 'numpy', 'future', 'typing', 'matplotlib', 'pillow', 'polytope', 'tabulate', 'problog'],
+    setup_requires=['pytest-runner'],
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "pywmi-install = pywmi.install:main"
+        ]
+    }
+)
