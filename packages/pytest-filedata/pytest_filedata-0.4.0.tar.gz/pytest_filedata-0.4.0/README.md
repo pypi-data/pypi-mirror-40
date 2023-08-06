d3dfd2102cda9954c129f802e95a68dc2371cb17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:26 +0000

    Add package pytest-filedata/0.4.0/pytest_filedata-0.4.0.tar.gz

---
+++ packages/pytest-filedata/pytest_filedata-0.4.0.tar.gz/pytest_filedata-0.4.0/README.md
@@ -0,0 +1,56 @@
+
+# pytest filedata
+
+[![PyPI](https://img.shields.io/pypi/v/pytest-filedata.svg?maxAge=3600)](https://pypi.python.org/pypi/pytest-filedata)
+[![PyPI](https://img.shields.io/pypi/pyversions/pytest-filedata.svg?maxAge=3600)](https://pypi.python.org/pypi/pytest-filedata)
+[![Travis CI](https://img.shields.io/travis/20c/pytest-filedata.svg?maxAge=3600)](https://travis-ci.org/20c/pytest-filedata)
+[![Code Health](https://landscape.io/github/20c/pytest-filedata/master/landscape.svg?style=flat)](https://landscape.io/github/20c/pytest-filedata/master)
+[![Codecov](https://img.shields.io/codecov/c/github/20c/pytest-filedata/master.svg?maxAge=3600)](https://codecov.io/github/20c/pytest-filedata)
+[![Requires.io](https://img.shields.io/requires/github/20c/pytest-filedata.svg?maxAge=3600)](https://requires.io/github/20c/pytest-filedata/requirements)
+
+easily load test data from files
+
+### Introduction
+
+This was created to save the tediousness of working with sets of data for
+testing inside the test files.
+
+### Installing
+
+```sh
+pip install pytest-filedata
+```
+
+### Using
+
+Add this to your `conftest.py` file:
+
+```python
+import pytest_filedata
+
+pytest_filedata.setup(os.path.dirname(__file__))
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith('data_'):
+            data = pytest_filedata.get_data(fixture)
+            metafunc.parametrize(fixture, list(data.values()), ids=list(data.keys()))
+```
+
+
+### License
+
+Copyright 2016 20C, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this softare except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ packages/pytest-filedata/pytest_filedata-0.4.0.tar.gz/pytest_filedata-0.4.0/pytest_filedata/__init__.py
@@ -0,0 +1,171 @@
+
+from future import standard_library
+standard_library.install_aliases()
+from builtins import map
+from builtins import object
+import collections
+from datetime import datetime
+import json
+import os
+import urllib.parse
+
+import requests_mock
+import decorator
+
+
+test_dir = None
+
+
+def setup(base_dir, fixture_prefixes=None):
+    """
+    setup filedata with base dir for data
+    """
+    global test_dir
+    test_dir = base_dir
+
+
+def json_hook(data):
+    date_keys = (
+        'last_change',
+        'last_reboot',
+        'last_reconfiguration',
+        )
+    for key in date_keys:
+        if key in data:
+            data[key] = datetime.strptime(data[key], "%Y-%m-%dT%H:%M:%S")
+    return data
+
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
+
+
+def dumps(data):
+    return json.dumps(data, cls=JSONEncoder)
+
+
+def load(fobj):
+    return json.load(fobj, object_hook=json_hook)
+
+
+def loads(data):
+    return json.loads(data, object_hook=json_hook)
+
+
+class FileTestData(object):
+    """ object to hold file test data """
+    def __init__(self, inp=None, exp=None, name=None, path=None):
+        self.input = inp
+        self.expected = exp
+        self.name = name
+        self.path = path
+
+    def dumps(self, data):
+        """ dump data in configured output method """
+        return dumps(data)
+
+    def loads(self, data):
+        """ load data in configured output method """
+        return loads(data)
+
+
+def get_test_files(dirname):
+    """
+    Gets a list of files in directory specified by name.
+    """
+    if not os.path.isdir(dirname):
+        return []
+    path = dirname + "/{}"
+    return list(map(path.format, sorted(os.listdir(dirname))))
+
+
+def get_data(name):
+    """
+    Gets data from fixture name.
+    """
+    data = collections.OrderedDict()
+    dirname = os.path.join(test_dir, *name.split('_'))
+
+    if not os.path.isdir(dirname):
+        raise ValueError("data directory '{}' does not exist".format(dirname))
+
+    for each in get_test_files(dirname):
+        if os.path.isdir(each):
+            continue
+
+        fname = os.path.basename(each)
+        if fname.startswith('.'):
+            continue
+
+        test_name, ext = os.path.splitext(fname)
+        data.setdefault(test_name, FileTestData(name=test_name, path=dirname))
+
+        # could setattr
+        attr = ext[1:]
+        if ext == '.expected':
+            with open(each) as fobj:
+                data[test_name].expected = json.load(fobj, object_hook=json_hook)
+        else:
+            with open(each) as fobj:
+                setattr(data[test_name], attr, fobj.read())
+
+    return data
+
+
+class RequestsData(object):
+    """
+    class to use test data from requests
+    """
+    def __init__(self, prefix, real_http=False):
+        self.prefix = prefix
+        adapter = requests_mock.Adapter()
+        adapter.register_uri(requests_mock.ANY, requests_mock.ANY, text=self.callback)
+        self.mocker = requests_mock.Mocker(adapter=adapter, real_http=real_http)
+
+    def callback(self, request, context):
+        path = urllib.parse.urlparse(request.url).path
+        path = os.path.join(test_dir, 'data', self.prefix, path.lstrip('/'))
+
+        files = get_test_files(path)
+
+        if len(files) > 1:
+            raise NotImplementedError("Currently there must be only one response file")
+
+        if len(files) == 0:
+            # dir not found, check for file.input
+            fname = path + '.input'
+            if not os.path.exists(fname):
+                raise ValueError("failed to find data for {}".format(path))
+
+        else:
+            fname = files[0]
+
+        try:
+            # file extension is status code
+            context.status_code = int(os.path.splitext(fname)[1].lstrip('.'))
+
+        except ValueError:
+            context.status_code = 200
+
+        with open(fname) as fobj:
+            return fobj.read()
+
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            with self.mocker:
+                # FIXME -- why is the test func arg 0?
+                func(*args[1:], **kwargs)
+
+        # use decorator because functools.wrap changes the function sig
+        return decorator.decorator(wrapper, func)
+
+    def __enter__(self):
+        self.mocker.start()
+
+    def __exit__(self, typ, value, traceback):
+        self.mocker.stop()
