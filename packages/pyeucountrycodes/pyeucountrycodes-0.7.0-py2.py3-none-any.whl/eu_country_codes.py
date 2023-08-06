158575ef5545f1b316c3b18d6e818181912761fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:17 +0000

    Add package pyeucountrycodes/0.7.0/pyeucountrycodes-0.7.0-py2.py3-none-any.whl

---
+++ packages/pyeucountrycodes/pyeucountrycodes-0.7.0-py2.py3-none-any.whl/eu_country_codes.py
@@ -0,0 +1,34 @@
+"""All ISO 3166 country codes for EU countries."""
+
+__version__ = "0.7.0"
+
+COUNTRY_CODES = [
+    "AT",
+    "BE",
+    "BG",
+    "CY",
+    "CZ",
+    "DE",
+    "DK",
+    "EE",
+    "ES",
+    "FI",
+    "FR",
+    "GB",
+    "GR",
+    "HR",
+    "HU",
+    "IE",
+    "IT",
+    "LT",
+    "LU",
+    "LV",
+    "MT",
+    "NL",
+    "PL",
+    "PT",
+    "RO",
+    "SE",
+    "SI",
+    "SK",
+]
