2c4e6f2777a98058c51c4756f9ccdd340d4361a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:03 +0000

    Add package swissbib-kafka-event-hub/1.0.0/swissbib_kafka_event_hub-1.0.0-py3-none-any.whl

---
+++ packages/swissbib-kafka-event-hub/swissbib_kafka_event_hub-1.0.0-py3-none-any.whl/kafka_event_hub/consumers/base_consumer.py
@@ -0,0 +1,36 @@
+from kafka_event_hub.config import BaseConfig
+
+from kafka import KafkaConsumer, TopicPartition
+
+from typing import List
+import logging
+
+
+class AbstractBaseConsumer(object):
+
+    def __init__(self, config: str, config_class: type(BaseConfig), logger=logging.getLogger(__name__)):
+        """
+
+        Consumer configs:
+        
+        """
+        self._configuration = config_class(config)
+        self._consumer = KafkaConsumer(**self.configuration['Consumer'])
+        self.subscribe(self.configuration['Topics'])
+        self._logger = logger
+
+    @property
+    def configuration(self):
+        return self._configuration
+
+    def assign(self, topic: TopicPartition):
+        self._consumer.assign([topic])
+
+    def close(self):
+        self._consumer.close()
+
+    def subscribe(self, topics: List[str]):
+        self._consumer.subscribe(topics)
+
+    def unsubscribe(self):
+        self._consumer.unsubscribe()
