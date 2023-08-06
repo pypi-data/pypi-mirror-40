c5deb4b879571846bf14f88d768d7c0ecc30ab6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:05 +0000

    Add package predicts/0.1/predicts-0.1.tar.gz

---
+++ packages/predicts/predicts-0.1.tar.gz/predicts-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='predicts',
+      version='0.1',
+      description='Predicting Titanic Survivor',
+      url='https://github.com/abbiyanaila/predicts',
+      author='Desi Ratna Ningsih',
+      author_email='abbiyanaila@gmail.com',
+      license='MIT',
+      packages=['predicts'],
+      zip_safe=False)
+
+
