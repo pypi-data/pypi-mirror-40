e3e95f13f0dc4de555140a23d37673c1fe309faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:51 +0000

    Add package aptly-api-client/0.2.0/aptly_api_client-0.2.0-py3-none-any.whl

---
+++ packages/aptly-api-client/aptly_api_client-0.2.0-py3-none-any.whl/aptly_api/__init__.py
@@ -0,0 +1,19 @@
+# -* encoding: utf-8 *-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from aptly_api.client import Client
+from aptly_api.base import AptlyAPIException
+from aptly_api.parts.packages import Package
+from aptly_api.parts.publish import PublishEndpoint
+from aptly_api.parts.repos import Repo, FileReport
+from aptly_api.parts.snapshots import Snapshot
+
+version = "0.2.0"
+
+
+__all__ = ['Client', 'AptlyAPIException', 'version', 'Package', 'PublishEndpoint', 'Repo', 'FileReport',
+           'Snapshot']
