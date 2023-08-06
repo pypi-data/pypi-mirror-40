7d97137cd9a8ca0c3ed9dc667856ee4eb9558a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:52 +0000

    Add package ouroboros-cli/0.4.2/ouroboros-cli-0.4.2.tar.gz

---
+++ packages/ouroboros-cli/ouroboros-cli-0.4.2.tar.gz/ouroboros-cli-0.4.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+requirements = ['docker',
+                'schedule',
+                'prometheus_client',
+                'requests']
+
+requirements_dev = ['docker',
+                    'schedule',
+                    'prometheus_client',
+                    'requests',
+                    'pytest >= 3.6',
+                    'pytest-cov',
+                    'pytest-mock',
+                    'codecov',
+                    'flake8']
+
+
+def readme():
+    with open('./README.md') as f:
+        return f.read()
+
+
+setup(
+    name='ouroboros-cli',
+    version='0.4.2',
+    description='Automatically update running docker containers',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/circa10a/ouroboros',
+    license='MIT',
+    classifiers=['Programming Language :: Python'],
+    packages=find_packages(exclude=['doc', 'tests']),
+    scripts=['ouroboros/ouroboros'],
+    install_requires=requirements,
+    tests_require=requirements_dev,
+    python_requires='>=3.0'
+)
