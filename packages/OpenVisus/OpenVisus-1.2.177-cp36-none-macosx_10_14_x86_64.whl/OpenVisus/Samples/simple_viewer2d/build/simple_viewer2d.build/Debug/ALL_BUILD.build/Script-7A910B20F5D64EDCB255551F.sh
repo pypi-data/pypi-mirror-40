aaa169d2e6acbba5b4f35066e947b5a2979ff938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:45 +0000

    Add package OpenVisus/1.2.177/OpenVisus-1.2.177-cp36-none-macosx_10_14_x86_64.whl

---
+++ packages/OpenVisus/OpenVisus-1.2.177-cp36-none-macosx_10_14_x86_64.whl/OpenVisus/Samples/simple_viewer2d/build/simple_viewer2d.build/Debug/ALL_BUILD.build/Script-7A910B20F5D64EDCB255551F.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/scrgiorgio/Desktop/OpenVisus/Samples/simple_viewer2d/build -f /Users/scrgiorgio/Desktop/OpenVisus/Samples/simple_viewer2d/build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
