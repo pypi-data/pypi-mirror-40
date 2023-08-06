bfe860984f8318ae1193fb748ee2f2627aabc5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:46 +0000

    Add package lair/0.5/lair-0.5-py3-none-any.whl

---
+++ packages/lair/lair-0.5-py3-none-any.whl/lair/run.py
@@ -0,0 +1,34 @@
+import sys
+import argparse
+from lair.project_generator import ProjectGenerator
+
+
+def main(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description='Service Example')
+    parser.add_argument('--project-name', help='Name of the project',
+                        type=str, default=None, required=True)
+    parser.add_argument('--with-db',
+                        help='With database', nargs='?',
+                        const=True, default=False, required=False)
+    args = parser.parse_args(args=args)
+
+    dependencies = []
+
+    if args.with_db:
+        dependencies.extend(['flask_sqlalchemy', 'sqlalchemy'])
+
+    # Make sure flask is below flask_sqlalchemy in the list
+    # otherwise it fails to install because 'flask is missing'
+    dependencies.extend(['flask', 'konfig'])
+
+    app = ProjectGenerator()
+    app.set('project-name', args.project_name)
+    app.set('with-db', args.with_db)
+    app.set('dependencies', dependencies)
+
+    try:
+        app.initialize()
+    except FileExistsError as e:
+        print("!!!", e)
+        sys.exit(1)
+    app.create()
