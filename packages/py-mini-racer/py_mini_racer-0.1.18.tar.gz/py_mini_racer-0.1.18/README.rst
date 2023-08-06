10f8d71d58fcebdec79864ba566066f331d78fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:27 +0000

    Add package py-mini-racer/0.1.18/py_mini_racer-0.1.18.tar.gz

---
+++ packages/py-mini-racer/py_mini_racer-0.1.18.tar.gz/py_mini_racer-0.1.18/README.rst
@@ -0,0 +1,216 @@
+===============================
+Python Mini Racer
+===============================
+
+.. image:: https://img.shields.io/pypi/v/py_mini_racer.svg
+        :target: https://pypi.python.org/pypi/py_mini_racer
+
+Minimal, modern embedded V8 for Python.
+
+* Free software: ISC license
+
+.. image:: data/py_mini_racer.png
+
+Features
+--------
+
+* Unicode support
+* Thread safe
+* Re-usable contexts
+* Binary object is Python agnostic
+
+MiniRacer can be easily used by Django or Flask projects to minify assets, run
+babel or compile CoffeeScript.
+
+Examples
+--------
+
+py_mini_racer is straightforward to use:
+
+.. code-block:: python
+
+    >>> from py_mini_racer import py_mini_racer
+    >>> ctx = py_mini_racer.MiniRacer()
+    >>> ctx.eval('1+1')
+    2
+    >>> ctx.eval("var x = {company: 'Sqreen'}; x.company")
+    u'Sqreen'
+    >>> print ctx.eval(u"'\N{HEAVY BLACK HEART}'")
+    ❤
+    >>> ctx.eval("var fun = () => ({ foo: 1 });")
+    >>> ctx.call("fun")
+    {u'foo': 1}
+
+Variables are kept inside of a context:
+
+.. code-block:: python
+
+    >>> ctx.eval("x.company")
+    u'Sqreen'
+
+
+You can give directly a custom JSON encoder when sending non-JSON encodable
+parameters:
+
+.. code-block:: python
+
+    import json
+
+    from datetime import datetime
+
+    class CustomEncoder(json.JSONEncoder):
+
+            def default(self, obj):
+                if isinstance(obj, datetime):
+                    return obj.isoformat()
+
+                return json.JSONEncoder.default(self, obj)
+
+
+.. code-block:: python
+
+    >>> ctx.eval("var f = function(args) { return args; }")
+    >>> ctx.call("f", datetime.now(), encoder=CustomEncoder)
+    u'2017-03-31T16:51:02.474118'
+
+
+PyMiniRacer is ES6 capable:
+
+.. code-block:: python
+
+    >>> ctx.eval("[1,2,3].includes(5)")
+    False
+
+V8 heap information can be retrieved:
+
+.. code-block:: python
+
+    >>> ctx.heap_stats()
+    {u'total_physical_size': 1613896,
+     u'used_heap_size': 1512520,
+     u'total_heap_size': 3997696,
+     u'total_heap_size_executable': 3145728,
+     u'heap_size_limit': 1501560832}
+
+
+Compatibility
+-------------
+
+PyMiniRacer is compatible with Python 2 and Python 3. Wheels are generated for python 2.7 and python 3.4 to python 3.6.
+
+Binary builds availability
+--------------------------
+
+The PyMiniRacer binary builds have been tested on x86_64 with:
+
+* OSX 10.10
+* Ubuntu >= 12.04
+* Debian >= 7
+* CentOS >= 6
+
+You need pip >= 8.1 to install the wheels - you can check and upgrade yours in
+this way:
+
+.. code-block:: bash
+
+    $ pip --version
+    $ pip install --upgrade pip
+
+It should work on any Linux with a libc >= 2.12 and a wheel compatible pip (>=
+8.1).
+
+If you have a up-to-date pip and it doesn't use a wheel, you might have an environment for which no wheel is built. Please open an issue.
+
+Installation
+------------
+
+We built Python wheels (prebuilt binaries) for OSX 64 bits and Linux 64 bits -
+most recent distributions. You need pip >= 1.4 and setuptools >= 0.8.
+
+.. code:: bash
+
+    $ pip install py-mini-racer
+
+Build
+-----
+
+You can build v8 with the command:
+
+.. code:: bash
+
+    $ python setup.py build_v8
+
+You can also build the ctype extension:
+
+.. code:: bash
+
+    $ python setup.py build_ext
+
+Which automatically builds v8.
+
+You can generate a wheel with the command:
+
+.. code:: bash
+
+    $ python setup.py bdist_wheel
+
+which builds v8, the extension, and generates a wheel.
+
+Notes for building on macOS
+'''''''''''''''''''''''''
+
+The legacy Python binary builds (OSX 10.6) need to be downloaded from:
+    https://www.python.org/downloads/
+
+They will allow to build a wheel compatible with former OSX versions.
+
+If you're having build issues, you may either have to run the build a second time (which will be much faster than the first run) or run the following command before running the first build: 
+
+``export LDSHARED="clang++ -bundle -undefined dynamic_lookup -arch i386 -arch x86_64"``
+
+Notes for building on Travis
+'''''''''''''''''''''''''
+
+The V8 build is cached in order to make the Travis builds faster.
+
+Whenever V8 is updated, the caches need to be flushed `on Travis here`_.
+
+.. _`on Travis here`: https://travis-ci.org/sqreen/PyMiniRacer/caches
+
+Tests
+-----
+
+If you want to run the tests, you need to build V8 first, then launch:
+
+.. code:: bash
+
+    $ python setup.py test --addopts tests
+
+Credits
+-------
+
+Built with love by Sqreen_.
+
+.. _Sqreen: https://www.sqreen.io
+
+PyMiniRacer launch was described in `this blog post`_.
+
+.. _`this blog post`: https://blog.sqreen.io/embedding-javascript-into-python/
+
+PyMiniRacer is inspired by mini_racer_, built for the Ruby world by Sam Saffron.
+
+.. _`mini_racer`: https://github.com/SamSaffron/mini_racer
+
+Tools used in rendering this package:
+
+*  Cookiecutter_
+*  `cookiecutter-pypackage`_
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
+Todo
+----
+
+Export V8 version.
+Fix circular structures export.
---
+++ packages/py-mini-racer/py_mini_racer-0.1.18.tar.gz/py_mini_racer-0.1.18/setup.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import pip
+import sys
+import codecs
+import pkg_resources
+import traceback
+
+from subprocess import check_call
+from os.path import dirname, abspath, join, isfile, isdir, basename
+
+from distutils.file_util import copy_file
+
+try:
+    from setuptools import setup, Extension, Command
+    from setuptools.command.build_ext import build_ext
+    from setuptools.command.install import install
+except ImportError:
+    from distutils.core import setup, Extension, Command
+    from distutils.command.build_ext import build_ext
+    from distutils.command.install import install
+
+import py_mini_racer
+from py_mini_racer.extension.v8_build import build_v8
+
+V8_PATH = os.environ.get("PY_MINI_RACER_V8_PATH")
+
+with codecs.open('README.rst', 'r', encoding='utf8') as readme_file:
+    readme = readme_file.read()
+
+    # Convert local image links by their github equivalent
+    readme = readme.replace(".. image:: data/",
+                            ".. image:: https://github.com/sqreen/PyMiniRacer/raw/master/data/")
+
+with codecs.open('HISTORY.rst', 'r', encoding='utf8') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+
+def _parse_requirements(filepath):
+    pip_version = list(map(int, pkg_resources.get_distribution('pip').version.split('.')[:2]))
+    if pip_version >= [10, 0]:
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+        raw = parse_requirements(filepath, session=PipSession())
+    elif pip_version >= [6, 0]:
+        from pip.download import PipSession
+        from pip.req import parse_requirements
+        raw = parse_requirements(filepath, session=PipSession())
+    else:
+        from pip.req import parse_requirements
+        raw = parse_requirements(filepath)
+
+    return [str(i.req) for i in raw]
+
+
+requirements = _parse_requirements('requirements/prod.txt')
+setup_requires = _parse_requirements('requirements/setup.txt')
+test_requirements = _parse_requirements('requirements/test.txt')
+
+
+def local_path(path):
+    """ Return path relative to this file
+    """
+    current_path = dirname(__file__)
+    return abspath(join(current_path, path))
+
+
+V8_LIB_DIRECTORY = local_path('py_mini_racer/extension/v8/v8')
+V8_STATIC_LIBRARIES = ['libv8_monolith.a']
+
+
+def is_v8_built():
+    """ Check if v8 has been built
+    """
+    if V8_PATH:
+        return True
+    return all(isfile(static_filepath) for static_filepath in get_raw_static_lib_path())
+
+
+def is_depot_tools_checkout():
+    """ Check if the depot tools submodule has been checkouted
+    """
+    return isdir(local_path('vendor/depot_tools'))
+
+
+def libv8_object(object_name):
+    """ Return a path for object_name which is OS independent
+    """
+
+    filename = join(V8_LIB_DIRECTORY, 'out.gn/x64.release/obj/{}'.format(object_name))
+
+    if not isfile(filename):
+        filename = join(local_path('vendor/v8/out.gn/libv8/obj/{}'.format(object_name)))
+
+    if not isfile(filename):
+        filename = join(V8_LIB_DIRECTORY, 'out.gn/x64.release/obj/{}'.format(object_name))
+
+    return filename
+
+
+def get_raw_static_lib_path():
+    """ Return the list of the static libraries files ONLY, use
+    get_static_lib_paths to get the right compilation flags
+    """
+    return [libv8_object(static_file) for static_file in V8_STATIC_LIBRARIES]
+
+
+def get_static_lib_paths():
+    """ Return the required static libraries path
+    """
+    libs = []
+    is_linux = sys.platform.startswith('linux')
+    if is_linux:
+        libs += ['-Wl,--start-group']
+    libs += get_raw_static_lib_path()
+    if is_linux:
+        libs += ['-Wl,--end-group']
+    return libs
+
+EXTRA_LINK_ARGS = [
+    '-ldl',
+    '-fstack-protector',
+]
+EXTRA_COMPILE_ARGS = [
+    '-std=c++11',
+    '-rdynamic',
+    '-fpermissive',
+    '-fno-common'
+]
+
+# Per platform customizations
+if sys.platform[:6] == "darwin":
+    # XXX: do we support older verions? If so, we may need to compile libv8
+    # against stdlibc++ and change the flags here as well
+    EXTRA_COMPILE_ARGS += ['-mmacosx-version-min=10.9', '-stdlib=libc++']
+    EXTRA_LINK_ARGS    += ['-lpthread', '-mmacosx-version-min=10.9', '-stdlib=libc++']
+elif sys.platform.startswith('linux'):
+    EXTRA_LINK_ARGS    += ['-lrt']
+
+
+PY_MINI_RACER_EXTENSION = Extension(
+    name="py_mini_racer._v8",
+    language='c++',
+    sources=['py_mini_racer/extension/mini_racer_extension.cc'],
+    include_dirs=[V8_LIB_DIRECTORY, join(V8_LIB_DIRECTORY, 'include')],
+    extra_objects=get_static_lib_paths(),
+    extra_compile_args=EXTRA_COMPILE_ARGS,
+    extra_link_args=EXTRA_LINK_ARGS
+)
+
+
+class MiniRacerBuildExt(build_ext):
+
+    def build_extension(self, ext):
+        """ Compile manually the py_mini_racer extension, bypass setuptools
+        """
+        try:
+            if not is_v8_built():
+                self.run_command('build_v8')
+
+            self.debug = True
+            if V8_PATH:
+                dest_filename = join(self.build_lib, "py_mini_racer")
+                copy_file(V8_PATH, dest_filename, verbose=self.verbose, dry_run=self.dry_run)
+            else:
+                build_ext.build_extension(self, ext)
+
+        except Exception as e:
+            traceback.print_exc()
+
+            # Alter message
+            err_msg = """py_mini_racer failed to build, ensure you have an up-to-date pip (>= 8.1) to use the wheel instead
+            To update pip: 'pip install -U pip'
+            See also: https://github.com/sqreen/PyMiniRacer#binary-builds-availability
+
+            Original error: %s"""
+
+            raise Exception(err_msg % repr(e))
+
+
+class MiniRacerBuildV8(Command):
+
+    description = 'Compile vendored v8'
+    user_options = [
+    ]
+
+    def initialize_options(self):
+        """Set default values for options."""
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def run(self):
+        if V8_PATH:
+            return
+
+        if not is_depot_tools_checkout():
+            print("cloning depot tools submodule")
+            # Clone the depot_tools repository, easier than using submodules
+            check_call(['git', 'init'])
+            check_call(['git', 'clone', 'https://chromium.googlesource.com/chromium/tools/depot_tools.git', 'vendor/depot_tools'])
+
+        if not is_v8_built():
+            print("building v8")
+            build_v8()
+        else:
+            print("v8 is already built")
+
+setup(
+    name='py_mini_racer',
+    version=py_mini_racer.__version__,
+    description="Minimal, modern embedded V8 for Python.",
+    long_description=readme + '\n\n' + history,
+    author='Sqreen',
+    author_email='hey@sqreen.io',
+    url='https://github.com/sqreen/PyMiniRacer',
+    packages=[
+        'py_mini_racer',
+        'py_mini_racer.extension'
+    ],
+    ext_modules=[PY_MINI_RACER_EXTENSION],
+    package_dir={'py_mini_racer':
+                 'py_mini_racer'},
+    include_package_data=True,
+    setup_requires=setup_requires,
+    install_requires=requirements,
+    license="ISCL",
+    zip_safe=False,
+    keywords='py_mini_racer',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    cmdclass={
+        'build_ext': MiniRacerBuildExt,
+        'build_v8': MiniRacerBuildV8,
+    }
+)
