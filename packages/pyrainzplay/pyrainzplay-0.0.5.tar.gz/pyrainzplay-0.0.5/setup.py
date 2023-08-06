83352712d56e5fdfad6c0a46cb80feb4423a453f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:33 +0000

    Add package pyrainzplay/0.0.5/pyrainzplay-0.0.5.tar.gz

---
+++ packages/pyrainzplay/pyrainzplay-0.0.5.tar.gz/pyrainzplay-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name = 'pyrainzplay',
+    packages = find_packages(),
+    version = '0.0.5',
+    license='MIT',
+    description = 'Playground app for py first timers!',
+    author = 'Rainz',
+    author_email = 'cbludev@gmail.com',
+    url = 'https://github.com/cbludev/pyrainzplay',
+    download_url = 'https://github.com/cbludev/pyrainzplay/archive/0.0.5.zip',
+    keywords = ['numpy'],
+    install_requires=[
+        'numpy',
+    ],
+)
