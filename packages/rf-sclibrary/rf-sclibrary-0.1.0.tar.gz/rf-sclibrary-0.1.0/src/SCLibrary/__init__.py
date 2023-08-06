9fe161bb9fb1470e592cd963c35689f1a62f4120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:09 +0000

    Add package rf-sclibrary/0.1.0/rf-sclibrary-0.1.0.tar.gz

---
+++ packages/rf-sclibrary/rf-sclibrary-0.1.0.tar.gz/rf-sclibrary-0.1.0/src/SCLibrary/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+import os
+from base import DynamicCore
+from validator import ValidatorKeywords
+from requester import RequesterKeywords
+from util import UtilKeywords
+from db import DBKeywords
+from loglistener import LogListener
+from robot.libraries.BuiltIn import BuiltIn
+from base import hook_zh
+
+__version__ = '0.1.0'
+
+
+class SCLibrary(DynamicCore):
+
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+    ROBOT_LIBRARY_VERSION = __version__
+ 
+    def __init__(self):
+        libraries = [ValidatorKeywords(), RequesterKeywords(), DBKeywords(), UtilKeywords()]
+        DynamicCore.__init__(self, libraries)
+        built_in = BuiltIn()
+        exec_dir = built_in.get_variable_value("${EXECDIR}")
+        env = built_in.get_variable_value("${RF_ENV}")
+        built_in.set_global_variable("${ENVDIR}", '%s/config/%s.env.robot' % (exec_dir, env))
+        if built_in.get_variable_value("${RF_DEBUG}") == True:
+            self.ROBOT_LIBRARY_LISTENER = LogListener()
+        # 复写robot的unic.py，支持Log打印中文
+        hook_zh()
