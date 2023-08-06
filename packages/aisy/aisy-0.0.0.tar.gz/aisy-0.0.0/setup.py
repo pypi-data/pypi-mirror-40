65bf82af3bb2587f8c2fc03b5b3b4176c4a70e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:49 +0000

    Add package aisy/0.0.0/aisy-0.0.0.tar.gz

---
+++ packages/aisy/aisy-0.0.0.tar.gz/aisy-0.0.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    author="Precognit",
+    author_email='info@precognit.io',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    license="MIT license",
+    include_package_data=True,
+    name='aisy',
+    version='0.0.0',
+    zip_safe=False,
+)
