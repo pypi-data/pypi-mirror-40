60a4a31ac6ad1e844bc4d696f4a557ec6e244bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:38 +0000

    Add package sovereign/0.1.9/sovereign-0.1.9.tar.gz

---
+++ packages/sovereign/sovereign-0.1.9.tar.gz/sovereign-0.1.9/src/sovereign/logs.py
@@ -0,0 +1,30 @@
+import re
+import structlog
+from structlog import DropEvent
+
+ignored_messages = '|'.join((
+    'ELB-HealthChecker',
+    'Pollinator',
+    '/healthcheck',
+    '/deepcheck',
+))
+
+
+# pylint: disable=too-few-public-methods
+
+class IgnoreRecordsStructlog:
+    def __call__(self, logger, method_name, event_dict):
+        logline = str(event_dict)
+        if re.search(ignored_messages, logline):
+            raise DropEvent
+        return event_dict
+
+
+structlog.configure(
+    processors=[
+        # IgnoreRecordsStructlog(),
+        structlog.processors.JSONRenderer()
+    ]
+)
+
+LOG = structlog.getLogger()
