2866685ef2efd3d9b1f15247d49e06850c210659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:49 +0000

    Add package print_sappai/1.1.0/print_sappai-1.1.0.tar.gz

---
+++ packages/print_sappai/print_sappai-1.1.0.tar.gz/print_sappai-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(        
+        name = 'print_sappai',
+        version = '1.1.0',
+        py_modules = [ 'print_sappai'],
+        author = 'subbaMan27',
+        author_email = 'anupam.subba27@gmail.com',
+        description = 'A simple printer of nested lists'
+      
+      )
