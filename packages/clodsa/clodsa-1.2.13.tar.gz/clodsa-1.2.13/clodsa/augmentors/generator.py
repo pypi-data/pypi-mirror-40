e7ce2d73f4b496dadef55757acd9e5b1cc4a4f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:08 +0000

    Add package clodsa/1.2.13/clodsa-1.2.13.tar.gz

---
+++ packages/clodsa/clodsa-1.2.13.tar.gz/clodsa-1.2.13/clodsa/augmentors/generator.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import
+from builtins import object
+from .detection import detectBox,detectBoxes
+from ..techniques.technique import PositionVariantTechnique,PositionInvariantTechnique,DecoratorTechnique
+
+
+class Generator(object):
+
+    def __init__(self,technique):
+        if (isinstance(technique, DecoratorTechnique)):
+            self.technique = technique
+        else:
+            raise ValueError("The technique should be an object of the class Technique")
+
+    def applyForClassification(self, image,label):
+        return (self.technique.apply(image),self.technique.transform_label(label))
+
+
+    def applyForLocalization(self,image,box):
+        if(isinstance(self.technique,PositionVariantTechnique)):
+            return [self.technique.apply(image),
+                    detectBox(image,box,self.technique)]
+        else:
+            return [self.technique.apply(image),(self.technique.transform_label(box[0]),box[1])]
+
+    def applyForDetection(self, image, boxes):
+        if (isinstance(self.technique, PositionVariantTechnique)):
+            return [self.technique.apply(image),
+                    detectBoxes(image, boxes, self.technique)]
+        else:
+            return [self.technique.apply(image),
+                    [(self.technique.transform_label(box[0]),box[1]) for box in boxes]]
+
+    def applyForSegmentation(self, image, imageLabel):
+        if (isinstance(self.technique, PositionVariantTechnique)):
+            return [self.technique.apply(image),
+                    self.technique.apply(imageLabel)]
+        else:
+            return [self.technique.apply(image), imageLabel]
+
+
