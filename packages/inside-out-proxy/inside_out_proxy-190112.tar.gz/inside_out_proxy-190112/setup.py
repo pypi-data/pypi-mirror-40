f6fa67b3b36f0d2a15f82f343a2be4d7824c6f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:20 +0000

    Add package inside-out-proxy/190112/inside_out_proxy-190112.tar.gz

---
+++ packages/inside-out-proxy/inside_out_proxy-190112.tar.gz/inside_out_proxy-190112/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os, sys
+from setuptools import setup, find_packages
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, 'README.md')) as fd:
+    md = fd.read()
+
+with open(os.path.join(HERE, 'src/inside_out_proxy/version.py')) as fd:
+    version = fd.read().split('=', 1)[1].split('\n', 1)[0].strip()
+
+# images hack for pypi - uncomment, replace:
+# gh = 'https://raw.githubusercontent.com/axiros/terminal_markdown_viewer/master'
+# md = md.replace('src="./', 'src="%s/' % gh)
+
+PY2 = sys.version_info[0] < 3
+INST_REQ = [
+    'attrs',
+    'structlog',
+    'attrs',
+    'colorama',
+    'absl-py',
+    'gevent',
+    'rx',
+]
+
+EXTRA_REQ = {'tests': ['coverage', 'pytest_to_md', 'pytest>=3.3.0']}
+try:
+    PACKAGES
+except NameError:
+    PACKAGES = find_packages(where='src')
+
+
+setup(
+    name='inside_out_proxy',
+    version=version,
+    packages=PACKAGES,
+    package_dir={'': 'src'},
+    author='Axiros GmbH',
+    author_email='gk@axiros.com',
+    description='HTTP Proxy',
+    install_requires=INST_REQ,
+    extras_require=EXTRA_REQ,
+    long_description=md,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    # url='http://github.com/axiros/inside_out_proxy',
+    # download_url='http://github.com/axiros/inside_out_proxy/tarball/',
+    keywords=['http', 'proxy'],
+    # test_suite='nose.collector',
+    # tests_require=['nose', 'unittest2', 'coveralls'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Pre-processors',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    entry_points={
+        'console_scripts': ['inside_out_proxy = inside_out_proxy.app:run']
+    },
+    zip_safe=False,
+)
