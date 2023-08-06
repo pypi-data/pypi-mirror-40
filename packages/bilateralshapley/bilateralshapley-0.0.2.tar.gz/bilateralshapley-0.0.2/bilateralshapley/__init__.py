b671cf33f1355ac53571f7fdddbd900a267472e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:29 +0000

    Add package bilateralshapley/0.0.2/bilateralshapley-0.0.2.tar.gz

---
+++ packages/bilateralshapley/bilateralshapley-0.0.2.tar.gz/bilateralshapley-0.0.2/bilateralshapley/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Mesa Agent-Based Modeling Framework
+
+Core Objects: Model, and Agent.
+
+"""
+import datetime
+
+from .bilateralshapley import BSV
+
+__all__ = ["BSV"]
+
+
+__title__ = 'bilateralshapley'
+__version__ = '0.0.2'
+__license__ = 'MIT'
+__copyright__ = 'Copyright %s Tom Pike' % datetime.date.today().year
