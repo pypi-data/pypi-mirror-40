b093c62ac4f41b7d11439ea74944dd04bb52b199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:30 +0000

    Add package inflame/0.2.0/inflame-0.2.0.tar.gz

---
+++ packages/inflame/inflame-0.2.0.tar.gz/inflame-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+   long_description = f.read()
+
+setup(
+   name="inflame",
+   version="0.2.0",
+   license='MIT',
+   description="Deep learning applied to text classification, in PyTorch",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author="John Jung",
+   author_email="tojohnjung@gmail.com",
+   url="https://github.com/zjohn77/inflame",
+   packages=find_packages(),
+   classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+   ],
+   keywords='deep learning'
+)
