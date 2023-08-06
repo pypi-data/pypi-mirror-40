c0f3015b28319137203842f5c59bc70f0207fa15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:38 +0000

    Add package nimpa/1.1.5/nimpa-1.1.5.tar.gz

---
+++ packages/nimpa/nimpa-1.1.5.tar.gz/nimpa-1.1.5/niftypet/nimpa/prc/spm/coreg_spm_m.m
@@ -0,0 +1,33 @@
+function out = coreg_spm_m(imref, imflo, costfun, sep, tol, fwhm, params, graphics, visual)
+
+    if visual>0
+        Fgraph = spm_figure('GetWin','Graphics');
+        Finter = spm_figure('GetWin','Interactive');
+    end
+
+    cflags.cost_fun = costfun;
+    cflags.sep = sep;
+    cflags.tol = tol;
+    cflags.fwhm = fwhm;
+    cflags.params = params;
+    cflags.graphics = graphics;
+
+    VG = strcat(imref,',1');
+    VF = strcat(imflo,',1');
+
+    disp('Matlab internal reference image:');
+    disp(imref);
+
+    disp('Matlab internal floating image:');
+    disp(imflo);
+
+    disp(cflags);
+    disp(tol);
+
+    spm_jobman('initcfg')
+
+    x = spm_coreg(VG,VF,cflags);
+    M = spm_matrix(x);
+
+    out = M;
+end
