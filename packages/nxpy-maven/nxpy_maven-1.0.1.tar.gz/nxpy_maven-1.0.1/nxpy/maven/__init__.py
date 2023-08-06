846bed08620bfbdc3f99dd18c14a26e264b3fa77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:59 +0000

    Add package nxpy-maven/1.0.1/nxpy_maven-1.0.1.tar.gz

---
+++ packages/nxpy-maven/nxpy_maven-1.0.1.tar.gz/nxpy_maven-1.0.1/nxpy/maven/__init__.py
@@ -0,0 +1,13 @@
+# nxpy_maven ------------------------------------------------------------------
+
+# Copyright Nicola Musatti 2011 - 2018
+# Use, modification, and distribution are subject to the Boost Software
+# License, Version 1.0. (See accompanying file LICENSE.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+# See https://github.com/nmusatti/nxpy/tree/master/libs/maven. ----------------
+
+r"""
+Tools to drive the Maven build tool and to manipulate its configuration files.
+
+"""
