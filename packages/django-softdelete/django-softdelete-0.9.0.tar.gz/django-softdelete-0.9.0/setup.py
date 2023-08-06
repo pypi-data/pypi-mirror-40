56d6bb74a13e6f3629f92d5719e839e8c381ddb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:30 +0000

    Add package django-softdelete/0.9.0/django-softdelete-0.9.0.tar.gz

---
+++ packages/django-softdelete/django-softdelete-0.9.0.tar.gz/django-softdelete-0.9.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='django-softdelete',
+      version='0.9.0',
+      description='Soft delete support for Django ORM, with undelete.',
+      author='Steve Coursen',
+      author_email='smcoursen@gmail.com',
+      maintainer='Steve Coursen',
+      maintainer_email='smcoursen@gmail.com',
+      license="BSD",
+      url="https://github.com/scoursen/django-softdelete",
+      packages=find_packages(),
+      install_requires=['setuptools',],
+      include_package_data=True,
+      setup_requires=['setuptools_hg',],
+      classifiers=[
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Web Environment',
+        ]
+)
