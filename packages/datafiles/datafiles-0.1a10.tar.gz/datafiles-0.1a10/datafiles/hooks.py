fa59cdc4832508035e453a5f8156b89f4459e530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:52 +0000

    Add package datafiles/0.1a10/datafiles-0.1a10.tar.gz

---
+++ packages/datafiles/datafiles-0.1a10.tar.gz/datafiles-0.1a10/datafiles/hooks.py
@@ -0,0 +1,141 @@
+import dataclasses
+from contextlib import contextmanager, suppress
+from functools import wraps
+from types import new_class
+
+import log
+
+from . import settings
+
+
+LOAD_BEFORE_METHODS = ['__getattribute__', '__getitem__', '__iter__']
+SAVE_AFTER_METHODS = [
+    '__setattr__',
+    '__setitem__',
+    '__delitem__',
+    'append',
+    'extend',
+    'insert',
+    'remove',
+    'pop',
+    'clear',
+    'sort',
+    'reverse',
+    'popitem',
+    'update',
+]
+
+
+def apply(instance, datafile, get_datafile):
+    """Path methods that get or set attributes."""
+    cls = instance.__class__
+
+    for name in LOAD_BEFORE_METHODS:
+        with suppress(AttributeError):
+            method = getattr(cls, name)
+            modified_method = load_before(cls, method, datafile)
+            setattr(cls, name, modified_method)
+
+    for name in SAVE_AFTER_METHODS:
+        with suppress(AttributeError):
+            method = getattr(cls, name)
+            modified_method = save_after(cls, method, datafile)
+            setattr(cls, name, modified_method)
+
+    if dataclasses.is_dataclass(instance):
+        for name in instance.datafile.attrs:
+            attr = getattr(instance, name)
+            if dataclasses.is_dataclass(attr):
+                attr.datafile = get_datafile(attr)
+                apply(attr, datafile, get_datafile)
+            elif type(attr) == list:  # pylint: disable=unidiomatic-typecheck
+                attr = new_class(f'list-{name}', (list,))(attr)
+                setattr(instance, name, attr)
+                apply(attr, datafile, get_datafile)
+            elif type(attr) == dict:  # pylint: disable=unidiomatic-typecheck
+                attr = new_class(f'dict-{name}', (dict,))(attr)
+                setattr(instance, name, attr)
+                apply(attr, datafile, get_datafile)
+
+
+def load_before(cls, method, datafile):
+    """Decorate methods that should load before call."""
+
+    name = method.__name__
+    log.debug(f'Patching method to load before call: {cls.__name__}.{name}')
+
+    @wraps(method)
+    def wrapped(self, *args, **kwargs):
+        __tracebackhide__ = settings.HIDE_TRACEBACK_IN_HOOKS
+
+        if enabled(datafile, args):
+            if datafile.exists and datafile.modified:
+                log.debug(f"Loading automatically before '{name}' call")
+
+                with disabled():
+                    datafile.load()
+                    datafile.modified = False
+                    # TODO: Implement this?
+                    # if mapper.auto_save_after_load:
+                    #     mapper.save()
+                    #     mapper.modified = False
+
+        return method(self, *args, **kwargs)
+
+    return wrapped
+
+
+def save_after(cls, method, datafile):
+    """Decorate methods that should save after call."""
+
+    name = method.__name__
+    log.debug(f'Patching method to save after call: {cls.__name__}.{name}')
+
+    @wraps(method)
+    def wrapped(self, *args, **kwargs):
+        __tracebackhide__ = settings.HIDE_TRACEBACK_IN_HOOKS
+
+        if enabled(datafile, args):
+            if datafile.exists and datafile.modified:
+                log.debug(f"Loading modified datafile before '{name}' call")
+                with disabled():
+                    datafile.load()
+
+        result = method(self, *args, **kwargs)
+
+        if enabled(datafile, args):
+            log.debug(f"Saving automatically after '{name}' call")
+            with disabled():
+                datafile.save()
+
+        return result
+
+    return wrapped
+
+
+def enabled(datafile, args) -> bool:
+    """Determine if hooks are enabled for the current method."""
+    if not settings.HOOKS_ENABLED:
+        return False
+
+    if datafile.manual:
+        return False
+
+    if args and isinstance(args[0], str):
+        if args[0] in {'Meta', 'datafile'}:
+            return False
+        if args[0].startswith('_'):
+            return False
+
+    return True
+
+
+@contextmanager
+def disabled():
+    """Globally disable method hooks, temporarily."""
+    if settings.HOOKS_ENABLED:
+        settings.HOOKS_ENABLED = False
+        yield
+        settings.HOOKS_ENABLED = True
+    else:
+        yield
---
+++ packages/datafiles/datafiles-0.1a10.tar.gz/datafiles-0.1a10/datafiles/models.py
@@ -0,0 +1,115 @@
+import dataclasses
+from dataclasses import dataclass
+from typing import Dict, Optional
+
+import log
+from classproperties import classproperty
+
+from . import hooks
+from .converters import Converter, map_type
+from .managers import InstanceManager, ModelManager
+
+
+@dataclass
+class ModelMeta:
+    datafile_attrs: Optional[Dict[str, Converter]] = None
+    datafile_pattern: Optional[str] = None
+
+    datafile_manual: bool = False
+    datafile_defaults: bool = False
+
+
+class Model:
+
+    Meta: ModelMeta = ModelMeta()
+
+    def __post_init__(self):
+        # pylint: disable=attribute-defined-outside-init
+        log.debug(f'Initializing {self.__class__} instance')
+
+        with hooks.disabled():
+
+            self.datafile = get_datafile(self)
+
+            path = self.datafile.path
+            exists = self.datafile.exists
+
+            if path:
+                log.debug(f'Datafile path: {path}')
+                log.debug(f'Datafile exists: {exists}')
+
+                if exists:
+                    self.datafile.load(first_load=True)
+                elif path:
+                    self.datafile.save()
+
+                hooks.apply(self, self.datafile, get_datafile)
+
+        log.debug(f'Initialized {self.__class__} instance')
+
+    @classproperty
+    def datafiles(cls) -> ModelManager:  # pylint: disable=no-self-argument
+        return ModelManager(cls)
+
+
+def get_datafile(obj) -> InstanceManager:
+    try:
+        return obj.datafile
+    except AttributeError:
+        log.debug(f"Getting 'datafile' for {obj.__class__} instance")
+
+    m = getattr(obj, 'Meta', None)
+    pattern = getattr(m, 'datafile_pattern', None)
+    attrs = getattr(m, 'datafile_attrs', None)
+    manual = getattr(m, 'datafile_manual', False)
+    defaults = getattr(m, 'datafile_defaults', False)
+
+    if attrs is None:
+        attrs = {}
+        log.debug(f'Mapping attributes for {obj.__class__} instance')
+        for field in dataclasses.fields(obj):
+            self_name = f'self.{field.name}'
+            if pattern is None or self_name not in pattern:
+                attrs[field.name] = map_type(field.type)
+
+    return InstanceManager(
+        obj, attrs=attrs, pattern=pattern, manual=manual, defaults=defaults
+    )
+
+
+def create_model(cls, *, attrs=None, pattern=None, manual=None, defaults=None):
+    """Patch datafile attributes on to an existing dataclass."""
+    log.debug(f'Converting {cls} to a datafile model')
+
+    if not dataclasses.is_dataclass(cls):
+        raise ValueError(f'{cls} must be a dataclass')
+
+    # Patch Meta
+
+    m = getattr(cls, 'Meta', ModelMeta())
+
+    if attrs is not None:
+        m.datafile_attrs = attrs
+    if pattern is not None:
+        m.datafile_pattern = pattern
+
+    if not hasattr(cls, 'Meta') and manual is not None:
+        m.datafile_manual = manual
+    if not hasattr(cls, 'Meta') and defaults is not None:
+        m.datafile_defaults = defaults
+
+    cls.Meta = m
+
+    # Patch __init__
+
+    init = cls.__init__
+
+    def modified_init(self, *args, **kwargs):
+        with hooks.disabled():
+            init(self, *args, **kwargs)
+        Model.__post_init__(self)
+
+    cls.__init__ = modified_init
+    cls.__init__.__doc__ = init.__doc__
+
+    return cls
