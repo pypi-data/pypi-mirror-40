50d8f10cfd66684bf6b31435cb1ba8b67987c5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:07 +0000

    Add package nes-py/4.0.1/nes_py-4.0.1.tar.gz

---
+++ packages/nes-py/nes_py-4.0.1.tar.gz/nes_py-4.0.1/nes_py/cpp/include/main_bus.hpp
@@ -0,0 +1,80 @@
+#ifndef MAIN_BUS_H
+#define MAIN_BUS_H
+#include <vector>
+#include <map>
+#include <functional>
+#include "cartridge.hpp"
+#include "mapper.hpp"
+
+/// The IO registers on the main bus
+enum IORegisters {
+    PPUCTRL = 0x2000,
+    PPUMASK,
+    PPUSTATUS,
+    OAMADDR,
+    OAMDATA,
+    PPUSCROL,
+    PPUADDR,
+    PPUDATA,
+    OAMDMA = 0x4014,
+    JOY1 = 0x4016,
+    JOY2 = 0x4017,
+};
+
+/// The main bus for data to travel along the NES hardware
+class MainBus {
+private:
+    /// The RAM on the main bus
+    std::vector<uint8_t> m_RAM;
+    /// The extended RAM (if the mapper has extended RAM)
+    std::vector<uint8_t> m_extRAM;
+    /// a pointer to the mapper on the cartridge
+    Mapper* m_mapper;
+    /// a map of IO registers to callback methods for writes
+    std::map<IORegisters, std::function<void(uint8_t)>> m_writeCallbacks;
+    /// a map of IO registers to callback methods for reads
+    std::map<IORegisters, std::function<uint8_t(void)>> m_readCallbacks;
+
+public:
+    /// Initialize a new main bus.
+    MainBus() : m_RAM(0x800, 0), m_mapper(nullptr) { };
+
+    /// Return a 8-bit pointer to the RAM buffer's first address.
+    ///
+    /// @return a 8-bit pointer to the RAM buffer's first address
+    ///
+    uint8_t* get_memory_buffer() { return &m_RAM.front(); };
+
+    /// Read a byte from an address on the RAM.
+    ///
+    /// @param addr the 16-bit address of the byte to read in the RAM
+    ///
+    /// @return the byte located at the given address
+    ///
+    uint8_t read(uint16_t addr);
+
+    /// Write a byte to an address in the RAM.
+    ///
+    /// @param addr the 16-bit address to write the byte to in RAM
+    /// @param value the byte to write to the given address
+    ///
+    void write(uint16_t addr, uint8_t value);
+
+    /// Set the mapper pointer to a new value.
+    ///
+    /// @param mapper the new mapper pointer for the bus to use
+    ///
+    bool set_mapper(Mapper* mapper);
+
+    /// Set a callback for when writes occur.
+    bool set_write_callback(IORegisters reg, std::function<void(uint8_t)> callback);
+
+    /// Set a callback for when reads occur.
+    bool set_read_callback(IORegisters reg, std::function<uint8_t(void)> callback);
+
+    /// Return a pointer to the page in memory.
+    const uint8_t* get_page_pointer(uint8_t page);
+
+};
+
+#endif // MAIN_BUS_H
---
+++ packages/nes-py/nes_py-4.0.1.tar.gz/nes_py-4.0.1/setup.py
@@ -0,0 +1,102 @@
+"""The setup script for installing and distributing the nes-py package."""
+import os
+from glob import glob
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
+
+
+class BuildExt(build_ext):
+    """
+    A build extension to omit the '-Wstrict-prototypes' flag.
+
+    Reference:
+        https://stackoverflow.com/questions/8106258/cc1plus-warning-command-line-option-wstrict-prototypes-is-valid-for-ada-c-o
+    """
+
+    def build_extensions(self):
+        """Build the extensions."""
+        try:
+            self.compiler.compiler_so.remove("-Wstrict-prototypes")
+        except (AttributeError, ValueError):
+            pass
+        build_ext.build_extensions(self)
+
+
+# read the contents from the README file
+with open('README.md') as README_file:
+    README = README_file.read()
+
+
+# The prefix name for the .so library to build. It will follow the format
+# lib_nes_env.*.so where the * changes depending on the build system
+LIB_NAME = 'nes_py.lib_nes_env'
+# The source files for building the extension. Globs locate all the cpp files
+# used by the LaiNES subproject. MANIFEST.in has to include the blanket
+# "cpp" directory to ensure that the .inc file gets included too
+SOURCES = glob('nes_py/cpp/*.cpp') + glob('nes_py/cpp/mappers/*.cpp')
+# The directory pointing to header files used by the LaiNES cpp files.
+# This directory has to be included using MANIFEST.in too to include the
+# headers with sdist
+INCLUDE_DIRS = ['nes_py/cpp/include']
+# Build arguments to pass to the compiler
+EXTRA_COMPILE_ARGS = ['-O2']
+# add additional arguments for Unix-based systems only
+if os.name != 'nt':
+    EXTRA_COMPILE_ARGS += [
+        '-std=c++1y',
+        '-march=native',
+        '-pipe',
+    ]
+# The official extension using the name, source, headers, and build args
+LIB_NES_ENV = Extension(LIB_NAME,
+    sources=SOURCES,
+    include_dirs=INCLUDE_DIRS,
+    extra_compile_args=EXTRA_COMPILE_ARGS,
+)
+
+
+setup(
+    name='nes_py',
+    version='4.0.1',
+    description='An NES Emulator and OpenAI Gym interface',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    keywords='NES Emulator OpenAI-Gym',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: C++',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Games/Entertainment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Emulators',
+    ],
+    url='https://github.com/Kautenja/nes-py',
+    author='Christian Kauten',
+    author_email='kautencreations@gmail.com',
+    license='Proprietary',
+    packages=find_packages(exclude=['tests', '*.tests', '*.tests.*']),
+    ext_modules=[LIB_NES_ENV],
+    zip_safe=False,
+    install_requires=[
+        'gym>=0.10.9',
+        'matplotlib>=2.0.2',
+        'numpy>=1.12.1',
+        'opencv-python>=3.4.0.12',
+        'pygame>=1.9.3',
+        'pyglet>=1.3.2',
+        'tqdm>=4.19.5',
+    ],
+    entry_points={
+        'console_scripts': [
+            'nes_py = nes_py._app.cli:main',
+        ],
+    },
+    cmdclass={'build_ext': BuildExt},
+)
