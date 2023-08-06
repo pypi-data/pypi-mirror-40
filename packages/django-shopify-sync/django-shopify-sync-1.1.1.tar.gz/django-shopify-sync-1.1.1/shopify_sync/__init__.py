2b860bff71c79524259807a91cd40b5616c8ea87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:37 +0000

    Add package django-shopify-sync/1.1.1/django-shopify-sync-1.1.1.tar.gz

---
+++ packages/django-shopify-sync/django-shopify-sync-1.1.1.tar.gz/django-shopify-sync-1.1.1/shopify_sync/__init__.py
@@ -0,0 +1,9 @@
+VERSION     = (1, 1, 1)
+__version__ = '.'.join(map(str, VERSION))
+__author__  = 'David Burke'
+
+
+SHOPIFY_API_PAGE_LIMIT = 250
+
+
+default_app_config = 'shopify_sync.apps.ShopifySyncConfig'
