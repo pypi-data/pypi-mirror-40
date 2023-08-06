655a841cb3a837578c4178ce75c65a0106ad6713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:33 +0000

    Add package tornado-directmail-aliyun/1.0.2/tornado_directmail_aliyun-1.0.2-py2-none-any.whl

---
+++ packages/tornado-directmail-aliyun/tornado_directmail_aliyun-1.0.2-py2-none-any.whl/tornado_directmail/test.py
@@ -0,0 +1,26 @@
+import tornado.ioloop
+import tornado.web
+from tornado import gen
+import tornado_directmail_aliyun
+# from tornado_directmail.client import AliMail
+
+class MainHandler(tornado.web.RequestHandler):
+    @gen.coroutine
+    def get(self):
+        access_id = 'LTAIXj3iWYkXZTKV'
+        access_secret = 'VnGuSvcm8dUdvVKsKcwzpcWacgMM3f'
+        from_address = 'system@blockvita.com'
+        from_alias = 'Test'
+        # resp = yield AliMail(access_id, access_secret, from_address, from_alias).send('haolee1990@qq.com', 'subject', 'content')
+        # print(resp)
+        self.write("Hello, world")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
