2810dd9e627cfb972ab3cf6bda025437fcd7f876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:43 +0000

    Add package SimpleVN/0.0.7/SimpleVN-0.0.7.tar.gz

---
+++ packages/SimpleVN/SimpleVN-0.0.7.tar.gz/SimpleVN-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SimpleVN",
+    version="0.0.7",
+    author="Menthol - Derek Zhang",
+    author_email="derek.zhang0210@gmail.com",
+    description="A simple module to create really quick visual novels",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/D3REKZHANG/SimpleVN",
+    packages=setuptools.find_packages(),
+    py_modules=["main"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
