1e33cb6ed4998d1439888ccfec54679470ff3f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:26 +0000

    Add package pythoncmd/0.1/pythoncmd-0.1.tar.gz

---
+++ packages/pythoncmd/pythoncmd-0.1.tar.gz/pythoncmd-0.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(
+    name='pythoncmd',
+    version='0.1',
+    description=(
+        'Python subprocess replacement'
+    ),
+    long_description='',
+    author='fjy',
+    author_email='2589660302@qq.com',
+    maintainer='fjy',
+    maintainer_email='2589660302@qq.com',
+    license='BSD License',
+    packages=['pythoncmd'],
+    platforms=["all"],
+    url='https://github.com/fjy202/pysh/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
