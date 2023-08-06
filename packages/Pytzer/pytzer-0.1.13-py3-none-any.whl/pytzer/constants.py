1f4893c5e373c370697950c8be9d9672ce330876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:25 +0000

    Add package Pytzer/0.1.13/pytzer-0.1.13-py3-none-any.whl

---
+++ packages/Pytzer/pytzer-0.1.13-py3-none-any.whl/pytzer/constants.py
@@ -0,0 +1,14 @@
+import autograd.numpy as np
+
+# Set constant values
+Mw = np.float_(0.018015)  # Molar mass of water  / kg/mol             [PubChem]
+R  = np.float_(8.3144598) # Universal gas const. / J /mol /K           [CODATA]
+F  = np.float_(96485.33289) # Faraday const.     / C /mol              [CODATA]
+b  = np.float_(1.2)       # Pitzer model coeff.  / sqrt(kg/mol)   [Pitzer 1991]
+Patm_bar = np.float_(1.01325) # Atmospheric pressure / bar
+
+# Unit conversion factors
+cal2J   = np.float_(4.184)         #  ENERGY  calorie    to Joule  / cal/J
+atm2Pa  = np.float_(101325)        # PRESSURE atmosphere to Pascal / atm/Pa
+Torr2Pa = atm2Pa / 760             # PRESSURE Torr       to Pascal / Torr/Pa
+mmHg2Pa = np.float_(133.322387415) # PRESSURE mmHg       to Pascal / mmHg/Pa
