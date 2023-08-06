683143e4896d4d15742c39bea5662397689426c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:58 +0000

    Add package iduoliao-ml/1.0.113/iduoliao_ml-1.0.113-py2-none-any.whl

---
+++ packages/iduoliao-ml/iduoliao_ml-1.0.113-py2-none-any.whl/iduoliao_ml/__init__.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import hot, es, dataDispose, itemCF, test, db
+
+def startUpdateHot(isTest):
+	es.setEnv(isTest)
+	hot.startUpdate(isTest, 'config.ini')
+
+def disposeData(isTest):
+	es.setEnv(isTest)
+	dataDispose.cycleDisposePersonas()
+
+def cfItem(isTest):
+	es.setEnv(isTest)
+	itemCF.cf()
+
+def updateHistoryActions(isTest):
+	es.setEnv(isTest)
+	dataDispose.updateHistoryActions()
+
+def testFunc():
+	es.setEnv(True)
+	print(es.searchDbConfig())
