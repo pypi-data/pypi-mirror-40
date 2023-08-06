a2186f11d06c3b4c5a1375a77830f158eaac5e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:08 +0000

    Add package iduoliao-ml/1.0.142/iduoliao_ml-1.0.142-py2-none-any.whl

---
+++ packages/iduoliao-ml/iduoliao_ml-1.0.142-py2-none-any.whl/iduoliao_ml/test/__init__.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .. import es, dataDispose
+
+def test():
+	worksList = dataDispose.getWorksList()
+	data = {}
+	for works in worksList:
+		body = {"query": {"bool": {"must": [{"term": {"vid": works.vid}}, {"terms": {"action": [3, 5]}}]}}, "size": 0, "aggs": {}}
+		body['aggs']['uid_diff'] = {"terms": {"field": "action", "size": 2}, "aggs": {"count": {"cardinality": {"field": "uid"}}}}
+		aggs = es.searchStatisticsAggs('works_actions', body)
+		
+		buckets = aggs['uid_diff']['buckets']:
+		data['vid'] = {'vid': works.vid, 'aid': works.aid, 'title': works.title, 'resShow': buckets[0]['count']['value'], 'resPlay': buckets[1]['count']['value']}
+
+	es.updateStatisticsData('works_info', data)
+
+
+	'''
+	values = []
+	for key, value in {'a': 1, 'b': 2}.items():
+		values += [key, value]
+	testArgs(**{'a': 1, 'b': 2})
+	'''
+	'''
+	es.setEnv(False)
+	hits = es.searchStatisticsDataByBody('works_actions', {"query": {"term": {"vid": "ecbf3aa1599e140896e0f6c241d93681"}}, "size": 10000})
+	
+	dataDict = {}
+	for hit in hits:
+		dataDict[hit['_id']] = hit['_source']
+
+	es.setEnv(True)
+	es.updateStatisticsData('works_actions', dataDict)
+	'''
+
+def testArgs(*args, **kwargs):
+	print(args)
+	print(kwargs)
