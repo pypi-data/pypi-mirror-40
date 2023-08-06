ee2cadecf476c193d9b8de75691051c77783b541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:06 +0000

    Add package emojislib/0.0.1/emojislib-0.0.1.tar.gz

---
+++ packages/emojislib/emojislib-0.0.1.tar.gz/emojislib-0.0.1/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+from emojislib.__version__ import version
+setuptools.setup(
+    name="emojislib",
+    version=version,
+    author="Tacey Wong",
+    author_email="xinyong.wang@qq.com",
+    description="Emojis for Python 😄 🐍 😋",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TaceyWong/emojislib",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'emojis = emojislib.__cli__:main',
+        ],
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Terminals',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
