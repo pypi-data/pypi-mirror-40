464f29eeddaa8b28cb5f330788d6bf08e943a146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:45 +0000

    Add package swissbib-kafka-event-hub/1.1.3/swissbib_kafka_event_hub-1.1.3.tar.gz

---
+++ packages/swissbib-kafka-event-hub/swissbib_kafka_event_hub-1.1.3.tar.gz/swissbib_kafka_event_hub-1.1.3/test/test_elastic_consumers.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import logging
+import pytest
+
+logging.basicConfig(filename='logs/test-elastic-consumer.log', filemode='w', level=logging.ERROR)
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from kafka_event_hub.consumers import SimpleElasticConsumer, BulkElasticConsumer
+from kafka_event_hub.producers import LineProducer
+
+from kafka import KafkaAdminClient
+
+
+class TestSimpleElasticConsumer(object):
+
+    def setup_class(self):
+        self.admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
+        self.consumer = SimpleElasticConsumer('configs/elastic/elastic-consumer-simple-test.yml')
+        self.bulk_consumer = BulkElasticConsumer('configs/elastic/elastic-consumer-bulk-test.yml')
+        self.producer = LineProducer("configs/elastic/json-lines-producer.yml")
+        self.producer.process()
+
+    def teardown_class(self):
+        self.admin.delete_topics(['json'])
+        self.consumer.close()
+
+
+    #@pytest.mark.skip()
+    def test_consume(self):
+        result = self.consumer.consume()
+        assert result
+
+    # @pytest.mark.skip()
+    def test_consume_bulk(self):
+        result = self.bulk_consumer.consume()
+        assert result
