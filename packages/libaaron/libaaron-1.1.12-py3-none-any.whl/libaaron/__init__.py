bac2dc3098fe13247ee1233949e7067c32a68143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:47 +0000

    Add package libaaron/1.1.12/libaaron-1.1.12-py3-none-any.whl

---
+++ packages/libaaron/libaaron-1.1.12-py3-none-any.whl/libaaron/__init__.py
@@ -0,0 +1,18 @@
+from .libaaron import (
+    reify,
+    cached,
+    w,
+    DotDict,
+    flatten,
+    deepupdate,
+    quietinterrupt,
+    PBytes,
+    mkdummy,
+    unpacktsv,
+    printtsv,
+)
+
+try:
+    from .libaaron import lxml_little_iter
+except ImportError:
+    pass
