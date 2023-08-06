0140188c9da1d0d670f8d541914a490c7340cd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:14 +0000

    Add package analyzdat/1.1.3/analyzdat-1.1.3.tar.gz

---
+++ packages/analyzdat/analyzdat-1.1.3.tar.gz/analyzdat-1.1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+
+setup(
+    name='analyzdat',
+    version='1.1.3',
+    description='There is "MNK", "f_MNK, "beaut_plot", "partial_derivative", "random_error"',
+    author='Your ggenchik',
+    author_email='feyke02@gmail.com',
+    py_modules=['analyzdat'],
+    )
