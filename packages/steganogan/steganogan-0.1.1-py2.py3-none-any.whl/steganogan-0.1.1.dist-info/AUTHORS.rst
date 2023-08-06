3f9e07d8420d855698a4bbfdf23a9e7ceaec2008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:34 +0000

    Add package steganogan/0.1.1/steganogan-0.1.1-py2.py3-none-any.whl

---
+++ packages/steganogan/steganogan-0.1.1-py2.py3-none-any.whl/steganogan-0.1.1.dist-info/AUTHORS.rst
@@ -0,0 +1,15 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Kalyan Veeramachaneni <kalyan@mit.edu>
+* Kevin Zhang <kevz@mit.edu>
+
+Contributors
+------------
+
+* Plamen Valentinov <plamen@pythiac.com>
+* Carles Sala <csala@csail.mit.edu>
---
+++ packages/steganogan/steganogan-0.1.1-py2.py3-none-any.whl/steganogan/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for SteganoGAN."""
+
+__author__ = """MIT Data To AI Lab"""
+__email__ = 'dailabmit@gmail.com'
+__version__ = '0.1.1'
+
+from steganogan import cli
+from steganogan.models import SteganoGAN
+
+__all__ = ('SteganoGAN', 'cli')
