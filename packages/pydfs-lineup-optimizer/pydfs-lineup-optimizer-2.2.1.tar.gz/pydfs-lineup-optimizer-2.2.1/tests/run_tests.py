d7139261d0577edc83bbee926bd53e5465c8339e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:27 +0000

    Add package pydfs-lineup-optimizer/2.2.1/pydfs-lineup-optimizer-2.2.1.tar.gz

---
+++ packages/pydfs-lineup-optimizer/pydfs-lineup-optimizer-2.2.1.tar.gz/pydfs-lineup-optimizer-2.2.1/tests/run_tests.py
@@ -0,0 +1,12 @@
+import unittest
+from test_exporter import *
+from test_optimizer import *
+from test_rules import *
+
+
+def run_tests():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    run_tests()
