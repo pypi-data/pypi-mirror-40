4a96874d9cd0548031bdd569a5fe2d353b1c013f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:59 +0000

    Add package pascal-voc-tools/0.1.3/pascal_voc_tools-0.1.3.tar.gz

---
+++ packages/pascal-voc-tools/pascal_voc_tools-0.1.3.tar.gz/pascal_voc_tools-0.1.3/pascal_voc_tools/__init__.py
@@ -0,0 +1,8 @@
+from .xmlreader import XmlReader
+from .xmlwriter import XmlWriter
+from .resize import DatasetResize
+from .datasplit import DataSplit
+
+name = 'pascal_voc_tools'
+__version__ = '0.1.2'
+
