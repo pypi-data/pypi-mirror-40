a63216dd2df4309c1b6e32a9782e39afed031409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:44 +0000

    Add package yxspkg-data-icon/1.0/yxspkg_data_icon-1.0.tar.gz

---
+++ packages/yxspkg-data-icon/yxspkg_data_icon-1.0.tar.gz/yxspkg_data_icon-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup 
+import sys
+setup(name='yxspkg_data_icon',   
+      version='1.0',    
+      description='mydata',    
+      author='blacksong',    
+      install_requires=[],
+      py_modules=['yxspkg_data_icon'],
+      platforms='any',
+      author_email='blacksong@yeah.net',       
+      url='https://github.com/blacksong',
+      classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)   
