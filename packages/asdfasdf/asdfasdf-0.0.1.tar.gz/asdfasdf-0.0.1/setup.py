04c5dc4eb0ee94a3bcc516cbdc135683c6436938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:02 +0000

    Add package asdfasdf/0.0.1/asdfasdf-0.0.1.tar.gz

---
+++ packages/asdfasdf/asdfasdf-0.0.1.tar.gz/asdfasdf-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asdfasdf",
+    packages=find_packages(),
+    version='0.0.1',
+    description="command line tool for auto tuner",
+    author="liyang",
+    author_email='1016799291@qq.com',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    url='https://github.com/ly1016799291/ceshi.git',
+    install_requires=[
+       
+    ]
+)
