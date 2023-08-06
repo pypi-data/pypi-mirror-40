fd749fc28a567d8cd371f76edb61a537ed3309ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:12 +0000

    Add package bulky/0.0.1b0/bulky-0.0.1b0.tar.gz

---
+++ packages/bulky/bulky-0.0.1b0.tar.gz/bulky-0.0.1b0/bulky/__init__.py
@@ -0,0 +1,13 @@
+__all__ = ("configure", "insert", "update")
+name = "bulky"
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from bulky.impl.insert import insert
+from bulky.impl.update import update
+
+
+def configure(base: declarative_base = None):
+    from bulky import conf
+
+    conf.Base = base
