a12aa886737b598596b1259a3d372ab34c3f9d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:08 +0000

    Add package tabledata/0.2.1/tabledata-0.2.1-py2.py3-none-any.whl

---
+++ packages/tabledata/tabledata-0.2.1-py2.py3-none-any.whl/tabledata/_logger.py
@@ -0,0 +1,54 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import dataproperty
+import logbook
+
+
+logger = logbook.Logger("tabledata")
+logger.disable()
+
+
+def set_logger(is_enable):
+    if is_enable != logger.disabled:
+        # logger setting have not changed
+        return
+
+    if is_enable:
+        logger.enable()
+    else:
+        logger.disable()
+
+    dataproperty.set_logger(is_enable)
+
+
+def set_log_level(log_level):
+    """
+    Set logging level of this module. Using
+    `logbook <https://logbook.readthedocs.io/en/stable/>`__ module for logging.
+
+    :param int log_level:
+        One of the log level of
+        `logbook <https://logbook.readthedocs.io/en/stable/api/base.html>`__.
+        Disabled logging if ``log_level`` is ``logbook.NOTSET``.
+    :raises LookupError: If ``log_level`` is an invalid value.
+    """
+
+    # validate log level
+    logbook.get_level_name(log_level)
+
+    if log_level == logger.level:
+        return
+
+    if log_level == logbook.NOTSET:
+        set_logger(is_enable=False)
+    else:
+        set_logger(is_enable=True)
+
+    logger.level = log_level
+    dataproperty.set_log_level(log_level)
