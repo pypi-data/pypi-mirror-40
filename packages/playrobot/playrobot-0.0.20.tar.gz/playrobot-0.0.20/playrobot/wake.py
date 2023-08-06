7555a11b2a4f13c34f9406dcdf61f24c0a347d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:05 +0000

    Add package playrobot/0.0.20/playrobot-0.0.20.tar.gz

---
+++ packages/playrobot/playrobot-0.0.20.tar.gz/playrobot-0.0.20/playrobot/wake.py
@@ -0,0 +1,29 @@
+import os
+from playrobot import snowboydecoder
+from platrobot import signal
+
+interrupted = False
+
+path = os.path.dirname(os.path.abspath(snowboydecoder.__file__))
+MODEL = path + '\snowboy.umdl'
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+def interrupt_callback():
+    global interrupted
+    return interrupted
+    
+def loadModel(model):
+    global MODEL
+    MODEL = model
+    
+def waitForWakeUp():
+    # capture SIGINT signal, e.g., Ctrl+C
+    signal.signal(signal.SIGINT, signal_handler)
+    detector = snowboydecoder.HotwordDetector(MODEL, sensitivity=0.5)
+    detector.start(detected_callback=detector.terminate(),
+                   interrupt_check=interrupt_callback,
+                   sleep_time=0.03)
+    return True
