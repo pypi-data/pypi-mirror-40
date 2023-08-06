98f2f416d4644f0a0f05da82f232b3a0035837ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:25 +0000

    Add package geoopt/0.0.1/geoopt-0.0.1-py3-none-any.whl

---
+++ packages/geoopt/geoopt-0.0.1-py3-none-any.whl/geoopt/__init__.py
@@ -0,0 +1,10 @@
+from . import manifolds
+from . import optim
+from . import tensor
+from . import samplers
+from . import util
+
+from .tensor import ManifoldParameter, ManifoldTensor
+from .manifolds import Stiefel, Euclidean
+
+__version__ = "0.0.1"
