35939b7fc0e39d53c9a79720951c30be6bbaa13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:55 +0000

    Add package ruia/0.1.2/ruia-0.1.2.tar.gz

---
+++ packages/ruia/ruia-0.1.2.tar.gz/ruia-0.1.2/ruia/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from .field import AttrField, BaseField, TextField
+from .item import Item
+from .middleware import Middleware
+from .request import Request
+from .spider import Spider
+
+__version__ = "0.1.2"
