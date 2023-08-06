3bb798b4c1cb58db6c95c77c1173cc400e50d580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:15 +0000

    Add package f-tile-nester/1.0.0/f-tile-nester-1.0.0.tar.gz

---
+++ packages/f-tile-nester/f-tile-nester-1.0.0.tar.gz/f-tile-nester-1.0.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'Futilechen'
+__mtime__ = '$2018.01.20'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
+
+from  distutils.core import setup
+
+
+setup(
+    name = "f-tile-nester",
+    version = '1.0.0',
+    py_modules = ['f-tile-nester'],
+    author = "f_tile",
+    author_email="futile.chen@gmail.com",
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists'
+)
