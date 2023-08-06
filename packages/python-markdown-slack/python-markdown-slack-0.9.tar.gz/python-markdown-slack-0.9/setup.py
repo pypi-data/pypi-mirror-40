5d0d06efa7705979712333dce946c5a81b7d5ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:57 +0000

    Add package python-markdown-slack/0.9/python-markdown-slack-0.9.tar.gz

---
+++ packages/python-markdown-slack/python-markdown-slack-0.9.tar.gz/python-markdown-slack-0.9/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='python-markdown-slack',
+    version='0.9',
+    py_modules=['python_markdown_slack', 'python_markdown_slack.list_handler'],
+    install_requires=['markdown>=2.5'],
+    license='MIT'
+)
