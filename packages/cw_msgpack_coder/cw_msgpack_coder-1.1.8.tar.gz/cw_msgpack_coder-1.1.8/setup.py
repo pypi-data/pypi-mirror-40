61b08b7cfa3af2edc6cb3afac1113c1c219392ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:06 +0000

    Add package cw_msgpack_coder/1.1.8/cw_msgpack_coder-1.1.8.tar.gz

---
+++ packages/cw_msgpack_coder/cw_msgpack_coder-1.1.8.tar.gz/cw_msgpack_coder-1.1.8/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+
+def long_description():
+    path = os.path.join(os.path.dirname(__file__), 'README.rst')
+    with open(path) as f:
+        return f.read()
+
+
+setup(name='cw_msgpack_coder',
+      version='1.1.8',
+      description='Simple and fast Python any object serialization with use msgpack.',
+      long_description=long_description(),
+      author='Cezary K. Wagner',
+      author_email='Cezary.Wagner@gmail.com',
+      url='https://github.com/ChameleonRed/cw_msgpack_coder',
+      license='MIT',
+      packages=['cw_msgpack_coder'],
+      install_requires='u-msgpack-python',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'],
+      keywords="msgpack serialization streaming object encoding decoding",
+      test_suite="tests.test_umsgpack_coder")
