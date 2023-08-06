2e6fec45f7b20a981a5e9244e4da11f660c100f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:06 +0000

    Add package solid-toolbox/0.0.4/solid_toolbox-0.0.4.tar.gz

---
+++ packages/solid-toolbox/solid_toolbox-0.0.4.tar.gz/solid_toolbox-0.0.4/solid_toolbox/__init__.py
@@ -0,0 +1,29 @@
+from solid_toolbox.units import *
+from solid_toolbox.transforms import *
+from solid_toolbox.poly import *
+from solid_toolbox.curves import *
+
+__all__ = [
+    # From 'units'
+    "Vec",
+    "Vec2d",
+    "Point",
+    "Point2d",
+    "mm",
+    "cm",
+    "x_axis",
+    "x_unit",
+    "y_axis",
+    "y_unit",
+    "z_axis",
+    "z_unit",
+    # From transforms
+    "noop",
+    "rotate_with_fake_origin",
+    # From poly
+    "construct_polyhedron",
+    "construct_polygon",
+    # From curves
+    "inclusive_range",
+    "bezier_curve",
+]
