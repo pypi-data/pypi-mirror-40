c815641b1221b0c7fca729e1a15598c44e6f5819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:15 +0000

    Add package directory-healthcheck/1.0.0/directory_healthcheck-1.0.0-py3-none-any.whl

---
+++ packages/directory-healthcheck/directory_healthcheck-1.0.0-py3-none-any.whl/directory_healthcheck/views.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseForbidden
+from django.utils.crypto import constant_time_compare
+from django.views import View
+
+import health_check.views
+
+
+class HealthcheckView(health_check.views.MainView):
+    template_name = 'directory_healthcheck/healthcheck.html'
+
+    def has_permission(self):
+        return constant_time_compare(
+            self.request.GET.get('token'),
+            settings.DIRECTORY_HEALTHCHECK_TOKEN
+        )
+
+    def get(self, *args, **kwargs):
+        if not self.has_permission():
+            return HttpResponseForbidden()
+        return super().get(*args, **kwargs)
+
+    def render_to_response(self, context, status):
+        context['errored_plugins'] = [
+            plugin for plugin in context['plugins'] if plugin.errors
+        ]
+        return super().render_to_response(context=context, status=status)
+
+
+class PingView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('OK')
