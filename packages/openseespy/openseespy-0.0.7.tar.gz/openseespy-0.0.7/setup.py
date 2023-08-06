28ca9da99f10b937881638857d2a50dc1efa3948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:02 +0000

    Add package openseespy/0.0.7/openseespy-0.0.7.tar.gz

---
+++ packages/openseespy/openseespy-0.0.7.tar.gz/openseespy-0.0.7/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+about = {}
+with open('openseespy/version.py') as fp:
+    exec(fp.read(), about)
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openseespy",
+    version=about['version_num'],
+    author="Minjie Zhu",
+    author_email="zhum@oregonstate.edu",
+    description="A OpenSeesPy package",
+    long_description=long_description,
+    url="https://github.com/openseespy/openseespy",
+    packages=setuptools.find_packages(),
+    package_data={
+        'openseespy': ['opensees.so','opensees.pyd','LICENSE.rst'],
+    },
+    license = 'LICENSE.rst',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows'
+    ],
+    python_requires='>=3.6',
+    py_modules=['opensees'],
+    zip_safe=False
+)
