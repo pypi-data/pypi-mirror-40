c5151fc70498c9ae5942b855777ca4b20466ee0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:57 +0000

    Add package tugit/1.1.1/tugit-1.1.1.tar.gz

---
+++ packages/tugit/tugit-1.1.1.tar.gz/tugit-1.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name='tugit',
+	version='1.1.1',
+	description='Python library for installing packages directly from a git repository',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/idin/tugit.git',
+	author='Idin',
+	author_email='d@idin.net',
+	license='MIT',
+	packages=find_packages(exclude=("jupyter_tests")),
+	install_requires=[],
+	zip_safe=False
+)
+
