07789f6665b333a9a9953c939807465fbe2e9a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:34 +0000

    Add package pytorch-models/0.2.0/pytorch-models-0.2.0.tar.gz

---
+++ packages/pytorch-models/pytorch-models-0.2.0.tar.gz/pytorch-models-0.2.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+import torchmodels
+
+
+setup(
+    name="pytorch-models",
+    version=torchmodels.__version__,
+    license="MIT",
+    description="A thin wrapper for scriptable PyTorch modules",
+    author="Kang Min Yoo",
+    author_email="kaniblurous@gmail.com",
+    url="https://github.com/kaniblu/pytorch-models",
+    packages=[
+        "torchmodels",
+        "torchmodels.modules"
+    ],
+    scripts=[
+        "scripts/scaffold"
+    ],
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Java",
+        "Programming Language :: Python",
+    ],
+    platforms=[
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=[
+        "torch>=1",
+        "pyyaml",
+        "tqdm"
+    ]
+)
