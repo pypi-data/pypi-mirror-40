146981b325019065da129a74e91938b868aff5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:57 +0000

    Add package lgblkb-tools/0.0.6/lgblkb_tools-0.0.6.tar.gz

---
+++ packages/lgblkb-tools/lgblkb_tools-0.0.6.tar.gz/lgblkb_tools-0.0.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md","r") as fh:
+	long_description=fh.read()
+
+setuptools.setup(
+	name="lgblkb_tools",
+	version="0.0.6",
+	author="Dias Bakhtiyarov",
+	author_email="dbakhtiyarov@nu.edu.kz",
+	description="Some useful tools for everyday routine coding improvisation)",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://bitbucket.org/lgblkb/lgblkb_tools",
+	packages=setuptools.find_packages(),
+	classifiers=(
+		"Programming Language :: Python :: 3.6",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	),
+)
