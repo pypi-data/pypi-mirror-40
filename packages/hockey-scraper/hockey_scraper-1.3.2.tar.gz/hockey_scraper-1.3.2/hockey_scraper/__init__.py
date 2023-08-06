8a00152ca1719d8f74a8bf90e98a96b954d1b533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:05 +0000

    Add package hockey-scraper/1.3.2/hockey_scraper-1.3.2.tar.gz

---
+++ packages/hockey-scraper/hockey_scraper-1.3.2.tar.gz/hockey_scraper-1.3.2/hockey_scraper/__init__.py
@@ -0,0 +1,3 @@
+from hockey_scraper.scrape_functions import scrape_games, scrape_date_range, scrape_seasons
+from hockey_scraper.live_scrape import ScrapeLiveGames, LiveGame
+from hockey_scraper.nwhl import scrape_functions as nwhl
