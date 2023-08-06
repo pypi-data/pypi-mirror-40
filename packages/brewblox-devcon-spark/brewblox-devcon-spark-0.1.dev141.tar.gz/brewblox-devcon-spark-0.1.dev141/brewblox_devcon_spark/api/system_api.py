01da056668365c0d004cef7023ccfea13dbb7047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:32 +0000

    Add package brewblox-devcon-spark/0.1.dev141/brewblox-devcon-spark-0.1.dev141.tar.gz

---
+++ packages/brewblox-devcon-spark/brewblox-devcon-spark-0.1.dev141.tar.gz/brewblox-devcon-spark-0.1.dev141/brewblox_devcon_spark/api/system_api.py
@@ -0,0 +1,79 @@
+"""
+Specific endpoints for using system objects
+"""
+
+
+from typing import List
+
+from aiohttp import web
+from brewblox_service import brewblox_logger
+
+from brewblox_devcon_spark.api import API_DATA_KEY, object_api
+from brewblox_devcon_spark.datastore import PROFILES_CONTROLLER_ID
+
+LOGGER = brewblox_logger(__name__)
+routes = web.RouteTableDef()
+
+
+def setup(app: web.Application):
+    app.router.add_routes(routes)
+
+
+class SystemApi():
+
+    def __init__(self, app: web.Application):
+        self._obj_api: object_api.ObjectApi = object_api.ObjectApi(app)
+
+    async def read_profiles(self) -> List[int]:
+        profiles = await self._obj_api.read(PROFILES_CONTROLLER_ID)
+        return profiles[API_DATA_KEY]['active']
+
+    async def write_profiles(self, profiles: List[int]) -> List[int]:
+        profile_obj = await self._obj_api.write(
+            input_id=PROFILES_CONTROLLER_ID,
+            profiles=[],
+            input_type='Profiles',
+            input_data={'active': profiles}
+        )
+        return profile_obj[API_DATA_KEY]['active']
+
+
+@routes.get('/system/profiles')
+async def profiles_read(request: web.Request) -> web.Response:
+    """
+    ---
+    summary: Read active profiles
+    tags:
+    - Spark
+    - System
+    - Profiles
+    operationId: controller.spark.profiles.read
+    produces:
+    - application/json
+    """
+    return web.json_response(
+        await SystemApi(request.app).read_profiles()
+    )
+
+
+@routes.put('/system/profiles')
+async def profiles_write(request: web.Request) -> web.Response:
+    """
+    ---
+    summary: Write active profiles
+    tags:
+    - Spark
+    - System
+    - Profiles
+    operationId: controller.spark.profiles.write
+    produces:
+    - application/json
+    parameters:
+    -
+        name: profiles
+        type: list
+        example: [0, 1, 2, 3]
+    """
+    return web.json_response(
+        await SystemApi(request.app).write_profiles(await request.json())
+    )
---
+++ packages/brewblox-devcon-spark/brewblox-devcon-spark-0.1.dev141.tar.gz/brewblox-devcon-spark-0.1.dev141/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='brewblox-devcon-spark',
+    use_scm_version={'local_scheme': lambda v: ''},
+    description='Communication with Spark controllers',
+    long_description=open('README.md').read(),
+    url='https://github.com/BrewBlox/brewblox-devcon-spark',
+    author='BrewPi',
+    author_email='Development@brewpi.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: System :: Hardware',
+    ],
+    license='GPLv3',
+    keywords='brewing brewpi brewblox embedded controller spark service',
+    packages=find_packages(exclude=['test']),
+    install_requires=[
+        'brewblox-service~=0.14.0',
+        'dpath~=1.4.2',
+        'pyserial-asyncio==0.4',
+        'construct==2.9.45',
+        'protobuf~=3.6.0',
+        'pint~=0.8',
+        'aiofiles~=0.4.0',
+        'aiohttp-sse~=2.0',
+    ],
+    python_requires='>=3.7',
+    setup_requires=['setuptools_scm'],
+)
