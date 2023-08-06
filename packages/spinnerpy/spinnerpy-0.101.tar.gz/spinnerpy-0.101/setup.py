119f8390de8b50a2ce34d512ea7f3f6335771f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:28 +0000

    Add package spinnerpy/0.101/spinnerpy-0.101.tar.gz

---
+++ packages/spinnerpy/spinnerpy-0.101.tar.gz/spinnerpy-0.101/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spinnerpy',
+    version='0.101',
+    description='🔥 Spinnerpy new python loading spinners repository 🔥',
+    long_description='Coming soon',
+    author='truedl',
+    author_email='dauzduz1@gmail.com',
+    url='https://github.com/truedl/spinnerpy',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=['psutil', 'lxml'],
+)
