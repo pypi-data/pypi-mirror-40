28d7b65d4e4961c0b56d8606ea0f185436c886cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:20 +0000

    Add package beautyacc/0.2.0/beautyacc-0.2.0.tar.gz

---
+++ packages/beautyacc/beautyacc-0.2.0.tar.gz/beautyacc-0.2.0/README.md
@@ -0,0 +1,18 @@
+# beautyacc
+
+beautyacc is a library to access EPICS archiver values in Python.
+It is designed for the EPICS RDB Archiver called "beauty".
+The name beautyacc is shorthand for 'beauty access'.
+
+This package was crafted for Python 3.4 or newer.
+
+### Installation
+
+Installing from PyPI is easy:
+
+    pip install beautyacc
+
+### Resources
+
+* Github: <https://github.com/pklaus/beautyacc>
+* PyPI: <https://pypi.org/project/beautyacc/>
---
+++ packages/beautyacc/beautyacc-0.2.0.tar.gz/beautyacc-0.2.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+# Monkey patching setuptools' safe_name function
+# to allow a package name containing underscores
+import pkg_resources
+pkg_resources.safe_name = lambda x: x
+
+try:
+    import pypandoc
+    LDESC = open('README.md', 'r').read()
+    LDESC = pypandoc.convert_text(LDESC, 'rst', format='md')
+except (ImportError, IOError, RuntimeError) as e:
+    print("Could not create long description:")
+    print(str(e))
+    LDESC = ''
+
+setup(name='beautyacc',
+      version='0.2.0',
+      description='Python package to access the EPICS RDB Archiver',
+      long_description=LDESC,
+      url='https://github.com/pklaus/beautyacc',
+      author='Philipp Klaus',
+      author_email='klaus@physik.uni-frankfurt.de',
+      packages = find_packages(),
+      py_modules=[],
+      install_requires=['psycopg2-binary'],
+      zip_safe=True,
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering :: Physics',
+      ]
+)
