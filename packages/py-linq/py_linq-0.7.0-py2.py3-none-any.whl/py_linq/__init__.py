1dfb45962734314af03fed4b055ff7a8ac23d633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:20 +0000

    Add package py-linq/0.7.0/py_linq-0.7.0-py2.py3-none-any.whl

---
+++ packages/py-linq/py_linq-0.7.0-py2.py3-none-any.whl/py_linq/__init__.py
@@ -0,0 +1,8 @@
+""" Linq for Python """
+
+__version__ = '0.7.0'
+
+try:
+    from py_linq import Enumerable  # noqa
+except ImportError:
+    from py_linq.py_linq import Enumerable3 as Enumerable  # noqa
