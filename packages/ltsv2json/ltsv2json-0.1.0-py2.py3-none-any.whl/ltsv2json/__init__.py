741bd925fd0264afcf988747ad34e2f49e9ac642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:18 +0000

    Add package ltsv2json/0.1.0/ltsv2json-0.1.0-py2.py3-none-any.whl

---
+++ packages/ltsv2json/ltsv2json-0.1.0-py2.py3-none-any.whl/ltsv2json/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import json
+import signal
+import sys
+
+
+def main():
+    signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+    for line in sys.stdin:
+        try:
+            record = dict(map(lambda f: f.split(":", 1), line.rstrip().split("\t")))
+            print(json.dumps(record, separators=(",", ":")))
+        except ValueError:
+            pass
+
+
+if __name__ == "__main__":
+    main()
