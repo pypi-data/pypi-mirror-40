43f474db4a378e21431a4c6734b55bf761d498ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:15 +0000

    Add package tree-storage/0.1.10/tree-storage-0.1.10.tar.gz

---
+++ packages/tree-storage/tree-storage-0.1.10.tar.gz/tree-storage-0.1.10/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tree-storage",
+    version="0.1.10",
+    author="Valerii Duz",
+    author_email="duz.valera.od@ex.ua",
+    description="Tree hash-storage files",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    test_suite="tests",
+    tests_require=['Pillow']
+)
