eb729788de7a991aeea5cbf976f60223da1f5e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:24 +0000

    Add package ocrd-fork-ocropy/1.4.0a3/ocrd-fork-ocropy-1.4.0a3.tar.gz

---
+++ packages/ocrd-fork-ocropy/ocrd-fork-ocropy-1.4.0a3.tar.gz/ocrd-fork-ocropy-1.4.0a3/setup.py
@@ -0,0 +1,35 @@
+from glob import glob
+from setuptools import setup
+
+models = [c for c in glob('models/*pyrnn.gz')]
+scripts = [c for c in glob('ocropus-*') if '.' not in c and '~' not in c]
+
+setup(
+    name='ocrd-fork-ocropy',
+    version='1.4.0a3',
+    author='Thomas Breuel',
+    maintainer='Konstantin Baierer',
+    maintainer_email='unixprog@gmail.com',
+    description='The OCRopy RNN-based Text Line Recognizer',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tmbdev/ocropy',
+    license='Apache-2.0',
+    python_requires='>=2.7',
+
+    packages=['ocrolib'],
+    data_files=[('share/ocropus', models)],
+    scripts=scripts,
+    include_package_data=True,
+
+    install_requires=[
+        'numpy      >= 1.15.4',
+        'scipy      >= 1.1.0',
+        'matplotlib >= 2.2.3',
+        'imageio    >= 2.4.1',
+        'Pillow     >= 2.7.0',
+        'lxml       >= 3.5.0',
+        'six        >= 1.11.0',
+    ],
+    keywords=['OCR', 'optical character recognition', 'ocropy', 'ocropus', 'kraken', 'calamari'],
+)
