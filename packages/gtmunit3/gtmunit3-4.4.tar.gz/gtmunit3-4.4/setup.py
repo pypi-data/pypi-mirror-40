4629c27d6a6c83bba7ef660c91a8c6bf12968d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:48 +0000

    Add package gtmunit3/4.4/gtmunit3-4.4.tar.gz

---
+++ packages/gtmunit3/gtmunit3-4.4.tar.gz/gtmunit3-4.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import os
+
+setup(
+    name='gtmunit3',
+    version=os.environ['VERSION'],
+
+    description='Package 1 for Gigantum Client unit testing.',
+    long_description='This package does not contain any real functionality. '
+                     'It exists for unit testing the pip integration for the Gigantum Client',
+
+    author='Gigantum',
+    author_email='hello@gigantum.com',
+
+    packages=['gtmunit3'],
+
+    url='https://github.com/gigantum/gigantum-client',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords=[]
+)
