d7a4723983d5a7416bf772ad1b6c83a8dd0825fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:14 +0000

    Add package j.osh/0.7/j.osh-0.7.tar.gz

---
+++ packages/j.osh/j.osh-0.7.tar.gz/j.osh-0.7/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='j.osh',
+    version='0.7',
+    packages=['j',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
