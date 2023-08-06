439c67387f6a138e860d0d1dfd6289df0b5eab0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:18 +0000

    Add package pyisy/1.1.1/PyISY-1.1.1.tar.gz

---
+++ packages/pyisy/PyISY-1.1.1.tar.gz/PyISY-1.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyISY',
+    version='1.1.1',
+    license='Apache License 2.0',
+    url='http://automic.us/projects/pyisy',
+    download_url='https://github.com/automicus/pyisy/tarball/1.1.1',
+    author='Ryan Kraus',
+    author_email='automicus@gmail.com',
+    description='Python module to talk to ISY994 from UDI.',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['requests', 'VarEvents'],
+    keywords=['home automation', 'isy', 'isy994', 'isy-994', 'UDI'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
