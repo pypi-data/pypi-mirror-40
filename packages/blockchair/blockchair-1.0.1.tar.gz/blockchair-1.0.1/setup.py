5d4de8dadafb70f422c0e8a8cbc6054985b70585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:34 +0000

    Add package blockchair/1.0.1/blockchair-1.0.1.tar.gz

---
+++ packages/blockchair/blockchair-1.0.1.tar.gz/blockchair-1.0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='blockchair',
+      version='1.0.1',
+      description='Blockchair API Python Library',
+      author='Rustam Sultanov',
+      author_email='info@blockchair.com',
+      url='https://golf.blockchair.com/RustamSultanov/blockchair-python-sdk',
+      license='MIT',
+      packages=['blockchair'],
+      keywords='blockchair.com api explorer',
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Office/Business :: Financial",
+        "License :: OSI Approved :: MIT License",
+        ],
+)
