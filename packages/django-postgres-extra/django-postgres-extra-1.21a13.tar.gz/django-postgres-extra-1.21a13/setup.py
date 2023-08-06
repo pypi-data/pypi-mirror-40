5f792c789e335870e0c68ba67bb52635353c8e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:13 +0000

    Add package django-postgres-extra/1.21a13/django-postgres-extra-1.21a13.tar.gz

---
+++ packages/django-postgres-extra/django-postgres-extra-1.21a13.tar.gz/django-postgres-extra-1.21a13/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as readme:
+    README = readme.read().split('h1>\n\n', 2)[1]
+
+setup(
+    name='django-postgres-extra',
+    version='1.21a13',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Bringing all of PostgreSQL\'s awesomeness to Django.',
+    long_description=README,
+    url='https://github.com/SectorLabs/django-postgres-extra',
+    author='Sector Labs',
+    author_email='open-source@sectorlabs.ro',
+    keywords=['django', 'postgres', 'extra', 'hstore', 'ltree'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
+)
---
+++ packages/django-postgres-extra/django-postgres-extra-1.21a13.tar.gz/django-postgres-extra-1.21a13/tests/test_query.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+from psqlextra.fields import HStoreField
+from psqlextra.expressions import HStoreRef
+
+from .fake_model import get_fake_model
+
+
+def test_annotate_hstore_key_ref():
+    """Tests whether annotating using a :see:HStoreRef expression
+    works correctly.
+
+    This allows you to select an individual hstore key."""
+
+    model_fk = get_fake_model({
+        'title': HStoreField(),
+    })
+
+    model = get_fake_model({
+        'fk': models.ForeignKey(model_fk, on_delete=models.CASCADE)
+    })
+
+    fk = model_fk.objects.create(title={'en': 'english', 'ar': 'arabic'})
+    model.objects.create(fk=fk)
+
+    queryset = (
+        model.objects
+        .annotate(english_title=HStoreRef('fk__title', 'en'))
+        .values('english_title')
+        .first()
+    )
+
+    assert queryset['english_title'] == 'english'
+
+
+def test_hstore_f_ref():
+    """Tests whether F(..) expressions can be used in
+    hstore values when performing update queries."""
+
+    model = get_fake_model({
+        'name': models.CharField(max_length=255),
+        'name_new': HStoreField()
+    })
+
+    model.objects.create(
+        name='waqas',
+        name_new=dict(en='swen')
+    )
+
+    model.objects.update(
+        name_new=dict(en=models.F('name'))
+    )
+
+    inst = model.objects.all().first()
+    assert inst.name_new.get('en') == 'waqas'
