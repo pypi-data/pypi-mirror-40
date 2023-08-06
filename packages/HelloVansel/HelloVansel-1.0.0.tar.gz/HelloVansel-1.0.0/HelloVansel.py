62d091ed08609bfdc8d9b353a8e1ce67b3918571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:24 +0000

    Add package HelloVansel/1.0.0/HelloVansel-1.0.0.tar.gz

---
+++ packages/HelloVansel/HelloVansel-1.0.0.tar.gz/HelloVansel-1.0.0/HelloVansel.py
@@ -0,0 +1,3 @@
+"""这只是一个测试模块"""
+def print_words():
+	print("HelloWorld")
---
+++ packages/HelloVansel/HelloVansel-1.0.0.tar.gz/HelloVansel-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'HelloVansel',
+        version = '1.0.0',
+        py_modules = ['HelloVansel'],
+        author = 'van',
+        author_email = 'van@sina.com',
+        url = 'http://www.baidu.com',
+        description = 'a test',
+    )
