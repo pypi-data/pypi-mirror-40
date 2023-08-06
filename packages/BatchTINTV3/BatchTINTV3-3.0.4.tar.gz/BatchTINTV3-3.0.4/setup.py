cf43c936ffb245203fab6786962824e2c8ff46e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:21 +0000

    Add package BatchTINTV3/3.0.4/BatchTINTV3-3.0.4.tar.gz

---
+++ packages/BatchTINTV3/BatchTINTV3-3.0.4.tar.gz/BatchTINTV3-3.0.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+pkgs = setuptools.find_packages()
+print('found these packages:', pkgs)
+
+pkg_name = "BatchTINTV3"
+
+setuptools.setup(
+    name=pkg_name,
+    version="3.0.4",
+    author="Geoffrey Barrett",
+    author_email="",
+    description="BatchTINTV3 - GUI created to more efficiently sort Axona/Tint data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HussainiLab/BatchTINTV3.git",
+    packages=pkgs,
+    install_requires=
+    [
+        'PyQt5',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3) ",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7 "
+    ),
+)
