89272458a8690d6855203fe50b91d2b96835b222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:49 +0000

    Add package newtopythonpypi/1.0/newtopythonpypi-1.0.tar.gz

---
+++ packages/newtopythonpypi/newtopythonpypi-1.0.tar.gz/newtopythonpypi-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='newtopythonpypi',
+      version='1.0',
+      description='Python newbi',
+      url='http://github.com/sourabhjains',
+      author='Sourabh Jain',
+      author_email='jainsourabh679@gmail.com',
+      license='MIT',
+      packages=['newtopythonpypi'],
+      zip_safe=False)
