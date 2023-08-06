c7c8823000deed13068daf91d097a31bba819bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:27 +0000

    Add package yamz/0.0.5/yamz-0.0.5.tar.gz

---
+++ packages/yamz/yamz-0.0.5.tar.gz/yamz-0.0.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='yamz',
+      version='0.0.5',
+      description='An easy way to manage environment specific configuration',
+      long_description=long_description,
+      author='Lemuel Boyce',
+      author_email='lemuelboyce@gmail.com',
+      packages=['yamz'],
+      url='https://github.com/rhymiz/yamz',
+      include_package_data=True,
+      zip_safe=False,
+      license='MIT',
+      python_requires='>=3.6',
+      install_requires=[
+          'PyYAML==4.2b4'
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ])
---
+++ packages/yamz/yamz-0.0.5.tar.gz/yamz-0.0.5/yamz/environment.py
@@ -0,0 +1,64 @@
+import os
+from pathlib import Path
+
+from yaml import load
+
+GLOBAL_KEY_NAME = 'global'
+
+
+class YamzEnvironmentError(RuntimeError):
+    pass
+
+
+def _load_config(path: str) -> dict:
+    if not Path(path).exists():
+        raise YamzEnvironmentError("%s was not found!" % path)
+
+    with open(path, 'r') as f:
+        config = load(f.read())
+    return config
+
+
+def _parse_value(key: str, value: str) -> str:
+    if value.startswith("$"):
+        return os.environ.get(key, value)
+    return value
+
+
+def _build(conf: dict) -> dict:
+    parsed_conf = {}
+    for k, v in conf.items():
+        parsed_conf[k] = _parse_value(k, v)
+    return parsed_conf
+
+
+def _load(path: str, environment: str) -> dict:
+    conf = _load_config(path)
+    if environment not in conf:
+        raise YamzEnvironmentError("environment %s does not exist "
+                                   "in settings.yaml" % environment)
+
+    defaults = {}
+    if GLOBAL_KEY_NAME in conf:
+        defaults = _build(conf[GLOBAL_KEY_NAME])
+
+    env = _build(conf[environment])
+    defaults.update(env)
+    return defaults
+
+
+class Environment(object):
+    def __init__(self, path: str):
+        self.path = path
+        self._loaded = False
+        self._settings = {}
+
+    def load(self, environment: str) -> None:
+        self._loaded = True
+        self._settings = _load(self.path, environment)
+
+    def __getattr__(self, key: str) -> str:
+        if not self._loaded:
+            raise YamzEnvironmentError("Tried to access key `%s` before "
+                                       "environment was loaded!" % key)
+        return self._settings[key]
