0e0478ed391c8c22ab8196f4c18ab4483c9955ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:58 +0000

    Add package pid-1zlab/1.0/pid-1zlab-1.0.tar.gz

---
+++ packages/pid-1zlab/pid-1zlab-1.0.tar.gz/pid-1zlab-1.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='pid-1zlab',
+    version='1.0',
+    packages=['pid'],
+    include_package_data=True,
+    license='MIT License',
+    description='PID controll algorithm.',
+    url='https://www.1zlab.com',
+    author='Akai',
+    author_email='xingshunkai@qq.com',
+    platforms='Linux,Unix,Windows',
+    keywords='PID 1zlab controll micropython',
+)
