627396cd4752f6f83dad68b91e67dee43b0201a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:39 +0000

    Add package pyMatrixStack/0.0.2/pyMatrixStack-0.0.2.tar.gz

---
+++ packages/pyMatrixStack/pyMatrixStack-0.0.2.tar.gz/pyMatrixStack-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, Extension
+
+
+setup (name = 'pyMatrixStack',
+       version = '0.0.2',
+       description = 'Matrix stack for OpenGL',
+       author = 'William Emerison Six',
+       author_email = 'billsix@gmail.com',
+       url = 'https://github.com/billsix/pyMatrixStack',
+       keywords = "nuklear imgui",
+       license = "MIT",
+       packages=['pyMatrixStack'],
+       package_dir={'pyMatrixStack': 'src/pyMatrixStack'},
+       classifiers=[
+           "Development Status :: 3 - Alpha",
+           "Topic :: Utilities",
+           "License :: OSI Approved :: MIT License",
+       ],
+       install_requires=[
+           'numpy'],
+       long_description = '''
+A matrix stack implementation for OpenGL 3+
+''')
