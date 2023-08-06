ccda4bfb6e89a545cfc8245907ced5579ae94857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:36 +0000

    Add package pcpartpicker/0.1.3/pcpartpicker-0.1.3-py3-none-any.whl

---
+++ packages/pcpartpicker/pcpartpicker-0.1.3-py3-none-any.whl/pcpartpicker/__init__.py
@@ -0,0 +1,8 @@
+from .api import API
+from .errors import UnsupportedRegion
+from .parts import *
+
+__name__ = ["pcpartpicker"]
+__version__ = '0.1.2'
+__author__ = 'Jonathan Vusich'
+__email__ = 'jonathanvusich@gmail.com'
