2811a17a693a204476587dc0f24067bb932d186d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:09 +0000

    Add package skitai/0.27.6a3/skitai-0.27.6a3.tar.gz

---
+++ packages/skitai/skitai-0.27.6a3.tar.gz/skitai-0.27.6a3/skitai/saddle/contrib/decorative/django.py
@@ -0,0 +1,6 @@
+#-----------------------------------------------------------
+# This module directory is deprecated 
+# USE skitai.saddle.contrib.tasks
+#-----------------------------------------------------------
+
+from ..services.django import auth
