894f57e3ca20653438e03b3b3934940eda9ce252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:14 +0000

    Add package updatehub-agent-sdk/1.0.1/updatehub-agent-sdk-1.0.1.tar.gz

---
+++ packages/updatehub-agent-sdk/updatehub-agent-sdk-1.0.1.tar.gz/updatehub-agent-sdk-1.0.1/updatehub/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2018 O.S. Systems Software LTDA.
+"""
+This is the updatehub agent package SDK for the Python language. With this
+package it's possible to access information about a running updatehub agent,
+such as current states, log information and also force a new version probe.
+
+For more information please access http://docs.updatehub.io/
+"""
+
+
+__version__ = '1.0.1'
+
+
+def get_version():
+    """
+    Returns the current version of the package.
+    """
+    return __version__
