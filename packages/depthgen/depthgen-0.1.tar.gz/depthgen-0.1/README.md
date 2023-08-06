0eab5b447847e295648e4193d97ca2641fe5ee9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:57 +0000

    Add package depthgen/0.1/depthgen-0.1.tar.gz

---
+++ packages/depthgen/depthgen-0.1.tar.gz/depthgen-0.1/README.md
@@ -0,0 +1,4 @@
+# depthgen
+
+python library for generating depth map from a single image (photo)
+based on https://github.com/lixx2938/MegaDepth under the MIT license
---
+++ packages/depthgen/depthgen-0.1.tar.gz/depthgen-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import os
+
+setup(name='depthgen',
+      version='0.1',
+      description='generating depth map from single image',
+      url='http://github.com/jaroslaw-weber/depthgen',
+      author='Jaroslaw Weber',
+      author_email='jaroslawweber@gmail.com',
+      license='MIT',
+      packages=['depthgen'],
+      zip_safe=False)
