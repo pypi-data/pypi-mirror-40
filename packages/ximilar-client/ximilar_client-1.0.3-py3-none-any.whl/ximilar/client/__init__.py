85a2bd35c7e933168e910438609cdb04b353e2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:00 +0000

    Add package ximilar-client/1.0.3/ximilar_client-1.0.3-py3-none-any.whl

---
+++ packages/ximilar-client/ximilar_client-1.0.3-py3-none-any.whl/ximilar/client/__init__.py
@@ -0,0 +1,5 @@
+from .client import RestClient
+from .recognition import RecognitionClient
+from .tagging import GenericTaggingClient, FashionTaggingClient
+from .colors import DominantColorProductClient, DominantColorGenericClient
+from .search import SmartSearchClient
---
+++ packages/ximilar-client/ximilar_client-1.0.3-py3-none-any.whl/ximilar/client/constants.py
@@ -0,0 +1,14 @@
+# General
+RECORDS = 'records'
+FILE = '_file'
+URL = '_url'
+BASE64 = '_base64'
+IMG_DATA = '_img_data'
+RESULT_OK = {'status': 'OK'}
+
+# Recognition/Vize related
+RESULTS = 'results'
+ID = 'id'
+NAME = 'name'
+TASK = 'task'
+TASKS_COUNT = 'tasksCount'
