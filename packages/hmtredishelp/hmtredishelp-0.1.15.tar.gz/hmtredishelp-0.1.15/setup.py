a05fe823efcd9d454264100f5cb543ce4e47f694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:44 +0000

    Add package hmtredishelp/0.1.15/hmtredishelp-0.1.15.tar.gz

---
+++ packages/hmtredishelp/hmtredishelp-0.1.15.tar.gz/hmtredishelp-0.1.15/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='hmtredishelp',
+      version='0.1.15',
+      description='A set of tools we use to easily access redis',
+      url='https://github.com/IntuitionMachines/redis-tools',
+      author='Intuition Machines',
+      author_email='dev@intuitionmachines.com',
+      license='MIT',
+      packages=['hmtredishelp'],
+      install_requires=["redis"],
+      zip_safe=False)
+
