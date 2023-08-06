20fd4efdf78fc71cea93d8e5e345cb39c49c610f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:54 +0000

    Add package cognitojwt/1.0.2/cognitojwt-1.0.2-py3-none-any.whl

---
+++ packages/cognitojwt/cognitojwt-1.0.2-py3-none-any.whl/cognitojwt/__init__.py
@@ -0,0 +1,23 @@
+
+from .jwt_sync import decode
+from .exceptions import CognitoJWTException
+
+name = 'cognitojwt'
+
+
+__all__ = ['CognitoJWTException']
+
+
+try:
+    import requests
+    __all__.append('decode')
+except ImportError:
+    pass
+
+try:
+    import aiohttp
+    from async_lru import alru_cache
+    from .jwt_async import decode_async
+    __all__.append('decode_async')
+except ImportError:
+    pass
