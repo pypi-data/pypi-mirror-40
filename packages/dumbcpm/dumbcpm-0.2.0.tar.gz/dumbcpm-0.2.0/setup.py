14b446b1abb9bdecc1b7aad921f0b547410e715b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:12 +0000

    Add package dumbcpm/0.2.0/dumbcpm-0.2.0.tar.gz

---
+++ packages/dumbcpm/dumbcpm-0.2.0.tar.gz/dumbcpm-0.2.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='dumbcpm',
+      version='0.2.0',
+      description='Possibly very dumb package manager for C/++, written in Python.',
+      author='Nickolay Ilyushin',
+      author_email='nickolay02@inbox.ru',
+      url='https://github.com/handicraftsman/dumbcpm/',
+      license='MIT',
+      packages=['dumbcpm'],
+      classifiers=[
+          'Programming Language :: C',
+          'Programming Language :: C++',
+          'Topic :: Software Development :: Build Tools',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: POSIX :: Linux'
+      ],
+      install_requires=[
+          'structlog',
+          'PyYAML',
+          'semver',
+          'colorama',
+          'pkgconfig'
+      ])
