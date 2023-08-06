3b35d34a5f51cc67a4a14355f0e2310fa914fe23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:35 +0000

    Add package raiseorlaunch/2.2.0/raiseorlaunch-2.2.0.tar.gz

---
+++ packages/raiseorlaunch/raiseorlaunch-2.2.0.tar.gz/raiseorlaunch-2.2.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+setup.py for raiseorlaunch
+"""
+
+from setuptools import setup
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='raiseorlaunch',
+    version='2.2.0',
+    description='A run-or-raise-application-launcher for i3 window manager.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/open-dynaMIX/raiseorlaunch',
+    author='Fabio Rämi',
+    author_email='fabio@dynamix-tontechnik.ch',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='i3 i3wm launcher run-or-raise navigation workspace scratchpad',
+    install_requires=['i3ipc'],
+    packages=['raiseorlaunch'],
+    entry_points={
+        'console_scripts': [
+            'raiseorlaunch = raiseorlaunch.__main__:main',
+        ],
+    },
+)
