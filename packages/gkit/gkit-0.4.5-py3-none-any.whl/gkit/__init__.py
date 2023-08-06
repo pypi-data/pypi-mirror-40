d9b972b9aaca2396c83491276b4ade78e53b0993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:13 +0000

    Add package gkit/0.4.5/gkit-0.4.5-py3-none-any.whl

---
+++ packages/gkit/gkit-0.4.5-py3-none-any.whl/gkit/__init__.py
@@ -0,0 +1,8 @@
+from .core import Raster, zonal_apply, split_by_shp
+from .math import *
+from .io import read, save
+
+
+__version__ = "0.4.5"
+__author__ = "titor"
+__email__ = "titor.sun@gmail.com"
