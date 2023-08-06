53731db4481fb1a9b2d8df2e3a102bb3eadcdc89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:06 +0000

    Add package webcomix/3.0/webcomix-3.0-py3-none-any.whl

---
+++ packages/webcomix/webcomix-3.0-py3-none-any.whl/webcomix/comic.py
@@ -0,0 +1,116 @@
+import os
+from typing import List, Mapping
+from zipfile import ZipFile, BadZipFile
+
+import click
+from fake_useragent import UserAgent
+
+from webcomix.scrapy.download.comic_spider import ComicSpider
+from webcomix.scrapy.verification.verification_spider import VerificationSpider
+from webcomix.scrapy.crawler_worker import CrawlerWorker
+
+ua = UserAgent()
+header = {"User-Agent": str(ua.chrome)}
+
+
+class Comic:
+    def __init__(
+        self,
+        name: str,
+        start_url: str,
+        comic_image_selector: str,
+        next_page_selector: str,
+        single_page: bool,
+    ):
+        self.name = name
+        self.start_url = start_url
+        self.next_page_selector = next_page_selector
+        self.comic_image_selector = comic_image_selector
+        self.single_page = single_page
+
+    def download(self) -> None:
+        """
+        Downloads an entire comic page by page starting from the first one
+        and saves them in the directory_name created in the current working
+        directory
+        """
+        if not os.path.isdir(self.name):
+            os.makedirs(self.name)
+
+        settings = {
+            "ITEM_PIPELINES": {
+                "webcomix.scrapy.download.comic_pipeline.ComicPipeline": 1,
+                "scrapy.pipelines.files.FilesPipeline": 500,
+            },
+            "LOG_ENABLED": False,
+            "FILES_STORE": self.name,
+            "MEDIA_ALLOW_REDIRECTS": True,
+        }
+
+        worker = CrawlerWorker(
+            settings,
+            False,
+            ComicSpider,
+            start_urls=[self.start_url],
+            comic_image_selector=self.comic_image_selector,
+            next_page_selector=self.next_page_selector,
+            directory=self.name,
+        )
+
+        worker.start()
+
+        click.echo("Finished downloading the images.")
+
+    def convert_to_cbz(self) -> None:
+        """
+        Takes all of the previously downloaded pages and compresses them in
+        a .cbz file, erasing them afterwards.
+        """
+        with ZipFile("{}.cbz".format(self.name), mode="w") as cbz_file:
+            images = os.listdir(self.name)
+            for image in images:
+                image_location = "{}/{}".format(self.name, image)
+                cbz_file.write(image_location, image)
+                os.remove(image_location)
+            os.rmdir(self.name)
+            if cbz_file.testzip() is not None:
+                raise BadZipFile(
+                    "Error while testing the archive; it might be corrupted."
+                )
+
+    def verify_xpath(self) -> List[Mapping]:
+        """
+        Takes a url and the XPath expressions for the next_page and image to
+        go three pages into the comic. It returns a tuple containing the url
+        of each page and their respective image urls.
+        """
+        settings = {"LOG_ENABLED": False}
+
+        worker = CrawlerWorker(
+            settings,
+            True,
+            VerificationSpider,
+            start_urls=[self.start_url],
+            comic_image_selector=self.comic_image_selector,
+            next_page_selector=self.next_page_selector,
+            number_of_pages_to_check=1 if self.single_page else 3,
+        )
+
+        verification = worker.start()
+
+        return verification
+
+    @staticmethod
+    def save_image_location(url: str, page: int, directory_name: str = "") -> str:
+        """
+        Returns the relative location in the filesystem under which the
+        webcomic will be saved. If directory_name is specified, it will be
+        relative to the current directory; if not specified, it will return
+        the name relative to the directory in which it is downloaded.
+        """
+        if url.count(".") <= 1:
+            # No file extension (only dot in url is domain name)
+            file_name = str(page)
+        else:
+            file_name = "{}{}".format(page, url[url.rindex(".") :])
+        return os.path.join(directory_name, file_name)
