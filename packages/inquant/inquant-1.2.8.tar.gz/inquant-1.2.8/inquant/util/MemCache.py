8f711a75c8de1fb357bbbd3ab895d3c8704d5bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:50 +0000

    Add package inquant/1.2.8/inquant-1.2.8.tar.gz

---
+++ packages/inquant/inquant-1.2.8.tar.gz/inquant-1.2.8/inquant/util/MemCache.py
@@ -0,0 +1,80 @@
+#coding=utf-8
+import time
+import threading
+from inquant.util.LogRecord import *
+import traceback
+
+class MemCache(object):
+    '''内存缓存'''
+
+    def __init__(self):
+        '''初始化'''
+        self.mem = {}
+        self.time = {}
+        self.lock = threading.Lock()
+        t = threading.Thread(target=self.__checkCache)
+        t.start()
+
+    def __checkCache(self):
+        """定时检查缓存，清除垃圾数据"""
+        while True:
+            time.sleep(5)
+            try:
+                self.lock.acquire(1)
+                for key in self.mem.keys():
+                    if self.time[key] != -1 and self.time[key] <= time.time():
+                        self.delete(key)
+            except Exception as e:
+                LogRecord.writeLog("MemCacheError", str(e))
+                LogRecord.writeLog("MemCacheError", traceback.format_exc())
+            finally:
+                self.lock.release()
+
+    def set(self, key, data, age=-1):
+        '''保存键为key的值，时间位age'''
+        try:
+            self.lock.acquire(1)
+            self.mem[key] = data
+            if age == -1:
+                self.time[key] = -1
+            else:
+                self.time[key] = time.time() + age
+            return True
+        finally:
+            self.lock.release()
+
+    def get(self,key):
+        '''获取键key对应的值'''
+        try:
+            self.lock.acquire(1)
+            if key in self.mem.keys():
+                if self.time[key] == -1 or self.time[key] > time.time():
+                    return self.mem[key]
+                else:
+                    self.delete(key)
+                    return None
+            else:
+                return None
+        finally:
+            self.lock.release()
+
+    def delete(self,key):
+        '''删除键为key的条目'''
+        try:
+            self.lock.acquire(1)
+            if key in self.mem.keys():
+                del self.mem[key]
+            if key in self.time.keys():
+                del self.time[key]
+            return True
+        finally:
+            self.lock.release()
+
+    def clear(self):
+        '''清空所有缓存'''
+        try:
+            self.lock.acquire(1)
+            self.mem.clear()
+            self.time.clear()
+        finally:
+            self.lock.release()
