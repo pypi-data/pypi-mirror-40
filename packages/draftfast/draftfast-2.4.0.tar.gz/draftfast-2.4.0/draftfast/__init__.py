4611d3b3b6f63496a55009ded3059e778459956d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:36 +0000

    Add package draftfast/2.4.0/draftfast-2.4.0.tar.gz

---
+++ packages/draftfast/draftfast-2.4.0.tar.gz/draftfast-2.4.0/draftfast/__init__.py
@@ -0,0 +1,15 @@
+from draftfast import (
+    optimize,
+    rules,
+    orm,
+    csv_parse,
+    exposure,
+    pickem,
+)
+
+assert optimize
+assert rules
+assert orm
+assert csv_parse
+assert exposure
+assert pickem
