bc6bd3db28adcc3aa2b924b133fbefe4389b7e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:14 +0000

    Add package particle/0.0.0/particle-0.0.0.tar.gz

---
+++ packages/particle/particle-0.0.0.tar.gz/particle-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='particle',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://github.com/scikit-hep/particle',
+    author='Eduardo Rodrigues',
+    author_email='eduardo.rodrigues@cern.ch',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
