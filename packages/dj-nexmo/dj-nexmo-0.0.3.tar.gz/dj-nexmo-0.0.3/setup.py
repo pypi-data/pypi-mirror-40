0b4498fe99026f6bd580f792d00463439a589a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:12 +0000

    Add package dj-nexmo/0.0.3/dj-nexmo-0.0.3.tar.gz

---
+++ packages/dj-nexmo/dj-nexmo-0.0.3.tar.gz/dj-nexmo-0.0.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dj-nexmo",
+    version="0.0.3",
+    author="Nexmo Developer Relations",
+    author_email="devrel@nexmo.com",
+    url="https://github.com/Nexmo/dj-nexmo/",
+    description="Utilities for Django developers using Nexmo's APIs",
+
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    zip_safe=True,
+    entry_points={
+        "console_scripts": ["dj = django.core.management:execute_from_command_line"]
+    },
+    install_requires=[
+        "nexmo          ~= 2.0",
+        "django         ~= 2.0",
+        "attrs          ~= 17.4",
+        "marshmallow    >= 3.0.0rc3",
+        "phonenumbers   ~= 8.9",
+    ],
+    python_requires=">=3.4",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Communications :: Telephony",
+    ],
+)
