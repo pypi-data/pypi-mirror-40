76aa7eeb32143dab163c2da207e5d30fb754af27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:42 +0000

    Add package onegov.swissvotes/0.0.27/onegov.swissvotes-0.0.27-py3-none-any.whl

---
+++ packages/onegov.swissvotes/onegov.swissvotes-0.0.27-py3-none-any.whl/onegov/swissvotes/models/__init__.py
@@ -0,0 +1,16 @@
+from onegov.swissvotes.models.actor import Actor
+from onegov.swissvotes.models.page import TranslatablePage
+from onegov.swissvotes.models.policy_area import PolicyArea
+from onegov.swissvotes.models.principal import Principal
+from onegov.swissvotes.models.vote import SwissVote
+from onegov.swissvotes.models.vote import SwissVoteFile
+
+
+__all__ = (
+    'Actor',
+    'PolicyArea',
+    'Principal',
+    'SwissVote',
+    'SwissVoteFile',
+    'TranslatablePage',
+)
