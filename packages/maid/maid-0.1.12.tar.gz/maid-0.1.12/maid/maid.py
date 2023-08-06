a112ae6b369ae35f61e1c0d994273ca47b1b9018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:04 +0000

    Add package maid/0.1.12/maid-0.1.12.tar.gz

---
+++ packages/maid/maid-0.1.12.tar.gz/maid-0.1.12/maid/maid.py
@@ -0,0 +1,88 @@
+# Libraries
+import argparse
+
+# Modules
+from .version import __version__
+from .core import get, rem, query, clear, list_formats
+
+# Argument parsing
+
+
+def main():
+    # Declare Argument Parser
+    parser = argparse.ArgumentParser(
+        prog='maid',
+        description=f'Maid - Package Manager - Alpha {__version__}')
+    # TODO: Add epilog
+
+    # --version argument
+    parser.add_argument(
+        '--version',
+        action='version',
+        version=__version__)
+
+    # TODO: --force, --verbose argument
+    # parser.add_argument(
+    #     '--force',
+    #     action='store_true'
+    # )
+
+    # Add subparser to main Parser
+    subparsers = parser.add_subparsers(
+        help="Maid's sub-command",
+        title='command',
+        required=True,
+        metavar='<command>')
+
+    # get sub-command
+    get_parse = subparsers.add_parser(
+        'get', help='Get package from url/local path')
+    get_parse.add_argument(
+        'package',
+        type=str,
+        help=f'Url or path to package',
+        metavar='<url/path>')
+    # nargs='+'
+    get_parse.set_defaults(func=get)
+
+    # rem sub-command
+    rem_parse = subparsers.add_parser(
+        'rem', help='Remove package from storage')
+    rem_parse.add_argument(
+        'package',
+        type=str,
+        help='Package name',
+        metavar='<pkg_name>')
+    # nargs='+'
+    rem_parse.set_defaults(func=rem)
+
+    # query sub-command
+    que_parse = subparsers.add_parser(
+        'que', help='Query package from storage')
+    que_parse.add_argument(
+        'package',
+        type=str,
+        help='Package name',
+        metavar='<pkg_name>',
+        nargs='?')
+    que_parse.set_defaults(func=query, package='.*?')
+
+    # clear sub-command
+    cle_parse = subparsers.add_parser(
+        'cle', help='Clear temporary package downloads')
+    cle_parse.set_defaults(func=clear)
+
+    # arc-support sub-command
+    sup_parse = subparsers.add_parser(
+        'sup', help='Show supported archive format on this machine')
+    sup_parse.set_defaults(func=list_formats)
+
+    # Call argument parser
+    args = parser.parse_args()
+    # Checking for argument
+    # If no argument -> clear
+    # else           -> other function
+    if 'package' in args:
+        args.func(args.package)
+    else:
+        args.func()
---
+++ packages/maid/maid-0.1.12.tar.gz/maid-0.1.12/maid/run.py
@@ -0,0 +1,61 @@
+import os
+from winshell import shortcut
+from .config import maidPackDir, maidBinDir
+
+
+def bin_walk(pkg_name, fun):
+    """Recursively browse for executables from pkg_name directory"""
+
+    print("""Searching for binaries & executables ...""")
+    # Maximum traversal depth
+    MAX_DEPTH = 2
+    # Generate location to search
+    src = os.path.join(os.fsdecode(maidPackDir), pkg_name) + '\\'
+    # Use os.walk to browse recursively
+    for path, dirs, files in os.walk(src):
+        # Check for traversal depth
+        if path[len(src) + 1:].count(os.sep) < MAX_DEPTH:
+            # Browsing through file in directory
+            for file in files:
+                # Filter executables
+                if os.path.splitext(file)[-1] in ['.exe', '.reg', '.bat', '.cmd']:
+                    print("Found:", os.path.join(path, file))
+                    fun(path, file)
+
+
+def bin_search(pkg_name):
+    """Search binaries and executables then hard-link to bin/ folder"""
+
+    bin_walk(pkg_name, linkBin)
+
+
+def link_rem(pkg_name):
+    """Search hard-link of binaries and executables then delete"""
+
+    bin_walk(pkg_name, unlinkBin)
+
+
+def linkBin(path, file):
+    # This is file that need to be linked
+    binary = os.path.join(path, file)
+    # This is where file will be linked to
+    link = os.path.join(os.fsdecode(maidBinDir), file)
+    link = str(link) + ".lnk"
+    # Try to link shortcut
+    scut = shortcut(binary)
+    scut.write(link)
+    print("\t->", link)
+
+
+def unlinkBin(path, file):
+    # This is where file will be unlinked
+    link = os.path.join(os.fsdecode(maidBinDir), file)
+    link = str(link) + ".lnk"
+    # Check if link is available in bin/ folder
+    if os.path.isfile(link):
+        # If yes, delete it
+        print("Unlink:", link)
+        os.remove(link)
+    else:
+        # Else, inform "Missing link"
+        print("Missing link:", link)
