4f07c175c8681f2fae3d7fb5510af46d34cdbe4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:23 +0000

    Add package 12306-booking/0.1.7/12306-booking-0.1.7.tar.gz

---
+++ packages/12306-booking/12306-booking-0.1.7.tar.gz/12306-booking-0.1.7/booking/user.py
@@ -0,0 +1,27 @@
+# encoding: utf8
+"""
+user.py
+@author Meng.yangyang
+@description User
+@created Wed Jan 09 2019 21:17:23 GMT+0800 (CST)
+@last-modified Sat Jan 12 2019 11:53:36 GMT+0800 (CST)
+"""
+
+import json
+import logging
+from hack12306.user import TrainUserAPI
+
+from . import settings
+
+__all__ = ('user_passengers',)
+
+_logger = logging.getLogger('booking')
+
+
+def user_passengers():
+    """
+    User passengers
+    """
+    passengers = TrainUserAPI().user_passengers(cookies=settings.COOKIES)
+    _logger.debug(json.dumps(passengers, ensure_ascii=False))
+    return passengers
