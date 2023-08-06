43ef3bf3d3e1785cc65dbda0fb30c22727915d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:58 +0000

    Add package django-modeltrans/0.3.4/django-modeltrans-0.3.4.tar.gz

---
+++ packages/django-modeltrans/django-modeltrans-0.3.4.tar.gz/django-modeltrans-0.3.4/modeltrans/apps.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import django.apps
+from django.apps import AppConfig
+
+from .conf import check_fallback_chain
+from .translator import translate_model
+
+
+class RegistrationConfig(AppConfig):
+    name = "modeltrans"
+    verbose_name = "Django modeltrans using a registry."
+
+    def ready(self):
+        check_fallback_chain()
+
+        for Model in django.apps.apps.get_models():
+            translate_model(Model)
