87ae749523a0445b6b39efa075576112f2ed03b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:08 +0000

    Add package rs4/0.2/rs4-0.2.tar.gz

---
+++ packages/rs4/rs4-0.2.tar.gz/rs4-0.2/rs4/__init__.py
@@ -0,0 +1,37 @@
+# 2016. 1. 10 by Hans Roh hansroh@gmail.com
+
+__version__ = "0.2"
+
+version_info = tuple (map (lambda x: not x.isdigit () and x or int (x),  __version__.split (".")))
+
+from multiprocessing import Pool
+from tqdm import tqdm as tqdm_
+import concurrent.futures
+from colorama import Fore
+from functools import partial
+from .termcolor import tc
+import subprocess as subprocess_
+import types
+
+def tqdm (iterable, desc = "", color = None, **karg):
+    if color:
+        return tqdm_ (iterable, desc, bar_format = "{l_bar}%s{bar}%s{r_bar}" % (getattr (Fore, color.upper ()), Fore.RESET), **karg)
+    else:
+        return tqdm_ (iterable, desc, **karg)
+
+# Abbreviations ----------------------------------------------------
+def thread (workers = 1):
+    return concurrent.futures.ThreadPoolExecutor (max_workers = workers)
+
+def process (workers = 1):
+    return concurrent.futures.ProcessPoolExecutor (max_workers = workers)
+    
+def subprocess (cmd, *args):
+    if type (cmd) in (types.FunctionType, types.MethodType, types.BuiltinFunctionType, types.BuiltinMethodType):
+        with Pool (1) as p:
+            return p.apply (cmd, args)
+    res = subprocess_.run(cmd, shell = True, stdout = subprocess_.PIPE, stderr = subprocess_.PIPE)    
+    out, err = res.stdout.decode ("utf8"), res.stderr.decode ("utf8")
+    if err:
+        raise RuntimeError (err)
+    return out
