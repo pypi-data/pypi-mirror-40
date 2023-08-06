9928d30af398dfd5cb5e1bd8c5363c9e6cec3d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:37 +0000

    Add package dials-data/0.2.0/dials_data-0.2.0-py2.py3-none-any.whl

---
+++ packages/dials-data/dials_data-0.2.0-py2.py3-none-any.whl/dials_data/download.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, division, print_function
+
+
+class DataFetcher:
+    """
+       Do not use this class directly in tests! Use the regression_data fixture.
+    """
+
+    def __init__(self, target_dir=None, read_only=False):
+        pass
