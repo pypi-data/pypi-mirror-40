feb7db2c37e7308ea0576187a8b92e4e19eb6548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:52 +0000

    Add package dobby/1.2/dobby-1.2-py3-none-any.whl

---
+++ packages/dobby/dobby-1.2-py3-none-any.whl/dobby/events/base/adapters/base_request_adapter.py
@@ -0,0 +1,10 @@
+class BaseRequestAdapter:
+    """A base class for all request adapter"""
+
+    def __init__(self):
+        pass
+
+    def translate(self, event, context):
+        """The translation method that map event and context to the correspondent request type. """
+
+        raise NotImplementedError
