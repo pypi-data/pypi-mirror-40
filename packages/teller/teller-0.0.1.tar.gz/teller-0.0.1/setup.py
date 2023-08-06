2365dfa4783ff134a27e083630f5d2e671f634f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:51 +0000

    Add package teller/0.0.1/teller-0.0.1.tar.gz

---
+++ packages/teller/teller-0.0.1.tar.gz/teller-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='teller',
+    version='0.0.1',
+    author='wrmsr',
+    author_email='timwilloney@gmail.com',
+    description='teller',
+    url='https://github.com/wrmsr/teller',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+)
