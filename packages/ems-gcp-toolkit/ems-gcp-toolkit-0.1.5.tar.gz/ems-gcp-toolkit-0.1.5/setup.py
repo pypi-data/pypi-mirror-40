e9b21726485c30e638778113ffae894fb5d80057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:06 +0000

    Add package ems-gcp-toolkit/0.1.5/ems-gcp-toolkit-0.1.5.tar.gz

---
+++ packages/ems-gcp-toolkit/ems-gcp-toolkit-0.1.5.tar.gz/ems-gcp-toolkit-0.1.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="ems-gcp-toolkit",
+    version="0.1.5",
+    packages=["bigquery"],
+    url="https://github.com/emartech/ems-gcp-toolkit",
+    license="MIT",
+    author="Emarsys",
+    author_email="",
+    description="",
+    install_requires=[
+        "google-cloud-bigquery==1.5.0"
+    ],
+)
