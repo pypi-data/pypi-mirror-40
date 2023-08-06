82993118227a7a269f65bd6b41a2ea417b58a869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:26 +0000

    Add package pywemo/0.4.35/pywemo-0.4.35.tar.gz

---
+++ packages/pywemo/pywemo-0.4.35.tar.gz/pywemo-0.4.35/setup.py
@@ -0,0 +1,16 @@
+"""pyWeMo setup script."""
+
+from setuptools import setup, find_packages
+
+CONST_DESC = 'Lightweight Python module to discover and control WeMo devices'
+
+setup(name='pywemo',
+      version='0.4.35',
+      description=CONST_DESC,
+      url='http://github.com/pavoni/pywemo',
+      author='Greg Dowling',
+      author_email='mail@gregdowling.com',
+      license='MIT',
+      install_requires=['netifaces>=0.10.0', 'requests>=2.0', 'six>=1.10.0'],
+      packages=find_packages(),
+      zip_safe=True)
