a4056db04605c4552cfccb5ac7d125eef82abe73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:59 +0000

    Add package cognitojwt/1.0.4/cognitojwt-1.0.4.tar.gz

---
+++ packages/cognitojwt/cognitojwt-1.0.4.tar.gz/cognitojwt-1.0.4/cognitojwt/token_utils.py
@@ -0,0 +1,31 @@
+import time
+
+from jose import jwt
+
+from .exceptions import CognitoJWTException
+
+
+def get_unverified_headers(token: str) -> dict:
+    return jwt.get_unverified_headers(token)
+
+
+def get_unverified_claims(token: str) -> dict:
+    return jwt.get_unverified_claims(token)
+
+
+def check_expired(exp: int, testmode: bool = False) -> None:
+    if time.time() > exp and not testmode:
+        raise CognitoJWTException('Token is expired')
+
+
+def check_aud(aud: str, app_client_id: str) -> None:
+    if aud != app_client_id:
+        raise CognitoJWTException('Token was not issued for this client id')
+
+
+__all__ = [
+    'get_unverified_headers',
+    'get_unverified_claims',
+    'check_expired',
+    'check_aud'
+]
