c77baf533167d72e50d34a2d15ed96159b1a91d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:23 +0000

    Add package sense-core/0.1.3/sense_core-0.1.3-py3-none-any.whl

---
+++ packages/sense-core/sense_core-0.1.3-py3-none-any.whl/sense_core/utils.py
@@ -0,0 +1,54 @@
+import numpy
+import json
+import datetime
+import time
+from dateutil import parser
+
+
+def dump_json(data):
+    return json.dumps(data, ensure_ascii=False)
+
+
+def load_json(data):
+    try:
+        return json.loads(data)
+    except:
+        return None
+
+
+def sleep(seconds):
+    time.sleep(seconds)
+
+
+def parse_date(str):
+    return parser.parse(str)
+
+
+def timestamp_to_str(time1, format='%Y-%m-%d'):
+    return datetime.datetime.fromtimestamp(time1).strftime(format)
+
+
+def get_current_millisecond():
+    return int(round(time.time() * 1000))
+
+
+def get_current_second():
+    return int(round(time.time()))
+
+
+def is_debug():
+    return sd.config('settings', 'debug', '0') == '1'
+
+
+def format_time_cost(val):
+    result = ''
+    if val >= 3600:
+        hour = int(val / 3600)
+        result += str(hour) + '小时'
+        val -= hour * 3600
+    if val >= 60:
+        min = int(val / 60)
+        result += str(min) + '分'
+        val -= min * 60
+    result += str(val) + '秒'
+    return result
