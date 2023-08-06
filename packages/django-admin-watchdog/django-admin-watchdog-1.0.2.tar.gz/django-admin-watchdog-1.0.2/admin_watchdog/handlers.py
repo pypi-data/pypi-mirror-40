19a62c9342d5263606022575749b4e12df13bdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:12 +0000

    Add package django-admin-watchdog/1.0.2/django-admin-watchdog-1.0.2.tar.gz

---
+++ packages/django-admin-watchdog/django-admin-watchdog-1.0.2.tar.gz/django-admin-watchdog-1.0.2/admin_watchdog/handlers.py
@@ -0,0 +1,48 @@
+import logging
+
+from django.views.debug import ExceptionReporter, get_exception_reporter_filter
+
+
+class AdminWatchdogHandler(logging.Handler):
+    """An exception log handler that register exception for the site backend.
+    """
+
+    def __init__(self):
+        logging.Handler.__init__(self)
+
+    def emit(self, record):
+        from admin_watchdog.models import LogEntry
+
+        # Get request specific info (location, ...)
+        try:
+            request = record.request
+            filter = get_exception_reporter_filter(request)
+            request_repr = filter.get_request_repr(request)
+        except AttributeError:
+            request = None
+            request_repr = u"unavailable"
+
+        LogEntry(
+            levelname=record.levelname,
+            shortmessage=record.getMessage(),
+            message=self.format(record),
+            request_repr=request_repr,
+        ).save()
+
+        # May be used for more advanced logging
+        """
+        # Get ExceptionReporter parameters
+        if record.exc_info:
+            exc_info = record.exc_info
+        else:
+            exc_info = (None, record.getMessage(), None)
+
+
+        message = "%s\n\nRequest repr(): %s" % (
+            self.format(record), request_repr
+        )
+        reporter = ExceptionReporter(request, is_email=True, *exc_info)
+        html_message = reporter.get_traceback_html()
+        text_message = reporter.get_traceback_text()
+        """
+
