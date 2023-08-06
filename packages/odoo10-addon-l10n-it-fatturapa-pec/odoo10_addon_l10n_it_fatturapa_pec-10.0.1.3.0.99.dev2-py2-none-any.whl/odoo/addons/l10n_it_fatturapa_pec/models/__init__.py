c1d9cd2f80517dadaec9867b5e4d3a765b88c9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:54 +0000

    Add package odoo10-addon-l10n-it-fatturapa-pec/10.0.1.3.0.99.dev2/odoo10_addon_l10n_it_fatturapa_pec-10.0.1.3.0.99.dev2-py2-none-any.whl

---
+++ packages/odoo10-addon-l10n-it-fatturapa-pec/odoo10_addon_l10n_it_fatturapa_pec-10.0.1.3.0.99.dev2-py2-none-any.whl/odoo/addons/l10n_it_fatturapa_pec/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from . import account
+from . import fatturapa_attachment_out
+from . import mail_thread
+from . import fetchmail
