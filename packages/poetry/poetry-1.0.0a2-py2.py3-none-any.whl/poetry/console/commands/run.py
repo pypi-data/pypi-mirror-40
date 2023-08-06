6308f2dc41f7eb4d655ece5fb3c650a123b97661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:53 +0000

    Add package poetry/1.0.0a2/poetry-1.0.0a2-py2.py3-none-any.whl

---
+++ packages/poetry/poetry-1.0.0a2-py2.py3-none-any.whl/poetry/console/commands/run.py
@@ -0,0 +1,59 @@
+from .env_command import EnvCommand
+
+
+class RunCommand(EnvCommand):
+    """
+    Runs a command in the appropriate environment.
+
+    run
+        { args* : The command and arguments/options to run. }
+    """
+
+    def __init__(self):  # type: () -> None
+        from poetry.console.args.run_args_parser import RunArgsParser
+
+        super(RunCommand, self).__init__()
+
+        self.config.set_args_parser(RunArgsParser())
+
+    def handle(self):
+        args = self.argument("args")
+        script = args[0]
+        scripts = self.poetry.local_config.get("scripts")
+
+        if scripts and script in scripts:
+            return self.run_script(scripts[script], args)
+
+        return self.env.execute(*args)
+
+    def run_script(self, script, args):
+        if isinstance(script, dict):
+            script = script["callable"]
+
+        module, callable_ = script.split(":")
+
+        src_in_sys_path = "sys.path.append('src'); " if self._module.is_in_src() else ""
+
+        cmd = ["python", "-c"]
+
+        cmd += [
+            '"import sys; '
+            "from importlib import import_module; "
+            "sys.argv = {!r}; {}"
+            "import_module('{}').{}()\"".format(
+                args, src_in_sys_path, module, callable_
+            )
+        ]
+
+        return self.env.run(*cmd, shell=True, call=True)
+
+    @property
+    def _module(self):
+        from ...masonry.utils.module import Module
+
+        poetry = self.poetry
+        package = poetry.package
+        path = poetry.file.parent
+        module = Module(package.name, path.as_posix(), package.packages)
+
+        return module
