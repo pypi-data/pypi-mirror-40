89a52fbc6d71c33abb254797200e561eea7b8862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:29 +0000

    Add package omnibus/0.0.1/omnibus-0.0.1.tar.gz

---
+++ packages/omnibus/omnibus-0.0.1.tar.gz/omnibus-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='omnibus',
+    version='0.0.1',
+    author='wrmsr',
+    author_email='timwilloney@gmail.com',
+    description='omnibus',
+    url='https://github.com/wrmsr/omnibus',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+)
