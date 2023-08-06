b9c9947342fbe28f93b5a97d87b7d3369b039b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:25 +0000

    Add package intelex/0.0.23/intelex-0.0.23.tar.gz

---
+++ packages/intelex/intelex-0.0.23.tar.gz/intelex-0.0.23/tests/test_intelex.py
@@ -0,0 +1,15 @@
+from intelex import get_version, get_endpoint, get_apikey, _select_format
+import os
+
+def test_get_version():
+    assert get_version() == '0.0.23'
+
+def test_get_endpoint():
+    assert get_endpoint() == os.environ['ilx_endpoint']
+
+def test_get_aoikey():
+    assert get_apikey() == os.environ['ilx_apikey']
+
+def test_select_format():
+    select_list = ['Hello', 'World', 'List']
+    assert _select_format(select_list) == 'Hello, World, List'
