0d310d6f1c1ed5d7860565ec8087501ec886aeba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:10 +0000

    Add package bio2bel-wikipathways/0.2.2/bio2bel_wikipathways-0.2.2.tar.gz

---
+++ packages/bio2bel-wikipathways/bio2bel_wikipathways-0.2.2.tar.gz/bio2bel_wikipathways-0.2.2/src/bio2bel_wikipathways/constants.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Constants for Bio2BEL WikiPathways."""
+
+from bio2bel import get_data_dir
+
+VERSION = '0.2.2'
+
+MODULE_NAME = 'wikipathways'
+DATA_DIR = get_data_dir(MODULE_NAME)
+
+HGNC = 'HGNC'
+WIKIPATHWAYS = 'WIKIPATHWAYS'
+
+# Human gene sets
+HOMO_SAPIENS_GENE_SETS = 'http://data.wikipathways.org/20180510/gmt/wikipathways-20180510-gmt-Homo_sapiens.gmt'
