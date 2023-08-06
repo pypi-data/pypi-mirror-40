f5eeec01450f439151e58631d7f2b69684d84c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:24 +0000

    Add package robotframework-seleniumscreenshots/0.9.4/robotframework-seleniumscreenshots-0.9.4.tar.gz

---
+++ packages/robotframework-seleniumscreenshots/robotframework-seleniumscreenshots-0.9.4.tar.gz/robotframework-seleniumscreenshots-0.9.4/CHANGELOG.rst
@@ -0,0 +1,36 @@
+Changelog
+=========
+
+0.9.4 (2019-01-06)
+------------------
+
+- Remove trailing slash from resources to fix build on Windows
+  [bollwyvl]
+
+0.9.3 (2019-01-05)
+------------------
+
+- Fix issue where support for name:-selector was broken
+  [datakurre]
+
+- Fix issue where capture and crop page screenshot did not return filename of
+  the captured screenshot
+  [datakurre]
+
+0.9.2 (2019-01-01)
+------------------
+
+- Update README and documentation
+  [datakurre]
+
+0.9.1 (2019-01-01)
+------------------
+
+- Update README
+  [datakurre]
+
+0.9.0 (2019-01-01)
+------------------
+
+- Forked from robotframework-selenium2screenshots
+  [datakurre]
---
+++ packages/robotframework-seleniumscreenshots/robotframework-seleniumscreenshots-0.9.4.tar.gz/robotframework-seleniumscreenshots-0.9.4/setup.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/76aafbf4bf4992c82da41ccefd8c6d481744524c.tar.gz";
+    sha256 = "1xz8249i9yag4sxfzd5lkhqsyg7n6h3n2klb1cqqmcsav87f6x65";
+  }) { overlays = []; }
+, setup ? import (fetchTarball {
+    url = "https://github.com/datakurre/setup.nix/archive/9f8529e003ea4d2f433d2999dc50b8938548e7d0.tar.gz";
+    sha256 = "15qzhz28jvgkna5zv7pj0gfnd0vcvafpckxcp850j64z7761apnm";
+ })
+, python ? "python3"
+, pythonPackages ? builtins.getAttr (python + "Packages") pkgs
+, requirements ? ./. + "/requirements-${python}.nix"
+}:
+
+let overrides = self: super: {
+  "flake8" = super."flake8".overrideDerivation(old: {
+    patches = [];
+  });
+  "robotframework" = super."robotframework".overridePythonAttrs(old: {
+    buildInputs = [ pkgs.unzip ];
+  });
+  "testfixtures" = super."testfixtures".overrideDerivation(old: {
+    patches = [];
+  });
+  "selenium" = super."selenium".overrideDerivation(old: {
+    # patch name to tweak around bad archive at cachix
+    name = "${old.name}-2019-01-01";
+  });
+}; in
+
+setup {
+  inherit pkgs pythonPackages overrides requirements;
+  src = ./.;
+  propagatedBuildInputs = with pkgs; [
+    geckodriver
+    firefox
+  ];
+}
