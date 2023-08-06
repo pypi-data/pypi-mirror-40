bf0be8308fc7f0fc20581c953c1f9201f44d5826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:54 +0000

    Add package mbq.tokens/1.0.0/mbq.tokens-1.0.0.tar.gz

---
+++ packages/mbq.tokens/mbq.tokens-1.0.0.tar.gz/mbq.tokens-1.0.0/mbq/tokens/__init__.py
@@ -0,0 +1,23 @@
+from .decoder import Decoder
+from .exceptions import TokenError  # noqa
+from .__version__ import (  # noqa
+    __author__,
+    __author_email__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
+
+
+def init(certificate=None, allowed_audiences=None):
+    decoder = Decoder(
+        certificate=certificate,
+        allowed_audiences=allowed_audiences,
+    )
+
+    global decode, decode_header, decode_with_unknown_audience
+    decode = decoder.decode
+    decode_header = decoder.decode_header
+    decode_with_unknown_audience = decoder.decode_with_unknown_audience
