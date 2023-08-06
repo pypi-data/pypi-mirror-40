106de3f0ab37989380ce88e3fa1e505b59646c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:49 +0000

    Add package inflame/0.8.0/inflame-0.8.0.tar.gz

---
+++ packages/inflame/inflame-0.8.0.tar.gz/inflame-0.8.0/inflame/__init__.py
@@ -0,0 +1,30 @@
+"""
+Fit a ConvNet to training sample and output its accuracy on holdout sample.
+"""
+from .data import data_pipeline
+from .models.news import ConvNet    # .news customizable to another model
+from .train import train_model
+
+def build_model(data, target, config):
+   '''API that takes raw data in lists and returns a pytorch model object that can
+   be used to predict the target for new tensors.
+   '''
+   ## Shape data into pytorch Datasets (batch data according to loader.py to reducing RAM use).
+   training_batches, validati_batches = data_pipeline(data, 
+                                                      target
+                                                     )
+   ## Call generic train_model wrapper with the ConvNet model type.                                                   
+   model = train_model(training_batches,
+                       validati_batches,
+                       ConvNet(input_length = config['input_length'], # similar to pixels in vision
+                               channels = config['channels'],
+                               kernel_size = config['kernel_size'],
+                               stride = config['stride'],
+                               padding = int((config['kernel_size'] - 1) / 2), # constrained to fix spatial size during convolution
+                               hidden_layer_nodes = config['hidden_layer_nodes'],
+                               output_layer_nodes = config['output_layer_nodes']
+                              ),
+                       config['learn_rate'],
+                       config['n_epochs']
+                      )
+   return model
