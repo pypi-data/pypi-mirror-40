9aca35dc8c40727262504d4347e3a0a5d9dacf94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:54 +0000

    Add package fn-mdowds/0.8/fn-mdowds-0.8.tar.gz

---
+++ packages/fn-mdowds/fn-mdowds-0.8.tar.gz/fn-mdowds-0.8/CHANGELOG.rst
@@ -0,0 +1,9 @@
+Changelog
+=========
+
+(v0.5.0) July 17, 2017
+----------------------
+
+Commit: `10efa8b <https://github.com/fnpy/fn.py/commit/10efa8b35c327ae77dfb01878451694bd5a47ea9>`_.
+
+- Added ``recur.stackless()`` Provides a "stackless" (constant Python stack space) recursion decorator for generators.
---
+++ packages/fn-mdowds/fn-mdowds-0.8.tar.gz/fn-mdowds-0.8/fn/uniform.py
@@ -0,0 +1,21 @@
+from sys import version_info
+
+# Syntax sugar to deal with Python 2/Python 3
+# differences: this one will return generator
+# even in Python 2.*
+if version_info[0] == 2:
+    from itertools import izip as zip, imap as map
+
+zip = zip
+
+if version_info[0] == 3:
+    from functools import reduce
+
+reduce = reduce
+
+if version_info[0] == 2:
+    from itertools import ifilterfalse as filterfalse
+    from itertools import izip_longest as zip_longest
+else:
+    from itertools import filterfalse
+    from itertools import zip_longest
