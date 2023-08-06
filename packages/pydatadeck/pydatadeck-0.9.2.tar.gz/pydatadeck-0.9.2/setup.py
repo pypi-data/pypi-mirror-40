c98b9de583e5669bd618b920faf1db4d825d1b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:34 +0000

    Add package pydatadeck/0.9.2/pydatadeck-0.9.2.tar.gz

---
+++ packages/pydatadeck/pydatadeck-0.9.2.tar.gz/pydatadeck-0.9.2/setup.py
@@ -0,0 +1,32 @@
+"""
+Pypi Setup script
+"""
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(name='pydatadeck',
+      version='0.9.2',
+      description='Python SDK for Datadeck',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3.7'
+      ],
+      keywords='datadeck',
+      url='https://www.datadeck.com',
+      author='Datadeck Dev',
+      author_email='datadeck_dev@ptmind.com',
+      license='MIT',
+      packages=['pydatadeck'],
+      install_requires=[
+          'flask==1.0.2',
+          'pyyaml==3.13'
+      ],
+      include_package_data=True,
+      zip_safe=False)
