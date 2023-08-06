53fd48e92701a8f3b11a1f2c92d78e5ddd9a1d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:27 +0000

    Add package sig-process-arrays/1.0/sig_process_arrays-1.0.tar.gz

---
+++ packages/sig-process-arrays/sig_process_arrays-1.0.tar.gz/sig_process_arrays-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sig_process_arrays',
+    version='1.0',
+    packages=['sig_process_arrays'],
+    url='',
+    license='MIT license',
+    author='Sammy Kolpinizki',
+    author_email='sammykol@gmail.com',
+    description='Plots magnitude of beam response for 1D receiver arrays - This is a "Portfolio" exercise from the Udacity Nano Degree in Datasciene '
+)
