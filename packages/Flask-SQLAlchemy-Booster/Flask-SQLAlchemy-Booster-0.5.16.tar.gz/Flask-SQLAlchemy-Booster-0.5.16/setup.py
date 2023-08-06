bf6984ad1e9bf56355c8b872306a19e8af147e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:07 +0000

    Add package Flask-SQLAlchemy-Booster/0.5.16/Flask-SQLAlchemy-Booster-0.5.16.tar.gz

---
+++ packages/Flask-SQLAlchemy-Booster/Flask-SQLAlchemy-Booster-0.5.16.tar.gz/Flask-SQLAlchemy-Booster-0.5.16/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup
+
+setup(
+    name='Flask-SQLAlchemy-Booster',
+    version='0.5.16',
+    description='A booster package for flask and sqlalchemy',
+    long_description='Allows querying on Model classes, supports several common query operations, allows JSONification of models and relations and provides a simple query language for dynamically fetching data',
+    packages=['flask_sqlalchemy_booster'],
+    include_package_data=True,
+    install_requires=[
+        "toolspy>=0.2.30",
+        "Flask>=0.10.1",
+        "SQLAlchemy>=1.1.11",
+        "Flask-SQLAlchemy>=2.3.2",
+        "Schemalite>=0.1.23"
+    ],
+    license='MIT',
+    url='https://github.com/inkmonk/flask-sqlalchemy-booster',
+    author='SuryaSankar',
+    author_email='suryashankar.m@gmail.com')
