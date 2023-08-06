d3750c72a8a1169259b45089a332cf8440af5e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:00 +0000

    Add package datasketch/1.4.1/datasketch-1.4.1.tar.gz

---
+++ packages/datasketch/datasketch-1.4.1.tar.gz/datasketch-1.4.1/datasketch/__init__.py
@@ -0,0 +1,13 @@
+from datasketch.hyperloglog import HyperLogLog, HyperLogLogPlusPlus
+from datasketch.minhash import MinHash
+from datasketch.b_bit_minhash import bBitMinHash
+from datasketch.lsh import MinHashLSH
+from datasketch.weighted_minhash import WeightedMinHash, WeightedMinHashGenerator
+from datasketch.lshforest import MinHashLSHForest
+from datasketch.lshensemble import MinHashLSHEnsemble
+from datasketch.lean_minhash import LeanMinHash
+from datasketch.hashfunc import sha1_hash32
+
+# Alias
+WeightedMinHashLSH = MinHashLSH
+WeightedMinHashLSHForest = MinHashLSHForest
