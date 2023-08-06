27d3a95d813e375359226899404d714b52bae36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:04 +0000

    Add package instrbuilder/0.0.12/instrbuilder-0.0.12.tar.gz

---
+++ packages/instrbuilder/instrbuilder-0.0.12.tar.gz/instrbuilder-0.0.12/instrbuilder/examples/oscilloscope.py
@@ -0,0 +1,19 @@
+# Lucas J. Koerner
+# 05/2018
+# koerner.lucas@stthomas.edu
+# University of St. Thomas
+
+from instrbuilder.instrument_opening import open_by_name
+import time
+
+osc = open_by_name(name='msox_scope')  # name within the configuration file (config.yaml)
+
+osc.set('time_range', 1e-3)
+osc.set('chan_scale', 0.8, configs={'chan': 1})
+osc.set('chan_offset', -0.2, configs={'chan': 1})
+
+osc.set('single_acq')
+time.sleep(0.1)
+
+# save a PNG screen-shot to host computer
+t = osc.save_display_data('test')
