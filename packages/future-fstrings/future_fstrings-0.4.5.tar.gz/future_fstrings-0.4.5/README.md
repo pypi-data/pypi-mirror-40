e7b7b6d881cae2873052c5358b6f92e4de89d1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:43 +0000

    Add package future-fstrings/0.4.5/future_fstrings-0.4.5.tar.gz

---
+++ packages/future-fstrings/future_fstrings-0.4.5.tar.gz/future_fstrings-0.4.5/README.md
@@ -0,0 +1,61 @@
+[![Build Status](https://travis-ci.org/asottile/future-fstrings.svg?branch=master)](https://travis-ci.org/asottile/future-fstrings)
+[![Coverage Status](https://coveralls.io/repos/github/asottile/future-fstrings/badge.svg?branch=reimplement_string_parser)](https://coveralls.io/github/asottile/future-fstrings?branch=reimplement_string_parser)
+
+future-fstrings
+===============
+
+A backport of fstrings to python<3.6.
+
+
+## Installation
+
+`pip install future-fstrings`
+
+
+## Usage
+
+Include the following encoding cookie at the top of your file (this replaces
+the utf-8 cookie if you already have it):
+
+```python
+# -*- coding: future_fstrings -*-
+```
+
+And then write python3.6 fstring code as usual!
+
+```python
+# -*- coding: future_fstrings -*-
+thing = 'world'
+print(f'hello {thing}')
+```
+
+```console
+$ python2.7 main.py
+hello world
+```
+
+## Showing transformed source
+
+`future-fstrings` also includes a cli to show transformed source.
+
+```console
+$ future-fstrings-show main.py
+# -*- coding: future_fstrings -*-
+thing = 'world'
+print('hello {}'.format((thing)))
+```
+
+## How does this work?
+
+`future-fstrings` has two parts:
+
+1. A utf-8 compatible `codec` which performs source manipulation
+    - The `codec` first decodes the source bytes using the UTF-8 codec
+    - The `codec` then leverages
+      [tokenize-rt](https://github.com/asottile/tokenize-rt) to rewrite
+      f-strings.
+2. A `.pth` file which registers a codec on interpreter startup.
+
+## you may also like
+
+- [future-breakpoint](https://github.com/asottile/future-breakpoint)
---
+++ packages/future-fstrings/future_fstrings-0.4.5.tar.gz/future_fstrings-0.4.5/setup.py
@@ -0,0 +1,71 @@
+import distutils
+import os.path
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+
+PTH = (
+    'try:\n'
+    '    import future_fstrings\n'
+    'except ImportError:\n'
+    '    pass\n'
+    'else:\n'
+    '    future_fstrings.register()\n'
+)
+
+
+class install(_install):
+    def initialize_options(self):
+        _install.initialize_options(self)
+        # Use this prefix to get loaded as early as possible
+        name = 'aaaaa_' + self.distribution.metadata.name
+
+        contents = 'import sys; exec({!r})\n'.format(PTH)
+        self.extra_path = (name, contents)
+
+    def finalize_options(self):
+        _install.finalize_options(self)
+
+        install_suffix = os.path.relpath(
+            self.install_lib, self.install_libbase,
+        )
+        if install_suffix == '.':
+            distutils.log.info('skipping install of .pth during easy-install')
+        elif install_suffix == self.extra_path[1]:
+            self.install_lib = self.install_libbase
+            distutils.log.info(
+                "will install .pth to '%s.pth'",
+                os.path.join(self.install_lib, self.extra_path[0]),
+            )
+        else:
+            raise AssertionError(
+                'unexpected install_suffix',
+                self.install_lib, self.install_libbase, install_suffix,
+            )
+
+
+setup(
+    name='future_fstrings',
+    description='A backport of fstrings to python<3.6',
+    url='https://github.com/asottile/future-fstrings',
+    version='0.4.5',
+    author='Anthony Sottile',
+    author_email='asottile@umich.edu',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    extras_require={':python_version<"3.6"': ['tokenize-rt']},
+    py_modules=['future_fstrings'],
+    entry_points={'console_scripts': [
+        'future-fstrings-show=future_fstrings:main',
+    ]},
+    cmdclass={'install': install},
+)
