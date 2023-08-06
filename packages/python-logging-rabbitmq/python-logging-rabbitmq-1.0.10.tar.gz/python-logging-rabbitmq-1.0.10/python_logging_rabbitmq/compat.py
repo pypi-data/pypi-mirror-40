11bf2da6f118770d26ef19d9305d18bdb04fab75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:39 +0000

    Add package python-logging-rabbitmq/1.0.10/python-logging-rabbitmq-1.0.10.tar.gz

---
+++ packages/python-logging-rabbitmq/python-logging-rabbitmq-1.0.10.tar.gz/python-logging-rabbitmq-1.0.10/python_logging_rabbitmq/compat.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Thanks to https://github.com/lobziik/rlog
+import sys
+
+if sys.version_info[0] == 2:
+    text_type = unicode
+    from Queue import Queue as Queue
+else:
+    text_type = str
+    from queue import Queue as Queue
+
+try:
+    import ujson as json  # noqa: F401
+except ImportError:
+    import json  # noqa: F401
