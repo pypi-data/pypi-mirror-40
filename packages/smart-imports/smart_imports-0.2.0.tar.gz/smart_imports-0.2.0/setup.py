6b877a390d709e5c1965969cf9bd70e41db9ea92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:41 +0000

    Add package smart-imports/0.2.0/smart_imports-0.2.0.tar.gz

---
+++ packages/smart-imports/smart_imports-0.2.0.tar.gz/smart_imports-0.2.0/setup.py
@@ -0,0 +1,30 @@
+
+import setuptools
+
+setuptools.setup(
+    name='smart_imports',
+    version='0.2.0',
+    description='automatic importing for Python modules',
+    url='https://github.com/Tiendil/smart-imports',
+    author='Aleksey Yeletsky <Tiendil>',
+    author_email='a.eletsky@gmail.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+
+        'Natural Language :: English',
+        'Natural Language :: Russian'],
+    keywords=['python', 'import'],
+    packages=setuptools.find_packages(),
+    install_requires=[],
+
+    include_package_data=True)
