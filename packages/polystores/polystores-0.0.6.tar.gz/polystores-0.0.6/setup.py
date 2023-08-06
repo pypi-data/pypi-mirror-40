3a0970d14f7a5d158b1e399046b926201b0411e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:16 +0000

    Add package polystores/0.0.6/polystores-0.0.6.tar.gz

---
+++ packages/polystores/polystores-0.0.6.tar.gz/polystores-0.0.6/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(name='polystores',
+      version='0.0.6',
+      description='Polystores is an abstraction and a collection of clients '
+                  'to interact with cloud storages.',
+      maintainer='Mourad Mourafiq',
+      maintainer_email='mourad@polyaxon.com',
+      author='Mourad Mourafiq',
+      author_email='mourad@polyaxon.com',
+      url='https://github.com/polyaxon/polystores',
+      license='MIT',
+      platforms='any',
+      packages=find_packages(),
+      keywords=[
+          'polyaxon',
+          'aws',
+          's3',
+          'microsoft',
+          'azure',
+          'google cloud storage',
+          'gcs',
+          'machine-learning',
+          'data-science',
+          'artificial-intelligence',
+          'ai',
+          'reinforcement-learning',
+          'kubernetes',
+          'docker'
+      ],
+      install_requires=[
+          "rhea>=0.4.2",
+      ],
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ],
+      tests_require=[
+          "pytest",
+      ],
+      cmdclass={'test': PyTest})
