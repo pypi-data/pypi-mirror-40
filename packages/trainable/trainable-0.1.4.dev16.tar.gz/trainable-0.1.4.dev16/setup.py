35c9b98173a237daec7017e85e8543ab69d1e748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:13 +0000

    Add package trainable/0.1.4.dev16/trainable-0.1.4.dev16.tar.gz

---
+++ packages/trainable/trainable-0.1.4.dev16.tar.gz/trainable-0.1.4.dev16/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='trainable',
+    version='0.1.4.dev16',
+    description='The flexible training toolbox',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Jeff Hilton',
+    author_email='jeffhilton.code@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords='deep-learning dnn training torch',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['torch', 'torchvision', 'tqdm', 'matplotlib', 'numpy']
+)
