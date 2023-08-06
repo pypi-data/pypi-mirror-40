c1f29102afa92a7d7f1ebb946a39ba59e3c8492b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:57 +0000

    Add package itree/0.0.3/itree-0.0.3.tar.gz

---
+++ packages/itree/itree-0.0.3.tar.gz/itree-0.0.3/setup.py
@@ -0,0 +1,30 @@
+import io
+from setuptools import setup
+
+
+setup(
+    name='itree',
+    version='0.0.3',
+    url='',
+    license='MIT',
+    author='Bob Zimmermann',
+    author_email='robert.zimmermann@univie.ac.at',
+    description='An interval tree data structure',
+    long_description=io.open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        " Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+    packages=["itree"],
+    test_require=["pytest"]
+)
+
+
