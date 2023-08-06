5bbe5ff841bf5e26703ce98f174a0119c8a74d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:20 +0000

    Add package sc2monitor/0.2.6/sc2monitor-0.2.6.tar.gz

---
+++ packages/sc2monitor/sc2monitor-0.2.6.tar.gz/sc2monitor-0.2.6/setup.py
@@ -0,0 +1,26 @@
+# read the contents of README file
+from os import path
+
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='sc2monitor',
+      version='0.2.6',
+      description=('When executed regularly keeps track of medium'
+                   ' amount of StarCraft 2 accounts on the 1vs1 ladder'),
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/2press/sc2monitor',
+      author='pressure',
+      author_email='pres.sure@ymail.com',
+      license='MIT',
+      python_requires='>=3.7.1',
+      packages=['sc2monitor'],
+      install_requires=[
+          'PyMySQL',
+          'aiohttp'
+      ],
+      zip_safe=False)
