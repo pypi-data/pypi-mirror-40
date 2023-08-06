ea80b812726c1048856c2275408ad51ce9817c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:34 +0000

    Add package pyipv8/1.4/pyipv8-1.4.tar.gz

---
+++ packages/pyipv8/pyipv8-1.4.tar.gz/pyipv8-1.4/setup.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyipv8',
+    author='Tribler',
+    description='The Python implementation of the IPV8 library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='1.4',
+    url='https://github.com/Tribler/py-ipv8',
+    package_data={'': ['*.*']},
+    packages=find_packages(),
+    install_requires=[
+        "cryptography",
+        "libnacl",
+        "netifaces",
+        "Twisted",
+        "pyOpenSSL",
+        "six"
+    ]
+)
