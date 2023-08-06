bb0116528f14fec531c7ff432eabf13992d968f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:36 +0000

    Add package pickleDB/0.9.post1/pickleDB-0.9.post1.tar.gz

---
+++ packages/pickleDB/pickleDB-0.9.post1.tar.gz/pickleDB-0.9.post1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name="pickleDB",
+    version="0.9.post1",
+    description="A lightweight and simple database using json.",
+    author="Harrison Erd",
+    author_email="erdh@mail.broward.edu",
+    license="three-clause BSD",
+    url="http://github.com/patx/pickledb",
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Topic :: Database" ],
+    py_modules=['pickledb'],)
+
