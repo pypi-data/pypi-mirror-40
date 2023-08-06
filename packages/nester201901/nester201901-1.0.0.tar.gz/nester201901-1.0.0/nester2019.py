95c72d469ecba4990bfdf8742d9cffc108c252f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:12 +0000

    Add package nester201901/1.0.0/nester201901-1.0.0.tar.gz

---
+++ packages/nester201901/nester201901-1.0.0.tar.gz/nester201901-1.0.0/nester2019.py
@@ -0,0 +1,4 @@
+#这是一个注释
+print ("hello world")
+for num in range(6):
+    print(num)
---
+++ packages/nester201901/nester201901-1.0.0.tar.gz/nester201901-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup        #从python发布工具导入setup函数
+
+setup(
+            name               ='nester201901',
+            version            ='1.0.0',
+            py_modules         =['nester2019'],   #将模块的元数据与setup函数的参数关联
+            author             ='pengsaihu', 
+            author_email       ='pengsaihu@foxmail.com',
+            url                ='',
+            description        =''
+    )
