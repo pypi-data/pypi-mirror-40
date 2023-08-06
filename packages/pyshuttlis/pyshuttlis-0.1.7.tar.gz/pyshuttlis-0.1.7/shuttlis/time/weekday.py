e6d265d4a3ccd7febea83cc7b7e052033d3c98f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:36 +0000

    Add package pyshuttlis/0.1.7/pyshuttlis-0.1.7.tar.gz

---
+++ packages/pyshuttlis/pyshuttlis-0.1.7.tar.gz/pyshuttlis-0.1.7/shuttlis/time/weekday.py
@@ -0,0 +1,40 @@
+import calendar
+from datetime import datetime
+from enum import Enum
+from functools import total_ordering
+
+
+@total_ordering
+class WeekDay(Enum):
+    MONDAY = 'MONDAY'
+    TUESDAY = 'TUESDAY'
+    WEDNESDAY = 'WEDNESDAY'
+    THURSDAY = 'THURSDAY'
+    FRIDAY = 'FRIDAY'
+    SATURDAY = 'SATURDAY'
+    SUNDAY = 'SUNDAY'
+
+    @classmethod
+    def extract_from_datetime(cls, dt: datetime):
+        return cls[calendar.day_name[dt.weekday()].upper()]
+
+    @classmethod
+    def from_string(cls, weekday: str):
+        return cls[weekday.upper()]
+
+    def __str__(self):
+        return str(self.name)
+
+    def __lt__(self, other):
+        return _order.index(self) < _order.index(other)
+
+
+_order = [
+    WeekDay.MONDAY,
+    WeekDay.TUESDAY,
+    WeekDay.WEDNESDAY,
+    WeekDay.THURSDAY,
+    WeekDay.FRIDAY,
+    WeekDay.SATURDAY,
+    WeekDay.SUNDAY,
+]
