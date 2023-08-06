4962a9667d592339e03270cd9022c2da1fd6d150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:58 +0000

    Add package django-graphene-permissions/0.0.4/django_graphene_permissions-0.0.4.tar.gz

---
+++ packages/django-graphene-permissions/django_graphene_permissions-0.0.4.tar.gz/django_graphene_permissions-0.0.4/setup.py
@@ -0,0 +1,31 @@
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+about = {}
+
+with open(os.path.join("django_graphene_permissions", "__version__.py")) as f:
+    exec(f.read(), about)
+
+setuptools.setup(
+	name="django_graphene_permissions",
+	version=about["__version__"],
+	author="Taoufik Abbassid",
+	author_email="abacidtaoufik@gmail.com",
+	description="DRF like permission system for django graphene",
+	license='BSD',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/taoufik07/django-graphene-permissions",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"License :: OSI Approved :: BSD License",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3",
+		"Framework :: Django",
+		"Operating System :: OS Independent",
+	],
+)
