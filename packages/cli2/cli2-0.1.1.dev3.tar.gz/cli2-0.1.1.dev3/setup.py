300e39feba7e901f4104874c667462bc58acb4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:10 +0000

    Add package cli2/0.1.1.dev3/cli2-0.1.1.dev3.tar.gz

---
+++ packages/cli2/cli2-0.1.1.dev3.tar.gz/cli2-0.1.1.dev3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='cli2',
+    version='0.1.1.dev3',
+    setup_requires='setupmeta',
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/cli2',
+    include_package_data=True,
+    license='MIT',
+    keywords='cli',
+    python_requires='>=3',
+)
