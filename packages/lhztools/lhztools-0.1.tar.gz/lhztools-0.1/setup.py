904f5902b3e000f32582adf9db554f2ca5b9a12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:33 +0000

    Add package lhztools/0.1/lhztools-0.1.tar.gz

---
+++ packages/lhztools/lhztools-0.1.tar.gz/lhztools-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lhztools',
+      version='0.1',
+      description='Tools for LHZ',
+      url='https://github.com/WolfgangLechner/lhztools',
+      author='Wolfgang Lechner',
+      author_email='wolfgang.lechner@gmail.com',
+      license='MIT',
+      packages=['constraintoptimizer'],
+      zip_safe=False)
