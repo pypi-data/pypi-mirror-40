d09984d763022dd1e8c93aacfdd313b3d178a889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:05 +0000

    Add package MLB/0.0.3/MLB-0.0.3-py3-none-any.whl

---
+++ packages/MLB/MLB-0.0.3-py3-none-any.whl/MLB____esc/__init__.py
@@ -0,0 +1,12 @@
+from .mlb_tabular_2class import *
+
+__version__ = '0.0.3'
+
+__copyright__    = 'Copyright (C) 2019 Taketo Kimura'
+__version__      = '0.0.3'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Taketo Kimura'
+__author_email__ = 'taketo_kimura@micin.jp'
+__url__          = 'https://github.com/micin-jp/MLB'
+
+__all__          = ['mlb_tabular_2class']
