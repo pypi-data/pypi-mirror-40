9e04629da43787672b7c36208a1575b58989a136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:29 +0000

    Add package cnki2bib/0.0.3/cnki2bib-0.0.3.tar.gz

---
+++ packages/cnki2bib/cnki2bib-0.0.3.tar.gz/cnki2bib-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open(r'README.md',"r",encoding="utf8") as f:
+    long_description = f.read()
+
+setup(
+    name='cnki2bib',
+    version='0.0.3',
+    author='Vopaaz',
+    author_email="liyifan945@163.com",
+    url="https://github.com/Vopaaz/CNKI_2_BibTeX",
+    description='Converting the NoteExpress file (.net) exported by CNKI to BibTeX file (.bib)',
+    long_description = long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click','jieba','pyperclip','pypinyin'
+    ],
+    entry_points='''
+        [console_scripts]
+        cnki2bib=cnki2bibtex.cnki2bib:launch
+    ''',
+)
