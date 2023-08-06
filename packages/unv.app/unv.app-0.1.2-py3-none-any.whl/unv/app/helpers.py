d7c3eb84b4802656150c705822e435cbcc231a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:13 +0000

    Add package unv.app/0.1.2/unv.app-0.1.2-py3-none-any.whl

---
+++ packages/unv.app/unv.app-0.1.2-py3-none-any.whl/unv/app/helpers.py
@@ -0,0 +1,18 @@
+import os
+import importlib
+import pathlib
+
+from .settings import SETTINGS
+
+
+def get_app_components():
+    for component in SETTINGS['components']:
+        component = '{}.app'.format(component)
+        yield importlib.import_module(component)
+
+
+def get_project_root(app_module: str = 'app.settings') -> pathlib.Path:
+    """Return project root path, outside "src" directory."""
+    module_path = os.environ.get('SETTINGS', 'app.settings')
+    module = importlib.import_module(module_path)
+    return pathlib.Path(module.__file__).parent
