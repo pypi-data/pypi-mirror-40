28f6686e73bc3f6ae519070293d60ddff0cd7459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:17 +0000

    Add package fam/2.0.13/fam-2.0.13.tar.gz

---
+++ packages/fam/fam-2.0.13.tar.gz/fam-2.0.13/src/fam/tests/test_firestore/config_local.py
@@ -0,0 +1,18 @@
+
+API_KEY="AIzaSyBTZuSld9QfRcMXGkfyze9Ws2YQUEhfIzE"
+CREDS = {
+  "type": "service_account",
+  "project_id": "roughcutpro-test",
+  "private_key_id": "2014f5e387e3892379cbc59d8e79d23bf7ce8ebd",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCPfNtled6tXbET\nM8MZN2oKvj+pMP4vVzH5RmqWo+XOETL5FL5Uvc1hNTYwFHP9FCv51cfL9ZVWv5wA\nXskulefrrKvP2/ck84x8Md9Z5ny9oNvAsqQ1mw37rsSZjOGpRWec2tiRm6OfunKc\nbPa7/pgVjmZDAbnVp6H3x3uSHrTOMal3CmCzf0lceNA7OKmGje6fwG0nz8qKu75S\nw0pvEoT4H+/aWM1lnE6V02v0iyj/4cRBGzBcg7/3xE34qgb2S1qnTQg64rZMVjzB\n/JeOKdp/2oxm3C72iU6EDGhCKV+DdG0XeK0KBtNLVgtlQFQVZfC1oOe5s5NSfNgN\n+naMNWvxAgMBAAECggEAA3MNdS2OWF2ZvavvY0JNRs/XCPK4EZxHg8YZFuD+y8rn\nDkzc3H2pm84jLNm5Bhr/iaSFhpYzTmAR2IJu1LNe3XALu1X+5YEDXhL22xrq4cYl\nska5It7E4cmMSWK1JBQajw9FyI5Uyyi4y6Dk95UBw/bl53N5q7S5dJLzmgop75EB\n2mHfYF1UdyE7TpOstHPA9Cgm6k8e3qQzhskG3O3Ae7DeJe6xOBca2XTbaV0EIPbE\nlLsb5AL5YhhZkmgMPYVbQXisVRQp/jcNjSUrbBfLKO0p/wsPNazBt5w1243I5sKa\n2JvsCASW1ceNl2SRzC8EzaunE287O3nh2+3r7OD3JwKBgQDAFMlwh0mLvXonuYi4\nEgIX9BWzyaIQ99fc1yzfA/vKZ/gG9nT3fOesB97nrPx5VQiHOhl4hxIzIFzPSu4Z\n9L6GNCr41cO6fVmUKoz7r2sPZCEPV8HgTcFKY60NyV9zoZCLeE7Y/Y8XO/lx+Sik\nQIITH5nG6o6B/fM+TOv6eZ91wwKBgQC/PHBC6/5cIn8yU95edtPq1Wi+AU+TGk/9\nRToFYg1RmueCZIZ2KFkp+naw7Yz7OkLZLbqw6I7Qq0A6EShIEch/0kgIcsVHTc0r\nMyUY3K3agkh9mBI5F3DwpkZBFKxGTLmd/e5/UifQeT2qktHeBLECRxDWYB2Q7lBZ\nruS/szkYOwKBgEWXQmRCn6LBQ0ChN6d43slO5Y39v3nmUY7xZpBmIRN0F6huInKI\nDmeFcnz9b3uGE1Dq+89vIGjlkLqJqzo8Mcrtm6QLzp4CTDIiOHH3XBJ3dILaiKN7\niEwrUMdt7ec24eel7usu8mi4ked5HgBkXrwO9ufak0cFettXgyd8aQmBAoGBAIwC\nBrqoJqBzzSYeiwYj/0DFn5rEQOsU740g2rrIL7IYM/id+meVYLpAGhesw3PHEtwI\nA8+zY6PYTPx2RUSGaIFUhdJRnKd4iYZgOn+6STpZ+hKhfG36AIDfaz4IkZwiXh8S\no0Lp9iZGLWSteyMN0j41o1kgI2VRZNhUGDtVTq6zAoGAa27jgoS46PSelAtH4OO1\nzsWBXkKw1KI1N78hOwKI4Bv+wqiKRRl1+VmNq4FWeBXuU6aKldqyegiJRCVBjCaM\nYWQzOkiUFqv9FZzeA/QhQWJ8ddTOHRB0JE0zkiYjQBsJrY/QUZXopL2CCB7YNaNl\n0AeUEgHwF0E1v7h4ILpvoxM=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-gnqzp@roughcutpro-test.iam.gserviceaccount.com",
+  "client_id": "109793085269527288269",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-gnqzp%40roughcutpro-test.iam.gserviceaccount.com"
+}
+
+
+
+
