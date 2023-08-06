7bd21c152e9bd3139aa63becb2b1f2143503aeb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:23 +0000

    Add package pipe-nonblock/0.4/pipe_nonblock-0.4.tar.gz

---
+++ packages/pipe-nonblock/pipe_nonblock-0.4.tar.gz/pipe_nonblock-0.4/setup.py
@@ -0,0 +1,28 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+config = {
+    'description': 'Non-blocking multiprocessing pipe',
+    'author': 'Maksim Afanasevsky',
+    'url': 'https://github.com/maxtwen/pipe-nonblock',
+    'download_url': 'https://github.com/maxtwen/pipe-nonblock',
+    'author_email': 'maxtwen1@gmail.com',
+    'version': '0.4',
+    'install_requires': [''],
+    'packages': ['pipe_nonblock'],
+    'scripts': [],
+    'name': 'pipe_nonblock',
+    'long_description': read('README.md'),
+    'long_description_content_type': 'text/markdown'
+}
+
+setup(**config)
