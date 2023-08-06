feda7591ed93310b4aa44e0fd2314a5a00cd1bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:49 +0000

    Add package phidl/1.0.0/phidl-1.0.0.tar.gz

---
+++ packages/phidl/phidl-1.0.0.tar.gz/phidl-1.0.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+install_requires=[
+   'gdspy>=1.3.1',
+   'numpy<=1.15.4',  # note: 1.16.0 exists, but there is a name change that breaks scikit-image
+   'matplotlib',
+   'pyyaml',
+   'scikit-image>=0.11',
+   'webcolors',
+]
+
+setup(name='phidl',
+      version='1.0.0',
+      description='PHIDL',
+      install_requires=install_requires,
+      author='Adam McCaughan',
+      author_email='amccaugh@gmail.com',
+      packages=['phidl'],
+      py_modules=['phidl.geometry', 'phidl.routing', 'phidl.utilities'],
+      package_dir = {'phidl': 'phidl'},
+     )
