967728be8a39dec6aeca19c0c7274252db91aa9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:33 +0000

    Add package mixlight/0.4/mixlight-0.4-py3-none-any.whl

---
+++ packages/mixlight/mixlight-0.4-py3-none-any.whl/mixlight/entities/__init__.py
@@ -0,0 +1,3 @@
+from .entities import mixlight_Output
+
+
---
+++ packages/mixlight/mixlight-0.4-py3-none-any.whl/mixlight/entities/entity_bina.py
@@ -0,0 +1,56 @@
+
+
+import logging
+
+PLATFORM = 'binary_sensor'
+
+PAYLOAD_CONFIG = '{"name": "%s", "unique_id": "%s.%s", "device_class": "motion" }'
+PAYLOAD_STATE = '%s'
+
+
+class entity_Bina():
+
+  def __init__(self, id, addr, nr, name, type, linker):
+    self.id = id
+    self.addr = addr
+    self.number = nr
+    self.name = name
+    self.linker = linker
+    self.topic_config = linker.buildTopic(PLATFORM, id, "config")
+    self.topic_state = linker.buildTopic(PLATFORM, id, "state")
+    
+  def create(self):
+    if self.linker.client is None:
+      return
+    payload = PAYLOAD_CONFIG % (self.name, PLATFORM, self.id)
+    rc = self.linker.client.publish(self.topic_config, payload, 1, True)
+    rc.wait_for_publish()
+    self.linker.client.will_set(self.topic_config, None, 1, True)
+    print (self.id, ' CREATE: ', payload)
+    self.write()
+    
+  def delete(self):
+    if self.linker.client is None:
+      return
+    rc = self.linker.client.publish(self.topic_config, None, 1, True)
+    rc.wait_for_publish()
+    print (self.id, ' DELETE: ')
+    
+  def get(self):
+    return self.linker.boards[self.addr].buttons[self.number]  
+    
+  def set(self, *data):
+    pass
+
+  def read(self, topic, values):
+    pass
+
+  def write(self):
+    if self.linker.client is None:
+      return
+    payload = "OFF"
+    if self.get() > 0:
+      payload = "ON"
+    self.linker.client.publish(self.topic_state, payload, 1, True)
+        
+        
