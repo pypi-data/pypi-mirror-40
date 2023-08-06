493b1d6cfad5951a9912c0c6a2a9eef34d1e5c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:09 +0000

    Add package py-evm/0.2.0a39/py_evm-0.2.0a39-py3-none-any.whl

---
+++ packages/py-evm/py_evm-0.2.0a39-py3-none-any.whl/eth/beacon/state_machines/forks/serenity/blocks.py
@@ -0,0 +1,12 @@
+from eth.beacon.types.blocks import (
+    BeaconBlock,
+    BeaconBlockBody,
+)
+
+
+class SerenityBeaconBlockBody(BeaconBlockBody):
+    pass
+
+
+class SerenityBeaconBlock(BeaconBlock):
+    pass
