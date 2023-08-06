c7839edd436e65a99884ebf1e248286401b0d8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:40 +0000

    Add package django-lazypage/0.3.3/django-lazypage-0.3.3.tar.gz

---
+++ packages/django-lazypage/django-lazypage-0.3.3.tar.gz/django-lazypage-0.3.3/lazypage/views.py
@@ -0,0 +1,29 @@
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from lazypage.settings import lazypage_settings
+from lazypage.utils import get_store_client
+
+
+store_client = get_store_client(decode_responses=False)
+expired_seconds = lazypage_settings.EXPIRED_SECONDS
+
+
+def loading(request, page_id):
+    url = store_client.get(page_id + ':url')
+    origin_url = '&'.join(url.split('&')[:-1])
+    if url:
+        url = url.decode()
+        response = store_client.get(url + ':response')
+        assert response, 'response cannot be null in this moment, please check!'
+    
+        if len(response) == 6:
+            # page is loading
+            ttl = store_client.ttl(page_id + ':url')
+            polling_seconds = lazypage_settings.POLLING_SECONDS
+        else:
+            # page has loaded
+            return HttpResponseRedirect(url)
+
+    return render(request, 'lazypage/loading.html', locals())
