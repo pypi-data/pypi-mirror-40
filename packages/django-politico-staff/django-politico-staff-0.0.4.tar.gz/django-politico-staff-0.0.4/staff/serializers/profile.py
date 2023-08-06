55a1f798b20f6f19861751be1b3d50bebb6a4ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:46 +0000

    Add package django-politico-staff/0.0.4/django-politico-staff-0.0.4.tar.gz

---
+++ packages/django-politico-staff/django-politico-staff-0.0.4.tar.gz/django-politico-staff-0.0.4/staff/serializers/profile.py
@@ -0,0 +1,18 @@
+from rest_framework.serializers import ModelSerializer
+
+from staff.models import Profile
+
+
+class ProfileSerializer(ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = (
+            "id",
+            "politico_title",
+            "politico_author_page",
+            "slack_api_id",
+            "slack_image",
+            "google_email",
+            "google_display_name",
+            "twitter_handle",
+        )
