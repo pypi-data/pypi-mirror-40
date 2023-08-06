09c0d992034198aebb1b5274c42176e2cd9cd254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:50 +0000

    Add package pytoolz/0.1/pytoolz-0.1.tar.gz

---
+++ packages/pytoolz/pytoolz-0.1.tar.gz/pytoolz-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='pytoolz',
+    version='0.1',
+    packages=find_packages(),
+    author="Andrea La Scola",
+    author_email="andrealascola@gmail.com",
+    url="https://github.com/andrea-lascola/Pytoolz",
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description="",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
