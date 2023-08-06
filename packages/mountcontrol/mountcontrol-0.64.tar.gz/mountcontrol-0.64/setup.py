baa6de7b6342bd94687304a20932829aba0c2c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:07 +0000

    Add package mountcontrol/0.64/mountcontrol-0.64.tar.gz

---
+++ packages/mountcontrol/mountcontrol-0.64.tar.gz/mountcontrol-0.64/setup.py
@@ -0,0 +1,41 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+# MOUNTCONTROL
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+# Python  v3.6.5
+#
+# Michael Würtenberger
+# (c) 2018
+#
+# Licence APL2.0
+#
+############################################################
+# standard libraries
+from setuptools import setup
+# external packages
+# local imports
+from mountcontrol.mount import Mount
+
+
+setup(
+    name='mountcontrol',
+    version=Mount.version,
+    packages=[
+        'mountcontrol',
+        'mountcontrol.test',
+    ],
+    python_requires='~=3.6.7',
+    install_requires=[
+        'PyQt5==5.11.3',
+        'numpy==1.15.4',
+        'skyfield==1.9',
+    ],
+    url='https://github.com/mworion/mountcontrol',
+    license='APL 2.0',
+    author='mw',
+    author_email='michael@wuertenberger.org',
+    description='tooling for a 10micron mount',
+)
