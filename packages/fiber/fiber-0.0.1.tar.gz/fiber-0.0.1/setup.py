90499a37161c1dde2df21f06cbdad3b26599f9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:35 +0000

    Add package fiber/0.0.1/fiber-0.0.1.tar.gz

---
+++ packages/fiber/fiber-0.0.1.tar.gz/fiber-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name='fiber',
+  version='0.0.1',
+  author='Unknown',
+  author_email='vipcalio@gmail.com',
+  url='https://github.com',
+  license="MIT",
+  long_description="yet another multi threding library",
+  packages=['fiber'],
+  scripts=[]
+)
