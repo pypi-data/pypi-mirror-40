1349eba1d2828c7e72423d54742006cf99cfc6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:06 +0000

    Add package pymc3-models/1.2.1/pymc3_models-1.2.1.tar.gz

---
+++ packages/pymc3-models/pymc3_models-1.2.1.tar.gz/pymc3_models-1.2.1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+with open('AUTHORS.txt') as a:
+    # reSt-ify the authors list
+    authors = ''
+    for author in a.read().split('\n'):
+        authors += '| '+author+'\n'
+
+with open('LICENSE') as l:
+    license = l.read()
+
+with open('pymc3_models/_version.py') as version_file:
+    exec(version_file.read())
+
+with open('README.md') as r:
+    readme = r.read()
+
+
+setup(
+    name='pymc3_models',
+    version=__version__,
+    description='Custom PyMC3 models built on top of the scikit-learn API',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Nicole Carlson',
+    author_email='nicole@parsingscience.com',
+    url='https://github.com/parsing-science/pymc3_models',
+    license=license,
+    packages=find_packages(),
+    package_data={'docs': ['*']},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'joblib',
+        'matplotlib',
+        'numpy',
+        'pandas>=0.19',
+        'pymc3>=3.3',
+        'scipy',
+        'seaborn',
+        'sklearn'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+    ]
+)
