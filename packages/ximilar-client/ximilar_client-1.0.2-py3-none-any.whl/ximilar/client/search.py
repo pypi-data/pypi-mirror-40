6674cb7b27db6b6a9b3660a934e9e5332b03fdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:50 +0000

    Add package ximilar-client/1.0.2/ximilar_client-1.0.2-py3-none-any.whl

---
+++ packages/ximilar-client/ximilar_client-1.0.2-py3-none-any.whl/ximilar/client/search.py
@@ -0,0 +1,48 @@
+from ximilar.client import RestClient
+from ximilar.client.constants import RECORDS
+
+SIMILARITY_PHOTOS = 'similarity/photos/v2/'
+SIMILARITY_PHOTOS = 'similarity/products/v2/'
+
+SMART_INSERT_ENDPOINT = 'smart/v2/product/insert'
+SMART_SEARCH_ENDPOINT = 'smart/v2/product/search'
+SMART_DETECT_ENDPOINT = 'smart/v2/product/detect'
+SMART_SEARCH_OBJ_ENDPOINT = 'smart/v2/product/search_by_object'
+
+
+class Similarity(RestClient):
+    def insert(self):
+        raise NotImplementedError
+
+    def search(self, records, collection):
+        raise NotImplementedError
+
+    def delete(self, records, collection):
+        raise NotImplementedError
+
+    def random(self, records, collection):
+        raise NotImplementedError
+
+    def get(self):
+        raise NotImplementedError
+
+
+class SmartSearchClient(RestClient):
+    def __init__(self, token, collection):
+        super(SmartSearchClient, self).__init__(token)
+
+        self.max_size = 1000
+        self.headers['collection'] = collection
+
+    def insert(self):
+        raise NotImplementedError
+
+    def search(self, records, collection):
+        raise NotImplementedError
+
+    def detect(self, records):
+        records = self.preprocess_records(records)
+        return self.post(SMART_DETECT_ENDPOINT, data={RECORDS: records})
+
+    def search_by_object(self, records, collection):
+        raise NotImplementedError
