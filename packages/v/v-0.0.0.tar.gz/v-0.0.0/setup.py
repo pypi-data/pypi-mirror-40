2761483e0732edb4a4518026e14e068610525056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:13 +0000

    Add package v/0.0.0/v-0.0.0.tar.gz

---
+++ packages/v/v-0.0.0.tar.gz/v-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="v",
+    version="0.0.0",
+    description='A placeholder for a future project.',
+    author='Pradyun Gedam',
+    author_email='pradyunsg@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ],
+    packages=find_packages()
+)
