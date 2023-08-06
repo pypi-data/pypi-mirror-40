2867277c76d9b07d0a3f3fff5e422e910027a7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:32 +0000

    Add package nonlinear-optimizations/0.1/nonlinear_optimizations-0.1.tar.gz

---
+++ packages/nonlinear-optimizations/nonlinear_optimizations-0.1.tar.gz/nonlinear_optimizations-0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='nonlinear_optimizations',
+                 version='0.1',
+                 description='Nonlinear-optimization algorithms',
+                 url='https://github.com/aostrun/Nonlinear-optimizations',
+                 author='Andrijan Ostrun',
+                 author_email='andrijan.ostrun@gmail.com',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
