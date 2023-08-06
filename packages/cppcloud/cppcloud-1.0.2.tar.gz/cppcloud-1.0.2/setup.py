6902e272f9886c489c263e9cd551d23689cfca8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:31 +0000

    Add package cppcloud/1.0.2/cppcloud-1.0.2.tar.gz

---
+++ packages/cppcloud/cppcloud-1.0.2.tar.gz/cppcloud-1.0.2/setup.py
@@ -0,0 +1,45 @@
+#! /usr/bin/pytdon
+# -*- coding:utf-8 -*- 
+
+
+import sys
+
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+with open('README.md') as fp:
+    readme = fp.read()
+
+with open('LICENSE') as fp:
+    license = fp.read()
+
+
+setup(
+    name='cppcloud',
+    version='1.0.2',
+    author = 'valueho',
+    author_email = "hjl_mvp@126.com",
+    description = 'CppCloud python3 客户端sdk',
+    long_description=readme,
+    license=license,
+    platforms=['any'],
+    python_requires = '>=3',
+
+    packages = ['cppcloud'],
+    #package_dir = {'xcppcloud': 'cppcloud'},
+
+    include_package_data=True,
+    install_requires = ['requests'],
+    
+    # 程序的所属分类列表
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License (GPL)"
+    ],
+    zip_safe=False
+
+)
