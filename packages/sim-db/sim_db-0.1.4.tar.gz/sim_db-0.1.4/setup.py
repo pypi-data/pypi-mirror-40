5e3bcc0d72e930c9f6208f4a7bba7dc0b688f0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:47 +0000

    Add package sim-db/0.1.4/sim_db-0.1.4.tar.gz

---
+++ packages/sim-db/sim_db-0.1.4.tar.gz/sim_db-0.1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from sim_db import __version__
+
+setuptools.setup(
+    name='sim_db',
+    version=__version__,
+    scripts=['sim_db/sim_db', 'sim_db/sdb', 'sim_db/sim_db_cd.sh'],
+    author='Hakon Austlid Tasken',
+    author_email='hakon.tasken@gmail.com',
+    description='A database for simulation parameters.',
+    long_description='A command line tool and set of functions for conveniently running a large number of simulations with different parameter values, while keeping track of these all simulation parameters and results along with metadata in a database for you.',
+    url='http://sim-db.readthedocs.io',
+    packages=setuptools.find_packages(),
+    classifiers=[
+     "Programming Language :: Python",
+     "Programming Language :: Python :: 2",
+     "Programming Language :: Python :: 3",
+     "Programming Language :: Python",
+     "License :: OSI Approved :: MIT License",
+    ],
+    include_package_data=True,
+)
