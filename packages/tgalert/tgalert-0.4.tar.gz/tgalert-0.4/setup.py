0be659d09b1d5b45cd9dbf3990c174d3a62d75e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:38 +0000

    Add package tgalert/0.4/tgalert-0.4.tar.gz

---
+++ packages/tgalert/tgalert-0.4.tar.gz/tgalert-0.4/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+setup(name='tgalert', description='Package for receiving Telegram notifications during code execution (i.e. training).', version=0.4,
+author="David Donahue", author_email="djd1283@gmail.com", url="https://github.com/djd1283/tgalert.git", scripts=['tg_alert.py'])
