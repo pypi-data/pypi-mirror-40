3ad6cca63fb838d7117cb00f8848776b61a7639d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:13 +0000

    Add package django-comuni-italiani/1.4.1/django_comuni_italiani-1.4.1-py3-none-any.whl

---
+++ packages/django-comuni-italiani/django_comuni_italiani-1.4.1-py3-none-any.whl/comuni_italiani/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+class ComuniItalianiConfig(AppConfig):
+    name = 'comuni_italiani'
+    verbose_name = "Comuni Italiani"
---
+++ packages/django-comuni-italiani/django_comuni_italiani-1.4.1-py3-none-any.whl/comuni_italiani/autocomplete/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class ComuniItalianiAutocompleteConfig(AppConfig):
+    name = 'comuni_italiani.autocomplete'
+    verbose_name = "Comuni Italiani Autocomplete"
