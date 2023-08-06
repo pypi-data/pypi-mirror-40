26c01f68baf2d8b664f00edc6d812513a4525e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:53 +0000

    Add package ghtrending/0.2.1/ghtrending-0.2.1.tar.gz

---
+++ packages/ghtrending/ghtrending-0.2.1.tar.gz/ghtrending-0.2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+requires = [
+    'lxml',
+    'requests',
+]
+
+setup(
+    name='ghtrending',
+    version='0.2.1',
+    author='chinsyo',
+    author_email='chinsyo@sina.cn',
+    license='MIT',
+    description='Github Trending Explorer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chinsyo/ghtrending",
+    packages=find_packages(),
+    package_dir={'ghtrending': 'ghtrending'},
+    package_data={'': ['LICENSE', 'NOTICE']},
+    include_package_data=True,
+    install_requires=requires,
+    zip_safe=False
+)
