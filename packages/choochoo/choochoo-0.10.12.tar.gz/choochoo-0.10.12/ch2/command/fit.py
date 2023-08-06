8eefe2e4d6cd9117ba9803e201df657d7f9527c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:58 +0000

    Add package choochoo/0.10.12/choochoo-0.10.12.tar.gz

---
+++ packages/choochoo/choochoo-0.10.12.tar.gz/choochoo-0.10.12/ch2/command/fit.py
@@ -0,0 +1,44 @@
+
+from .args import PATH, FORMAT, ALL_FIELDS, ALL_MESSAGES, AFTER, LIMIT, WARN, GREP, MESSAGE, NAME, NOT, MATCH, \
+    WIDTH, NO_VALIDATE, INTERNAL
+from ..fit.profile.profile import read_fit
+from ..fit.summary import summarize
+
+
+def fit(args, log, db):
+    '''
+## fit
+
+    > ch2 fit PATH [PATH ...]
+
+Print the contents of fit files.
+
+The format and details displayed can be selected with --records,
+--tables, --grep, --messages, --fields, and --csv.
+
+For full options see `ch2 fit -h`.
+
+Note: When using bash use `shopt -s globstar` to enable ** globbing.
+
+### Examples
+
+    > ch2 -v 0 fit --records ride.fit
+
+Will print the contents of the file to stdout (use `-v 0` to suppress logging
+or redirect stderr elsewhere).
+
+    > ch2 -v 0 fit --grep '.*:sport=cycling' --match 0 --name directory/**/*.fit
+
+Will list file names that contain cycling data.
+
+    > ch2 fit --grep PATTERN -- FILE
+
+You may need a `--` between patterns and file paths so that the argument parser can decide where patterns
+finish and paths start.
+    '''
+    for file_path in args[PATH]:
+        summarize(log, args[FORMAT], read_fit(log, file_path),
+                  all_fields=args[ALL_FIELDS], all_messages=args[ALL_MESSAGES], internal=args[INTERNAL],
+                  after=args[AFTER], limit=args[LIMIT], messages=args[MESSAGE], warn=args[WARN],
+                  grep=args[GREP], name_file=file_path if args[NAME] else None, invert=args[NOT], match=args[MATCH],
+                  no_validate=args[NO_VALIDATE], width=args[WIDTH])
---
+++ packages/choochoo/choochoo-0.10.12.tar.gz/choochoo-0.10.12/ch2/data/data_frame.py
@@ -0,0 +1,151 @@
+
+from collections import defaultdict
+
+import pandas as pd
+from sqlalchemy import inspect, select
+from sqlalchemy.sql.functions import coalesce
+
+from ..squeal import StatisticName, StatisticJournal, StatisticJournalInteger, \
+    StatisticJournalFloat, StatisticJournalText, Interval, StatisticMeasure, Source
+from ..squeal.database import connect
+
+# because this is intended to be called from jupyter we hide the log here
+# other callers can use these routines by calling set_log() first.
+LOG = [None]
+
+
+def df(query):
+    # https://stackoverflow.com/questions/29525808/sqlalchemy-orm-conversion-to-pandas-dataframe
+    return pd.read_sql(query.statement, query.session.bind)
+
+
+def session(*args):
+    ns, log, db = connect(args)
+    set_log(log)
+    return db.session()
+
+
+def get_log():
+    if not LOG[0]:
+        raise Exception('Create session first (or call set_log from within python)')
+    return LOG[0]
+
+
+def set_log(log):
+    LOG[0] = log
+
+
+def _collect_statistics(s, names):
+    if not names:
+        names = ['%']
+    statistic_ids, statistic_names = set(), set()
+    for name in names:
+        for statistic_name in s.query(StatisticName). \
+                filter(StatisticName.name.like(name)).all():
+            statistic_ids.add(statistic_name.id)
+            statistic_names.add(statistic_name.name)
+    return statistic_names, statistic_ids
+
+
+def _build_statistic_journal_query(statistic_ids, start, finish, owner, constraint, source_ids, schedule):
+
+    # use more efficient expression interface and exploit the fact that
+    # alternative journal types will be null in an outer join.
+
+    sj = inspect(StatisticJournal).local_table
+    sn = inspect(StatisticName).local_table
+    sji = inspect(StatisticJournalInteger).local_table
+    sjf = inspect(StatisticJournalFloat).local_table
+    sjt = inspect(StatisticJournalText).local_table
+    inv = inspect(Interval).local_table
+
+    q = select([sn.c.name, sj.c.time, coalesce(sjf.c.value, sji.c.value, sjt.c.value)]). \
+        select_from(sj.join(sn).outerjoin(sjf).outerjoin(sji).outerjoin(sjt)). \
+        where(sn.c.id.in_(statistic_ids))
+    if start:
+        q = q.where(sj.c.time >= start)
+    if finish:
+        q = q.where(sj.c.time <= finish)
+    if owner:
+        q = q.where(sn.c.owner == owner)
+    if constraint:
+        q = q.where(sn.c.constraint == constraint)
+    if source_ids is not None:  # avoid testing DataFrame sequences as bools
+        # int() to convert numpy types
+        q = q.where(sj.c.source_id.in_(int(id) for id in source_ids))
+    if schedule:
+        q = q.join(inv).where(inv.c.schedule == schedule)
+    q = q.order_by(sj.c.time)
+    return q
+
+
+class MissingData(Exception): pass
+
+
+def statistics(s, *statistics,
+               start=None, finish=None, owner=None, constraint=None, source_ids=None, schedule=None,):
+    statistic_names, statistic_ids = _collect_statistics(s, statistics)
+    q = _build_statistic_journal_query(statistic_ids, start, finish, owner, constraint, source_ids, schedule)
+    data, times, err_cnt = defaultdict(list), [], defaultdict(lambda: 0)
+
+    def pad():
+        nonlocal data, times
+        n = len(times)
+        for name in statistic_names:
+            if len(data[name]) != n:
+                err_cnt[name] += 1
+                if err_cnt[name] <= 1:
+                    get_log().warning('Missing %s at %s (single warning)' % (name, times[-1]))
+                data[name].append(None)
+
+    for name, time, value in s.connection().execute(q):
+        if times and times[-1] != time:
+            pad()
+        if not times or times[-1] != time:
+            times.append(time)
+        if len(data[name]) >= len(times):
+            raise Exception('Duplicate data for %s at %s ' % (name, time) +
+                            '(you may need to specify more constraints to make the query unique)')
+        data[name].append(value)
+    pad()
+    return pd.DataFrame(data, index=times)
+
+
+def statistic_quartiles(s, *statistics, schedule='m',
+                        start=None, finish=None, owner=None, constraint=None, source_ids=None):
+    statistic_names, statistic_ids = _collect_statistics(s, statistics)
+    q = s.query(StatisticMeasure). \
+        join(StatisticJournal, StatisticMeasure.statistic_journal_id == StatisticJournal.id). \
+        join(StatisticName, StatisticJournal.statistic_name_id == StatisticName.id). \
+        join(Source, StatisticJournal.source_id == Source.id). \
+        filter(StatisticName.id.in_(statistic_ids)). \
+        filter(StatisticMeasure.quartile != None)
+    if start:
+        q = q.filter(StatisticJournal.time >= start)
+    if finish:
+        q = q.filter(StatisticJournal.time <= finish)
+    if owner:
+        q = q.filter(StatisticName.owner == owner)
+    if constraint:
+        q = q.filter(StatisticName.constraint == constraint)
+    if source_ids is not None:
+        q = q.filter(StatisticJournal.source_id.in_(source_ids))
+    if schedule:
+        q = q.join((Interval, StatisticMeasure.source_id == Interval.id)). \
+            filter(Interval.schedule == schedule)
+    get_log().debug(q)
+    raw_data = defaultdict(lambda: defaultdict(lambda: [0] * 5))
+    for measure in q.all():
+        raw_data[measure.source.start][measure.statistic_journal.statistic_name.name][measure.quartile] = \
+            measure.statistic_journal.value
+    data, times = defaultdict(list), []
+    for time in sorted(raw_data.keys()):
+        times.append(time)
+        sub_data = raw_data[time]
+        for statistic in statistic_names:
+            if statistic in sub_data:
+                data[statistic].append(sub_data[statistic])
+            else:
+                data[statistic].append(None)
+    return pd.DataFrame(data, index=times)
+
