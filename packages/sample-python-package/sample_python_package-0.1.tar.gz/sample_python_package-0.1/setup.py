2f344f9cdfa7fb2ea0184f44a42ffa167fbd534e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:11 +0000

    Add package sample-python-package/0.1/sample_python_package-0.1.tar.gz

---
+++ packages/sample-python-package/sample_python_package-0.1.tar.gz/sample_python_package-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sample_python_package',
+      version='0.1',
+      description='Testing python package creation',
+      url='https://ghe.kst3.jp/contents-create-dev/sample_python_package',
+      author='Mashiyat Zaman',
+      author_email='Mashiyat_Zaman@r.recruit.co.jp',
+      license='RCO',
+      packages=['sample_python_package'],
+      zip_safe=False)
