2a724bea154e7c53206832dede05db6263807269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:50 +0000

    Add package Brill/0.1/Brill-0.1.tar.gz

---
+++ packages/Brill/Brill-0.1.tar.gz/Brill-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="Brill",
+      version="0.1",
+      description="The Brill API",
+      author="Etienne Posthumus",
+      author_email="posthumus@brill.com",
+      url="https://gitlab.com/brillpublishers",
+      py_modules=["brill", ],
+      classifiers=(
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent",
+      ),
+      )
