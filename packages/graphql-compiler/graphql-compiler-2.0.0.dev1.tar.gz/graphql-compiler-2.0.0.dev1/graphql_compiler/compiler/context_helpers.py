0acbd8ad27a28082dd2caf25ebcd667d8a40e39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:45 +0000

    Add package graphql-compiler/2.0.0.dev1/graphql-compiler-2.0.0.dev1.tar.gz

---
+++ packages/graphql-compiler/graphql-compiler-2.0.0.dev1.tar.gz/graphql-compiler-2.0.0.dev1/graphql_compiler/compiler/context_helpers.py
@@ -0,0 +1,32 @@
+# Copyright 2017-present Kensho Technologies, LLC.
+"""Helper functions for dealing with the frontend "context" object."""
+
+from ..exceptions import GraphQLCompilationError
+
+
+def is_in_fold_innermost_scope_scope(context):
+    """Return True if the current context is within a scope marked @fold."""
+    return 'fold_innermost_scope' in context
+
+
+def is_in_fold_scope(context):
+    """Return True if the current context is within a scope marked @fold."""
+    return 'fold' in context
+
+
+def is_in_optional_scope(context):
+    """Return True if the current context is within a scope marked @optional."""
+    return 'optional' in context
+
+
+def has_encountered_output_source(context):
+    """Return True if the current context has already encountered an @output_source directive."""
+    return 'output_source' in context
+
+
+def validate_context_for_visiting_vertex_field(parent_location, vertex_field_name, context):
+    """Ensure that the current context allows for visiting a vertex field."""
+    if is_in_fold_innermost_scope_scope(context):
+        raise GraphQLCompilationError(u'Traversing inside a @fold block after output is '
+                                      u'not supported! Parent location: {}, vertex field name: {}'
+                                      .format(parent_location, vertex_field_name))
