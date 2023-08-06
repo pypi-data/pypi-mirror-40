6d88c6d970d6f0e7680720d3dc8854ccde5a29ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:45 +0000

    Add package gpuview/0.3.4/gpuview-0.3.4.tar.gz

---
+++ packages/gpuview/gpuview-0.3.4.tar.gz/gpuview-0.3.4/gpuview/app.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+"""
+Web API of gpuview.
+
+@author Fitsum Gaim
+@url https://github.com/fgaim
+"""
+
+import os
+import json
+from datetime import datetime
+
+from bottle import Bottle, TEMPLATE_PATH, template, response
+
+from .utils import arg_parser, install_service
+from .core import my_gpustat, all_gpustats, add_host, remove_host, safe_zone, print_hosts
+
+
+app = Bottle()
+abs_path = os.path.dirname(os.path.realpath(__file__))
+abs_views_path = os.path.join(abs_path, 'views')
+TEMPLATE_PATH.insert(0, abs_views_path)
+
+EXCLUDE_SELF = False  # Do not report to `/gpustat` calls.
+
+
+@app.route('/')
+def index():
+    gpustats = all_gpustats()
+    now = datetime.now().strftime('Updated at %Y-%m-%d %H-%M-%S')
+    return template('index', gpustats=gpustats, update_time=now)
+
+
+@app.route('/gpustat', methods=['GET'])
+def report_gpustat():
+    """
+    Returns the gpustat of this host.
+        See `exclude-self` option of `gpuview start`.
+    """
+
+    def _date_handler(obj):
+        if hasattr(obj, 'isoformat'):
+            return obj.isoformat()
+        else:
+            raise TypeError(type(obj))
+
+    response.content_type = 'application/json'
+    if EXCLUDE_SELF:
+        resp = {'error': 'Excluded self!'}
+    else:
+        resp = my_gpustat()
+    return json.dumps(resp, default=_date_handler)
+
+
+def main():
+    parser = arg_parser()
+    args = parser.parse_args()
+
+    if 'start' == args.action:
+        safe_zone(args.safe_zone)
+        global EXCLUDE_SELF
+        EXCLUDE_SELF = args.exclude_self
+        app.run(host=args.host, port=args.port, debug=args.debug)
+    elif 'add' == args.action:
+        add_host(args.url, args.name)
+    elif 'remove' == args.action:
+        remove_host(args.url)
+    elif 'hosts' == args.action:
+        print_hosts()
+    elif 'service' == args.action:
+        print(args.safe_zone, args.exclude_self)
+        install_service(safe_zone=args.safe_zone, exclude_self=args.exclude_self)
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
