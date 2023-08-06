c7f5b25aabbbeaec78fab727427e6d42ed8bd73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:56 +0000

    Add package iduoliao-ml/1.0.118/iduoliao_ml-1.0.118-py2-none-any.whl

---
+++ packages/iduoliao-ml/iduoliao_ml-1.0.118-py2-none-any.whl/iduoliao_ml/test/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .. import es
+
+def test():
+	es.setEnv(False)
+	hits = es.searchStatisticsDataByBody('works_actions', {"query": {"term": {"vid": "ecbf3aa1599e140896e0f6c241d93681"}}, "size": 10000})
+	
+	dataDict = {}
+	for hit in hits:
+		dataDict[hit['_id']] = hit['_source']
+
+	es.setEnv(True)
+	es.updateStatisticsData('works_actions', dataDict)
