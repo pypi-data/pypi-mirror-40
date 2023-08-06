e936c61c9854aa341e78d0971bf18d37b7495bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:04 +0000

    Add package syslog-rfc5424-parser/0.2.0/syslog-rfc5424-parser-0.2.0.tar.gz

---
+++ packages/syslog-rfc5424-parser/syslog-rfc5424-parser-0.2.0.tar.gz/syslog-rfc5424-parser-0.2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="syslog-rfc5424-parser",
+    version="0.2.0",
+    author="James Brown",
+    author_email="jbrown@easypost.com",
+    url="https://github.com/easypost/syslog-rfc5424-parser",
+    description="Parser for RFC5424-compatible Syslog messages",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    license="ISC",
+    install_requires=[
+        'pyparsing>=2.3,<3.0',
+        'enum34'
+    ],
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: ISC License (ISCL)",
+    ]
+)
