f234854fb244203b0f36b7a6f3516f0149fd18e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:57 +0000

    Add package nester-kk87/1.0.0/nester-kk87-1.0.0.tar.gz

---
+++ packages/nester-kk87/nester-kk87-1.0.0.tar.gz/nester-kk87-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+		name = 'nester-kk87',
+		version = '1.0.0',
+		py_modules = ['nester'],
+		author = 'karthick',
+		author_email = 'karthickmurugadhas@gmail.com',
+		url = 'https://github.com/karthick87/scripts',
+		description = 'A simple printer of nested lists',
+)
