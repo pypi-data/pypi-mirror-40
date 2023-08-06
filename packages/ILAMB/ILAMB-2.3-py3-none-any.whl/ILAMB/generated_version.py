0e2b9f1e6b314243c3143541ac03b6ae521be6a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:52 +0000

    Add package ILAMB/2.3/ILAMB-2.3-py3-none-any.whl

---
+++ packages/ILAMB/ILAMB-2.3-py3-none-any.whl/ILAMB/generated_version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ILAMB SETUP.PY
+short_version = '2.3'
+version = '2.3'
+git_revision = '5a63f22169dcb07b61c5b4c56250d7be9f8cc56c'
+full_version = '2.3 (%(git_revision)s)' % {
+    'git_revision': git_revision}
+release = False
+if not release:
+    version = full_version
