52b50bfe290fafe6e2508110ba84b3fc5a12af5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:56 +0000

    Add package RESTEasyCLI/0.4.1/RESTEasyCLI-0.4.1-py3.5.egg

---
+++ packages/RESTEasyCLI/RESTEasyCLI-0.4.1-py3.5.egg/resteasycli/cmd/initialize.py
@@ -0,0 +1,21 @@
+import logging
+from cliff.command import Command
+
+from resteasycli.config import Config
+from resteasycli.config.template import ConfigTemplate
+from resteasycli.lib.workspace import WorkspaceTemplates
+
+
+class Init(Command):
+    'Initialize template files in current directory'
+
+    def get_parser(self, prog_name):
+        parser = super(Init, self).get_parser(prog_name)
+        parser.add_argument('-f', '--force', action='store_true')
+        parser.add_argument('-e', '--extension', help=('|'.join(Config.SUPPORTED_FILE_EXTENSIONS)),
+                default=Config.DEFAULT_FILE_EXTENSION)
+        return parser
+
+    def take_action(self, args):
+        ConfigTemplate.initialize(force=args.force)
+        WorkspaceTemplates.initialize(force=args.force, extension=args.extension)
