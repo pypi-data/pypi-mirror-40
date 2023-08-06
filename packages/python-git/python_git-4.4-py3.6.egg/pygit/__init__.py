d7167611ff6614677d4c9872badf0b4ca82c35ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:51 +0000

    Add package python-git/4.4/python_git-4.4-py3.6.egg

---
+++ packages/python-git/python_git-4.4-py3.6.egg/pygit/__init__.py
@@ -0,0 +1,10 @@
+"""Docstring"""
+
+from .__version__ import __version__
+from .app import (
+    cleanup, check_git_support, is_git_repo, initialize,
+    Commands, show_repos, load, load_multiple, pull, push, all_status
+)
+
+__all__ = ["all_status", "cleanup", "check_git_support", "is_git_repo", "initialize",
+           "Commands", "show_repos", "load", "load_multiple", "pull", "push"]
