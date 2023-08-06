9b4bea748106637d51aea70580363f87ac31128d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:44 +0000

    Add package asammdf/4.7.2/asammdf-4.7.2.tar.gz

---
+++ packages/asammdf/asammdf-4.7.2.tar.gz/asammdf-4.7.2/asammdf/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+""" asammdf is a parser and editor for ASAM MDF files """
+
+import logging
+
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+
+logger = logging.getLogger("asammdf")
+logger.setLevel(logging.INFO)
+logger.addFilter(NullHandler())
+
+from .blocks.mdf_v2 import MDF2
+from .blocks.mdf_v3 import MDF3
+from .blocks.mdf_v4 import MDF4
+from .mdf import MDF, SUPPORTED_VERSIONS
+from .signal import Signal
+from .version import __version__
+
+
+__all__ = ["__version__", "MDF", "MDF2", "MDF3", "MDF4", "Signal", "SUPPORTED_VERSIONS"]
