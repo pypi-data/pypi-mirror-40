e0f945a8eb0c8c123d783759949515415d90b0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:39 +0000

    Add package Telecontrol/1.6/Telecontrol-1.6.tar.gz

---
+++ packages/Telecontrol/Telecontrol-1.6.tar.gz/Telecontrol-1.6/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup #如果没有需要先安装
+
+setup(
+      name='Telecontrol',  #打包后的包文件名 
+      version='1.6',  #版本
+      description='a Telecontrol',   
+      author=' Mr.yan',   
+      license="Apache-2.0",
+      author_email=' mryan050415@gmail.com',   
+      url=' https://github.com/SuperSystemStudio/TelecontrolCodeRepairwoman',   
+      py_modules=['TelecontrolCodeRepairwoman'],  #与前面的新建文件名一致 
+      entry_points={
+        'console_scripts': [
+            'Telecontrol start = main:main',#pip安装完成后可使用demo命令调用demo下的main方法
+        ],
+      },
+)
