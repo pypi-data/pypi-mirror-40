87227dc775340b493acbfc4620e6bb6226965001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:19 +0000

    Add package k-vision/0.1.4/k_vision-0.1.4.tar.gz

---
+++ packages/k-vision/k_vision-0.1.4.tar.gz/k_vision-0.1.4/k_vision/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Import our names here.
+"""
+
+
+__author__ = "Jakrin Juangbhanich"
+__email__ = "juangbhanich.k@gmail.com"
+__version__ = "0.1.4"
+
+
+from k_vision.visual import grid
